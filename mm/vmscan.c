52244e057091ce5f64a5fb251a2b37ad572ccd35
Yang Shi <yang.shi@linux.alibaba.com>
2019-08-02 02:01:40 +0800

    anolis: mm: memcontrol: treat memcg wmark reclaim work as kswapd
    ANBZ: #34087
    
    Since background water mark reclaim is scheduled by workqueue, it could
    do more work than direct reclaim, i.e. write out dirty page, etc.
    
    So, add PF_KSWAPD flag, so that current_is_kswapd() would return true
    for memcg background reclaim.  The condition "current_is_kswapd() &&
    !global_reclaim(sc)" is good enough to tell current is global kswapd or
    memcg background reclaim.
    
    And, kswapd is not allowed to break memory.low protection for now, memcg
    kswapd should not break it either.
    
    [Zelin Deng: Partial shrink returns first rather than async reclaim.]
    
    Reviewed-by: Gavin Shan <shan.gavin@linux.alibaba.com>
    Reviewed-by: Xunlei Pang <xlpang@linux.alibaba.com>
    Signed-off-by: Yang Shi <yang.shi@linux.alibaba.com>
    Signed-off-by: zhongjiang-ali <zhongjiang-ali@linux.alibaba.com>
    Signed-off-by: Kun(llfl) <llfl@linux.alibaba.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Reviewed-by: Xu Yu <xuyu@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/4154
    Signed-off-by: Zelin Deng <zelin.deng@linux.alibaba.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -6149,6 +6149,16 @@ static void shrink_node_memcgs(pg_data_t *pgdat, struct scan_control *sc)
 			mem_cgroup_iter_break(target_memcg, memcg);
 			break;
 		}
+
+		/*
+		 * Memcg background reclaim would break iter once water
+		 * mark is satisfied.
+		 */
+		if (cgroup_reclaim(sc) && current_is_kswapd() &&
+		     is_wmark_ok(target_memcg, false)) {
+			mem_cgroup_iter_break(target_memcg, memcg);
+			break;
+		}
 	} while ((memcg = mem_cgroup_iter(target_memcg, memcg, partial)));
 }
 
@@ -6191,7 +6201,7 @@ again:
 	if (nr_node_reclaimed)
 		reclaimable = true;
 
-	if (current_is_kswapd()) {
+	if (current_is_kswapd() && !cgroup_reclaim(sc)) {
 		/*
 		 * If reclaim is isolating dirty pages under writeback,
 		 * it implies that the long-lived page allocation rate
@@ -6474,6 +6484,10 @@ retry:
 		__count_zid_vm_events(ALLOCSTALL, sc->reclaim_idx, 1);
 
 	do {
+		if (current_is_kswapd() && cgroup_reclaim(sc) &&
+		    is_wmark_ok(sc->target_mem_cgroup, false))
+			break;
+
 		if (!sc->proactive)
 			vmpressure_prio(sc->gfp_mask, sc->target_mem_cgroup,
 					sc->priority);
@@ -6545,8 +6559,12 @@ retry:
 		goto retry;
 	}
 
-	/* Untapped cgroup reserves?  Don't OOM, retry. */
-	if (sc->memcg_low_skipped) {
+	/*
+	 * Untapped cgroup reserves?  Don't OOM, retry.
+	 *
+	 * Memcg kswapd should not break low protection.
+	 */
+	if (sc->memcg_low_skipped && !current_is_kswapd()) {
 		sc->priority = initial_priority;
 		sc->force_deactivate = 0;
 		sc->memcg_low_reclaim = 1;
