6904d515b9cb3212ee9b5447dac4eadd7db701be
Xu Yu <xuyu@linux.alibaba.com>
2019-09-01 19:10:49 +0800

    anolis: mm,memcg: record latency of memcg wmark reclaim
    ANBZ: #11973
    
    to #32655467
    
    The memcg background async page reclaim, a.k.a, memcg kswapd, is
    implemented with a dedicated unbound workqueue in 4.19, eliminating
    original kthreads.
    
    However, memcg kswapd will run too frequently, resulting in high
    overhead, page cache thrashing, frequent dirty page writeback, etc., due
    to improper memcg memory.wmark_ratio, unreasonable memcg memor capacity,
    or even abnormal memcg memory usage.
    
    We need to find out the problematic memcg(s) where memcg kswapd
    introduces significant overhead.
    
    This records the latency of each run of memcg kswapd work, and then
    aggregates into the exstat of per memcg.
    
    Signed-off-by: Xu Yu <xuyu@linux.alibaba.com>
    Reviewed-by: Xunlei Pang <xlpang@linux.alibaba.com>
    Signed-off-by: zhongjiang-ali <zhongjiang-ali@linux.alibaba.com>
    Signed-off-by: Kun(llfl) <llfl@linux.alibaba.com>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -2561,6 +2561,8 @@ static void reclaim_wmark(struct mem_cgroup *memcg)
 {
 	long nr_pages;
 	unsigned long pflags;
+	struct mem_cgroup *iter;
+	u64 start, duration;
 
 	if (is_wmark_ok(memcg, false))
 		return;
@@ -2572,9 +2574,23 @@ static void reclaim_wmark(struct mem_cgroup *memcg)
 
 	nr_pages = max_t(unsigned long, SWAP_CLUSTER_MAX, nr_pages);
 
+	/*
+	 * Typically, we would like to record the actual cpu% of reclaim_wmark
+	 * work, excluding any sleep/resched time.  However, currently we just
+	 * simply record the whole duration of reclaim_wmark work for the
+	 * overhead-accuracy trade-off.
+	 */
+	start = ktime_get_ns();
 	psi_memstall_enter(&pflags);
 	try_to_free_mem_cgroup_pages(memcg, nr_pages, GFP_KERNEL, true);
 	psi_memstall_leave(&pflags);
+	duration = ktime_get_ns() - start;
+
+	css_get(&memcg->css);
+	for (iter = memcg; iter; iter = parent_mem_cgroup(iter))
+		this_cpu_add(iter->exstat_cpu->item[MEMCG_WMARK_RECLAIM],
+			     duration);
+	css_put(&memcg->css);
 }
 
 static void wmark_work_func(struct work_struct *work)
@@ -4448,6 +4464,28 @@ static void memcg1_stat_format(struct mem_cgroup *memcg, struct seq_buf *s)
 #endif
 }
 
+static u64 memcg_exstat_gather(struct mem_cgroup *memcg,
+			       enum memcg_exstat_item idx)
+{
+	u64 sum = 0;
+	int cpu;
+
+	for_each_online_cpu(cpu)
+		sum += per_cpu_ptr(memcg->exstat_cpu, cpu)->item[idx];
+
+	return sum;
+}
+
+static int memcg_exstat_show(struct seq_file *m, void *v)
+{
+	struct mem_cgroup *memcg = mem_cgroup_from_css(seq_css(m));
+
+	seq_printf(m, "wmark_reclaim_work_ms %llu\n",
+		   memcg_exstat_gather(memcg, MEMCG_WMARK_RECLAIM) >> 20);
+
+	return 0;
+}
+
 static u64 mem_cgroup_swappiness_read(struct cgroup_subsys_state *css,
 				      struct cftype *cft)
 {
@@ -5497,6 +5535,10 @@ static struct cftype mem_cgroup_legacy_files[] = {
 		.seq_show = memory_stat_show,
 	},
 	{
+		.name = "exstat",
+		.seq_show = memcg_exstat_show,
+	},
+	{
 		.name = "wmark_ratio",
 		.flags = CFTYPE_NOT_ON_ROOT,
 		.seq_show = memory_wmark_ratio_show,
@@ -5793,6 +5835,7 @@ static void __mem_cgroup_free(struct mem_cgroup *memcg)
 		free_mem_cgroup_per_node_info(memcg, node);
 	kfree(memcg->vmstats);
 	free_percpu(memcg->vmstats_percpu);
+	free_percpu(memcg->exstat_cpu);
 	kfree(memcg);
 }
 
@@ -5827,6 +5870,10 @@ static struct mem_cgroup *mem_cgroup_alloc(void)
 	memcg->vmstats_percpu = alloc_percpu_gfp(struct memcg_vmstats_percpu,
 						 GFP_KERNEL_ACCOUNT);
 	if (!memcg->vmstats_percpu)
+		goto fail;
+
+	memcg->exstat_cpu = alloc_percpu(struct mem_cgroup_exstat_cpu);
+	if (!memcg->exstat_cpu)
 		goto fail;
 
 	for_each_node(node)
