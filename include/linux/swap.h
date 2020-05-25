a21f49427d9cd1a08c5dc28dbe047ae1e95747e3
zhongjiang-ali <zhongjiang-ali@linux.alibaba.com>
2020-05-26 01:34:55 +0800

    anolis: mm: kidled: isolate slab scan with page scan
    ANBZ: #22925
    ANBZ: #1702
    
    Currently, page scan only use 2/HZ to scan the part of memory to
    avoid the cpu load. but As to slab scan, Generally, It will take
    more time to scan an round when there are a lot of slab objects
    in the system. Idlemd will enable the slab scan and acceptable
    for cpu load for a while. Meanwhile, we should not scale up the
    scan period. Because it will disturb the page scan.
    
    Reviewed-by: Xu Yu <xuyu@linux.alibaba.com>
    Signed-off-by: zhongjiang-ali <zhongjiang-ali@linux.alibaba.com>
    Signed-off-by: Rongwei Wang <rongwei.wang@linux.alibaba.com>
    Acked-by: Gang Deng <gavin.dg@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/565
    Signed-off-by: Weilin Tong <tongweilin@linux.alibaba.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5537

--- include/linux/swap.h
+++ include/linux/swap.h
@@ -438,7 +438,8 @@ extern unsigned long mem_cgroup_shrink_node(struct mem_cgroup *mem,
 						unsigned long *nr_scanned);
 extern unsigned long shrink_all_memory(unsigned long nr_pages);
 #ifdef CONFIG_KIDLED
-extern void kidled_scan_slab(int nid, struct mem_cgroup *memcg);
+extern void kidled_scan_slab(int nid, struct mem_cgroup *memcg,
+			     struct kidled_scan_period scan_period);
 #endif
 extern int vm_swappiness;
 long remove_mapping(struct address_space *mapping, struct folio *folio);
