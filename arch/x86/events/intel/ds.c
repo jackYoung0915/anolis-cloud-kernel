455d8413d7b58b0e6bede2af59a5009a74e8b2c8
Kan Liang <kan.liang@linux.intel.com>
2020-10-01 06:57:47 -0700

    perf/x86/intel: Support PERF_SAMPLE_DATA_PAGE_SIZE
    ANBZ: #11861
    
    commit 76a5433f95f32d8a17c9f836be2084ed947c466b upstream
    
    The new sample type, PERF_SAMPLE_DATA_PAGE_SIZE, requires the virtual
    address. Update the data->addr if the sample type is set.
    
    The large PEBS is disabled with the sample type, because perf doesn't
    support munmap tracking yet. The PEBS buffer for large PEBS cannot be
    flushed for each munmap. Wrong page size may be calculated. The large
    PEBS can be enabled later separately when munmap tracking is supported.
    
    [Backport changes]
    
    1. The PERF_SAMPLE_PHYS_ADDR and PERF_SAMPLE_WEIGHT were replaced into
    a single line during merge to match upstream and avoid future line
    number conflict.
    
    Signed-off-by: Kan Liang <kan.liang@linux.intel.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20201001135749.2804-3-kan.liang@linux.intel.com
    Signed-off-by: Jeevan Deep J <j.jeevandeep@amd.com>
    Signed-off-by: Kun(llfl) <llfl@linux.alibaba.com>

--- arch/x86/events/intel/ds.c
+++ arch/x86/events/intel/ds.c
@@ -1163,9 +1163,9 @@ static void adaptive_pebs_record_size_update(void)
 }
 
 #define PERF_PEBS_MEMINFO_TYPE	(PERF_SAMPLE_ADDR | PERF_SAMPLE_DATA_SRC |   \
-				PERF_SAMPLE_PHYS_ADDR |			     \
-				PERF_SAMPLE_WEIGHT_TYPE |		     \
-				PERF_SAMPLE_TRANSACTION)
+				PERF_SAMPLE_PHYS_ADDR | PERF_SAMPLE_WEIGHT | \
+				PERF_SAMPLE_TRANSACTION |		     \
+				PERF_SAMPLE_DATA_PAGE_SIZE)
 
 static u64 pebs_update_adaptive_cfg(struct perf_event *event)
 {
@@ -1581,6 +1581,10 @@ static void setup_pebs_time(struct perf_event *event,
 	data->sample_flags |= PERF_SAMPLE_TIME;
 }
 
+#define PERF_SAMPLE_ADDR_TYPE	(PERF_SAMPLE_ADDR |		\
+				 PERF_SAMPLE_PHYS_ADDR |	\
+				 PERF_SAMPLE_DATA_PAGE_SIZE)
+
 static void setup_pebs_fixed_sample_data(struct perf_event *event,
 				   struct pt_regs *iregs, void *__pebs,
 				   struct perf_sample_data *data,
@@ -1699,7 +1703,7 @@ static void setup_pebs_fixed_sample_data(struct perf_event *event,
 	}
 
 
-	if ((sample_type & (PERF_SAMPLE_ADDR | PERF_SAMPLE_PHYS_ADDR)) &&
+	if ((sample_type & PERF_SAMPLE_ADDR_TYPE) &&
 	    x86_pmu.intel_cap.pebs_format >= 1)
 		data->addr = pebs->dla;
 
@@ -1862,7 +1866,7 @@ static void setup_pebs_adaptive_sample_data(struct perf_event *event,
 			data->sample_flags |= PERF_SAMPLE_DATA_SRC;
 		}
 
-		if (sample_type & (PERF_SAMPLE_ADDR | PERF_SAMPLE_PHYS_ADDR))
+		if (sample_type & PERF_SAMPLE_ADDR_TYPE)
 			data->addr = meminfo->address;
 
 		if (sample_type & PERF_SAMPLE_TRANSACTION) {
