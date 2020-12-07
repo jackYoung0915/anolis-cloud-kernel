c67861c9b6d037b6e5a99a1742982f79c794ec98
Xiang Chen <chenxiang66@hisilicon.com>
2020-12-07 21:30:55 +0800

    scsi: hisi_sas: Select a suitable queue for internal I/Os
    ANBZ: #11194
    
    commit 359db63378eded1ee9c8c9ad72245f9b0158ae95 openeuler
    
    For when managed interrupts are used (and shost->nr_hw_queues is set), a
    fixed queue - set per-device - is still used for internal I/Os.
    
    If all the CPUs mapped to that queue are offlined, then the completions for
    that queue are not serviced and any internal I/Os will time out.
    
    Fix by selecting a queue for internal I/Os from the queue mapped from the
    current CPU in this scenario.
    
    This is still not ideal as it does not deal with CPU hotplug for inflight
    internal I/Os, and needs proper support from [0].
    
    [0] https://lore.kernel.org/linux-scsi/20200703130122.111448-1-hare@suse.de/T/#m7d77d049b18f33a24ef206af69ebb66d07440556
    
    Link: https://lore.kernel.org/r/1607347855-59091-1-git-send-email-john.garry@huawei.com
    Fixes: 8d98416a55eb ("scsi: hisi_sas: Switch v3 hw to MQ")
    Signed-off-by: Xiang Chen <chenxiang66@hisilicon.com>
    Signed-off-by: John Garry <john.garry@huawei.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    Signed-off-by: Slim6882 <yangjunshuo@huawei.com>
    Signed-off-by: Min Li <gumi@linux.alibaba.com>

--- drivers/scsi/hisi_sas/hisi_sas_v3_hw.c
+++ drivers/scsi/hisi_sas/hisi_sas_v3_hw.c
@@ -2461,6 +2461,11 @@ static int interrupt_init_v3_hw(struct hisi_hba *hisi_hba)
 			dev_err(dev, "could not get cq%d irq affinity!\n", i);
 			return -ENOENT;
 		}
+		cq->irq_mask = pci_irq_get_affinity(pdev, i + BASE_VECTORS_V3_HW);
+		if (!cq->irq_mask) {
+			dev_err(dev, "could not get cq%d irq affinity!\n", i);
+			return -ENOENT;
+		}
 	}
 
 	return 0;
