38b3b50c416399ef76e3a292900139a5e9087963
Peter Zijlstra <peterz@infradead.org>
2020-12-10 17:14:08 +0100

    sched: Optimize finish_lock_switch()
    ANBZ: #19745
    
    commit ae7927023243dcc7389b2d59b16c09cbbeaecc36 upstream.
    
    The kernel test robot measured a -1.6% performance regression on
    will-it-scale/sched_yield due to commit:
    
      2558aacff858 ("sched/hotplug: Ensure only per-cpu kthreads run during hotplug")
    
    Even though we were careful to replace a single load with another
    single load from the same cacheline.
    
    Restore finish_lock_switch() to the exact state before the offending
    patch and solve the problem differently.
    
    Fixes: 2558aacff858 ("sched/hotplug: Ensure only per-cpu kthreads run during hotplug")
    Reported-by: kernel test robot <oliver.sang@intel.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20201210161408.GX3021@hirez.programming.kicks-ass.net
    Signed-off-by: Tianchen Ding <dtcccc@linux.alibaba.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1306,7 +1306,6 @@ struct rq {
 	unsigned long		cpu_capacity_orig;
 
 	struct callback_head	*balance_callback;
-	unsigned char		balance_flags;
 
 	unsigned char		nohz_idle_balance;
 	unsigned char		idle_balance;
@@ -1973,6 +1972,8 @@ struct rq_flags {
 #endif
 };
 
+extern struct callback_head balance_push_callback;
+
 /*
  * Lockdep annotation that avoids accidental unlocks; it's like a
  * sticky/continuous lockdep_assert_held().
@@ -1990,9 +1991,9 @@ static inline void rq_pin_lock(struct rq *rq, struct rq_flags *rf)
 #ifdef CONFIG_SCHED_DEBUG
 	rq->clock_update_flags &= (RQCF_REQ_SKIP|RQCF_ACT_SKIP);
 	rf->clock_update_flags = 0;
-#endif
 #ifdef CONFIG_SMP
-	SCHED_WARN_ON(rq->balance_callback);
+	SCHED_WARN_ON(rq->balance_callback && rq->balance_callback != &balance_push_callback);
+#endif
 #endif
 }
 
@@ -2161,9 +2162,6 @@ init_numa_balancing(unsigned long clone_flags, struct task_struct *p)
 
 #ifdef CONFIG_SMP
 
-#define BALANCE_WORK	0x01
-#define BALANCE_PUSH	0x02
-
 static inline void
 queue_balance_callback(struct rq *rq,
 		       struct callback_head *head,
@@ -2171,13 +2169,12 @@ queue_balance_callback(struct rq *rq,
 {
 	lockdep_assert_rq_held(rq);
 
-	if (unlikely(head->next || (rq->balance_flags & BALANCE_PUSH)))
+	if (unlikely(head->next || rq->balance_callback == &balance_push_callback))
 		return;
 
 	head->func = (void (*)(struct callback_head *))func;
 	head->next = rq->balance_callback;
 	rq->balance_callback = head;
-	rq->balance_flags |= BALANCE_WORK;
 }
 
 #define rcu_dereference_check_sched_domain(p) \
