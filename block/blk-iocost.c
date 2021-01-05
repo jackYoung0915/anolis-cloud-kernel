0b37153ee5659d1c0a741634a8c8044b284b682d
Tejun Heo <tj@kernel.org>
2021-01-05 12:37:23 -0500

    blk-iocost: fix NULL iocg deref from racing against initialization
    ANBZ: #10868
    
    commit d16baa3f1453c14d680c5fee01cd122a22d0e0ce upstream.
    
    When initializing iocost for a queue, its rqos should be registered before
    the blkcg policy is activated to allow policy data initiailization to lookup
    the associated ioc. This unfortunately means that the rqos methods can be
    called on bios before iocgs are attached to all existing blkgs.
    
    While the race is theoretically possible on ioc_rqos_throttle(), it mostly
    happened in ioc_rqos_merge() due to the difference in how they lookup ioc.
    The former determines it from the passed in @rqos and then bails before
    dereferencing iocg if the looked up ioc is disabled, which most likely is
    the case if initialization is still in progress. The latter looked up ioc by
    dereferencing the possibly NULL iocg making it a lot more prone to actually
    triggering the bug.
    
    * Make ioc_rqos_merge() use the same method as ioc_rqos_throttle() to look
      up ioc for consistency.
    
    * Make ioc_rqos_throttle() and ioc_rqos_merge() test for NULL iocg before
      dereferencing it.
    
    * Explain the danger of NULL iocgs in blk_iocost_init().
    
    [backport note]
    fix conflict caused by "alinux: iocost: fix NULL pointer dereference in
    ioc_rqos_throttle".
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Reported-by: Jonathan Lemon <bsd@fb.com>
    Cc: stable@vger.kernel.org # v5.4+
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    Signed-off-by: Ferry Meng <mengferry@linux.alibaba.com>

--- block/blk-iocost.c
+++ block/blk-iocost.c
@@ -1762,7 +1762,7 @@ static void ioc_rqos_throttle(struct rq_qos *rqos, struct bio *bio, spinlock_t *
 
 	iocg = blkg_to_iocg(blkg);
 	/* bypass IOs for root cgroup */
-	if (!iocg->level)
+	if (!iocg || !iocg->level)
 		return;
 
 	/* always activate so that even 0 cost IOs get protected to some level */
@@ -1915,7 +1915,7 @@ static void ioc_rqos_merge(struct rq_qos *rqos, struct request *rq,
 
 	iocg = blkg_to_iocg(blkg);
 	/* bypass for root cgroup */
-	if (!iocg->level)
+	if (!iocg || !iocg->level)
 		return;
 
 	abs_cost = calc_vtime_cost(bio, iocg, true);
@@ -2069,6 +2069,12 @@ static int blk_iocost_init(struct request_queue *q)
 	ioc_refresh_params(ioc, true);
 	spin_unlock_irq(&ioc->lock);
 
+	/*
+	 * rqos must be added before activation to allow iocg_pd_init() to
+	 * lookup the ioc from q. This means that the rqos methods may get
+	 * called before policy activation completion, can't assume that the
+	 * target bio has an iocg associated and need to test for NULL iocg.
+	 */
 	rq_qos_add(q, rqos);
 	ret = blkcg_activate_policy(q, &blkcg_policy_iocost);
 	if (ret) {
