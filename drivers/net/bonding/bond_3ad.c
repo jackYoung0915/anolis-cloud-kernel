c03f61d4c256885d308fd3bef0536d74f7f12fae
Nikolay Aleksandrov <nikolay@nvidia.com>
2021-02-11 04:43:32 +0800

    bonding: 3ad: add support for 400G speed
    ANBZ: #11369
    
    commit 138e3b3cc0bbbd795e3b3f2ab607597e2f0b80f9 upstream.
    
    In order to be able to use 3ad mode with 400G devices we need to extend
    the supported speeds.
    
    Signed-off-by: Nikolay Aleksandrov <nikolay@nvidia.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Signed-off-by: Xiao Long <xiaolong@openanolis.org>
    Signed-off-by: Philo Lu <lulie@linux.alibaba.com>
    Acked-by: Xuan Zhuo <xuanzhuo@linux.alibaba.com>
    Reviewed-by: Guixin Liu <kanie@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/3992

--- drivers/net/bonding/bond_3ad.c
+++ drivers/net/bonding/bond_3ad.c
@@ -74,6 +74,7 @@ enum ad_link_speed_type {
 	AD_LINK_SPEED_56000MBPS,
 	AD_LINK_SPEED_100000MBPS,
 	AD_LINK_SPEED_200000MBPS,
+	AD_LINK_SPEED_400000MBPS,
 };
 
 /* compare MAC addresses */
@@ -248,6 +249,7 @@ static inline int __check_agg_selection_timer(struct port *port)
  *     %AD_LINK_SPEED_56000MBPS
  *     %AD_LINK_SPEED_100000MBPS
  *     %AD_LINK_SPEED_200000MBPS
+ *     %AD_LINK_SPEED_400000MBPS
  */
 static u16 __get_link_speed(struct port *port)
 {
@@ -317,6 +319,10 @@ static u16 __get_link_speed(struct port *port)
 
 		case SPEED_200000:
 			speed = AD_LINK_SPEED_200000MBPS;
+			break;
+
+		case SPEED_400000:
+			speed = AD_LINK_SPEED_400000MBPS;
 			break;
 
 		default:
@@ -742,6 +748,9 @@ static u32 __get_agg_bandwidth(struct aggregator *aggregator)
 			break;
 		case AD_LINK_SPEED_200000MBPS:
 			bandwidth = nports * 200000;
+			break;
+		case AD_LINK_SPEED_400000MBPS:
+			bandwidth = nports * 400000;
 			break;
 		default:
 			bandwidth = 0; /* to silence the compiler */
