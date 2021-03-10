2f9e490fd50a77b2d6e8e65d696b10ab74459fdd
Viresh Kumar <viresh.kumar@linaro.org>
2021-03-10 08:21:04 +0530

    arch_topology: Allow multiple entities to provide sched_freq_tick() callback
    ANBZ: #26358
    
    commit 01e055c120a46e78650b5f903088badbbdaae9ad upstream
    
    This patch attempts to make it generic enough so other parts of the
    kernel can also provide their own implementation of scale_freq_tick()
    callback, which is called by the scheduler periodically to update the
    per-cpu arch_freq_scale variable.
    
    The implementations now need to provide 'struct scale_freq_data' for the
    CPUs for which they have hardware counters available, and a callback
    gets registered for each possible CPU in a per-cpu variable.
    
    The arch specific (or ARM AMU) counters are updated to adapt to this and
    they take the highest priority if they are available, i.e. they will be
    used instead of CPPC based counters for example.
    
    The special code to rebuild the sched domains, in case invariance status
    change for the system, is moved out of arm64 specific code and is added
    to arch_topology.c.
    
    Note that this also defines SCALE_FREQ_SOURCE_CPUFREQ but doesn't use it
    and it is added to show that cpufreq is also acts as source of
    information for FIE and will be used by default if no other counters are
    supported for a platform.
    
    Reviewed-by: Ionela Voinescu <ionela.voinescu@arm.com>
    Tested-by: Ionela Voinescu <ionela.voinescu@arm.com>
    Acked-by: Will Deacon <will@kernel.org> # for arm64
    Tested-by: Vincent Guittot <vincent.guittot@linaro.org>
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>

--- arch/arm64/kernel/topology.c
+++ arch/arm64/kernel/topology.c
@@ -211,6 +211,12 @@ unsigned int arch_cpufreq_get_khz(int cpu)
 }
 
 #ifdef CONFIG_ARM64_AMU_EXTN
+#define read_corecnt()	read_sysreg_s(SYS_AMEVCNTR0_CORE_EL0)
+#define read_constcnt()	read_sysreg_s(SYS_AMEVCNTR0_CONST_EL0)
+#else
+#define read_corecnt()	(0UL)
+#define read_constcnt()	(0UL)
+#endif
 
 #undef pr_fmt
 #define pr_fmt(fmt) "AMU: " fmt
@@ -220,6 +226,12 @@ static DEFINE_PER_CPU_READ_MOSTLY(unsigned long, arch_max_freq_scale);
 static DEFINE_PER_CPU(u64, arch_const_cycles_prev);
 static DEFINE_PER_CPU(u64, arch_core_cycles_prev);
 static cpumask_var_t amu_fie_cpus;
+
+void update_freq_counters_refs(void)
+{
+	this_cpu_write(arch_core_cycles_prev, read_corecnt());
+	this_cpu_write(arch_const_cycles_prev, read_constcnt());
+}
 
 /*
  * Sample cpu freq.
@@ -346,12 +358,47 @@ static int freq_inv_set_max_ratio(int cpu, u64 max_rate, u64 ref_rate)
 	return 0;
 }
 
-static DEFINE_STATIC_KEY_FALSE(amu_fie_key);
-#define amu_freq_invariant() static_branch_unlikely(&amu_fie_key)
+static void amu_scale_freq_tick(void)
+{
+	u64 prev_core_cnt, prev_const_cnt;
+	u64 core_cnt, const_cnt, scale;
+
+	prev_const_cnt = this_cpu_read(arch_const_cycles_prev);
+	prev_core_cnt = this_cpu_read(arch_core_cycles_prev);
+
+	update_freq_counters_refs();
+
+	const_cnt = this_cpu_read(arch_const_cycles_prev);
+	core_cnt = this_cpu_read(arch_core_cycles_prev);
+
+	if (unlikely(core_cnt <= prev_core_cnt ||
+		     const_cnt <= prev_const_cnt))
+		return;
+
+	/*
+	 *	    /\core    arch_max_freq_scale
+	 * scale =  ------- * --------------------
+	 *	    /\const   SCHED_CAPACITY_SCALE
+	 *
+	 * See validate_cpu_freq_invariance_counters() for details on
+	 * arch_max_freq_scale and the use of SCHED_CAPACITY_SHIFT.
+	 */
+	scale = core_cnt - prev_core_cnt;
+	scale *= this_cpu_read(arch_max_freq_scale);
+	scale = div64_u64(scale >> SCHED_CAPACITY_SHIFT,
+			  const_cnt - prev_const_cnt);
+
+	scale = min_t(unsigned long, scale, SCHED_CAPACITY_SCALE);
+	this_cpu_write(arch_freq_scale, (unsigned long)scale);
+}
+
+static struct scale_freq_data amu_sfd = {
+	.source = SCALE_FREQ_SOURCE_ARCH,
+	.set_freq_scale = amu_scale_freq_tick,
+};
 
 static void amu_fie_setup(const struct cpumask *cpus)
 {
-	bool invariant;
 	int cpu;
 
 	/* We are already set since the last insmod of cpufreq driver */
@@ -368,25 +415,10 @@ static void amu_fie_setup(const struct cpumask *cpus)
 
 	cpumask_or(amu_fie_cpus, amu_fie_cpus, cpus);
 
-	invariant = topology_scale_freq_invariant();
-
-	/* We aren't fully invariant yet */
-	if (!invariant && !cpumask_equal(amu_fie_cpus, cpu_present_mask))
-		return;
-
-	static_branch_enable(&amu_fie_key);
+	topology_set_scale_freq_source(&amu_sfd, amu_fie_cpus);
 
 	pr_debug("CPUs[%*pbl]: counters will be used for FIE.",
 		 cpumask_pr_args(cpus));
-
-	/*
-	 * Task scheduler behavior depends on frequency invariance support,
-	 * either cpufreq or counter driven. If the support status changes as
-	 * a result of counter initialisation and use, retrigger the build of
-	 * scheduling domains to ensure the information is propagated properly.
-	 */
-	if (!invariant)
-		rebuild_sched_domains_energy();
 }
 
 static int init_amu_fie_callback(struct notifier_block *nb, unsigned long val,
@@ -430,4 +462,3 @@ static int __init init_amu_fie(void)
 }
 core_initcall(init_amu_fie);
 
-#endif /* CONFIG_ARM64_AMU_EXTN */
