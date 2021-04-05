d7b226c9b56f71f7419bbddbe7fb7f94b8eb8a33
Hui Zhu <teawater@antfin.com>
2021-04-05 21:03:46 +0800

    anolis: virtio-balloon: Add VIRTIO_BALLOON_F_ALLOC_RETRY
    ANBZ: #8389
    
    When VIRTIO_BALLOON_F_ALLOC_RETRY is set, virtio-balloon will use
    __GFP_RETRY_MAYFAIL to allocate the pages but not __GFP_NORETRY.
    
    Signed-off-by: Hui Zhu <teawater@antfin.com>
    Signed-off-by: hr567 <hr567@linux.alibaba.com>
    Signed-off-by: zhongjiang-ali <zhongjiang-ali@linux.alibaba.com>
    Reviewed-by: Issac Hai <hjwissac@linux.alibaba.com>
    Acked-by: Liu Jiang <gerry@linux.alibaba.com>
    Acked-by: Xunlei Pang <xlpang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/2883

--- drivers/virtio/virtio_balloon.c
+++ drivers/virtio/virtio_balloon.c
@@ -241,6 +241,12 @@ static unsigned int fill_balloon(struct virtio_balloon *vb, size_t num)
 	struct page *page;
 	LIST_HEAD(pages);
 	bool is_cont = vb->current_pages_order != 0;
+	gfp_t gfp;
+
+	if (virtio_has_feature(vb->vdev, VIRTIO_BALLOON_F_ALLOC_RETRY))
+		gfp = __GFP_RETRY_MAYFAIL;
+	else
+		gfp = __GFP_NORETRY;
 
 	if (is_cont)
 		pfn_per_alloc = 2;
@@ -258,7 +264,7 @@ static unsigned int fill_balloon(struct virtio_balloon *vb, size_t num)
 			    num - num_allocated_pages <
 				VIRTIO_BALLOON_PAGES_PER_PAGE << vb->current_pages_order)
 				continue;
-			page = balloon_pages_alloc(vb->current_pages_order);
+			page = balloon_pages_alloc(vb->current_pages_order, gfp);
 			if (page) {
 				/* If the first allocated page is not continuous pages,
 				 * go back to transport page as signle page.
@@ -1252,6 +1258,7 @@ static unsigned int features[] = {
 	VIRTIO_BALLOON_F_PAGE_POISON,
 	VIRTIO_BALLOON_F_REPORTING,
 	VIRTIO_BALLOON_F_CONT_PAGES,
+	VIRTIO_BALLOON_F_ALLOC_RETRY,
 };
 
 static struct virtio_driver virtio_balloon_driver = {
