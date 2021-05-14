e3b07c9b1f6fbf9cdb8f574152b5c9a56fcd5214
Alex Kogan <alex.kogan@oracle.com>
2021-05-14 16:07:40 -0400

    locking/qspinlock: Introduce CNA into the slow path of qspinlock
    ANBZ: #13056
    
    cherry-picked from https://lore.kernel.org/all/20210514200743.3026725-3-alex.kogan@oracle.com/
    
    In CNA, spinning threads are organized in two queues, a primary queue for
    threads running on the same node as the current lock holder, and a
    secondary queue for threads running on other nodes. After acquiring the
    MCS lock and before acquiring the spinlock, the MCS lock
    holder checks whether the next waiter in the primary queue (if exists) is
    running on the same NUMA node. If it is not, that waiter is detached from
    the main queue and moved into the tail of the secondary queue. This way,
    we gradually filter the primary queue, leaving only waiters running on
    the same preferred NUMA node. For more details, see
    https://arxiv.org/abs/1810.05600.
    
    Note that this variant of CNA may introduce starvation by continuously
    passing the lock between waiters in the main queue. This issue will be
    addressed later in the series.
    
    Enabling CNA is controlled via a new configuration option
    (NUMA_AWARE_SPINLOCKS). By default, the CNA variant is patched in at the
    boot time only if we run on a multi-node machine in native environment and
    the new config is enabled. (For the time being, the patching requires
    CONFIG_PARAVIRT_SPINLOCKS to be enabled as well. However, this should be
    resolved once static_call() is available.) This default behavior can be
    overridden with the new kernel boot command-line option
    "numa_spinlock=on/off" (default is "auto").
    
    Signed-off-by: Alex Kogan <alex.kogan@oracle.com>
    Reviewed-by: Steve Sistare <steven.sistare@oracle.com>
    Reviewed-by: Waiman Long <longman@redhat.com>
    Signed-off-by: Kong Yingqiao <kongyingqiao@hygon.cn>
    Reviewed-by: Cruz Zhao <CruzZhao@linux.alibaba.com>
    Reviewed-by: Artie Ding <artie.ding@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/4406
    [rebase 6.6.102]

--- arch/x86/kernel/alternative.c
+++ arch/x86/kernel/alternative.c
@@ -1883,6 +1883,10 @@ void __init alternative_instructions(void)
 	 */
 	paravirt_set_cap();
 
+#if defined(CONFIG_NUMA_AWARE_SPINLOCKS)
+	cna_configure_spin_lock_slowpath();
+#endif
+
 	/* Keep CET-IBT disabled until caller/callee are patched */
 	ibt = ibt_save(/*disable*/ true);
 
