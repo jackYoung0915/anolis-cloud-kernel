e654fe078afdb73e43bc67d52c881983929241f0
Keith Busch <kbusch@kernel.org>
2021-06-10 14:44:36 -0700

    block: return errors from blk_execute_rq()
    ANBZ: #11744
    
    commit fb9b16e15cd70e21d8af7f03d700deb9509c2ce8 upstream.
    
    The synchronous blk_execute_rq() had not provided a way for its callers
    to know if its request was successful or not. Return the blk_status_t
    result of the request.
    
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Ming Lei <ming.lei@redhat.com>
    Signed-off-by: Keith Busch <kbusch@kernel.org>
    Reviewed-by: Chaitanya Kulkarni <chaitanya.kulkarni@wdc.com>
    Link: https://lore.kernel.org/r/20210610214437.641245-4-kbusch@kernel.org
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Joseph Qi <joseph.qi@linux.alibaba.com>
    Reviewed-by: Ferry Meng <mengferry@linux.alibaba.com>
    Reviewed-by: Guixin Liu <kanie@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/4084

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -979,9 +979,10 @@ extern int blk_rq_map_kern(struct request_queue *, struct request *, void *, uns
 extern int blk_rq_map_user_iov(struct request_queue *, struct request *,
 			       struct rq_map_data *, const struct iov_iter *,
 			       gfp_t);
-extern void blk_execute_rq(struct gendisk *, struct request *, int);
 extern void blk_execute_rq_nowait(struct gendisk *,
 				  struct request *, int, rq_end_io_fn *);
+blk_status_t blk_execute_rq(struct gendisk *bd_disk, struct request *rq,
+			    int at_head);
 extern bool blk_rq_is_poll(struct request *);
 
 /* Helper to convert REQ_OP_XXX to its string format XXX */
