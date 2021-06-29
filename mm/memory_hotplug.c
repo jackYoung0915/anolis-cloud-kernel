d8a12fd6f0eebe7cf7f01ebf740977aebe370ec9
Mel Gorman <mgorman@techsingularity.net>
2021-06-28 19:42:12 -0700

    mm/page_alloc: disassociate the pcp->high from pcp->batch
    ANBZ: #26810
    
    commit b92ca18e8ca596f4f3d80c1fe833bc57a1b2458c upstream.
    
    The pcp high watermark is based on the batch size but there is no
    relationship between them other than it is convenient to use early in
    boot.
    
    This patch takes the first step and bases pcp->high on the zone low
    watermark split across the number of CPUs local to a zone while the batch
    size remains the same to avoid increasing allocation latencies.  The
    intent behind the default pcp->high is "set the number of PCP pages such
    that if they are all full that background reclaim is not started
    prematurely".
    
    Note that in this patch the pcp->high values are adjusted after memory
    hotplug events, min_free_kbytes adjustments and watermark scale factor
    adjustments but not CPU hotplug events which is handled later in the
    series.
    
    On a test KVM instance;
    
    Before grep -E "high:|batch" /proc/zoneinfo | tail -2
                  high:  378
                  batch: 63
    
    After grep -E "high:|batch" /proc/zoneinfo | tail -2
                  high:  649
                  batch: 63
    
    [mgorman@techsingularity.net:  fix __setup_per_zone_wmarks for parallel memory
    hotplug]
      Link: https://lkml.kernel.org/r/20210528105925.GN30378@techsingularity.net
    
    Link: https://lkml.kernel.org/r/20210525080119.5455-3-mgorman@techsingularity.net
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>
    Acked-by: Vlastimil Babka <vbabka@suse.cz>
    Cc: Dave Hansen <dave.hansen@linux.intel.com>
    Cc: Hillf Danton <hdanton@sina.com>
    Cc: Michal Hocko <mhocko@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Weilin Tong <tongweilin@linux.alibaba.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5984

--- mm/memory_hotplug.c
+++ mm/memory_hotplug.c
@@ -1186,7 +1186,6 @@ int __ref __online_pages(unsigned long pfn, unsigned long nr_pages,
 	node_states_set_node(nid, &arg);
 	if (need_zonelists_rebuild)
 		build_all_zonelists(NULL);
-	zone_pcp_update(zone);
 
 	/* Basic onlining is complete, allow allocation of onlined pages. */
 	undo_isolate_page_range(pfn, pfn + nr_pages, MIGRATE_MOVABLE);
@@ -1199,6 +1198,7 @@ int __ref __online_pages(unsigned long pfn, unsigned long nr_pages,
 	 */
 	shuffle_zone(zone);
 
+	/* reinitialise watermarks and update pcp limits */
 	init_per_zone_wmark_min();
 	init_min_cache_kbytes();
 
@@ -2134,14 +2134,14 @@ int __ref offline_pages(unsigned long start_pfn, unsigned long nr_pages,
 	adjust_managed_page_count(pfn_to_page(start_pfn), -nr_pages);
 	adjust_present_page_count(pfn_to_page(start_pfn), group, -nr_pages);
 
+	/* reinitialise watermarks and update pcp limits */
 	init_per_zone_wmark_min();
 	init_min_cache_kbytes();
 
 	if (!populated_zone(zone)) {
 		zone_pcp_reset(zone);
 		build_all_zonelists(NULL);
-	} else
-		zone_pcp_update(zone);
+	}
 
 	node_states_clear_node(node, &arg);
 	if (arg.status_change_nid >= 0) {
