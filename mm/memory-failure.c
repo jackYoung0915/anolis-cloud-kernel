9fa78b3382c12655100c612a6d6fc6e3a937e985
Naoya Horiguchi <naoya.horiguchi@nec.com>
2021-06-30 18:48:38 -0700

    mm/hwpoison: disable pcp for page_handle_poison()
    ANBZ: #10910
    
    commit 510d25c92ec4ace4199a94f2f0cc9b8208c0de57 upstream
    
    Recent changes by patch "mm/page_alloc: allow high-order pages to be
    stored on the per-cpu lists" makes kernels determine whether to use pcp by
    pcp_allowed_order(), which breaks soft-offline for hugetlb pages.
    
    Soft-offline dissolves a migration source page, then removes it from buddy
    free list, so it's assumed that any subpage of the soft-offlined hugepage
    are recognized as a buddy page just after returning from
    dissolve_free_huge_page().  pcp_allowed_order() returns true for hugetlb,
    so this assumption is no longer true.
    
    So disable pcp during dissolve_free_huge_page() and take_page_off_buddy()
    to prevent soft-offlined hugepages from linking to pcp lists.
    Soft-offline should not be common events so the impact on performance
    should be minimal.  And I think that the optimization of Mel's patch could
    benefit to hugetlb so zone_pcp_disable() is called only in hwpoison
    context.
    
    Link: https://lkml.kernel.org/r/20210617092626.291006-1-nao.horiguchi@gmail.com
    Signed-off-by: Naoya Horiguchi <naoya.horiguchi@nec.com>
    Acked-by: Mel Gorman <mgorman@techsingularity.net>
    Cc: Mike Kravetz <mike.kravetz@oracle.com>
    Cc: David Hildenbrand <david@redhat.com>
    Cc: Oscar Salvador <osalvador@suse.de>
    Cc: Michal Hocko <mhocko@suse.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Kaihao Bai <carlo.bai@linux.alibaba.com>

--- mm/memory-failure.c
+++ mm/memory-failure.c
@@ -67,6 +67,19 @@ int sysctl_memory_failure_recovery __read_mostly = 1;
 
 atomic_long_t num_poisoned_pages __read_mostly = ATOMIC_LONG_INIT(0);
 
+static bool __page_handle_poison(struct page *page)
+{
+	bool ret;
+
+	zone_pcp_disable(page_zone(page));
+	ret = dissolve_free_huge_page(page);
+	if (!ret)
+		ret = take_page_off_buddy(page);
+	zone_pcp_enable(page_zone(page));
+
+	return ret;
+}
+
 static bool page_handle_poison(struct page *page, bool hugepage_or_freepage, bool release)
 {
 	if (hugepage_or_freepage) {
@@ -74,7 +87,7 @@ static bool page_handle_poison(struct page *page, bool hugepage_or_freepage, boo
 		 * Doing this check for free pages is also fine since dissolve_free_huge_page
 		 * returns 0 for non-hugetlb pages as well.
 		 */
-		if (dissolve_free_huge_page(page) || !take_page_off_buddy(page))
+		if (!__page_handle_poison(page))
 			/*
 			 * We could fail to take off the target page from buddy
 			 * for example due to racy page allocaiton, but that's
@@ -1091,7 +1104,7 @@ static int me_huge_page(struct page_state *ps, struct page *p)
 		 */
 		if (PageAnon(hpage))
 			put_page(hpage);
-		if (dissolve_free_huge_page(p) && take_page_off_buddy(p)) {
+		if (__page_handle_poison(p)) {
 			page_ref_inc(p);
 			res = MF_RECOVERED;
 		}
@@ -1536,7 +1549,7 @@ static int memory_failure_hugetlb(unsigned long pfn, int flags)
 		}
 		unlock_page(head);
 		res = MF_FAILED;
-		if (!dissolve_free_huge_page(p) && take_page_off_buddy(p)) {
+		if (__page_handle_poison(p)) {
 			page_ref_inc(p);
 			res = MF_RECOVERED;
 		}
