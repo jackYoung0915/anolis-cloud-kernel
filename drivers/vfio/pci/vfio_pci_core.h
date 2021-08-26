ea61f6cb5ba35fb0a22cf80af8700f70b16ac419
Yishai Hadas <yishaih@nvidia.com>
2021-08-26 13:39:07 +0300

    vfio/pci: Move module parameters to vfio_pci.c
    ANBZ: #10925
    
    commit c61302aa48f7c46b5c9d893109488af951be12e4 upstream.
    
    This is a preparation before splitting vfio_pci.ko to 2 modules.
    
    As module parameters are a kind of uAPI they need to stay on vfio_pci.ko
    to avoid a user visible impact.
    
    For now continue to keep the implementation of these options in
    vfio_pci_core.c. Arguably they are vfio_pci functionality, but further
    splitting of vfio_pci_core.c will be better done in another series
    
    Signed-off-by: Yishai Hadas <yishaih@nvidia.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>
    Link: https://lore.kernel.org/r/20210826103912.128972-9-yishaih@nvidia.com
    Signed-off-by: Alex Williamson <alex.williamson@redhat.com>
    Signed-off-by: Qinyun Tan <qinyuntan@linux.alibaba.com>
    Reviewed-by: Guanghui Feng <guanghuifeng@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/3855

--- drivers/vfio/pci/vfio_pci_core.h
+++ drivers/vfio/pci/vfio_pci_core.h
@@ -262,6 +262,8 @@ int vfio_pci_migration_init(struct vfio_pci_core_device *vdev, uint32_t size);
 /* Will be exported for vfio pci drivers usage */
 void vfio_pci_core_cleanup(void);
 int vfio_pci_core_init(void);
+void vfio_pci_core_set_params(bool nointxmask, bool is_disable_vga,
+			      bool is_disable_idle_d3);
 void vfio_pci_core_close_device(struct vfio_device *core_vdev);
 void vfio_pci_core_init_device(struct vfio_pci_core_device *vdev,
 			       struct pci_dev *pdev,
