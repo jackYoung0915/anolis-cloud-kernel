c4e10e96e862d0013f84b47700c2c8483e863bd4
Ricardo Neri <ricardo.neri-calderon@linux.intel.com>
2021-09-10 18:18:15 -0700

    sched/topology: Introduce sched_group::flags
    ANBZ: #11194
    
    commit eb54b9e56ceb3afc5626f4203fc59c56570ae819 openeuler
    
    mainline inclusion
    from mainline-v5.15-rc5
    commit 16d364ba6ef2aa59b409df70682770f3ed23f7c0
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I8E8NN
    CVE: NA
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/commit/?id=16d364ba6ef2aa59b409df70682770f3ed23f7c0
    
    ----------------------------------------------------------------------
    
    There exist situations in which the load balance needs to know the
    properties of the CPUs in a scheduling group. When using asymmetric
    packing, for instance, the load balancer needs to know not only the
    state of dst_cpu but also of its SMT siblings, if any.
    
    Use the flags of the child scheduling domains to initialize scheduling
    group flags. This will reflect the properties of the CPUs in the
    group.
    
    A subsequent changeset will make use of these new flags. No functional
    changes are introduced.
    
    Originally-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Ricardo Neri <ricardo.neri-calderon@linux.intel.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Joel Fernandes (Google) <joel@joelfernandes.org>
    Reviewed-by: Len Brown <len.brown@intel.com>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Link: https://lkml.kernel.org/r/20210911011819.12184-3-ricardo.neri-calderon@linux.intel.com
    Signed-off-by: Jie Liu <liujie375@h-partners.com>
    Signed-off-by: Min Li <gumi@linux.alibaba.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -2153,6 +2153,7 @@ struct sched_group {
 	unsigned int		group_weight;
 	struct sched_group_capacity *sgc;
 	int			asym_prefer_cpu;	/* CPU of highest priority in group */
+	int			flags;
 
 	CK_KABI_RESERVE(1)
 	CK_KABI_RESERVE(2)
