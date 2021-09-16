b4f58878e1b9b4f2345275d0c3eaa518cc3af61d
Gang Deng <gavin.dg@linux.alibaba.com>
2021-09-16 12:38:04 +0800

    anolis: mm, kidled: skip node which has none memory
    ANBZ: #22925
    
    fix #36834759
    
    Or the vmalloc() will print a warn message when the alloc size
    equals zero, and kidled stop working.
    
    Signed-off-by: Gang Deng <gavin.dg@linux.alibaba.com>
    Reviewed-by: zhongjiang-ali <zhongjiang-ali@linux.alibaba.com>
    Reviewed-by: Xu Yu <xuyu@linux.alibaba.com>
    Signed-off-by: Weilin Tong <tongweilin@linux.alibaba.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5530

--- mm/kidled.c
+++ mm/kidled.c
@@ -387,6 +387,10 @@ static bool kidled_scan_node(pg_data_t *pgdat,
 
 #ifdef KIDLED_AGE_NOT_IN_PAGE_FLAGS
 	if (unlikely(!pgdat->node_folio_age)) {
+		/* This node has none memory, skip it. */
+		if (!pgdat->node_spanned_pages)
+			return true;
+
 		pgdat->node_folio_age = vzalloc(pgdat->node_spanned_pages);
 		if (unlikely(!pgdat->node_folio_age))
 			return false;
