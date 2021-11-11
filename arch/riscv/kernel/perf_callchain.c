3339c913b6986030625b0b2093ea1c7c40da3e8c
Sean Christopherson <seanjc@google.com>
2021-11-11 02:07:26 +0000

    perf: Drop dead and useless guest "support" from arm, csky, nds32 and riscv
    ANBZ: #11861
    
    commit 84af21d850ee1ccc990df37dd47c13fdfe93be75 upstream
    
    Drop "support" for guest callbacks from architectures that don't implement
    the guest callbacks.  Future patches will convert the callbacks to
    static_call; rather than churn a bunch of arch code (that was presumably
    copy+pasted from x86), remove it wholesale as it's useless and at best
    wasting cycles.
    
    A future patch will also add a Kconfig to force architcture to opt into
    the callbacks to make it more difficult for uses "support" to sneak in in
    the future.
    
    No functional change intended.
    
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Paolo Bonzini <pbonzini@redhat.com>
    Link: https://lore.kernel.org/r/20211111020738.2512932-6-seanjc@google.com
    Signed-off-by: Jeevan Deep J <j.jeevandeep@amd.com>
    Signed-off-by: Kun(llfl) <llfl@linux.alibaba.com>

--- arch/riscv/kernel/perf_callchain.c
+++ arch/riscv/kernel/perf_callchain.c
@@ -60,12 +60,7 @@ static unsigned long user_backtrace(struct perf_callchain_entry_ctx *entry,
 void perf_callchain_user(struct perf_callchain_entry_ctx *entry,
 			 struct pt_regs *regs)
 {
-	struct perf_guest_info_callbacks *guest_cbs = perf_get_guest_cbs();
 	unsigned long fp = 0;
-
-	/* RISC-V does not support perf in guest mode. */
-	if (guest_cbs && guest_cbs->is_in_guest())
-		return;
 
 	fp = regs->s0;
 	perf_callchain_store(entry, regs->epc);
@@ -85,13 +80,5 @@ void notrace walk_stackframe(struct task_struct *task,
 void perf_callchain_kernel(struct perf_callchain_entry_ctx *entry,
 			   struct pt_regs *regs)
 {
-	struct perf_guest_info_callbacks *guest_cbs = perf_get_guest_cbs();
-
-	/* RISC-V does not support perf in guest mode. */
-	if (guest_cbs && guest_cbs->is_in_guest()) {
-		pr_warn("RISC-V does not support perf in guest mode!");
-		return;
-	}
-
 	walk_stackframe(NULL, regs, fill_callchain, entry);
 }
