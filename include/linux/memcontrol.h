6f719078370a9342b916303e99d0cbf83eb365e0
Xunlei Pang <xlpang@linux.alibaba.com>
2021-11-12 09:59:30 +0800

    anolis: mm: coldpgs: support reclaiming cold pages
    ANBZ: #1702
    
    This module supports reclaiming cold pages, which works based on the
    statistics exposed by kidled, which starts kernel thread to periodically
    scan the consumed memory, to determine their access frequency (cold vs
    hot) and exports through "memory.idle_page_stats". The design is as simple
    as to iterate the memory cgroup's LRU lists, reclaim those pages that are
    colder than the user-supplied threshold. For the design details, please
    refer to "Documentation/vm/coldpgs.rst". The testing result can be checked
    out from that document either.
    
    Note that several fields are introduced to "struct mem_cgroup", to be
    used in future when the memory migration is supported. The module can
    be detached from the kernel completely with this.
    
    [ Zelin Deng:
    	Big changes: use folio to replace pages ]
    
    Signed-off-by: Xunlei Pang <xlpang@linux.alibaba.com>
    Reviewed-by: Yang Shi <yang.shi@linux.alibaba.com>
    Signed-off-by: Rongwei Wang <rongwei.wang@linux.alibaba.com>
    Reviewed-by: Xu Yu <xuyu@linux.alibaba.com>
    Acked-by: Gang Deng <gavin.dg@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/565
    Signed-off-by: Zelin Deng <zelin.deng@linux.alibaba.com>

--- include/linux/memcontrol.h
+++ include/linux/memcontrol.h
@@ -212,6 +212,40 @@ struct mem_cgroup_thresholds {
 	struct mem_cgroup_threshold_ary *spare;
 };
 
+#if IS_ENABLED(CONFIG_RECLAIM_COLDPGS)
+struct reclaim_coldpgs_control {
+	struct rw_semaphore	rwsem;
+	unsigned long		threshold;
+	unsigned long		size;
+	unsigned long		flags;
+
+	CK_KABI_RESERVE(1)
+	CK_KABI_RESERVE(2)
+	CK_KABI_RESERVE(3)
+	CK_KABI_RESERVE(4)
+};
+
+#define RECLAIM_COLDPGS_STAT_PCACHE_IN_MIGRATE	0
+#define RECLAIM_COLDPGS_STAT_PCACHE_OUT_MIGRATE	1
+#define RECLAIM_COLDPGS_STAT_PCACHE_OUT_DROP	2
+#define RECLAIM_COLDPGS_STAT_ANON_IN_MIGRATE	3
+#define RECLAIM_COLDPGS_STAT_ANON_IN_ZSWAP	4
+#define RECLAIM_COLDPGS_STAT_ANON_IN_SWAP	5
+#define RECLAIM_COLDPGS_STAT_ANON_OUT_MIGRATE	6
+#define RECLAIM_COLDPGS_STAT_ANON_OUT_ZSWAP	7
+#define RECLAIM_COLDPGS_STAT_ANON_OUT_SWAP	8
+#define RECLAIM_COLDPGS_STAT_MAX		9
+
+struct reclaim_coldpgs_stats {
+	unsigned long		counts[RECLAIM_COLDPGS_STAT_MAX];
+
+	CK_KABI_RESERVE(1)
+	CK_KABI_RESERVE(2)
+	CK_KABI_RESERVE(3)
+	CK_KABI_RESERVE(4)
+};
+#endif /* CONFIG_RECLAIM_COLDPGS */
+
 /*
  * Remember four most recent foreign writebacks with dirty pages in this
  * cgroup.  Inode sharing is expected to be uncommon and, even if we miss
@@ -440,6 +474,11 @@ struct mem_cgroup {
 
 #ifdef CONFIG_ASYNC_FORK
 	unsigned long async_fork;
+#endif
+
+#if IS_ENABLED(CONFIG_RECLAIM_COLDPGS)
+	struct reclaim_coldpgs_control	coldpgs_control;
+	struct reclaim_coldpgs_stats __percpu *coldpgs_stats;
 #endif
 
 	CK_KABI_RESERVE(1)
@@ -833,6 +872,12 @@ static inline void mem_cgroup_uncharge_list(struct list_head *page_list)
 }
 
 void mem_cgroup_migrate(struct folio *old, struct folio *new);
+
+static inline struct mem_cgroup_per_node *
+mem_cgroup_nodeinfo(struct mem_cgroup *memcg, int nid)
+{
+	return memcg->nodeinfo[nid];
+}
 
 /**
  * mem_cgroup_lruvec - get the lru list vector for a memcg & node
--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -6798,6 +6798,13 @@ static struct mem_cgroup *mem_cgroup_alloc(void)
 	if (!memcg->exstat_cpu)
 		goto fail;
 
+#if IS_ENABLED(CONFIG_RECLAIM_COLDPGS)
+	init_rwsem(&memcg->coldpgs_control.rwsem);
+	memcg->coldpgs_stats = alloc_percpu(struct reclaim_coldpgs_stats);
+	if (!memcg->coldpgs_stats)
+		goto fail;
+#endif
+
 	for_each_node(node)
 		if (alloc_mem_cgroup_per_node_info(memcg, node))
 			goto fail;
