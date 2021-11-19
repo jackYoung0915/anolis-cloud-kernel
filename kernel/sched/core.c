0a55b8419620a9af9a65f9c4508f40c2a92f913c
Erwei Deng <erwei@linux.alibaba.com>
2021-11-19 10:38:25 +0800

    anolis: sched: Add a stub function in task's exiting
    ANBZ: #8765
    
    OpenAnolis Bug Tracker: 0000548
    
    Add a stub for release task's some other memory allocated in
    scheduler. The task is a part of scheduler, but the fork and
    exit are outside of scheduler. Sometimes, scheduler want to
    free other memory for tasks, but there is no suitable
    function of scheduler called when task's exiting to free them.
    
    We insert a stub in release_task function, and scheduler can
    allocate memory by wake_up_new_task, and free by this stub.
    
    Signed-off-by: Erwei Deng <erwei@linux.alibaba.com>
    Signed-off-by: Yihao Wu <wuyihao@linux.alibaba.com>
    Signed-off-by: Vincent Gao <vincentbo@linux.alibaba.com>
    Reviewed-by: Cruz Zhao <cruzzhao@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5412

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -10722,6 +10722,11 @@ void call_trace_sched_update_nr_running(struct rq *rq, int count)
         trace_sched_update_nr_running_tp(rq, count);
 }
 
+/* A hook point for hotfix to release reserve memory used for scheduler. */
+void sched_task_release(struct task_struct *p)
+{
+}
+
 #ifdef CONFIG_SCHED_MM_CID
 
 /*
