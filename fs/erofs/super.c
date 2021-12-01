f76fd2513eb513789c8785a6a29847b16f86e882
Huang Jianan <huangjianan@oppo.com>
2021-12-01 22:54:36 +0800

    erofs: add sysfs interface
    ANBZ: #23261
    
    commit 168e9a76200c54c584a23aa88c62c53c4b0edd66 upstream.
    
    Add sysfs interface to configure erofs related parameters later.
    
    Link: https://lore.kernel.org/r/20211201145436.4357-1-huangjianan@oppo.com
    Reviewed-by: Chao Yu <chao@kernel.org>
    Reviewed-by: Gao Xiang <hsiangkao@linux.alibaba.com>
    Signed-off-by: Huang Jianan <huangjianan@oppo.com>
    Conflicts:
    	fs/erofs/Makefile
    	fs/erofs/internal.h
    	fs/erofs/super.c
    Signed-off-by: Gao Xiang <hsiangkao@linux.alibaba.com>
    Acked-by: Joseph Qi <joseph.qi@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5571

--- fs/erofs/super.c
+++ fs/erofs/super.c
@@ -831,6 +831,10 @@ static int erofs_fc_fill_super(struct super_block *sb, struct fs_context *fc)
 	if (err)
 		return err;
 
+	err = erofs_register_sysfs(sb);
+	if (err)
+		return err;
+
 	erofs_info(sb, "mounted with root inode @ nid %llu.", ROOT_NID(sbi));
 	return 0;
 }
@@ -1023,6 +1027,7 @@ static void erofs_put_super(struct super_block *sb)
 
 	DBG_BUGON(!sbi);
 
+	erofs_unregister_sysfs(sb);
 	erofs_shrinker_unregister(sb);
 #ifdef CONFIG_EROFS_FS_ZIP
 	iput(sbi->managed_cache);
@@ -1077,6 +1082,10 @@ static int __init erofs_module_init(void)
 	if (err)
 		goto fscache_err;
 
+	err = erofs_init_sysfs();
+	if (err)
+		goto sysfs_err;
+
 	err = register_filesystem(&erofs_fs_type);
 	if (err)
 		goto fs_err;
@@ -1084,6 +1093,8 @@ static int __init erofs_module_init(void)
 	return 0;
 
 fs_err:
+	erofs_exit_sysfs();
+sysfs_err:
 	erofs_fscache_unregister();
 fscache_err:
 	z_erofs_exit_zip_subsystem();
@@ -1101,11 +1112,12 @@ shrinker_err:
 static void __exit erofs_module_exit(void)
 {
 	unregister_filesystem(&erofs_fs_type);
-	erofs_fscache_unregister();
 
 	/* Ensure all RCU free inodes / pclusters are safe to be destroyed. */
 	rcu_barrier();
 
+	erofs_exit_sysfs();
+	erofs_fscache_unregister();
 	z_erofs_exit_zip_subsystem();
 	z_erofs_deflate_exit();
 	z_erofs_lzma_exit();
