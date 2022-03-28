0d4f18fc6b026ec6f7c13765e0570491ae41fdcd
Xin Hao <xhao@linux.alibaba.com>
2022-03-28 11:11:33 +0800

    anolis: mm/damon: Fix NUMA statistics bug
    ANBZ: #708
    ANBZ: #20937
    
    In the previous patch, we used NUMA simulation to count the cross-NUMA
    access of each process, However, we ignore a problem, that is threads,
    The values of thread and process 'PID' are different, so the statistics
    are inaccurate, Here we use the 'tgid' of "task_struct" values are
    consistent to resolve this bug.
    
    Fixes: e7c2c8a9182f ("anolis: mm/damon: Add 'damon_region' NUMA local and remote access statistics")
    Signed-off-by: Xin Hao <xhao@linux.alibaba.com>
    Signed-off-by: Rongwei Wang <rongwei.wang@linux.alibaba.com>
    Acked-by: Gang Deng <gavin.dg@linux.alibaba.com>
    Acked-by: Xunlei Pang <xlpang@linux.alibaba.com>
    Signed-off-by: Weilin Tong <tongweilin@linux.alibaba.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5316

--- mm/damon/core.c
+++ mm/damon/core.c
@@ -1621,7 +1621,6 @@ int damon_set_region_biggest_system_ram_default(struct damon_target *t,
 static struct damon_target *get_damon_target(struct task_struct *task)
 {
 	int i;
-	unsigned long id1, id2;
 	struct damon_target *t;
 
 	rcu_read_lock();
@@ -1633,13 +1632,15 @@ static struct damon_target *get_damon_target(struct task_struct *task)
 		damon_for_each_target(t, dbgfs_ctxs[i]) {
 			struct task_struct *ts = damon_get_task_struct(t);
 
-			if (ts) {
-				id1 = (unsigned long)pid_vnr((struct pid *)t->pid);
-				id2 = (unsigned long)pid_vnr(get_task_pid(task, PIDTYPE_PID));
+			if (!ts)
+				continue;
+
+			if (ts->tgid == task->tgid) {
 				put_task_struct(ts);
-				if (id1 == id2)
-					return t;
+				rcu_read_unlock();
+				return t;
 			}
+			put_task_struct(ts);
 		}
 	}
 	rcu_read_unlock();
