0c7450aa1013211300623462a93fa0254668482a
Lu Baolu <baolu.lu@linux.intel.com>
2022-04-18 08:49:50 +0800

    iommu: Add DMA ownership management interfaces
    ANBZ: #26970
    
    KOSBZ: #0000233
    
    commit 1ea2a07a532b0e22aabe7e8483f935c672b9e7ed upstream.
    
    Multiple devices may be placed in the same IOMMU group because they
    cannot be isolated from each other. These devices must either be
    entirely under kernel control or userspace control, never a mixture.
    
    This adds dma ownership management in iommu core and exposes several
    interfaces for the device drivers and the device userspace assignment
    framework (i.e. VFIO), so that any conflict between user and kernel
    controlled dma could be detected at the beginning.
    
    The device driver oriented interfaces are,
    
    	int iommu_device_use_default_domain(struct device *dev);
    	void iommu_device_unuse_default_domain(struct device *dev);
    
    By calling iommu_device_use_default_domain(), the device driver tells
    the iommu layer that the device dma is handled through the kernel DMA
    APIs. The iommu layer will manage the IOVA and use the default domain
    for DMA address translation.
    
    The device user-space assignment framework oriented interfaces are,
    
    	int iommu_group_claim_dma_owner(struct iommu_group *group,
    					void *owner);
    	void iommu_group_release_dma_owner(struct iommu_group *group);
    	bool iommu_group_dma_owner_claimed(struct iommu_group *group);
    
    The device userspace assignment must be disallowed if the DMA owner
    claiming interface returns failure.
    
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>
    Signed-off-by: Kevin Tian <kevin.tian@intel.com>
    Signed-off-by: Lu Baolu <baolu.lu@linux.intel.com>
    Reviewed-by: Robin Murphy <robin.murphy@arm.com>
    Link: https://lore.kernel.org/r/20220418005000.897664-2-baolu.lu@linux.intel.com
    Signed-off-by: Joerg Roedel <jroedel@suse.de>
    Reviewed-by: Guanghui Feng <guanghuifeng@linux.alibaba.com>
    Reviewed-by: Guixin Liu <kanie@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6004

--- include/linux/iommu.h
+++ include/linux/iommu.h
@@ -766,6 +766,13 @@ struct iommu_sva *iommu_sva_bind_device(struct device *dev,
 void iommu_sva_unbind_device(struct iommu_sva *handle);
 u32 iommu_sva_get_pasid(struct iommu_sva *handle);
 
+int iommu_device_use_default_domain(struct device *dev);
+void iommu_device_unuse_default_domain(struct device *dev);
+
+int iommu_group_claim_dma_owner(struct iommu_group *group, void *owner);
+void iommu_group_release_dma_owner(struct iommu_group *group);
+bool iommu_group_dma_owner_claimed(struct iommu_group *group);
+
 int iommu_attach_device_pasid(struct iommu_domain *domain,
 			      struct device *dev, ioasid_t pasid);
 void iommu_detach_device_pasid(struct iommu_domain *domain,
@@ -1265,6 +1272,30 @@ static ioasid_t iommu_get_pasid_from_domain(struct device *dev,
 					    struct iommu_domain *domain)
 {
 	return INVALID_IOASID;
+}
+
+static inline int iommu_device_use_default_domain(struct device *dev)
+{
+	return 0;
+}
+
+static inline void iommu_device_unuse_default_domain(struct device *dev)
+{
+}
+
+static inline int
+iommu_group_claim_dma_owner(struct iommu_group *group, void *owner)
+{
+	return -ENODEV;
+}
+
+static inline void iommu_group_release_dma_owner(struct iommu_group *group)
+{
+}
+
+static inline bool iommu_group_dma_owner_claimed(struct iommu_group *group)
+{
+	return false;
 }
 #endif /* CONFIG_IOMMU_API */
 
