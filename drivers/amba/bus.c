320d02575abb99d75a19bbcf8a66e2bc5eb52134
Lu Baolu <baolu.lu@linux.intel.com>
2022-04-18 08:49:52 +0800

    amba: Stop sharing platform_dma_configure()
    ANBZ: #26970
    
    KOSBZ: #0000233
    
    commit 4a6d9dd564d0e7339fc15ecc5ce66db4ad842be2 upstream.
    
    Stop sharing platform_dma_configure() helper as they are about to have
    their own bus dma_configure callbacks.
    
    Signed-off-by: Lu Baolu <baolu.lu@linux.intel.com>
    Reviewed-by: Jason Gunthorpe <jgg@nvidia.com>
    Link: https://lore.kernel.org/r/20220418005000.897664-4-baolu.lu@linux.intel.com
    Signed-off-by: Joerg Roedel <jroedel@suse.de>
    Signed-off-by: Chu Guangqing <chuguangqing@inspur.com>

--- drivers/amba/bus.c
+++ drivers/amba/bus.c
@@ -21,6 +21,8 @@
 #include <linux/reset.h>
 
 #include <asm/irq.h>
+#include <linux/of_device.h>
+#include <linux/acpi.h>
 
 #define to_amba_driver(d)	container_of(d, struct amba_driver, drv)
 
@@ -152,6 +154,21 @@ static struct attribute *amba_dev_attrs[] = {
 };
 ATTRIBUTE_GROUPS(amba_dev);
 
+static int amba_dma_configure(struct device *dev)
+{
+	enum dev_dma_attr attr;
+	int ret = 0;
+
+	if (dev->of_node) {
+		ret = of_dma_configure(dev, dev->of_node, true);
+	} else if (has_acpi_companion(dev)) {
+		attr = acpi_get_dma_attr(to_acpi_device_node(dev->fwnode));
+		ret = acpi_dma_configure(dev, attr);
+	}
+
+	return ret;
+}
+
 #ifdef CONFIG_PM
 /*
  * Hooks to provide runtime PM of the pclk (bus clock).  It is safe to
@@ -217,7 +234,7 @@ struct bus_type amba_bustype = {
 	.dev_groups	= amba_dev_groups,
 	.match		= amba_match,
 	.uevent		= amba_uevent,
-	.dma_configure	= platform_dma_configure,
+	.dma_configure	= amba_dma_configure,
 	.pm		= &amba_pm,
 };
 EXPORT_SYMBOL_GPL(amba_bustype);
