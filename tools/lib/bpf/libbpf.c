e667eb204f762d096ceed8fef41c6eea79d9c769
Andrii Nakryiko <andrii@kernel.org>
2022-04-18 17:24:50 -0700

    libbpf: Support opting out from autoloading BPF programs declaratively
    ANBZ: #9462
    
    commit a3820c48111247f4ec2ca2949597f8fa57d2c424 upstream.
    
    Establish SEC("?abc") naming convention (i.e., adding question mark in
    front of otherwise normal section name) that allows to set corresponding
    program's autoload property to false. This is effectively just
    a declarative way to do bpf_program__set_autoload(prog, false).
    
    Having a way to do this declaratively in BPF code itself is useful and
    convenient for various scenarios. E.g., for testing, when BPF object
    consists of multiple independent BPF programs that each needs to be
    tested separately. Opting out all of them by default and then setting
    autoload to true for just one of them at a time simplifies testing code
    (see next patch for few conversions in BPF selftests taking advantage of
    this new feature).
    
    Another real-world use case is in libbpf-tools for cases when different
    BPF programs have to be picked depending on particulars of the host
    kernel due to various incompatible changes (like kernel function renames
    or signature change, or to pick kprobe vs fentry depending on
    corresponding kernel support for the latter). Marking all the different
    BPF program candidates as non-autoloaded declaratively makes this more
    obvious in BPF source code and allows simpler code in user-space code.
    
    When BPF program marked as SEC("?abc") it is otherwise treated just like
    SEC("abc") and bpf_program__section_name() reported will be "abc".
    
    Signed-off-by: Andrii Nakryiko <andrii@kernel.org>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Link: https://lore.kernel.org/bpf/20220419002452.632125-1-andrii@kernel.org
    Signed-off-by: Tianchen Ding <dtcccc@linux.alibaba.com>

--- tools/lib/bpf/libbpf.c
+++ tools/lib/bpf/libbpf.c
@@ -273,7 +273,7 @@ struct bpf_program {
 	void *priv;
 	bpf_program_clear_priv_t clear_priv;
 
-	bool load;
+	bool autoload;
 	enum bpf_prog_type type;
 	enum bpf_attach_type expected_attach_type;
 	int prog_ifindex;
@@ -588,7 +588,18 @@ bpf_object__init_prog(struct bpf_object *obj, struct bpf_program *prog,
 	prog->insns_cnt = prog->sec_insn_cnt;
 
 	prog->type = BPF_PROG_TYPE_UNSPEC;
-	prog->load = true;
+
+	/* libbpf's convention for SEC("?abc...") is that it's just like
+	 * SEC("abc...") but the corresponding bpf_program starts out with
+	 * autoload set to false.
+	 */
+	if (sec_name[0] == '?') {
+		prog->autoload = false;
+		/* from now on forget there was ? in section name */
+		sec_name++;
+	} else {
+		prog->autoload = true;
+	}
 
 	prog->instances.fds = NULL;
 	prog->instances.nr = -1;
@@ -2558,7 +2569,7 @@ static int bpf_object__load_vmlinux_btf(struct bpf_object *obj)
 	}
 
 	bpf_object__for_each_program(prog, obj) {
-		if (!prog->load)
+		if (!prog->autoload)
 			continue;
 		if (libbpf_prog_needs_vmlinux_btf(prog)) {
 			need_vmlinux_btf = true;
@@ -6016,7 +6027,7 @@ bpf_object__relocate_core(struct bpf_object *obj, const char *targ_btf_path)
 			/* no need to apply CO-RE relocation if the program is
 			 * not going to be loaded
 			 */
-			if (!prog->load)
+			if (!prog->autoload)
 				continue;
 
 			err = bpf_core_apply_relo(prog, rec, i, obj->btf,
@@ -6989,7 +7000,7 @@ bpf_object__load_progs(struct bpf_object *obj, int log_level)
 		prog = &obj->programs[i];
 		if (prog_is_subprog(obj, prog))
 			continue;
-		if (!prog->load) {
+		if (!prog->autoload) {
 			pr_debug("prog '%s': skipped loading\n", prog->name);
 			continue;
 		}
@@ -8203,7 +8214,7 @@ const char *bpf_program__title(const struct bpf_program *prog, bool needs_copy)
 
 bool bpf_program__autoload(const struct bpf_program *prog)
 {
-	return prog->load;
+	return prog->autoload;
 }
 
 int bpf_program__set_autoload(struct bpf_program *prog, bool autoload)
@@ -8211,7 +8222,7 @@ int bpf_program__set_autoload(struct bpf_program *prog, bool autoload)
 	if (prog->obj->loaded)
 		return -EINVAL;
 
-	prog->load = autoload;
+	prog->autoload = autoload;
 	return 0;
 }
 
@@ -10990,7 +11001,7 @@ int bpf_object__attach_skeleton(struct bpf_object_skeleton *s)
 		struct bpf_link **link = s->progs[i].link;
 		const struct bpf_sec_def *sec_def;
 
-		if (!prog->load)
+		if (!prog->autoload)
 			continue;
 
 		sec_def = find_sec_def(prog->sec_name);
