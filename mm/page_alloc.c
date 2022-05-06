44b00c51cf26a7c7878a3be77bedb5120ba5ba91
Gang Deng <gavin.dg@linux.alibaba.com>
2022-05-06 10:20:10 +0800

    anolis: mm, kidled: optimize kidled_set_page_age() call
    ANBZ: #22925
    ANBZ: #1115
    
    kidled_set_page_age() will be called each time in free_pages_prepare(),
    even when kidled was disabled. It'll leads performance degradation
    when free pages frequently (e.g. sockperf test).
    
    We set the age be zero directly to avoid a function call and the
    cmpxchg instruction when idle age is stored in page's flag field.
    Note we can just do this when free the page because others shouldn't
    write the flag at the same time.
    
    Signed-off-by: Gang Deng <gavin.dg@linux.alibaba.com>
    Reviewed-by: Xu Yu <xuyu@linux.alibaba.com>
    Reviewed-by: zhongjiang-ali <zhongjiang-ali@linux.alibaba.com>
    Acked-by: Xunlei Pang <xlpang@linux.alibaba.com>
    Signed-off-by: Weilin Tong <tongweilin@linux.alibaba.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5530

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -1154,8 +1154,12 @@ __always_inline bool free_pages_prepare(struct page *page,
 			 * maintain age information for each page in the
 			 * compound page, So we have to clear them one by one.
 			 */
+#if defined(CONFIG_KIDLED) && !defined(KIDLED_AGE_NOT_IN_PAGE_FLAGS)
+			(page + i)->flags &= ~(KIDLED_AGE_MASK << KIDLED_AGE_PGSHIFT);
+#else
 			kidled_set_folio_age(page_pgdat(page + i),
 					    page_to_pfn(page + i), 0);
+#endif
 			(page + i)->flags &= ~PAGE_FLAGS_CHECK_AT_PREP;
 		}
 	}
@@ -1174,7 +1178,11 @@ __always_inline bool free_pages_prepare(struct page *page,
 	}
 
 	page_cpupid_reset_last(page);
+#if defined(CONFIG_KIDLED) && !defined(KIDLED_AGE_NOT_IN_PAGE_FLAGS)
+	page->flags &= ~(KIDLED_AGE_MASK << KIDLED_AGE_PGSHIFT);
+#else
 	kidled_set_folio_age(page_pgdat(page), page_to_pfn(page), 0);
+#endif
 	page->flags &= ~PAGE_FLAGS_CHECK_AT_PREP | __PG_KFENCE;
 	reset_page_owner(page, order);
 	page_table_check_free(page, order);
