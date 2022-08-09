1d43e86c1775ec32e965dc0cf484b3a3695d8b8c
Liu Jian <liujian56@huawei.com>
2022-08-09 17:49:15 +0800

    skmsg: Fix wrong last sg check in sk_msg_recvmsg()
    ANBZ: #9462
    
    commit 583585e48d965338e73e1eb383768d16e0922d73 upstream.
    
    Fix one kernel NULL pointer dereference as below:
    
    [  224.462334] Call Trace:
    [  224.462394]  __tcp_bpf_recvmsg+0xd3/0x380
    [  224.462441]  ? sock_has_perm+0x78/0xa0
    [  224.462463]  tcp_bpf_recvmsg+0x12e/0x220
    [  224.462494]  inet_recvmsg+0x5b/0xd0
    [  224.462534]  __sys_recvfrom+0xc8/0x130
    [  224.462574]  ? syscall_trace_enter+0x1df/0x2e0
    [  224.462606]  ? __do_page_fault+0x2de/0x500
    [  224.462635]  __x64_sys_recvfrom+0x24/0x30
    [  224.462660]  do_syscall_64+0x5d/0x1d0
    [  224.462709]  entry_SYSCALL_64_after_hwframe+0x65/0xca
    
    In commit 9974d37ea75f ("skmsg: Fix invalid last sg check in
    sk_msg_recvmsg()"), we change last sg check to sg_is_last(),
    but in sockmap redirection case (without stream_parser/stream_verdict/
    skb_verdict), we did not mark the end of the scatterlist. Check the
    sk_msg_alloc, sk_msg_page_add, and bpf_msg_push_data functions, they all
    do not mark the end of sg. They are expected to use sg.end for end
    judgment. So the judgment of '(i != msg_rx->sg.end)' is added back here.
    
    Fixes: 9974d37ea75f ("skmsg: Fix invalid last sg check in sk_msg_recvmsg()")
    Signed-off-by: Liu Jian <liujian56@huawei.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: John Fastabend <john.fastabend@gmail.com>
    Acked-by: Jakub Sitnicki <jakub@cloudflare.com>
    Link: https://lore.kernel.org/bpf/20220809094915.150391-1-liujian56@huawei.com
    [dtcccc: move the change to net/ipv4/tcp_bpf.c]
    Signed-off-by: Tianchen Ding <dtcccc@linux.alibaba.com>

--- net/ipv4/tcp_bpf.c
+++ net/ipv4/tcp_bpf.c
@@ -66,7 +66,7 @@ int __tcp_bpf_recvmsg(struct sock *sk, struct sk_psock *psock,
 
 			if (copied == len)
 				break;
-		} while (!sg_is_last(sge));
+		} while ((i != msg_rx->sg.end) && !sg_is_last(sge));
 
 		if (unlikely(peek)) {
 			if (msg_rx == list_last_entry(&psock->ingress_msg,
@@ -77,7 +77,7 @@ int __tcp_bpf_recvmsg(struct sock *sk, struct sk_psock *psock,
 		}
 
 		msg_rx->sg.start = i;
-		if (!sge->length && sg_is_last(sge)) {
+		if (!sge->length && (i == msg_rx->sg.end || sg_is_last(sge))) {
 			list_del(&msg_rx->list);
 			if (msg_rx->skb)
 				consume_skb(msg_rx->skb);
