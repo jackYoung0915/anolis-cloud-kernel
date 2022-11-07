2cd5051e7927449c72c2767ce518d587caa893b7
Ke Chen <chenke54@huawei.com>
2022-11-07 19:53:01 +0800

    net: hns3: HNAE3 framework add support for ROH client
    ANBZ: #19067
    
    commit f10325965f9cb671690602afd21b3b982d63cb95 openEuler.
    
    driver inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I9A3QT
    CVE: NA
    
    ----------------------------------------------------------------------
    
    HNAE3 framework supports ROH clients to register with HNAE3
    devices and their associated operations.
    
    The ROH driver works as a client at the HNAE layer. The NIC
    driver needs to provide some necessary information, such as
    the vector base address, and suppor the registration of the
    ROH client.
    
    This patch also supports roh device IDs in the hns3 and hclge
    modules.
    
    Signed-off-by: Yufeng Mo <moyufeng@huawei.com>
    Signed-off-by: Ke Chen <chenke54@huawei.com>
    Reviewed-by: Gang Zhang <gang.zhang@huawei.com>
    Reviewed-by: Yefeng Yan <yanyefeng@huawei.com>
    Reviewed-by: Jingchao Dai <daijingchao1@huawei.com>
    Reviewed-by: Jian Shen <shenjian15@huawei.com>
    Signed-off-by: Jiantao Xiao <xiaojiantao1@h-partners.com>
    Signed-off-by: wangkaiyuan <wangkaiyuan@inspur.com>
    Reviewed-by: Bitao Hu <yaoma@linux.alibaba.com>
    Reviewed-by: Guixin Liu <kanie@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/4864

--- drivers/net/ethernet/hisilicon/hns3/hnae3.c
+++ drivers/net/ethernet/hisilicon/hns3/hnae3.c
@@ -58,7 +58,8 @@ EXPORT_SYMBOL(hnae3_release_unload_lock);
 static bool hnae3_client_match(enum hnae3_client_type client_type)
 {
 	if (client_type == HNAE3_CLIENT_KNIC ||
-	    client_type == HNAE3_CLIENT_ROCE)
+	    client_type == HNAE3_CLIENT_ROCE ||
+	    client_type == HNAE3_CLIENT_ROH)
 		return true;
 
 	return false;
@@ -77,6 +78,9 @@ void hnae3_set_client_init_flag(struct hnae3_client *client,
 		break;
 	case HNAE3_CLIENT_ROCE:
 		hnae3_set_bit(ae_dev->flag, HNAE3_ROCE_CLIENT_INITED_B, inited);
+		break;
+	case HNAE3_CLIENT_ROH:
+		hnae3_set_bit(ae_dev->flag, HNAE3_ROH_CLIENT_INITED_B, inited);
 		break;
 	default:
 		break;
@@ -97,6 +101,10 @@ static int hnae3_get_client_init_flag(struct hnae3_client *client,
 	case HNAE3_CLIENT_ROCE:
 		inited = hnae3_get_bit(ae_dev->flag,
 				       HNAE3_ROCE_CLIENT_INITED_B);
+		break;
+	case HNAE3_CLIENT_ROH:
+		inited = hnae3_get_bit(ae_dev->flag,
+				       HNAE3_ROH_CLIENT_INITED_B);
 		break;
 	default:
 		break;
