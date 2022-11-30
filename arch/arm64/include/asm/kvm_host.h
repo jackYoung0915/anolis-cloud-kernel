a1f64b518d384b799ba5aacce71ac329f32c208e
Quan Zhou <zhouquan65@huawei.com>
2022-11-30 18:23:43 +0800

    KVM: arm64: Implement the capability of DVMBM
    ANBZ: #11194
    
    commit 1489b74dd421820c008ae0da42e828244e128fe5 openeuler
    
    virt inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I62Q2L
    CVE: NA
    
    ----------------------------------------------------
    
    Implement the capability of DVMBM. Before each vcpu is loaded, we
    re-calculate the VM-wide dvm_cpumask, and if it's changed we will kick all
    other vcpus out to reload the latest LSUDVMBM value to the register, and a
    new request KVM_REQ_RELOAD_DVMBM is added to implement this.
    
    Otherwise if the dvm_cpumask is not changed by this single vcpu, in order
    to ensure the correctness of the contents in the register, we reload the
    LSUDVMBM value to the register and nothing else will be done.
    
    Signed-off-by: Quan Zhou <zhouquan65@huawei.com>
    Reviewed-by: Zenghui Yu <yuzenghui@huawei.com>
    Reviewed-by: Nianyao Tang <tangnianyao@huawei.com>
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>
    Signed-off-by: Min Li <gumi@linux.alibaba.com>
    
     Conflicts:
    	arch/arm64/include/asm/kvm_host.h
    	arch/arm64/kvm/arm.c

--- arch/arm64/include/asm/kvm_host.h
+++ arch/arm64/include/asm/kvm_host.h
@@ -45,6 +45,7 @@
 #define KVM_REQ_VCPU_RESET	KVM_ARCH_REQ(2)
 #define KVM_REQ_RECORD_STEAL	KVM_ARCH_REQ(3)
 #define KVM_REQ_RELOAD_GICv4	KVM_ARCH_REQ(4)
+#define KVM_REQ_RELOAD_DVMBM	KVM_ARCH_REQ(6)
 
 #define KVM_DIRTY_LOG_MANUAL_CAPS   (KVM_DIRTY_LOG_MANUAL_PROTECT_ENABLE | \
 				     KVM_DIRTY_LOG_INITIALLY_SET)
@@ -123,6 +124,7 @@ struct kvm_arch {
 #ifdef CONFIG_KVM_HISI_VIRT
 	spinlock_t dvm_lock;
 	cpumask_t *dvm_cpumask;	/* Union of all vcpu's cpus_ptr */
+	u64 lsudvmbm_el2;
 #endif
 };
 
