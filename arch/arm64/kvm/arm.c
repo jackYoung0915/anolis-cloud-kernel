8e198b168cf72f84d91b4f3c909e7d98c38539dc
Quan Zhou <zhouquan65@huawei.com>
2022-11-30 18:23:42 +0800

    KVM: arm64: Add kvm_arch::dvm_cpumask and dvm_lock
    ANBZ: #11194
    
    commit f0a92b35997a9a71f58f8e3ccdb1692616036fa9 openeuler
    
    virt inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I62Q2L
    CVE: NA
    
    ----------------------------------------------------
    
    Introduce dvm_cpumask and dvm_lock in struct kvm_arch. dvm_cpumask will
    store the union of all vcpus' cpus_ptr and will be used for the TLBI
    broadcast range. dvm_lock ensures a exclusive manipulation of dvm_cpumask.
    
    In vcpu_load, we should decide whether to perform the subsequent update
    operation by checking whether dvm_cpumask has changed.
    
    Signed-off-by: Quan Zhou <zhouquan65@huawei.com>
    Reviewed-by: Zenghui Yu <yuzenghui@huawei.com>
    Reviewed-by: Nianyao Tang <tangnianyao@huawei.com>
    Signed-off-by: Zheng Zengkai <zhengzengkai@huawei.com>
    Signed-off-by: Min Li <gumi@linux.alibaba.com>
    Reviewed-by: Bin Guo <guobin@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/4240

--- arch/arm64/kvm/arm.c
+++ arch/arm64/kvm/arm.c
@@ -144,6 +144,12 @@ int kvm_arch_init_vm(struct kvm *kvm, unsigned long type)
 {
 	int ret;
 
+#ifdef CONFIG_KVM_HISI_VIRT
+	ret = kvm_hisi_init_dvmbm(kvm);
+	if (ret)
+		return ret;
+#endif
+
 	ret = kvm_arm_setup_stage2(kvm, type);
 	if (ret)
 		return ret;
@@ -182,6 +188,10 @@ vm_fault_t kvm_arch_vcpu_fault(struct kvm_vcpu *vcpu, struct vm_fault *vmf)
 void kvm_arch_destroy_vm(struct kvm *kvm)
 {
 	int i;
+
+#ifdef CONFIG_KVM_HISI_VIRT
+	kvm_hisi_destroy_dvmbm(kvm);
+#endif
 
 	bitmap_free(kvm->arch.pmu_filter);
 
