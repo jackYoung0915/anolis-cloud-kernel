b6a735446cf8ba2934fc35e77feb738fc9cfcb8e
Xin Hao <xhao@linux.alibaba.com>
2022-12-02 19:27:01 +0800

    anolis: mm: unevictable: add memcg relative interface support
    ANBZ: #18709
    
    ANBZ: #3351
    
    In order to support memcg granularity text section unevictable, there
    add two interface memory.{allow_text_unevictable, text_unevictable_percent}
    to allow each memcg to enable text section unevitable function.
    
    Signed-off-by: Xin Hao <xhao@linux.alibaba.com>
    Reviewed-by: Xu Yu <xuyu@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/953
    Signed-off-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/4573

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -5930,6 +5930,51 @@ static int mem_cgroup_slab_show(struct seq_file *m, void *p)
 
 static int memory_stat_show(struct seq_file *m, void *v);
 
+#ifdef CONFIG_TEXT_UNEVICTABLE
+static u64 mem_cgroup_allow_unevictable_read(struct cgroup_subsys_state *css,
+					     struct cftype *cft)
+{
+	struct mem_cgroup *memcg = mem_cgroup_from_css(css);
+
+	return memcg->allow_unevictable;
+}
+
+static int mem_cgroup_allow_unevictable_write(struct cgroup_subsys_state *css,
+					      struct cftype *cft, u64 val)
+{
+	struct mem_cgroup *memcg = mem_cgroup_from_css(css);
+
+	if (val > 1)
+		return -EINVAL;
+	if (memcg->allow_unevictable == val)
+		return 0;
+
+	memcg->allow_unevictable = val;
+
+	return 0;
+}
+
+static u64 mem_cgroup_unevictable_percent_read(struct cgroup_subsys_state *css,
+					       struct cftype *cft)
+{
+	struct mem_cgroup *memcg = mem_cgroup_from_css(css);
+
+	return memcg->unevictable_percent;
+}
+
+static int mem_cgroup_unevictable_percent_write(struct cgroup_subsys_state *css,
+						struct cftype *cft, u64 val)
+{
+	struct mem_cgroup *memcg = mem_cgroup_from_css(css);
+
+	if (val > 100)
+		return -EINVAL;
+
+	memcg->unevictable_percent = val;
+	return 0;
+}
+#endif
+
 #ifdef CONFIG_PAGECACHE_LIMIT
 static u64 mem_cgroup_allow_pgcache_limit_read(struct cgroup_subsys_state *css,
 					     struct cftype *cft)
@@ -6252,6 +6297,18 @@ static struct cftype mem_cgroup_legacy_files[] = {
 		.write_u64 = mem_cgroup_allow_pgcache_sync_write,
 	},
 #endif
+#ifdef CONFIG_TEXT_UNEVICTABLE
+	{
+		.name = "allow_text_unevictable",
+		.read_u64 = mem_cgroup_allow_unevictable_read,
+		.write_u64 = mem_cgroup_allow_unevictable_write,
+	},
+	{
+		.name = "text_unevictable_percent",
+		.read_u64 = mem_cgroup_unevictable_percent_read,
+		.write_u64 = mem_cgroup_unevictable_percent_write,
+	},
+ #endif
 	{ },	/* terminate */
 };
 
@@ -6518,6 +6575,9 @@ mem_cgroup_css_alloc(struct cgroup_subsys_state *parent_css)
 	memcg->zswap_max = PAGE_COUNTER_MAX;
 #endif
 	page_counter_set_high(&memcg->swap, PAGE_COUNTER_MAX);
+#ifdef CONFIG_TEXT_UNEVICTABLE
+	memcg->unevictable_percent = 100;
+#endif
 	if (parent) {
 		WRITE_ONCE(memcg->swappiness, mem_cgroup_swappiness(parent));
 		WRITE_ONCE(memcg->oom_kill_disable, READ_ONCE(parent->oom_kill_disable));
@@ -6534,6 +6594,9 @@ mem_cgroup_css_alloc(struct cgroup_subsys_state *parent_css)
 #ifdef CONFIG_PAGECACHE_LIMIT
 		memcg->allow_pgcache_limit = parent->allow_pgcache_limit;
 		memcg->pgcache_limit_sync = parent->pgcache_limit_sync;
+#endif
+#ifdef CONFIG_TEXT_UNEVICTABLE
+		memcg->allow_unevictable = parent->allow_unevictable;
 #endif
 		page_counter_init(&memcg->memory, &parent->memory);
 		page_counter_init(&memcg->swap, &parent->swap);
