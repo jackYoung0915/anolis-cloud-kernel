a154b5642aa754f7971f1e208860610d26a5dad0
Xin Hao <xhao@linux.alibaba.com>
2023-01-10 04:14:05 +0800

    anolis: mm: pagecache_limit: add memcg granularity related inferface
    ANBZ: #12235
    ANBZ: #3907
    
    From this patch, the "pagecache_limit" feature begin to support memcg
    granularity pagecache reclaim function, this patch is aimed to add some
    key interfaces to configure this function.
    
    Signed-off-by: Xin Hao <xhao@linux.alibaba.com>
    Reviewed-by: Xu Yu <xuyu@linux.alibaba.com>
    Reviewed-by: Rongwei Wang <rongwei.wang@linux.alibaba.com>
    Reviewed-by: Kaihao Bai <carlo.bai@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/1157
    Signed-off-by: Weilin Tong <tongweilin@linux.alibaba.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/4207

--- include/linux/memcontrol.h
+++ include/linux/memcontrol.h
@@ -360,6 +360,11 @@ struct mem_cgroup {
 	struct deferred_split deferred_split_queue;
 #endif
 
+#ifdef CONFIG_PAGECACHE_LIMIT
+	bool allow_pgcache_limit;
+	unsigned long pgcache_limit_size;
+#endif
+
 #ifdef CONFIG_LRU_GEN
 	/* per-memcg mm_struct list */
 	struct lru_gen_mm_list mm_list;
