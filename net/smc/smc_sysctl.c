f15ef5c45795f06da6f3c96dfbde183e7dbbee1b
Guangguan Wang <guangguan.wang@linux.alibaba.com>
2023-05-08 22:17:08 +0800

    anolis: net/smc: support experiment_vendor_options sysctl
    ANBZ: #18818
    
    This patch is aggregated for experiment_vendor_options sysctl:
    [1] 8a2793ba28a9 ("anolis: net/smc: add sysctl for alibaba vendor unique exp options")
    [2] 8320bf304873 ("anolis: net/smc: change sysctl vendor_exp_option to experiment_vendor_options")
    [3] 6fe99b554d7b ("anolis: net/smc: add experiment_vendor_options sysctl description to documentation")
    
    Add sysctl for alibaba vendor unique exp options. Sysctl values
    can control all of the exp options to switch on/off, and also
    can control single option to switch on/off.
    
    Sysctl_vendor_exp_options is defined in the same format of
    struct smc_clc_vendor_opt_ali, and the value of the corresponding
    field ctontrols the corresponding option to switch on/off.
    
    Signed-off-by: Guangguan Wang <guangguan.wang@linux.alibaba.com>
    Reviewed-by: Tony Lu <tonylu@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/1759
    Reviewed-by: D. Wythe <alibuda@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/4611

--- net/smc/smc_sysctl.c
+++ net/smc/smc_sysctl.c
@@ -90,6 +90,13 @@ static struct ctl_table smc_table[] = {
 		.extra1		= &conns_per_lgr_min,
 		.extra2		= &conns_per_lgr_max,
 	},
+	{
+		.procname	= "experiment_vendor_options",
+		.data		= &init_net.smc.sysctl_vendor_exp_options,
+		.maxlen		= sizeof(init_net.smc.sysctl_vendor_exp_options),
+		.mode		= 0644,
+		.proc_handler	= proc_douintvec,
+	},
 	{  }
 };
 
@@ -116,6 +123,7 @@ int __net_init smc_sysctl_net_init(struct net *net)
 
 	net->smc.sysctl_autocorking_size = SMC_AUTOCORKING_DEFAULT_SIZE;
 	net->smc.sysctl_smcr_buf_type = SMCR_MIXED_BUFS;
+	net->smc.sysctl_vendor_exp_options = ~0U;
 	net->smc.sysctl_smcr_testlink_time = SMC_LLC_TESTLINK_DEFAULT_TIME;
 	net->smc.sysctl_wmem = 262144; /* 256 KiB */
 	net->smc.sysctl_rmem = 262144; /* 256 KiB */
