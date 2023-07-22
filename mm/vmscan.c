83be902bbe5a6ff87648dce52fa2b5e554a1bf75
Xu Yu <xuyu@linux.alibaba.com>
2023-07-22 12:25:08 +0800

    anolis: mm: make min_cache_kbytes behave explicitly
    ANBZ: #12338
    ANBZ: #6025
    
    The page cache reserve feature, i.e., min_cache_kbytes, does not take
    effect in some scenarios. This is because that min_cache_kbytes is
    subject to sc->may_deactivate and min_wmark.
    
    This makes min_cache_kbytes behave explicitly, i.e., eliminating
    additional restrictions such as sc->may_deactivate and min_wmark.
    
    Fixes: 1f86172dbe16 ("anolis: mm: introduce ability to reserve page cache on system wide")
    Signed-off-by: Xu Yu <xuyu@linux.alibaba.com>
    Reviewed-by: zhongjiang-ali <zhongjiang-ali@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/1968
    Signed-off-by: Weilin Tong <tongweilin@linux.alibaba.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/4219
    Signed-off-by: Kaihao Bai <carlo.bai@linux.alibaba.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -2372,7 +2372,6 @@ static void prepare_scan_control(pg_data_t *pgdat, struct scan_control *sc)
 	 */
 	if (!cgroup_reclaim(sc)) {
 		unsigned long total_high_wmark = 0;
-		unsigned long total_min_wmark = 0;
 		unsigned long free, anon;
 		unsigned long min_cache_kbytes;
 		int z;
@@ -2384,7 +2383,6 @@ static void prepare_scan_control(pg_data_t *pgdat, struct scan_control *sc)
 
 		for_each_managed_zone_pgdat(zone, pgdat, z, MAX_NR_ZONES - 1) {
 			total_high_wmark += high_wmark_pages(zone);
-			total_min_wmark += min_wmark_pages(zone);
 		}
 
 		/*
@@ -2405,10 +2403,8 @@ static void prepare_scan_control(pg_data_t *pgdat, struct scan_control *sc)
 		 * given watermark.
 		 */
 		min_cache_kbytes = READ_ONCE(sysctl_min_cache_kbytes);
-		if (min_cache_kbytes) {
-			sc->file_is_reserved = (sc->may_deactivate & DEACTIVATE_FILE) &&
-					file <= min(total_min_wmark, pgdat->min_cache_pages);
-		}
+		if (min_cache_kbytes)
+			sc->file_is_reserved = file <= pgdat->min_cache_pages;
 	}
 }
 
