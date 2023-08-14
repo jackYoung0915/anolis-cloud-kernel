114f49aca048b15d77b97648e4d32f4f8d6bb638
Thomas Gleixner <tglx@linutronix.de>
2023-08-14 10:18:29 +0200

    x86/cpu: Encapsulate topology information in cpuinfo_x86
    ANBZ: #26668
    
    commit b9655e702dc5d856e5e05ae414b71708ca98b30c upstream.
    
    The topology related information is randomly scattered across cpuinfo_x86.
    
    Create a new structure cpuinfo_topo and move in a first step initial_apicid
    and apicid into it.
    
    Aside of being better readable this is in preparation for replacing the
    horribly fragile CPU topology evaluation code further down the road.
    
    Consolidate APIC ID fields to u32 as that represents the hardware type.
    
    No functional change.
    
    Intel-SIG: commit b9655e702dc5 x86/cpu: Encapsulate topology information in cpuinfo_x86.
    x86/cpu: Rework the topology evaluation - part1
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Tested-by: Juergen Gross <jgross@suse.com>
    Tested-by: Sohil Mehta <sohil.mehta@intel.com>
    Tested-by: Michael Kelley <mikelley@microsoft.com>
    Tested-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Tested-by: Zhang Rui <rui.zhang@intel.com>
    Acked-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lore.kernel.org/r/20230814085112.269787744@linutronix.de
    [ Quanxian Wang: amend commit log ]
    Signed-off-by: Quanxian Wang <quanxian.wang@intel.com>

--- arch/x86/kernel/cpu/proc.c
+++ arch/x86/kernel/cpu/proc.c
@@ -33,8 +33,8 @@ static void show_cpuinfo_core(struct seq_file *m, struct cpuinfo_x86 *c,
 		seq_printf(m, "core id\t\t: %d\n", c->cpu_core_id);
 		seq_printf(m, "cpu cores\t: %d\n", c->booted_cores);
 	}
-	seq_printf(m, "apicid\t\t: %d\n", c->apicid);
-	seq_printf(m, "initial apicid\t: %d\n", c->initial_apicid);
+	seq_printf(m, "apicid\t\t: %d\n", c->topo.apicid);
+	seq_printf(m, "initial apicid\t: %d\n", c->topo.initial_apicid);
 #endif
 }
 
