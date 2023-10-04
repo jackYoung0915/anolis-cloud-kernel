7a97727cc4e19385a347d25d63e40ad09d12891a
Clément Léger <cleger@rivosinc.com>
2023-10-04 17:14:04 +0200

    riscv: report misaligned accesses emulation to hwprobe
    ANBZ: #32730
    
    commit 71c54b3d169db5569655cbd2a3616bc701fd5eec upstream.
    
    hwprobe provides a way to report if misaligned access are emulated. In
    order to correctly populate that feature, we can check if it actually
    traps when doing a misaligned access. This can be checked using an
    exception table entry which will actually be used when a misaligned
    access is done from kernel mode.
    
    Signed-off-by: Clément Léger <cleger@rivosinc.com>
    Link: https://lore.kernel.org/r/20231004151405.521596-8-cleger@rivosinc.com
    Signed-off-by: Palmer Dabbelt <palmer@rivosinc.com>
    [Ruidong:
    Conflicts:
      resolve: arch/riscv/kernel/cpufeature.c
      reason:
        Context conflict only, patch content unchanged
    ]
    Signed-off-by: Ruidong Tian <tianruidong@linux.alibaba.com>
    Acked-by: Jakub Kicinski <kuba@kernel.org>
    Reviewed-by: Simon Horman <horms@kernel.org>
    Reviewed-by: Alexandre Ghiti <alexghiti@rivosinc.com>
    Reviewed-by: Fangyu Yu <fangyu.yu@linux.alibaba.com>
    Reviewed-by: Rui Gao <gao.rui@zte.com.cn>
    Reviewed-by: Jiankang Chen <jkchen@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6707

--- arch/riscv/kernel/cpufeature.c
+++ arch/riscv/kernel/cpufeature.c
@@ -976,6 +976,9 @@ void check_unaligned_access(int cpu)
 	void *src;
 	long speed = RISCV_HWPROBE_MISALIGNED_SLOW;
 
+	if (check_unaligned_access_emulated(cpu))
+		return;
+
 	/* We are already set since the last check */
 	if (per_cpu(misaligned_access_speed, cpu) != RISCV_HWPROBE_MISALIGNED_UNKNOWN)
 		return;
@@ -1060,6 +1063,7 @@ out:
 static int __init check_unaligned_access_boot_cpu(void)
 {
 	check_unaligned_access(0);
+	unaligned_emulation_finish();
 	return 0;
 }
 
