9b7b4dd0384a36265377fb70d41c5bc0a4acb4a0
Clément Léger <cleger@rivosinc.com>
2023-10-04 17:14:04 +0200

    riscv: report misaligned accesses emulation to hwprobe
    ANBZ: #30842
    
    commit 71c54b3d169db5569655cbd2a3616bc701fd5eec
    
    hwprobe provides a way to report if misaligned access are emulated. In
    order to correctly populate that feature, we can check if it actually
    traps when doing a misaligned access. This can be checked using an
    exception table entry which will actually be used when a misaligned
    access is done from kernel mode.
    
    Signed-off-by: Clément Léger <cleger@rivosinc.com>
    Link: https://lore.kernel.org/r/20231004151405.521596-8-cleger@rivosinc.com
    Signed-off-by: Palmer Dabbelt <palmer@rivosinc.com>
    Signed-off-by: Fangyu Yu <fangyu.yu@linux.alibaba.com>

--- arch/riscv/kernel/cpufeature.c
+++ arch/riscv/kernel/cpufeature.c
@@ -975,8 +975,7 @@ void check_unaligned_access(int cpu)
 	void *src;
 	long speed = RISCV_HWPROBE_MISALIGNED_SLOW;
 
-	/* We are already set since the last check */
-	if (per_cpu(misaligned_access_speed, cpu) != RISCV_HWPROBE_MISALIGNED_UNKNOWN)
+	if (check_unaligned_access_emulated(cpu))
 		return;
 
 	page = alloc_pages(GFP_NOWAIT, get_order(MISALIGNED_BUFFER_SIZE));
@@ -1059,6 +1058,7 @@ out:
 static int check_unaligned_access_boot_cpu(void)
 {
 	check_unaligned_access(0);
+	unaligned_emulation_finish();
 	return 0;
 }
 
