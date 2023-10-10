a518286cbc754e3f7a98e93a3ec8207ed05fa111
Alexander Shishkin <alexander.shishkin@linux.intel.com>
2023-10-10 17:52:19 +0300

    x86/sev: Move sev_setup_arch() to mem_encrypt.c
    ANBZ: #34453
    
    commit 6e74b125155dc8c747d76fb45d8e6d20e9e4fb4d upstream.
    
    Since commit:
    
      4d96f9109109b ("x86/sev: Replace occurrences of sev_active() with cc_platform_has()")
    
    ... the SWIOTLB bounce buffer size adjustment and restricted virtio memory
    setting also inadvertently apply to TDX: the code is using
    cc_platform_has(CC_ATTR_GUEST_MEM_ENCRYPT) as a gatekeeping condition,
    which is also true for TDX, and this is also what we want.
    
    To reflect this, move the corresponding code to generic mem_encrypt.c.
    
    No functional changes intended.
    
    Signed-off-by: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Reviewed-by: Tom Lendacky <thomas.lendacky@amd.com>
    Link: https://lore.kernel.org/r/20231010145220.3960055-2-alexander.shishkin@linux.intel.com
    Signed-off-by: Abhishek Rajput <Abhishek.Rajput@amd.com>
    Signed-off-by: mohanasv <mohanasv@amd.com>
    Reviewed-by: Zongyao Chen <zongyao.chen@linux.alibaba.com>
    Reviewed-by: Tianjia Zhang <tianjia.zhang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/7640

--- arch/x86/kernel/setup.c
+++ arch/x86/kernel/setup.c
@@ -1171,7 +1171,7 @@ void __init setup_arch(char **cmdline_p)
 	 * Needs to run after memblock setup because it needs the physical
 	 * memory size.
 	 */
-	sev_setup_arch();
+	mem_encrypt_setup_arch();
 	cc_random_init();
 
 	efi_fake_memmap();
