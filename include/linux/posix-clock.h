7a31fefad641887eb9768092a0e59860bdc24f47
Xabier Marquiegui <reibax@gmail.com>
2023-10-12 00:39:53 +0200

    posix-clock: introduce posix_clock_context concept
    ANBZ: #35228
    
    commit 221e4d1e29bb442ac4c47bc8ee095abf009ee8f3 stable.
    
    commit 60c6946675fc06dd2fd2b7a4b6fd1c1f046f1056 upstream.
    
    Add the necessary structure to support custom private-data per
    posix-clock user.
    
    The previous implementation of posix-clock assumed all file open
    instances need access to the same clock structure on private_data.
    
    The need for individual data structures per file open instance has been
    identified when developing support for multiple timestamp event queue
    users for ptp_clock.
    
    Signed-off-by: Xabier Marquiegui <reibax@gmail.com>
    Suggested-by: Richard Cochran <richardcochran@gmail.com>
    Suggested-by: Vinicius Costa Gomes <vinicius.gomes@intel.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Stable-dep-of: e859d375d169 ("posix-clock: Store file pointer in struct posix_clock_context")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Weilin Tong <tongweilin@linux.alibaba.com>

--- include/linux/posix-clock.h
+++ include/linux/posix-clock.h
@@ -14,6 +14,7 @@
 #include <linux/rwsem.h>
 
 struct posix_clock;
+struct posix_clock_context;
 
 /**
  * struct posix_clock_operations - functional interface to the clock
@@ -50,18 +51,18 @@ struct posix_clock_operations {
 	/*
 	 * Optional character device methods:
 	 */
-	long    (*ioctl)   (struct posix_clock *pc,
-			    unsigned int cmd, unsigned long arg);
+	long (*ioctl)(struct posix_clock_context *pccontext, unsigned int cmd,
+		      unsigned long arg);
 
-	int     (*open)    (struct posix_clock *pc, fmode_t f_mode);
+	int (*open)(struct posix_clock_context *pccontext, fmode_t f_mode);
 
-	__poll_t (*poll)   (struct posix_clock *pc,
-			    struct file *file, poll_table *wait);
+	__poll_t (*poll)(struct posix_clock_context *pccontext, struct file *file,
+			 poll_table *wait);
 
-	int     (*release) (struct posix_clock *pc);
+	int (*release)(struct posix_clock_context *pccontext);
 
-	ssize_t (*read)    (struct posix_clock *pc,
-			    uint flags, char __user *buf, size_t cnt);
+	ssize_t (*read)(struct posix_clock_context *pccontext, uint flags,
+			char __user *buf, size_t cnt);
 
 	CK_KABI_RESERVE(1)
 	CK_KABI_RESERVE(2)
@@ -102,6 +103,24 @@ struct posix_clock {
 	CK_KABI_RESERVE(6)
 	CK_KABI_RESERVE(7)
 	CK_KABI_RESERVE(8)
+};
+
+/**
+ * struct posix_clock_context - represents clock file operations context
+ *
+ * @clk:              Pointer to the clock
+ * @private_clkdata:  Pointer to user data
+ *
+ * Drivers should use struct posix_clock_context during specific character
+ * device file operation methods to access the posix clock.
+ *
+ * Drivers can store a private data structure during the open operation
+ * if they have specific information that is required in other file
+ * operations.
+ */
+struct posix_clock_context {
+	struct posix_clock *clk;
+	void *private_clkdata;
 };
 
 /**
