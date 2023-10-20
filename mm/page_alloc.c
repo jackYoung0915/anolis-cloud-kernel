c9397b9baeb644c9722ac0a6fb841328523466d7
Liu Shixin <liushixin2@huawei.com>
2023-10-20 11:17:01 +0800

    mm/memory_hotplug: support to hotplug memory on ZONE_EXTMEM
    ANCK ID: #IJSBBS
    
    commit 37dd9a48ab30c3bece06e0ac0b567fb6f37a198e openEuler.
    
    Support to hotplug memory on ZONE_EXTMEM.
    
    Signed-off-by: Liu Shixin <liushixin2@huawei.com>
    Signed-off-by: Liu Jian <liujian56@huawei.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -6317,9 +6317,13 @@ static void __setup_per_zone_wmarks(void)
 	struct zone *zone;
 	unsigned long flags;
 
-	/* Calculate total number of !ZONE_HIGHMEM and !ZONE_MOVABLE pages */
+	/*
+	 * Calculate total number of !ZONE_HIGHMEM and !ZONE_MOVABLE and
+	 * !ZONE_EXTMEM pages.
+	 */
 	for_each_zone(zone) {
-		if (!is_highmem(zone) && zone_idx(zone) != ZONE_MOVABLE)
+		if (!is_highmem(zone) && zone_idx(zone) != ZONE_MOVABLE
+		    && !zone_is_zone_extmem(zone))
 			lowmem_pages += zone_managed_pages(zone);
 	}
 
@@ -6329,7 +6333,8 @@ static void __setup_per_zone_wmarks(void)
 		spin_lock_irqsave(&zone->lock, flags);
 		tmp = (u64)pages_min * zone_managed_pages(zone);
 		do_div(tmp, lowmem_pages);
-		if (is_highmem(zone) || zone_idx(zone) == ZONE_MOVABLE) {
+		if (is_highmem(zone) || zone_idx(zone) == ZONE_MOVABLE ||
+		    zone_is_zone_extmem(zone)) {
 			/*
 			 * __GFP_HIGH and PF_MEMALLOC allocations usually don't
 			 * need highmem and movable zones pages, so cap pages_min
@@ -6337,7 +6342,7 @@ static void __setup_per_zone_wmarks(void)
 			 *
 			 * The WMARK_HIGH-WMARK_LOW and (WMARK_LOW-WMARK_MIN)
 			 * deltas control async page reclaim, and so should
-			 * not be capped for highmem and movable zones.
+			 * not be capped for highmem, movable and extmem zones.
 			 */
 			unsigned long min_pages;
 
