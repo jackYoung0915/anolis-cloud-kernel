229040bc31c43b3a7a484976864ad206b7cd4596
Sean Christopherson <seanjc@google.com>
2023-10-27 11:22:07 -0700

    KVM: selftests: Convert lib's mem regions to KVM_SET_USER_MEMORY_REGION2
    ANBZ: #34453
    
    commit 8d99e347c097ab3f9fb93d0f88dddf20051d7c88 upstream.
    
    Use KVM_SET_USER_MEMORY_REGION2 throughout KVM's selftests library so that
    support for guest private memory can be added without needing an entirely
    separate set of helpers.
    
    Note, this obviously makes selftests backwards-incompatible with older KVM
    versions from this point forward.
    
    [Backport Changes]
    
    1. In tools/testing/selftests/kvm/lib/kvm_util.c, during the earlier
    backport of Anolis commit 818676db5e0c7b, the vm_ioctl()  function call,
    was moved from __vm_mem_region_delete() to vm_mem_region_delete() with
    KVM_SET_USER_MEMORY_REGION instead of KVM_SET_USER_MEMORY_REGION2
    deviating form upstream commit 5afe18dfa47dae. Since the current upstream
    commit replaces KVM_SET_USER_MEMORY_REGION with
    KVM_SET_USER_MEMORY_REGION2, fixed the deviation in the vm_ioctl() call to
    match with with upstream commit.
    
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    Message-Id: <20231027182217.3615211-26-seanjc@google.com>
    Reviewed-by: Fuad Tabba <tabba@google.com>
    Tested-by: Fuad Tabba <tabba@google.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Signed-off-by: Abhishek Rajput <Abhishek.Rajput@amd.com>
    Signed-off-by: mohanasv <mohanasv@amd.com>
    Reviewed-by: James Gowans <jgowans@amazon.com>
    Reviewed-by: Muhammad Usama Anjum <usama.anjum@collabora.com>
    Reviewed-by: Tianjia Zhang <tianjia.zhang@linux.alibaba.com>
    Reviewed-by: Zongyao Chen <zongyao.chen@linux.alibaba.com>
    Reviewed-by: Qinyun Tan <qinyuntan@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6878

--- tools/testing/selftests/kvm/lib/kvm_util.c
+++ tools/testing/selftests/kvm/lib/kvm_util.c
@@ -453,8 +453,9 @@ void kvm_vm_restart(struct kvm_vm *vmp)
 		vm_create_irqchip(vmp);
 
 	hash_for_each(vmp->regions.slot_hash, ctr, region, slot_node) {
-		int ret = ioctl(vmp->fd, KVM_SET_USER_MEMORY_REGION, &region->region);
-		TEST_ASSERT(ret == 0, "KVM_SET_USER_MEMORY_REGION IOCTL failed,\n"
+		int ret = ioctl(vmp->fd, KVM_SET_USER_MEMORY_REGION2, &region->region);
+
+		TEST_ASSERT(ret == 0, "KVM_SET_USER_MEMORY_REGION2 IOCTL failed,\n"
 			    "  rc: %i errno: %i\n"
 			    "  slot: %u flags: 0x%x\n"
 			    "  guest_phys_addr: 0x%llx size: 0x%llx",
@@ -1011,8 +1012,8 @@ void vm_userspace_mem_region_add(struct kvm_vm *vm,
 	region->region.guest_phys_addr = guest_paddr;
 	region->region.memory_size = npages * vm->page_size;
 	region->region.userspace_addr = (uintptr_t) region->host_mem;
-	ret = __vm_ioctl(vm, KVM_SET_USER_MEMORY_REGION, &region->region);
-	TEST_ASSERT(ret == 0, "KVM_SET_USER_MEMORY_REGION IOCTL failed,\n"
+	ret = __vm_ioctl(vm, KVM_SET_USER_MEMORY_REGION2, &region->region);
+	TEST_ASSERT(ret == 0, "KVM_SET_USER_MEMORY_REGION2 IOCTL failed,\n"
 		"  rc: %i errno: %i\n"
 		"  slot: %u flags: 0x%x\n"
 		"  guest_phys_addr: 0x%lx size: 0x%lx",
@@ -1094,9 +1095,9 @@ void vm_mem_region_set_flags(struct kvm_vm *vm, uint32_t slot, uint32_t flags)
 
 	region->region.flags = flags;
 
-	ret = __vm_ioctl(vm, KVM_SET_USER_MEMORY_REGION, &region->region);
+	ret = __vm_ioctl(vm, KVM_SET_USER_MEMORY_REGION2, &region->region);
 
-	TEST_ASSERT(ret == 0, "KVM_SET_USER_MEMORY_REGION IOCTL failed,\n"
+	TEST_ASSERT(ret == 0, "KVM_SET_USER_MEMORY_REGION2 IOCTL failed,\n"
 		"  rc: %i errno: %i slot: %u flags: 0x%x",
 		ret, errno, slot, flags);
 }
@@ -1124,9 +1125,9 @@ void vm_mem_region_move(struct kvm_vm *vm, uint32_t slot, uint64_t new_gpa)
 
 	region->region.guest_phys_addr = new_gpa;
 
-	ret = __vm_ioctl(vm, KVM_SET_USER_MEMORY_REGION, &region->region);
+	ret = __vm_ioctl(vm, KVM_SET_USER_MEMORY_REGION2, &region->region);
 
-	TEST_ASSERT(!ret, "KVM_SET_USER_MEMORY_REGION failed\n"
+	TEST_ASSERT(!ret, "KVM_SET_USER_MEMORY_REGION2 failed\n"
 		    "ret: %i errno: %i slot: %u new_gpa: 0x%lx",
 		    ret, errno, slot, new_gpa);
 }
@@ -1149,7 +1150,7 @@ void vm_mem_region_delete(struct kvm_vm *vm, uint32_t slot)
 	struct userspace_mem_region *region = memslot2region(vm, slot);
 
 	region->region.memory_size = 0;
-	vm_ioctl(vm, KVM_SET_USER_MEMORY_REGION, &region->region);
+	vm_ioctl(vm, KVM_SET_USER_MEMORY_REGION2, &region->region);
 
 	__vm_mem_region_delete(vm, region, true);
 }
