02fdda90c78c0669f512c4d141099ce485a2f72c
yangwencheng <yangwencheng@hygon.cn>
2023-11-16 16:32:43 +0800

    anolis: KVM: SVM: CSV: Add ioctl API to unpin shared pages of CSV3 guest
    ANBZ: #20684
    
    The shared pages between CSV3 guest and host are pinned in memory,
    and managed in list, they will be released to system till the guest
    VM was destroyed.
    
    The new ioctl API supports to unpin the shared pages, and remove
    them from the list.
    
    For shared memory allocated from guest user-space process, they
    must be unpinned dynamically when the process exits.
    
    Signed-off-by: yangwencheng <yangwencheng@hygon.cn>
    Signed-off-by: hanliyang <hanliyang@hygon.cn>

--- include/uapi/linux/kvm.h
+++ include/uapi/linux/kvm.h
@@ -2371,6 +2371,7 @@ enum csv3_cmd_id {
 	KVM_CSV3_SEND_ENCRYPT_CONTEXT,
 	KVM_CSV3_RECEIVE_ENCRYPT_DATA,
 	KVM_CSV3_RECEIVE_ENCRYPT_CONTEXT,
+	KVM_CSV3_HANDLE_MEMORY,
 
 	KVM_CSV3_SET_GUEST_PRIVATE_MEMORY = 0xc8,
 
@@ -2417,6 +2418,14 @@ struct kvm_csv3_receive_encrypt_context {
 	__u32 hdr_len;
 	__u64 trans_uaddr;
 	__u32 trans_len;
+};
+
+#define KVM_CSV3_RELEASE_SHARED_MEMORY (0x0001)
+
+struct kvm_csv3_handle_memory {
+	__u64 gpa;
+	__u32 num_pages;
+	__u32 opcode;
 };
 
 /* Available with KVM_CAP_MEMORY_ATTRIBUTES */
