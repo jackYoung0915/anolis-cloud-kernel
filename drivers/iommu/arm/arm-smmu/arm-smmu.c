7cc503f099952bf7d7a6adcfcac933c7efaa0c95
Robin Murphy <robin.murphy@arm.com>
2023-11-21 18:04:03 +0000

    iommu: Clean up open-coded ownership checks
    ANBZ: #13617
    
    commit e7080665c977ea1aafb8547a9c7bd08b199311d6 upstream.
    
    Some drivers already implement their own defence against the possibility
    of being given someone else's device. Since this is now taken care of by
    the core code (and via a slightly different path from the original
    fwspec-based idea), let's clean them up.
    
    Acked-by: Will Deacon <will@kernel.org>
    Reviewed-by: Jason Gunthorpe <jgg@nvidia.com>
    Reviewed-by: Jerry Snitselaar <jsnitsel@redhat.com>
    Signed-off-by: Robin Murphy <robin.murphy@arm.com>
    Link: https://lore.kernel.org/r/58a9879ce3f03562bb061e6714fe6efb554c3907.1700589539.git.robin.murphy@arm.com
    Signed-off-by: Joerg Roedel <jroedel@suse.de>
    
    [ Shuai Xue:
     Conflicts:
    	drivers/iommu/arm/arm-smmu/arm-smmu.c
     bugfix 4a9485918a04 is rebased by stable which defer probe of clients
     after smmu device bound
    ]
    
    Signed-off-by: Shuai Xue <xueshuai@linux.alibaba.com>
    Reviewed-by: Jay Chen <jkchen@linux.alibaba.com>
    Reviewed-by: Guanghui Feng <guanghuifeng@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5796

--- drivers/iommu/arm/arm-smmu/arm-smmu.c
+++ drivers/iommu/arm/arm-smmu/arm-smmu.c
@@ -1121,11 +1121,6 @@ static int arm_smmu_attach_dev(struct iommu_domain *domain, struct device *dev)
 	struct arm_smmu_device *smmu;
 	int ret;
 
-	if (!fwspec || fwspec->ops != &arm_smmu_ops) {
-		dev_err(dev, "cannot attach to SMMU, is it on the same bus?\n");
-		return -ENXIO;
-	}
-
 	/*
 	 * FIXME: The arch/arm DMA API code tries to attach devices to its own
 	 * domains between of_xlate() and probe_device() - we have no way to cope
@@ -1362,7 +1357,7 @@ static struct iommu_device *arm_smmu_probe_device(struct device *dev)
 		fwspec = dev_iommu_fwspec_get(dev);
 		if (ret)
 			goto out_free;
-	} else if (fwspec && fwspec->ops == &arm_smmu_ops) {
+	} else {
 		smmu = arm_smmu_get_by_fwnode(fwspec->iommu_fwnode);
 
 		/*
@@ -1375,8 +1370,6 @@ static struct iommu_device *arm_smmu_probe_device(struct device *dev)
 		if (!smmu)
 			return ERR_PTR(dev_err_probe(dev, -EPROBE_DEFER,
 						"smmu dev has not bound yet\n"));
-	} else {
-		return ERR_PTR(-ENODEV);
 	}
 
 #ifdef CONFIG_ARCH_PHYTIUM
