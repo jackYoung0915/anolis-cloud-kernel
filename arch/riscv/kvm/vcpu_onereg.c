34fbf8207130da4a7a76d691b48ccb9fc123aaf2
Anup Patel <apatel@ventanamicro.com>
2023-11-27 22:15:10 +0530

    RISC-V: KVM: Allow Zihintntl extension for Guest/VM
    ANBZ: #24718
    
    commit ab6da9cdc3f3d1d091d657219fb6e98f710ee098 upstream.
    
    We extend the KVM ISA extension ONE_REG interface to allow KVM
    user space to detect and enable Zihintntl extension for Guest/VM.
    
    Signed-off-by: Anup Patel <apatel@ventanamicro.com>
    Reviewed-by: Andrew Jones <ajones@ventanamicro.com>
    Signed-off-by: Anup Patel <anup@brainfault.org>

--- arch/riscv/kvm/vcpu_onereg.c
+++ arch/riscv/kvm/vcpu_onereg.c
@@ -68,6 +68,7 @@ static const unsigned long kvm_isa_ext_arr[] = {
 	KVM_ISA_EXT_ARR(ZICOND),
 	KVM_ISA_EXT_ARR(ZICSR),
 	KVM_ISA_EXT_ARR(ZIFENCEI),
+	KVM_ISA_EXT_ARR(ZIHINTNTL),
 	KVM_ISA_EXT_ARR(ZIHINTPAUSE),
 	KVM_ISA_EXT_ARR(ZIHPM),
 	KVM_ISA_EXT_ARR(ZKND),
@@ -154,6 +155,7 @@ static bool kvm_riscv_vcpu_isa_disable_allowed(unsigned long ext)
 	case KVM_RISCV_ISA_EXT_ZICOND:
 	case KVM_RISCV_ISA_EXT_ZICSR:
 	case KVM_RISCV_ISA_EXT_ZIFENCEI:
+	case KVM_RISCV_ISA_EXT_ZIHINTNTL:
 	case KVM_RISCV_ISA_EXT_ZIHINTPAUSE:
 	case KVM_RISCV_ISA_EXT_ZIHPM:
 	case KVM_RISCV_ISA_EXT_ZKND:
