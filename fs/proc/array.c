e683dc96645886368234c766c6f1872a6c99e8a1
Yi Tao <escape@linux.alibaba.com>
2023-12-05 17:12:32 +0800

    anolis: Add rich container feature control
    ANBZ: #29419
    
    The functionality of rich containers is supported in cgroup v2 through
    eBPF, but only a subset of features is supported. To ensure consistency
    between both sides, add switches for each feature of the rich container
    in cgroup v1.
    
    Features and their names are: cpuinfo, meminfo, cpuusage, loadavg,
    uptime, diskquota. All features are enabled by default if rich container
    is enabled。
    
    Add a new sysctl interface named rich_container_feature_control. When
    read, it shows space separated list of the features which are enabled.
    Space separated list of features prefixed with '+' or '-' can be written
    to enable or disable features.  A feature name prefixed with '+' enables
    the feature and '-' disables.  If a feature appears more than once on
    the list, the last one is effective.  When multiple enable and disable
    operations are specified, either all succeed or all fail.
    
    eg:
    	echo “+cpuinfo -meminfo +uptime” >
    	/proc/sys/kernel/rich_container_feature_control
    
    Signed-off-by: Yi Tao <escape@linux.alibaba.com>
    Reviewed-by: Joseph Qi <joseph.qi@linux.alibaba.com>
    Reviewed-by: Tony Lu <tonylu@linux.alibaba.com>
    Reviewed-by: Cruz Zhao <CruzZhao@linux.alibaba.com>
    Reviewed-by: Cruz Zhao <cruzzhao@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6346

--- fs/proc/array.c
+++ fs/proc/array.c
@@ -593,7 +593,7 @@ static int do_task_stat(struct seq_file *m, struct pid_namespace *ns,
 	 * time will show in "ps".
 	 */
 	rcu_read_lock();
-	if (in_rich_container(current)) {
+	if (in_rich_container(current, RC_UPTIME)) {
 		init_tsk = task_active_pid_ns(current)->child_reaper;
 		start_time -= nsec_to_clock_t(timens_add_boottime_ns(init_tsk->start_boottime));
 	}
