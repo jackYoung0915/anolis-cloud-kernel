6ceeeb306e055845da09a01cf82ebc97b3765c01
Amir Goldstein <amir73il@gmail.com>
2023-12-12 11:44:38 +0200

    fsnotify: split fsnotify_perm() into two hooks
    ANBZ: #33963
    
    commit 36e28c42187c95eb148873ffb059bfdcb8cdb75b upstream
    
    We would like to make changes to the fsnotify access permission hook -
    add file range arguments and add the pre modify event.
    
    In preparation for these changes, split the fsnotify_perm() hook into
    fsnotify_open_perm() and fsnotify_file_perm().
    
    This is needed for fanotify "pre content" events.
    
    Reviewed-by: Josef Bacik <josef@toxicpanda.com>
    Reviewed-by: Jan Kara <jack@suse.cz>
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>
    Link: https://lore.kernel.org/r/20231212094440.250945-4-amir73il@gmail.com
    Signed-off-by: Christian Brauner <brauner@kernel.org>
    Signed-off-by: Gao Xiang <hsiangkao@linux.alibaba.com>
    Acked-by: Joseph Qi <joseph.qi@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6833

--- security/security.c
+++ security/security.c
@@ -2614,7 +2614,7 @@ int security_file_permission(struct file *file, int mask)
 	if (ret)
 		return ret;
 
-	return fsnotify_perm(file, mask);
+	return fsnotify_file_perm(file, mask);
 }
 
 /**
@@ -2883,7 +2883,7 @@ int security_file_open(struct file *file)
 	if (ret)
 		return ret;
 
-	return fsnotify_perm(file, MAY_OPEN);
+	return fsnotify_open_perm(file);
 }
 
 /**
