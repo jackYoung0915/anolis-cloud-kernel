320db611106036ba6819e8dd7ad5f31455120d09
Shuai Xue <xueshuai@linux.alibaba.com>
2023-12-18 10:12:16 +0800

    mm: memory-failure: move return value documentation to function declaration
    ANBZ: #18913
    
    cherry-picked from https://patchwork.kernel.org/project/linux-acpi/patch/20250107081735.16159-2-xueshuai@linux.alibaba.com/
    
    Part of return value comments for memory_failure() were originally
    documented at the call site. Move those comments to the function
    declaration to improve code readability and to provide developers with
    immediate access to function usage and return information.
    
    Signed-off-by: Shuai Xue <xueshuai@linux.alibaba.com>
    Reviewed-by: Jarkko Sakkinen <jarkko@kernel.org>
    Reviewed-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    Reviewed-by: Yazen Ghannam <yazen.ghannam@amd.com>
    Reviewed-by: Jane Chu <jane.chu@oracle.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/4660

--- mm/memory-failure.c
+++ mm/memory-failure.c
@@ -2184,9 +2184,13 @@ out:
  * Must run in process context (e.g. a work queue) with interrupts
  * enabled and no spinlocks held.
  *
- * Return: 0 for successfully handled the memory error,
- *         -EOPNOTSUPP for hwpoison_filter() filtered the error event,
- *         < 0(except -EOPNOTSUPP) on failure.
+ * Return:
+ *   0             - success,
+ *   -ENXIO        - memory not managed by the kernel
+ *   -EOPNOTSUPP   - hwpoison_filter() filtered the error event,
+ *   -EHWPOISON    - the page was already poisoned, potentially
+ *                   kill process,
+ *   other negative values - failure.
  */
 int memory_failure(unsigned long pfn, int flags)
 {
