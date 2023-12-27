4f247054981c7521b60ed031bc06fcdd8476585c
Kinsey Ho <kinseyho@google.com>
2023-12-27 14:12:02 +0000

    mm/mglru: add CONFIG_LRU_GEN_WALKS_MMU
    ANBZ: #29854
    
    commit 61dd3f246b3adaabff3241c586f2210ac91b05a4 upstream
    
    Add CONFIG_LRU_GEN_WALKS_MMU such that if disabled, the code that
    walks page tables to promote pages into the youngest generation will
    not be built.
    
    Also improves code readability by adding two helper functions
    get_mm_state() and get_next_mm().
    
    Link: https://lkml.kernel.org/r/20231227141205.2200125-3-kinseyho@google.com
    Signed-off-by: Kinsey Ho <kinseyho@google.com>
    Co-developed-by: Aneesh Kumar K.V <aneesh.kumar@linux.ibm.com>
    Signed-off-by: Aneesh Kumar K.V <aneesh.kumar@linux.ibm.com>
    Tested-by: Donet Tom <donettom@linux.vnet.ibm.com>
    Acked-by: Yu Zhao <yuzhao@google.com>
    Cc: kernel test robot <lkp@intel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Reviewed-by Xu Yu <xuyu@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6406

--- include/linux/mm_types.h
+++ include/linux/mm_types.h
@@ -1020,7 +1020,7 @@ struct mm_struct {
 		 */
 		atomic_long_t ksm_zero_pages;
 #endif /* CONFIG_KSM */
-#ifdef CONFIG_LRU_GEN
+#ifdef CONFIG_LRU_GEN_WALKS_MMU
 		struct {
 			/* this mm_struct is on lru_gen_mm_list */
 			struct list_head list;
@@ -1035,7 +1035,7 @@ struct mm_struct {
 			struct mem_cgroup *memcg;
 #endif
 		} lru_gen;
-#endif /* CONFIG_LRU_GEN */
+#endif /* CONFIG_LRU_GEN_WALKS_MMU */
 #ifdef CONFIG_DUPTEXT
 		unsigned long duptext_flags;
 #endif
@@ -1091,6 +1091,10 @@ struct lru_gen_mm_list {
 	spinlock_t lock;
 };
 
+#endif /* CONFIG_LRU_GEN */
+
+#ifdef CONFIG_LRU_GEN_WALKS_MMU
+
 void lru_gen_add_mm(struct mm_struct *mm);
 void lru_gen_del_mm(struct mm_struct *mm);
 #ifdef CONFIG_MEMCG
@@ -1116,7 +1120,7 @@ static inline void lru_gen_use_mm(struct mm_struct *mm)
 	WRITE_ONCE(mm->lru_gen.bitmap, -1);
 }
 
-#else /* !CONFIG_LRU_GEN */
+#else /* !CONFIG_LRU_GEN_WALKS_MMU */
 
 static inline void lru_gen_add_mm(struct mm_struct *mm)
 {
@@ -1140,7 +1144,7 @@ static inline void lru_gen_use_mm(struct mm_struct *mm)
 {
 }
 
-#endif /* CONFIG_LRU_GEN */
+#endif /* CONFIG_LRU_GEN_WALKS_MMU */
 
 struct vma_iterator {
 	struct ma_state mas;
