ec865d8e200f03f48d4d0c273f44a76f762e5cc5
Charlie Jenkins <charlie@rivosinc.com>
2024-01-08 15:57:03 -0800

    riscv: Add static key for misaligned accesses
    ANBZ: #32730
    
    commit 2ce5729fce8f62b5118f56110d16006c0e22c522 upstream.
    
    Support static branches depending on the value of misaligned accesses.
    This will be used by a later patch in the series. At any point in time,
    this static branch will only be enabled if all online CPUs are
    considered "fast".
    
    Signed-off-by: Charlie Jenkins <charlie@rivosinc.com>
    Reviewed-by: Evan Green <evan@rivosinc.com>
    Link: https://lore.kernel.org/r/20240108-optimize_checksum-v15-2-1c50de5f2167@rivosinc.com
    Signed-off-by: Palmer Dabbelt <palmer@rivosinc.com>
    [Ruidong:
    Conflicts:
      resolve: arch/riscv/include/asm/cpufeature.h, arch/riscv/kernel/cpufeature.c
      reason:
        Context conflict only, patch content unchanged
    ]
    Signed-off-by: Ruidong Tian <tianruidong@linux.alibaba.com>
    Acked-by: Jakub Kicinski <kuba@kernel.org>
    Reviewed-by: Simon Horman <horms@kernel.org>
    Reviewed-by: Alexandre Ghiti <alexghiti@rivosinc.com>
    Reviewed-by: Clément Léger <cleger@rivosinc.com>
    Reviewed-by: Fangyu Yu <fangyu.yu@linux.alibaba.com>
    Reviewed-by: Rui Gao <gao.rui@zte.com.cn>
    Reviewed-by: Jiankang Chen <jkchen@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6707

--- arch/riscv/include/asm/cpufeature.h
+++ arch/riscv/include/asm/cpufeature.h
@@ -69,6 +69,8 @@ static __always_inline bool riscv_cpu_has_extension_unlikely(int cpu, const unsi
 	return __riscv_isa_extension_available(hart_isa[cpu].isa, ext);
 }
 
+DECLARE_STATIC_KEY_FALSE(fast_misaligned_access_speed_key);
+
 #ifdef CONFIG_RISCV_MISALIGNED
 bool unaligned_ctl_available(void);
 bool check_unaligned_access_emulated(int cpu);
