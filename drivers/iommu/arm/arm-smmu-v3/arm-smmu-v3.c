2e42c62282e6510fb05c3e1287fa8d00dd4974f6
Kunkun Jiang <jiangkunkun@huawei.com>
2024-01-10 19:36:36 +0800

    iommu/arm-smmu-v3: Realize support_dirty_log iommu ops
    ANBZ: #IJSBSS
    
    commit 86f3d00a30b2d0fa0733ebdc9a6f73a49269292a openEuler.
    
    We have implemented these interfaces required to support iommu
    dirty log tracking. The last step is reporting this feature to
    upper user, then the user can perform higher policy base on it.
    For arm smmuv3, it is equal to ARM_SMMU_FEAT_HD.
    
    Signed-off-by: Keqian Zhu <zhukeqian1@huawei.com>
    Signed-off-by: Kunkun Jiang <jiangkunkun@huawei.com>
    Signed-off-by: jiaqingtong <jiaqingtong@huawei.com>
    [frankyj915: Resolve conflicts in drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.c]
    Signed-off-by: frankyj915 <yangjieyj.yang@huawei.com>

--- drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.c
+++ drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.c
@@ -3877,6 +3877,13 @@ static int arm_smmu_merge_page(struct iommu_domain *domain, unsigned long iova,
 	return ret;
 }
 
+static bool arm_smmu_support_dirty_log(struct iommu_domain *domain)
+{
+	struct arm_smmu_domain *smmu_domain = to_smmu_domain(domain);
+
+	return !!(smmu_domain->smmu->features & ARM_SMMU_FEAT_HD);
+}
+
 static int arm_smmu_switch_dirty_log(struct iommu_domain *domain, bool enable,
 				     unsigned long iova, size_t size, int prot)
 {
@@ -4047,6 +4054,7 @@ static struct iommu_ops arm_smmu_ops = {
 #endif
 		.iova_to_phys		= arm_smmu_iova_to_phys,
 		.free			= arm_smmu_domain_free_paging,
+		.support_dirty_log	= arm_smmu_support_dirty_log,
 		.switch_dirty_log	= arm_smmu_switch_dirty_log,
 		.sync_dirty_log		= arm_smmu_sync_dirty_log,
 		.clear_dirty_log	= arm_smmu_clear_dirty_log,
