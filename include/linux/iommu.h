bf4460926a91d38528aea7321a25dc5a7c4e4cb5
Kunkun Jiang <jiangkunkun@huawei.com>
2024-01-10 19:36:45 +0800

    iommu: Introduce dirty log tracking framework
    ANBZ: #IJSBSS
    
    commit 03b784a5bc5b7c9964a407dbcb8bfc9c4cf36992 openEuler.
    
    Some types of IOMMU are capable of tracking DMA dirty log, such as
    ARM SMMU with HTTU or Intel IOMMU with SLADE. This introduces the
    dirty log tracking framework in the IOMMU base layer.
    
    Four new essential interfaces are added, and we maintaince the status
    of dirty log tracking in iommu_domain.
    1. iommu_support_dirty_log: Check whether domain supports dirty log
    tracking
    2. iommu_switch_dirty_log: Perform actions to start|stop dirty log tracking
    3. iommu_sync_dirty_log: Sync dirty log from IOMMU into a dirty bitmap
    4. iommu_clear_dirty_log: Clear dirty log of IOMMU by a mask bitmap
    
    Note: Don't concurrently call these interfaces with other ops that
    access underlying page table.
    
    Signed-off-by: Keqian Zhu <zhukeqian1@huawei.com>
    Signed-off-by: Kunkun Jiang <jiangkunkun@huawei.com>
    Signed-off-by: Jia Qingtong <jiaqingtong@huawei.com>
    [frankyj915: Resolve conflicts introduced by comit a04c334
    (iommu: Create __iommu_alloc_identity_domain())]
    Signed-off-by: frankyj915 <yangjieyj.yang@huawei.com>

--- include/linux/iommu.h
+++ include/linux/iommu.h
@@ -253,6 +253,9 @@ struct iommu_domain {
 			struct list_head next;
 		};
 	};
+
+	struct mutex switch_log_lock;
+
 #ifdef CONFIG_IOMMU_LIVEUPDATE
 	struct iommu_domain_ser *preserved_state;
 #endif
@@ -846,6 +849,10 @@ struct iommu_ops {
  *                           including no-snoop TLPs on PCIe or other platform
  *                           specific mechanisms.
  * @set_pgtable_quirks: Set io page table quirks (IO_PGTABLE_QUIRK_*)
+ * @support_dirty_log: Check whether domain supports dirty log tracking
+ * @switch_dirty_log: Perform actions to start|stop dirty log tracking
+ * @sync_dirty_log: Sync dirty log from IOMMU into a dirty bitmap
+ * @clear_dirty_log: Clear dirty log of IOMMU by a mask bitmap
  * @free: Release the domain after use.
  * @preserve: Preserve the iommu domain for liveupdate.
  *            Returns 0 on success, a negative errno on failure.
@@ -881,6 +888,21 @@ struct iommu_domain_ops {
 	int (*set_pgtable_quirks)(struct iommu_domain *domain,
 				  unsigned long quirks);
 
+	/*
+	 * Track dirty log. Note: Don't concurrently call these interfaces with
+	 * other ops that access underlying page table.
+	 */
+	bool (*support_dirty_log)(struct iommu_domain *domain);
+	int (*switch_dirty_log)(struct iommu_domain *domain, bool enable,
+				unsigned long iova, size_t size, int prot);
+	int (*sync_dirty_log)(struct iommu_domain *domain,
+			      unsigned long iova, size_t size,
+			      unsigned long *bitmap, unsigned long base_iova,
+			      unsigned long bitmap_pgshift);
+	int (*clear_dirty_log)(struct iommu_domain *domain,
+			       unsigned long iova, size_t size,
+			       unsigned long *bitmap, unsigned long base_iova,
+			       unsigned long bitmap_pgshift);
 	void (*free)(struct iommu_domain *domain);
 	int (*preserve)(struct iommu_domain *domain, struct iommu_domain_ser *ser);
 	void (*unpreserve)(struct iommu_domain *domain, struct iommu_domain_ser *ser);
@@ -1085,6 +1107,18 @@ extern struct iommu_domain *iommu_group_default_domain(struct iommu_group *);
 
 int iommu_set_pgtable_quirks(struct iommu_domain *domain,
 		unsigned long quirks);
+
+extern bool iommu_support_dirty_log(struct iommu_domain *domain);
+extern int iommu_switch_dirty_log(struct iommu_domain *domain, bool enable,
+				  unsigned long iova, size_t size, int prot);
+extern int iommu_sync_dirty_log(struct iommu_domain *domain, unsigned long iova,
+				size_t size, unsigned long *bitmap,
+				unsigned long base_iova,
+				unsigned long bitmap_pgshift);
+extern int iommu_clear_dirty_log(struct iommu_domain *domain, unsigned long iova,
+				 size_t dma_size, unsigned long *bitmap,
+				 unsigned long base_iova,
+				 unsigned long bitmap_pgshift);
 
 void iommu_set_dma_strict(void);
 
@@ -1533,6 +1567,36 @@ static inline int iommu_set_pgtable_quirks(struct iommu_domain *domain,
 		unsigned long quirks)
 {
 	return 0;
+}
+
+static inline bool iommu_support_dirty_log(struct iommu_domain *domain)
+{
+	return false;
+}
+
+static inline int iommu_switch_dirty_log(struct iommu_domain *domain,
+					 bool enable, unsigned long iova,
+					 size_t size, int prot)
+{
+	return -EINVAL;
+}
+
+static inline int iommu_sync_dirty_log(struct iommu_domain *domain,
+				       unsigned long iova, size_t size,
+				       unsigned long *bitmap,
+				       unsigned long base_iova,
+				       unsigned long pgshift)
+{
+	return -EINVAL;
+}
+
+static inline int iommu_clear_dirty_log(struct iommu_domain *domain,
+					unsigned long iova, size_t size,
+					unsigned long *bitmap,
+					unsigned long base_iova,
+					unsigned long pgshift)
+{
+	return -EINVAL;
 }
 
 static inline int iommu_device_register(struct iommu_device *iommu,
