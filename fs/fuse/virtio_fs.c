b7fbfb42c48b4157ecb36630ec02d4e136c44d6e
Li RongQing <lirongqing@baidu.com>
2024-01-15 11:09:14 +0800

    virtio_fs: remove duplicate check if queue is broken
    ANBZ: #10812
    
    commit f9c29137392e77319f9974c2cdf27d087f05abee upstream.
    
    virtqueue_enable_cb() will call virtqueue_poll() which will check if
    queue is broken at beginning, so remove the virtqueue_is_broken() call
    
    Signed-off-by: Li RongQing <lirongqing@baidu.com>
    Reviewed-by: Stefan Hajnoczi <stefanha@redhat.com>
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>
    Signed-off-by: Jingbo Xu <jefflexu@linux.alibaba.com>
    Reviewed-by: Joseph Qi <joseph.qi@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/3810

--- fs/fuse/virtio_fs.c
+++ fs/fuse/virtio_fs.c
@@ -360,7 +360,7 @@ static void virtio_fs_hiprio_done_work(struct work_struct *work)
 			kfree(req);
 			dec_in_flight_req(fsvq);
 		}
-	} while (!virtqueue_enable_cb(vq) && likely(!virtqueue_is_broken(vq)));
+	} while (!virtqueue_enable_cb(vq));
 	spin_unlock(&fsvq->lock);
 }
 
@@ -642,7 +642,7 @@ static void virtio_fs_requests_done_work(struct work_struct *work)
 			list_move_tail(&req->list, &reqs);
 			spin_unlock(&fpq->lock);
 		}
-	} while (!virtqueue_enable_cb(vq) && likely(!virtqueue_is_broken(vq)));
+	} while (!virtqueue_enable_cb(vq));
 	spin_unlock(&fsvq->lock);
 
 	/* End requests */
