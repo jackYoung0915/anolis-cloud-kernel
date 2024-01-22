5a4b8ed30e7e064ad08f3c9bc4aec2b22843b36b
Marc Zyngier <maz@kernel.org>
2024-01-22 18:13:35 +0000

    arm64: Add macro to compose a sysreg field value
    ANBZ: #31782
    
    commit 53eaeb7fbe2702520125ae7d72742362c071a1f2 upstream.
    
    A common idiom is to compose a tupple (reg, field, val) into
    a symbol matching an autogenerated definition.
    
    Add a help performing the concatenation and replace it when
    open-coded implementations exist.
    
    Suggested-by: Oliver Upton <oliver.upton@linux.dev>
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Reviewed-by: Catalin Marinas <catalin.marinas@arm.com>
    Link: https://lore.kernel.org/r/20240122181344.258974-2-maz@kernel.org
    Signed-off-by: Oliver Upton <oliver.upton@linux.dev>
    Signed-off-by: Jia He <justin.he@arm.com>
    Reviewed-by: Bin Guo <guobin@linux.alibaba.com>
    Reviewed-by: Shuai Xue <xueshuai@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6659

--- arch/arm64/include/asm/sysreg.h
+++ arch/arm64/include/asm/sysreg.h
@@ -1112,6 +1112,8 @@
 	par;								\
 })
 
+#define SYS_FIELD_VALUE(reg, field, val)	reg##_##field##_##val
+
 #define SYS_FIELD_GET(reg, field, val)		\
 		 FIELD_GET(reg##_##field##_MASK, val)
 
@@ -1119,7 +1121,8 @@
 		 FIELD_PREP(reg##_##field##_MASK, val)
 
 #define SYS_FIELD_PREP_ENUM(reg, field, val)		\
-		 FIELD_PREP(reg##_##field##_MASK, reg##_##field##_##val)
+		 FIELD_PREP(reg##_##field##_MASK,	\
+			    SYS_FIELD_VALUE(reg, field, val))
 
 #endif
 
