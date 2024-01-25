c7a671b977bfdaf2c2c557ef5ff1672309bea300
Suzuki K Poulose <suzuki.poulose@arm.com>
2024-01-25 23:20:39 +0000

    virtio: uapi: Drop __packed attribute in linux/virtio_pci.h
    ANBZ: #22277
    
    commit ec6ecb844d14d38b7dae8beb74e3d65db9c7b3e6 upstream.
    
    Commit 92792ac752aa ("virtio-pci: Introduce admin command sending function")
    added "__packed" structures to UAPI header linux/virtio_pci.h. This triggers
    build failures in the consumer userspace applications without proper "definition"
    of __packed (e.g., kvmtool build fails).
    
    Moreover, the structures are already packed well, and doesn't need explicit
    packing, similar to the rest of the structures in all virtio_* headers. Remove
    the __packed attribute.
    
    Fixes: 92792ac752aa ("virtio-pci: Introduce admin command sending function")
    Cc: Feng Liu <feliu@nvidia.com>
    Cc: Michael S. Tsirkin <mst@redhat.com>
    Cc: Yishai Hadas <yishaih@nvidia.com>
    Cc: Alex Williamson <alex.williamson@redhat.com>
    Cc: Jean-Philippe Brucker <jean-philippe@linaro.org>
    Reviewed-by: Jean-Philippe Brucker <jean-philippe@linaro.org>
    Acked-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Suzuki K Poulose <suzuki.poulose@arm.com>
    Message-Id: <20240125232039.913606-1-suzuki.poulose@arm.com>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Qinyun Tan <qinyuntan@linux.alibaba.com>

--- include/uapi/linux/virtio_pci.h
+++ include/uapi/linux/virtio_pci.h
@@ -249,7 +249,7 @@ struct virtio_pci_cfg_cap {
 #define VIRTIO_ADMIN_CMD_DEV_PARTS_SET			0x10
 #define VIRTIO_ADMIN_CMD_DEV_MODE_SET			0x11
 
-struct __packed virtio_admin_cmd_hdr {
+struct virtio_admin_cmd_hdr {
 	__le16 opcode;
 	/*
 	 * 1 - SR-IOV
@@ -261,20 +261,20 @@ struct __packed virtio_admin_cmd_hdr {
 	__le64 group_member_id;
 };
 
-struct __packed virtio_admin_cmd_status {
+struct virtio_admin_cmd_status {
 	__le16 status;
 	__le16 status_qualifier;
 	/* Unused, reserved for future extensions. */
 	__u8 reserved2[4];
 };
 
-struct __packed virtio_admin_cmd_legacy_wr_data {
+struct virtio_admin_cmd_legacy_wr_data {
 	__u8 offset; /* Starting offset of the register(s) to write. */
 	__u8 reserved[7];
 	__u8 registers[];
 };
 
-struct __packed virtio_admin_cmd_legacy_rd_data {
+struct virtio_admin_cmd_legacy_rd_data {
 	__u8 offset; /* Starting offset of the register(s) to read. */
 };
 
@@ -284,7 +284,7 @@ struct __packed virtio_admin_cmd_legacy_rd_data {
 
 #define VIRTIO_ADMIN_CMD_MAX_NOTIFY_INFO 4
 
-struct __packed virtio_admin_cmd_notify_info_data {
+struct virtio_admin_cmd_notify_info_data {
 	__u8 flags; /* 0 = end of list, 1 = owner device, 2 = member device */
 	__u8 bar; /* BAR of the member or the owner device */
 	__u8 padding[6];
