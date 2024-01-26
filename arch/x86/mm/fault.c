1d07fbe16913df431e5e1e4566a109a91c06d501
Michael Roth <michael.roth@amd.com>
2024-01-25 22:11:09 -0600

    x86/fault: Dump RMP table information when RMP page faults occur
    ANBZ: #34453
    
    commit e8bbd303d7de3fb32be1434a5d5ce3e1cb182018 upstream.
    
    RMP faults on kernel addresses are fatal and should never happen in
    practice. They indicate a bug in the host kernel somewhere. Userspace
    RMP faults shouldn't occur either, since even for VMs the memory used
    for private pages is handled by guest_memfd and by design is not
    mappable by userspace.
    
    Dump RMP table information about the PFN corresponding to the faulting
    HVA to help diagnose any issues of this sort when show_fault_oops() is
    triggered by an RMP fault.
    
    Signed-off-by: Michael Roth <michael.roth@amd.com>
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    Link: https://lore.kernel.org/r/20240126041126.1927228-10-michael.roth@amd.com
    Signed-off-by: Hemanth Selam <Hemanth.Selam@amd.com>
    Signed-off-by: mohanasv <mohanasv@amd.com>
    Reviewed-by: Tianjia Zhang <tianjia.zhang@linux.alibaba.com>
    Reviewed-by: Qinyun Tan <qinyuntan@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/7076

--- arch/x86/mm/fault.c
+++ arch/x86/mm/fault.c
@@ -36,6 +36,7 @@
 #include <asm/vdso.h>			/* fixup_vdso_exception()	*/
 #include <asm/irq_stack.h>
 #include <asm/fred.h>
+#include <asm/sev.h>			/* snp_dump_hva_rmpentry()	*/
 
 #define CREATE_TRACE_POINTS
 #include <asm/trace/exceptions.h>
@@ -582,6 +583,9 @@ show_fault_oops(struct pt_regs *regs, unsigned long error_code, unsigned long ad
 	}
 
 	dump_pagetable(address);
+
+	if (error_code & X86_PF_RMP)
+		snp_dump_hva_rmpentry(address);
 }
 
 static noinline void
