6be78dfadeb0518b76c722e85be1b2d428aa9c8b
Brijesh Singh <brijesh.singh@amd.com>
2024-01-25 22:11:21 -0600

    KVM: SEV: Make AVIC backing, VMSA and VMCB memory allocation SNP safe
    ANBZ: #34453
    
    commit 75253db41a467ab7983b62616b25ff083c28803a upstream.
    
    Implement a workaround for an SNP erratum where the CPU will incorrectly
    signal an RMP violation #PF if a hugepage (2MB or 1GB) collides with the
    RMP entry of a VMCB, VMSA or AVIC backing page.
    
    When SEV-SNP is globally enabled, the CPU marks the VMCB, VMSA, and AVIC
    backing pages as "in-use" via a reserved bit in the corresponding RMP
    entry after a successful VMRUN. This is done for _all_ VMs, not just
    SNP-Active VMs.
    
    If the hypervisor accesses an in-use page through a writable
    translation, the CPU will throw an RMP violation #PF. On early SNP
    hardware, if an in-use page is 2MB-aligned and software accesses any
    part of the associated 2MB region with a hugepage, the CPU will
    incorrectly treat the entire 2MB region as in-use and signal a an RMP
    violation #PF.
    
    To avoid this, the recommendation is to not use a 2MB-aligned page for
    the VMCB, VMSA or AVIC pages. Add a generic allocator that will ensure
    that the page returned is not 2MB-aligned and is safe to be used when
    SEV-SNP is enabled. Also implement similar handling for the VMCB/VMSA
    pages of nested guests.
    
      [ mdr: Squash in nested guest handling from Ashish, commit msg fixups. ]
    
    Reported-by: Alper Gun <alpergun@google.com> # for nested VMSA case
    Signed-off-by: Brijesh Singh <brijesh.singh@amd.com>
    Co-developed-by: Marc Orr <marcorr@google.com>
    Signed-off-by: Marc Orr <marcorr@google.com>
    Co-developed-by: Ashish Kalra <ashish.kalra@amd.com>
    Signed-off-by: Ashish Kalra <ashish.kalra@amd.com>
    Signed-off-by: Michael Roth <michael.roth@amd.com>
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    Acked-by: Vlastimil Babka <vbabka@suse.cz>
    Acked-by: Paolo Bonzini <pbonzini@redhat.com>
    Link: https://lore.kernel.org/r/20240126041126.1927228-22-michael.roth@amd.com
    Signed-off-by: Mukesh Ogare <OgreKumar.Mukesh@amd.com>
    Signed-off-by: mohanasv <mohanasv@amd.com>
    Reviewed-by: Tianjia Zhang <tianjia.zhang@linux.alibaba.com>
    Reviewed-by: Qinyun Tan <qinyuntan@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/7076

--- arch/x86/include/asm/kvm_host.h
+++ arch/x86/include/asm/kvm_host.h
@@ -1808,6 +1808,7 @@ struct kvm_x86_ops {
 	unsigned long (*vcpu_get_apicv_inhibit_reasons)(struct kvm_vcpu *vcpu);
 
 	gva_t (*get_untagged_addr)(struct kvm_vcpu *vcpu, gva_t gva, unsigned int flags);
+	void *(*alloc_apic_backing_page)(struct kvm_vcpu *vcpu);
 
 	/*
 	 * Interfaces for HYGON CSV guest
