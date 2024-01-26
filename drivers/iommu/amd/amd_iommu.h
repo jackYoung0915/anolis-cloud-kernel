48e900cb450e46c08aa27b475c2ea0c000e79242
Ashish Kalra <ashish.kalra@amd.com>
2024-01-25 22:11:03 -0600

    iommu/amd: Don't rely on external callers to enable IOMMU SNP support
    ANBZ: #13617
    
    commit 04d65a9dbb33e20500005e151d720acead78c539 upstream.
    
    Currently, the expectation is that the kernel will call
    amd_iommu_snp_enable() to perform various checks and set the
    amd_iommu_snp_en flag that the IOMMU uses to adjust its setup routines
    to account for additional requirements on hosts where SNP is enabled.
    
    This is somewhat fragile as it relies on this call being done prior to
    IOMMU setup. It is more robust to just do this automatically as part of
    IOMMU initialization, so rework the code accordingly.
    
    There is still a need to export information about whether or not the
    IOMMU is configured in a manner compatible with SNP, so relocate the
    existing amd_iommu_snp_en flag so it can be used to convey that
    information in place of the return code that was previously provided by
    calls to amd_iommu_snp_enable().
    
    While here, also adjust the kernel messages related to IOMMU SNP
    enablement for consistency/grammar/clarity.
    
    Suggested-by: Borislav Petkov (AMD) <bp@alien8.de>
    Signed-off-by: Ashish Kalra <ashish.kalra@amd.com>
    Co-developed-by: Michael Roth <michael.roth@amd.com>
    Signed-off-by: Michael Roth <michael.roth@amd.com>
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    Acked-by: Joerg Roedel <jroedel@suse.de>
    Link: https://lore.kernel.org/r/20240126041126.1927228-4-michael.roth@amd.com
    Signed-off-by: Jay Chen <jkchen@linux.alibaba.com>
    Reviewed-by: Shuai Xue <xueshuai@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5816

--- drivers/iommu/amd/amd_iommu.h
+++ drivers/iommu/amd/amd_iommu.h
@@ -181,7 +181,6 @@ void amd_iommu_domain_set_pgtable(struct protection_domain *domain,
 				  u64 *root, int mode);
 struct dev_table_entry *get_dev_table(struct amd_iommu *iommu);
 
-extern bool amd_iommu_snp_en;
 #endif
 
 struct dev_table_entry *amd_iommu_get_ivhd_dte_flags(u16 segid, u16 devid);
