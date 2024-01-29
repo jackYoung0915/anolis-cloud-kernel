615a86a011f97a4b137ff6f77a20392e4cf1d2f8
Lance Yang <ioworker0@gmail.com>
2024-01-29 13:45:51 +0800

    mm/khugepaged: bypassing unnecessary scans with MMF_DISABLE_THP check
    ANBZ: #30191
    
    commit 879c6000e191b61b97e17bce44c4564ee42eb612 upstream.
    
    khugepaged scans the entire address space in the background for each
    given mm, looking for opportunities to merge sequences of basic pages
    into huge pages.  However, when an mm is inserted to the mm_slots list,
    and the MMF_DISABLE_THP flag is set later, this scanning process
    becomes unnecessary for that mm and can be skipped to avoid redundant
    operations, especially in scenarios with a large address space.
    
    On an Intel Core i5 CPU, the time taken by khugepaged to scan the
    address space of the process, which has been set with the
    MMF_DISABLE_THP flag after being added to the mm_slots list, is as
    follows (shorter is better):
    
    VMA Count |   Old   |   New   |  Change
    ---------------------------------------
        50    |   23us  |    9us  |  -60.9%
       100    |   32us  |    9us  |  -71.9%
       200    |   44us  |    9us  |  -79.5%
       400    |   75us  |    9us  |  -88.0%
       800    |   98us  |    9us  |  -90.8%
    
    Once the count of VMAs for the process exceeds page_to_scan, khugepaged
    needs to wait for scan_sleep_millisecs ms before scanning the next
    process.  IMO, unnecessary scans could actually be skipped with a very
    inexpensive mm->flags check in this case.
    
    This commit introduces a check before each scanning process to test the
    MMF_DISABLE_THP flag for the given mm; if the flag is set, the scanning
    process is bypassed, thereby improving the efficiency of khugepaged.
    
    This optimization is not a correctness issue but rather an enhancement
    to save expensive checks on each VMA when userspace cannot prctl itself
    before spawning into the new process.
    
    On some servers within our company, we deploy a daemon responsible for
    monitoring and updating local applications.  Some applications prefer
    not to use THP, so the daemon calls prctl to disable THP before
    fork/exec.  Conversely, for other applications, the daemon calls prctl
    to enable THP before fork/exec.
    
    Ideally, the daemon should invoke prctl after the fork, but its current
    implementation follows the described approach.  In the Go standard
    library, there is no direct encapsulation of the fork system call;
    instead, fork and execve are combined into one through
    syscall.ForkExec.
    
    Link: https://lkml.kernel.org/r/20240129054551.57728-1-ioworker0@gmail.com
    Signed-off-by: Lance Yang <ioworker0@gmail.com>
    Acked-by: David Hildenbrand <david@redhat.com>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Minchan Kim <minchan@kernel.org>
    Cc: Muchun Song <songmuchun@bytedance.com>
    Cc: Peter Xu <peterx@redhat.com>
    Cc: Zach O'Keefe <zokeefe@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Yuanhe Shu <xiangzao@linux.alibaba.com>

--- mm/khugepaged.c
+++ mm/khugepaged.c
@@ -434,6 +434,12 @@ static bool hugepage_pmd_enabled(void)
 	return false;
 }
 
+static inline int hpage_collapse_test_exit_or_disable(struct mm_struct *mm)
+{
+	return hpage_collapse_test_exit(mm) ||
+	       test_bit(MMF_DISABLE_THP, &mm->flags);
+}
+
 void __khugepaged_enter(struct mm_struct *mm)
 {
 	struct khugepaged_mm_slot *mm_slot;
@@ -1440,7 +1446,7 @@ static void collect_mm_slot(struct khugepaged_mm_slot *mm_slot)
 
 	lockdep_assert_held(&khugepaged_mm_lock);
 
-	if (hpage_collapse_test_exit(mm)) {
+	if (hpage_collapse_test_exit_or_disable(mm)) {
 		/* free mm_slot */
 		hash_del(&slot->hash);
 		list_del(&slot->mm_node);
@@ -2401,7 +2407,7 @@ static unsigned int khugepaged_scan_mm_slot(unsigned int pages, int *result,
 		goto breakouterloop_mmap_lock;
 
 	progress++;
-	if (unlikely(hpage_collapse_test_exit(mm)))
+	if (unlikely(hpage_collapse_test_exit_or_disable(mm)))
 		goto breakouterloop;
 
 	vma_iter_init(&vmi, mm, khugepaged_scan.address);
@@ -2409,7 +2415,7 @@ static unsigned int khugepaged_scan_mm_slot(unsigned int pages, int *result,
 		unsigned long hstart, hend;
 
 		cond_resched();
-		if (unlikely(hpage_collapse_test_exit(mm))) {
+		if (unlikely(hpage_collapse_test_exit_or_disable(mm))) {
 			progress++;
 			break;
 		}
@@ -2431,7 +2437,7 @@ skip:
 			bool mmap_locked = true;
 
 			cond_resched();
-			if (unlikely(hpage_collapse_test_exit(mm)))
+			if (unlikely(hpage_collapse_test_exit_or_disable(mm)))
 				goto breakouterloop;
 
 			VM_BUG_ON(khugepaged_scan.address < hstart ||
@@ -2449,7 +2455,7 @@ skip:
 				fput(file);
 				if (*result == SCAN_PTE_MAPPED_HUGEPAGE) {
 					mmap_read_lock(mm);
-					if (hpage_collapse_test_exit(mm))
+					if (hpage_collapse_test_exit_or_disable(mm))
 						goto breakouterloop;
 					*result = collapse_pte_mapped_thp(mm,
 						khugepaged_scan.address, false);
@@ -2491,7 +2497,7 @@ breakouterloop_mmap_lock:
 	 * Release the current mm_slot if this mm is about to die, or
 	 * if we scanned all vmas of this mm.
 	 */
-	if (hpage_collapse_test_exit(mm) || !vma) {
+	if (hpage_collapse_test_exit_or_disable(mm) || !vma) {
 		/*
 		 * Make sure that if mm_users is reaching zero while
 		 * khugepaged runs here, khugepaged_exit will find
