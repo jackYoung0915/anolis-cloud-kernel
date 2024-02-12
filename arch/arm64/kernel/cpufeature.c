c825b95f0a0cef19d9da8bed822fd07d896c7a76
Marc Zyngier <maz@kernel.org>
2024-02-12 14:47:36 +0000

    arm64: cpufeatures: Only check for NV1 if NV is present
    ANBZ: #31782
    
    commit 3673d01a2f555603cbf756874c7388b76bfbc967 upstream.
    
    We handle ID_AA64MMFR4_EL1.E2H0 being 0 as NV1 being present.
    However, this is only true if FEAT_NV is implemented.
    
    Add the required check to has_nv1(), avoiding spuriously advertising
    NV1 on HW that doesn't have NV at all.
    
    Fixes: da9af5071b25 ("arm64: cpufeature: Detect HCR_EL2.NV1 being RES0")
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Link: https://lore.kernel.org/r/20240212144736.1933112-3-maz@kernel.org
    Signed-off-by: Oliver Upton <oliver.upton@linux.dev>
    Signed-off-by: Jia He <justin.he@arm.com>
    Reviewed-by: Bin Guo <guobin@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6809

--- arch/arm64/kernel/cpufeature.c
+++ arch/arm64/kernel/cpufeature.c
@@ -1904,8 +1904,9 @@ static bool has_nv1(const struct arm64_cpu_capabilities *entry, int scope)
 		{}
 	};
 
-	return !(has_cpuid_feature(entry, scope) ||
-		 is_midr_in_range_list(read_cpuid_id(), nv1_ni_list));
+	return (this_cpu_has_cap(ARM64_HAS_NESTED_VIRT) &&
+		!(has_cpuid_feature(entry, scope) ||
+		  is_midr_in_range_list(read_cpuid_id(), nv1_ni_list)));
 }
 
 #ifdef CONFIG_UNMAP_KERNEL_AT_EL0
