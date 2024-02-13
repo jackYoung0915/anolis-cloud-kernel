10226d86aab82fac2187b918de006ad3d0c6d58a
Thomas Gleixner <tglx@linutronix.de>
2024-02-13 22:06:07 +0100

    x86/cpu/topology: Provide logical pkg/die mapping
    ANBZ: #26668
    
    commit b7065f4f844c7876ed071b67e2ba57838152bd63 upstream.
    
    With the topology bitmaps in place the logical package and die IDs can
    trivially be retrieved by determining the bitmap weight of the relevant
    topology domain level up to and including the physical ID in question.
    
    Provide a function to that effect.
    
    Intel-SIG: commit b7065f4f844c x86/cpu/topology: Provide logical pkg/die mapping.
    x86/apic: Rework APIC registration
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Tested-by: Michael Kelley <mhklinux@outlook.com>
    Tested-by: Sohil Mehta <sohil.mehta@intel.com>
    Link: https://lore.kernel.org/r/20240213210252.846136196@linutronix.de
    [ Quanxian Wang: amend commit log ]
    Signed-off-by: Quanxian Wang <quanxian.wang@intel.com>

--- arch/x86/include/asm/topology.h
+++ arch/x86/include/asm/topology.h
@@ -159,6 +159,15 @@ static inline unsigned int topology_max_die_per_package(void)
 extern struct cpumask __cpu_primary_thread_mask;
 #define cpu_primary_thread_mask ((const struct cpumask *)&__cpu_primary_thread_mask)
 
+#ifdef CONFIG_X86_LOCAL_APIC
+int topology_get_logical_id(u32 apicid, enum x86_topology_domains at_level);
+#else
+static inline int topology_get_logical_id(u32 apicid, enum x86_topology_domains at_level)
+{
+	return 0;
+}
+#endif
+
 #ifdef CONFIG_SMP
 #define topology_cluster_id(cpu)		(cpu_data(cpu).topo.l2c_id)
 #define topology_die_cpumask(cpu)		(per_cpu(cpu_die_map, cpu))
