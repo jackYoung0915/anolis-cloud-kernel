93980a9016f24ac749de74a705a371ae74087021
Thomas Gleixner <tglx@linutronix.de>
2024-02-13 22:04:14 +0100

    x86/cpu: Use common topology code for AMD
    ANBZ: #26668
    
    commit c749ce393b8fe9db5ed894411f06eafa88f0e13a upstream.
    
    Switch it over to the new topology evaluation mechanism and remove the
    random bits and pieces which are sprinkled all over the place.
    
    No functional change intended.
    
    Intel-SIG: commit c749ce393b8f x86/cpu: Use common topology code for AMD.
    x86/cpu: Rework the topology evaluation - part2
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Tested-by: Juergen Gross <jgross@suse.com>
    Tested-by: Sohil Mehta <sohil.mehta@intel.com>
    Tested-by: Michael Kelley <mhklinux@outlook.com>
    Tested-by: Zhang Rui <rui.zhang@intel.com>
    Tested-by: Wang Wendy <wendy.wang@intel.com>
    Tested-by: K Prateek Nayak <kprateek.nayak@amd.com>
    Link: https://lore.kernel.org/r/20240212153625.145745053@linutronix.de
    [ Quanxian Wang: amend commit log ]
    Signed-off-by: Quanxian Wang <quanxian.wang@intel.com>
    Reviewed-by: Guanghui Feng <guanghuifeng@linux.alibaba.com>
    Reviewed-by: Yuanhe Shu <xiangzao@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6072

--- arch/x86/include/asm/processor.h
+++ arch/x86/include/asm/processor.h
@@ -722,7 +722,6 @@ static inline u32 per_cpu_l2c_id(unsigned int cpu)
 }
 
 #ifdef CONFIG_CPU_SUP_AMD
-extern u32 amd_get_nodes_per_socket(void);
 /*
  * Issue a DIV 0/1 insn to clear any division data from previous DIV
  * operations.
@@ -735,7 +734,6 @@ static __always_inline void amd_clear_divider(void)
 
 extern void amd_check_microcode(void);
 #else
-static inline u32 amd_get_nodes_per_socket(void)	{ return 0; }
 static inline void amd_clear_divider(void)		{ }
 static inline void amd_check_microcode(void)		{ }
 #endif
