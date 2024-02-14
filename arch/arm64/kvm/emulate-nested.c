89fca2f2b6196cc24ebfc6e09d7fba223f4418c2
Marc Zyngier <maz@kernel.org>
2024-02-14 13:18:09 +0000

    KVM: arm64: Unify HDFG[WR]TR_GROUP FGT identifiers
    ANBZ: #31782
    
    commit 0beb14de740df93a5af0edc0bd4941dc037e6688 upstream.
    
    There is no reason to have separate FGT group identifiers for
    the debug fine grain trapping. The sole requirement is to provide
    the *names* so that the SR_FGF() macro can do its magic of picking
    the correct bit definition.
    
    So let's alias HDFGWTR_GROUP and HDFGRTR_GROUP.
    
    Reviewed-by: Joey Gouly <joey.gouly@arm.com>
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Link: https://lore.kernel.org/r/20240214131827.2856277-9-maz@kernel.org
    Signed-off-by: Oliver Upton <oliver.upton@linux.dev>
    Signed-off-by: Jia He <justin.he@arm.com>

--- arch/arm64/kvm/emulate-nested.c
+++ arch/arm64/kvm/emulate-nested.c
@@ -945,7 +945,7 @@ enum fgt_group_id {
 	__NO_FGT_GROUP__,
 	HFGxTR_GROUP,
 	HDFGRTR_GROUP,
-	HDFGWTR_GROUP,
+	HDFGWTR_GROUP = HDFGRTR_GROUP,
 	HFGITR_GROUP,
 	HAFGRTR_GROUP,
 
@@ -1873,7 +1873,6 @@ bool __check_nv_sr_forward(struct kvm_vcpu *vcpu)
 		break;
 
 	case HDFGRTR_GROUP:
-	case HDFGWTR_GROUP:
 		if (is_read)
 			val = __vcpu_sys_reg(vcpu, HDFGRTR_EL2);
 		else
