a42e26ba2b1947c12f4cf93bbf7e5a809c978a81
Kaihao Bai <carlo.bai@linux.alibaba.com>
2024-02-21 17:47:39 +0800

    anolis: mm: pagecache_limit: support page cache limit with cgroup v2
    ANBZ: #12235
    ANBZ: #8281
    
    Pagecache limit of per cgroup has been supported in cgroup v1, extend it to
    cgroup v2.
    
    Signed-off-by: Kaihao Bai <carlo.bai@linux.alibaba.com>
    Reviewed-by: Xu Yu <xuyu@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/2777
    Signed-off-by: Weilin Tong <tongweilin@linux.alibaba.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/4207

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -7577,6 +7577,23 @@ static struct cftype memory_files[] = {
 		.read_u64 = memcg_reap_background_read,
 		.write_u64 = memcg_reap_background_write,
 	},
+#ifdef CONFIG_PAGECACHE_LIMIT
+	{
+		.name = "pagecache_limit.enable",
+		.read_u64 = mem_cgroup_allow_pgcache_limit_read,
+		.write_u64 = mem_cgroup_allow_pgcache_limit_write,
+	},
+	{
+		.name = "pagecache_limit.size",
+		.read_u64 = mem_cgroup_pgcache_limit_size_read,
+		.write = mem_cgroup_pgcache_limit_size_write,
+	},
+	{
+		.name = "pagecache_limit.sync",
+		.read_u64 = mem_cgroup_allow_pgcache_sync_read,
+		.write_u64 = mem_cgroup_allow_pgcache_sync_write,
+	},
+#endif
 	{ }	/* terminate */
 };
 
