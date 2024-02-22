284478c2784de76c1b22c81f7d8479c12f7a88a2
Zengruan Ye <yezengruan@huawei.com>
2024-02-22 20:53:28 +0800

    KVM: arm64: Enable PV qspinlock
    ANBZ: #20469
    
    commit d09c855efadc067d65456b04a352d559a402f273 openEuler.
    
    virt inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I8WMFU
    CVE: NA
    
    --------------------------------
    
    Linux kernel builds were run in KVM guest on HiSilicon Kunpeng920 system.
    VM guests were set up with 32, 48 and 64 vCPUs on the 32 physical CPUs.
    The kernel build (make -j<n>) was done in a VM with unpinned vCPUs 3
    times with the best time selected and <n> is the number of vCPUs
    available. The build times of the original linux 4.19.87, pvqspinlock
    with various number of vCPUs are as follows:
    
      Kernel        32 vCPUs    48 vCPUs    60 vCPUs
      ----------    --------    --------    --------
      4.19.87       342.336s    602.048s    950.340s
      pvqsinlock    341.366s    376.135s    437.037s
    
    Signed-off-by: Zengruan Ye <yezengruan@huawei.com>
    Signed-off-by: lishusen <lishusen2@huawei.com>
    [ Shuai Xue: fix minor conflict, add config depends on PARAVIRT_SCHED,
     and move ahead to init pv_sched in setup_arch() to aovid calltrace,
     add ARM_SMCCC_HV_PV_QSPINLOCK_FEATURES to check feature ]
    Signed-off-by: Shuai Xue <xueshuai@linux.alibaba.com>
    Reviewed-by: Wu Zongyong <wuzongyong@linux.alibaba.com>
    Reviewed-by: Jay Chen <jkchen@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5125

--- arch/arm64/include/asm/paravirt.h
+++ arch/arm64/include/asm/paravirt.h
@@ -30,6 +30,45 @@ static inline bool pv_vcpu_is_preempted(int cpu)
 {
 	return static_call(pv_vcpu_preempted)(cpu);
 }
+
+#if defined(CONFIG_SMP) && defined(CONFIG_PARAVIRT_SPINLOCKS)
+void __init pv_qspinlock_init(void);
+bool pv_is_native_spin_unlock(void);
+
+void dummy_queued_spin_lock_slowpath(struct qspinlock *lock, u32 val);
+DECLARE_STATIC_CALL(pv_qspinlock_queued_spin_lock_slowpath,
+		    dummy_queued_spin_lock_slowpath);
+static inline void pv_queued_spin_lock_slowpath(struct qspinlock *lock, u32 val)
+{
+	return static_call(pv_qspinlock_queued_spin_lock_slowpath)(lock, val);
+}
+
+void dummy_queued_spin_unlock(struct qspinlock *lock);
+DECLARE_STATIC_CALL(pv_qspinlock_queued_spin_unlock, dummy_queued_spin_unlock);
+static inline void pv_queued_spin_unlock(struct qspinlock *lock)
+{
+	return static_call(pv_qspinlock_queued_spin_unlock)(lock);
+}
+
+void dummy_wait(u8 *ptr, u8 val);
+DECLARE_STATIC_CALL(pv_qspinlock_wait, dummy_wait);
+static inline void pv_wait(u8 *ptr, u8 val)
+{
+	return static_call(pv_qspinlock_wait)(ptr, val);
+}
+
+void dummy_kick(int cpu);
+DECLARE_STATIC_CALL(pv_qspinlock_kick, dummy_kick);
+static inline void pv_kick(int cpu)
+{
+	return static_call(pv_qspinlock_kick)(cpu);
+}
+#else
+
+#define pv_qspinlock_init() do {} while (0)
+
+#endif /* SMP && PARAVIRT_SPINLOCKS */
+
 #endif /* CONFIG_PARAVIRT_SCHED */
 
 #else
