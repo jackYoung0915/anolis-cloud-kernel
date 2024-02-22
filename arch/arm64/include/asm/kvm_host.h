932984b080f51239b10190ca98649605b81e66a3
Zengruan Ye <yezengruan@huawei.com>
2024-02-22 20:53:27 +0800

    KVM: arm64: Implement PV_SCHED_KICK_CPU call
    ANBZ: #20469
    
    commit 1573064aa7756bc021fde0283f985a6339398356 openEuler.
    
    virt inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I8WMFU
    CVE: NA
    
    --------------------------------
    
    Implement the service call for waking up a WFI state vCPU.
    
    Signed-off-by: Zengruan Ye <yezengruan@huawei.com>
    Signed-off-by: lishusen <lishusen2@huawei.com>
    [ Shuai Xue: Update 'pv_unhalted' as the last step to prevent potential
    hangs, originally suggested by Wu Zongyong ]
    Signed-off-by: Shuai Xue <xueshuai@linux.alibaba.com>

--- arch/arm64/include/asm/kvm_host.h
+++ arch/arm64/include/asm/kvm_host.h
@@ -592,6 +592,7 @@ struct kvm_vcpu_arch {
 #ifdef CONFIG_PARAVIRT_SCHED
 	/* Guest PV sched state */
 	struct {
+		bool pv_unhalted;
 		gpa_t base;
 	} pvsched;
 #endif
@@ -1047,6 +1048,7 @@ static inline bool kvm_arm_is_pvtime_enabled(struct kvm_vcpu_arch *vcpu_arch)
 
 #ifdef CONFIG_PARAVIRT_SCHED
 void kvm_update_pvsched_preempted(struct kvm_vcpu *vcpu, u32 preempted);
+long kvm_pvsched_kick_vcpu(struct kvm_vcpu *vcpu);
 
 static inline void kvm_arm_pvsched_vcpu_init(struct kvm_vcpu_arch *vcpu_arch)
 {
@@ -1060,6 +1062,10 @@ static inline bool kvm_arm_is_pvsched_enabled(struct kvm_vcpu_arch *vcpu_arch)
 #else
 static inline void kvm_update_pvsched_preempted(struct kvm_vcpu *vcpu,
 						u32 preempted) {}
+static inline long kvm_pvsched_kick_vcpu(struct kvm_vcpu *vcpu)
+{
+	return 0;
+}
 static inline void kvm_arm_pvsched_vcpu_init(struct kvm_vcpu_arch *vcpu_arch) {}
 static inline bool kvm_arm_is_pvsched_enabled(struct kvm_vcpu_arch *vcpu_arch)
 {
