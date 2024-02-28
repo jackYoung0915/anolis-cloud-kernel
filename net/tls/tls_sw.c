1d6833126814bf52d4143bf0c45a4da653ffb8d6
Sabrina Dubroca <sd@queasysnail.net>
2024-02-29 06:43:59 +0800

    tls: separate no-async decryption request handling from async
    ANBZ: #24384
    
    commit 41532b785e9d79636b3815a64ddf6a096647d011 upstream.
    
    If we're not doing async, the handling is much simpler. There's no
    reference counting, we just need to wait for the completion to wake us
    up and return its result.
    
    We should preferably also use a separate crypto_wait. I'm not seeing a
    UAF as I did in the past, I think aec7961916f3 ("tls: fix race between
    async notify and socket close") took care of it.
    
    This will make the next fix easier.
    
    Signed-off-by: Sabrina Dubroca <sd@queasysnail.net>
    Link: https://lore.kernel.org/r/47bde5f649707610eaef9f0d679519966fc31061.1709132643.git.sd@queasysnail.net
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    
    Signed-off-by: Xiao Long <xiaolong@openanolis.org>
    
    [Fixes conflicts]
    Fixes: CVE-2024-58240
    Signed-off-by: Xiao Long <xiaolong@openanolis.org>
    Signed-off-by: Wen Gu <guwen@linux.alibaba.com>
    Reviewed-by: Xuan Zhuo <xuanzhuo@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6377

--- net/tls/tls_sw.c
+++ net/tls/tls_sw.c
@@ -273,9 +273,15 @@ static int tls_do_decryption(struct sock *sk,
 		BUILD_BUG_ON_INVALID(atomic_read(&ctx->decrypt_pending) < 1);
 		atomic_inc(&ctx->decrypt_pending);
 	} else {
+		DECLARE_CRYPTO_WAIT(wait);
+
 		aead_request_set_callback(aead_req,
 					  CRYPTO_TFM_REQ_MAY_BACKLOG,
-					  crypto_req_done, &ctx->async_wait);
+					  crypto_req_done, &wait);
+		ret = crypto_aead_decrypt(aead_req);
+		if (ret == -EINPROGRESS || ret == -EBUSY)
+			ret = crypto_wait_req(ret, &wait);
+		return ret;
 	}
 
 	ret = crypto_aead_decrypt(aead_req);
@@ -283,12 +289,8 @@ static int tls_do_decryption(struct sock *sk,
 		ret = tls_decrypt_async_wait(ctx);
 		ret = ret ?: -EINPROGRESS;
 	}
-	if (ret == -EINPROGRESS) {
-		if (async)
-			return ret;
-
-		ret = crypto_wait_req(ret, &ctx->async_wait);
-	}
+	if (ret == -EINPROGRESS)
+		return 0;
 
 	if (async)
 		atomic_dec(&ctx->decrypt_pending);
