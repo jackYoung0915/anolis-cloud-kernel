1841a7f0299dd8aa9681cc1a2f1a85fd82cc0a18
Ferry Meng <mengferry@linux.alibaba.com>
2024-03-06 17:49:08 +0800

    anolis: io_uring: re-add sqthread percpu polling support
    ANBZ: #8410
    
    add sqthread percpu polling feature to acnk-6.6.
    
    ----
    we should deal with sqthread
    creation method 'create_io_thread' when applying SQTHREAD_PERCPU feature
    in ANCK, which used to be 'kthread_create'.
    
    There are some cases we found and solved in this patch:
    
    1. avoid premature destruction in sqthread_percpu_polling
    
    We found a cornor case in sqthread_percpu_polling usage: There are two
    processes (not in the same thread group) sharing the same sqthread.
    Besides, the first process created the sqthread. If the first process
    end its execution, the sqthread end its life and exit too, causing the
    second process's halt.
    
    This behaviour is not accepted. And the reason is: we want to make
    'sqd->refs' to be the trigger for sqthread exiting, but when the first
    process end, it will also let sqthread end. Thus, we should check
    sqd->refs to make sure there isn't exist others using this sqthread.
    
    2. modify create_io_thread to adapt sqthread_percpu_poll.
    
    In contrast to original kthread_create sqthread, create_io_thread is
    more suitable for threads in the same group sharing one sqthread. But
    this is not perfect for io_thread sharing between processes( like
    sqthread_percpu usage).
    
    This patch originates from a cornor case:
    There are two processes sharing one sqthread, and the process0
    create the sqthread. If we send a SIGKILL to the process0, sqthread
    can't EOL normally.
    
    The reason is that sqthread shared io_uring_files and mm with process0
    by using 'CLONE_VM' 'CLONE_FILES' etc.
    As we 'kill -9 process0', duing to reference count, the io_uring_file
    finally will be released by sqthread, not process0. What we want is:
    
    process0..1..2 <io_uring_release> ->
    	<io_put_sq_data> ->
    		sqd->refs-- && sqd->refs == 0 ? <io_sq_thread_stop> ->
    			io_sq_thread EOL.
    
    But now it changes to: io_sq_thread should get out of its loop, and
    release io_uring file in <do_exit>. But we use 'sqd->refs' judgement to
    avoid its premature exit, which causing a 'deadlock'.
    
    In fact, if we make a sqthread sharing between processes, it has better
    not shared only special one process's FILES or VM. By delete these
    flags, sqthread's runtime is more like an independent kernel thread.
    
    By the way, in the original kthread_create sqthread creation mode, as
    it's generated by kthreadd, it has no mm, sharing 'init_fs' and
    'init_files', this is quite different from current create_io_thread.
    
    Signed-off-by: Ferry Meng <mengferry@linux.alibaba.com>
    Reviewed-by: Joseph Qi <joseph.qi@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/2822

--- io_uring/io_uring.c
+++ io_uring/io_uring.c
@@ -137,6 +137,8 @@ struct io_defer_entry {
 	u32			seq;
 };
 
+extern struct io_sq_data __percpu **percpu_sqd;
+
 /* requests with any of those set should undergo io_disarm_next() */
 #define IO_DISARM_MASK (REQ_F_ARM_LTIMEOUT | REQ_F_LINK_TIMEOUT | REQ_F_FAIL)
 #define IO_REQ_LINK_FLAGS (REQ_F_LINK | REQ_F_HARDLINK)
@@ -4071,7 +4073,7 @@ static long io_uring_setup(u32 entries, struct io_uring_params __user *params)
 			IORING_SETUP_SQE128 | IORING_SETUP_CQE32 |
 			IORING_SETUP_SINGLE_ISSUER | IORING_SETUP_DEFER_TASKRUN |
 			IORING_SETUP_NO_MMAP | IORING_SETUP_REGISTERED_FD_ONLY |
-			IORING_SETUP_NO_SQARRAY))
+			IORING_SETUP_NO_SQARRAY | IORING_SETUP_SQPOLL_PERCPU))
 		return -EINVAL;
 
 	return io_uring_create(entries, &p, params);
@@ -4360,7 +4362,7 @@ static __cold int io_register_iowq_max_workers(struct io_ring_ctx *ctx,
 	if (sqd) {
 		mutex_unlock(&ctx->uring_lock);
 		mutex_unlock(&sqd->lock);
-		io_put_sq_data(sqd);
+		io_put_sq_data(ctx, sqd);
 		mutex_lock(&ctx->uring_lock);
 	}
 
@@ -4388,9 +4390,8 @@ err:
 	if (sqd) {
 		mutex_unlock(&ctx->uring_lock);
 		mutex_unlock(&sqd->lock);
-		io_put_sq_data(sqd);
+		io_put_sq_data(ctx, sqd);
 		mutex_lock(&ctx->uring_lock);
-
 	}
 	return ret;
 }
@@ -4611,6 +4612,8 @@ out_fput:
 
 static int __init io_uring_init(void)
 {
+	int cpu;
+
 #define __BUILD_BUG_VERIFY_OFFSET_SIZE(stype, eoffset, esize, ename) do { \
 	BUILD_BUG_ON(offsetof(stype, ename) != eoffset); \
 	BUILD_BUG_ON(sizeof_field(stype, ename) != esize); \
@@ -4700,6 +4703,9 @@ static int __init io_uring_init(void)
 				sizeof_field(struct io_kiocb, cmd.data), NULL);
 
 	iou_wq = alloc_workqueue("iou_exit", WQ_UNBOUND, 64);
+	percpu_sqd = alloc_percpu(struct io_sq_data *);
+	for_each_possible_cpu(cpu)
+		*per_cpu_ptr(percpu_sqd, cpu) = NULL;
 
 #ifdef CONFIG_SYSCTL
 	register_sysctl_init("kernel", kernel_io_uring_disabled_table);
