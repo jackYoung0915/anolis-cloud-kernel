dc5108d2b6d5b6719ef0625e3fb823bd09ab754a
Ferry Meng <mengferry@linux.alibaba.com>
2024-03-06 17:51:39 +0800

    anolis: blk-throttle: add throttled io/bytes counter
    ANBZ: #34471
    
    cherry picked from devel-6.6 commit 7256805e79fdd0db40e234a2b479bef5a7072e51.
    
    Add 2 interfaces to stat io throttle information:
      blkio.throttle.total_io_queued
      blkio.throttle.total_bytes_queued
    
    These interfaces are used for monitoring throttled io/bytes and
    analyzing if delay has relation with io throttle.
    
    Signed-off-by: Joseph Qi <joseph.qi@linux.alibaba.com>
    Signed-off-by: Ferry Meng <mengferry@linux.alibaba.com>

--- block/blk-throttle.c
+++ block/blk-throttle.c
@@ -271,7 +271,9 @@ static struct blkg_policy_data *throtl_pd_alloc(struct gendisk *disk,
 	if (blkg_rwstat_init(&tg->stat_bytes, gfp) ||
 	    blkg_rwstat_init(&tg->stat_ios, gfp) ||
 	    blkg_rwstat_init(&tg->service_time, gfp) ||
-	    blkg_rwstat_init(&tg->wait_time, gfp))
+	    blkg_rwstat_init(&tg->wait_time, gfp) ||
+	    blkg_rwstat_init(&tg->total_bytes_queued, gfp) ||
+	    blkg_rwstat_init(&tg->total_io_queued, gfp))
 		goto err;
 
 	throtl_service_queue_init(&tg->service_queue);
@@ -294,6 +296,8 @@ err:
 	blkg_rwstat_exit(&tg->stat_ios);
 	blkg_rwstat_exit(&tg->service_time);
 	blkg_rwstat_exit(&tg->wait_time);
+	blkg_rwstat_exit(&tg->total_bytes_queued);
+	blkg_rwstat_exit(&tg->total_io_queued);
 	kfree(tg);
 	return NULL;
 }
@@ -366,6 +370,8 @@ static void throtl_pd_free(struct blkg_policy_data *pd)
 	blkg_rwstat_exit(&tg->stat_ios);
 	blkg_rwstat_exit(&tg->service_time);
 	blkg_rwstat_exit(&tg->wait_time);
+	blkg_rwstat_exit(&tg->total_bytes_queued);
+	blkg_rwstat_exit(&tg->total_io_queued);
 	kfree(tg);
 }
 
@@ -375,6 +381,8 @@ static void throtl_pd_reset(struct blkg_policy_data *pd)
 
 	blkg_rwstat_reset(&tg->service_time);
 	blkg_rwstat_reset(&tg->wait_time);
+	blkg_rwstat_reset(&tg->total_bytes_queued);
+	blkg_rwstat_reset(&tg->total_io_queued);
 }
 
 static struct throtl_grp *
@@ -1025,6 +1033,9 @@ static void throtl_add_bio_tg(struct bio *bio, struct throtl_qnode *qn,
 	    bio == throtl_peek_queued(&sq->queued[rw]))
 		tg->flags |= THROTL_TG_IOPS_WAS_EMPTY;
 
+	blkg_rwstat_add(&tg->total_bytes_queued, bio_op(bio),
+			throtl_bio_data_size(bio));
+	blkg_rwstat_add(&tg->total_io_queued, bio_op(bio), 1);
 	throtl_enqueue_tg(tg);
 }
 
@@ -1551,6 +1562,16 @@ static struct cftype throtl_legacy_files[] = {
 		.private = offsetof(struct throtl_grp, wait_time),
 		.seq_show = tg_print_rwstat,
 	},
+	{
+		.name = "throttle.total_bytes_queued",
+		.private = offsetof(struct throtl_grp, total_bytes_queued),
+		.seq_show = tg_print_rwstat,
+	},
+	{
+		.name = "throttle.total_io_queued",
+		.private = offsetof(struct throtl_grp, total_io_queued),
+		.seq_show = tg_print_rwstat,
+	},
 	{ }	/* terminate */
 };
 
@@ -1747,6 +1768,10 @@ static void throtl_pd_offline(struct blkg_policy_data *pd)
 				    &tg->service_time);
 		blkg_rwstat_add_aux(&blkg_to_tg(parent)->wait_time,
 				    &tg->wait_time);
+		blkg_rwstat_add_aux(&blkg_to_tg(parent)->total_bytes_queued,
+				    &tg->total_bytes_queued);
+		blkg_rwstat_add_aux(&blkg_to_tg(parent)->total_io_queued,
+				    &tg->total_io_queued);
 	}
 }
 
