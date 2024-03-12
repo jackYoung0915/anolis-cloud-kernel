1a90a93112671220cb0d2f803e432e01eb46e210
Charlie Jenkins <charlie@rivosinc.com>
2024-03-12 16:53:41 -0700

    riscv: Include riscv_set_icache_flush_ctx prctl
    ANBZ: #32730
    
    commit 6b9391b581fddd8579239dad4de4f0393149e10a upstream.
    
    Support new prctl with key PR_RISCV_SET_ICACHE_FLUSH_CTX to enable
    optimization of cross modifying code. This prctl enables userspace code
    to use icache flushing instructions such as fence.i with the guarantee
    that the icache will continue to be clean after thread migration.
    
    Signed-off-by: Charlie Jenkins <charlie@rivosinc.com>
    Reviewed-by: Atish Patra <atishp@rivosinc.com>
    Reviewed-by: Alexandre Ghiti <alexghiti@rivosinc.com>
    Reviewed-by: Samuel Holland <samuel.holland@sifive.com>
    Link: https://lore.kernel.org/r/20240312-fencei-v13-2-4b6bdc2bbf32@rivosinc.com
    Signed-off-by: Palmer Dabbelt <palmer@rivosinc.com>
    [Ruidong:
    Conflicts:
      resolve: arch/riscv/include/asm/processor.h, arch/riscv/include/asm/switch_to.h
      reason:
        Context conflict only, patch content unchanged
    ]
    Signed-off-by: Ruidong Tian <tianruidong@linux.alibaba.com>
    Acked-by: Jakub Kicinski <kuba@kernel.org>
    Reviewed-by: Simon Horman <horms@kernel.org>
    Reviewed-by: Clément Léger <cleger@rivosinc.com>
    Reviewed-by: Fangyu Yu <fangyu.yu@linux.alibaba.com>
    Reviewed-by: Rui Gao <gao.rui@zte.com.cn>
    Reviewed-by: Jiankang Chen <jkchen@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6707

--- arch/riscv/include/asm/processor.h
+++ arch/riscv/include/asm/processor.h
@@ -109,6 +109,12 @@ struct thread_struct {
 	struct __riscv_v_ext_state vstate;
 	unsigned long align_ctl;
 	struct __riscv_v_ext_state kernel_vstate;
+#ifdef CONFIG_SMP
+	/* Flush the icache on migration */
+	bool force_icache_flush;
+	/* A forced icache flush is not needed if migrating to the previous cpu. */
+	unsigned int prev_cpu;
+#endif
 };
 
 /* Whitelist the fstate from the task_struct for hardened usercopy */
@@ -188,6 +194,9 @@ extern int set_unalign_ctl(struct task_struct *tsk, unsigned int val);
 
 #define GET_UNALIGN_CTL(tsk, addr)	get_unalign_ctl((tsk), (addr))
 #define SET_UNALIGN_CTL(tsk, val)	set_unalign_ctl((tsk), (val))
+
+#define RISCV_SET_ICACHE_FLUSH_CTX(arg1, arg2)	riscv_set_icache_flush_ctx(arg1, arg2)
+extern int riscv_set_icache_flush_ctx(unsigned long ctx, unsigned long per_thread);
 
 #ifdef CONFIG_RISCV_ISA_SUPM
 /* PR_{SET,GET}_TAGGED_ADDR_CTRL prctl */
