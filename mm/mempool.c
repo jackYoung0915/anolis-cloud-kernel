b32fe4d5945aef9904b68db651f76e60ea086152
Kent Overstreet <kent.overstreet@linux.dev>
2024-03-21 09:36:48 -0700

    mempool: hook up to memory allocation profiling
    ANBZ: #32420
    
    commit e26d8769da6d4c7319fdd4ddb497caa7d87a6c5c upstream.
    
    This adds hooks to mempools for correctly annotating mempool-backed
    allocations at the correct source line, so they show up correctly in
    /sys/kernel/debug/allocations.
    
    Various inline functions are converted to wrappers so that we can invoke
    alloc_hooks() in fewer places.
    
    [surenb@google.com: undo _noprof additions in the documentation]
      Link: https://lkml.kernel.org/r/20240326231453.1206227-4-surenb@google.com
    [surenb@google.com: add missing mempool_create_node documentation]
      Link: https://lkml.kernel.org/r/20240402180835.1661905-1-surenb@google.com
    Link: https://lkml.kernel.org/r/20240321163705.3067592-27-surenb@google.com
    Signed-off-by: Kent Overstreet <kent.overstreet@linux.dev>
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Tested-by: Kees Cook <keescook@chromium.org>
    Cc: Alexander Viro <viro@zeniv.linux.org.uk>
    Cc: Alex Gaynor <alex.gaynor@gmail.com>
    Cc: Alice Ryhl <aliceryhl@google.com>
    Cc: Andreas Hindborg <a.hindborg@samsung.com>
    Cc: Benno Lossin <benno.lossin@proton.me>
    Cc: "Björn Roy Baron" <bjorn3_gh@protonmail.com>
    Cc: Boqun Feng <boqun.feng@gmail.com>
    Cc: Christoph Lameter <cl@linux.com>
    Cc: Dennis Zhou <dennis@kernel.org>
    Cc: Gary Guo <gary@garyguo.net>
    Cc: Miguel Ojeda <ojeda@kernel.org>
    Cc: Pasha Tatashin <pasha.tatashin@soleen.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Tejun Heo <tj@kernel.org>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Cc: Wedson Almeida Filho <wedsonaf@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Yuanhe Shu <xiangzao@linux.alibaba.com>
    Acked-by: David Rientjes <rientjes@google.com>
    Acked-by: Harry Yoo <harry.yoo@oracle.com>
    Acked-by: Muchun Song <muchun.song@linux.dev>
    Acked-by: Vlastimil Babka <vbabka@suse.cz>
    Reviewed-by: Kees Cook <kees@kernel.org>
    Reviewed-by: Hao Li <hao.li@linux.dev>
    Reviewed-by: Miaohe Lin <linmiaohe@huawei.com>
    Acked-by: David Hildenbrand <david@redhat.com>
    Reviewed-by: Pasha Tatashin <pasha.tatashin@soleen.com>
    Acked-by: Shakeel Butt <shakeel.butt@linux.dev>
    Acked-by: SeongJae Park <sj@kernel.org>
    Acked-by: Yu Zhao <yuzhao@google.com>
    Reviewed-by: Anshuman Khandual <anshuman.khandual@arm.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6766

--- mm/mempool.c
+++ mm/mempool.c
@@ -230,22 +230,24 @@ EXPORT_SYMBOL(mempool_init_node);
  *
  * Return: %0 on success, negative error code otherwise.
  */
-int mempool_init(mempool_t *pool, int min_nr, mempool_alloc_t *alloc_fn,
-		 mempool_free_t *free_fn, void *pool_data)
+int mempool_init_noprof(mempool_t *pool, int min_nr, mempool_alloc_t *alloc_fn,
+			mempool_free_t *free_fn, void *pool_data)
 {
 	return mempool_init_node(pool, min_nr, alloc_fn, free_fn,
 				 pool_data, GFP_KERNEL, NUMA_NO_NODE);
 
 }
-EXPORT_SYMBOL(mempool_init);
+EXPORT_SYMBOL(mempool_init_noprof);
 
 /**
- * mempool_create - create a memory pool
+ * mempool_create_node - create a memory pool
  * @min_nr:    the minimum number of elements guaranteed to be
  *             allocated for this pool.
  * @alloc_fn:  user-defined element-allocation function.
  * @free_fn:   user-defined element-freeing function.
  * @pool_data: optional private data available to the user-defined functions.
+ * @gfp_mask:  memory allocation flags
+ * @node_id:   numa node to allocate on
  *
  * this function creates and allocates a guaranteed size, preallocated
  * memory pool. The pool can be used from the mempool_alloc() and mempool_free()
@@ -255,17 +257,9 @@ EXPORT_SYMBOL(mempool_init);
  *
  * Return: pointer to the created memory pool object or %NULL on error.
  */
-mempool_t *mempool_create(int min_nr, mempool_alloc_t *alloc_fn,
-				mempool_free_t *free_fn, void *pool_data)
-{
-	return mempool_create_node(min_nr, alloc_fn, free_fn, pool_data,
-				   GFP_KERNEL, NUMA_NO_NODE);
-}
-EXPORT_SYMBOL(mempool_create);
-
-mempool_t *mempool_create_node(int min_nr, mempool_alloc_t *alloc_fn,
-			       mempool_free_t *free_fn, void *pool_data,
-			       gfp_t gfp_mask, int node_id)
+mempool_t *mempool_create_node_noprof(int min_nr, mempool_alloc_t *alloc_fn,
+				      mempool_free_t *free_fn, void *pool_data,
+				      gfp_t gfp_mask, int node_id)
 {
 	mempool_t *pool;
 
@@ -281,7 +275,16 @@ mempool_t *mempool_create_node(int min_nr, mempool_alloc_t *alloc_fn,
 
 	return pool;
 }
-EXPORT_SYMBOL(mempool_create_node);
+EXPORT_SYMBOL(mempool_create_node_noprof);
+/* kapi: preserve original exported symbol */
+#undef mempool_create
+mempool_t *mempool_create(int min_nr, mempool_alloc_t *alloc_fn,
+			  mempool_free_t *free_fn, void *pool_data)
+{
+	return mempool_create_node_noprof(min_nr, alloc_fn, free_fn, pool_data,
+					  GFP_KERNEL, NUMA_NO_NODE);
+}
+EXPORT_SYMBOL(mempool_create);
 
 /**
  * mempool_resize - resize an existing memory pool
@@ -377,7 +380,7 @@ EXPORT_SYMBOL(mempool_resize);
  *
  * Return: pointer to the allocated element or %NULL on error.
  */
-void *mempool_alloc(mempool_t *pool, gfp_t gfp_mask)
+void *mempool_alloc_noprof(mempool_t *pool, gfp_t gfp_mask)
 {
 	void *element;
 	unsigned long flags;
@@ -444,6 +447,10 @@ repeat_alloc:
 	finish_wait(&pool->wait, &wait);
 	goto repeat_alloc;
 }
+EXPORT_SYMBOL(mempool_alloc_noprof);
+/* kapi: preserve original exported symbol */
+#undef mempool_alloc
+extern __malloc typeof(mempool_alloc_noprof) mempool_alloc __attribute__((alias("mempool_alloc_noprof")));
 EXPORT_SYMBOL(mempool_alloc);
 
 /**
@@ -515,7 +522,7 @@ void *mempool_alloc_slab(gfp_t gfp_mask, void *pool_data)
 {
 	struct kmem_cache *mem = pool_data;
 	VM_BUG_ON(mem->ctor);
-	return kmem_cache_alloc(mem, gfp_mask);
+	return kmem_cache_alloc_noprof(mem, gfp_mask);
 }
 EXPORT_SYMBOL(mempool_alloc_slab);
 
@@ -533,7 +540,7 @@ EXPORT_SYMBOL(mempool_free_slab);
 void *mempool_kmalloc(gfp_t gfp_mask, void *pool_data)
 {
 	size_t size = (size_t)pool_data;
-	return kmalloc(size, gfp_mask);
+	return kmalloc_noprof(size, gfp_mask);
 }
 EXPORT_SYMBOL(mempool_kmalloc);
 
@@ -550,7 +557,7 @@ EXPORT_SYMBOL(mempool_kfree);
 void *mempool_alloc_pages(gfp_t gfp_mask, void *pool_data)
 {
 	int order = (int)(long)pool_data;
-	return alloc_pages(gfp_mask, order);
+	return alloc_pages_noprof(gfp_mask, order);
 }
 EXPORT_SYMBOL(mempool_alloc_pages);
 
