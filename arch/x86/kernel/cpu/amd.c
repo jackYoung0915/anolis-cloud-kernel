9e2e054ca7101bbef9fef0dd8d650412409222f8
Borislav Petkov (AMD) <bp@alien8.de>
2024-03-27 16:43:17 +0100

    x86/CPU/AMD: Track SNP host status with cc_platform_*()
    ANBZ: #34453
    
    commit 0ecaefb303de69929dc0036d5021d01cec7ea052 upstream
    
    The host SNP worthiness can determined later, after alternatives have
    been patched, in snp_rmptable_init() depending on cmdline options like
    iommu=pt which is incompatible with SNP, for example.
    
    Which means that one cannot use X86_FEATURE_SEV_SNP and will need to
    have a special flag for that control.
    
    Use that newly added CC_ATTR_HOST_SEV_SNP in the appropriate places.
    
    Move kdump_sev_callback() to its rightful place, while at it.
    
    Fixes: 216d106c7ff7 ("x86/sev: Add SEV-SNP host initialization support")
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    Reviewed-by: Tom Lendacky <thomas.lendacky@amd.com>
    Tested-by: Srikanth Aithal <sraithal@amd.com>
    Link: https://lore.kernel.org/r/20240327154317.29909-6-bp@alien8.de
    Signed-off-by: Abhishek Rajput <Abhishek.Rajput@amd.com>
    Signed-off-by: mohanasv <mohanasv@amd.com>
    Reviewed-by: Tianjia Zhang <tianjia.zhang@linux.alibaba.com>
    Reviewed-by: Qinyun Tan <qinyuntan@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/7076

--- arch/x86/kernel/cpu/amd.c
+++ arch/x86/kernel/cpu/amd.c
@@ -407,6 +407,28 @@ static void tsa_init(struct cpuinfo_x86 *c)
 		setup_force_cpu_cap(X86_FEATURE_TSA_L1_NO);
 	}
 }
+static void bsp_determine_snp(struct cpuinfo_x86 *c)
+{
+#ifdef CONFIG_ARCH_HAS_CC_PLATFORM
+	cc_vendor = CC_VENDOR_AMD;
+
+	if (cpu_has(c, X86_FEATURE_SEV_SNP)) {
+		/*
+		 * RMP table entry format is not architectural and is defined by the
+		 * per-processor PPR. Restrict SNP support on the known CPU models
+		 * for which the RMP table entry format is currently defined for.
+		 */
+		if (!cpu_has(c, X86_FEATURE_HYPERVISOR) &&
+		    c->x86 >= 0x19 && snp_probe_rmptable_info()) {
+			cc_platform_set(CC_ATTR_HOST_SEV_SNP);
+		} else {
+			setup_clear_cpu_cap(X86_FEATURE_SEV_SNP);
+			cc_platform_clear(CC_ATTR_HOST_SEV_SNP);
+		}
+	}
+#endif
+}
+
 
 static void bsp_init_amd(struct cpuinfo_x86 *c)
 {
@@ -521,22 +543,8 @@ static void bsp_init_amd(struct cpuinfo_x86 *c)
 
 
 	tsa_init(c);
-	if (cpu_has(c, X86_FEATURE_SEV_SNP)) {
-		/*
-		 * RMP table entry format is not architectural and it can vary by processor
-		 * and is defined by the per-processor PPR. Restrict SNP support on the
-		 * known CPU model and family for which the RMP table entry format is
-		 * currently defined for.
-		 */
-		if (!boot_cpu_has(X86_FEATURE_ZEN3) &&
-		    !boot_cpu_has(X86_FEATURE_ZEN4) &&
-		    !boot_cpu_has(X86_FEATURE_ZEN5))
-			setup_clear_cpu_cap(X86_FEATURE_SEV_SNP);
-		else if (!snp_probe_rmptable_info())
-			setup_clear_cpu_cap(X86_FEATURE_SEV_SNP);
-	}
 
-
+	bsp_determine_snp(c);
 	return;
 
 warn:
