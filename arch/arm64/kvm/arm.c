c47ab5ca6c517bce253f07d9d75ac149ac297bce
Xiang Chen <chenxiang66@hisilicon.com>
2024-04-01 10:04:48 +0800

    KVM: arm64: Translate logic cluster id to physical cluster id when updating lsudvmbm
    ANBZ: #11194
    
    commit 2933228be762e08bc76ea225b539ee622a0f2e22 openeuler
    
    ****************************************************************
    squash the two patches for ci entrance guard.
    ****************************************************************
    
    virt inclusion
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/I99TYA
    
    -----------------------------------------------------------------
    
    For dvmbm feature, MN requires physical cluster id while it is
    filled with logic cluster id right now. In some situations which
    physical cluster id is not equal to logic cluster id such as in
    PG boards, it will cause issues when enabling dvmbm.
    
    To avoid the issue, translate logic cluster id to physical cluster
    id when updating lsudvmbm.
    
    Signed-off-by: Xiang Chen <chenxiang66@hisilicon.com>
    Signed-off-by: Yanan Wang <wangyanan55@huawei.com>
    Signed-off-by: caijian <caijian11@h-partners.com>
    Signed-off-by: Kunkun Jiang <jiangkunkun@huawei.com>
    Signed-off-by: Min Li <gumi@linux.alibaba.com>
    
    kvm: arm64: Fix a compilation error
    
    ANBZ: #11194
    
    commit e576e77219e82e673669ce7cbe32abad5be9359f openeuler
    
    virt inclusion
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/I99TYA
    CVE: NA
    
    ---------------------------------------------------------------
    
    When HISI_VIRT config is turned off, a compilation error will be reported.
    
    Fixes: 2933228be762 ("KVM: arm64: Translate logic cluster id to physical cluster id when updating lsudvmbm")
    Signed-off-by: Kunkun Jiang <jiangkunkun@huawei.com>
    Signed-off-by: Min Li <gumi@linux.alibaba.com>

--- arch/arm64/kvm/arm.c
+++ arch/arm64/kvm/arm.c
@@ -1921,6 +1921,8 @@ int kvm_arch_init(void *opaque)
 	probe_hisi_cpu_type();
 	kvm_ncsnp_support = hisi_ncsnp_supported();
 	kvm_dvmbm_support = hisi_dvmbm_supported();
+	if (kvm_dvmbm_support)
+		kvm_get_pg_cfg();
 #endif
 	kvm_info("KVM ncsnp %s\n", kvm_ncsnp_support ? "enabled" : "disabled");
 	kvm_info("KVM dvmbm %s\n", kvm_dvmbm_support ? "enabled" : "disabled");
