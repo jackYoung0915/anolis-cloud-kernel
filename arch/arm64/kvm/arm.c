8b5a2f06b8a9f6ee276f745d7b9bfba08b425358
Xiang Chen <chenxiang66@hisilicon.com>
2024-04-01 10:04:48 +0800

    KVM: arm64: Translate logic cluster id to physical cluster id when updating lsudvmbm
    ANBZ: #11194
    
    commit 2933228be762e08bc76ea225b539ee622a0f2e22 openeuler
    
    virt inclusion
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/I99TYA
    
    -----------------------------------------------------------------
    
    For dvmbm feature, MN requires physical cluster id while it is
    filled with logic cluster id right now. In some situations which
    physical cluster id is not equal to logic cluster id such as in
    PG boards, it will cause issues when enabling dvmbm.
    
    To avoid the issue, translate logic cluster id to physical cluster
    id when updating lsudvmbm.
    
    Signed-off-by: Xiang Chen <chenxiang66@hisilicon.com>
    Signed-off-by: Yanan Wang <wangyanan55@huawei.com>
    Signed-off-by: caijian <caijian11@h-partners.com>
    Signed-off-by: Kunkun Jiang <jiangkunkun@huawei.com>
    Signed-off-by: Min Li <gumi@linux.alibaba.com>

--- arch/arm64/kvm/arm.c
+++ arch/arm64/kvm/arm.c
@@ -1919,6 +1919,9 @@ int kvm_arch_init(void *opaque)
 #endif
 	kvm_info("KVM dvmbm %s\n", kvm_dvmbm_support ? "enabled" : "disabled");
 
+	if (kvm_dvmbm_support)
+		kvm_get_pg_cfg();
+
 	in_hyp_mode = is_kernel_in_hyp_mode();
 
 	if (!in_hyp_mode && kvm_arch_requires_vhe()) {
