96555987f42d48d37ba2ef058472193562e27b7c
Matthew Wilcox (Oracle) <willy@infradead.org>
2024-04-05 16:32:25 +0100

    mm: inline destroy_large_folio() into __folio_put_large()
    ANBZ: #30843
    
    commit 2542b1ac9a46ac58f9565de0048457956898d481 upstream
    
    destroy_large_folio() has only one caller, move its contents there.
    
    [Backport notes: coldpgs module still use destroy_large_folio(), so
    we will not remove this function in this patch.]
    
    Link: https://lkml.kernel.org/r/20240405153228.2563754-4-willy@infradead.org
    Signed-off-by: Matthew Wilcox (Oracle) <willy@infradead.org>
    Reviewed-by: Zi Yan <ziy@nvidia.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Baolin Wang <baolin.wang@linux.alibaba.com>

--- mm/swap.c
+++ mm/swap.c
@@ -113,9 +113,15 @@ static void __folio_put_large(struct folio *folio)
 	 * (it's never listed to any LRU lists) and no memcg routines should
 	 * be called for hugetlb (it has a separate hugetlb_cgroup.)
 	 */
-	if (!folio_test_hugetlb(folio))
-		page_cache_release(folio);
-	destroy_large_folio(folio);
+	if (folio_test_hugetlb(folio)) {
+		free_huge_folio(folio);
+		return;
+	}
+
+	page_cache_release(folio);
+	folio_unqueue_deferred_split(folio);
+	mem_cgroup_uncharge(folio);
+	free_unref_page(&folio->page, folio_order(folio));
 }
 
 void __folio_put(struct folio *folio)
