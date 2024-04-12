1d2ae5eb5f2c3e320481e595e3e46973e8b0990e
Kefeng Wang <wangkefeng.wang@huawei.com>
2024-04-12 14:47:50 +0800

    mm: move mm counter updating out of set_pte_range()
    ANBZ: #9728
    
    commit 1f2d8b4421bd0da2c97fb8bad5cc85fc929fef64 upstream
    
    Patch series "mm: batch mm counter updating in filemap_map_pages()", v3.
    
    Let's batch mm counter updating to accelerate filemap_map_pages().
    
    This patch (of 2):
    
    In order to support batch mm counter updating in filemap_map_pages(), move
    mm counter updating out of set_pte_range(), the folios are file from
    filemap, and distinguish folios by vmf->flags and vma->vm_flags from
    another caller finish_fault().
    
    Link: https://lkml.kernel.org/r/20240412064751.119015-1-wangkefeng.wang@huawei.com
    Link: https://lkml.kernel.org/r/20240412064751.119015-2-wangkefeng.wang@huawei.com
    Signed-off-by: Kefeng Wang <wangkefeng.wang@huawei.com>
    Cc: Matthew Wilcox (Oracle) <willy@infradead.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/4420

--- mm/filemap.c
+++ mm/filemap.c
@@ -3588,6 +3588,8 @@ static vm_fault_t filemap_map_folio_range(struct vm_fault *vmf,
 skip:
 		if (count) {
 			set_pte_range(vmf, folio, page, count, addr);
+			add_mm_counter(vmf->vma->vm_mm, mm_counter_file(page),
+				       count);
 			folio_ref_add(folio, count);
 			if (in_range(vmf->address, addr, count * PAGE_SIZE))
 				ret = VM_FAULT_NOPAGE;
@@ -3602,6 +3604,7 @@ skip:
 
 	if (count) {
 		set_pte_range(vmf, folio, page, count, addr);
+		add_mm_counter(vmf->vma->vm_mm, mm_counter_file(page), count);
 		folio_ref_add(folio, count);
 		if (in_range(vmf->address, addr, count * PAGE_SIZE))
 			ret = VM_FAULT_NOPAGE;
@@ -3636,6 +3639,7 @@ static vm_fault_t filemap_map_order0_folio(struct vm_fault *vmf,
 		ret = VM_FAULT_NOPAGE;
 
 	set_pte_range(vmf, folio, page, 1, addr);
+	add_mm_counter(vmf->vma->vm_mm, mm_counter_file(page), 1);
 	folio_ref_inc(folio);
 
 	return ret;
