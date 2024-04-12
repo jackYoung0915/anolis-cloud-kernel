2bcf389a72c1c23bf88e111591f8bd131ebf1274
Ryan Roberts <ryan.roberts@arm.com>
2024-04-12 14:19:06 +0100

    arm64: mm: Don't remap pgtables per-cont(pte|pmd) block
    ANBZ: #23525
    
    commit 5c63db59c5f89925add57642be4f789d0d671ccd upstream.
    
    A large part of the kernel boot time is creating the kernel linear map
    page tables. When rodata=full, all memory is mapped by pte. And when
    there is lots of physical ram, there are lots of pte tables to populate.
    The primary cost associated with this is mapping and unmapping the pte
    table memory in the fixmap; at unmap time, the TLB entry must be
    invalidated and this is expensive.
    
    Previously, each pmd and pte table was fixmapped/fixunmapped for each
    cont(pte|pmd) block of mappings (16 entries with 4K granule). This means
    we ended up issuing 32 TLBIs per (pmd|pte) table during the population
    phase.
    
    Let's fix that, and fixmap/fixunmap each page once per population, for a
    saving of 31 TLBIs per (pmd|pte) table. This gives a significant boot
    speedup.
    
    Execution time of map_mem(), which creates the kernel linear map page
    tables, was measured on different machines with different RAM configs:
    
                   | Apple M2 VM | Ampere Altra| Ampere Altra| Ampere Altra
                   | VM, 16G     | VM, 64G     | VM, 256G    | Metal, 512G
    ---------------|-------------|-------------|-------------|-------------
                   |   ms    (%) |   ms    (%) |   ms    (%) |    ms    (%)
    ---------------|-------------|-------------|-------------|-------------
    before         |  168   (0%) | 2198   (0%) | 8644   (0%) | 17447   (0%)
    after          |   78 (-53%) |  435 (-80%) | 1723 (-80%) |  3779 (-78%)
    
    [Backport comment]
    We keep the pudp parameter in init_pmd to support compatibility with commit
    1ec014eab214 ("anolis: arm64: support splitting the block mapping kernel page table").
    
    Signed-off-by: Ryan Roberts <ryan.roberts@arm.com>
    Tested-by: Itaru Kitayama <itaru.kitayama@fujitsu.com>
    Tested-by: Eric Chanudet <echanude@redhat.com>
    Reviewed-by: Mark Rutland <mark.rutland@arm.com>
    Reviewed-by: Ard Biesheuvel <ardb@kernel.org>
    Link: https://lore.kernel.org/r/20240412131908.433043-2-ryan.roberts@arm.com
    Signed-off-by: Will Deacon <will@kernel.org>
    Signed-off-by: Weilin Tong <tongweilin@linux.alibaba.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5635

--- arch/arm64/mm/mmu.c
+++ arch/arm64/mm/mmu.c
@@ -219,12 +219,9 @@ static bool should_split_pud(p4d_t *p4dp, unsigned long addr, phys_addr_t phys)
 }
 #endif
 
-static void init_pte(pmd_t *pmdp, unsigned long addr, unsigned long end,
+static void init_pte(pte_t *ptep, unsigned long addr, unsigned long end,
 		     phys_addr_t phys, pgprot_t prot)
 {
-	pte_t *ptep;
-
-	ptep = pte_set_fixmap_offset(pmdp, addr);
 	do {
 		pte_t old_pte = __ptep_get(ptep);
 
@@ -239,8 +236,6 @@ static void init_pte(pmd_t *pmdp, unsigned long addr, unsigned long end,
 
 		phys += PAGE_SIZE;
 	} while (ptep++, addr += PAGE_SIZE, addr != end);
-
-	pte_clear_fixmap();
 }
 
 static void alloc_init_cont_pte(pmd_t *pmdp, unsigned long addr,
@@ -251,6 +246,7 @@ static void alloc_init_cont_pte(pmd_t *pmdp, unsigned long addr,
 {
 	unsigned long next;
 	pmd_t pmd = READ_ONCE(*pmdp);
+	pte_t *ptep;
 
 	BUG_ON(pmd_sect(pmd));
 	if (pmd_none(pmd)) {
@@ -266,6 +262,7 @@ static void alloc_init_cont_pte(pmd_t *pmdp, unsigned long addr,
 	}
 	BUG_ON(pmd_bad(pmd));
 
+	ptep = pte_set_fixmap_offset(pmdp, addr);
 	do {
 		pgprot_t __prot = prot;
 
@@ -277,20 +274,21 @@ static void alloc_init_cont_pte(pmd_t *pmdp, unsigned long addr,
 		    !should_clear_cont_pte(pmdp, addr, phys))
 			__prot = __pgprot(pgprot_val(prot) | PTE_CONT);
 
-		init_pte(pmdp, addr, next, phys, __prot);
+		init_pte(ptep, addr, next, phys, __prot);
 
+		ptep += pte_index(next) - pte_index(addr);
 		phys += next - addr;
 	} while (addr = next, addr != end);
+
+	pte_clear_fixmap();
 }
 
-static void init_pmd(pud_t *pudp, unsigned long addr, unsigned long end,
+static void init_pmd(pud_t *pudp, pmd_t *pmdp, unsigned long addr, unsigned long end,
 		     phys_addr_t phys, pgprot_t prot,
 		     phys_addr_t (*pgtable_alloc)(int), int flags)
 {
 	unsigned long next;
-	pmd_t *pmdp;
 
-	pmdp = pmd_set_fixmap_offset(pudp, addr);
 	/*
 	 * the physical address of PMDs with contiguous flag might locate in the
 	 * physical range they point to. Thus clear the CONT flag earlier to
@@ -325,8 +323,6 @@ static void init_pmd(pud_t *pudp, unsigned long addr, unsigned long end,
 		}
 		phys += next - addr;
 	} while (pmdp++, addr = next, addr != end);
-
-	pmd_clear_fixmap();
 }
 
 static void alloc_init_cont_pmd(pud_t *pudp, unsigned long addr,
@@ -336,6 +332,7 @@ static void alloc_init_cont_pmd(pud_t *pudp, unsigned long addr,
 {
 	unsigned long next;
 	pud_t pud = READ_ONCE(*pudp);
+	pmd_t *pmdp;
 
 	/*
 	 * Check for initial section mappings in the pgd/pud.
@@ -354,6 +351,7 @@ static void alloc_init_cont_pmd(pud_t *pudp, unsigned long addr,
 	}
 	BUG_ON(pud_bad(pud));
 
+	pmdp = pmd_set_fixmap_offset(pudp, addr);
 	do {
 		pgprot_t __prot = prot;
 
@@ -364,10 +362,13 @@ static void alloc_init_cont_pmd(pud_t *pudp, unsigned long addr,
 		    (flags & NO_CONT_MAPPINGS) == 0)
 			__prot = __pgprot(pgprot_val(prot) | PTE_CONT);
 
-		init_pmd(pudp, addr, next, phys, __prot, pgtable_alloc, flags);
+		init_pmd(pudp, pmdp, addr, next, phys, __prot, pgtable_alloc, flags);
 
+		pmdp += pmd_index(next) - pmd_index(addr);
 		phys += next - addr;
 	} while (addr = next, addr != end);
+
+	pmd_clear_fixmap();
 }
 
 static void alloc_init_pud(pgd_t *pgdp, unsigned long addr, unsigned long end,
