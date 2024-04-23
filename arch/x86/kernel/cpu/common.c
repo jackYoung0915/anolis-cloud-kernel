36f5643bca456c8677ea411eb87389ad7aa35a2b
Jacob Pan <jacob.jun.pan@linux.intel.com>
2024-04-23 10:41:08 -0700

    x86/irq: Set up per host CPU posted interrupt descriptors
    ANBZ: #27431
    
    commit 43650dcf6d6322ec2d0938bb51f755810ffa783a upstream.
    
    To support posted MSIs, create a posted interrupt descriptor (PID) for each
    host CPU. Later on, when setting up interrupt affinity, the IOMMU's
    interrupt remapping table entry (IRTE) will point to the physical address
    of the matching CPU's PID.
    
    Each PID is initialized with the owner CPU's physical APICID as the
    destination.
    
    Originally-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Jacob Pan <jacob.jun.pan@linux.intel.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Link: https://lore.kernel.org/r/20240423174114.526704-7-jacob.jun.pan@linux.intel.com
    [fix up for applying patch - Yi Liu]
    Signed-off-by: Yi Liu <yi.l.liu@intel.com>
    Tested-by: Kaihui Kang <kaihui.kang@intel.com>

--- arch/x86/kernel/cpu/common.c
+++ arch/x86/kernel/cpu/common.c
@@ -65,6 +65,7 @@
 #include <asm/set_memory.h>
 #include <asm/traps.h>
 #include <asm/sev.h>
+#include <asm/posted_intr.h>
 
 #include "cpu.h"
 
@@ -2359,6 +2360,8 @@ void cpu_init(void)
 		barrier();
 
 		x2apic_setup();
+
+		intel_posted_msi_init();
 	}
 
 	mmgrab(&init_mm);
