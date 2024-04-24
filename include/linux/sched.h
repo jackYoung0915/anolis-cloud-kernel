e7dd6a54bf99ff9915d7ec7b5c2dee4c35aabbd1
Cruz Zhao <CruzZhao@linux.alibaba.com>
2024-04-24 17:23:52 +0800

    anolis: sched: introduce cpu.soft_cpus interface
    ANBZ: #8765
    
    Introduce cpu.soft_cpus interface to get and set soft cpus of task group,
    if cpu.soft_cpus is set and group_balancer is enabled, the tasks will
    run on the soft_cpus since next schedule.
    
    Signed-off-by: Cruz Zhao <CruzZhao@linux.alibaba.com>
    Signed-off-by: Vincent Gao <vincentbo@linux.alibaba.com>
    Reviewed-by: Tianchen Ding <dtcccc@linux.alibaba.com>
    Reviewed-by: Yi Tao <escape@linux.aibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/3215
    Reviewed-by: Cruz Zhao <cruzzhao@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5412

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1632,6 +1632,9 @@ struct task_struct {
 	unsigned long wait_moment;
 	bool				proxy_exec;
 
+#ifdef CONFIG_GROUP_BALANCER
+	struct cpumask			cpus_allowed_alt;
+#endif
 	CK_KABI_RESERVE(1)
 	CK_KABI_RESERVE(2)
 	CK_KABI_RESERVE(3)
