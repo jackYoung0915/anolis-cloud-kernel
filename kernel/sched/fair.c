e3815a87ba2c5f1a588fdf5b6734ac0c65a9ef2c
Peter Zijlstra <peterz@infradead.org>
2024-04-26 13:00:50 +0200

    sched/eevdf: Fixup PELT vs DELAYED_DEQUEUE
    ANBZ: #18707
    
    commit fc1892becd5672f52329a75c73117b60ac7841b7 upstream.
    
    Note that tasks that are kept on the runqueue to burn off negative
    lag, are not in fact runnable anymore, they'll get dequeued the moment
    they get picked.
    
    As such, don't count this time towards runnable.
    
    Thanks to Valentin for spotting I had this backwards initially.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Valentin Schneider <vschneid@redhat.com>
    Tested-by: Valentin Schneider <vschneid@redhat.com>
    Link: https://lkml.kernel.org/r/20240727105030.514088302@infradead.org
    Signed-off-by: Tianchen Ding <dtcccc@linux.alibaba.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5430,6 +5430,7 @@ dequeue_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int flags)
 		    !entity_eligible(cfs_rq, se)) {
 			if (cfs_rq->next == se)
 				cfs_rq->next = NULL;
+			update_load_avg(cfs_rq, se, 0);
 			se->sched_delayed = 1;
 			return false;
 		}
@@ -6879,6 +6880,7 @@ requeue_delayed_entity(struct sched_entity *se)
 		}
 	}
 
+	update_load_avg(cfs_rq, se, 0);
 	se->sched_delayed = 0;
 }
 
