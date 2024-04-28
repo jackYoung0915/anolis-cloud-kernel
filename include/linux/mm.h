49ec9df54d4c2baab09f622d663bbd9ea1386205
Dan Carpenter <dan.carpenter@linaro.org>
2024-04-28 17:26:44 +0300

    mm/slab: make __free(kfree) accept error pointers
    ANBZ: #20392
    
    commit cd7eb8f83fcf258f71e293f7fc52a70be8ed0128 upstream.
    
    Currently, if an automatically freed allocation is an error pointer that
    will lead to a crash.  An example of this is in wm831x_gpio_dbg_show().
    
       171	char *label __free(kfree) = gpiochip_dup_line_label(chip, i);
       172	if (IS_ERR(label)) {
       173		dev_err(wm831x->dev, "Failed to duplicate label\n");
       174		continue;
       175  }
    
    The auto clean up function should check for error pointers as well,
    otherwise we're going to keep hitting issues like this.
    
    Fixes: 54da6a092431 ("locking: Introduce __cleanup() based infrastructure")
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Dan Carpenter <dan.carpenter@linaro.org>
    Acked-by: David Rientjes <rientjes@google.com>
    Signed-off-by: Vlastimil Babka <vbabka@suse.cz>
    Signed-off-by: Zelin Deng <zelin.deng@linux.alibaba.com>
    Acked-by: Xuchun Shang <xuchun.shang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5060

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -842,7 +842,7 @@ static inline void *kvcalloc(size_t n, size_t size, gfp_t flags)
 extern void *kvrealloc(const void *p, size_t oldsize, size_t newsize,
 		gfp_t flags);
 extern void kvfree(const void *addr);
-DEFINE_FREE(kvfree, void *, if (_T) kvfree(_T))
+DEFINE_FREE(kvfree, void *, if (!IS_ERR_OR_NULL(_T)) kvfree(_T))
 
 extern void kvfree_sensitive(const void *addr, size_t len);
 
