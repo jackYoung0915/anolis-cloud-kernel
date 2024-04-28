ddeef347fd295f9a953afa126feb29df68a2b2c6
Jingxian He <hejingxian@huawei.com>
2024-04-28 11:01:02 +0800

    swiotlb: Add restricted DMA alloc/free support
    ANBZ: #11194
    
    commit 13c1076273aacace25ad0f21e29793f577e106a4 openeuler
    
    mainline inclusion
    from mainline-v6.9-rc1
    commit f4111e39a52aa5d5136d890bbd1aa87c1c8fe3bc
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I9CC0X
    
    Reference: https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/commit/?id=f4111e39a52aa5d5136d890bbd1aa87c1c8fe3bc
    
    --------------------------------
    
    [ Upstream commit f4111e39a52aa5d5136d890bbd1aa87c1c8fe3bc ]
    
    Add the functions, swiotlb_{alloc,free} and is_swiotlb_for_alloc to
    support the memory allocation from restricted DMA pool.
    
    The restricted DMA pool is preferred if available.
    
    Note that since coherent allocation needs remapping, one must set up
    another device coherent pool by shared-dma-pool and use
    dma_alloc_from_dev_coherent instead for atomic coherent allocation.
    
    Signed-off-by: Claire Chang <tientzu@chromium.org>
    Reviewed-by: Christoph Hellwig <hch@lst.de>
    Tested-by: Stefano Stabellini <sstabellini@kernel.org>
    Tested-by: Will Deacon <will@kernel.org>
    Acked-by: Stefano Stabellini <sstabellini@kernel.org>
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    
     Conflicts:
    	include/linux/swiotlb.h
    	kernel/dma/swiotlb.c
    [hejingxian: delete modify for struct io_tlb_mem which not exist in OLK5.10]
    [hejingxian: modify per dev dma_io_tlb_mem start to io_tlb_start]
    [hejingxian: add define for swiotlb_release_slots and  swiotlb_find_slots]
    [hejingxian: move is_swiotlb_for_alloc into direct.c and return false]
    Signed-off-by: Jingxian He <hejingxian@huawei.com>
    Signed-off-by: Min Li <gumi@linux.alibaba.com>

--- include/linux/swiotlb.h
+++ include/linux/swiotlb.h
@@ -155,4 +155,21 @@ static inline void swiotlb_adjust_size(unsigned long size)
 extern void swiotlb_print_info(void);
 extern void swiotlb_set_max_segment(unsigned int);
 
+#ifdef CONFIG_DMA_RESTRICTED_POOL
+
+struct page *swiotlb_alloc(struct device *dev, size_t size);
+bool swiotlb_free(struct device *dev, struct page *page, size_t size);
+
+#else
+static inline struct page *swiotlb_alloc(struct device *dev, size_t size)
+{
+	return NULL;
+}
+static inline bool swiotlb_free(struct device *dev, struct page *page,
+				size_t size)
+{
+	return false;
+}
+#endif /* CONFIG_DMA_RESTRICTED_POOL */
+
 #endif /* __LINUX_SWIOTLB_H */
