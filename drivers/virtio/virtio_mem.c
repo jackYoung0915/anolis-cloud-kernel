c3780a4ececbdba2c4376aa66fcd0045bd6ebbcd
zhongjiang-ali <zhongjiang-ali@linux.alibaba.com>
2024-05-09 16:37:51 +0800

    anolis: virtio-mem: restrict offline memmap on movable in x86_64
    ANBZ: #12923
    
    Currently, remapping vmemmap depends on hugepage vmemmap optimization.
    because it reuse the some logical, but it disable the feautre in
    arm64, hence we also need to restrict it in x86_64.
    
    Meanwhile, the patch also restrict the supported memmap on movable
    will not impact the original local.
    
    Signed-off-by: zhongjiang-ali <zhongjiang-ali@linux.alibaba.com>
    Signed-off-by: hr567 <hr567@linux.alibaba.com>

--- drivers/virtio/virtio_mem.c
+++ drivers/virtio/virtio_mem.c
@@ -613,7 +613,7 @@ static int virtio_mem_add_memory(struct virtio_mem *vm, uint64_t addr,
 				 uint64_t size)
 {
 	int rc;
-	mhp_t mhp_flags = MHP_MERGE_RESOURCE | MHP_NID_IS_MGID;
+	mhp_t extra_flags = 0;
 
 	/*
 	 * When force-unloading the driver and we still have memory added to
@@ -630,10 +630,13 @@ static int virtio_mem_add_memory(struct virtio_mem *vm, uint64_t addr,
 		addr + size - 1);
 	/* Memory might get onlined immediately. */
 	atomic64_add(size, &vm->offline_size);
+#ifdef CONFIG_X86_64
 	/* only support memmap_on_memory on sbm scenario */
 	if (vm->in_sbm)
-		mhp_flags |= MHP_MEMMAP_ON_MEMORY;
-	rc = add_memory_driver_managed(vm->mgid, addr, size, vm->resource_name, mhp_flags);
+		extra_flags |= MHP_MEMMAP_ON_MEMORY;
+#endif
+	rc = add_memory_driver_managed(vm->mgid, addr, size, vm->resource_name,
+			MHP_MERGE_RESOURCE | MHP_NID_IS_MGID, extra_flags);
 	if (rc) {
 		atomic64_sub(size, &vm->offline_size);
 		dev_warn(&vm->vdev->dev, "adding memory failed: %d\n", rc);
@@ -1940,7 +1943,7 @@ static int virtio_mem_sbm_unplug_sb_online(struct virtio_mem *vm,
 		return rc;
 
 	if (map) {
-		/* Make sure that memblock has rebuild the page mapping */
+		/* Make sure that memblock will record the page mapping */
 		if (vmemmap_remap_alloc(block_start, block_end,
 					VIRTIO_MEMMAP_COPY, GFP_KERNEL, &vmemmap_pages))
 			return -ENOMEM;
