d371810acdb792e9fd371baaf62a3acdfc7d9f96
Aurelien Aptel <aaptel@nvidia.com>
2024-05-17 16:10:28 +0000

    fuse: use correct name fuse_conn_list in docstring
    ANBZ: #11284
    
    commit 506b21c945b9716a1e092189c260d9400c52fa14 upstream.
    
    fuse_mount_list doesn't exist, use fuse_conn_list.
    
    Signed-off-by: Aurelien Aptel <aaptel@nvidia.com>
    Reviewed-by: Bernd Schubert <bschubert@ddn.com>
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>
    Signed-off-by: Jingbo Xu <jefflexu@linux.alibaba.com>
    Reviewed-by: Joseph Qi <joseph.qi@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/3953

--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -833,7 +833,7 @@ struct fuse_conn {
 	/** Negotiated minor version */
 	unsigned minor;
 
-	/** Entry on the fuse_mount_list */
+	/** Entry on the fuse_conn_list */
 	struct list_head entry;
 
 	/** Device ID from the root super block */
