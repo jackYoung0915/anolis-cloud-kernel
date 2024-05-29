6c81bd2d61efdf92e59eb0725159d9164d4ad4fe
Ju Fu <fuju1@huawei.com>
2024-05-29 13:23:15 +0800

    cvm: enable secure memory alloc on multiple numa nodes
    ANBZ: #11194
    
    commit b82426dd122648291aca77c9b68d45eb7a3b467b openeuler
    
    virtcca inclusion
    category: feature
    bugzilla: https://gitee.com/openeuler/kernel/issues/I9CC0X
    
    --------------------------------
    
    enable cvm secure memory alloc on multiple numa nodes
    
    Signed-off-by: Ju Fu <fuju1@huawei.com>
    Signed-off-by: Min Li <gumi@linux.alibaba.com>

--- include/uapi/linux/kvm.h
+++ include/uapi/linux/kvm.h
@@ -1383,12 +1383,13 @@ struct id_registers {
 #define KVM_CAP_ARM_TMM 300  /* FIXME: Large number to prevent conflicts */
 #define MAX_NUMA_NODE 8
 #define MAX_CPU_BIT_MAP 4
+#define MAX_NUMA_BIT_MAP 2
 
 struct kvm_numa_node {
 	__u64 numa_id;
 	__u64 ipa_start;
 	__u64 ipa_size;
-	__u64 host_numa_node;
+	__u64 host_numa_nodes[MAX_NUMA_BIT_MAP];
 	__u64 cpu_id[MAX_CPU_BIT_MAP];
 };
 
