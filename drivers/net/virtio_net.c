7f0250f1411a7cc6b01a33ec9926ad9578866fb9
Heng Qi <hengqi@linux.alibaba.com>
2024-05-30 11:41:43 +0800

    virtio_net: fix missing lock protection on control_buf access
    ANBZ: #20951
    
    commit 30636258a7c9174be44ddb2318bae4f66d4beab0 upstream.
    
    Refactored the handling of control_buf to be within the cvq_lock
    critical section, mitigating race conditions between reading device
    responses and new command submissions.
    Fixes: 6f45ab3e0409 ("virtio_net: Add a lock for the command VQ.")
    Signed-off-by: Heng Qi <hengqi@linux.alibaba.com>
    Reviewed-by: Hariprasad Kelam <hkelam@marvell.com>
    Acked-by: Jason Wang <jasowang@redhat.com>
    Reviewed-by: Xuan Zhuo <xuanzhuo@linux.alibaba.com>
    Link: https://lore.kernel.org/r/20240530034143.19579-1-hengqi@linux.alibaba.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    Signed-off-by: Xuan Zhuo <xuanzhuo@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5280

--- drivers/net/virtio_net.c
+++ drivers/net/virtio_net.c
@@ -2746,6 +2746,7 @@ static bool virtnet_send_command_reply(struct virtnet_info *vi, u8 class, u8 cmd
 {
 	struct scatterlist *sgs[5], hdr, stat;
 	u32 out_num = 0, tmp, in_num = 0;
+	bool ok;
 	int ret;
 
 	/* Caller should know better */
@@ -2791,8 +2792,9 @@ static bool virtnet_send_command_reply(struct virtnet_info *vi, u8 class, u8 cmd
 	}
 
 unlock:
+	ok = vi->ctrl->status == VIRTIO_NET_OK;
 	mutex_unlock(&vi->cvq_lock);
-	return vi->ctrl->status == VIRTIO_NET_OK;
+	return ok;
 }
 
 static bool virtnet_send_command(struct virtnet_info *vi, u8 class, u8 cmd,
