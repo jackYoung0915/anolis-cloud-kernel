5b09a6dd0adc7c627d923019299ef94a3df9eedd
Cruz Zhao <CruzZhao@linux.alibaba.com>
2024-05-31 17:52:03 +0800

    anolis: sched: fix group balancer interface write error
    ANBZ: #8765
    
    If we echo $cpu_list > $cg/cpu.soft_cpus, we will be stuck in an
    infinite loop, because cpu_soft_cpus_write() returns 0 instead of
    nbytes. To fix this problem, we let cpu_soft_cpus_write() return nbytes
    if the cpu list is valid.
    
    If we echo 1 > $cg/cpu.group_balancer, there will be write error:
    Argument list too long. As we will check the value in
    cpu_group_balancer_write_u64(), we remove max_write_len of
    cpu.group_balancer.
    
    Fixes: 148ae6a68959 ("anolis: sched: introduce cpu.soft_cpus interface")
    Fixes: 3fc99497fd87 ("anolis: sched: introduce per cgroup group_balancer switch")
    Signed-off-by: Cruz Zhao <CruzZhao@linux.alibaba.com>
    Signed-off-by: Vincent Gao <vincentbo@linux.alibaba.com>
    Reviewed-by: Tianchen Ding <dtcccc@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/3304
    Reviewed-by: Cruz Zhao <cruzzhao@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5412

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -10102,7 +10102,7 @@ static ssize_t cpu_soft_cpus_write(struct kernfs_open_file *of,
 			tg->soft_cpus_version = 0;
 	}
 
-	return 0;
+	return nbytes;
 }
 
 static u64 cpu_group_balancer_read_u64(struct cgroup_subsys_state *css,
@@ -10298,7 +10298,6 @@ static struct cftype cpu_legacy_files[] = {
 		.flags = CFTYPE_NOT_ON_ROOT,
 		.read_u64 = cpu_group_balancer_read_u64,
 		.write_u64 = cpu_group_balancer_write_u64,
-		.max_write_len = 1,
 	},
 #endif
 	{ }	/* Terminate */
@@ -10878,7 +10877,6 @@ static struct cftype cpu_files[] = {
 		.flags = CFTYPE_NOT_ON_ROOT,
 		.read_u64 = cpu_group_balancer_read_u64,
 		.write_u64 = cpu_group_balancer_write_u64,
-		.max_write_len = 1,
 	},
 #endif
 	{ }	/* terminate */
