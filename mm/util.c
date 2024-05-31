b92c1b34c3b6a6976bb7b507b65d2020b13e49c7
Suren Baghdasaryan <surenb@google.com>
2024-05-31 13:53:50 -0700

    mm: fix xyz_noprof functions calling profiled functions
    ANBZ: #32420
    
    commit 9415983599413f847ec9f081e9f9e5ed6cdeb342 upstream.
    
    Grepping /proc/allocinfo for "noprof" reveals several xyz_noprof
    functions, which means internally they are calling profiled functions.
    This should never happen as such calls move allocation charge from a
    higher level location where it should be accounted for into these lower
    level helpers.  Fix this by replacing profiled function calls with noprof
    ones.
    
    Link: https://lkml.kernel.org/r/20240531205350.3973009-1-surenb@google.com
    Fixes: b951aaff5035 ("mm: enable page allocation tagging")
    Fixes: e26d8769da6d ("mempool: hook up to memory allocation profiling")
    Fixes: 88ae5fb755b0 ("mm: vmalloc: enable memory allocation profiling")
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Cc: Kent Overstreet <kent.overstreet@linux.dev>
    Reviewed-by: Kees Cook <kees@kernel.org>
    Acked-by: Vlastimil Babka <vbabka@suse.cz>
    Cc: Pasha Tatashin <pasha.tatashin@soleen.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Yuanhe Shu <xiangzao@linux.alibaba.com>
    Acked-by: David Rientjes <rientjes@google.com>
    Acked-by: Harry Yoo <harry.yoo@oracle.com>
    Acked-by: Kent Overstreet <kent.overstreet@linux.dev>
    Acked-by: Muchun Song <muchun.song@linux.dev>
    Reviewed-by: Hao Li <hao.li@linux.dev>
    Reviewed-by: Miaohe Lin <linmiaohe@huawei.com>
    Acked-by: David Hildenbrand <david@redhat.com>
    Reviewed-by: Pasha Tatashin <pasha.tatashin@soleen.com>
    Acked-by: Shakeel Butt <shakeel.butt@linux.dev>
    Acked-by: SeongJae Park <sj@kernel.org>
    Acked-by: Yu Zhao <yuzhao@google.com>
    Reviewed-by: Anshuman Khandual <anshuman.khandual@arm.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6766

--- mm/util.c
+++ mm/util.c
@@ -713,7 +713,7 @@ void *kvrealloc_noprof(const void *p, size_t oldsize, size_t newsize, gfp_t flag
 
 	if (oldsize >= newsize)
 		return (void *)p;
-	newp = kvmalloc(newsize, flags);
+	newp = kvmalloc_noprof(newsize, flags);
 	if (!newp)
 		return NULL;
 	memcpy(newp, p, oldsize);
@@ -734,7 +734,7 @@ void *__vmalloc_array_noprof(size_t n, size_t size, gfp_t flags)
 
 	if (unlikely(check_mul_overflow(n, size, &bytes)))
 		return NULL;
-	return __vmalloc(bytes, flags);
+	return __vmalloc_noprof(bytes, flags);
 }
 EXPORT_SYMBOL(__vmalloc_array_noprof);
 
@@ -745,7 +745,7 @@ EXPORT_SYMBOL(__vmalloc_array_noprof);
  */
 void *vmalloc_array_noprof(size_t n, size_t size)
 {
-	return __vmalloc_array(n, size, GFP_KERNEL);
+	return __vmalloc_array_noprof(n, size, GFP_KERNEL);
 }
 EXPORT_SYMBOL(vmalloc_array_noprof);
 
@@ -757,7 +757,7 @@ EXPORT_SYMBOL(vmalloc_array_noprof);
  */
 void *__vcalloc_noprof(size_t n, size_t size, gfp_t flags)
 {
-	return __vmalloc_array(n, size, flags | __GFP_ZERO);
+	return __vmalloc_array_noprof(n, size, flags | __GFP_ZERO);
 }
 EXPORT_SYMBOL(__vcalloc_noprof);
 
@@ -768,7 +768,7 @@ EXPORT_SYMBOL(__vcalloc_noprof);
  */
 void *vcalloc_noprof(size_t n, size_t size)
 {
-	return __vmalloc_array(n, size, GFP_KERNEL | __GFP_ZERO);
+	return __vmalloc_array_noprof(n, size, GFP_KERNEL | __GFP_ZERO);
 }
 EXPORT_SYMBOL(vcalloc_noprof);
 
