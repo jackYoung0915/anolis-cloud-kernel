9202c1107ee51d4d10a574226d93d10935bf7333
Zhang Zekun <zhangzekun11@huawei.com>
2024-06-04 10:08:34 +0800

    iommu/arm-smmu-v3: Reducing the CMD_SYNC times
    ANBZ: #11194
    
    commit 2da3f6871b6eaa467cb53f7a351b375e5d7ccd5c openeuler
    
    hulk inclusion
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/I9G9TI
    
    ----------------------------------------
    
    Benifiting from the hardware same address blocking mechanism.
    We only need to send the CMD_SYNC when creating pte entry other
    than lvl 3, this can reduce the amounts of CMD_SYNC times sent
    to avoiding the errata. Detecting the hardware ability by read
    the SMMU_USER_CONFIG1.
    
    Fixes: 46f0c5798ba5 ("iommu/arm-smmu-v3: Enable iotlb_sync_map according to SMMU_IIDR")
    Signed-off-by: Zhang Zekun <zhangzekun11@huawei.com>
    Signed-off-by: Slim6882 <yangjunshuo@huawei.com>
    Signed-off-by: Min Li <gumi@linux.alibaba.com>

--- drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.h
+++ drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.h
@@ -163,6 +163,8 @@
 #define ARM_SMMU_PRIQ_IRQ_CFG1		0xd8
 #define ARM_SMMU_PRIQ_IRQ_CFG2		0xdc
 
+#define ARM_SMMU_USER_CFG1		0xe04
+
 #define ARM_SMMU_REG_SZ			0xe00
 
 /* Common MSI config fields */
@@ -628,6 +630,7 @@ struct arm_smmu_device {
 #define ARM_SMMU_OPT_PAGE0_REGS_ONLY	(1 << 1)
 #define ARM_SMMU_OPT_MSIPOLL		(1 << 2)
 #define ARM_SMMU_OPT_SYNC_MAP		(1 << 3)
+#define ARM_SMMU_OPT_SYNC_BATCH		(1 << 4)
 	u32				options;
 
 	struct arm_smmu_cmdq		cmdq;
