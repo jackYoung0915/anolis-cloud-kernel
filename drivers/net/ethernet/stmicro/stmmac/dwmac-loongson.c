e2859f5d5c070e8aa09c98c0bd679515b874e963
Juxin Gao <gaojuxin@loongson.cn>
2024-06-06 11:46:03 +0800

    anolis: net: stmmac: Fix build error when config CONFIG_DWMAC_LOONGSON=m
    ANBZ: #9291
    
    Signed-off-by: Juxin Gao <gaojuxin@loongson.cn>
    Reviewed-by: Guixin Liu <kanie@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/3325

--- drivers/net/ethernet/stmicro/stmmac/dwmac-loongson.c
+++ drivers/net/ethernet/stmicro/stmmac/dwmac-loongson.c
@@ -400,7 +400,6 @@ static struct mac_device_info *loongson_dwmac_setup(void *apriv)
 		mac->dma = dma;
 	}
 
-	mac->mac = &dwmac1000_ops;
 	priv->dev->priv_flags |= IFF_UNICAST_FLT;
 
 	/* Pre-initialize the respective "mac" fields as it's done in
