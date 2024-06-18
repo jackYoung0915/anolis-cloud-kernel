c55296093bbcea15a187dea47a9e444c040381a9
Tejun Heo <tj@kernel.org>
2024-06-18 10:09:17 -1000

    sched: Factor out update_other_load_avgs() from __update_blocked_others()
    ANBZ: #13548
    
    commit 96fd6c65efc652e9054163e6d3cf254b9e5b93d2 upstream.
    
    RT, DL, thermal and irq load and utilization metrics need to be decayed and
    updated periodically and before consumption to keep the numbers reasonable.
    This is currently done from __update_blocked_others() as a part of the fair
    class load balance path. Let's factor it out to update_other_load_avgs().
    Pure refactor. No functional changes.
    
    This will be used by the new BPF extensible scheduling class to ensure that
    the above metrics are properly maintained.
    
    v2: Refreshed on top of tip:sched/core.
    
    Signed-off-by: Tejun Heo <tj@kernel.org>
    Reviewed-by: David Vernet <dvernet@meta.com>
    Signed-off-by: Xuchun Shang <xuchun.shang@linux.alibaba.com>
    Reviewed-by: Cruz Zhao <cruzzhao@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/4865
    [rebase 6.6.102]

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -3323,6 +3323,8 @@ static inline void cpufreq_update_util(struct rq *rq, unsigned int flags) {}
 #endif
 
 #ifdef CONFIG_SMP
+bool update_other_load_avgs(struct rq *rq);
+
 unsigned long effective_cpu_util(int cpu, unsigned long util_cfs,
 				 unsigned long *min,
 				 unsigned long *max);
@@ -3365,6 +3367,9 @@ static inline unsigned long cpu_util_rt(struct rq *rq)
 {
 	return READ_ONCE(rq->avg_rt.util_avg);
 }
+
+#else /* !CONFIG_SMP */
+static inline bool update_other_load_avgs(struct rq *rq) { return false; }
 #endif
 
 #ifdef CONFIG_UCLAMP_TASK
