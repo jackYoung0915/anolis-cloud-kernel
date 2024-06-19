6d16777522c8d68ada0a0269716906d5c26681c9
Clément Léger <cleger@rivosinc.com>
2024-06-19 13:35:14 +0200

    RISC-V: KVM: Allow Zimop extension for Guest/VM
    ANBZ: #22929
    
    commit fb2a3d63efefe6bd3718201daba479f4339bb4bf upstream.
    
    Extend the KVM ISA extension ONE_REG interface to allow KVM user space
    to detect and enable Zimop extension for Guest/VM.
    
    Signed-off-by: Clément Léger <cleger@rivosinc.com>
    Reviewed-by: Anup Patel <anup@brainfault.org>
    Acked-by: Anup Patel <anup@brainfault.org>
    Link: https://lore.kernel.org/r/20240619113529.676940-5-cleger@rivosinc.com
    Signed-off-by: Palmer Dabbelt <palmer@rivosinc.com>
    Reviewed-by: Fangyu <Yu fangyu.yu@linux.alibaba.com>
    Reviewed-by: Ruidong Tian <tianruidong@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5550

--- arch/riscv/kvm/vcpu_onereg.c
+++ arch/riscv/kvm/vcpu_onereg.c
@@ -51,6 +51,7 @@ static const unsigned long kvm_isa_ext_arr[] = {
 	KVM_ISA_EXT_ARR(ZIFENCEI),
 	KVM_ISA_EXT_ARR(ZIHINTPAUSE),
 	KVM_ISA_EXT_ARR(ZIHPM),
+	KVM_ISA_EXT_ARR(ZIMOP),
 };
 
 static unsigned long kvm_riscv_vcpu_base2isa_ext(unsigned long base_ext)
@@ -99,6 +100,7 @@ static bool kvm_riscv_vcpu_isa_disable_allowed(unsigned long ext)
 	case KVM_RISCV_ISA_EXT_ZIFENCEI:
 	case KVM_RISCV_ISA_EXT_ZIHINTPAUSE:
 	case KVM_RISCV_ISA_EXT_ZIHPM:
+	case KVM_RISCV_ISA_EXT_ZIMOP:
 		return false;
 	/* Extensions which can be disabled using Smstateen */
 	case KVM_RISCV_ISA_EXT_SSAIA:
