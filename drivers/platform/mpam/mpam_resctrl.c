01870ccd89469a944ace200e2c3a47bc85b3ba22
Tony Luck <tony.luck@intel.com>
2024-06-28 14:56:01 -0700

    x86/resctrl: Prepare for new domain scope
    ANBZ: #12351
    
    commit f436cb6913a57bf3e1e66d18bc663e6c20751929 upstream.
    
    Resctrl resources operate on subsets of CPUs in the system with the
    defining attribute of each subset being an instance of a particular
    level of cache. E.g. all CPUs sharing an L3 cache would be part of the
    same domain.
    
    In preparation for features that are scoped at the NUMA node level,
    change the code from explicit references to "cache_level" to a more
    generic scope. At this point the only options for this scope are groups
    of CPUs that share an L2 cache or L3 cache.
    
    Clean up the error handling when looking up domains. Report invalid ids
    before calling rdt_find_domain() in preparation for better messages when
    scope can be other than cache scope. This means that rdt_find_domain()
    will never return an error. So remove checks for error from the call sites.
    
    Signed-off-by: Tony Luck <tony.luck@intel.com>
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    Reviewed-by: Reinette Chatre <reinette.chatre@intel.com>
    Tested-by: Babu Moger <babu.moger@amd.com>
    Link: https://lore.kernel.org/r/20240628215619.76401-2-tony.luck@intel.com
    Signed-off-by: Kun(llfl) <llfl@linux.alibaba.com>

--- drivers/platform/mpam/mpam_resctrl.c
+++ drivers/platform/mpam/mpam_resctrl.c
@@ -671,7 +671,7 @@ static int mpam_resctrl_resource_init(struct mpam_resctrl_res *res)
 		bool has_csu = cache_has_usable_csu(class);
 		bool has_mbwu = class_has_usable_mbwu(class);
 
-		r->cache_level = class->level;
+		r->scope = class->level;
 
 		/* TODO: Scaling is not yet supported */
 		r->cache.cbm_len = class->props.cpbm_wd;
