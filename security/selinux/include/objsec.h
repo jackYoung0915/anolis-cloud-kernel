6a1ab1163cbc581ec03701b938ae93445ee5ebff
Casey Schaufler <casey@schaufler-ca.com>
2024-07-10 14:32:28 -0700

    lsm: infrastructure management of the dev_tun blob
    ANBZ: #33439
    
    commit a39c0f77dbbe083f3eec6c3b32d90f168f7575eb upstream.
    
    Move management of the dev_tun security blob out of the individual
    security modules and into the LSM infrastructure.  The security modules
    tell the infrastructure how much space they require at initialization.
    There are no longer any modules that require the dev_tun_free hook.
    The hook definition has been removed.
    
    Signed-off-by: Casey Schaufler <casey@schaufler-ca.com>
    Reviewed-by: John Johansen <john.johansen@canonical.com>
    [PM: subject tweak, selinux style fixes]
    Signed-off-by: Paul Moore <paul@paul-moore.com>
    [Zongyao Chen: resolve conflict in security_tun_dev_alloc_security(),
     keep 6.6 call_int_hook() default-return argument, and update Lua-LSM
     for the new tun_dev_alloc_security() prototype and removed free hook.]
    Signed-off-by: Zongyao Chen <ZongYao.Chen@linux.alibaba.com>
    Reviewed-by: Tianjia Zhang <tianjia.zhang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6842

--- security/selinux/include/objsec.h
+++ security/selinux/include/objsec.h
@@ -206,4 +206,9 @@ static inline struct sk_security_struct *selinux_sock(const struct sock *sock)
 	return sock->sk_security + selinux_blob_sizes.lbs_sock;
 }
 
+static inline struct tun_security_struct *selinux_tun_dev(void *security)
+{
+	return security + selinux_blob_sizes.lbs_tun_dev;
+}
+
 #endif /* _SELINUX_OBJSEC_H_ */
