63b1984dbb9ca5fffa4731ec055d391eecde19d3
lvjianmin <lvjianmin@loongson.cn>
2024-07-10 12:03:13 +0800

    anolis: drm/amdgpu: make duplicated EOP packet for GFX7/8 have real content
    ANBZ: #20490
    
    The duplication of EOP packets for GFX7/8, with the former one have
    seq-1 written and the latter one have seq written, seems to confuse some
    hardware platform (e.g. Loongson 7A series PCIe controllers).
    
    Make the content of the duplicated EOP packet the same with the real
    one, only masking any possible interrupts.
    
    Signed-off-by: Icenowy Zheng <uwu@icenowy.me>
    Signed-off-by: lvjianmin <lvjianmin@loongson.cn>
    Signed-off-by: Juxin Gao <gaojuxin@loongson.cn>
    Reviewed-by: Min Li <gumi@linux.alibaba.com>
    Reviewed-by: Guixin Liu <kanie@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5113

--- drivers/gpu/drm/amd/amdgpu/gfx_v7_0.c
+++ drivers/gpu/drm/amd/amdgpu/gfx_v7_0.c
@@ -2127,9 +2127,15 @@ static void gfx_v7_0_ring_emit_fence_gfx(struct amdgpu_ring *ring, u64 addr,
 				 EVENT_INDEX(5)));
 	amdgpu_ring_write(ring, addr & 0xfffffffc);
 	amdgpu_ring_write(ring, (upper_32_bits(addr) & 0xffff) |
+#ifdef CONFIG_LOONGARCH
+				DATA_SEL(write64bit ? 2 : 1) | INT_SEL(0));
+	amdgpu_ring_write(ring, lower_32_bits(seq));
+	amdgpu_ring_write(ring, upper_32_bits(seq));
+#else
 				DATA_SEL(1) | INT_SEL(0));
 	amdgpu_ring_write(ring, lower_32_bits(seq - 1));
 	amdgpu_ring_write(ring, upper_32_bits(seq - 1));
+#endif
 
 	/* Then send the real EOP event down the pipe. */
 	amdgpu_ring_write(ring, PACKET3(PACKET3_EVENT_WRITE_EOP, 4));
