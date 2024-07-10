d4ddd30165df4deeac7adebf532f82a84c4957a1
Matthew Wilcox (Oracle) <willy@infradead.org>
2024-07-10 15:45:32 -0400

    fs: Convert aops->write_end to take a folio
    ANBZ: #21672
    
    commit a225800f322a3d6cc8b8b6c7dc4d5281f2f5375b upstream.
    
    Most callers have a folio, and most implementations operate on a folio,
    so remove the conversion from folio->page->folio to fit through this
    interface.
    
    Reviewed-by: Josef Bacik <josef@toxicpanda.com>
    Signed-off-by: Matthew Wilcox (Oracle) <willy@infradead.org>
    Signed-off-by: Christian Brauner <brauner@kernel.org>
    [ joe: code conflict fixes for 9p, afs, f2fs, ntfs, cifs, xfs ]
    Signed-off-by: Joseph Qi <joseph.qi@linux.alibaba.com>
    Reviewed-by: Jingbo Xu <jefflexu@linux.alibaba.com>
    Reviewed-by: Jia He <justin.he@arm.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5416

--- fs/afs/write.c
+++ fs/afs/write.c
@@ -152,9 +152,8 @@ error:
  */
 int afs_write_end(struct file *file, struct address_space *mapping,
 		  loff_t pos, unsigned len, unsigned copied,
-		  struct page *subpage, void *fsdata)
+		  struct folio *folio, void *fsdata)
 {
-	struct folio *folio = page_folio(subpage);
 	struct afs_vnode *vnode = AFS_FS_I(file_inode(file));
 	unsigned long priv;
 	unsigned int f, from = offset_in_folio(folio, pos);
