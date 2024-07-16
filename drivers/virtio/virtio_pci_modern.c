3543ea31c4ac7640cbfa54b304e228033fc714ad
Jiri Pirko <jiri@nvidia.com>
2024-07-16 13:35:45 +0200

    virtio_pci_modern: treat vp_dev->admin_vq.info.vq pointer as static
    ANBZ: #22277
    
    commit 4199107e39122e49bbddafb50932943127b63aee upstream.
    
    It is guaranteed by the virtio_pci and PCI layers that none of the VFs
    is probed before setup_vq() is called for admin queue and after del_vq()
    is called for admin queue. Therefore treat vp_dev->admin_vq.info.vq as
    static, don't null it and don't take cmd lock during assign.
    
    Signed-off-by: Jiri Pirko <jiri@nvidia.com>
    Message-Id: <20240716113552.80599-7-jiri@resnulli.us>
    Signed-off-by: Michael S. Tsirkin <mst@redhat.com>
    Signed-off-by: Qinyun Tan <qinyuntan@linux.alibaba.com>

--- drivers/virtio/virtio_pci_modern.c
+++ drivers/virtio/virtio_pci_modern.c
@@ -562,9 +562,7 @@ static struct virtqueue *setup_vq(struct virtio_pci_device *vp_dev,
 	}
 
 	if (is_avq) {
-		mutex_lock(&vp_dev->admin_vq.cmd_lock);
 		vp_dev->admin_vq.info.vq = vq;
-		mutex_unlock(&vp_dev->admin_vq.cmd_lock);
 	}
 
 	return vq;
@@ -601,12 +599,6 @@ static void del_vq(struct virtio_pci_vq_info *info)
 	struct virtqueue *vq = info->vq;
 	struct virtio_pci_device *vp_dev = to_vp_device(vq->vdev);
 	struct virtio_pci_modern_device *mdev = &vp_dev->mdev;
-
-	if (vp_is_avq(&vp_dev->vdev, vq->index)) {
-		mutex_lock(&vp_dev->admin_vq.cmd_lock);
-		vp_dev->admin_vq.info.vq = NULL;
-		mutex_unlock(&vp_dev->admin_vq.cmd_lock);
-	}
 
 	if (vp_dev->msix_enabled)
 		vp_modern_queue_vector(mdev, vq->index,
