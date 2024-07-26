e590269f13b89cccbf90b93998842dd70f0cc726
Haifeng Xu <haifeng.xu@shopee.com>
2024-07-26 11:05:25 +0800

    fs: don't flush in-flight wb switches for superblocks without cgroup writeback
    ANBZ: #35658
    
    commit c393eaa85349941badf3ce5f087400dbaf3bbe02 upstream.
    
    When deactivating any type of superblock, it had to wait for the in-flight
    wb switches to be completed. wb switches are executed in inode_switch_wbs_work_fn()
    which needs to acquire the wb_switch_rwsem and races against sync_inodes_sb().
    If there are too much dirty data in the superblock, the waiting time may increase
    significantly.
    
    For superblocks without cgroup writeback such as tmpfs, they have nothing to
    do with the wb swithes, so the flushing can be avoided.
    
    Signed-off-by: Haifeng Xu <haifeng.xu@shopee.com>
    Link: https://lore.kernel.org/r/20240726030525.180330-1-haifeng.xu@shopee.com
    Reviewed-by: Jan Kara <jack@suse.cz>
    Suggested-by: Jan Kara <jack@suse.cz>
    Signed-off-by: Christian Brauner <brauner@kernel.org>
    
    Assisted-by: PatchPilot <patchpilot@openanolis.org>
    Signed-off-by: Baokun Li <libaokun@linux.alibaba.com>
    Reviewed-by: Joseph Qi <joseph.qi@linux.alibaba.com>
    Reviewed-by: Xunlei Pang <xlpang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/7110

--- fs/super.c
+++ fs/super.c
@@ -523,7 +523,7 @@ void generic_shutdown_super(struct super_block *sb)
 		sync_filesystem(sb);
 		sb->s_flags &= ~SB_ACTIVE;
 
-		cgroup_writeback_umount();
+		cgroup_writeback_umount(sb);
 
 		/* evict all inodes with zero refcount */
 		evict_inodes(sb);
