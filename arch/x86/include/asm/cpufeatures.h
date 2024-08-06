87e407f5e4538670d185b17005cb15b258722bf2
Babu Moger <babu.moger@amd.com>
2024-08-06 17:00:38 -0500

    x86/cpufeatures: Add support for Assignable Bandwidth Monitoring Counters (ABMC)
    ANBZ: #20932
    
    cherry-picked from https://lore.kernel.org/all/cover.1722981659.git.babu.moger@amd.com/
    
    Users can create as many monitor groups as RMIDs supported by the hardware.
    However, bandwidth monitoring feature on AMD system only guarantees that
    RMIDs currently assigned to a processor will be tracked by hardware. The
    counters of any other RMIDs which are no longer being tracked will be
    reset to zero. The MBM event counters return "Unavailable" for the RMIDs
    that are not tracked by hardware. So, there can be only limited number of
    groups that can give guaranteed monitoring numbers. With ever changing
    configurations there is no way to definitely know which of these groups
    are being tracked for certain point of time. Users do not have the option
    to monitor a group or set of groups for certain period of time without
    worrying about RMID being reset in between.
    
    The ABMC feature provides an option to the user to assign a hardware
    counter to an RMID and monitor the bandwidth as long as it is assigned.
    The assigned RMID will be tracked by the hardware until the user unassigns
    it manually. There is no need to worry about counters being reset during
    this period. Additionally, the user can specify a bitmask identifying the
    specific bandwidth types from the given source to track with the counter.
    
    Without ABMC enabled, monitoring will work in current mode without
    assignment option.
    
    Linux resctrl subsystem provides the interface to count maximum of two
    memory bandwidth events per group, from a combination of available total
    and local events. Keeping the current interface, users can enable a maximum
    of 2 ABMC counters per group. User will also have the option to enable only
    one counter to the group. If the system runs out of assignable ABMC
    counters, kernel will display an error. Users need to disable an already
    enabled counter to make space for new assignments.
    
    The feature can be detected via CPUID_Fn80000020_EBX_x00 bit 5.
    Bits Description
    5    ABMC (Assignable Bandwidth Monitoring Counters)
    
    The feature details are documented in APM listed below [1].
    [1] AMD64 Architecture Programmer's Manual Volume 2: System Programming
    Publication # 24593 Revision 3.41 section 19.3.3.3 Assignable Bandwidth
    Monitoring (ABMC).
    
    Link: https://bugzilla.kernel.org/show_bug.cgi?id=206537
    Signed-off-by: Babu Moger <babu.moger@amd.com>
    Reviewed-by: Reinette Chatre <reinette.chatre@intel.com>
    Signed-off-by: Qinyun Tan <qinyuntan@linux.alibaba.com>
    Reviewed-by: Min Li <gumi@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5266

--- arch/x86/include/asm/cpufeatures.h
+++ arch/x86/include/asm/cpufeatures.h
@@ -501,6 +501,7 @@
 #define X86_FEATURE_BHI_CTRL		(21*32+ 2) /* "" BHI_DIS_S HW control available */
 #define X86_FEATURE_CLEAR_BHB_HW	(21*32+ 3) /* "" BHI_DIS_S HW control enabled */
 #define X86_FEATURE_CLEAR_BHB_LOOP_ON_VMEXIT (21*32+ 4) /* "" Clear branch history at vmexit using SW loop */
+#define X86_FEATURE_ABMC		(21*32+ 6) /* "" Assignable Bandwidth Monitoring Counters */
 
 /*
  * BUG word(s)
