8db45175260128145f8895a84e71d9e56b763ee4
Babu Moger <babu.moger@amd.com>
2024-08-06 17:00:51 -0500

    x86/resctrl: Add the interface to assign a hardware counter
    ANBZ: #20932
    
    cherry-picked from https://lore.kernel.org/all/cover.1722981659.git.babu.moger@amd.com/
    
    The ABMC feature provides an option to the user to assign a hardware
    counter to an RMID and monitor the bandwidth as long as it is assigned.
    The assigned RMID will be tracked by the hardware until the user unassigns
    it manually.
    
    Counters are configured by writing to L3_QOS_ABMC_CFG MSR and
    specifying the counter id, bandwidth source, and bandwidth types.
    
    Provide the interface to assign the counter ids to RMID.
    
    The feature details are documented in the APM listed below [1].
    [1] AMD64 Architecture Programmer's Manual Volume 2: System Programming
        Publication # 24593 Revision 3.41 section 19.3.3.3 Assignable Bandwidth
        Monitoring (ABMC).
    
    Link: https://bugzilla.kernel.org/show_bug.cgi?id=206537
    Signed-off-by: Babu Moger <babu.moger@amd.com>
    Signed-off-by: Qinyun Tan <qinyuntan@linux.alibaba.com>
    Reviewed-by: Min Li <gumi@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5266

--- fs/resctrl/internal.h
+++ fs/resctrl/internal.h
@@ -315,6 +315,8 @@ void rdt_staged_configs_clear(void);
 bool closid_allocated(unsigned int closid);
 int resctrl_find_cleanest_closid(void);
 unsigned int mon_event_config_index_get(u32 evtid);
+int rdtgroup_assign_cntr(struct rdtgroup *rdtgrp, enum resctrl_event_id evtid);
+int rdtgroup_alloc_cntr(struct rdtgroup *rdtgrp, int index);
 
 #ifdef CONFIG_RESCTRL_FS_PSEUDO_LOCK
 int rdtgroup_locksetup_enter(struct rdtgroup *rdtgrp);
