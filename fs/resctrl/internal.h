83e3d5d187b55106ae90840290a8e8285aca7495
Babu Moger <babu.moger@amd.com>
2024-08-06 17:00:51 -0500

    x86/resctrl: Introduce cntr_id in mongroup for assignments
    ANBZ: #20932
    
    cherry-picked from https://lore.kernel.org/all/cover.1722981659.git.babu.moger@amd.com/
    
    mbm_cntr_assignable feature provides an option to the user to assign a
    hardware counter to an RMID and monitor the bandwidth as long as the
    counter is assigned. There can be two counters per monitor group, one
    for total event and another for local event.
    
    Introduce cntr_id to manage the assignments.
    
    Signed-off-by: Babu Moger <babu.moger@amd.com>
    Signed-off-by: Qinyun Tan <qinyuntan@linux.alibaba.com>
    Reviewed-by: Min Li <gumi@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5266

--- fs/resctrl/internal.h
+++ fs/resctrl/internal.h
@@ -11,6 +11,11 @@
 
 #include <asm/resctrl.h>
 
+/* Maximum assignable counters per resctrl group */
+#define MAX_CNTRS			2
+
+#define MON_CNTR_UNSET			U32_MAX
+
 /**
  * cpumask_any_housekeeping() - Choose any CPU in @mask, preferring those that
  *			        aren't marked nohz_full
@@ -155,12 +160,14 @@ enum rdtgrp_mode {
  * @parent:			parent rdtgrp
  * @crdtgrp_list:		child rdtgroup node list
  * @rmid:			rmid for this rdtgroup
+ * @cntr_id:			Counter ids for assignment
  */
 struct mongroup {
 	struct kernfs_node	*mon_data_kn;
 	struct rdtgroup		*parent;
 	struct list_head	crdtgrp_list;
 	u32			rmid;
+	u32			cntr_id[MAX_CNTRS];
 };
 
 /**
