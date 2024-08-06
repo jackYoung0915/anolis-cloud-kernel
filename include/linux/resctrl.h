26bec061fb1fc7a32f2d6ac6d051bad506d16a9d
Babu Moger <babu.moger@amd.com>
2024-08-06 17:00:47 -0500

    x86/resctrl: Introduce mbm_total_cfg and mbm_local_cfg
    ANBZ: #20932
    
    cherry-picked from https://lore.kernel.org/all/cover.1722981659.git.babu.moger@amd.com/
    
    If the BMEC (Bandwidth Monitoring Event Configuration) feature is
    supported, the bandwidth events can be configured to track specific
    events. The event configuration is domain specific. ABMC (Assignable
    Bandwidth Monitoring Counters) feature needs event configuration
    information to assign hardware counter to an RMID. Event configurations
    are not stored in resctrl but instead always read from or written to
    hardware directly when prompted by user space.
    
    Read the event configuration from the hardware during the domain
    initialization. Save the configuration information in rdt_hw_mon_domain,
    so it can be used for counter assignment.
    
    Signed-off-by: Babu Moger <babu.moger@amd.com>
    Signed-off-by: Qinyun Tan <qinyuntan@linux.alibaba.com>
    Reviewed-by: Min Li <gumi@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5266

--- include/linux/resctrl.h
+++ include/linux/resctrl.h
@@ -272,6 +272,28 @@ struct resctrl_mon_config_info {
 	int                  err;
 };
 
+/**
+ * mon_event_config_index_get - get the hardware index for the
+ *                              configurable event
+ * @evtid: event id.
+ *
+ * Return: 0 for evtid == QOS_L3_MBM_TOTAL_EVENT_ID
+ *         1 for evtid == QOS_L3_MBM_LOCAL_EVENT_ID
+ *         INVALID_CONFIG_INDEX for invalid evtid
+ */
+static inline unsigned int mon_event_config_index_get(u32 evtid)
+{
+	switch (evtid) {
+	case QOS_L3_MBM_TOTAL_EVENT_ID:
+		return 0;
+	case QOS_L3_MBM_LOCAL_EVENT_ID:
+		return 1;
+	default:
+		/* Should never reach here */
+		return INVALID_CONFIG_INDEX;
+	}
+}
+
 /*
  * Update and re-load this CPUs defaults. Called via IPI, takes a pointer to
  * struct resctrl_cpu_sync, or NULL.
