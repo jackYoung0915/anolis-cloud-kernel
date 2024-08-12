ccdd6f3276de47cb19f996623a2ed55907257589
Baolin Wang <baolin.wang@linux.alibaba.com>
2024-08-12 15:42:02 +0800

    mm: swap: extend swap_shmem_alloc() to support batch SWAP_MAP_SHMEM flag setting
    ANBZ: #9728
    
    commit 650180760be6bb448609d4d155eef3b728ace641 upstream
    
    Patch series "support large folio swap-out and swap-in for shmem", v5.
    
    Shmem will support large folio allocation [1] [2] to get a better
    performance, however, the memory reclaim still splits the precious large
    folios when trying to swap-out shmem, which may lead to the memory
    fragmentation issue and can not take advantage of the large folio for
    shmeme.
    
    Moreover, the swap code already supports for swapping out large folio
    without split, and large folio swap-in[3] series is queued into
    mm-unstable branch.  Hence this patch set also supports the large folio
    swap-out and swap-in for shmem.
    
    This patch (of 9):
    
    To support shmem large folio swap operations, add a new parameter to
    swap_shmem_alloc() that allows batch SWAP_MAP_SHMEM flag setting for shmem
    swap entries.
    
    While we are at it, using folio_nr_pages() to get the number of pages of
    the folio as a preparation.
    
    Link: https://lkml.kernel.org/r/cover.1723434324.git.baolin.wang@linux.alibaba.com
    Link: https://lkml.kernel.org/r/99f64115d04b285e009580eb177352c57119ffd0.1723434324.git.baolin.wang@linux.alibaba.com
    Signed-off-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Reviewed-by: Barry Song <baohua@kernel.org>
    Cc: Chris Li <chrisl@kernel.org>
    Cc: Daniel Gomez <da.gomez@samsung.com>
    Cc: David Hildenbrand <david@redhat.com>
    Cc: "Huang, Ying" <ying.huang@intel.com>
    Cc: Hugh Dickins <hughd@google.com>
    Cc: Kefeng Wang <wangkefeng.wang@huawei.com>
    Cc: Lance Yang <ioworker0@gmail.com>
    Cc: Matthew Wilcox <willy@infradead.org>
    Cc: Pankaj Raghav <p.raghav@samsung.com>
    Cc: Ryan Roberts <ryan.roberts@arm.com>
    Cc: Yang Shi <shy828301@gmail.com>
    Cc: Zi Yan <ziy@nvidia.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Reviewed-by: Xu Yu <xuyu@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/4122

--- mm/shmem.c
+++ mm/shmem.c
@@ -1445,6 +1445,7 @@ static int shmem_writepage(struct page *page, struct writeback_control *wbc)
 	struct shmem_sb_info *sbinfo = SHMEM_SB(inode->i_sb);
 	swp_entry_t swap;
 	pgoff_t index;
+	int nr_pages;
 
 	/*
 	 * Our capabilities prevent regular writeback or sync from ever calling
@@ -1477,6 +1478,7 @@ static int shmem_writepage(struct page *page, struct writeback_control *wbc)
 	}
 
 	index = folio->index;
+	nr_pages = folio_nr_pages(folio);
 
 	/*
 	 * This is somewhat ridiculous, but without plumbing a SWAP_MAP_FALLOC
@@ -1529,8 +1531,8 @@ static int shmem_writepage(struct page *page, struct writeback_control *wbc)
 	if (add_to_swap_cache(folio, swap,
 			__GFP_HIGH | __GFP_NOMEMALLOC | __GFP_NOWARN,
 			NULL) == 0) {
-		shmem_recalc_inode(inode, 0, 1);
-		swap_shmem_alloc(swap);
+		shmem_recalc_inode(inode, 0, nr_pages);
+		swap_shmem_alloc(swap, nr_pages);
 		shmem_delete_from_page_cache(folio, swp_to_radix_entry(swap));
 
 		mutex_unlock(&shmem_swaplist_mutex);
