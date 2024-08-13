caae9e235142f2d848387a300d2a9aea5a8e189b
Suren Baghdasaryan <surenb@google.com>
2024-08-13 08:07:56 -0700

    alloc_tag: introduce clear_page_tag_ref() helper function
    ANBZ: #32420
    
    commit a8fc28dad6d574582cdf2f7e78c73c59c623df30 upstream.
    
    In several cases we are freeing pages which were not allocated using
    common page allocators.  For such cases, in order to keep allocation
    accounting correct, we should clear the page tag to indicate that the page
    being freed is expected to not have a valid allocation tag.  Introduce
    clear_page_tag_ref() helper function to be used for this.
    
    Link: https://lkml.kernel.org/r/20240813150758.855881-1-surenb@google.com
    Fixes: d224eb0287fb ("codetag: debug: mark codetags for reserved pages as empty")
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Suggested-by: David Hildenbrand <david@redhat.com>
    Acked-by: David Hildenbrand <david@redhat.com>
    Reviewed-by: Pasha Tatashin <pasha.tatashin@soleen.com>
    Cc: Kees Cook <keescook@chromium.org>
    Cc: Kent Overstreet <kent.overstreet@linux.dev>
    Cc: Sourav Panda <souravpanda@google.com>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Cc: <stable@vger.kernel.org>	[6.10]
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Yuanhe Shu <xiangzao@linux.alibaba.com>
    Acked-by: David Rientjes <rientjes@google.com>
    Acked-by: Harry Yoo <harry.yoo@oracle.com>
    Acked-by: Kent Overstreet <kent.overstreet@linux.dev>
    Acked-by: Muchun Song <muchun.song@linux.dev>
    Acked-by: Vlastimil Babka <vbabka@suse.cz>
    Reviewed-by: Kees Cook <kees@kernel.org>
    Reviewed-by: Hao Li <hao.li@linux.dev>
    Reviewed-by: Miaohe Lin <linmiaohe@huawei.com>
    Acked-by: Shakeel Butt <shakeel.butt@linux.dev>
    Acked-by: SeongJae Park <sj@kernel.org>
    Acked-by: Yu Zhao <yuzhao@google.com>
    Reviewed-by: Anshuman Khandual <anshuman.khandual@arm.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6766

--- include/linux/pgalloc_tag.h
+++ include/linux/pgalloc_tag.h
@@ -45,6 +45,18 @@ static inline void put_page_tag_ref(union codetag_ref *ref)
 	page_ext_put(page_ext_from_codetag_ref(ref));
 }
 
+static inline void clear_page_tag_ref(struct page *page)
+{
+	if (mem_alloc_profiling_enabled()) {
+		union codetag_ref *ref = get_page_tag_ref(page);
+
+		if (ref) {
+			set_codetag_empty(ref);
+			put_page_tag_ref(ref);
+		}
+	}
+}
+
 static inline void pgalloc_tag_add(struct page *page, struct task_struct *task,
 				   unsigned int nr)
 {
@@ -128,6 +140,7 @@ static inline void pgalloc_tag_sub_pages(struct alloc_tag *tag, unsigned int nr)
 
 static inline union codetag_ref *get_page_tag_ref(struct page *page) { return NULL; }
 static inline void put_page_tag_ref(union codetag_ref *ref) {}
+static inline void clear_page_tag_ref(struct page *page) {}
 static inline void pgalloc_tag_add(struct page *page, struct task_struct *task,
 				   unsigned int nr) {}
 static inline void pgalloc_tag_sub(struct page *page, unsigned int nr) {}
