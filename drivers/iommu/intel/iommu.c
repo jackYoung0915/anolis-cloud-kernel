4399f90730c98f72749b82df47253d5b656a1249
Lu Baolu <baolu.lu@linux.intel.com>
2024-08-15 20:48:57 +0800

    iommu/vt-d: Fix incorrect domain ID in context flush helper
    ANBZ: #13617
    
    commit 7af6c720417f21f015f46baa33e182f349ddc93b upstream.
    
    The helper intel_context_flush_present() is designed to flush all related
    caches when a context entry with the present bit set is modified. It
    currently retrieves the domain ID from the context entry and uses it to
    flush the IOTLB and context caches. This is incorrect when the context
    entry transitions from present to non-present, as the domain ID field is
    cleared before calling the helper.
    
    Fix it by passing the domain ID programmed in the context entry before the
    change to intel_context_flush_present(). This ensures that the correct
    domain ID is used for cache invalidation.
    
    Fixes: f90584f4beb8 ("iommu/vt-d: Add helper to flush caches for context change")
    Reported-by: Alex Williamson <alex.williamson@redhat.com>
    Closes: https://lore.kernel.org/linux-iommu/20240814162726.5efe1a6e.alex.williamson@redhat.com/
    Signed-off-by: Lu Baolu <baolu.lu@linux.intel.com>
    Tested-by: Alex Williamson <alex.williamson@redhat.com>
    Reviewed-by: Alex Williamson <alex.williamson@redhat.com>
    Reviewed-by: Jerry Snitselaar <jsnitsel@redhat.com>
    Reviewed-by: Jacob Pan <jacob.pan@linux.microsoft.com>
    Link: https://lore.kernel.org/r/20240815124857.70038-1-baolu.lu@linux.intel.com
    Signed-off-by: Joerg Roedel <jroedel@suse.de>
    Signed-off-by: Shuai Xue <xueshuai@linux.alibaba.com>

--- drivers/iommu/intel/iommu.c
+++ drivers/iommu/intel/iommu.c
@@ -1956,6 +1956,7 @@ static void domain_context_clear_one(struct device_domain_info *info, u8 bus, u8
 {
 	struct intel_iommu *iommu = info->iommu;
 	struct context_entry *context;
+	u16 did;
 
 	spin_lock(&iommu->lock);
 	context = iommu_context_addr(iommu, bus, devfn, 0);
@@ -1964,10 +1965,11 @@ static void domain_context_clear_one(struct device_domain_info *info, u8 bus, u8
 		return;
 	}
 
+	did = context_domain_id(context);
 	context_clear_entry(context);
 	__iommu_flush_cache(iommu, context, sizeof(*context));
 	spin_unlock(&iommu->lock);
-	intel_context_flush_present(info, context, true);
+	intel_context_flush_present(info, context, did, true);
 }
 
 static int domain_setup_first_level(struct intel_iommu *iommu,
@@ -4322,6 +4324,7 @@ static int context_flip_pri(struct device_domain_info *info, bool enable)
 	struct intel_iommu *iommu = info->iommu;
 	u8 bus = info->bus, devfn = info->devfn;
 	struct context_entry *context;
+	u16 did;
 
 	spin_lock(&iommu->lock);
 	if (context_copied(iommu, bus, devfn)) {
@@ -4334,6 +4337,7 @@ static int context_flip_pri(struct device_domain_info *info, bool enable)
 		spin_unlock(&iommu->lock);
 		return -ENODEV;
 	}
+	did = context_domain_id(context);
 
 	if (enable)
 		context_set_sm_pre(context);
@@ -4342,7 +4346,7 @@ static int context_flip_pri(struct device_domain_info *info, bool enable)
 
 	if (!ecap_coherent(iommu->ecap))
 		clflush_cache_range(context, sizeof(*context));
-	intel_context_flush_present(info, context, true);
+	intel_context_flush_present(info, context, did, true);
 	spin_unlock(&iommu->lock);
 
 	return 0;
