caa4302fb72193f73d019ac1e2de79457f2890c2
Shawn Wang <shawnwang@linux.alibaba.com>
2024-08-16 14:24:48 +0800

    anolis: mm: kidled: Add a new *KIDLED_YOUNG flag to replace *REFERENCED checking
    ANBZ: #22924
    
    cherry-picked from https://gitee.com/anolis/cloud-kernel/pulls/3720
    
    Now kidled will skip the dentry/inode with *REFERENCED set, which means
    the age of these entries is always 0. To reclaim these entries and avoid
    pollution of the *REFERENCED flag, introduce a new *KIDLED_YOUNG flag for
    kidled to indicate the entry has been referenced since the last kidled
    scanning.
    
    *KIDLED_YOUNG should be set at the same time as the *REFERENCED. More
    specifically, it will be set in retain_dentry() and inode_lru_list_add()
    for now.
    
    Besides, kidled and coldpgs modules are modified to check *KIDELD_YOUNG
    instead of *REFERENCED:
    
    When kidled scans an entry with *KIDLED_YOUNG set, it clears this flag
    and set the age of this entry to 0.
    
    When coldpgs scans an entry with *KIDLED_YOUNG set, it just ignores this
    entry.
    
    Signed-off-by: Shawn Wang <shawnwang@linux.alibaba.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Reviewed-by: Xu Yu <xuyu@linux.alibaba.com>
    Acked-by: Joseph Qi <joseph.qi@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/3720
    Signed-off-by: Zelin Deng <zelin.deng@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5541

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -2352,6 +2352,10 @@ static inline void kiocb_clone(struct kiocb *kiocb, struct kiocb *kiocb_src,
 #define __I_LRU_ISOLATING	19
 #define I_LRU_ISOLATING		(1 << __I_LRU_ISOLATING)
 
+#ifdef CONFIG_KIDLED
+#define I_KIDLED_YOUNG		(1 << 31)
+#endif
+
 #define I_DIRTY_INODE (I_DIRTY_SYNC | I_DIRTY_DATASYNC)
 #define I_DIRTY (I_DIRTY_INODE | I_DIRTY_PAGES)
 #define I_DIRTY_ALL (I_DIRTY | I_DIRTY_TIME)
