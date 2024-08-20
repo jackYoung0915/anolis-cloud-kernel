4150495d3de8bf34a585c800c2c8e8597fae4ffa
Yafang Shao <laoar.shao@gmail.com>
2024-08-20 10:26:39 +0800

    mm: allow read-ahead with IOCB_NOWAIT set
    ANBZ: #18912
    
    commit 0a2d82946be67e02fdf85a4010606bdc0546ba44 upstream.
    
    Readahead support for IOCB_NOWAIT was introduced in commit 2e85abf053b9
    ("mm: allow read-ahead with IOCB_NOWAIT set").  However, this
    implementation broke the semantics of IOCB_NOWAIT by potentially causing
    it to wait on I/O during memory reclamation.  This behavior was later
    modified in commit efa8480a8316 ("fs: RWF_NOWAIT should imply IOCB_NOIO").
    
    To resolve the blocking issue during memory reclamation, we can use
    memalloc_noio_{save,restore} to ensure non-blocking behavior.  This change
    restores the original functionality, allowing preadv2(IOCB_NOWAIT) to
    trigger readahead if the file content is not present in the page cache.
    
    While this process may trigger direct memory reclamation, the
    __GFP_NORETRY flag is set in the readahead GFP flags, ensuring it won't
    block.
    
    A use case for this change is when we want to trigger readahead in the
    preadv2(2) syscall if the file cache is absent, but without waiting for
    certain filesystem locks, like xfs_ilock.  A simple example is as follows:
    
    retry:
        if (preadv2(fd, iovec, cnt, offset, RWF_NOWAIT) < 0) {
            do_other_work();
            goto retry;
        }
    
    Link: https://lore.gnuweeb.org/io-uring/20200624164127.GP21350@casper.infradead.org/
    Link: https://lkml.kernel.org/r/20240820022639.89562-1-laoar.shao@gmail.com
    Signed-off-by: Yafang Shao <laoar.shao@gmail.com>
    Cc: Jens Axboe <axboe@kernel.dk>
    Cc: Matthew Wilcox <willy@infradead.org>
    Cc: Dave Chinner <david@fromorbit.com>
    Cc: Jan Kara <jack@suse.cz>
    Cc: Christian Brauner <brauner@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Jingbo Xu <jefflexu@linux.alibaba.com>
    Reviewed-by: Joseph Qi <joseph.qi@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/4656

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -3371,7 +3371,6 @@ static inline int kiocb_set_rw_flags(struct kiocb *ki, rwf_t flags)
 	if (flags & RWF_NOWAIT) {
 		if (!(ki->ki_filp->f_mode & FMODE_NOWAIT))
 			return -EOPNOTSUPP;
-		kiocb_flags |= IOCB_NOIO;
 	}
 	kiocb_flags |= (__force int) (flags & RWF_SUPPORTED);
 	if (flags & RWF_SYNC)
