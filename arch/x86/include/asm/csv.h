1b1998c87101b211a6040f805f477a2c9db03080
hanliyang <hanliyang@hygon.cn>
2024-08-21 12:01:23 +0800

    anolis: x86/csv: Add support for CSV3 ATTESTATION secure call
    ANBZ: #13045
    
    Expose the function to modules so that user can get attestation report
    of CSV3 guest through ioctl interface.
    
    It's suggested that the user make use of the ioctl interface of the
    module csv-guest.
    
    Signed-off-by: hanliyang <hanliyang@hygon.cn>

--- arch/x86/include/asm/csv.h
+++ arch/x86/include/asm/csv.h
@@ -10,6 +10,8 @@
 #ifndef _ASM_X86_CSV_H
 #define _ASM_X86_CSV_H
 
+#include "csv_command.h"
+
 #ifdef CONFIG_HYGON_CSV
 
 #define CSV_MR_ALIGN_BITS		(28)
@@ -28,6 +30,12 @@ phys_addr_t csv_alloc_from_contiguous(size_t size, nodemask_t *nodes_allowed,
 void csv_release_to_contiguous(phys_addr_t pa, size_t size);
 uint32_t csv_get_smr_entry_shift(void);
 
-#endif
+int csv3_issue_request_report(phys_addr_t paddr, size_t size);
+
+#else
+
+static inline int csv3_issue_request_report(phys_addr_t paddr, size_t size) { return -EIO; }
+
+#endif	/* CONFIG_HYGON_CSV */
 
 #endif
