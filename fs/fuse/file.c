0dd76fb0f84bf69ae08b36ed5f6daacff6c364b4
yangyun <yangyun50@huawei.com>
2024-08-26 21:06:12 +0800

    fuse: remove useless IOCB_DIRECT in fuse_direct_read/write_iter
    ANBZ: #12160
    
    commit cc23d537e56153560bb2f88fd826675a5a8c6af6 upstream.
    
    Commit 23c94e1cdcbf ("fuse: Switch to using async direct IO
    for FOPEN_DIRECT_IO") gave the async direct IO code path in the
    fuse_direct_read_iter() and fuse_direct_write_iter(). But since
    these two functions are only called under FOPEN_DIRECT_IO is set,
    it seems that we can also use the async direct IO even the flag
    IOCB_DIRECT is not set to enjoy the async direct IO method. Also
    move the definition of fuse_io_priv to where it is used in fuse_
    direct_write_iter.
    
    Signed-off-by: yangyun <yangyun50@huawei.com>
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>
    Signed-off-by: Jingbo Xu <jefflexu@linux.alibaba.com>

--- fs/fuse/file.c
+++ fs/fuse/file.c
@@ -1716,7 +1716,7 @@ static ssize_t fuse_direct_read_iter(struct kiocb *iocb, struct iov_iter *to)
 {
 	ssize_t res;
 
-	if (!is_sync_kiocb(iocb) && iocb->ki_flags & IOCB_DIRECT) {
+	if (!is_sync_kiocb(iocb)) {
 		res = fuse_direct_IO(iocb, to);
 	} else {
 		struct fuse_io_priv io = FUSE_IO_PRIV_SYNC(iocb);
@@ -1730,16 +1730,17 @@ static ssize_t fuse_direct_read_iter(struct kiocb *iocb, struct iov_iter *to)
 static ssize_t fuse_direct_write_iter(struct kiocb *iocb, struct iov_iter *from)
 {
 	struct inode *inode = file_inode(iocb->ki_filp);
-	struct fuse_io_priv io = FUSE_IO_PRIV_SYNC(iocb);
 	ssize_t res;
 
 	/* Don't allow parallel writes to the same file */
 	inode_lock(inode);
 	res = generic_write_checks(iocb, from);
 	if (res > 0) {
-		if (!is_sync_kiocb(iocb) && iocb->ki_flags & IOCB_DIRECT) {
+		if (!is_sync_kiocb(iocb)) {
 			res = fuse_direct_IO(iocb, from);
 		} else {
+			struct fuse_io_priv io = FUSE_IO_PRIV_SYNC(iocb);
+
 			res = fuse_direct_io(&io, from, &iocb->ki_pos,
 					     FUSE_DIO_WRITE);
 		}
