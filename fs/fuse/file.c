d80825976a67038d1322956dfd6e2c193892603f
Joanne Koong <joannelkoong@gmail.com>
2024-08-26 14:19:07 -0700

    fuse: move fuse file initialization to wpa allocation time
    ANBZ: #11413
    
    commit 4046d3adcca42b7678f11c71e46bd32bafb4dad1 upstream.
    
    Before this change, wpa->ia.ff is initialized with an acquired reference
    on the fuse file right before it submits the writeback request. If there
    are auxiliary writebacks, then the initialization and reference
    acquisition needs to also be set before we submit the auxiliary writeback
    request.
    
    To make the logic simpler and to pave the way for a subsequent
    refactoring of fuse_writepages_fill() and fuse_writepage_locked(), this
    change initializes and acquires wpa->ia.ff when the wpa is allocated.
    
    No functional changes added.
    
    Signed-off-by: Joanne Koong <joannelkoong@gmail.com>
    Reviewed-by: Josef Bacik <josef@toxicpanda.com>
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>
    [jingbo: resolve codebase conflict since prototype change of fuse_file_put()]
    Signed-off-by: Jingbo Xu <jefflexu@linux.alibaba.com>
    Reviewed-by: Joseph Qi <joseph.qi@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/4035

--- fs/fuse/file.c
+++ fs/fuse/file.c
@@ -1808,8 +1808,7 @@ static void fuse_writepage_free(struct fuse_writepage_args *wpa)
 	for (i = 0; i < ap->num_pages; i++)
 		__free_page(ap->pages[i]);
 
-	if (wpa->ia.ff)
-		fuse_file_put(wpa->ia.ff, false, false);
+	fuse_file_put(wpa->ia.ff, false, false);
 
 	kfree(ap->pages);
 	kfree(wpa);
@@ -1982,7 +1981,6 @@ static void fuse_writepage_end(struct fuse_mount *fm, struct fuse_args *args,
 
 		wpa->next = next->next;
 		next->next = NULL;
-		next->ia.ff = fuse_file_get(wpa->ia.ff);
 		tree_insert(&fi->writepages, next);
 
 		/*
@@ -2209,7 +2207,6 @@ static void fuse_writepages_send(struct fuse_fill_wb_data *data)
 	int num_pages = wpa->ia.ap.num_pages;
 	int i;
 
-	wpa->ia.ff = fuse_file_get(data->ff);
 	spin_lock(&fi->lock);
 	list_add_tail(&wpa->queue_entry, &fi->queued_writes);
 	fuse_flush_writepages(inode);
@@ -2363,6 +2360,7 @@ static int fuse_writepages_fill(struct page *page,
 		ap = &wpa->ia.ap;
 		fuse_write_args_fill(&wpa->ia, data->ff, page_offset(page), 0);
 		wpa->ia.write.in.write_flags |= FUSE_WRITE_CACHE;
+		wpa->ia.ff = fuse_file_get(data->ff);
 		wpa->next = NULL;
 		ap->args.in_pages = true;
 		ap->args.end = fuse_writepage_end;
