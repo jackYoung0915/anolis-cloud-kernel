5aa7d7619b06bc64ec7414385a98575aa5ef65db
Gu Zitao <guzitao@wxiat.com>
2024-08-27 09:27:14 +0800

    anolis: sw64: fix compile warnings for redefinitions in mman.h
    ANBZ: #4688
    
    A recent merged patch introduced redefined compile warnings in
    mman.h, so fix it.
    
    Fixes: 44d40e7a3975 ("anolis: anolis: sw64: fix compile errors for CONFIG_ADVISE_SYSCALLS=y")
    Signed-off-by: Gu Zitao <guzitao@wxiat.com>
    Reviewed-by: He Sheng <hesheng@wxiat.com>
    Signed-off-by: Gu Zitao <guzitao@wxiat.com>
    Reviewed-by: Min Li <gumi@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5175

--- arch/sw_64/include/uapi/asm/mman.h
+++ arch/sw_64/include/uapi/asm/mman.h
@@ -2,8 +2,6 @@
 #ifndef _UAPI_ASM_SW64_MMAN_H
 #define _UAPI_ASM_SW64_MMAN_H
 
-#include <asm-generic/mman-common.h>
-
 #define PROT_READ		0x1		/* page can be read */
 #define PROT_WRITE		0x2		/* page can be written */
 #define PROT_EXEC		0x4		/* page can be executed */
@@ -78,6 +76,9 @@
 #define MADV_DONTNEED_LOCKED	24	/* like DONTNEED, but drop locked pages too */
 
 #define MADV_COLLAPSE	25		/* Synchronous hugepage collapse */
+
+#define MADV_HWPOISON		100		/* poison a page for testing */
+#define MADV_SOFT_OFFLINE	101		/* soft offline page for testing */
 
 /* compatibility flags */
 #define MAP_FILE		0
