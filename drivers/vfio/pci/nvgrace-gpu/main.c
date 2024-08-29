e5e27d7acfc91a972e8756318d9ed2b897dd2c74
Qinyun Tan <qinyuntan@linux.alibaba.com>
2024-08-29 08:15:40 +0000

    vfio/nvgrace-egm: Introduce module to manage EGM
    ANBZ: #13559
    
    cherry-picked from: https://github.com/NVIDIA/NV-Kernels/commit/fbcd0ea6e905db71d56b83dd7cbf289b58e169ab
    
    The Extended GPU Memory (EGM) feature enables the GPU access to
    the system memory across sockets and nodes. In this mode, the
    physical memory can be allocated for GPU usage from anywhere
    in a multi-node system. The feature is being extended to
    virtualization.
    
    EGM when enabled in the virtualization stack, the host memory
    is partitioned into 2: One partition for the Host OS usage, and
    a second EGM region. The EGM region essentially becomes the
    system memory of the VM. The following figure shows the memory map
    in the virtualization environment.
    
    |---- Sysmem ----|                  |--- GPU mem ---|  VM Memory Map
    |                |                  |               |
    |                |                  |               |
    |------ EGM -----|--Host Mem----|   |--- GPU mem ---|  Host Memory Map
    
    The EGM region is not available to the host memory for its usage as it
    is not added to the kernel. Its base HPA and the length is communicated
    through the DSDT entries. A linear mapping between the VM IPA and system
    HPA is a requirement for EGM support. The EGM region is thus assigned to
    a VM by mapping the QEMU VMA to a linearly increasing HPA of the EGM
    region using remap_pfn_range().
    
    Introduce a new nvgrace-egm helper module to nvgrace-gpu to manage the
    EGM/VM region for the VM.
    
    nvgrace-egm module handles the following:
    1. Fetch the EGM memory properties (base HPA, length, proximity domain).
    2. Create a char device that can be used as memory-backend-file by Qemu
    for the VM and implement file operations. The char device is /dev/egmX,
    where X is the PXM node ID of the EGM being mapped fetched in 1.
    3. Zero the EGM memory on first device open().
    4. Map the QEMU VMA to the EGM region using remap_pfn_range.
    5. Cleaning up state and destroying the chardev on device unbind.
    
    Signed-off-by: Ankit Agrawal <ankita@nvidia.com>
    Signed-off-by: Matthew R. Ochs <mochs@nvidia.com>
    [Fixes conflicts]
    Signed-off-by: Qinyun Tan <qinyuntan@linux.alibaba.com>

--- drivers/vfio/pci/nvgrace-gpu/main.c
+++ drivers/vfio/pci/nvgrace-gpu/main.c
@@ -7,6 +7,7 @@
 #include <linux/vfio_pci_core.h>
 #include <linux/delay.h>
 #include <linux/jiffies.h>
+#include "egm.h"
 
 /*
  * The device memory usable to the workloads running in the VM is cached
@@ -58,7 +59,10 @@ struct nvgrace_gpu_pci_core_device {
 	/* Lock to control device memory kernel mapping */
 	struct mutex remap_lock;
 	bool has_mig_hw_bug;
+	int egm_node;
 };
+
+static bool egm_enabled;
 
 static void nvgrace_gpu_init_fake_bar_emu_regs(struct vfio_device *core_vdev)
 {
@@ -758,6 +762,13 @@ nvgrace_gpu_fetch_memory_property(struct pci_dev *pdev,
 }
 
 static int
+nvgrace_gpu_has_egm_property(struct pci_dev *pdev, u64 *pegmpxm)
+{
+	return device_property_read_u64(&pdev->dev, "nvidia,egm-pxm",
+					pegmpxm);
+}
+
+static int
 nvgrace_gpu_init_nvdev_struct(struct pci_dev *pdev,
 			      struct nvgrace_gpu_pci_core_device *nvdev,
 			      u64 memphys, u64 memlength)
@@ -935,6 +946,7 @@ static int nvgrace_gpu_probe(struct pci_dev *pdev,
 	const struct vfio_device_ops *ops = &nvgrace_gpu_pci_core_ops;
 	struct nvgrace_gpu_pci_core_device *nvdev;
 	u64 memphys, memlength;
+	u64 egmpxm;
 	int ret;
 
 	ret = nvgrace_gpu_wait_device_ready(pdev);
@@ -942,8 +954,13 @@ static int nvgrace_gpu_probe(struct pci_dev *pdev,
 		return ret;
 
 	ret = nvgrace_gpu_fetch_memory_property(pdev, &memphys, &memlength);
-	if (!ret)
+	if (!ret) {
 		ops = &nvgrace_gpu_pci_ops;
+
+		ret = nvgrace_gpu_has_egm_property(pdev, &egmpxm);
+		if (!ret)
+			egm_enabled = true;
+	}
 
 	nvdev = vfio_alloc_device(nvgrace_gpu_pci_core_device, core_device.vdev,
 				  &pdev->dev, ops);
@@ -963,6 +980,12 @@ static int nvgrace_gpu_probe(struct pci_dev *pdev,
 						    memphys, memlength);
 		if (ret)
 			goto out_put_vdev;
+
+		if (egm_enabled) {
+			register_egm_node(pdev);
+			nvdev->egm_node = egmpxm;
+		}
+
 	}
 
 	ret = vfio_pci_core_register_device(&nvdev->core_device);
@@ -979,6 +1002,12 @@ out_put_vdev:
 static void nvgrace_gpu_remove(struct pci_dev *pdev)
 {
 	struct vfio_pci_core_device *core_device = dev_get_drvdata(&pdev->dev);
+	struct nvgrace_gpu_pci_core_device *nvdev =
+		container_of(core_device, struct nvgrace_gpu_pci_core_device,
+			     core_device);
+
+	if (egm_enabled)
+		unregister_egm_node(nvdev->egm_node);
 
 	vfio_pci_core_unregister_device(core_device);
 	vfio_put_device(&core_device->vdev);
