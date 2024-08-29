b2c6ec78d35cad3bd08471d4ec7bd3526cf6e24a
Tong Tiangen <tongtiangen@huawei.com>
2024-08-29 10:25:51 +0800

    mm: add memory reclaim notifier
    ANCK ID: #IJSBBS
    
    commit cb361f0efc59c9c70c50e004631c9c692f51f67b openEuler.
    
    In memory reclaim scenario, such as direct reclaim/kswapd, if the expected
    pages cannot be reclaimed, some modules may need to be notified, as an
    option, they can get additional memory from the managed memory region.
    
    This add a notifier call for memory reclaim which is enabled by
    CONFIG_RECLAIM_NOTIFY.
    
    Note that this notifier could used in all memory reclaim scenarios,
    current only direct reclaim and kswapd scenarios been supported, and
    callbacks registered to the notification chain cannot be blocked.
    
    Signed-off-by: Tong Tiangen <tongtiangen@huawei.com>
    (cherry picked from commit cb361f0efc59c9c70c50e004631c9c692f51f67b)
    Conflicts:
    	arch/arm64/configs/openeuler_defconfig
    	mm/Kconfig
    	mm/Makefile
    [Context conflicts.]
    Signed-off-by: Liu Jian <liujian56@huawei.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -4171,6 +4171,11 @@ __alloc_pages_direct_reclaim(gfp_t gfp_mask, unsigned int order,
 
 	psi_memstall_enter(&pflags);
 	*did_some_progress = __perform_reclaim(gfp_mask, order, ac);
+
+	if (unlikely(*did_some_progress < min(1UL << order, SWAP_CLUSTER_MAX)))
+		*did_some_progress += do_reclaim_notify(RR_DIRECT_RECLAIM,
+							(const void *)ac);
+
 	if (unlikely(!(*did_some_progress)))
 		goto out;
 
