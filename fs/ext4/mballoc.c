bfa0eaa30d551b7efb354b71ebda91830cf55425
Ojaswin Mujoo <ojaswin@linux.ibm.com>
2024-08-30 12:50:58 +0530

    ext4: convert EXT4_B2C(sbi->s_stripe) users to EXT4_NUM_B2C
    ANBZ: #27259
    
    commit ff2beee206d23f49d022650122f81285849033e4 upstream.
    
    Although we have checks to make sure s_stripe is a multiple of cluster
    size, in case we accidentally end up with a scenario where this is not
    the case, use EXT4_NUM_B2C() so that we don't end up with unexpected
    cases where EXT4_B2C(stripe) becomes 0.
    
    Also make the is_stripe_aligned check in regular_allocator a bit more
    robust while we are at it. This should ideally have no functional change
    unless we have a bug somewhere causing (stripe % cluster_size != 0)
    
    Reviewed-by: Kemeng Shi <shikemeng@huaweicloud.com>
    Signed-off-by: Ojaswin Mujoo <ojaswin@linux.ibm.com>
    Reviewed-by: Ritesh Harjani (IBM) <ritesh.list@gmail.com>
    Link: https://patch.msgid.link/e0c0a3b58a40935a1361f668851d041575861411.1725002410.git.ojaswin@linux.ibm.com
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    Signed-off-by: Joseph Qi <joseph.qi@linux.alibaba.com>
    Acked-by: Gao Xiang <hsiangkao@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6040

--- fs/ext4/mballoc.c
+++ fs/ext4/mballoc.c
@@ -2359,7 +2359,7 @@ int ext4_mb_find_by_goal(struct ext4_allocation_context *ac,
 	ex.fe_logical = 0xDEADFA11; /* debug value */
 
 	if (max >= ac->ac_g_ex.fe_len &&
-	    ac->ac_g_ex.fe_len == EXT4_B2C(sbi, sbi->s_stripe)) {
+	    ac->ac_g_ex.fe_len == EXT4_NUM_B2C(sbi, sbi->s_stripe)) {
 		ext4_fsblk_t start;
 
 		start = ext4_grp_offs_to_block(ac->ac_sb, &ex);
@@ -2556,7 +2556,7 @@ void ext4_mb_scan_aligned(struct ext4_allocation_context *ac,
 	do_div(a, sbi->s_stripe);
 	i = (a * sbi->s_stripe) - first_group_block;
 
-	stripe = EXT4_B2C(sbi, sbi->s_stripe);
+	stripe = EXT4_NUM_B2C(sbi, sbi->s_stripe);
 	i = EXT4_B2C(sbi, i);
 	while (i < EXT4_CLUSTERS_PER_GROUP(sb)) {
 		if (!mb_test_bit(i, bitmap)) {
@@ -2941,9 +2941,11 @@ repeat:
 			if (cr == CR_POWER2_ALIGNED)
 				ext4_mb_simple_scan_group(ac, &e4b);
 			else {
-				bool is_stripe_aligned = sbi->s_stripe &&
+				bool is_stripe_aligned =
+					(sbi->s_stripe >=
+					 sbi->s_cluster_ratio) &&
 					!(ac->ac_g_ex.fe_len %
-					  EXT4_B2C(sbi, sbi->s_stripe));
+					  EXT4_NUM_B2C(sbi, sbi->s_stripe));
 
 				if ((cr == CR_GOAL_LEN_FAST ||
 				     cr == CR_BEST_AVAIL_LEN) &&
@@ -3726,7 +3728,7 @@ int ext4_mb_init(struct super_block *sb)
 	 */
 	if (sbi->s_stripe > 1) {
 		sbi->s_mb_group_prealloc = roundup(
-			sbi->s_mb_group_prealloc, EXT4_B2C(sbi, sbi->s_stripe));
+			sbi->s_mb_group_prealloc, EXT4_NUM_B2C(sbi, sbi->s_stripe));
 	}
 
 	sbi->s_mb_nr_global_goals = umin(num_possible_cpus(),
