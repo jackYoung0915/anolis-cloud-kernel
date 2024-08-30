7c04d786377d975857a7c08137f36e2a28d96da4
Gao Xiang <hsiangkao@linux.alibaba.com>
2024-08-30 11:28:37 +0800

    erofs: add file-backed mount support
    ANBZ: #11101
    
    commit fb176750266a3d7f42ebdcf28e8ba40350b27847 upstream.
    
    It actually has been around for years: For containers and other sandbox
    use cases, there will be thousands (and even more) of authenticated
    (sub)images running on the same host, unlike OS images.
    
    Of course, all scenarios can use the same EROFS on-disk format, but
    bdev-backed mounts just work well for OS images since golden data is
    dumped into real block devices.  However, it's somewhat hard for
    container runtimes to manage and isolate so many unnecessary virtual
    block devices safely and efficiently [1]: they just look like a burden
    to orchestrators and file-backed mounts are preferred indeed.  There
    were already enough attempts such as Incremental FS, the original
    ComposeFS and PuzzleFS acting in the same way for immutable fses.  As
    for current EROFS users, ComposeFS, containerd and Android APEXs will
    be directly benefited from it.
    
    On the other hand, previous experimental feature "erofs over fscache"
    was once also intended to provide a similar solution (inspired by
    Incremental FS discussion [2]), but the following facts show file-backed
    mounts will be a better approach:
     - Fscache infrastructure has recently been moved into new Netfslib
       which is an unexpected dependency to EROFS really, although it
       originally claims "it could be used for caching other things such as
       ISO9660 filesystems too." [3]
    
     - It takes an unexpectedly long time to upstream Fscache/Cachefiles
       enhancements.  For example, the failover feature took more than
       one year, and the deamonless feature is still far behind now;
    
     - Ongoing HSM "fanotify pre-content hooks" [4] together with this will
       perfectly supersede "erofs over fscache" in a simpler way since
       developers (mainly containerd folks) could leverage their existing
       caching mechanism entirely in userspace instead of strictly following
       the predefined in-kernel caching tree hierarchy.
    
    After "fanotify pre-content hooks" lands upstream to provide the same
    functionality, "erofs over fscache" will be removed then (as an EROFS
    internal improvement and EROFS will not have to bother with on-demand
    fetching and/or caching improvements anymore.)
    
    [1] https://github.com/containers/storage/pull/2039
    [2] https://lore.kernel.org/r/CAOQ4uxjbVxnubaPjVaGYiSwoGDTdpWbB=w_AeM6YM=zVixsUfQ@mail.gmail.com
    [3] https://docs.kernel.org/filesystems/caching/fscache.html
    [4] https://lore.kernel.org/r/cover.1723670362.git.josef@toxicpanda.com
    
    Closes: https://github.com/containers/composefs/issues/144
    Reviewed-by: Sandeep Dhavale <dhavale@google.com>
    Reviewed-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Gao Xiang <hsiangkao@linux.alibaba.com>
    Link: https://lore.kernel.org/r/20240830032840.3783206-1-hsiangkao@linux.alibaba.com
    
    Conflicts:
    1. fs/erofs/data.c
      1) erofs_fill_from_devinfo()
    
    2. fs/erofs/super.c
      1) erofs_init_device()
    
    Resolution:
    1. fs/erofs/data.c
      1) erofs_fill_from_devinfo():
      Reimplement it according to the intent of this upstream patch,
      as this upstream patch depends on other third-party patches
      (e.g., file_bdev(), etc.).
    
    2. fs/erofs/super.c
      1) erofs_init_device():
      Reimplement it according to the intent of this upstream patch,
      as the upstream patch depends on other third-party patches
      (e.g., file_bdev(), etc.).
    
    Signed-off-by: Hongzhen Luo <hongzhen@linux.alibaba.com>
    Acked-by: Joseph Qi <joseph.qi@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/4224

--- fs/erofs/data.c
+++ fs/erofs/data.c
@@ -59,8 +59,12 @@ void *erofs_bread(struct erofs_buf *buf, erofs_off_t offset,
 
 void erofs_init_metabuf(struct erofs_buf *buf, struct super_block *sb)
 {
-	if (erofs_is_fscache_mode(sb))
-		buf->mapping = EROFS_SB(sb)->s_fscache->inode->i_mapping;
+	struct erofs_sb_info *sbi = EROFS_SB(sb);
+
+	if (erofs_is_fileio_mode(sbi))
+		buf->mapping = file_inode(sbi->fdev)->i_mapping;
+	else if (erofs_is_fscache_mode(sb))
+		buf->mapping = sbi->s_fscache->inode->i_mapping;
 	else
 		buf->mapping = sb->s_bdev->bd_inode->i_mapping;
 }
@@ -189,10 +193,25 @@ out:
 	return err;
 }
 
+static void erofs_fill_from_devinfo(struct super_block *sb,
+				    struct erofs_map_dev *map,
+				    struct erofs_device_info *dif)
+{
+	map->m_bdev = dif->bdev_handle ? dif->bdev_handle->bdev : NULL;
+	if (!dif->file && !map->m_bdev) {
+		erofs_err(sb, "invalid device handle for path %s", dif->path);
+		DBG_BUGON(1);
+	}
+	map->m_daxdev = dif->dax_dev;
+	map->m_dax_part_off = dif->dax_part_off;
+	map->m_fscache = dif->fscache;
+}
+
 int erofs_map_dev(struct super_block *sb, struct erofs_map_dev *map)
 {
 	struct erofs_dev_context *devs = EROFS_SB(sb)->devs;
 	struct erofs_device_info *dif;
+	erofs_off_t startoff, length;
 	int id;
 
 	map->m_bdev = sb->s_bdev;
@@ -212,29 +231,20 @@ int erofs_map_dev(struct super_block *sb, struct erofs_map_dev *map)
 			up_read(&devs->rwsem);
 			return 0;
 		}
-		map->m_bdev = dif->bdev_handle ? dif->bdev_handle->bdev : NULL;
-		map->m_daxdev = dif->dax_dev;
-		map->m_dax_part_off = dif->dax_part_off;
-		map->m_fscache = dif->fscache;
+		erofs_fill_from_devinfo(sb, map, dif);
 		up_read(&devs->rwsem);
 	} else if (devs->extra_devices && !devs->flatdev) {
 		down_read(&devs->rwsem);
 		idr_for_each_entry(&devs->tree, dif, id) {
-			erofs_off_t startoff, length;
-
 			if (!dif->mapped_blkaddr)
 				continue;
+
 			startoff = erofs_pos(sb, dif->mapped_blkaddr);
 			length = erofs_pos(sb, dif->blocks);
-
 			if (map->m_pa >= startoff &&
 			    map->m_pa < startoff + length) {
 				map->m_pa -= startoff;
-				map->m_bdev = dif->bdev_handle ?
-					      dif->bdev_handle->bdev : NULL;
-				map->m_daxdev = dif->dax_dev;
-				map->m_dax_part_off = dif->dax_part_off;
-				map->m_fscache = dif->fscache;
+				erofs_fill_from_devinfo(sb, map, dif);
 				break;
 			}
 		}
