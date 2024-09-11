b3416886ab7d17d03b7e6501cb816daa760b0b89
Madhavan T. Venkataraman <madvenka@linux.microsoft.com>
2024-09-11 05:40:59 +0000

    objtool: Reorganize Unwind hint code
    ANBZ: #9262
    
    cherry-picked from https://lore.kernel.org/lkml/20230202074036.507249-1-madvenka@linux.microsoft.com/
    
    Unwind hint macros and struct unwind_hint are arch-specific. Move them
    into the arch-specific file asm/unwind_hints.h. But the unwind hint
    types are generic. Retain them in linux/objtool.h.
    
    Unwind hints can be used with static stack validation as well as other
    forms of validation such as dynamic FP validation. Move the function
    read_unwind_hints() from check.c to a new file unwind_hints.c so that
    it can be shared across validation schemes.
    
    Signed-off-by: Madhavan T. Venkataraman <madvenka@linux.microsoft.com>
    Signed-off-by: Wei Chen <wei.chen@arm.com>
    Acked-by: ydzhang <ydzhang@linux.alibaba.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/3299

--- tools/objtool/include/objtool/insn.h
+++ tools/objtool/include/objtool/insn.h
@@ -109,6 +109,7 @@ bool is_first_func_insn(struct objtool_file *file,
 			struct instruction *insn, struct symbol *sym);
 
 int decode_instructions(struct objtool_file *file);
+int read_unwind_hints(struct objtool_file *file);
 
 #define sec_for_each_insn(file, _sec, insn)				\
 	for (insn = find_insn(file, _sec, 0);				\
