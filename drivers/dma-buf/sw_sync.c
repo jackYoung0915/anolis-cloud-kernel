6c438d9a0f6a287dc55fe3a9697ce0f76a36f3ee
Christian König <christian.koenig@amd.com>
2024-09-18 08:16:57 +0200

    dma-buf/dma-fence: remove unnecessary callbacks
    ANBZ: #31784
    
    commit 2ce07fea3cc8b866f7955a7ce1d62b0cc1f74819 upstream.
    
    The fence_value_str and timeline_value_str callbacks were just an
    unnecessary abstraction in the SW sync implementation.
    
    The only caller of those callbacks already knew that the fence in
    questions is a timeline_fence. So print the values directly instead
    of using a redirection.
    
    Additional to that remove the implementations from virtgpu and vgem.
    As far as I can see those were never used in the first place.
    
    Signed-off-by: Christian König <christian.koenig@amd.com>
    Reviewed-by: Simona Vetter <simona.vetter@ffwll.ch>
    Link: https://patchwork.freedesktop.org/patch/msgid/20250211163109.12200-3-christian.koenig@amd.com
    [Ruidong:
      Conflicts:
        drivers/dma-buf/sw_sync.c
        (context mismatch: set_deadline not yet present in this branch;
         drop _value_str callbacks and add set_deadline manually)
    ]
    Signed-off-by: Ruidong Tian <tianruidong@linux.alibaba.com>
    Reviewed-by: Shuai Xue <xueshuai@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6649

--- drivers/dma-buf/sw_sync.c
+++ drivers/dma-buf/sw_sync.c
@@ -157,28 +157,12 @@ static bool timeline_fence_enable_signaling(struct dma_fence *fence)
 	return true;
 }
 
-static void timeline_fence_value_str(struct dma_fence *fence,
-				    char *str, int size)
-{
-	snprintf(str, size, "%lld", fence->seqno);
-}
-
-static void timeline_fence_timeline_value_str(struct dma_fence *fence,
-					     char *str, int size)
-{
-	struct sync_timeline *parent = dma_fence_parent(fence);
-
-	snprintf(str, size, "%d", parent->value);
-}
-
 static const struct dma_fence_ops timeline_fence_ops = {
 	.get_driver_name = timeline_fence_get_driver_name,
 	.get_timeline_name = timeline_fence_get_timeline_name,
 	.enable_signaling = timeline_fence_enable_signaling,
 	.signaled = timeline_fence_signaled,
 	.release = timeline_fence_release,
-	.fence_value_str = timeline_fence_value_str,
-	.timeline_value_str = timeline_fence_timeline_value_str,
 };
 
 /**
