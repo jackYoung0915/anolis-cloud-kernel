116fa8877c0513b83a6a62963f3e30287a415cc5
Wen Gu <guwen@linux.alibaba.com>
2024-09-19 18:40:41 +0800

    anolis: net/smc: add netlink interface for dump_ndev operations
    ANBZ: #11098
    
    In smc, we multiplex pcap to capture smc data by forwarding udp
    packets containing target data to the dummy device (dump_ndev).
    So here introduces the netlink interfaces for dump_ndev operations,
    providing the functions of setting, resetting, and getting smc
    dump_ndev. Setting and resetting dump_ndev means enabling and
    disabling smc dump.
    
    Signed-off-by: Wen Gu <guwen@linux.alibaba.com>
    Reviewed-by: Guangguan Wang <guangguan.wang@linux.alibaba.com>
    Reviewed-by: D. Wythe <alibuda@linux.alibaba.com>
    Reviewed-by: Tony Lu <tonylu@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/3876

--- include/net/netns/smc.h
+++ include/net/netns/smc.h
@@ -9,6 +9,7 @@
 
 struct smc_stats_rsn;
 struct smc_stats;
+struct smc_dump_ctx;
 struct netns_smc {
 	/* per cpu counters for SMC */
 	struct smc_stats __percpu	*smc_stats;
@@ -32,8 +33,8 @@ struct netns_smc {
 	CK_KABI_USE_SPLIT(2, unsigned int sysctl_autosplit_size)
 	CK_KABI_USE(3, 4, 5, long sysctl_mem[3])
 	CK_KABI_USE(6, atomic_long_t memory_allocated)
+	CK_KABI_USE(7, struct smc_dump_ctx *dump_ctx)
 
-	CK_KABI_RESERVE(7)
 	CK_KABI_RESERVE(8)
 	CK_KABI_RESERVE(9)
 	CK_KABI_RESERVE(10)
