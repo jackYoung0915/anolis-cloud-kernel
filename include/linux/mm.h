e70716f07a4d094fa346151dfedefc1e6716190d
Qinyun Tan <qinyuntan@linux.alibaba.com>
2024-09-19 13:31:14 +0800

    anolis: mm: introduce vma flag VM_PGOFF_IS_PFN
    ANBZ: #10842
    
    Introduce a new vma flag 'VM_PGOFF_IS_PFN', which means
    vma->vm_pgoff == pfn. This allows us to directly obtain pfn
    through vma->vm_pgoff. No Functional Change.
    
    Signed-off-by: Qinyun Tan <qinyuntan@linux.alibaba.com>
    Reviewed-by: Guanghui Feng <guanghuifeng@linux.alibaba.com>
    Reviewed-by: Xunlei Pang <xlpang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/3834
    Link: https://gitee.com/anolis/cloud-kernel/pulls/4526

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -317,6 +317,12 @@ extern unsigned int kobjsize(const void *objp);
 #define VM_NOHUGEPAGE	0x40000000	/* MADV_NOHUGEPAGE marked this vma */
 #define VM_MERGEABLE	0x80000000	/* KSM may merge identical pages */
 
+#ifdef CONFIG_ARCH_USES_HIGH_VMA_FLAGS	/* vma->vm_pgoff == pfn */
+    #define VM_PGOFF_IS_PFN   BIT(62)
+#else
+    #define VM_PGOFF_IS_PFN   VM_NONE
+#endif
+
 #ifdef CONFIG_ARCH_USES_HIGH_VMA_FLAGS
 #define VM_HIGH_ARCH_BIT_0	32	/* bit only usable on 64-bit architectures */
 #define VM_HIGH_ARCH_BIT_1	33	/* bit only usable on 64-bit architectures */
