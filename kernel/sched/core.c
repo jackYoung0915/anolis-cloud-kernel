622dc6e70609c3e9900bae4c170e81cec6c36329
Cruz Zhao <CruzZhao@linux.alibaba.com>
2024-09-24 20:11:14 +0800

    anolis: sched: modify the interface of attach/detach_tg_to/from_group_balancer_sched_domain
    ANBZ: #8765
    
    We add a parameter to
    attach/detach_tg_to/from_group_balancer_sched_domain(), marking that
    whether this attach/detach is enable/disable. If enable/disable, we
    will set tg->gb_sd to target/NULL, and set all the gb_sd pointer of
    all the descedents to target/NULL. If not, we will not change it.
    
    By the way, we also changed the logic of tg_set_specs_ratio(). Once
    the specs changes, we will check whether the current gb_sd is still
    satisfying, if so, keep it, and if not, upper the task group to a
    satisfying ancestor.
    
    This modification aims to reduce overhead, and make the soft cpus of
    task group stay convergent during load balance and specs change.
    
    Signed-off-by: Cruz Zhao <CruzZhao@linux.alibaba.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -8534,7 +8534,6 @@ void __init sched_init(void)
 	root_task_group.group_balancer = 0;
 	root_task_group.soft_cpus_version = 0;
 	root_task_group.gb_sd = NULL;
-	root_task_group.prev_gb_sd = NULL;
 #endif
 #ifdef CONFIG_CPUMASK_OFFSTACK
 	for_each_possible_cpu(i) {
@@ -8982,7 +8981,6 @@ struct task_group *sched_create_group(struct task_group *parent)
 	tg->group_balancer = 0;
 	tg->soft_cpus_version = 0;
 	tg->gb_sd = NULL;
-	tg->prev_gb_sd = NULL;
 #endif
 	return tg;
 
@@ -10085,16 +10083,11 @@ static int cpu_group_balancer_write_u64(struct cgroup_subsys_state *css,
 		retval = validate_group_balancer(tg);
 		if (retval)
 			goto out;
-		retval = attach_tg_to_group_balancer_sched_domain(tg);
+		retval = attach_tg_to_group_balancer_sched_domain(tg, NULL, true);
 		if (retval)
 			goto out;
 	} else {
-		/*
-		 * As gb_sd may be freed by user, set tg->prev_gb_sd NULL to prevent
-		 * use after free.
-		 */
-		tg->prev_gb_sd = NULL;
-		detach_tg_from_group_balancer_sched_domain(tg);
+		detach_tg_from_group_balancer_sched_domain(tg, true);
 	}
 	tg->group_balancer = new;
 out:
