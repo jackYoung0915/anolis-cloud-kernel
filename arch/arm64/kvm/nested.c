4fb26f1ab988e7917d61dc01b3e49e2e8f254d20
Oliver Upton <oliver.upton@linux.dev>
2024-10-07 23:30:27 +0000

    KVM: arm64: nv: Punt stage-2 recycling to a vCPU request
    ANBZ: #31782
    
    commit c268f204f7c5784e84583c1c44d427bac09f517a upstream.
    
    Currently, when a nested MMU is repurposed for some other MMU context,
    KVM unmaps everything during vcpu_load() while holding the MMU lock for
    write. This is quite a performance bottleneck for large nested VMs, as
    all vCPU scheduling will spin until the unmap completes.
    
    Start punting the MMU cleanup to a vCPU request, where it is then
    possible to periodically release the MMU lock and CPU in the presence of
    contention.
    
    Ensure that no vCPU winds up using a stale MMU by tracking the pending
    unmap on the S2 MMU itself and requesting an unmap on every vCPU that
    finds it.
    
    Signed-off-by: Oliver Upton <oliver.upton@linux.dev>
    Link: https://lore.kernel.org/r/20241007233028.2236133-4-oliver.upton@linux.dev
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Signed-off-by: Jia He <justin.he@arm.com>

--- arch/arm64/kvm/nested.c
+++ arch/arm64/kvm/nested.c
@@ -635,9 +635,9 @@ static struct kvm_s2_mmu *get_s2_mmu_nested(struct kvm_vcpu *vcpu)
 	/* Set the scene for the next search */
 	kvm->arch.nested_mmus_next = (i + 1) % kvm->arch.nested_mmus_size;
 
-	/* Clear the old state */
+	/* Make sure we don't forget to do the laundry */
 	if (kvm_s2_mmu_valid(s2_mmu))
-		kvm_stage2_unmap_range(s2_mmu, 0, kvm_phys_size(s2_mmu), false);
+		s2_mmu->pending_unmap = true;
 
 	/*
 	 * The virtual VMID (modulo CnP) will be used as a key when matching
@@ -653,6 +653,16 @@ static struct kvm_s2_mmu *get_s2_mmu_nested(struct kvm_vcpu *vcpu)
 
 out:
 	atomic_inc(&s2_mmu->refcnt);
+
+	/*
+	 * Set the vCPU request to perform an unmap, even if the pending unmap
+	 * originates from another vCPU. This guarantees that the MMU has been
+	 * completely unmapped before any vCPU actually uses it, and allows
+	 * multiple vCPUs to lend a hand with completing the unmap.
+	 */
+	if (s2_mmu->pending_unmap)
+		kvm_make_request(KVM_REQ_NESTED_S2_UNMAP, vcpu);
+
 	return s2_mmu;
 }
 
@@ -1228,4 +1238,18 @@ out:
 	mutex_unlock(&kvm->arch.config_lock);
 
 	return ret;
+}
+
+void check_nested_vcpu_requests(struct kvm_vcpu *vcpu)
+{
+	if (kvm_check_request(KVM_REQ_NESTED_S2_UNMAP, vcpu)) {
+		struct kvm_s2_mmu *mmu = vcpu->arch.hw_mmu;
+
+		write_lock(&vcpu->kvm->mmu_lock);
+		if (mmu->pending_unmap) {
+			kvm_stage2_unmap_range(mmu, 0, kvm_phys_size(mmu), true);
+			mmu->pending_unmap = false;
+		}
+		write_unlock(&vcpu->kvm->mmu_lock);
+	}
 }
