b4d3054dddd1545837d4cb489931459e7e776e69
Yihang Li <liyihang9@huawei.com>
2024-10-08 10:18:12 +0800

    scsi: hisi_sas: Add firmware information check
    ANBZ: #11194
    
    For security purposes, after information is obtained through the FW,
    check information to ensure data correctness.
    
     - In v1 and v2 hw, the maximum number of PHYs is 9, while in v3 it is 8.
    
     - In v2 and v3 hw, the maximum number of hardware queues is 16, while
       in v1 it is 32.
    
    Also add some debug logs for failure.
    
    Signed-off-by: Yihang Li <liyihang9@huawei.com>
    Link: https://lore.kernel.org/r/20241008021822.2617339-4-liyihang9@huawei.com
    Reviewed-by: Xiang Chen <chenxiang66@hisilicon.com>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    Signed-off-by: Slim6882 <yangjunshuo@huawei.com>
    Signed-off-by: Min Li <gumi@linux.alibaba.com>

--- drivers/scsi/hisi_sas/hisi_sas_v3_hw.c
+++ drivers/scsi/hisi_sas/hisi_sas_v3_hw.c
@@ -3327,6 +3327,23 @@ static const struct hisi_sas_hw hisi_sas_v3_hw = {
 	.debugfs_snapshot_regs = debugfs_snapshot_regs_v3_hw,
 };
 
+static int check_fw_info_v3_hw(struct hisi_hba *hisi_hba)
+{
+	struct device *dev = hisi_hba->dev;
+
+	if (hisi_hba->n_phy < 0 || hisi_hba->n_phy > 8) {
+		dev_err(dev, "invalid phy number from FW\n");
+		return -EINVAL;
+	}
+
+	if (hisi_hba->queue_count < 0 || hisi_hba->queue_count > 16) {
+		dev_err(dev, "invalid queue count from FW\n");
+		return -EINVAL;
+	}
+
+	return 0;
+}
+
 static struct Scsi_Host *
 hisi_sas_shost_alloc_pci(struct pci_dev *pdev)
 {
@@ -3357,13 +3374,15 @@ hisi_sas_shost_alloc_pci(struct pci_dev *pdev)
 	if (hisi_sas_get_fw_info(hisi_hba) < 0)
 		goto err_out;
 
+	if (check_fw_info_v3_hw(hisi_hba) < 0)
+		goto err_out;
+
 	if (hisi_sas_alloc(hisi_hba)) {
 		hisi_sas_free(hisi_hba);
 		goto err_out;
 	}
 
 	return shost;
-err_out:
 	scsi_host_put(shost);
 	dev_err(dev, "shost alloc failed\n");
 	return NULL;
