f2eb41e4b2d239508cd8d49b20b8d0705a6c6358
Baolin Wang <baolin.wang@linux.alibaba.com>
2024-10-14 18:24:44 +0800

    mm: khugepaged: fix the incorrect statistics when collapsing large file folios
    ANBZ: #9728
    
    commit d60fcaf00d752c52fdf566d4184e6d04d9d08879 upstream
    
    Khugepaged already supports collapsing file large folios (including shmem
    mTHP) by commit 7de856ffd007 ("mm: khugepaged: support shmem mTHP
    collapse"), and the control parameters in khugepaged:
    'khugepaged_max_ptes_swap' and 'khugepaged_max_ptes_none', still compare
    based on PTE granularity to determine whether a file collapse is needed.
    However, the statistics for 'present' and 'swap' in
    hpage_collapse_scan_file() do not take into account the large folios,
    which may lead to incorrect judgments regarding the
    khugepaged_max_ptes_swap/none parameters, resulting in unnecessary file
    collapses.
    
    To fix this issue, take into account the large folios' statistics for
    'present' and 'swap' variables in the hpage_collapse_scan_file().
    
    Link: https://lkml.kernel.org/r/c76305d96d12d030a1a346b50503d148364246d2.1728901391.git.baolin.wang@linux.alibaba.com
    Fixes: 7de856ffd007 ("mm: khugepaged: support shmem mTHP collapse")
    Signed-off-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Acked-by: David Hildenbrand <david@redhat.com>
    Reviewed-by: Barry Song <baohua@kernel.org>
    Reviewed-by: Zi Yan <ziy@nvidia.com>
    Reviewed-by: Yang Shi <shy828301@gmail.com>
    Cc: Hugh Dickins <hughd@google.com>
    Cc: Matthew Wilcox <willy@infradead.org>
    Cc: Ryan Roberts <ryan.roberts@arm.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Reviewed-by: Xu Yu <xuyu@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/4128

--- mm/khugepaged.c
+++ mm/khugepaged.c
@@ -2239,7 +2239,7 @@ static int hpage_collapse_scan_file(struct mm_struct *mm, unsigned long addr,
 			continue;
 
 		if (xa_is_value(folio)) {
-			++swap;
+			swap += 1 << xas_get_order(&xas);
 			if (cc->is_khugepaged &&
 			    swap > khugepaged_max_ptes_swap) {
 				result = SCAN_EXCEED_SWAP_PTE;
@@ -2286,7 +2286,7 @@ static int hpage_collapse_scan_file(struct mm_struct *mm, unsigned long addr,
 		 * is just too costly...
 		 */
 
-		present++;
+		present += folio_nr_pages(folio);
 
 		if (need_resched()) {
 			xas_pause(&xas);
