55a4f476593742adc89c0efe10023fd077c30c0f
Samuel Holland <samuel.holland@sifive.com>
2024-10-16 13:27:46 -0700

    riscv: Add support for the tagged address ABI
    ANBZ: #23232
    
    commit 2e1743085887ba3f553c2bb472a75a3ff744b242 upstream.
    
    When pointer masking is enabled for userspace, the kernel can accept
    tagged pointers as arguments to some system calls. Allow this by
    untagging the pointers in access_ok() and the uaccess routines. The
    uaccess routines must peform untagging in software because U-mode and
    S-mode have entirely separate pointer masking configurations. In fact,
    hardware may not even implement pointer masking for S-mode.
    
    Since the number of tag bits is variable, untagged_addr_remote() needs
    to know what PMLEN to use for the remote mm. Therefore, the pointer
    masking mode must be the same for all threads sharing an mm. Enforce
    this with a lock flag in the mm context, as x86 does for LAM. The flag
    gets reset in init_new_context() during fork(), as the new mm is no
    longer multithreaded.
    
    Reviewed-by: Charlie Jenkins <charlie@rivosinc.com>
    Tested-by: Charlie Jenkins <charlie@rivosinc.com>
    Signed-off-by: Samuel Holland <samuel.holland@sifive.com>
    Link: https://lore.kernel.org/r/20241016202814.4061541-6-samuel.holland@sifive.com
    Signed-off-by: Palmer Dabbelt <palmer@rivosinc.com>
    Signed-off-by: luohaiyang <luo.haiyang@zte.com.cn>
    Reviewed-by: Fangyu <Yu fangyu.yu@linux.alibaba.com>
    Reviewed-by: Ruidong Tian <tianruidong@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5568

--- arch/riscv/include/asm/mmu.h
+++ arch/riscv/include/asm/mmu.h
@@ -24,7 +24,14 @@ typedef struct {
 	unsigned long exec_fdpic_loadmap;
 	unsigned long interp_fdpic_loadmap;
 #endif
+	unsigned long flags;
+#ifdef CONFIG_RISCV_ISA_SUPM
+	u8 pmlen;
+#endif
 } mm_context_t;
+
+/* Lock the pointer masking mode because this mm is multithreaded */
+#define MM_CONTEXT_LOCK_PMLEN	0
 
 void __init create_pgd_mapping(pgd_t *pgdp, uintptr_t va, phys_addr_t pa,
 			       phys_addr_t sz, pgprot_t prot);
