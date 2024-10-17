8ccdf5a79332d21fdfa49c6871d2b372cfcd0ad7
Jesse Taube <jesse@rivosinc.com>
2024-10-17 12:00:18 -0700

    RISC-V: Check scalar unaligned access on all CPUs
    ANBZ: #32730
    
    commit 8d20a739f17a2de9e269db72330f5655d6545dd4 upstream.
    
    Originally, the check_unaligned_access_emulated_all_cpus function
    only checked the boot hart. This fixes the function to check all
    harts.
    
    Fixes: 71c54b3d169d ("riscv: report misaligned accesses emulation to hwprobe")
    Signed-off-by: Jesse Taube <jesse@rivosinc.com>
    Reviewed-by: Charlie Jenkins <charlie@rivosinc.com>
    Reviewed-by: Evan Green <evan@rivosinc.com>
    Cc: stable@vger.kernel.org
    Link: https://lore.kernel.org/r/20241017-jesse_unaligned_vector-v10-1-5b33500160f8@rivosinc.com
    Signed-off-by: Palmer Dabbelt <palmer@rivosinc.com>
    [Ruidong:
    Conflicts:
      resolve: arch/riscv/include/asm/cpufeature.h, arch/riscv/kernel/traps_misaligned.c
      reason:
        Missing dependency patch 'RISC-V: hwprobe: Add SCALAR to misaligned
      perf defines', modified patch content to adapt without including the
      dependency
    ]
    Signed-off-by: Ruidong Tian <tianruidong@linux.alibaba.com>

--- arch/riscv/include/asm/cpufeature.h
+++ arch/riscv/include/asm/cpufeature.h
@@ -11,6 +11,7 @@
 #include <linux/kconfig.h>
 #include <linux/percpu-defs.h>
 #include <linux/threads.h>
+#include <linux/workqueue.h>
 #include <asm/hwcap.h>
 #include <asm/cpufeature-macros.h>
 
@@ -86,6 +87,7 @@ static __always_inline bool riscv_cpu_has_extension_unlikely(int cpu, const unsi
 
 #if defined(CONFIG_RISCV_MISALIGNED)
 bool check_unaligned_access_emulated_all_cpus(void);
+void check_unaligned_access_emulated(struct work_struct *work __always_unused);
 void unaligned_emulation_finish(void);
 bool unaligned_ctl_available(void);
 DECLARE_PER_CPU(long, misaligned_access_speed);
