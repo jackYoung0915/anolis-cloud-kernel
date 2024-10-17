09e6722a3a1525b68defd8a6aec51fcf6dc21bf1
hanliyang <hanliyang@hygon.cn>
2024-10-17 18:04:51 +0800

    anolis: crypto: ccp: Move Hygon CSV api version interface to hygon/csv-dev.c
    ANBZ: #11419
    
    To reduce the code intrusion, we move the code about Hygon CSV API
    version from .../sev-dev.c to .../hygon/csv-dev.c.
    
    Signed-off-by: hanliyang <hanliyang@hygon.cn>
    Reviewed-by: Xingrui Yi <yixingrui@linux.alibaba.com>
    Reviewed-by: Artie Ding <artie.ding@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/4009

--- drivers/crypto/ccp/hygon/csv-dev.h
+++ drivers/crypto/ccp/hygon/csv-dev.h
@@ -12,7 +12,15 @@
 
 #include <linux/psp-sev.h>
 
+extern u32 hygon_csv_build;
+
+void csv_update_api_version(struct sev_user_data_status *status);
 int csv_cmd_buffer_len(int cmd);
 int csv_ioctl_do_hgsc_import(struct sev_issue_cmd *argp);
+
+static inline bool csv_version_greater_or_equal(u32 build)
+{
+	return hygon_csv_build >= build;
+}
 
 #endif	/* __CCP_HYGON_CSV_DEV_H__ */
