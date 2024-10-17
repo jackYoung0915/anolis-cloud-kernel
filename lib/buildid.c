fce362fc02d7a34c635c61803fc15c4c51705d98
Andrii Nakryiko <andrii@kernel.org>
2024-10-17 10:47:13 -0700

    lib/buildid: Handle memfd_secret() files in build_id_parse()
    ANBZ: #11800
    
    commit 5ac9b4e935dfc6af41eee2ddc21deb5c36507a9f upstream.
    
    >From memfd_secret(2) manpage:
    
      The memory areas backing the file created with memfd_secret(2) are
      visible only to the processes that have access to the file descriptor.
      The memory region is removed from the kernel page tables and only the
      page tables of the processes holding the file descriptor map the
      corresponding physical memory. (Thus, the pages in the region can't be
      accessed by the kernel itself, so that, for example, pointers to the
      region can't be passed to system calls.)
    
    We need to handle this special case gracefully in build ID fetching
    code. Return -EFAULT whenever secretmem file is passed to build_id_parse()
    family of APIs. Original report and repro can be found in [0].
    
      [0] https://lore.kernel.org/bpf/ZwyG8Uro%2FSyTXAni@ly-workstation/
    
    Fixes: de3ec364c3c3 ("lib/buildid: add single folio-based file reader abstraction")
    Reported-by: Yi Lai <yi1.lai@intel.com>
    Suggested-by: Shakeel Butt <shakeel.butt@linux.dev>
    Signed-off-by: Andrii Nakryiko <andrii@kernel.org>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>
    Acked-by: Shakeel Butt <shakeel.butt@linux.dev>
    Link: https://lore.kernel.org/bpf/20241017175431.6183-A-hca@linux.ibm.com
    Link: https://lore.kernel.org/bpf/20241017174713.2157873-1-andrii@kernel.org
    Signed-off-by: Kun(llfl) <llfl@linux.alibaba.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Reviewed-by: Xu Yu <xuyu@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/4097

--- lib/buildid.c
+++ lib/buildid.c
@@ -65,6 +65,10 @@ static int freader_get_folio(struct freader *r, loff_t file_off)
 
 	freader_put_folio(r);
 
+	/* reject secretmem folios created with memfd_secret() */
+	if (secretmem_mapping(r->file->f_mapping))
+		return -EFAULT;
+
 	r->folio = filemap_get_folio(r->file->f_mapping, file_off >> PAGE_SHIFT);
 
 	/* if sleeping is allowed, wait for the page, if necessary */
