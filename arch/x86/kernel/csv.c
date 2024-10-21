c47696f5fdb7f953636825757dff08c7d1a5763c
hanliyang <hanliyang@hygon.cn>
2024-10-21 18:09:55 +0800

    anolis: x86/mm: Rename csv.c to mem_encrypt_hygon.c
    ANBZ: #11454
    
    The name csv.c is limited, rename it to mem_encrypt_hygon.c and
    aggregate basic functions about Hygon memory encryption into the file
    mem_encrypt_hygon.c.
    
    Signed-off-by: hanliyang <hanliyang@hygon.cn>
    Reviewed-by: Xingrui Yi <yixingrui@linux.alibaba.com>
    Reviewed-by: Zelin Deng <zelin.deng@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/4015

--- arch/x86/kernel/csv.c
+++ arch/x86/kernel/csv.c
@@ -14,10 +14,6 @@
 #include "../mm/mm_internal.h"
 #include "csv-shared.c"
 
-u32 vendor_ebx __section(".data") = 0;
-u32 vendor_ecx __section(".data") = 0;
-u32 vendor_edx __section(".data") = 0;
-
 struct secure_call_pages {
 	struct csv3_secure_call_cmd page_a;
 	struct csv3_secure_call_cmd page_b;
@@ -31,23 +27,6 @@ static DEFINE_PER_CPU(int, secure_call_page_idx);
 
 typedef void (*csv3_secure_call_func)(u64 base_address, u64 num_pages,
 				      enum csv3_secure_command_type cmd_type);
-
-bool noinstr csv3_active(void)
-{
-	if (vendor_ebx == 0 || vendor_ecx == 0 || vendor_edx == 0) {
-		u32 eax = 0;
-
-		native_cpuid(&eax, &vendor_ebx, &vendor_ecx, &vendor_edx);
-	}
-
-	/* HygonGenuine */
-	if (vendor_ebx == CPUID_VENDOR_HygonGenuine_ebx &&
-	    vendor_ecx == CPUID_VENDOR_HygonGenuine_ecx &&
-	    vendor_edx == CPUID_VENDOR_HygonGenuine_edx)
-		return !!(sev_status & MSR_CSV3_ENABLED);
-	else
-		return false;
-}
 
 void __init csv_early_reset_memory(struct boot_params *bp)
 {
