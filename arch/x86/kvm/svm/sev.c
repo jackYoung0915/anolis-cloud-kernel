8d58ee4f9766bdd5ff4dbf98ac015623df29d1a0
hanliyang <hanliyang@hygon.cn>
2024-10-22 16:28:40 +0800

    anolis: KVM: SVM: CSV: Move MSR_AMD64_SEV_ES_GHCB emulation to csv.c
    ANBZ: #11454
    
    The emulation on MSR_AMD64_SEV_ES_GHCB only support for Hygon CSV2 and
    CSV3 guest.
    
    Signed-off-by: hanliyang <hanliyang@hygon.cn>
    Reviewed-by: Xingrui Yi <yixingrui@linux.alibaba.com>
    Reviewed-by: Zelin Deng <zelin.deng@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/4015

--- arch/x86/kvm/svm/sev.c
+++ arch/x86/kvm/svm/sev.c
@@ -3409,22 +3409,3 @@ void sev_vcpu_deliver_sipi_vector(struct kvm_vcpu *vcpu, u8 vector)
 
 	ghcb_set_sw_exit_info_2(svm->sev_es.ghcb, 1);
 }
-
-int sev_es_ghcb_map(struct vcpu_svm *svm, u64 ghcb_gpa)
-{
-	if (kvm_vcpu_map(&svm->vcpu, ghcb_gpa >> PAGE_SHIFT, &svm->sev_es.ghcb_map)) {
-		/* Unable to map GHCB from guest */
-		vcpu_unimpl(&svm->vcpu, "Missing GHCB [%#llx] from guest\n",
-			    ghcb_gpa);
-
-		svm->sev_es.receiver_ghcb_map_fail = true;
-		return -EINVAL;
-	}
-
-	svm->sev_es.ghcb = svm->sev_es.ghcb_map.hva;
-	svm->sev_es.receiver_ghcb_map_fail = false;
-
-	pr_info("Mapping GHCB [%#llx] from guest at recipient\n", ghcb_gpa);
-
-	return 0;
-}
