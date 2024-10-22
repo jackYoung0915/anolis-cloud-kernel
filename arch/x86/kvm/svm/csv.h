7baaa69085c90178946c577097492d7819add5d9
hanliyang <hanliyang@hygon.cn>
2024-10-22 11:10:02 +0800

    anolis: KVM: SVM: CSV: Skip initialization if CSV is unsupported
    ANBZ: #11454
    
    Also keep csv_init() and csv_exit() in pair.
    
    Signed-off-by: hanliyang <hanliyang@hygon.cn>
    Reviewed-by: Xingrui Yi <yixingrui@linux.alibaba.com>
    Reviewed-by: Zelin Deng <zelin.deng@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/4015

--- arch/x86/kvm/svm/csv.h
+++ arch/x86/kvm/svm/csv.h
@@ -15,10 +15,12 @@
 #ifdef CONFIG_HYGON_CSV
 
 void __init csv_init(struct kvm_x86_ops *ops);
+void csv_exit(void);
 
 #else	/* !CONFIG_HYGON_CSV */
 
 static inline void __init csv_init(struct kvm_x86_ops *ops) { }
+static inline void csv_exit(void) { }
 
 #endif	/* CONFIG_HYGON_CSV */
 
