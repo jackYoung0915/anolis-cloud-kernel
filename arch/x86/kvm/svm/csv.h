4a653886e49ebedcdc86f4ead3f307fac41ae9e0
hanliyang <hanliyang@hygon.cn>
2024-10-22 14:17:45 +0800

    anolis: KVM: SVM: CSV: Move Hygon CSV RING_BUFFER handler to csv.c
    ANBZ: #11454
    
    This will reduce code intrusion in sev.c.
    
    Signed-off-by: hanliyang <hanliyang@hygon.cn>
    Reviewed-by: Xingrui Yi <yixingrui@linux.alibaba.com>
    Reviewed-by: Zelin Deng <zelin.deng@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/4015

--- arch/x86/kvm/svm/csv.h
+++ arch/x86/kvm/svm/csv.h
@@ -12,6 +12,26 @@
 
 #include <asm/processor-hygon.h>
 
+/* same to the ring buffer max num */
+#define SVM_RING_BUFFER_MAX 4094
+
+struct csv_ringbuf_info_item {
+	struct page **pages;
+	uintptr_t hdr_vaddr;
+	uintptr_t trans_vaddr;
+	uintptr_t data_vaddr;
+	uintptr_t trans_uaddr;
+	uintptr_t hdr_uaddr;
+	unsigned long trans_len;
+	unsigned long hdr_len;
+	unsigned long n;
+};
+
+struct csv_ringbuf_infos {
+	struct csv_ringbuf_info_item *item[SVM_RING_BUFFER_MAX];
+	int num;
+};
+
 #ifdef CONFIG_HYGON_CSV
 
 /*
@@ -20,6 +40,8 @@
  */
 extern struct hygon_kvm_hooks_table {
 	bool sev_hooks_installed;
+	bool *sev_enabled;
+	unsigned long *sev_me_mask;
 	int (*sev_issue_cmd)(struct kvm *kvm, int id, void *data, int *error);
 	unsigned long (*get_num_contig_pages)(unsigned long idx,
 					      struct page **inpages,
@@ -29,15 +51,20 @@ extern struct hygon_kvm_hooks_table {
 					int write);
 	void (*sev_unpin_memory)(struct kvm *kvm, struct page **pages,
 				 unsigned long npages);
+	void (*sev_clflush_pages)(struct page *pages[], unsigned long npages);
 } hygon_kvm_hooks;
 
 void __init csv_init(struct kvm_x86_ops *ops);
 void csv_exit(void);
+int csv_alloc_trans_mempool(void);
+void csv_free_trans_mempool(void);
 
 #else	/* !CONFIG_HYGON_CSV */
 
 static inline void __init csv_init(struct kvm_x86_ops *ops) { }
 static inline void csv_exit(void) { }
+static inline int csv_alloc_trans_mempool(void) { return -ENOMEM; }
+static inline void csv_free_trans_mempool(void) { }
 
 #endif	/* CONFIG_HYGON_CSV */
 
