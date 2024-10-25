8a4e6f09b8b7c66e53f06a613dde1af02b440718
Oliver Upton <oliver.upton@linux.dev>
2024-10-25 18:23:48 +0000

    KVM: arm64: nv: Adjust range of accessible PMCs according to HPMN
    ANBZ: #31782
    
    commit 9a1c58cfefb06974a804174f127de3fedc779394 upstream.
    
    The value of MDCR_EL2.HPMN controls the number of event counters made
    visible to EL0 and EL1. This means it is possible for the guest
    hypervisor to allow direct access to event counters to the L2.
    
    Rework KVM's PMU register emulation to take the effects of HPMN into
    account when handling a trap. For bitmask-style registers, writes only
    affect accessible registers.
    
    Reviewed-by: Marc Zyngier <maz@kernel.org>
    Link: https://lore.kernel.org/r/20241025182354.3364124-14-oliver.upton@linux.dev
    Signed-off-by: Oliver Upton <oliver.upton@linux.dev>
    Signed-off-by: Jia He <justin.he@arm.com>

--- include/kvm/arm_pmu.h
+++ include/kvm/arm_pmu.h
@@ -51,6 +51,7 @@ extern struct mutex arm_pmus_lock;
 u64 kvm_pmu_get_counter_value(struct kvm_vcpu *vcpu, u64 select_idx);
 void kvm_pmu_set_counter_value(struct kvm_vcpu *vcpu, u64 select_idx, u64 val);
 u64 kvm_pmu_implemented_counter_mask(struct kvm_vcpu *vcpu);
+u64 kvm_pmu_accessible_counter_mask(struct kvm_vcpu *vcpu);
 u64 kvm_pmu_get_pmceid(struct kvm_vcpu *vcpu, bool pmceid1);
 void kvm_pmu_vcpu_init(struct kvm_vcpu *vcpu);
 void kvm_pmu_vcpu_reset(struct kvm_vcpu *vcpu);
@@ -118,6 +119,10 @@ static inline u64 kvm_pmu_get_counter_value(struct kvm_vcpu *vcpu,
 static inline void kvm_pmu_set_counter_value(struct kvm_vcpu *vcpu,
 					     u64 select_idx, u64 val) {}
 static inline u64 kvm_pmu_implemented_counter_mask(struct kvm_vcpu *vcpu)
+{
+	return 0;
+}
+static inline u64 kvm_pmu_accessible_counter_mask(struct kvm_vcpu *vcpu)
 {
 	return 0;
 }
