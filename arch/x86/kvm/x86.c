3eeb16007d032581be59177bda45c4c4bd269412
Xianglai Li <lixianglai@loongson.cn>
2024-10-26 16:50:20 +0800

    anolis: revert "KVM: Introduce vcpu->wants_to_run"
    ANBZ: #11534
    
    Signed-off-by: Xianglai Li <lixianglai@loongson.cn>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/4031
    Reviewed-by: Juxin Gao <gaojuxin@loongson.cn>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -11176,7 +11176,7 @@ int kvm_arch_vcpu_ioctl_run(struct kvm_vcpu *vcpu)
 
 	kvm_vcpu_srcu_read_lock(vcpu);
 	if (unlikely(vcpu->arch.mp_state == KVM_MP_STATE_UNINITIALIZED)) {
-		if (!vcpu->wants_to_run) {
+		if (kvm_run->immediate_exit) {
 			r = -EINTR;
 			goto out;
 		}
@@ -11254,7 +11254,7 @@ int kvm_arch_vcpu_ioctl_run(struct kvm_vcpu *vcpu)
 		WARN_ON_ONCE(vcpu->mmio_needed);
 	}
 
-	if (!vcpu->wants_to_run) {
+	if (kvm_run->immediate_exit) {
 		r = -EINTR;
 		goto out;
 	}
