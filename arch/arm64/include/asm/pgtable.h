593aaaff2892a24a2b474f4c9b86f159fe9b5412
Anshuman Khandual <anshuman.khandual@arm.com>
2024-10-29 10:15:29 +0530

    arm64/mm: Re-organize arch_make_huge_pte()
    ANBZ: #21761
    
    commit f8192813dcbe6d27e7031d9d353ea1fcc67052fd upstream.
    
    Core HugeTLB defines a fallback definition for arch_make_huge_pte(), which
    calls platform provided pte_mkhuge(). But if any platform already provides
    an override for arch_make_huge_pte(), then it does not need to provide the
    helper pte_mkhuge().
    
    arm64 override for arch_make_huge_pte() calls pte_mkhuge() internally, thus
    creating an impression, that both of these callbacks are being used in core
    HugeTLB and hence required to be defined. This drops off pte_mkhuge() which
    was never required to begin with as there could not be any section mappings
    at the PTE level. Re-organize arch_make_huge_pte() based on requested page
    size and create the entry for the applicable page table level as needed. It
    also removes a redundancy of clearing PTE_TABLE_BIT bit followed by setting
    both PTE_TABLE_BIT and PTE_VALID bits (via PTE_TYPE_MASK) in the pte, while
    creating CONT_PTE_SIZE size entries.
    
    Cc: Will Deacon <will@kernel.org>
    Cc: Ard Biesheuvel <ardb@kernel.org>
    Cc: Ryan Roberts <ryan.roberts@arm.com>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: linux-arm-kernel@lists.infradead.org
    Cc: linux-kernel@vger.kernel.org
    Signed-off-by: Anshuman Khandual <anshuman.khandual@arm.com>
    Link: https://lore.kernel.org/r/20241029044529.2624785-1-anshuman.khandual@arm.com
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Feng Su <sufeng@linux.alibaba.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5431

--- arch/arm64/include/asm/pgtable.h
+++ arch/arm64/include/asm/pgtable.h
@@ -389,11 +389,6 @@ static inline void __set_ptes(struct mm_struct *mm,
 }
 
 /*
- * Huge pte definitions.
- */
-#define pte_mkhuge(pte)		(__pte(pte_val(pte) & ~PTE_TABLE_BIT))
-
-/*
  * Hugetlb definitions.
  */
 #define HUGE_MAX_HSTATE		4
