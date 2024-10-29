cd3331c333185f7d95feae32933f2c39f2628fa1
Yuanhe Shu <xiangzao@linux.alibaba.com>
2024-10-29 15:00:11 +0800

    anolis: mm: set default value for min_cache_kbytes
    ANBZ: #12338
    ANBZ: #11569
    
    Set the default value for min_cache_kbytes to resolve the issue
    of not triggering OOM.
    
    min_cache_kbytes would be set to:
    150M,  when total memory is (  0,   4G]
    300M,  when total memory is ( 4G,   8G]
    400M,  when total memory is ( 8G,  16G]
    500M,  when total memory is (16G, 128G]
    1024M, when total memory is above 128G
    
    Signed-off-by: Yuanhe Shu <xiangzao@linux.alibaba.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Reviewed-by: Xu Yu <xuyu@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/4044
    Signed-off-by: Weilin Tong <tongweilin@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/4219

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -3207,6 +3207,7 @@ extern int __meminit early_pfn_to_nid(unsigned long pfn);
 #endif
 
 extern void set_dma_reserve(unsigned long new_dma_reserve);
+extern int __meminit init_min_cache_kbytes(void);
 extern void mem_init(void);
 extern void __init mmap_init(void);
 
