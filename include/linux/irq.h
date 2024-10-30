7f697b4571d4a2ac2293ed9695e45e64fd07b226
Guanjun <guanjun@linux.alibaba.com>
2024-10-30 09:50:10 +0800

    anolis: genirq/cpuhotplug: Handle managed IRQs when the last CPU hotplug out in the affinity
    ANBZ: #10929
    
    Once we limit the number of managed interrupts, if the last online CPU in
    the affinity goes offline, it will result in the interrupt becoming unavailable
    util one of the assigned CPUs comes online again. So prevent the last online
    CPU in the affinity from going offline, and return -EBUSY in this situation.
    
    Signed-off-by: Guanjun <guanjun@linux.alibaba.com>
    Reviewed-by: Zelin Deng <zelin.deng@linux.alibaba.com>
    Reviewed-by: Cruz Zhao <CruzZhao@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/3856

--- include/linux/irq.h
+++ include/linux/irq.h
@@ -628,8 +628,10 @@ extern int irq_set_vcpu_affinity(unsigned int irq, void *vcpu_info);
 #if defined(CONFIG_SMP) && defined(CONFIG_GENERIC_IRQ_MIGRATION)
 extern void irq_migrate_all_off_this_cpu(void);
 extern int irq_affinity_online_cpu(unsigned int cpu);
+extern int irq_affinity_offline_cpu(unsigned int cpu);
 #else
 # define irq_affinity_online_cpu	NULL
+# define irq_affinity_offline_cpu	NULL
 #endif
 
 #if defined(CONFIG_SMP) && defined(CONFIG_GENERIC_PENDING_IRQ)
