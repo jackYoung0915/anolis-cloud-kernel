90608af0e42270d793c70ce8648b3acbb42790d5
Jia He <justin.he@arm.com>
2024-10-30 00:24:06 +0000

    objtool: Seperate arch-specific and generic parts
    ANBZ: #11595
    
    commit b8e85e6f3a09fc56b0ff574887798962ef8a8f80 upstream.
    
    This reverts commit 9ec25e3e74653d228e00dfb7d816919ec5003d27.
    
    In addition, it cherry-pick commit b8e85e6f3a09 ("objtool/x86: Separate
    arch-specific and generic parts")
    
    And it also implements the arm64 specific orc.c
    
    Co-developed-by: Jinyang He <hejinyang@loongson.cn>
    Signed-off-by: Jinyang He <hejinyang@loongson.cn>
    Co-developed-by: Youling Tang <tangyouling@loongson.cn>
    Signed-off-by: Youling Tang <tangyouling@loongson.cn>
    Signed-off-by: Tiezhu Yang <yangtiezhu@loongson.cn>
    Signed-off-by: Huacai Chen <chenhuacai@loongson.cn>
    Signed-off-by: Jia He <justin.he@arm.com>
    Acked-by: Wardenjohn <ydjohn@linux.alibaba.com>
    Reviewed-by: Qiao Ma <mqaio@linux.alibaba.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/4069

--- tools/objtool/arch/arm64/orc.c
+++ tools/objtool/arch/arm64/orc.c
@@ -9,7 +9,8 @@
 #include <linux/objtool_types.h>
 
 #include <objtool/insn.h>
-#include <objtool/orc.h>
+#include <arch/orc.h>
+#include <objtool/endianness.h>
 
 int init_orc_entry(struct orc_entry *orc, struct cfi_state *cfi,
 		   struct instruction *insn)
@@ -35,6 +36,27 @@ int init_orc_entry(struct orc_entry *orc, struct cfi_state *cfi,
 	orc->fp_offset = fp->offset;
 	orc->type = cfi->type;
 	orc->signal = cfi->end;
+
+	return 0;
+}
+
+int write_orc_entry(struct elf *elf, struct section *orc_sec,
+		    struct section *ip_sec, unsigned int idx,
+		    struct section *insn_sec, unsigned long insn_off,
+		    struct orc_entry *o)
+{
+	struct orc_entry *orc;
+
+	/* populate ORC data */
+	orc = (struct orc_entry *)orc_sec->data->d_buf + idx;
+	memcpy(orc, o, sizeof(*orc));
+	orc->sp_offset = bswap_if_needed(elf, orc->sp_offset);
+	orc->fp_offset = bswap_if_needed(elf, orc->fp_offset);
+
+	/* populate reloc for ip */
+	if (!elf_init_reloc_text_sym(elf, ip_sec, idx * sizeof(int), idx,
+				     insn_sec, insn_off))
+		return -1;
 
 	return 0;
 }
@@ -65,6 +87,21 @@ const char *orc_type_name(unsigned int type)
 	default:
 		return "?";
 	}
+}
+
+void orc_print_dump(struct elf *dummy_elf, struct orc_entry *orc, int i)
+{
+	printf("type:%s", orc_type_name(orc[i].type));
+
+	printf(" sp:");
+
+	orc_print_reg(orc[i].sp_reg, bswap_if_needed(dummy_elf, orc[i].sp_offset));
+
+	printf(" fp:");
+
+	orc_print_reg(orc[i].fp_reg, bswap_if_needed(dummy_elf, orc[i].fp_offset));
+
+	printf(" signal:%d\n", orc[i].signal);
 }
 
 void orc_print_reg(unsigned int reg, int offset)
