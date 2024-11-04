c0f2108e740c4552fe2a258a30f400cb1798d9cb
Tianchen Ding <dtcccc@linux.alibaba.com>
2024-11-04 14:03:26 +0800

    anolis: sched: remove the limit about cpu burst setting
    ANBZ: #8586
    
    When porting anolis own cpu burst to ANCK 6.6, we missed to remove the
    limited that burst must under quota which is in upstream version.
    
    Signed-off-by: Tianchen Ding <dtcccc@linux.alibaba.com>
    Reviewed-by: Cruz Zhao <CruzZhao@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/4065

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -11118,10 +11118,6 @@ static int tg_set_cfs_bandwidth(struct task_group *tg, u64 period, u64 quota,
 	if (quota != RUNTIME_INF && quota > max_cfs_runtime)
 		return -EINVAL;
 
-	if (quota != RUNTIME_INF && (burst > quota ||
-				     burst + quota > max_cfs_runtime))
-		return -EINVAL;
-
 	/*
 	 * Bound burst to defend burst against overflow during bandwidth shift.
 	 */
