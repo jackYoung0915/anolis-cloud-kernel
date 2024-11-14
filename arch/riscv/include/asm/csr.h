6933b4ed2977bd34965b203c519c84265704d2a0
Charlie Jenkins <charlie@rivosinc.com>
2024-11-13 18:21:13 -0800

    riscv: csr: Add CSR encodings for CSR_VXRM/CSR_VXSAT
    ANBZ: #32704
    commit b9a9314424512e536db5e54ff554c2f10759c657 upstream.
    
    The VXRM vector csr for xtheadvector has an encoding of 0xa and VXSAT
    has an encoding of 0x9.
    
    Co-developed-by: Heiko Stuebner <heiko@sntech.de>
    Signed-off-by: Heiko Stuebner <heiko@sntech.de>
    Signed-off-by: Charlie Jenkins <charlie@rivosinc.com>
    Tested-by: Yangyu Chen <cyy@cyyself.name>
    Link: https://lore.kernel.org/r/20241113-xtheadvector-v11-7-236c22791ef9@rivosinc.com
    Signed-off-by: Palmer Dabbelt <palmer@rivosinc.com>
    Signed-off-by: Fangyu Yu <fangyu.yu@linux.alibaba.com>
    Signed-off-by: ZhenXing Zhu <zhenxing.zhu@linux.alibaba.com>

--- arch/riscv/include/asm/csr.h
+++ arch/riscv/include/asm/csr.h
@@ -335,9 +335,14 @@
 /* zicfiss user mode csr.  CSR_SSP holds current shadow stack pointer */
 #define CSR_SSP			0x011
 
-#define VCSR_VXRM_MASK			3
-#define VCSR_VXRM_SHIFT			1
-#define VCSR_VXSAT_MASK			1
+/* xtheadvector symbolic CSR names */
+#define CSR_VXSAT		0x9
+#define CSR_VXRM		0xa
+
+/* xtheadvector CSR masks */
+#define CSR_VXRM_MASK		3
+#define CSR_VXRM_SHIFT		1
+#define CSR_VXSAT_MASK		1
 
 /* Supervisor-Level Window to Indirectly Accessed Registers (AIA) */
 #define CSR_SISELECT		0x150
