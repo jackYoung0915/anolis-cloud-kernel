9856403d7b8bbe4e5c13d3ee541f4f4c8442073f
Mel Gorman <mgorman@techsingularity.net>
2024-11-14 11:00:02 +0800

    sched/fair: Move avg_scan_cost calculations under SIS_PROP
    ANBZ: #11194
    
    commit 6d95eb173f74d7ea2cf7f0de8302af8ca0d768da openeuler
    
    As noted by Vincent Guittot, avg_scan_costs are calculated for SIS_PROP
    even if SIS_PROP is disabled. Move the time calculations under a SIS_PROP
    check and while we are at it, exclude the cost of initialising the CPU
    mask from the average scan cost.
    
    Signed-off-by: Mel Gorman <mgorman@techsingularity.net>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Link: https://lkml.kernel.org/r/20210125085909.4600-3-mgorman@techsingularity.net
    Signed-off-by: Cruz Zhao <CruzZhao@linux.alibaba.com>
    Signed-off-by: Min Li <gumi@linux.alibaba.com>
    Reviewed-by: Xunlei Pang <xlpang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/4250

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8933,6 +8933,8 @@ static noinline int select_idle_cpu(struct task_struct *p, struct sched_domain *
 	if (!this_sd)
 		return -1;
 
+	cpumask_and(cpus, sched_domain_span(sd), task_allowed_cpu(p));
+
 	if (sched_feat(SIS_PROP)) {
 		u64 avg_cost, avg_idle, span_avg;
 
@@ -8951,11 +8953,8 @@ static noinline int select_idle_cpu(struct task_struct *p, struct sched_domain *
 			else
 				nr = 4;
 		}
+		time = cpu_clock(this);
 	}
-
-	time = cpu_clock(this);
-
-	cpumask_and(cpus, sched_domain_span(sd), task_allowed_cpu(p));
 
 	is_expellee = is_expellee_task(p);
 	for_each_cpu_wrap(cpu, cpus, target) {
@@ -8981,8 +8980,10 @@ static noinline int select_idle_cpu(struct task_struct *p, struct sched_domain *
 		}
 	}
 
-	time = cpu_clock(this) - time;
-	update_avg(&this_sd->avg_scan_cost, time);
+	if (sched_feat(SIS_PROP)) {
+		time = cpu_clock(this) - time;
+		update_avg(&this_sd->avg_scan_cost, time);
+	}
 
 	if (!group_identity_disabled())
 		return (unsigned int)cpu < nr_cpumask_bits ? cpu : id_backup;
