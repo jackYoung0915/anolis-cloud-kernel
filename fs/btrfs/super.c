67d93f5b221877d470b4791716504928d9084a7e
Josef Bacik <josef@toxicpanda.com>
2024-11-15 10:30:32 -0500

    fs: enable pre-content events on supported file systems
    ANBZ: #33963
    
    commit 5121711eb8dbcbed70b1db429a4665f413844164 upstream
    
    Now that all the code has been added for pre-content events, and the
    various file systems that need the page fault hooks for fsnotify have
    been updated, add SB_I_ALLOW_HSM to the supported file systems.
    
    Signed-off-by: Josef Bacik <josef@toxicpanda.com>
    Signed-off-by: Jan Kara <jack@suse.cz>
    Link: https://patch.msgid.link/46960dcb2725fa0317895ed66a8409ba1c306a82.1731684329.git.josef@toxicpanda.com
    Signed-off-by: Gao Xiang <hsiangkao@linux.alibaba.com>
    Reviewed-by: Christian Brauner <brauner@kernel.org>
    Acked-by: Joseph Qi <joseph.qi@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6833

--- fs/btrfs/super.c
+++ fs/btrfs/super.c
@@ -1152,7 +1152,7 @@ static int btrfs_fill_super(struct super_block *sb,
 	sb->s_flags |= SB_POSIXACL;
 #endif
 	sb->s_flags |= SB_I_VERSION;
-	sb->s_iflags |= SB_I_CGROUPWB;
+	sb->s_iflags |= SB_I_CGROUPWB | SB_I_ALLOW_HSM;
 
 	err = super_setup_bdi(sb);
 	if (err) {
