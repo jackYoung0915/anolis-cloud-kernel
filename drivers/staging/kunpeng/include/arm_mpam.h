240b238b330303d1c735a95a8d713c9e218a72aa
Min Li <gumi367755@alibaba-inc.com>
2024-11-19 19:52:50 +0800

    anck: arm64: refactor kunpeng mpam frame
    ANBZ: #11194
    
    immigrate newest kunpeng mpam code from openeuler (branch OLK-5.10) for
    incompatibility between yitian and kunpeng. old mpam code for kunpeng920 located in
    dir drivers/staging/kunpeng in anck is cleaned, and then transfer newest
    kunpeng mpam from openeuler, The detailed code information for the transfer is as follows
    ------------------------------------------------------------------------------------
    |  location in openeuler          ->       localtion in anck                      |
    ------------------------------------------------------------------------------------
    /arch/arm64/kernel/mpam           -> drivers/staging/kunpeng/mpam
    /drivers/acpi/arm64/              -> drivers/staging/kunpeng/acpi
    /arch/arm64/include/asm/mpam.h    -> drivers/staging/kunpeng/include/mpam.h
    /include/linux/arm_mpam.h         -> drivers/staging/kunpeng/include/arm_mpam.h
    /arch/arm64/include/asm/resctrl.h -> drivers/staging/kunpeng/include/resctrl.h
    /include/linux/resctrlfs.h        -> drivers/staging/kunpeng/include/resctrlfs.h
    /fs/resctrlfs.c                   -> drivers/staging/kunpeng/fs/resctrlfs.c
    
    Another thing we done is that some kconfigs in kunpeng-mpam have
    modified for distiction with kconfigs in anck-mpam. as follow:
    ----------------------------------------------------------------------------------
    |   kconfig in openeuler         ->        kconfig in anck                       |
    ----------------------------------------------------------------------------------
       CONFIG_MPAM                   ->       CONFIG_KUNPENG_MPAM
       CONFIG_RESCTRL                ->       CONFIG_KUNPENG_RESCTRL
    
    Signed-off-by: Min Li <gumi@linux.alibaba.com>

--- drivers/staging/kunpeng/include/arm_mpam.h
+++ drivers/staging/kunpeng/include/arm_mpam.h
@@ -7,72 +7,186 @@
 #include <linux/cpumask.h>
 #include <linux/types.h>
 
+/*  */
 /*
- * MPAM - Memory Partitioning and Monitoring table (Kunpeng)
+ * MPAM - Memory Partitioning and Monitoring table
  *
  * Conforms to "MPAM ACPI Description 1.0",
  * Null 0, 2017. Copyright 2017 ARM Limited or its affiliates.
  *
  ******************************************************************************/
-
 #define ACPI_SIG_MPAM           "MPAM"	/* Memory Partitioning and Monitoring Table */
 
 #pragma pack(1)
 
-struct kunpeng_acpi_table_mpam {
-	struct acpi_table_header        header;/* Common ACPI table header */
-};
+/*
+ * The reason for comment this struct is that the struct has been defined in actbl3.h
+ * and there is no caller to this struct, so we annotate it here.
+ */
+
+//struct acpi_table_mpam {
+//	struct acpi_table_header	header;/* Common ACPI table header */
+//};
+
 
 /* Subtable header for MPAM */
 
-struct kunpeng_acpi_mpam_header {
-	u8                      type;
-	u16                     length;
-	u8                      reserved;
-	u64                     base_address;
-	u32                     overflow_interrupt;
-	u32                     overflow_flags;
-	u32                     error_interrupt;
-	u32                     error_interrupt_flags;
-	u32                     not_ready_max;
-	u32                     offset;
+struct acpi_mpam_header {
+	u8			type;
+	u16			length;
+	u8			reserved;
+	u64			base_address;
+	u32			overflow_interrupt;
+	u32			overflow_flags;
+	u32			error_interrupt;
+	u32			error_interrupt_flags;
+	u32			not_ready_max;
+	u32			offset;
 };
 
 /* Values for subtable type in ACPI_MPAM_NODE_HEADER */
 
-enum KunpengAcpiMpamType {
-	KUNPENG_ACPI_MPAM_TYPE_SMMU             = 0,
-	KUNPENG_ACPI_MPAM_TYPE_CACHE            = 1,
-	KUNPENG_ACPI_MPAM_TYPE_MEMORY           = 2,
-	KUNPENG_ACPI_MPAM_TYPE_UNKNOWN          = 3
+enum AcpiMpamType {
+	ACPI_MPAM_TYPE_SMMU		= 0,
+	ACPI_MPAM_TYPE_CACHE		= 1,
+	ACPI_MPAM_TYPE_MEMORY		= 2,
+	ACPI_MPAM_TYPE_UNKNOWN		= 3
 };
 
 /* Flags */
-#define KUNPENG_MPAM_ACPI_MPAM_IRQ_FLAGS    (1)     /* Interrupt mode */
+#define ACPI_MPAM_IRQ_FLAGS    (1)     /* Interrupt mode */
 
 /*
  *  MPAM Subtables
  */
-struct kunpeng_acpi_mpam_node_smmu {
-	struct kunpeng_acpi_mpam_header header;
-	u32                     IORT_ref;
+struct acpi_mpam_node_smmu {
+	struct acpi_mpam_header	header;
+	u32			IORT_ref;
 };
 
-struct kunpeng_acpi_mpam_node_cache {
-	struct kunpeng_acpi_mpam_header header;
-	u32                     PPTT_ref;
+struct acpi_mpam_node_cache {
+	struct acpi_mpam_header	header;
+	u32			PPTT_ref;
 };
 
-struct kunpeng_acpi_mpam_node_memory {
-	struct kunpeng_acpi_mpam_header header;
-	u8                      proximity_domain;
-	u8                      reserved1[3];
+struct acpi_mpam_node_memory {
+	struct acpi_mpam_header	header;
+	u8			proximity_domain;
+	u8			reserved1[3];
 };
 
+/*******************************************************************************
+ *
+ * MPAM - Memory System Resource Partitioning and Monitoring
+ *
+ * Conforms to "ACPI for Memory System Resource Partitioning and Monitoring 2.0"
+ * Document number: ARM DEN 0065, December, 2022.
+ *
+ ******************************************************************************/
+
+/* MPAM RIS locator types. Table 11, Location types */
+enum acpi_mpam_locator_type {
+//	ACPI_MPAM_LOCATION_TYPE_PROCESSOR_CACHE = 0,
+//	ACPI_MPAM_LOCATION_TYPE_MEMORY = 1,
+//	ACPI_MPAM_LOCATION_TYPE_SMMU = 2,
+//	ACPI_MPAM_LOCATION_TYPE_MEMORY_CACHE = 3,
+//	ACPI_MPAM_LOCATION_TYPE_ACPI_DEVICE = 4,
+	ACPI_MPAM_LOCATION_TYPE_INTERCONNECT = 5,
+	ACPI_MPAM_LOCATION_TYPE_UNKNOWN = 0xFF
+};
+
+/* MPAM Functional dependency descriptor. Table 10 */
+struct acpi_mpam_func_deps {
+	u32 producer;
+	u32 reserved;
+};
+
+/* MPAM Processor cache locator descriptor. Table 13 */
+struct acpi_mpam_resource_cache_locator {
+	u64 cache_reference;
+	u32 reserved;
+};
+
+/* MPAM Memory locator descriptor. Table 14 */
+struct acpi_mpam_resource_memory_locator {
+	u64 proximity_domain;
+	u32 reserved;
+};
+
+/* MPAM SMMU locator descriptor. Table 15 */
+struct acpi_mpam_resource_smmu_locator {
+	u64 smmu_interface;
+	u32 reserved;
+};
+
+/* MPAM Memory-side cache locator descriptor. Table 16 */
+struct acpi_mpam_resource_memcache_locator {
+	u8 reserved[7];
+	u8 level;
+	u32 reference;
+};
+
+/* MPAM ACPI device locator descriptor. Table 17 */
+struct acpi_mpam_resource_acpi_locator {
+	u64 acpi_hw_id;
+	u32 acpi_unique_id;
+};
+
+/* MPAM Interconnect locator descriptor. Table 18 */
+struct acpi_mpam_resource_interconnect_locator {
+	u64 inter_connect_desc_tbl_off;
+	u32 reserved;
+};
+
+/* MPAM Locator structure. Table 12 */
+struct acpi_mpam_resource_generic_locator {
+	u64 descriptor1;
+	u32 descriptor2;
+};
+
+union acpi_mpam_resource_locator {
+	struct acpi_mpam_resource_cache_locator cache_locator;
+	struct acpi_mpam_resource_memory_locator memory_locator;
+	struct acpi_mpam_resource_smmu_locator smmu_locator;
+	struct acpi_mpam_resource_memcache_locator mem_cache_locator;
+	struct acpi_mpam_resource_acpi_locator acpi_locator;
+	struct acpi_mpam_resource_interconnect_locator interconnect_ifc_locator;
+	struct acpi_mpam_resource_generic_locator generic_locator;
+};
+
+/* Memory System Component Resource Node Structure Table 9 */
+struct acpi_mpam_resource_node {
+	u32 identifier;
+	u8 ris_index;
+	u16 reserved1;
+	u8 locator_type;
+	union acpi_mpam_resource_locator locator;
+	u32 num_functional_deps;
+};
+
+/* Memory System Component (MSC) Node Structure. Table 4 */
+struct acpi_mpam_msc_node {
+	u16 length;
+	u8 interface_type;
+	u8 reserved;
+	u32 identifier;
+	u64 base_address;
+	u32 mmio_size;
+	u32 overflow_interrupt;
+	u32 overflow_interrupt_flags;
+	u32 reserved1;
+	u32 overflow_interrupt_affinity;
+	u32 error_interrupt;
+	u32 error_interrupt_flags;
+	u32 reserved2;
+	u32 error_interrupt_affinity;
+	u32 max_nrdy_usec;
+	u64 hardware_id_linked_device;
+	u32 instance_id_linked_device;
+	u32 num_resouce_nodes;
+};
 #pragma pack()
-
 /* End of Kunpeng MPAM ACPI Definitions  */
-
 struct mpam_device;
 
 enum mpam_class_types {
@@ -82,7 +196,7 @@ enum mpam_class_types {
 	MPAM_CLASS_UNKNOWN, /* Everything else, e.g. TLBs etc */
 };
 
-struct mpam_device * __init
+struct mpam_device *
 __mpam_device_create(u8 level_idx, enum mpam_class_types type,
 			int component_id, const struct cpumask *fw_affinity,
 			phys_addr_t hwpage_address);
@@ -110,7 +224,7 @@ mpam_device_create_cache(u8 level_idx, int cache_id,
  * which will be exported as resctrl domains. MSCs for memory must
  * be accessible from any cpu.
  */
-static inline struct mpam_device * __init
+static inline struct mpam_device *
 mpam_device_create_memory(int nid, phys_addr_t hwpage_address)
 {
 	struct cpumask dev_affinity;
@@ -120,16 +234,17 @@ mpam_device_create_memory(int nid, phys_addr_t hwpage_address)
 	return __mpam_device_create(~0, MPAM_CLASS_MEMORY, nid,
 			&dev_affinity, hwpage_address);
 }
-int __init mpam_discovery_start(void);
-int __init mpam_discovery_complete(void);
-void __init mpam_discovery_failed(void);
+int mpam_discovery_start(void);
+int mpam_discovery_complete(void);
+void mpam_discovery_failed(void);
 
-enum kunpeng_mpam_enable_type {
-	KUNPENG_MPAM_ENABLE_DENIED = 0,
-	KUNPENG_MPAM_ENABLE_ACPI,
+enum mpam_enable_type {
+	MPAM_ENABLE_DENIED = 0,
+	MPAM_ENABLE_ACPI,
+	MPAM_ENABLE_OF,
 };
 
-extern enum kunpeng_mpam_enable_type kunpeng_mpam_enabled;
+extern enum mpam_enable_type kunpeng_mpam_enabled;
 
 #define MPAM_IRQ_MODE_LEVEL    0x1
 #define MPAM_IRQ_FLAGS_MASK    0x7f
@@ -137,12 +252,12 @@ extern enum kunpeng_mpam_enable_type kunpeng_mpam_enabled;
 #define mpam_irq_flags_to_acpi(x) ((x & MPAM_IRQ_MODE_LEVEL) ?  \
 			ACPI_LEVEL_SENSITIVE : ACPI_EDGE_SENSITIVE)
 
-void __init mpam_device_set_error_irq(struct mpam_device *dev, u32 irq,
+void mpam_device_set_error_irq(struct mpam_device *dev, u32 irq,
 			u32 flags);
-void __init mpam_device_set_overflow_irq(struct mpam_device *dev, u32 irq,
+void mpam_device_set_overflow_irq(struct mpam_device *dev, u32 irq,
 			u32 flags);
 
-static inline int __init mpam_register_device_irq(struct mpam_device *dev,
+static inline int mpam_register_device_irq(struct mpam_device *dev,
 			u32 overflow_interrupt, u32 overflow_flags,
 			u32 error_interrupt, u32 error_flags)
 {
@@ -180,5 +295,7 @@ static inline int __init mpam_register_device_irq(struct mpam_device *dev,
 
 	return ret;
 }
+
+int __init acpi_mpam_parse_version(void);
 
 #endif
