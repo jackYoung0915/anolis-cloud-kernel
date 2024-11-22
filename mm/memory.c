f416b8f57116708ff1eb624eb042ce5e7d824e06
Qinyun Tan <qinyuntan@linux.alibaba.com>
2024-11-22 16:44:40 +0800

    anolis: mm/follow_pfn: use the new follow_pfnmap API
    ANBZ: #11975
    
    Use the new API that can understand huge pfn mappings.
    
    Signed-off-by: Qinyun Tan <qinyuntan@linux.alibaba.com>

--- mm/memory.c
+++ mm/memory.c
@@ -5585,17 +5585,16 @@ int follow_pfn(struct vm_area_struct *vma, unsigned long address,
 	unsigned long *pfn)
 {
 	int ret = -EINVAL;
-	spinlock_t *ptl;
-	pte_t *ptep;
+	struct follow_pfnmap_args args = { .vma = vma, .address = address };
 
 	if (!(vma->vm_flags & (VM_IO | VM_PFNMAP)))
 		return ret;
 
-	ret = follow_pte(vma->vm_mm, address, &ptep, &ptl);
+	ret = follow_pfnmap_start(&args);
 	if (ret)
 		return ret;
-	*pfn = pte_pfn(*ptep);
-	pte_unmap_unlock(ptep, ptl);
+	*pfn = args.pfn;
+	follow_pfnmap_end(&args);
 	return 0;
 }
 EXPORT_SYMBOL(follow_pfn);
