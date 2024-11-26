794c1e44047ba2567175e0805ee3d1a9f705583c
Jia He <justin.he@arm.com>
2024-11-26 08:02:40 +0000

    anolis: objtool: arm64: Move structure orc_entry to arm64 asm/orc_types.h
    ANBZ: #12121
    
    It is a good idea to abstract the similar struct orc_entry into a generic
    header file orc_entry.h. However, to maintain alignment between the Anolis
    livepatch code layout and the upstream kernel, we must retain arch specific
    definitions of struct orc_entry.
    
    Signed-off-by: Jia He <justin.he@arm.com>
    Reviewed-by: Tiezhu Yang <yangtiezhu@loongson.cn>
    Reviewed-by: Shuai Xue <xueshuai@linux.alibaba.com>
    Reviewed-by: Jay Chen <jkchen@linux.alibaba.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/4161

--- arch/arm64/include/asm/orc_types.h
+++ arch/arm64/include/asm/orc_types.h
@@ -10,7 +10,6 @@
 
 #include <linux/types.h>
 #include <linux/compiler.h>
-#include <linux/orc_entry.h>
 
 /*
  * The ORC_REG_* registers are base registers which are used to find other
@@ -37,5 +36,35 @@
 #define ORC_TYPE_CALL			2
 #define ORC_TYPE_REGS			3
 #define ORC_TYPE_REGS_PARTIAL		4
+
+#ifndef __ASSEMBLY__
+#include <asm/byteorder.h>
+
+/*
+ * This struct is more or less a vastly simplified version of the DWARF Call
+ * Frame Information standard.  It contains only the necessary parts of DWARF
+ * CFI, simplified for ease of access by the in-kernel unwinder.  It tells the
+ * unwinder how to find the previous SP and BP (and sometimes entry regs) on
+ * the stack for a given code address.  Each instance of the struct corresponds
+ * to one or more code locations.
+ */
+struct orc_entry {
+	s16             sp_offset;
+	s16             fp_offset;
+#if defined(__LITTLE_ENDIAN_BITFIELD)
+	unsigned        sp_reg:4;
+	unsigned        fp_reg:4;
+	unsigned        type:4;
+	unsigned        signal:1;
+#elif defined(__BIG_ENDIAN_BITFIELD)
+	unsigned        fp_reg:4;
+	unsigned        sp_reg:4;
+	unsigned        unused:3;
+	unsigned        signal:1;
+	unsigned        type:4;
+#endif
+} __packed;
+
+#endif /* __ASSEMBLY__ */
 
 #endif /* _ORC_TYPES_H */
