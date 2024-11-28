2e3924c68232d43e842a0dcb4323af028395409e
Amir Goldstein <amir73il@gmail.com>
2024-11-28 15:25:32 +0100

    fs: don't block write during exec on pre-content watched files
    ANBZ: #33963
    
    commit 0357ef03c94ef835bd44a0658b8edb672a9dbf51 upstream.
    
    Commit 2a010c412853 ("fs: don't block i_writecount during exec") removed
    the legacy behavior of getting ETXTBSY on attempt to open and executable
    file for write while it is being executed.
    
    This commit was reverted because an application that depends on this
    legacy behavior was broken by the change.
    
    We need to allow HSM writing into executable files while executed to
    fill their content on-the-fly.
    
    To that end, disable the ETXTBSY legacy behavior for files that are
    watched by pre-content events.
    
    This change is not expected to cause regressions with existing systems
    which do not have any pre-content event listeners.
    
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>
    Acked-by: Christian Brauner <brauner@kernel.org>
    Signed-off-by: Jan Kara <jack@suse.cz>
    Link: https://patch.msgid.link/20241128142532.465176-1-amir73il@gmail.com
    Signed-off-by: Gao Xiang <hsiangkao@linux.alibaba.com>

--- fs/exec.c
+++ fs/exec.c
@@ -939,7 +939,7 @@ static struct file *do_open_execat(int fd, struct filename *name, int flags)
 	    path_noexec(&file->f_path))
 		goto exit;
 
-	err = deny_write_access(file);
+	err = exe_file_deny_write_access(file);
 	if (err)
 		goto exit;
 
@@ -958,7 +958,7 @@ exit:
  * Returns ERR_PTR on failure or allocated struct file on success.
  *
  * As this is a wrapper for the internal do_open_execat(), callers
- * must call allow_write_access() before fput() on release. Also see
+ * must call exe_file_allow_write_access() before fput() on release. Also see
  * do_close_execat().
  */
 struct file *open_exec(const char *name)
@@ -1529,7 +1529,7 @@ static void do_close_execat(struct file *file)
 {
 	if (!file)
 		return;
-	allow_write_access(file);
+	exe_file_allow_write_access(file);
 	fput(file);
 }
 
@@ -1847,7 +1847,7 @@ static int exec_binprm(struct linux_binprm *bprm)
 		bprm->file = bprm->interpreter;
 		bprm->interpreter = NULL;
 
-		allow_write_access(exec);
+		exe_file_allow_write_access(exec);
 		if (unlikely(bprm->have_execfd)) {
 			if (bprm->executable) {
 				fput(exec);
