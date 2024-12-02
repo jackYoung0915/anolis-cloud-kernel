5bd6f18ac3c889da4e4aa47a96f68d545b528de1
Quan Zhou <zhouquan@iscas.ac.cn>
2024-12-02 11:22:01 +0800

    RISC-V: KVM: Allow Ziccrse extension for Guest/VM
    ANBZ: #24718
    
    commit 79be257b579e72ae7c5aeb942700a449d467405f upstream.
    
    Extend the KVM ISA extension ONE_REG interface to allow KVM user space
    to detect and enable Ziccrse extension for Guest/VM.
    
    Signed-off-by: Quan Zhou <zhouquan@iscas.ac.cn>
    Reviewed-by: Andrew Jones <ajones@ventanamicro.com>
    Link: https://lore.kernel.org/r/d10e746d165074174f830aa3d89bf3c92017acee.1732854096.git.zhouquan@iscas.ac.cn
    Signed-off-by: Anup Patel <anup@brainfault.org>
    Reviewed-by: Fangyu <Yu fangyu.yu@linux.alibaba.com>
    Reviewed-by: Ruidong Tian <tianruidong@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5798

--- arch/riscv/kvm/vcpu_onereg.c
+++ arch/riscv/kvm/vcpu_onereg.c
@@ -68,6 +68,7 @@ static const unsigned long kvm_isa_ext_arr[] = {
 	KVM_ISA_EXT_ARR(ZCMOP),
 	KVM_ISA_EXT_ARR(ZICBOM),
 	KVM_ISA_EXT_ARR(ZICBOZ),
+	KVM_ISA_EXT_ARR(ZICCRSE),
 	KVM_ISA_EXT_ARR(ZICNTR),
 	KVM_ISA_EXT_ARR(ZICOND),
 	KVM_ISA_EXT_ARR(ZICSR),
@@ -157,6 +158,7 @@ static bool kvm_riscv_vcpu_isa_disable_allowed(unsigned long ext)
 	case KVM_RISCV_ISA_EXT_ZFA:
 	case KVM_RISCV_ISA_EXT_ZFH:
 	case KVM_RISCV_ISA_EXT_ZFHMIN:
+	case KVM_RISCV_ISA_EXT_ZICCRSE:
 	case KVM_RISCV_ISA_EXT_ZCA:
 	case KVM_RISCV_ISA_EXT_ZCB:
 	case KVM_RISCV_ISA_EXT_ZCD:
