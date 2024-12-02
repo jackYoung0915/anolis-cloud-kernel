840005ee8dfec1389383be9dcad46637fd622adc
Tom Lendacky <thomas.lendacky@amd.com>
2024-12-02 14:50:47 -0600

    x86/sev: Add support for the RMPREAD instruction
    ANBZ: #34453
    
    commit 0cbc0258415814c86eb6db50237ae3d90fbf3b3d upstream.
    
    The RMPREAD instruction returns an architecture defined format of an
    RMP table entry. This is the preferred method for examining RMP entries.
    
    The instruction is advertised in CPUID 0x8000001f_EAX[21]. Use this
    instruction when available.
    
    Signed-off-by: Tom Lendacky <thomas.lendacky@amd.com>
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    Reviewed-by: Nikunj A Dadhania <nikunj@amd.com>
    Reviewed-by: Neeraj Upadhyay <Neeraj.Upadhyay@amd.com>
    Reviewed-by: Ashish Kalra <ashish.kalra@amd.com>
    Link: https://lore.kernel.org/r/72c734ac8b324bbc0c839b2c093a11af4a8881fa.1733172653.git.thomas.lendacky@amd.com
    Signed-off-by: Abhishek Rajput <Abhishek.Rajput@amd.com>
    Signed-off-by: mohanasv <mohanasv@amd.com>
    Reviewed-by: Tianjia Zhang <tianjia.zhang@linux.alibaba.com>
    Reviewed-by: Zongyao Chen <zongyao.chen@linux.alibaba.com>
    Reviewed-by: Bin Guo <guobin@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/7708

--- arch/x86/include/asm/cpufeatures.h
+++ arch/x86/include/asm/cpufeatures.h
@@ -475,6 +475,7 @@
 #define X86_FEATURE_V_TSC_AUX		(19*32+ 9) /* Virtual TSC_AUX */
 #define X86_FEATURE_SME_COHERENT	(19*32+10) /* hardware-enforced cache coherency */
 #define X86_FEATURE_DEBUG_SWAP		(19*32+14) /* "debug_swap" SEV-ES full debug state swap support */
+#define X86_FEATURE_RMPREAD		(19*32+21) /* RMPREAD instruction */
 #define X86_FEATURE_HV_INUSE_WR_ALLOWED	(19*32+30) /* Allow Write to in-use hypervisor-owned pages */
 /*
  * Bit 30 of CPUID 0x8000001F EAX is a shared hardware bit:
