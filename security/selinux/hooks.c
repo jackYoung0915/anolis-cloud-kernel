5166e5e57cb962c6451a560142697d6dc6b1dd29
Christian Göttsche <cgzones@googlemail.com>
2024-12-16 17:40:00 +0100

    selinux: avoid using types indicating user space interaction
    ANBZ: #35678
    
    commit 046b85a993a19c992da317b2c19e168d1da795af upstream.
    
    Integer types starting with a double underscore, like __u32, are
    intended for usage of variables interacting with user-space.
    
    Just use the plain variant.
    
    Signed-off-by: Christian Göttsche <cgzones@googlemail.com>
    Signed-off-by: Paul Moore <paul@paul-moore.com>
    Signed-off-by: Tianjia Zhang <tianjia.zhang@linux.alibaba.com>

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -3132,7 +3132,7 @@ static int selinux_inode_setattr(struct dentry *dentry, struct iattr *iattr)
 	const struct cred *cred = current_cred();
 	struct inode *inode = d_backing_inode(dentry);
 	unsigned int ia_valid = iattr->ia_valid;
-	__u32 av = FILE__WRITE;
+	u32 av = FILE__WRITE;
 
 	/* ATTR_FORCE is just used for ATTR_KILL_S[UG]ID. */
 	if (ia_valid & ATTR_FORCE) {
