33f27c4ea0d30b71d14f8f670a9f012ce9b31cad
Rik van Riel <riel@surriel.com>
2024-12-19 15:32:53 -0500

    mm: remove unnecessary calls to lru_add_drain
    ANBZ: #27142
    
    commit 1aa43598c03b71d945f8b12d6d579f0ad836f8f8 upstream.
    
    There seem to be several categories of calls to lru_add_drain and
    lru_add_drain_all.
    
    The first are code paths that recently allocated, swapped in, or otherwise
    processed a batch of pages, and want them all on the LRU.  These drain
    pages that were recently allocated, probably on the local CPU.
    
    A second category are code paths that are actively trying to reclaim,
    migrate, or offline memory.  These often use lru_add_drain_all, to drain
    the caches on all CPUs.
    
    However, there also seem to be some other callers where we aren't really
    doing either.  They are calling lru_add_drain(), despite operating on
    pages that may have been allocated long ago, and quite possibly on
    different CPUs.
    
    Those calls are not likely to be effective at anything but creating lock
    contention on the LRU locks.
    
    Remove the lru_add_drain calls in the latter category.
    
    For detailed reasoning, see [1] and [2].
    
    Link: https://lkml.kernel.org/r/dca2824e8e88e826c6b260a831d79089b5b9c79d.camel@surriel.com [1]
    Link: https://lkml.kernel.org/r/xxfhcjaq2xxcl5adastz5omkytenq7izo2e5f4q7e3ns4z6lko@odigjjc7hqrg [2]
    Link: https://lkml.kernel.org/r/20241219153253.3da9e8aa@fangorn
    Signed-off-by: Rik van Riel <riel@surriel.com>
    Suggested-by: David Hildenbrand <david@redhat.com>
    Acked-by: Shakeel Butt <shakeel.butt@linux.dev>
    Acked-by: David Hildenbrand <david@redhat.com>
    Acked-by: Lorenzo Stoakes <lorenzo.stoakes@oracle.com>
    Cc: Chris Li <chrisl@kernel.org>
    Cc: Matthew Wilcox (Oracle) <willy@infradead.org>
    Cc: Ryan Roberts <ryan.roberts@arm.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Zelin Deng <zelin.deng@linux.alibaba.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6015

--- mm/mmap.c
+++ mm/mmap.c
@@ -2399,7 +2399,6 @@ static void unmap_region(struct mm_struct *mm, struct ma_state *mas,
 	struct mmu_gather tlb;
 	unsigned long mt_start = mas->index;
 
-	lru_add_drain();
 	tlb_gather_mmu(&tlb, mm);
 	update_hiwater_rss(mm);
 	unmap_vmas(&tlb, mas, vma, start, end, tree_end, mm_wr_locked);
@@ -3362,7 +3361,6 @@ void exit_mmap(struct mm_struct *mm)
 		goto destroy;
 	}
 
-	lru_add_drain();
 	flush_cache_mm(mm);
 	tlb_gather_mmu_fullmm(&tlb, mm);
 	/* update_hiwater_rss(mm) here? but nobody should be looking */
