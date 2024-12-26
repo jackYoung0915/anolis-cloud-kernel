c74fcd82f8ecd4b03d0c5141eb3b8548537946a5
Suren Baghdasaryan <surenb@google.com>
2024-12-26 13:16:39 -0800

    alloc_tag: skip pgalloc_tag_swap if profiling is disabled
    ANBZ: #32420
    
    commit 05c82ee363f64c64b87a0cfd744298e9333475f5 upstream.
    
    When memory allocation profiling is disabled, there is no need to swap
    allocation tags during migration.  Skip it to avoid unnecessary overhead.
    
    Once I added these checks, the overhead of the mode when memory profiling
    is enabled but turned off went down by about 50%.
    
    Link: https://lkml.kernel.org/r/20241226211639.1357704-2-surenb@google.com
    Fixes: e0a955bf7f61 ("mm/codetag: add pgalloc_tag_copy()")
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>
    Cc: David Wang <00107082@163.com>
    Cc: Kent Overstreet <kent.overstreet@linux.dev>
    Cc: Yu Zhao <yuzhao@google.com>
    Cc: Zhenhua Huang <quic_zhenhuah@quicinc.com>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Yuanhe Shu <xiangzao@linux.alibaba.com>
    Acked-by: David Rientjes <rientjes@google.com>
    Acked-by: Harry Yoo <harry.yoo@oracle.com>
    Acked-by: Kent Overstreet <kent.overstreet@linux.dev>
    Acked-by: Muchun Song <muchun.song@linux.dev>
    Acked-by: Vlastimil Babka <vbabka@suse.cz>
    Reviewed-by: Kees Cook <kees@kernel.org>
    Reviewed-by: Hao Li <hao.li@linux.dev>
    Reviewed-by: Miaohe Lin <linmiaohe@huawei.com>
    Acked-by: David Hildenbrand <david@redhat.com>
    Reviewed-by: Pasha Tatashin <pasha.tatashin@soleen.com>
    Acked-by: Shakeel Butt <shakeel.butt@linux.dev>
    Acked-by: SeongJae Park <sj@kernel.org>
    Acked-by: Yu Zhao <yuzhao@google.com>
    Reviewed-by: Anshuman Khandual <anshuman.khandual@arm.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6766

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -4609,6 +4609,9 @@ static inline void pgalloc_tag_swap(struct folio *new, struct folio *old)
 	union codetag_ref *ref_old, *ref_new;
 	struct alloc_tag *tag_old, *tag_new;
 
+	if (!mem_alloc_profiling_enabled())
+		return;
+
 	tag_old = pgalloc_tag_get(&old->page);
 	if (!tag_old)
 		return;
