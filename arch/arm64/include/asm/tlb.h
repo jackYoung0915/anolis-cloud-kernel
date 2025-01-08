bc3c7c54dba0139e9bfeb8704c39c834c8c5dbe8
Qi Zheng <zhengqi.arch@bytedance.com>
2025-01-08 14:57:25 +0800

    arm64: pgtable: move pagetable_dtor() to __tlb_remove_table()
    ANBZ: #24557
    
    commit 12359c039b5f510339cb3e68f8e6020e9a848e8c upstream.
    
    Move pagetable_dtor() to __tlb_remove_table(), so that ptlock and page
    table pages can be freed together (regardless of whether RCU is used).
    This prevents the use-after-free problem where the ptlock is freed
    immediately but the page table pages is freed later via RCU.
    
    Page tables shouldn't have swap cache, so use pagetable_free() instead of
    free_page_and_swap_cache() to free page table pages.
    
    Link: https://lkml.kernel.org/r/cf4b847caf390f96a3e3d534dacb2c174e16c154.1736317725.git.zhengqi.arch@bytedance.com
    Signed-off-by: Qi Zheng <zhengqi.arch@bytedance.com>
    Suggested-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Kevin Brodsky <kevin.brodsky@arm.com>
    Acked-by: Will Deacon <will@kernel.org>
    Cc: Alexander Gordeev <agordeev@linux.ibm.com>
    Cc: Alexandre Ghiti <alex@ghiti.fr>
    Cc: Alexandre Ghiti <alexghiti@rivosinc.com>
    Cc: Andreas Larsson <andreas@gaisler.com>
    Cc: Aneesh Kumar K.V (Arm) <aneesh.kumar@kernel.org>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: Dave Hansen <dave.hansen@linux.intel.com>
    Cc: David Hildenbrand <david@redhat.com>
    Cc: David Rientjes <rientjes@google.com>
    Cc: Hugh Dickins <hughd@google.com>
    Cc: Jann Horn <jannh@google.com>
    Cc: Lorenzo Stoakes <lorenzo.stoakes@oracle.com>
    Cc: Matthew Wilcox (Oracle) <willy@infradead.org>
    Cc: Mike Rapoport (Microsoft) <rppt@kernel.org>
    Cc: Muchun Song <muchun.song@linux.dev>
    Cc: Nicholas Piggin <npiggin@gmail.com>
    Cc: Palmer Dabbelt <palmer@dabbelt.com>
    Cc: Ryan Roberts <ryan.roberts@arm.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Vishal Moola (Oracle) <vishal.moola@gmail.com>
    Cc: Yu Zhao <yuzhao@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Zelin Deng <zelin.deng@linux.alibaba.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5749

--- arch/arm64/include/asm/tlb.h
+++ arch/arm64/include/asm/tlb.h
@@ -9,11 +9,13 @@
 #define __ASM_TLB_H
 
 #include <linux/pagemap.h>
-#include <linux/swap.h>
 
 static inline void __tlb_remove_table(void *_table)
 {
-	free_page_and_swap_cache((struct page *)_table);
+	struct ptdesc *ptdesc = (struct ptdesc *)_table;
+
+	pagetable_dtor(ptdesc);
+	pagetable_free(ptdesc);
 }
 
 #define tlb_flush tlb_flush
@@ -77,7 +79,6 @@ static inline void __pte_free_tlb(struct mmu_gather *tlb, pgtable_t pte,
 {
 	struct ptdesc *ptdesc = page_ptdesc(pte);
 
-	pagetable_dtor(ptdesc);
 	tlb_remove_ptdesc(tlb, ptdesc);
 }
 
@@ -87,7 +88,6 @@ static inline void __pmd_free_tlb(struct mmu_gather *tlb, pmd_t *pmdp,
 {
 	struct ptdesc *ptdesc = virt_to_ptdesc(pmdp);
 
-	pagetable_dtor(ptdesc);
 	tlb_remove_ptdesc(tlb, ptdesc);
 }
 #endif
