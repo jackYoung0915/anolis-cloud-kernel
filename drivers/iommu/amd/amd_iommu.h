f7a86ce8b047c56abc6d50577ae6eddb7b32f0f7
Jason Gunthorpe <jgg@nvidia.com>
2025-01-10 12:35:04 -0400

    iommu/amd: Move the nid to pdom_setup_pgtable()
    ANBZ: #26808
    
    commit 5a081f7f428cc68c2df8eb281bfbaedf65b749c1 upstream.
    
    The only thing that uses the nid is the io_pgtable code, and it should be
    set before calling alloc_io_pgtable_ops() to ensure that the top levels
    are allocated on the correct nid.
    
    Since dev is never NULL now we can just do this trivially and remove the
    other uses of nid. SVA and identity code paths never use it since they
    don't use io_pgtable.
    
    Reviewed-by: Vasant Hegde <vasant.hegde@amd.com>
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>
    Link: https://lore.kernel.org/r/6-v2-9776c53c2966+1c7-amd_paging_flags_jgg@nvidia.com
    Signed-off-by: Joerg Roedel <jroedel@suse.de>
    Signed-off-by: Zelin Deng <zelin.deng@linux.alibaba.com>
    Reviewed-by: Kevin Tian <kevin.tian@intel.com>
    Reviewed-by: Christophe Leroy <christophe.leroy@csgroup.eu>
    Reviewed-by: Lu Baolu <baolu.lu@linux.intel.com>
    Reviewed-by: Randy Dunlap <rdunlap@infradead.org>
    Reviewed-by: Rae Moar <rmoar@google.com>
    Reviewed-by: David Gow <davidgow@google.com>
    Reviewed-by: Kees Cook <kees@kernel.org>
    Reviewed-by: Maxime Ripard <mripard@kernel.org>
    Reviewed-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Reviewed-by: Guanghui Feng <guanghuifeng@linux.alibaba.com>
    Reviewed-by: Shuai Xue <xueshuai@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6802

--- drivers/iommu/amd/amd_iommu.h
+++ drivers/iommu/amd/amd_iommu.h
@@ -48,7 +48,7 @@ extern bool amd_iommu_hatdis;
 
 /* Protection domain ops */
 void amd_iommu_init_identity_domain(void);
-struct protection_domain *protection_domain_alloc(int nid);
+struct protection_domain *protection_domain_alloc(void);
 void protection_domain_free(struct protection_domain *domain);
 struct iommu_domain *amd_iommu_domain_alloc_sva(struct device *dev,
 						struct mm_struct *mm);
