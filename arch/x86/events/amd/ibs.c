f1e5e5a385c1e6b04dd72ba3783343a0246b051a
Ravi Bangoria <ravi.bangoria@amd.com>
2025-01-15 05:44:36 +0000

    perf/amd/ibs: Add ->check_period() callback
    ANBZ: #22494
    
    commit 1afbdd970f50f2e0431fae26b25d4e54e561fa7f upstream
    
    IBS Fetch and IBS Op PMUs have constraints on sample period. The sample
    period is verified at the time of opening an event but not at the ioctl()
    interface. Hence, a user can open an event with valid period but change
    it later with ioctl(). Add a ->check_period() callback to verify the
    period provided at ioctl() is also valid.
    
    Signed-off-by: Ravi Bangoria <ravi.bangoria@amd.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Acked-by: Namhyung Kim <namhyung@kernel.org>
    Link: https://lkml.kernel.org/r/20250115054438.1021-8-ravi.bangoria@amd.com
    Signed-off-by: Shu Ming (AMD) <ming.shu@amd.com>
    Reviewed-by: Min Li <gumi@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5487

--- arch/x86/events/amd/ibs.c
+++ arch/x86/events/amd/ibs.c
@@ -549,6 +549,28 @@ static void perf_ibs_del(struct perf_event *event, int flags)
 
 static void perf_ibs_read(struct perf_event *event) { }
 
+static int perf_ibs_check_period(struct perf_event *event, u64 value)
+{
+	struct perf_ibs *perf_ibs;
+	u64 low_nibble;
+
+	if (event->attr.freq)
+		return 0;
+
+	perf_ibs = container_of(event->pmu, struct perf_ibs, pmu);
+	low_nibble = value & 0xFULL;
+
+	/*
+	 * This contradicts with perf_ibs_init() which allows sample period
+	 * with lower nibble bits set but silently masks them off. Whereas
+	 * this returns error.
+	 */
+	if (low_nibble || value < perf_ibs->min_period)
+		return -EINVAL;
+
+	return 0;
+}
+
 /*
  * We need to initialize with empty group if all attributes in the
  * group are dynamic.
@@ -674,6 +696,7 @@ static struct perf_ibs perf_ibs_fetch = {
 		.start		= perf_ibs_start,
 		.stop		= perf_ibs_stop,
 		.read		= perf_ibs_read,
+		.check_period	= perf_ibs_check_period,
 		.capabilities	= PERF_PMU_CAP_NO_EXCLUDE,
 	},
 	.msr			= MSR_AMD64_IBSFETCHCTL,
@@ -699,6 +722,7 @@ static struct perf_ibs perf_ibs_op = {
 		.start		= perf_ibs_start,
 		.stop		= perf_ibs_stop,
 		.read		= perf_ibs_read,
+		.check_period	= perf_ibs_check_period,
 		.capabilities	= PERF_PMU_CAP_NO_EXCLUDE,
 	},
 	.msr			= MSR_AMD64_IBSOPCTL,
