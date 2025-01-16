23b0c95c6e5fd7e0d682fbdf2ba80df40e58980a
Kurt Borja <kuurtb@gmail.com>
2025-01-15 19:27:06 -0500

    ACPI: platform_profile: Add `ops` member to handlers
    ANBZ: #36733
    
    commit b5ca1a4488a5e6dfb9962e2319c03c7414e50ec3 upstream.
    
    Replace *profile_get and *profile_set members with a general *ops
    member.
    
    [Backport Changes]
    Upstream commit b5ca1a4488a5 ("ACPI: platform_profile: Add `ops` member to
    handlers") modifies 12 files; only 8 are applied here. The remaining 4 hunks
    are skipped because they are not applicable to this baseline:
      - drivers/platform/x86/dell/dell-pc.c and
        drivers/platform/x86/inspur_platform_profile.c are not present in this tree.
      - drivers/platform/x86/acer-wmi.c and
        drivers/platform/x86/dell/alienware-wmi.c are present but do not carry the
        platform_profile_handler usage in this baseline (the corresponding
        platform_profile support for these drivers is not backported).
    
    Reviewed-by: Mario Limonciello <mario.limonciello@amd.com>
    Signed-off-by: Kurt Borja <kuurtb@gmail.com>
    Reviewed-by: Mark Pearson <mpearson-lenovo@squebb.ca>
    Tested-by: Mark Pearson <mpearson-lenovo@squebb.ca>
    Link: https://lore.kernel.org/r/20250116002721.75592-5-kuurtb@gmail.com
    Reviewed-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>
    Signed-off-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>
    Signed-off-by: Jeevan deep J <j.jeevandeep@amd.com>
    Signed-off-by: PrithivishS <sprithiv@amd.com>

--- drivers/platform/x86/hp/hp-wmi.c
+++ drivers/platform/x86/hp/hp-wmi.c
@@ -1379,6 +1379,21 @@ static int platform_profile_victus_set(struct device *dev,
 	return 0;
 }
 
+static const struct platform_profile_ops platform_profile_omen_ops = {
+	.profile_get = platform_profile_omen_get,
+	.profile_set = platform_profile_omen_set,
+};
+
+static const struct platform_profile_ops platform_profile_victus_ops = {
+	.profile_get = platform_profile_victus_get,
+	.profile_set = platform_profile_victus_set,
+};
+
+static const struct platform_profile_ops hp_wmi_platform_profile_ops = {
+	.profile_get = hp_wmi_platform_profile_get,
+	.profile_set = hp_wmi_platform_profile_set,
+};
+
 static int thermal_profile_setup(struct platform_device *device)
 {
 	int err, tp;
@@ -1397,8 +1412,7 @@ static int thermal_profile_setup(struct platform_device *device)
 		if (err < 0)
 			return err;
 
-		platform_profile_handler.profile_get = platform_profile_omen_get;
-		platform_profile_handler.profile_set = platform_profile_omen_set;
+		platform_profile_handler.ops = &platform_profile_omen_ops;
 
 		set_bit(PLATFORM_PROFILE_COOL, platform_profile_handler.choices);
 	} else if (is_victus_thermal_profile()) {
@@ -1414,8 +1428,7 @@ static int thermal_profile_setup(struct platform_device *device)
 		if (err < 0)
 			return err;
 
-		platform_profile_handler.profile_get = platform_profile_victus_get;
-		platform_profile_handler.profile_set = platform_profile_victus_set;
+		platform_profile_handler.ops = &platform_profile_victus_ops;
 
 		set_bit(PLATFORM_PROFILE_QUIET, platform_profile_handler.choices);
 	} else {
@@ -1432,8 +1445,7 @@ static int thermal_profile_setup(struct platform_device *device)
 		if (err)
 			return err;
 
-		platform_profile_handler.profile_get = hp_wmi_platform_profile_get;
-		platform_profile_handler.profile_set = hp_wmi_platform_profile_set;
+		platform_profile_handler.ops = &hp_wmi_platform_profile_ops;
 
 		set_bit(PLATFORM_PROFILE_QUIET, platform_profile_handler.choices);
 		set_bit(PLATFORM_PROFILE_COOL, platform_profile_handler.choices);
