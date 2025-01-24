305442bd740e65c8d116a7ab1474ce135309aef0
Tejun Heo <tj@kernel.org>
2025-01-24 12:22:12 -1000

    sched_ext: Fix incorrect autogroup migration detection
    ANBZ: #13548
    
    commit d6f3e7d564b2309e1f17e709a70eca78d7ca2bb8 upstream.
    
    scx_move_task() is called from sched_move_task() and tells the BPF scheduler
    that cgroup migration is being committed. sched_move_task() is used by both
    cgroup and autogroup migrations and scx_move_task() tried to filter out
    autogroup migrations by testing the destination cgroup and PF_EXITING but
    this is not enough. In fact, without explicitly tagging the thread which is
    doing the cgroup migration, there is no good way to tell apart
    scx_move_task() invocations for racing migration to the root cgroup and an
    autogroup migration.
    
    This led to scx_move_task() incorrectly ignoring a migration from non-root
    cgroup to an autogroup of the root cgroup triggering the following warning:
    
      WARNING: CPU: 7 PID: 1 at kernel/sched/ext.c:3725 scx_cgroup_can_attach+0x196/0x340
      ...
      Call Trace:
      <TASK>
        cgroup_migrate_execute+0x5b1/0x700
        cgroup_attach_task+0x296/0x400
        __cgroup_procs_write+0x128/0x140
        cgroup_procs_write+0x17/0x30
        kernfs_fop_write_iter+0x141/0x1f0
        vfs_write+0x31d/0x4a0
        __x64_sys_write+0x72/0xf0
        do_syscall_64+0x82/0x160
        entry_SYSCALL_64_after_hwframe+0x76/0x7e
    
    Fix it by adding an argument to sched_move_task() that indicates whether the
    moving is for a cgroup or autogroup migration. After the change,
    scx_move_task() is called only for cgroup migrations and renamed to
    scx_cgroup_move_task().
    
    Link: https://github.com/sched-ext/scx/issues/370
    Fixes: 819513666966 ("sched_ext: Add cgroup support")
    Cc: stable@vger.kernel.org # v6.12+
    Acked-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Tejun Heo <tj@kernel.org>
    [ Xuchun: Fix conflict in kernel/sched/core.c ]
    Signed-off-by: Xuchun Shang <xuchun.shang@linux.alibaba.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -9055,7 +9055,7 @@ static void sched_change_group(struct task_struct *tsk)
  * now. This function just updates tsk->se.cfs_rq and tsk->se.parent to reflect
  * its new group.
  */
-void sched_move_task(struct task_struct *tsk)
+void sched_move_task(struct task_struct *tsk, bool for_autogroup)
 {
 	int queued, running, queue_flags =
 		DEQUEUE_SAVE | DEQUEUE_MOVE | DEQUEUE_NOCLOCK;
@@ -9080,7 +9080,8 @@ void sched_move_task(struct task_struct *tsk)
 		update_nr_uninterruptible(tsk, -1);
 
 	sched_change_group(tsk);
-	scx_move_task(tsk);
+	if (!for_autogroup)
+		scx_cgroup_move_task(tsk);
 
 	/* increase new group after change */
 	if ((!queued && task_contributes_to_load(tsk)) ||
@@ -9188,7 +9189,7 @@ static void cpu_cgroup_attach(struct cgroup_taskset *tset)
 	struct cgroup_subsys_state *css;
 
 	cgroup_taskset_for_each(task, css, tset)
-		sched_move_task(task);
+		sched_move_task(task, false);
 
 	scx_cgroup_finish_attach();
 }
