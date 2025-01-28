deddc9c75858310c8d33c5a283a30863e0b48047
Yury Norov <yury.norov@gmail.com>
2025-01-28 11:46:34 -0500

    cpumask: deprecate cpumask_next_wrap()
    ANBZ: #31060
    
    commit dc5bb9b769c9c3e471609a4e7444ab539c5f3f1f upstream.
    
    The next patch aligns implementation of cpumask_next_wrap() with the
    find_next_bit_wrap(), and it changes function signature.
    
    To make the transition smooth, this patch deprecates current
    implementation by adding an _old suffix. The following patches switch
    current users to the new implementation one by one.
    
    No functional changes were intended.
    
    Signed-off-by: Yury Norov <yury.norov@gmail.com>
    [Apply same change to nvme host tcp.]
    Signed-off-by: Wei Chen <wei.chen@arm.com>

--- include/linux/cpumask.h
+++ include/linux/cpumask.h
@@ -300,7 +300,7 @@ unsigned int cpumask_next_and(int n, const struct cpumask *src1p,
 
 #if NR_CPUS == 1
 static inline
-unsigned int cpumask_next_wrap(int n, const struct cpumask *mask, int start, bool wrap)
+unsigned int cpumask_next_wrap_old(int n, const struct cpumask *mask, int start, bool wrap)
 {
 	cpumask_check(start);
 	if (n != -1)
@@ -316,7 +316,7 @@ unsigned int cpumask_next_wrap(int n, const struct cpumask *mask, int start, boo
 	return cpumask_first(mask);
 }
 #else
-unsigned int __pure cpumask_next_wrap(int n, const struct cpumask *mask, int start, bool wrap);
+unsigned int __pure cpumask_next_wrap_old(int n, const struct cpumask *mask, int start, bool wrap);
 #endif
 
 /**
