82f2df1c1f84da1fdca71efc98881740f700e55c
Guangguan Wang <guangguan.wang@linux.alibaba.com>
2025-02-06 14:17:15 +0800

    anolis: net/smc: optimize SMC_LINKS_PER_LGR_MAX_PREFER
    ANBZ: #18818
    
    Optimize SMC_LINKS_PER_LGR_MAX_PREFER to 1 for anolis.
    
    Signed-off-by: Guangguan Wang <guangguan.wang@linux.alibaba.com>
    Reviewed-by: D. Wythe <alibuda@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/4611

--- net/smc/smc_core.h
+++ net/smc/smc_core.h
@@ -188,7 +188,7 @@ struct smc_link {
 #define SMC_LINKS_ADD_LNK_MAX	2	/* max. # of links per link group, also is the
 					 * default value for smc-r v1.0 and v2.0
 					 */
-#define SMC_LINKS_PER_LGR_MAX_PREFER	2	/* Preferred max links per link group used for
+#define SMC_LINKS_PER_LGR_MAX_PREFER	1	/* Preferred max links per link group used for
 						 * SMC-R v2.1 and later negotiation, vendors or
 						 * distrubutions may modify it to a value between
 						 * 1-2 as needed.
