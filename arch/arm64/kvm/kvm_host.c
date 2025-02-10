c84012292271dae87c05b6985a8420fb3e7b450e
Hou Wenlong <houwenlong.hwl@antgroup.com>
2025-02-10 17:27:19 +0800

    anolis: KVM: arm64: PMU: Move 'arm_pmus' list into kvm_host.c
    ANBZ: #4924
    
    Move the 'arm_pmus' list into kvm_host.c to allow it to be managed by
    the host when KVM is built as a module.
    
    Signed-off-by: Hou Wenlong <houwenlong.hwl@antgroup.com>
    Reviewed-by: xuejun-xj <jiyunxue@linux.alibaba.com>
    Reviewed-by: Bin Guo <guobin@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/4591

--- arch/arm64/kvm/kvm_host.c
+++ arch/arm64/kvm/kvm_host.c
@@ -3,6 +3,8 @@
  * Copyright (C) 2024 Ant Group.
  */
 #include <linux/kvm_host.h>
+#include <kvm/arm_pmu.h>
+#include <asm/arm_pmuv3.h>
 
 #include "vgic/vgic.h"
 
@@ -59,4 +61,47 @@ void __init vgic_set_kvm_info(const struct gic_kvm_info *info)
 	gic_kvm_info = kmalloc(sizeof(*info), GFP_KERNEL);
 	if (gic_kvm_info)
 		*gic_kvm_info = *info;
+}
+
+DEFINE_STATIC_KEY_FALSE(kvm_arm_pmu_available);
+
+LIST_HEAD(arm_pmus);
+DEFINE_MUTEX(arm_pmus_lock);
+
+void kvm_host_pmu_init(struct arm_pmu *pmu)
+{
+	struct arm_pmu_entry *entry;
+
+	/*
+	 * Check the sanitised PMU version for the system, as KVM does not
+	 * support implementations where PMUv3 exists on a subset of CPUs.
+	 */
+	if (!pmuv3_implemented(kvm_arm_pmu_get_pmuver_limit()))
+		return;
+
+	mutex_lock(&arm_pmus_lock);
+
+	entry = kmalloc(sizeof(*entry), GFP_KERNEL);
+	if (!entry)
+		goto out_unlock;
+
+	entry->arm_pmu = pmu;
+	list_add_tail(&entry->entry, &arm_pmus);
+
+	if (list_is_singular(&arm_pmus))
+		static_branch_enable(&kvm_arm_pmu_available);
+
+out_unlock:
+	mutex_unlock(&arm_pmus_lock);
+}
+
+u8 kvm_arm_pmu_get_pmuver_limit(void)
+{
+	u64 tmp;
+
+	tmp = read_sanitised_ftr_reg(SYS_ID_AA64DFR0_EL1);
+	tmp = cpuid_feature_cap_perfmon_field(tmp,
+					      ID_AA64DFR0_EL1_PMUVer_SHIFT,
+					      ID_AA64DFR0_EL1_PMUVer_V3P5);
+	return FIELD_GET(ARM64_FEATURE_MASK(ID_AA64DFR0_EL1_PMUVer), tmp);
 }
