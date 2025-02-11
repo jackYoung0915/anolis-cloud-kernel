9e2736ae781be224d505a6f5f87ed79bffe77ddf
Hou Wenlong <houwenlong.hwl@antgroup.com>
2025-02-11 14:59:07 +0800

    anolis: KVM: arm64: Introduce new config to export symbol for KVM module
    ANBZ: #4924
    
    When KVM is built as a module, some host kernel symbols need to be
    exported for KVM, which seems a little unexpected. Therefore, use the
    symbol namespace to limit the usage of exported symbols exclusively for
    KVM.
    
    Signed-off-by: Hou Wenlong <houwenlong.hwl@antgroup.com>
    Reviewed-by: xuejun-xj <jiyunxue@linux.alibaba.com>
    Reviewed-by: Bin Guo <guobin@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/4591
    [rebase 6.6.102]

--- include/linux/export.h
+++ include/linux/export.h
@@ -96,4 +96,10 @@ extern struct module __this_module;
 
 #define EXPORT_SYMBOL_GPL_FOR_MODULES(sym, mods) __EXPORT_SYMBOL(sym, "GPL", "module:" mods)
 
+#ifdef CONFIG_KVM_EXPORT_SYMBOL
+#define EXPORT_SYMBOL_FOR_KVM(sym)	EXPORT_SYMBOL_NS(sym, KVM)
+#else
+#define EXPORT_SYMBOL_FOR_KVM(sym)
+#endif
+
 #endif /* _LINUX_EXPORT_H */
