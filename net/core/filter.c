c055635896235a100c2f25553d2b1bc32944c662
Philo Lu <lulie@linux.alibaba.com>
2025-02-12 17:05:11 +0800

    anolis: bpf: add bpf_anolis_ipv6_addr_set kfunc for vtoa
    ANBZ: #8068
    
    This kfunc is used by vtoa-bpf to make getpeername() return ipv6
    address in ipv4 network.
    
    inet_getname() should be updated to make this actually take effect.
    
    This patch is derived from ANCK-5.10 [0], which uses a private helper.
    Now we use kfunc to achieve the same functionality.
    
    [0] https://gitee.com/anolis/cloud-kernel/pulls/2701
    
    Signed-off-by: Philo Lu <lulie@linux.alibaba.com>
    Reviewed-by: Xuan Zhuo <xuanzhuo@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/4604

--- net/core/filter.c
+++ net/core/filter.c
@@ -11892,6 +11892,29 @@ __bpf_kfunc int bpf_dynptr_from_xdp(struct xdp_buff *xdp, u64 flags,
 	return 0;
 }
 
+__bpf_kfunc int bpf_anolis_ipv6_addr_set(struct bpf_sock_addr_kern *ctx,
+					 struct in6_addr *addr,
+					 size_t addr_len__sz)
+{
+	struct sockaddr_in6 *sin = (struct sockaddr_in6 *)ctx->uaddr;
+
+	if (ctx->sk->sk_family != AF_INET)
+		return -EINVAL;
+
+	if (addr_len__sz == 0 || addr_len__sz > sizeof(sin->sin6_addr))
+		return -EINVAL;
+
+	/* sin->sin6_port should be set directly in bpf prog without helper */
+	sin->sin6_family = AF_INET6;
+	sin->sin6_flowinfo = 0;
+	sin->sin6_scope_id = 0;
+
+	memcpy(&sin->sin6_addr, addr, addr_len__sz);
+	ctx->uaddrlen = sizeof(struct sockaddr_in6);
+
+	return 0;
+}
+
 __bpf_kfunc int bpf_sock_addr_set_sun_path(struct bpf_sock_addr_kern *sa_kern,
 					   const u8 *sun_path, u32 sun_path__sz)
 {
@@ -11938,6 +11961,7 @@ BTF_KFUNCS_END(bpf_kfunc_check_set_xdp)
 
 BTF_KFUNCS_START(bpf_kfunc_check_set_sock_addr)
 BTF_ID_FLAGS(func, bpf_sock_addr_set_sun_path)
+BTF_ID_FLAGS(func, bpf_anolis_ipv6_addr_set)
 BTF_KFUNCS_END(bpf_kfunc_check_set_sock_addr)
 
 static const struct btf_kfunc_id_set bpf_kfunc_set_skb = {
