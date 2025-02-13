eb9ac0ff0e5992be438d87b6e5ec272d44777e31
Guanjun <guanjun@linux.alibaba.com>
2025-02-13 16:40:52 +0800

    anolis: mm: duptext: avoid writes to file with duplicated pages
    ANBZ: #19420
    
    NOTE: this is an experimental feature.
    
    This avoids writes to duplicated pages by dropping the page cache when
    the file with duplicated pages is open for write.
    
    Signed-off-by: Xu Yu <xuyu@linux.alibaba.com>
    Reviewed-by: zhongjiang-ali <zhongjiang-ali@linux.alibaba.com>
    Reviewed-by: Gang Deng <gavin.dg@linux.alibaba.com>
    Signed-off-by: Guanjun <guanjun@linux.alibaba.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/4791

--- fs/open.c
+++ fs/open.c
@@ -953,6 +953,10 @@ static int do_dentry_open(struct file *f,
 	/*
 	 * XXX: Huge page cache doesn't support writing yet. Drop all page
 	 * cache for this file before processing writes.
+	 *
+	 * XXX: Duptext doesn't support writing yet. Drop all page
+	 * cache for this file before processing writes. TODO: Only drop
+	 * duplicated pages.
 	 */
 	if (f->f_mode & FMODE_WRITE) {
 		/*
@@ -962,7 +966,8 @@ static int do_dentry_open(struct file *f,
 		 * of THPs into the page cache will fail.
 		 */
 		smp_mb();
-		if (filemap_nr_thps(inode->i_mapping)) {
+		if (filemap_nr_thps(inode->i_mapping) ||
+		    filemap_nr_duptext(inode->i_mapping)) {
 			struct address_space *mapping = inode->i_mapping;
 
 			filemap_invalidate_lock(inode->i_mapping);
