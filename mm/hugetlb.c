2f44d4db0043f55299096bbb2abc27ee665721e5
Jinjiang Tu <tujinjiang@huawei.com>
2025-02-14 14:48:19 +0800

    mm/pfn_range_alloc: add tracepoint for hugetlb_pool_alloc and hugetlb_pool_free
    ANCK ID: #IJSBBS
    
    commit 032f7429838dff81a67787268a9b292799b0ccbc openEuler.
    
    Add tracepoint for hugetlb_pool_alloc() and hugetlb_pool_free().
    
    Signed-off-by: Jinjiang Tu <tujinjiang@huawei.com>
    (cherry picked from commit 032f7429838dff81a67787268a9b292799b0ccbc)
    Signed-off-by: Liu Jian <liujian56@huawei.com>

--- mm/hugetlb.c
+++ mm/hugetlb.c
@@ -35,6 +35,9 @@
 #include <linux/delayacct.h>
 #include <linux/memory.h>
 #include <linux/mm_inline.h>
+#ifndef __GENKSYMS__
+#include <trace/events/kmem.h>
+#endif
 
 #include <asm/page.h>
 #include <asm/pgalloc.h>
@@ -7788,6 +7791,7 @@ struct folio *hugetlb_pool_alloc(int nid)
 		folio = ERR_PTR(-ENOMEM);
 
 out:
+	trace_hugetlb_pool_alloc(folio, nid);
 	return folio;
 }
 EXPORT_SYMBOL_GPL(hugetlb_pool_alloc);
@@ -7802,6 +7806,7 @@ int hugetlb_pool_free(struct folio *folio)
 	ret = 0;
 	folio_put(folio);
 out:
+	trace_hugetlb_pool_free(folio, ret);
 	return ret;
 }
 EXPORT_SYMBOL_GPL(hugetlb_pool_free);
