25575c0bf4325caa70be6290b89793f6044b895b
Matthew Wilcox (Oracle) <willy@infradead.org>
2025-02-17 19:20:06 +0000

    fs: convert block_commit_write() to take a folio
    ANBZ: #21672
    
    commit 63a23847dc47113b879a5f53cc0ca5cedc881ffd upstream.
    
    All callers now have a folio, so pass it in instead of converting
    folio->page->folio.
    
    Link: https://lkml.kernel.org/r/20250217192009.437916-1-willy@infradead.org
    Signed-off-by: Matthew Wilcox (Oracle) <willy@infradead.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    [ code conflicts fixes for ocfs2 and udf ]
    Signed-off-by: Joseph Qi <joseph.qi@linux.alibaba.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Reviewed-by: Jingbo Xu <jefflexu@linux.alibaba.com>
    Reviewed-by: Jia He <justin.he@arm.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5492

--- fs/iomap/buffered-io.c
+++ fs/iomap/buffered-io.c
@@ -1427,7 +1427,7 @@ static loff_t iomap_folio_mkwrite_iter(struct iomap_iter *iter,
 					      &iter->iomap);
 		if (ret)
 			return ret;
-		block_commit_write(&folio->page, 0, length);
+		block_commit_write(folio, 0, length);
 	} else {
 		WARN_ON_ONCE(!folio_test_uptodate(folio));
 		folio_mark_dirty(folio);
