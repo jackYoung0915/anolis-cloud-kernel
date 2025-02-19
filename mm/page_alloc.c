jeevandeepj994/anolis_perf

anolis: mm: support allocating page table pages binded with local numa node
ANBZ: #6618

Currently page table can not be migrated through numa balancing. If most of page table
pages are located on remote numa node, the performance can be degraded in some scenarios.
Thus this patch provides a way to allocate page table pages in local numa node with
reserved memory range.

To switch on the pgtable bind feature globally:
echo 2 > /sys/kernel/mm/pgtable_bind/enabled

Only for misplaced page table pages statisitcs
echo 1 > /sys/kernel/mm/pgtable_bind/enabled

To enable the pgtable bind feature of cgroup level:
echo 1 > /sys/fs/cgroup/memory/<online_memcg>/memory.pgtable_bind

To get the amount of misplaced page table pages
cat /sys/fs/cgroup/memory/<online_memcg>/memory.pgtable_misplaced
Besides, if needs to reset the value of pgtable_misplaced
echo 0 > /sys/fs/cgroup/memory/<online_memcg>/memory.pgtable_misplaced

Signed-off-by: Kaihao Bai <carlo.bai@linux.alibaba.com>
Reviewed-by: Xu Yu <xuyu@linux.alibaba.com>

mm/page_alloc.c
@@ -4196,6 +4196,24 @@ try_this_zone:
 		if (page) {
 			prep_new_page(page, order, gfp_mask, alloc_flags);
 
+#ifdef CONFIG_PGTABLE_BIND
+			/*
+			 * If allocated page belongs to remote numa node,
+			 * accumulate memcg->ck_reserved2 to show how many pages
+			 * are from remote node.
+			 */
+			if ((gfp_mask & __GFP_PGTABLE) &&
+			    (zone_to_nid(ac->preferred_zoneref->zone) != zone_to_nid(zone))) {
+				struct mem_cgroup *memcg;
+
+				memcg = get_mem_cgroup_from_mm(current->mm);
+				if (memcg) {
+					memcg->pgtable_misplaced++;
+					css_put(&memcg->css);
+				}
+			}
+#endif
+
 			/*
 			 * If this is a high-order atomic allocation then check
 			 * if the pageblock should be reserved for the future
@@ -5279,6 +5297,16 @@ __alloc_pages_nodemask(gfp_t gfp_mask, unsigned int order, int preferred_nid,
 	 * &cpuset_current_mems_allowed to optimize the fast-path attempt.
 	 */
 	ac.nodemask = nodemask;
+
+	/*
+	 * Restore the __GFP_THISNODE restriction if current allocation is page
+	 * table.
+	 */
+	if (gfp_mask & __GFP_PGTABLE) {
+		gfp_mask &= ~__GFP_THISNODE;
+		alloc_mask &= ~__GFP_THISNODE;
+		ac.zonelist = node_zonelist(preferred_nid, gfp_mask);
+	}
 
 	page = __alloc_pages_slowpath(alloc_mask, order, &ac);
 

jackYoung0915/anolis-cloud-kernel

mm/page_alloc.c
@@ -3365,6 +3365,24 @@ try_this_zone:
 		if (page) {
 			prep_new_page(page, order, gfp_mask, alloc_flags);
 
+#ifdef CONFIG_PGTABLE_BIND
+			/*
+			 * If allocated page belongs to remote numa node,
+			 * accumulate memcg->ck_reserved2 to show how many pages
+			 * are from remote node.
+			 */
+			if ((gfp_mask & __GFP_PGTABLE) &&
+			    (zone_to_nid(ac->preferred_zoneref->zone) != zone_to_nid(zone))) {
+				struct mem_cgroup *memcg;
+
+				memcg = get_mem_cgroup_from_mm(current->mm);
+				if (memcg) {
+					memcg->pgtable_misplaced++;
+					css_put(&memcg->css);
+				}
+			}
+#endif
+
 			/*
 			 * If this is a high-order atomic allocation then check
 			 * if the pageblock should be reserved for the future
@@ -4650,6 +4668,16 @@ struct page *__alloc_pages(gfp_t gfp, unsigned int order, int preferred_nid,
 	 * &cpuset_current_mems_allowed to optimize the fast-path attempt.
 	 */
 	ac.nodemask = nodemask;
+
+	/*
+	 * Restore the __GFP_THISNODE restriction if current allocation is page
+	 * table.
+	 */
+	if (gfp & __GFP_PGTABLE) {
+		gfp &= ~__GFP_THISNODE;
+		alloc_gfp &= ~__GFP_THISNODE;
+		ac.zonelist = node_zonelist(preferred_nid, gfp);
+	}
 
 	page = __alloc_pages_slowpath(alloc_gfp, order, &ac);
 