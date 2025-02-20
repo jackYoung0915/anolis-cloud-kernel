9d056b81e6e8d4b82a4ff8143bdfe29d7a1d0fa1
Gao Chen <gaochen@wxiat.com>
2025-02-20 17:05:34 +0800

    anolis: sw64: fix ftrace_make_call & ftrace_make_nop
    ANBZ: #4688
    
    This patch fix the bug by reorganizing the instruction sequence.
    
    Signed-off-by: Gao Chen <gaochen@wxiat.com>
    Reviewed-by: He Sheng <hesheng@wxiat.com>
    Signed-off-by: Gu Zitao <guzitao@wxiat.com>

--- drivers/irqchip/irq-sunway-cpu.c
+++ drivers/irqchip/irq-sunway-cpu.c
@@ -82,9 +82,6 @@ asmlinkage void noinstr do_entInt(unsigned long type, unsigned long vector,
 	struct pt_regs *old_regs;
 	extern char __idle_start[], __idle_end[];
 
-	if (*((u32 *)(regs->pc)) >> 16 == 0x79c)
-		regs->pc -= 4;
-
 	/* restart idle routine if it is interrupted */
 	if (regs->pc > (u64)__idle_start && regs->pc < (u64)__idle_end)
 		regs->pc = (u64)__idle_start;
