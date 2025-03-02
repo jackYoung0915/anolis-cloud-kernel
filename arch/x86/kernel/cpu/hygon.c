62cb6fd6d93510a0c7006b856fb4ce15c42f87d1
Liao Xuan <liaoxuan@hygon.cn>
2025-03-02 04:40:13 -0500

    anolis: x86/cpu/hygon: Refactor the CPU topology deriving method for Hygon
    ANBZ: #19177
    
    Refactor the CPU topology deriving method to be compatible
    for all the processors since Hygon family 18h model 4h.
    
    Signed-off-by: Liao Xuan <liaoxuan@hygon.cn>

--- arch/x86/kernel/cpu/hygon.c
+++ arch/x86/kernel/cpu/hygon.c
@@ -85,16 +85,11 @@ static void hygon_get_topology(struct cpuinfo_x86 *c)
 		if (smp_num_siblings > 1)
 			c->x86_max_cores /= smp_num_siblings;
 
-		switch (c->x86_model) {
-		case 0x0 ... 0x3:
-			if (boot_cpu_has(X86_FEATURE_HYPERVISOR))
-				break;
+		if (c->x86 == 0x18 && c->x86_model < 0x4 &&
+		    !boot_cpu_has(X86_FEATURE_HYPERVISOR)) {
 			/* Socket ID is ApicId[6] for these processors. */
 			c->phys_proc_id = c->apicid >> APICID_SOCKET_ID_BIT;
-			break;
-		case 0x4:
-		case 0x5:
-		case 0x6:
+		} else {
 			/*
 			 * In case leaf 0xB is available, use it to derive
 			 * topology information.
@@ -104,9 +99,6 @@ static void hygon_get_topology(struct cpuinfo_x86 *c)
 				c->x86_coreid_bits =
 					get_count_order(c->x86_max_cores);
 			__max_die_per_package = nodes_per_socket;
-			break;
-		default:
-			break;
 		}
 
 		cacheinfo_hygon_init_llc_id(c, cpu);
