c3c1236ff96d6f86d42cf1ff6a0acaa537998cae
Jinjiang Tu <tujinjiang@huawei.com>
2025-03-03 15:17:58 +0800

    mm/numa_remote: introduce hugetlb_nowatermark mode for remote node
    ANCK ID: #IJSBBS
    
    commit 3a1587ef612ce9ccc0e13c61fa5ea26a9736f792 openEuler.
    
    In hugetlb_nowatermark mode, alloc_pool_huge_page() for remote node will
    ignore the watermark limit.
    
    Signed-off-by: Jinjiang Tu <tujinjiang@huawei.com>
    (cherry picked from commit 3a1587ef612ce9ccc0e13c61fa5ea26a9736f792)
    Signed-off-by: Liu Jian <liujian56@huawei.com>

--- include/linux/mmzone.h
+++ include/linux/mmzone.h
@@ -1581,6 +1581,8 @@ static inline bool zone_is_zone_extmem(struct zone *zone)
 {
 	return zone_idx(zone) == ZONE_EXTMEM;
 }
+
+#define get_extmem_zone(nid) (&NODE_DATA((nid))->node_zones[ZONE_EXTMEM])
 #else
 static inline bool is_zone_extmem_page(const struct page *page)
 {
@@ -1591,6 +1593,8 @@ static inline bool zone_is_zone_extmem(struct zone *zone)
 {
 	return false;
 }
+
+#define get_extmem_zone(nid) NULL
 #endif
 
 /*
