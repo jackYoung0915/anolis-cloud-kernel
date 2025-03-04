780e813ae49657ed3467fae666c1461831314dff
Andrew Jones <ajones@ventanamicro.com>
2025-03-04 13:00:16 +0100

    riscv: Annotate unaligned access init functions
    ANBZ: #32730
    
    commit a00e022be5315c5a1f47521a1cc6d3b71c8e9c44 upstream.
    
    Several functions used in unaligned access probing are only run at
    init time. Annotate them appropriately.
    
    Fixes: f413aae96cda ("riscv: Set unaligned access speed at compile time")
    Reviewed-by: Alexandre Ghiti <alexghiti@rivosinc.com>
    Signed-off-by: Andrew Jones <ajones@ventanamicro.com>
    Link: https://lore.kernel.org/r/20250304120014.143628-11-ajones@ventanamicro.com
    Signed-off-by: Alexandre Ghiti <alexghiti@rivosinc.com>
    [Ruidong:
    Conflicts:
      resolve: arch/riscv/include/asm/cpufeature.h
      reason:
        Context conflict only, patch content unchanged
    ]
    Signed-off-by: Ruidong Tian <tianruidong@linux.alibaba.com>
    Acked-by: Palmer Dabbelt <palmer@rivosinc.com>
    Acked-by: Jakub Kicinski <kuba@kernel.org>
    Reviewed-by: Simon Horman <horms@kernel.org>
    Reviewed-by: Clément Léger <cleger@rivosinc.com>
    Reviewed-by: Fangyu Yu <fangyu.yu@linux.alibaba.com>
    Reviewed-by: Rui Gao <gao.rui@zte.com.cn>
    Reviewed-by: Jiankang Chen <jkchen@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6707

--- arch/riscv/include/asm/cpufeature.h
+++ arch/riscv/include/asm/cpufeature.h
@@ -85,7 +85,7 @@ static __always_inline bool riscv_cpu_has_extension_unlikely(int cpu, const unsi
 	return __riscv_isa_extension_available(hart_isa[cpu].isa, ext);
 }
 
-bool check_unaligned_access_emulated_all_cpus(void);
+bool __init check_unaligned_access_emulated_all_cpus(void);
 #if defined(CONFIG_RISCV_SCALAR_MISALIGNED)
 void check_unaligned_access_emulated(struct work_struct *work __always_unused);
 void unaligned_emulation_finish(void);
@@ -98,7 +98,7 @@ static inline bool unaligned_ctl_available(void)
 }
 #endif
 
-bool check_vector_unaligned_access_emulated_all_cpus(void);
+bool __init check_vector_unaligned_access_emulated_all_cpus(void);
 #if defined(CONFIG_RISCV_VECTOR_MISALIGNED)
 void check_vector_unaligned_access_emulated(struct work_struct *work __always_unused);
 DECLARE_PER_CPU(long, vector_misaligned_access);
