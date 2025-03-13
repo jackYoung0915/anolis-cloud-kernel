e16680e6b0da4c76c651b15d31d387c86a491999
Zelin Deng <zelin.deng@linux.alibaba.com>
2025-03-13 13:30:45 +0800

    anolis: mm: Add refcnt in case messy dup seq count
    ANBZ: #19611
    
    The assumption of mm->write_protected_seq lock is only parent who holds
    mmap write lock will do copy page range. However this assumption has
    changed after async fork is introduced: copy page range could be done by
    both parent and child concurrently without holding parent's mmap write
    lock, mm->write_protected_seq lock will be messy when this case happens.
    
    Add refcnt to prevent dup seq lock/unlock, the premise is: before async
    fork has been done, any copy page range operation has to take the lock,
    so anyone has taken the lock do not take it again, all has done copy
    page range the lock has to be released.
    
    Signed-off-by: Zelin Deng <zelin.deng@linux.alibaba.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/4855

--- include/linux/mm_types.h
+++ include/linux/mm_types.h
@@ -1003,6 +1003,7 @@ struct mm_struct {
 		/* Pairing mm_struct ptr and its flags */
 		struct mm_struct *async_fork_mm;
 		unsigned long async_fork_flags;
+		atomic_t async_fork_refcnt;
 #endif
 	} __randomize_layout;
 
