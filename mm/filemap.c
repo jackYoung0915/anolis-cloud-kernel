bf506425d87be3bca8d1a1ca3831481be99df0fe
Guanjun <guanjun@linux.alibaba.com>
2025-03-14 11:23:57 +0800

    anolis: mm: duptext: Fix potential dup folio's race condition during page faults
    ANBZ: #19420
    
    A race condition could occur when two concurrent page faults attempted to get the
    same duplicated folio. This commit adds folio lock to synchronize access to the
    duplicated folio.
    
    Signed-off-by: Guanjun <guanjun@linux.alibaba.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/4791

--- mm/filemap.c
+++ mm/filemap.c
@@ -3810,6 +3810,7 @@ vm_fault_t filemap_map_pages(struct vm_fault *vmf,
 			folio_unlock(folio);
 			folio_put(folio);
 			folio = d_folio;
+			folio_lock(folio);
 		}
 
 		if (!folio_test_large(folio))
@@ -3820,8 +3821,7 @@ vm_fault_t filemap_map_pages(struct vm_fault *vmf,
 					xas.xa_index - folio->index, addr,
 					nr_pages, &rss, &mmap_miss);
 
-		if (!d_folio)
-			folio_unlock(folio);
+		folio_unlock(folio);
 
 		/* Dup slave folio should also put refcnt here */
 		folio_put(folio);
