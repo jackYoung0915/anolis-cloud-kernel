9644a7ada207a8c58e1a7954b670f68216ee8d33
Ojaswin Mujoo <ojaswin@linux.ibm.com>
2025-03-18 13:22:57 +0530

    ext4: Make sb update interval tunable
    ANBZ: #34001
    
    commit 896b02d0b9e7deb4a4eb365e13dd912b49916519 upstream.
    
    Currently, outside error paths, we auto commit the super block after 1
    hour has passed and 16MB worth of updates have been written since last
    commit. This is a policy decision so make this tunable while keeping the
    defaults same. This is useful if user wants to tweak the superblock
    behavior or for debugging the codepath by allowing to trigger it more
    frequently.
    We can now tweak the super block update using sb_update_sec and
    sb_update_kb files in /sys/fs/ext4/<dev>/
    
    Reviewed-by: Jan Kara <jack@suse.cz>
    Reviewed-by: Ritesh Harjani (IBM) <ritesh.list@gmail.com>
    Reviewed-by: Baokun Li <libaokun1@huawei.com>
    Signed-off-by: Ojaswin Mujoo <ojaswin@linux.ibm.com>
    Link: https://patch.msgid.link/950fb8c9b2905620e16f02a3b9eeea5a5b6cb87e.1742279837.git.ojaswin@linux.ibm.com
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    Signed-off-by: Xianglai Li <lixianglai@loongson.cn>
    Signed-off-by: Juxin Gao <gaojuxin@loongson.cn>
    Acked-by: Joseph Qi <joseph.qi@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6823

--- fs/ext4/ext4.h
+++ fs/ext4/ext4.h
@@ -1588,6 +1588,8 @@ struct ext4_sb_info {
 	unsigned int s_mb_prefetch;
 	unsigned int s_mb_prefetch_limit;
 	unsigned int s_mb_best_avail_max_trim_order;
+	unsigned int s_sb_update_sec;
+	unsigned int s_sb_update_kb;
 
 	/* where last allocation was done - for stream allocation */
 	ext4_group_t *s_mb_last_groups;
@@ -2269,6 +2271,13 @@ static inline int ext4_forced_shutdown(struct super_block *sb)
  */
 #define EXT4_DEF_MIN_BATCH_TIME	0
 #define EXT4_DEF_MAX_BATCH_TIME	15000 /* 15ms */
+
+/*
+ * Default values for superblock update
+ */
+#define EXT4_DEF_SB_UPDATE_INTERVAL_SEC (3600) /* seconds (1 hour) */
+#define EXT4_DEF_SB_UPDATE_INTERVAL_KB (16384) /* kilobytes (16MB) */
+
 
 /*
  * Minimum number of groups in a flexgroup before we separate out
