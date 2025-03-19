afe20fb9510edf54a531e5731206b6d59ffe0a98
Rik van Riel <riel@surriel.com>
2025-03-19 11:08:26 +0100

    x86/mm: Add INVLPGB feature and Kconfig entry
    ANBZ: #26519
    
    commit 767ae437a32d644786c0779d0d54492ff9cbe574 upstream
    
    In addition, the CPU advertises the maximum number of pages that can be
    shot down with one INVLPGB instruction in CPUID. Save that information
    for later use.
    
      [ bp: use cpu_has(), typos, massage. ]
    
    Signed-off-by: Rik van Riel <riel@surriel.com>
    Signed-off-by: Borislav Petkov (AMD) <bp@alien8.de>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Link: https://lore.kernel.org/r/20250226030129.530345-3-riel@surriel.com
    Signed-off-by: Abhishek Rajput <Abhishek.Rajput@amd.com>
    Signed-off-by: mohanasv <mohanasv@amd.com>

--- arch/x86/kernel/cpu/amd.c
+++ arch/x86/kernel/cpu/amd.c
@@ -28,6 +28,8 @@
 
 #include "cpu.h"
 
+u16 invlpgb_count_max __ro_after_init;
+
 /*
  * nodes_per_socket: Stores the number of nodes per socket.
  * Refer to Fam15h Models 00-0fh BKDG - CPUID Fn8000_001E_ECX
@@ -1279,6 +1281,10 @@ static void cpu_detect_tlb_amd(struct cpuinfo_x86 *c)
 		tlb_lli_2m[ENTRIES] = eax & mask;
 
 	tlb_lli_4m[ENTRIES] = tlb_lli_2m[ENTRIES] >> 1;
+
+	/* Max number of pages INVLPGB can invalidate in one shot */
+	if (cpu_has(c, X86_FEATURE_INVLPGB))
+		invlpgb_count_max = (cpuid_edx(0x80000008) & 0xffff) + 1;
 }
 
 static const struct cpu_dev amd_cpu_dev = {
