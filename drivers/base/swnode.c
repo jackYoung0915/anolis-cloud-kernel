bdeccda597fc3ef2a30bdc9a22487efdb1b6e042
Lizhi Xu <lizhi.xu@windriver.com>
2025-04-14 15:11:23 +0800

    software node: Prevent link creation failure from causing kobj reference count imbalance
    ANBZ: #13617
    
    commit bc2c46426f2d95e58c82f394531afdd034c8706c upstream.
    
    syzbot reported a uaf in software_node_notify_remove. [1]
    
    When any of the two sysfs_create_link() in software_node_notify() fails,
    the swnode->kobj reference count will not increase normally, which will
    cause swnode to be released incorrectly due to the imbalance of kobj reference
    count when executing software_node_notify_remove().
    
    Increase the reference count of kobj before creating the link to avoid uaf.
    
    [1]
    BUG: KASAN: slab-use-after-free in software_node_notify_remove+0x1bc/0x1c0 drivers/base/swnode.c:1108
    Read of size 1 at addr ffff888033c08908 by task syz-executor105/5844
    Freed by task 5844:
     software_node_notify_remove+0x159/0x1c0 drivers/base/swnode.c:1106
     device_platform_notify_remove drivers/base/core.c:2387 [inline]
    
    Fixes: 9eb59204d519 ("iommufd/selftest: Add set_dev_pasid in mock iommu")
    Reported-by: syzbot+2ff22910687ee0dfd48e@syzkaller.appspotmail.com
    Closes: https://syzkaller.appspot.com/bug?extid=2ff22910687ee0dfd48e
    Tested-by: syzbot+2ff22910687ee0dfd48e@syzkaller.appspotmail.com
    Signed-off-by: Lizhi Xu <lizhi.xu@windriver.com>
    Reviewed-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Reviewed-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Link: https://lore.kernel.org/r/20250414071123.1228331-1-lizhi.xu@windriver.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Shuai Xue <xueshuai@linux.alibaba.com>
    Reviewed-by: Shuai Xue <xueshuai@linxu.alibaba.com>
    Reviewed-by: Guanghui Feng <guanghuifeng@linux.alibaba.com>
    Reviewed-by: Jay Chen <jkchen@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5822

--- drivers/base/swnode.c
+++ drivers/base/swnode.c
@@ -1068,6 +1068,7 @@ void software_node_notify(struct device *dev)
 	if (!swnode)
 		return;
 
+	kobject_get(&swnode->kobj);
 	ret = sysfs_create_link(&dev->kobj, &swnode->kobj, "software_node");
 	if (ret)
 		return;
@@ -1077,8 +1078,6 @@ void software_node_notify(struct device *dev)
 		sysfs_remove_link(&dev->kobj, "software_node");
 		return;
 	}
-
-	kobject_get(&swnode->kobj);
 }
 
 void software_node_notify_remove(struct device *dev)
