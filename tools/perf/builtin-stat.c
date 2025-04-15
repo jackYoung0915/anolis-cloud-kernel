d961a3031865be3daabcb13e639c51303b04202c
Li Linhang <lilinhang@linux.alibaba.com>
2025-04-15 14:31:14 +0800

    anolis: perf stat: fix on "enable ignore_missing_thread"
    ANBZ: #19874
    
    This patch fixes ignore_missing_thread in perf stat, which enabled by
    commit 448ce0e6ea93ae99e0b36055e5f5a3f723fe3665, when working on
    multiple-event monitoring case.
    
    The commit 448ce0e enables ignore_missing_thread on perf stat,
    but only enables it on the first evsel in evlist. It works only when
    thread dies before the first event is opened. When a thread dies when
    opening the following events while monitoring more than one events, perf
    still returns a sys_perf_event_open failure with 3(No such process).
    
    This patch makes it work on all events, by enable ignore_missing_thread
    on all evsels, and fix fd using when fd is removed by
    ignore_missing_thread.
    
    Signed-off-by: Li Linhang <lilinhang@linux.alibaba.com>

--- tools/perf/builtin-stat.c
+++ tools/perf/builtin-stat.c
@@ -2486,6 +2486,7 @@ int cmd_stat(int argc, const char **argv)
 	unsigned int interval, timeout;
 	const char * const stat_subcommands[] = { "record", "report" };
 	char errbuf[BUFSIZ];
+	struct evsel *evsel;
 
 	setlocale(LC_ALL, "");
 
@@ -2832,7 +2833,9 @@ int cmd_stat(int argc, const char **argv)
 		goto out;
 
 	/* Enable ignoring missing threads when -p option is defined. */
-	evlist__first(evsel_list)->ignore_missing_thread = target.pid;
+	evlist__for_each_entry(evsel_list, evsel) {
+		evsel->ignore_missing_thread = target.pid;
+	}
 	status = 0;
 	for (run_idx = 0; forever || run_idx < stat_config.run_count; run_idx++) {
 		if (stat_config.run_count != 1 && verbose > 0)
