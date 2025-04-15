3f02406fc4ae828c4680567059e85c0bce03e717
Jia He <justin.he@arm.com>
2025-04-15 08:16:55 +0000

    iomap: pass the iomap to the punch callback
    ANBZ: #20084
    
    commit 492f53758fad4fde3e9a98696780f8b53f87cdae upstream.
    
    XFS will need to look at the flags in the iomap structure, so pass it
    down all the way to the callback.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Link: https://lore.kernel.org/r/20240910043949.3481298-5-hch@lst.de
    Reviewed-by: Darrick J. Wong <djwong@kernel.org>
    Signed-off-by: Christian Brauner <brauner@kernel.org>
    Signed-off-by: Jia He <justin.he@arm.com>

--- fs/xfs/xfs_iomap.c
+++ fs/xfs/xfs_iomap.c
@@ -1194,7 +1194,8 @@ static int
 xfs_buffered_write_delalloc_punch(
 	struct inode		*inode,
 	loff_t			offset,
-	loff_t			length)
+	loff_t			length,
+	struct iomap            *iomap)
 {
 	return xfs_bmap_punch_delalloc_range(XFS_I(inode), offset,
 			offset + length);
