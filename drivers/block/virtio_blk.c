jackYoung0915/anolis-cloud-kernel

commit 4ea1f3ef75030550e524825ab61bb177a8f1a0d3

anolis: virtio-blk: add no_align extra feature bit
ANBZ: #19447

Add NO_ALIGN support feature bit at ext-feature bit-1. This controls
blk-mq dma alignment(0).

Signed-off-by: Ferry Meng <mengferry@linux.alibaba.com>
Acked-by: Xuan Zhuo <xuanzhuo@linux.alibaba.com>
Reviewed-by: Jingbo Xu <jefflexu@linux.alibaba.com>
Reviewed-by: Joseph Qi <joseph.qi@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/4799

drivers/block/virtio_blk.c
@@ -141,6 +141,7 @@ struct virtio_blk {
 
 #ifdef CONFIG_VIRTIO_BLK_RING_PAIR
 	bool ring_pair;
+	bool no_algin;
 	/* saved indirect desc pointer, dma_addr and dma_len for SQ */
 	struct virtblk_indir_desc **indir_desc;
 #endif
@@ -1291,6 +1292,9 @@ int check_ext_feature(struct virtio_blk *vblk, void __iomem *ioaddr,
 	vblk->ring_pair = !!(*host_ext_features & VIRTIO_BLK_EXT_F_RING_PAIR);
 	if (vblk->ring_pair)
 		*guest_ext_features |= (VIRTIO_BLK_EXT_F_RING_PAIR);
+	vblk->no_algin = !!(*host_ext_features & VIRTIO_BLK_EXT_F_RING_NO_ALIGN);
+	if (vblk->no_algin)
+		*guest_ext_features |= (VIRTIO_BLK_EXT_F_RING_NO_ALIGN);
 
 	return 0;
 }
@@ -1479,6 +1483,7 @@ static int init_vq(struct virtio_blk *vblk)
 
 #ifdef CONFIG_VIRTIO_BLK_RING_PAIR
 	vblk->ring_pair = false;
+	vblk->no_algin = false;
 
 	if (!virtblk_rpair_disable)
 		err = init_vq_rpair(vblk);
@@ -2264,6 +2269,11 @@ static int virtblk_probe(struct virtio_device *vdev)
 		max_size = min(max_size, v);
 
 	blk_queue_max_segment_size(q, max_size);
+
+#ifdef CONFIG_VIRTIO_BLK_RING_PAIR
+	if (vblk->no_algin)
+		blk_queue_dma_alignment(q, 0);
+#endif
 
 	/* Host can optionally specify the block size of the device */
 	err = virtio_cread_feature(vdev, VIRTIO_BLK_F_BLK_SIZE,

drivers/block/virtio_blk_ext.h
@@ -5,6 +5,7 @@
 
 /* ext feature bit definition */
 #define VIRTIO_BLK_EXT_F_RING_PAIR	(1U << 0)
+#define VIRTIO_BLK_EXT_F_RING_NO_ALIGN	(1U << 1)
 #define VIRTIO_BLK_EXT_F_INVAL		(-1)
 
 #ifdef CONFIG_VIRTIO_BLK_RING_PAIR

commit 5500e7f97cea029e5e4717c69484d2b151275d78

anolis: virtio-blk: fix typo of no_align
ANBZ: #19447

no_align was mistakenly written as no_algin. Now fix it.

Fixes: 4ea1f3ef7503 ("anolis: virtio-blk: add no_align extra feature bit")
Signed-off-by: Ferry Meng <mengferry@linux.alibaba.com>

drivers/block/virtio_blk.c
@@ -145,7 +145,7 @@ struct virtio_blk {
 
 #ifdef CONFIG_VIRTIO_BLK_RING_PAIR
 	bool ring_pair;
-	bool no_algin;
+	bool no_align;
 	bool hide_bdev;
 	/* saved indirect desc pointer, dma_addr and dma_len for SQ */
 	struct virtblk_indir_desc **indir_desc;
@@ -1308,8 +1308,8 @@ int check_ext_feature(struct virtio_blk *vblk, void __iomem *ioaddr,
 	vblk->ring_pair = !!(*host_ext_features & VIRTIO_BLK_EXT_F_RING_PAIR);
 	if (vblk->ring_pair)
 		*guest_ext_features |= (VIRTIO_BLK_EXT_F_RING_PAIR);
-	vblk->no_algin = !!(*host_ext_features & VIRTIO_BLK_EXT_F_RING_NO_ALIGN);
-	if (vblk->no_algin)
+	vblk->no_align = !!(*host_ext_features & VIRTIO_BLK_EXT_F_RING_NO_ALIGN);
+	if (vblk->no_align)
 		*guest_ext_features |= (VIRTIO_BLK_EXT_F_RING_NO_ALIGN);
 	vblk->hide_bdev = !!(*host_ext_features & VIRTIO_BLK_EXT_F_HIDE_BLOCK);
 	if (vblk->hide_bdev)
@@ -1502,7 +1502,7 @@ static int init_vq(struct virtio_blk *vblk)
 
 #ifdef CONFIG_VIRTIO_BLK_RING_PAIR
 	vblk->ring_pair = false;
-	vblk->no_algin = false;
+	vblk->no_align = false;
 	vblk->hide_bdev = false;
 
 	if (!virtblk_rpair_disable)
@@ -2409,7 +2409,7 @@ static int virtblk_probe(struct virtio_device *vdev)
 	blk_queue_max_segment_size(q, max_size);
 
 #ifdef CONFIG_VIRTIO_BLK_RING_PAIR
-	if (vblk->no_algin)
+	if (vblk->no_align)
 		blk_queue_dma_alignment(q, 0);
 #endif
 