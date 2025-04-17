bcf402e51dcd628a9d1e08bf4a2142a6fe34ae51
Jia He <justin.he@arm.com>
2025-04-17 07:25:21 +0000

    ext4: passing block allocation information to ext4_es_insert_extent()
    ANBZ: #20084
    
    commit f3baf33b9cae0e00fe1870abca952d5dfea53dc6 upstream.
    
    Just pass the block allocation flag to ext4_es_insert_extent() when we
    replacing a current extent after an actually block allocation or extent
    status conversion, this flag will be used by later changes.
    
    Suggested-by: Jan Kara <jack@suse.cz>
    Signed-off-by: Zhang Yi <yi.zhang@huawei.com>
    Link: https://patch.msgid.link/20240813123452.2824659-6-yi.zhang@huaweicloud.com
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    Signed-off-by: Jia He <justin.he@arm.com>

--- fs/ext4/extents.c
+++ fs/ext4/extents.c
@@ -3117,7 +3117,7 @@ static void ext4_zeroout_es(struct inode *inode, struct ext4_extent *ex)
 		return;
 
 	ext4_es_insert_extent(inode, ee_block, ee_len, ee_pblock,
-			      EXTENT_STATUS_WRITTEN);
+			      EXTENT_STATUS_WRITTEN, 0);
 }
 
 /* FIXME!! we need to try to merge to left or right after zero-out  */
@@ -4118,7 +4118,8 @@ again:
 insert_hole:
 	/* Put just found gap into cache to speed up subsequent requests */
 	ext_debug(inode, " -> %u:%u\n", hole_start, len);
-	ext4_es_insert_extent(inode, hole_start, len, ~0, EXTENT_STATUS_HOLE);
+	ext4_es_insert_extent(inode, hole_start, len, ~0,
+			      EXTENT_STATUS_HOLE, 0);
 
 	/* Update hole_len to reflect hole size after lblk */
 	if (hole_start != lblk)
