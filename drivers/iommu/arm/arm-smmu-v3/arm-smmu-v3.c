d4de1398e2d23f7b3bcd13055095a77d204c1a10
Jason Gunthorpe <jgg@nvidia.com>
2025-04-18 16:01:24 +0800

    iommu: Remove IOMMU_DEV_FEAT_SVA
    ANBZ: #13617
    
    commit 7c8896dd4a2a27c84b04dcf0990e6f6b118cb6b2 upstream.
    
    None of the drivers implement anything here anymore, remove the dead code.
    
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>
    Signed-off-by: Lu Baolu <baolu.lu@linux.intel.com>
    Reviewed-by: Kevin Tian <kevin.tian@intel.com>
    Reviewed-by: Yi Liu <yi.l.liu@intel.com>
    Tested-by: Zhangfei Gao <zhangfei.gao@linaro.org>
    Link: https://lore.kernel.org/r/20250418080130.1844424-3-baolu.lu@linux.intel.com
    Signed-off-by: Joerg Roedel <jroedel@suse.de>
    Signed-off-by: Jay Chen <jkchen@linux.alibaba.com>
    [Jay Chen
    Conflicts:
    	drivers/accel/amdxdna/aie2_pci.c
    	Missing this file causes conflict
    ]
    Reviewed-by: Shuai Xue <xueshuai@linxu.alibaba.com>
    Reviewed-by: Guanghui Feng <guanghuifeng@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5822

--- drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.c
+++ drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.c
@@ -3656,7 +3656,6 @@ static int arm_smmu_dev_enable_feature(struct device *dev,
 
 	switch (feat) {
 	case IOMMU_DEV_FEAT_IOPF:
-	case IOMMU_DEV_FEAT_SVA:
 		return 0;
 	default:
 		return -EINVAL;
@@ -3673,7 +3672,6 @@ static int arm_smmu_dev_disable_feature(struct device *dev,
 
 	switch (feat) {
 	case IOMMU_DEV_FEAT_IOPF:
-	case IOMMU_DEV_FEAT_SVA:
 		return 0;
 	default:
 		return -EINVAL;
