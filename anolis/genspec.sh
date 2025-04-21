03f40681f1d7e422716349283ce3dd4f3003261b
luohaiyang <luo.haiyang@zte.com.cn>
2025-04-21 17:00:23 +0800

    anolis: support to build RISC-V rpm packages
    ANBZ: #20509
    
    add RISC-V support to build kernel packages.
    
    Signed-off-by: luohaiyang <luo.haiyang@zte.com.cn>
    Reviewed-by: Qinyun Tan <qinyuntan@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5119

--- anolis/genspec.sh
+++ anolis/genspec.sh
@@ -28,6 +28,8 @@ function generate_cmdline() {
 x86_cmdline=$(generate_cmdline x86)
 arm_cmdline=$(generate_cmdline arm64)
 loongarch_cmdline=$(generate_cmdline loongarch64)
+riscv_cmdline=$(generate_cmdline riscv)
 sed -i -e "s/%%X86_CMDLINE%%/$x86_cmdline/" ${DIST_OUTPUT}/${DIST_SPEC_FILE}
 sed -i -e "s/%%ARM_CMDLINE%%/$arm_cmdline/" ${DIST_OUTPUT}/${DIST_SPEC_FILE}
 sed -i -e "s/%%LOONGARCH_CMDLINE%%/$loongarch_cmdline/" ${DIST_OUTPUT}/${DIST_SPEC_FILE}
+sed -i -e "s/%%RISCV_CMDLINE%%/$riscv_cmdline/" ${DIST_OUTPUT}/${DIST_SPEC_FILE}
