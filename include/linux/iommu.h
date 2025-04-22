1da0019607a9e12cd60d849ad854b0a08d226655
gaorui <gao.rui@zte.com.cn>
2025-04-22 11:02:17 +0800

    iommu: Add iommu_ops->identity_domain
    ANBZ: #20591
    
    commit df31b298477e65a01deff0af352be3a61524d930 upstream.
    
    This allows a driver to set a global static to an IDENTITY domain and
    the core code will automatically use it whenever an IDENTITY domain
    is requested.
    
    By making it always available it means the IDENTITY can be used in error
    handling paths to force the iommu driver into a known state. Devices
    implementing global static identity domains should avoid failing their
    attach_dev ops.
    
    To make global static domains simpler allow drivers to omit their free
    function and update the iommufd selftest.
    
    Convert rockchip to use the new mechanism.
    
    Tested-by: Steven Price <steven.price@arm.com>
    Tested-by: Marek Szyprowski <m.szyprowski@samsung.com>
    Tested-by: Nicolin Chen <nicolinc@nvidia.com>
    Reviewed-by: Lu Baolu <baolu.lu@linux.intel.com>
    Reviewed-by: Jerry Snitselaar <jsnitsel@redhat.com>
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>
    Link: https://lore.kernel.org/r/1-v8-81230027b2fa+9d-iommu_all_defdom_jgg@nvidia.com
    Signed-off-by: Joerg Roedel <jroedel@suse.de>
    Signed-off-by: Gao Rui <gao.rui@zte.com.cn>

--- include/linux/iommu.h
+++ include/linux/iommu.h
@@ -310,6 +310,8 @@ struct iommu_dirty_ops {
  *                    will be blocked by the hardware.
  * @pgsize_bitmap: bitmap of all possible supported page sizes
  * @owner: Driver module providing these ops
+ * @identity_domain: An always available, always attachable identity
+ *                   translation.
  */
 struct iommu_ops {
 	bool (*capable)(struct device *dev, enum iommu_cap);
@@ -345,6 +347,7 @@ struct iommu_ops {
 	const struct iommu_domain_ops *default_domain_ops;
 	unsigned long pgsize_bitmap;
 	struct module *owner;
+	struct iommu_domain *identity_domain;
 
 	CK_KABI_RESERVE(1)
 	CK_KABI_RESERVE(2)
