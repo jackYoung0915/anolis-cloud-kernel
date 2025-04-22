5560be84b2d8ad7948490250797ba575eecf95f3
Ryan Roberts <ryan.roberts@arm.com>
2025-04-22 09:18:19 +0100

    arm64/mm: Batch barriers when updating kernel mappings
    ANBZ: #23525
    
    commit 5fdd05efa1cd3bb98060f409eca57dc0a6d171b4 upstream.
    
    Because the kernel can't tolerate page faults for kernel mappings, when
    setting a valid, kernel space pte (or pmd/pud/p4d/pgd), it emits a
    dsb(ishst) to ensure that the store to the pgtable is observed by the
    table walker immediately. Additionally it emits an isb() to ensure that
    any already speculatively determined invalid mapping fault gets
    canceled.
    
    We can improve the performance of vmalloc operations by batching these
    barriers until the end of a set of entry updates.
    arch_enter_lazy_mmu_mode() and arch_leave_lazy_mmu_mode() provide the
    required hooks.
    
    vmalloc improves by up to 30% as a result.
    
    Two new TIF_ flags are created; TIF_LAZY_MMU tells us if the task is in
    the lazy mode and can therefore defer any barriers until exit from the
    lazy mode. TIF_LAZY_MMU_PENDING is used to remember if any pte operation
    was performed while in the lazy mode that required barriers. Then when
    leaving lazy mode, if that flag is set, we emit the barriers.
    
    Since arch_enter_lazy_mmu_mode() and arch_leave_lazy_mmu_mode() are used
    for both user and kernel mappings, we need the second flag to avoid
    emitting barriers unnecessarily if only user mappings were updated.
    
    Reviewed-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Ryan Roberts <ryan.roberts@arm.com>
    Reviewed-by: Anshuman Khandual <anshuman.khandual@arm.com>
    Tested-by: Luiz Capitulino <luizcap@redhat.com>
    Link: https://lore.kernel.org/r/20250422081822.1836315-12-ryan.roberts@arm.com
    Signed-off-by: Will Deacon <will@kernel.org>
    Signed-off-by: Weilin Tong <tongweilin@linux.alibaba.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5635

--- arch/arm64/include/asm/thread_info.h
+++ arch/arm64/include/asm/thread_info.h
@@ -86,6 +86,9 @@ void arch_setup_new_exec(void);
 #define TIF_SME			27	/* SME in use */
 #define TIF_SME_VL_INHERIT	28	/* Inherit SME vl_onexec across exec */
 
+#define TIF_LAZY_MMU		31	/* Task in lazy mmu mode */
+#define TIF_LAZY_MMU_PENDING	32	/* Ops pending for lazy mmu mode exit */
+
 #define _TIF_SIGPENDING		(1 << TIF_SIGPENDING)
 #define _TIF_NEED_RESCHED	(1 << TIF_NEED_RESCHED)
 #define _TIF_NOTIFY_RESUME	(1 << TIF_NOTIFY_RESUME)
