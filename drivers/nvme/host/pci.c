c1fce156fd2d9a3808a631e28107b2673fa7c221
Bitao Hu <yaoma@linux.alibaba.com>
2025-04-24 11:04:11 +0800

    anolis: nvme: support set per device's timeout
    ANBZ: #20656
    
    The current admin_timeout and io_timeout parameters in the NVMe driver
    are global, meaning they apply to all NVMe devices in the system.
    However, in certain scenarios, we need to set separate timeout values
    for different types of NVMe devices.
    
    To address this, Add two new fields, admin_timeout and io_timeout, to
    the sysfs interface for each NVMe device. By default, these values are
    consistent with the global parameters. If a user sets these values
    individually, the user-defined values will take precedence.
    
    Usage example:
    To set admin_timeout=100 and io_timeout=50 for the NVMe device nvme1:
    echo 100 > /sys/class/nvme/nvme1/admin_timeout
    echo 50 > /sys/class/nvme/nvme1/io_timeout
    
    Signed-off-by: Bitao Hu <yaoma@linux.alibaba.com>

--- drivers/nvme/host/pci.c
+++ drivers/nvme/host/pci.c
@@ -2431,8 +2431,8 @@ static bool __nvme_disable_io_queues(struct nvme_dev *dev, u8 opcode)
 	int nr_queues = dev->online_queues - 1, sent = 0;
 	unsigned long timeout;
 
- retry:
-	timeout = ADMIN_TIMEOUT;
+retry:
+	timeout = dev->ctrl.admin_q->rq_timeout;
 	while (nr_queues > 0) {
 		if (nvme_delete_queue(&dev->queues[nr_queues], opcode))
 			break;
@@ -2631,7 +2631,7 @@ static void nvme_dev_disable(struct nvme_dev *dev, bool shutdown)
 		 * if doing a safe shutdown.
 		 */
 		if (!dead && shutdown)
-			nvme_wait_freeze_timeout(&dev->ctrl, NVME_IO_TIMEOUT);
+			nvme_wait_freeze_timeout(&dev->ctrl);
 	}
 
 	nvme_stop_queues(&dev->ctrl);
