b0a9c695c01821f36a37137be5c2c4f4eb36f97c
Bitao Hu <yaoma@linux.alibaba.com>
2025-04-24 11:04:11 +0800

    anolis: nvme: support set per device's timeout
    ANBZ: #20656
    
    The current admin_timeout and io_timeout parameters in the NVMe driver
    are global, meaning they apply to all NVMe devices in the system.
    However, in certain scenarios, we need to set separate timeout values
    for different types of NVMe devices.
    
    To address this, Add two new fields, admin_timeout and io_timeout, to
    the sysfs interface for each NVMe device. By default, these values are
    consistent with the global parameters. If a user sets these values
    individually, the user-defined values will take precedence.
    
    Usage example:
    To set admin_timeout=100 and io_timeout=50 for the NVMe device nvme1:
    echo 100 > /sys/class/nvme/nvme1/admin_timeout
    echo 50 > /sys/class/nvme/nvme1/io_timeout
    
    Signed-off-by: Bitao Hu <yaoma@linux.alibaba.com>
    Reviewed-by: Guixin Liu <kanie@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5146

--- drivers/nvme/host/sysfs.c
+++ drivers/nvme/host/sysfs.c
@@ -10,6 +10,70 @@
 #include "nvme.h"
 #include "fabrics.h"
 
+static ssize_t admin_timeout_show(struct device *dev, struct device_attribute *attr, char *buf)
+{
+	struct nvme_ctrl *ctrl = dev_get_drvdata(dev);
+
+	return sysfs_emit(buf, "%u\n", ctrl->admin_tagset->timeout / HZ);
+}
+
+static ssize_t admin_timeout_store(struct device *dev,
+				   struct device_attribute *attr, const char *buf,
+				   size_t count)
+{
+	int ret;
+	unsigned int timeout;
+	struct nvme_ctrl *ctrl = dev_get_drvdata(dev);
+
+	ret = kstrtouint(buf, 10, &timeout);
+	if (ret < 0)
+		return ret;
+
+	if (timeout > 0) {
+		timeout = timeout * HZ;
+		ctrl->admin_tagset->timeout = timeout;
+		blk_queue_rq_timeout(ctrl->admin_q, timeout);
+	}
+
+	return count;
+}
+static DEVICE_ATTR_RW(admin_timeout);
+
+static ssize_t io_timeout_show(struct device *dev, struct device_attribute *attr, char *buf)
+{
+	struct nvme_ctrl *ctrl = dev_get_drvdata(dev);
+
+	return sysfs_emit(buf, "%u\n", ctrl->tagset->timeout / HZ);
+}
+
+static ssize_t io_timeout_store(struct device *dev,
+				struct device_attribute *attr, const char *buf,
+				size_t count)
+{
+	int ret, srcu_idx;
+	unsigned int timeout;
+	struct nvme_ns *ns;
+	struct nvme_ctrl *ctrl = dev_get_drvdata(dev);
+
+	ret = kstrtouint(buf, 10, &timeout);
+	if (ret < 0)
+		return ret;
+
+	if (timeout > 0) {
+		timeout = timeout * HZ;
+		ctrl->tagset->timeout = timeout;
+		srcu_idx = srcu_read_lock(&ctrl->srcu);
+		list_for_each_entry_srcu(ns, &ctrl->namespaces, list,
+					srcu_read_lock_held(&ctrl->srcu)) {
+			blk_queue_rq_timeout(ns->queue, timeout);
+		}
+		srcu_read_unlock(&ctrl->srcu, srcu_idx);
+	}
+
+	return count;
+}
+static DEVICE_ATTR_RW(io_timeout);
+
 static ssize_t nvme_sysfs_reset(struct device *dev,
 				struct device_attribute *attr, const char *buf,
 				size_t count)
@@ -530,6 +594,8 @@ static DEVICE_ATTR(dhchap_ctrl_secret, S_IRUGO | S_IWUSR,
 static struct attribute *nvme_dev_attrs[] = {
 	&dev_attr_reset_controller.attr,
 	&dev_attr_rescan_controller.attr,
+	&dev_attr_admin_timeout.attr,
+	&dev_attr_io_timeout.attr,
 	&dev_attr_model.attr,
 	&dev_attr_serial.attr,
 	&dev_attr_firmware_rev.attr,
