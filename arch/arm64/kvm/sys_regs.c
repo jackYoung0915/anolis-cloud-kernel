e283beaa020f225af64fba8cdd52d7355b1ddcd5
Wang Zhimin <wangzhimin1179@phytium.com.cn>
2025-04-25 10:51:37 +0800

    anolis: KVM: uncheck writeable_mask in arm64_check_features
    ANBZ: #20670
    
    Userspace is allowed to modify kvm sys_reg during VM live migration.
    
    Signed-off-by: Peng Mengguang <pengmengguang@phytium.com.cn>
    Signed-off-by: Wang Zhimin <wangzhimin1179@phytium.com.cn>
    Reviewed-by: Bin Guo <guobin@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5166

--- arch/arm64/kvm/sys_regs.c
+++ arch/arm64/kvm/sys_regs.c
@@ -1310,6 +1310,7 @@ static int arm64_check_features(struct kvm_vcpu *vcpu,
 	u32 id = reg_to_encoding(rd);
 	u64 writable_mask = rd->val;
 	u64 limit, mask = 0;
+	u32 midr = read_cpuid_id();
 
 	/*
 	 * Hidden and unallocated ID registers may not have a corresponding
@@ -1331,8 +1332,11 @@ static int arm64_check_features(struct kvm_vcpu *vcpu,
 		u64 ftr_mask;
 
 		ftr_mask = arm64_ftr_mask(ftrp);
-		if ((ftr_mask & writable_mask) != ftr_mask)
-			continue;
+
+		if (MIDR_IMPLEMENTOR(midr) != ARM_CPU_IMP_PHYTIUM) {
+			if ((ftr_mask & writable_mask) != ftr_mask)
+				continue;
+		}
 
 		f_val = arm64_ftr_value(ftrp, val);
 		f_lim = arm64_ftr_value(ftrp, limit);
