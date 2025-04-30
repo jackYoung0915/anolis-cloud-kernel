dcfaaa13f058a735d2b35dc00cabfbbcdd89484f
Juxin Gao <gaojuxin@loongson.cn>
2025-04-30 15:30:28 +0800

    anolis: mm: Fixed build warning when disable CONFIG_FS_DAX_PMD
    ANBZ: #21660
    
    A warning will be shown when building the kernel:
    mm/memory.c: In function 'fr_apply_vma':
    mm/memory.c:6823:15: warning: unused variable 'pmd' [-Wunused-variable]
     6823 |         pmd_t pmd;
    
    Signed-off-by: Juxin Gao <gaojuxin@loongson.cn>

--- mm/memory.c
+++ mm/memory.c
@@ -7386,7 +7386,9 @@ static void fr_apply_vma(struct vm_area_struct *vma)
 	unsigned long next;
 	spinlock_t *pml;
 	pmd_t *pmdp = NULL;
+#ifdef CONFIG_FS_DAX_PMD
 	pmd_t pmd;
+#endif
 	bool applied = false;
 
 	do {
