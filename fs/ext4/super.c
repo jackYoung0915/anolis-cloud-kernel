6120a4c9d31f4dfa7d96c57994204b4fb6a4e974
Jia He <justin.he@arm.com>
2025-05-08 08:45:52 +0000

    ext4: introduce a mount option for iomap buffered I/O path
    ANBZ: #20084
    
    cherry-picked from https://lore.kernel.org/all/20241022111059.2566137-1-yi.zhang@huaweicloud.com/
    
    Introduce the buffered_iomap and the nobuffered_iomap mount options to
    enable the iomap buffered I/O path for regular files. This option is
    currently disabled by default until we can support more comprehensive
    features along this path.
    
    [justin] Mark the feature as EXPERIMENTAL.
    
    Signed-off-by: Zhang Yi <yi.zhang@huawei.com>
    Signed-off-by: Jia He <justin.he@arm.com>
    Reviewed-by: Joseph Qi <joseph.qi@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5167

--- fs/ext4/super.c
+++ fs/ext4/super.c
@@ -1713,6 +1713,7 @@ enum {
 	Opt_discard, Opt_nodiscard, Opt_init_itable, Opt_noinit_itable,
 	Opt_max_dir_size_kb, Opt_nojournal_checksum, Opt_nombcache,
 	Opt_no_prefetch_block_bitmaps, Opt_mb_optimize_scan,
+	Opt_buffered_iomap, Opt_nobuffered_iomap,
 	Opt_errors, Opt_data, Opt_data_err, Opt_jqfmt, Opt_dax_type,
 #ifdef CONFIG_EXT4_DEBUG
 	Opt_fc_debug_max_replay, Opt_fc_debug_force
@@ -1855,6 +1856,8 @@ static const struct fs_parameter_spec ext4_param_specs[] = {
 	fsparam_flag	("no_prefetch_block_bitmaps",
 						Opt_no_prefetch_block_bitmaps),
 	fsparam_s32	("mb_optimize_scan",	Opt_mb_optimize_scan),
+	fsparam_flag	("buffered_iomap",	Opt_buffered_iomap),
+	fsparam_flag	("nobuffered_iomap",	Opt_nobuffered_iomap),
 	fsparam_string	("check",		Opt_removed),	/* mount option from ext2/3 */
 	fsparam_flag	("nocheck",		Opt_removed),	/* mount option from ext2/3 */
 	fsparam_flag	("reservation",		Opt_removed),	/* mount option from ext2/3 */
@@ -1949,6 +1952,10 @@ static const struct mount_opts {
 	{Opt_nombcache, EXT4_MOUNT_NO_MBCACHE, MOPT_SET},
 	{Opt_no_prefetch_block_bitmaps, EXT4_MOUNT_NO_PREFETCH_BLOCK_BITMAPS,
 	 MOPT_SET},
+	{Opt_buffered_iomap, EXT4_MOUNT2_BUFFERED_IOMAP,
+	 MOPT_SET | MOPT_2 | MOPT_EXT4_ONLY},
+	{Opt_nobuffered_iomap, EXT4_MOUNT2_BUFFERED_IOMAP,
+	 MOPT_CLEAR | MOPT_2 | MOPT_EXT4_ONLY},
 #ifdef CONFIG_EXT4_DEBUG
 	{Opt_fc_debug_force, EXT4_MOUNT2_JOURNAL_FAST_COMMIT,
 	 MOPT_SET | MOPT_2 | MOPT_EXT4_ONLY},
@@ -2821,6 +2828,10 @@ static int ext4_check_opt_consistency(struct fs_context *fc,
 			 ctx->s_want_extra_isize);
 		return -EINVAL;
 	}
+
+	if (ctx_test_mount_opt2(ctx, EXT4_MOUNT2_BUFFERED_IOMAP))
+		ext4_msg(NULL, KERN_WARNING,
+			 "Warning: mounting with an experimental option 'buffered_iomap'");
 
 	err = ext4_check_test_dummy_encryption(fc, sb);
 	if (err)
