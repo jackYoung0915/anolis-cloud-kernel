jackYoung0915/anolis-cloud-kernel

commit d8c8d96e4a922ac40a4dc2c16c3a5e554cac1d99

anolis: virtio-blk: add ext_feature negotiation method and ring-pair feature bit
ANBZ: #19447

Use external feature bits to management ring pair feature. This needs
backend specific support.

Driver read host_ext_features, and writes to guest_ext_features.

Now ring pair feature holds virtio-blk extra feature bit 0.

Signed-off-by: Ferry Meng <mengferry@linux.alibaba.com>
Reviewed-by: Yifei Zhou <yifei.zhou@linux.alibaba.com>
Acked-by: Xuan Zhuo <xuanzhuo@linux.alibaba.com>
Reviewed-by: Jingbo Xu <jefflexu@linux.alibaba.com>
Reviewed-by: Joseph Qi <joseph.qi@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/4799

drivers/block/virtio_blk.c
@@ -20,6 +20,11 @@
 #include <linux/io_uring.h>
 #include <linux/types.h>
 #include <linux/uio.h>
+#ifdef CONFIG_VIRTIO_BLK_RING_PAIR
+#include "../virtio/virtio_pci_common.h"
+#include <linux/virtio_config.h>
+#include "virtio_blk_ext.h"
+#endif
 
 #define PART_BITS 4
 #define VQ_NAME_LEN 16
@@ -1273,6 +1278,23 @@ bool virtblk_rpair_disable;
 module_param_named(rpair_disable, virtblk_rpair_disable, bool, 0444);
 MODULE_PARM_DESC(rpair_disable, "disable vring pair detective. (0=Not [default], 1=Yes)");
 
+int check_ext_feature(struct virtio_blk *vblk, void __iomem *ioaddr,
+						u32 *host_ext_features,
+						u32 *guest_ext_features)
+{
+	int ret = 0;
+
+	ret = virtblk_get_ext_feature(ioaddr, host_ext_features);
+	if (ret < 0)
+		return ret;
+
+	vblk->ring_pair = !!(*host_ext_features & VIRTIO_BLK_EXT_F_RING_PAIR);
+	if (vblk->ring_pair)
+		*guest_ext_features |= (VIRTIO_BLK_EXT_F_RING_PAIR);
+
+	return 0;
+}
+
 static int init_vq_rpair(struct virtio_blk *vblk)
 {
 	int err = 0;
@@ -1282,8 +1304,27 @@ static int init_vq_rpair(struct virtio_blk *vblk)
 	struct virtqueue **vqs;
 	unsigned short num_vqs;
 	unsigned int num_poll_vqs, num_queues, num_poll_queues, vring_size;
+	u32 ext_host_features = 0, ext_guest_features = 0, ext_bar_offset = 0;
 	struct virtio_device *vdev = vblk->vdev;
 	struct irq_affinity desc = { 0, };
+	void __iomem *ioaddr = NULL;
+
+	err = virtblk_get_ext_feature_bar(vdev, &ext_bar_offset);
+	/* if check ext feature error, fall back to orig virtqueue use. */
+	if ((err < 0) || !ext_bar_offset)
+		return 1;
+
+	ioaddr = pci_iomap_range(to_vp_device(vdev)->pci_dev, 0, ext_bar_offset, 16);
+	if (!ioaddr) {
+		err = 1;
+		goto negotiate_err;
+	}
+
+	err = check_ext_feature(vblk, ioaddr, &ext_host_features, &ext_guest_features);
+	if ((err < 0) || !vblk->ring_pair) {
+		err = 1;
+		goto negotiate_err;
+	}
 
 	err = virtio_cread_feature(vdev, VIRTIO_BLK_F_MQ,
 				   struct virtio_blk_config, num_queues,
@@ -1293,25 +1334,29 @@ static int init_vq_rpair(struct virtio_blk *vblk)
 
 	if (!err && !num_vqs) {
 		dev_err(&vdev->dev, "MQ advertised but zero queues reported\n");
-		return -EINVAL;
+		err = -EINVAL;
+		goto negotiate_err;
 	}
 
 	if (num_vqs % VIRTBLK_RING_NUM) {
 		dev_err(&vdev->dev,
 			"RING_PAIR advertised but odd queues reported\n");
-		vblk->ring_pair = false;
+		err = 1;
+		goto negotiate_err;
 	}
 
 	/* ring pair only support split virtqueue + indirect enabled */
 	if (virtio_has_feature(vdev, VIRTIO_F_RING_PACKED) ||
 		!virtio_has_feature(vdev, VIRTIO_RING_F_INDIRECT_DESC)) {
 		dev_err(&vdev->dev, "rpair only support indir+split queue\n");
-		vblk->ring_pair = false;
+		err = 1;
+		goto negotiate_err;
 	}
 
-	/* If vring pair is not enabled, fall back to orig virtqueue use. */
-	if (!vblk->ring_pair)
-		return 1;
+	virtblk_set_ext_feature(ioaddr, ext_guest_features);
+	pci_iounmap(to_vp_device(vdev)->pci_dev, ioaddr);
+	dev_info(&vdev->dev, "rpair enabled, ext_guest_feature set 0x%x\n",
+						ext_guest_features);
 
 	num_queues = num_vqs / VIRTBLK_RING_NUM;
 	num_queues = min_t(unsigned int,
@@ -1407,6 +1452,15 @@ out:
 		virtblk_kfree_vqs_cq_reqs(vblk);
 		kfree(vblk->vqs);
 	}
+	return err;
+
+negotiate_err:
+	if (ioaddr) {
+		ext_guest_features &= ~VIRTIO_BLK_EXT_F_RING_PAIR;
+		virtblk_set_ext_feature(ioaddr, ext_guest_features);
+		pci_iounmap(to_vp_device(vdev)->pci_dev, ioaddr);
+	}
+	vblk->ring_pair = false;
 	return err;
 }
 #endif

drivers/block/Makefile
@@ -29,6 +29,9 @@ obj-$(CONFIG_BLK_DEV_NBD)	+= nbd.o
 obj-$(CONFIG_BLK_DEV_CRYPTOLOOP) += cryptoloop.o
 obj-$(CONFIG_VIRTIO_BLK)	+= virtio_blk.o
 
+virtio_blk-y			:= virtio_blk.o
+virtio_blk-$(CONFIG_VIRTIO_BLK_RING_PAIR)	+= virtio_blk_ext.o
+
 obj-$(CONFIG_BLK_DEV_SX8)	+= sx8.o
 
 obj-$(CONFIG_XEN_BLKDEV_FRONTEND)	+= xen-blkfront.o

drivers/block/virtio_blk_ext.c
@@ -1,0 +1,171 @@
+// SPDX-License-Identifier: GPL-2.0-only
+#include <linux/types.h>
+#include <linux/virtio_blk.h>
+#include "../virtio/virtio_pci_common.h"
+#include "virtio_blk_ext.h"
+#include <linux/virtio_config.h>
+
+
+#define VIRTIO_PCI_VSF_MAGIC_NUM			0x0
+#define VIRTIO_PCI_VSF_MAGIC_NUM_VAL			0x7D4FEE9D
+#define VIRTIO_PCI_HOST_VNDR_SPEC_FEATURE_SELECT	0x04
+/* A 32-bit r/o bitmask of the vendor specific features supported by the host */
+#define VIRTIO_PCI_HOST_VNDR_SPEC_FEATURES		0x08
+
+#define VIRTIO_PCI_GUEST_VNDR_SPEC_FEATURE_SELECT	0x0c
+/* A 32-bit r/w bitmask of the vendor specific features activated by the guest */
+#define VIRTIO_PCI_GUEST_VNDR_SPEC_FEATURES		0x10
+
+
+/* xdragon vsc */
+#define PCI_CAP_ID_VNDR                         0x09	/* Vendor specific */
+#define PCI_XDRAGON_VSC_CFGTYPE                 0xff
+
+/* xdragon vsec */
+#define PCI_EXT_CAP_ID_VNDR                     0x0B
+#define PCI_EXP_XDRAGON_VSEC_CFGTYPE            0xff
+#define XDRAGON_VSEC_VERSION                    1
+
+#define XDRAGON_XVCS_MAGIC                      0x53435658
+#define XDRAGON_XVCS_VSF_KEY                    "xvcs-vsf"
+#define XDRAGON_XVCS_VERSION                    1
+#define XDRAGON_XVCS_NUM_MAX                    32U
+#define XDRAGON_XVCS_KEY_MAX			16
+
+#define XDRAGON_XVCS_O_MAGIC			0
+#define XDRAGON_XVCS_O_VER			4
+#define XDRAGON_XVCS_O_ADDR			12
+#define XDRAGON_XVCS_O_F_CNT			16
+#define XDRAGON_XVCS_O_CUR                      16
+#define XDRAGON_XVCS_O_NEXT                     20
+#define XDRAGON_XVCS_O_VSF                      32
+static void xdragon_read_xvcs(struct pci_dev *d, u32 pos,
+				u32 cap_len, u32 addr, u32 num, void *data)
+{
+	u32 idx, where;
+
+	for (idx = 0; idx < num; idx += 4) {
+		where = addr + idx;
+		pci_write_config_dword(d, pos + cap_len - 8, where);
+		pci_read_config_dword(d, pos + cap_len - 4, (u32 *)((u8 *)data + idx));
+	}
+}
+
+static int xdragon_vcs_find_vsf_bar0_offset(struct pci_dev *dev, uint32_t cap_len,
+				uint32_t pos, u32 *bar0_offset)
+{
+	u8 buf[XDRAGON_XVCS_KEY_MAX+1];
+	u32 where;
+	u32 idx, num;
+	u32 reg;
+
+	/* check xvcs magic */
+	xdragon_read_xvcs(dev, pos, cap_len, XDRAGON_XVCS_O_MAGIC, sizeof(reg), &reg);
+	if (reg != XDRAGON_XVCS_MAGIC) {
+		pr_err("%s: xvcs magic 0x%x not match\n", __func__, reg);
+		return -1;
+	}
+	/* check xvcs version */
+	xdragon_read_xvcs(dev, pos, cap_len, XDRAGON_XVCS_O_VER, sizeof(reg), &reg);
+	if (reg != XDRAGON_XVCS_VERSION) {
+		pr_err("%s: xvcs version 0x%x not match\n", __func__, reg);
+		return -1;
+	}
+	/* xvcs feat block addr */
+	xdragon_read_xvcs(dev, pos, cap_len, XDRAGON_XVCS_O_ADDR, sizeof(reg), &reg);
+	where = reg;
+	/* xvcs feat cnt */
+	xdragon_read_xvcs(dev, pos, cap_len, XDRAGON_XVCS_O_F_CNT, sizeof(reg), &reg);
+	num = reg;
+	for (idx = 0; (idx < min(XDRAGON_XVCS_NUM_MAX, num)) && (where > 0); idx++) {
+		memset(buf, 0, sizeof(buf));
+
+		/* self addr check */
+		xdragon_read_xvcs(dev, pos, cap_len,
+				where + XDRAGON_XVCS_O_CUR, sizeof(reg), &reg);
+		if (reg != where)
+			return -1;
+
+		/* check key */
+		xdragon_read_xvcs(dev, pos, cap_len, where, XDRAGON_XVCS_KEY_MAX, buf);
+
+		/* found vsf */
+		if (strncmp(buf, XDRAGON_XVCS_VSF_KEY, sizeof(XDRAGON_XVCS_VSF_KEY)) == 0) {
+			xdragon_read_xvcs(dev, pos, cap_len, where + XDRAGON_XVCS_O_VSF,
+					sizeof(reg), &reg);
+			*bar0_offset = reg;
+			return 0;
+		}
+		/* next vcs feat */
+		xdragon_read_xvcs(dev, pos, cap_len,
+					where + XDRAGON_XVCS_O_NEXT, sizeof(reg), &reg);
+		where = reg;
+	}
+	pr_err("%s: vsf offset not found\n", __func__);
+	return -1;
+}
+
+int virtblk_get_ext_feature_bar(struct virtio_device *vdev, u32 *bar_offset)
+{
+	struct pci_dev *dev = to_vp_device(vdev)->pci_dev;
+	int cap_len, vsec = 0;
+	u16 val;
+	u8 type, len = 0;
+	bool found = false;
+
+	/* try to find vsc */
+	for (vsec = pci_find_capability(dev, PCI_CAP_ID_VNDR);
+		 vsec > 0;
+		 vsec = pci_find_next_capability(dev, vsec, PCI_CAP_ID_VNDR)) {
+		pci_read_config_byte(dev, vsec + offsetof(struct virtio_pci_cap, cfg_type), &type);
+		if (type == PCI_XDRAGON_VSC_CFGTYPE) {
+			pci_read_config_byte(dev,
+					vsec + offsetof(struct virtio_pci_cap, cap_len), &len);
+			cap_len = len;
+			found = true;
+			break;
+		}
+	}
+
+	/* try to find vsec */
+	if (!found) {
+		vsec = 0;
+		while ((vsec = pci_find_next_ext_capability(dev, vsec,
+						PCI_EXT_CAP_ID_VNDR))) {
+			pci_read_config_word(dev, vsec + 0x4, &val);
+			/* vsec found */
+			if (val == PCI_EXP_XDRAGON_VSEC_CFGTYPE) {
+				/* get vsec cap len */
+				pci_read_config_word(dev, vsec + 0x6, &val);
+				if ((val & 0xF) != XDRAGON_VSEC_VERSION)
+					continue;
+				cap_len = (val >> 4) & (0xFFF);
+				found = true;
+				break;
+			}
+		}
+	}
+
+	return found ? xdragon_vcs_find_vsf_bar0_offset(dev, cap_len, vsec, bar_offset) : -1;
+}
+
+int virtblk_get_ext_feature(void __iomem *ioaddr, u32 *host_features)
+{
+	int ret;
+
+	/* read ext bar magci number */
+	ret = ioread32(ioaddr);
+	if (ret != VIRTIO_PCI_VSF_MAGIC_NUM_VAL)
+		return -EOPNOTSUPP;
+
+	iowrite32(0, ioaddr + VIRTIO_PCI_HOST_VNDR_SPEC_FEATURE_SELECT);
+	*host_features = ioread32(ioaddr + VIRTIO_PCI_HOST_VNDR_SPEC_FEATURES);
+
+	return 0;
+}
+
+void virtblk_set_ext_feature(void __iomem *ioaddr, u32 guest_ext_features)
+{
+	iowrite32(0, ioaddr + VIRTIO_PCI_GUEST_VNDR_SPEC_FEATURE_SELECT);
+	iowrite32(guest_ext_features, ioaddr + VIRTIO_PCI_GUEST_VNDR_SPEC_FEATURES);
+}

drivers/block/virtio_blk_ext.h
@@ -1,0 +1,16 @@
+// SPDX-License-Identifier: GPL-2.0-only
+#include <linux/types.h>
+#include <linux/virtio.h>
+#include <linux/virtio_blk.h>
+
+/* ext feature bit definition */
+#define VIRTIO_BLK_EXT_F_RING_PAIR	(1U << 0)
+#define VIRTIO_BLK_EXT_F_INVAL		(-1)
+
+#ifdef CONFIG_VIRTIO_BLK_RING_PAIR
+void virtblk_set_ext_feature(void __iomem *ioaddr, u32 guest_ext_features);
+
+int virtblk_get_ext_feature(void __iomem *ioaddr, u32 *host_features);
+
+int virtblk_get_ext_feature_bar(struct virtio_device *vdev, u32 *bar_offset);
+#endif

commit eb3f2e0a611f769aa9782212b6e3e4472d3cb88a

anolis: virtio_blk: make ext feature support only over pci
ANBZ: #19447

Virtio device may be over various bus types, pci\mmio\vdpa...
But ext feature only support pci device currently. So we
use a hack modification to avoid calling init_vq_rpair() on virtio-blk
over mmio (or other types).

Fixes: d8c8d96e4a92 ("anolis: virtio-blk: add ext_feature negotiation method and ring-pair feature bit")
Signed-off-by: Ferry Meng <mengferry@linux.alibaba.com>

drivers/block/virtio_blk.c
@@ -1505,7 +1505,8 @@ static int init_vq(struct virtio_blk *vblk)
 	vblk->no_align = false;
 	vblk->hide_bdev = false;
 
-	if (!virtblk_rpair_disable)
+	/* ext feature only support for virtio_blk over pci device currently */
+	if (!virtblk_rpair_disable && dev_is_pci(&vblk->vdev->dev))
 		err = init_vq_rpair(vblk);
 
 	/* if err > 0, then vring pair fall back to original virtqueue use*/