be15d4dbc03466afa5be5e1901a13aa12f433ae2
Alexander Graf <graf@amazon.com>
2025-05-09 00:46:30 -0700

    x86/e820: temporarily enable KHO scratch for memory below 1M
    ANBZ: #26808
    
    commit a2daf83e10378ff4ef61f75da710cac9b84e3eaa upstream.
    
    KHO kernels are special and use only scratch memory for memblock
    allocations, but memory below 1M is ignored by kernel after early boot and
    cannot be naturally marked as scratch.
    
    To allow allocation of the real-mode trampoline and a few (if any) other
    very early allocations from below 1M forcibly mark the memory below 1M as
    scratch.
    
    After real mode trampoline is allocated, clear that scratch marking.
    
    Link: https://lkml.kernel.org/r/20250509074635.3187114-13-changyuanl@google.com
    Signed-off-by: Alexander Graf <graf@amazon.com>
    Co-developed-by: Mike Rapoport (Microsoft) <rppt@kernel.org>
    Signed-off-by: Mike Rapoport (Microsoft) <rppt@kernel.org>
    Co-developed-by: Changyuan Lyu <changyuanl@google.com>
    Signed-off-by: Changyuan Lyu <changyuanl@google.com>
    Acked-by: Dave Hansen <dave.hansen@linux.intel.com>
    Cc: Andy Lutomirski <luto@kernel.org>
    Cc: Anthony Yznaga <anthony.yznaga@oracle.com>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: Ashish Kalra <ashish.kalra@amd.com>
    Cc: Ben Herrenschmidt <benh@kernel.crashing.org>
    Cc: Borislav Betkov <bp@alien8.de>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: David Woodhouse <dwmw2@infradead.org>
    Cc: Eric Biederman <ebiederm@xmission.com>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: James Gowans <jgowans@amazon.com>
    Cc: Jason Gunthorpe <jgg@nvidia.com>
    Cc: Jonathan Corbet <corbet@lwn.net>
    Cc: Krzysztof Kozlowski <krzk@kernel.org>
    Cc: Marc Rutland <mark.rutland@arm.com>
    Cc: Paolo Bonzini <pbonzini@redhat.com>
    Cc: Pasha Tatashin <pasha.tatashin@soleen.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Pratyush Yadav <ptyadav@amazon.de>
    Cc: Rob Herring <robh@kernel.org>
    Cc: Saravana Kannan <saravanak@google.com>
    Cc: Stanislav Kinsburskii <skinsburskii@linux.microsoft.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Thomas Gleinxer <tglx@linutronix.de>
    Cc: Thomas Lendacky <thomas.lendacky@amd.com>
    Cc: Will Deacon <will@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Zelin Deng <zelin.deng@linux.alibaba.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5993

--- arch/x86/kernel/e820.c
+++ arch/x86/kernel/e820.c
@@ -1344,6 +1344,24 @@ void __init e820__memblock_setup(void)
 		memblock_add(entry->addr, entry->size);
 	}
 
+	/*
+	 * At this point memblock is only allowed to allocate from memory
+	 * below 1M (aka ISA_END_ADDRESS) up until direct map is completely set
+	 * up in init_mem_mapping().
+	 *
+	 * KHO kernels are special and use only scratch memory for memblock
+	 * allocations, but memory below 1M is ignored by kernel after early
+	 * boot and cannot be naturally marked as scratch.
+	 *
+	 * To allow allocation of the real-mode trampoline and a few (if any)
+	 * other very early allocations from below 1M forcibly mark the memory
+	 * below 1M as scratch.
+	 *
+	 * After real mode trampoline is allocated, we clear that scratch
+	 * marking.
+	 */
+	memblock_mark_kho_scratch(0, SZ_1M);
+
 	/* Throw away partial pages: */
 	memblock_trim_memory(PAGE_SIZE);
 
