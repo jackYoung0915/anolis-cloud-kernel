bff3f3d2c60867d801fc090b21d16b183e62895e
Mike Rapoport (Microsoft) <rppt@kernel.org>
2025-05-09 00:46:21 -0700

    memblock: introduce memmap_init_kho_scratch()
    ANBZ: #26808
    
    commit b8a8f96a6dce527ad316184ff1e20f238ed413d8 upstream.
    
    With deferred initialization of struct page it will be necessary to
    initialize memory map for KHO scratch regions early.
    
    Add memmap_init_kho_scratch() method that will allow such initialization
    in upcoming patches.
    
    [Zelin Deng: init_deferred_page() is just new name of
    init_reserved_page(), so no need to backporting the patches which renamed
    init_reserved_page().]
    
    Link: https://lkml.kernel.org/r/20250509074635.3187114-4-changyuanl@google.com
    Signed-off-by: Mike Rapoport (Microsoft) <rppt@kernel.org>
    Signed-off-by: Changyuan Lyu <changyuanl@google.com>
    Cc: Alexander Graf <graf@amazon.com>
    Cc: Andy Lutomirski <luto@kernel.org>
    Cc: Anthony Yznaga <anthony.yznaga@oracle.com>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: Ashish Kalra <ashish.kalra@amd.com>
    Cc: Ben Herrenschmidt <benh@kernel.crashing.org>
    Cc: Borislav Betkov <bp@alien8.de>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Dave Hansen <dave.hansen@linux.intel.com>
    Cc: David Woodhouse <dwmw2@infradead.org>
    Cc: Eric Biederman <ebiederm@xmission.com>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: James Gowans <jgowans@amazon.com>
    Cc: Jason Gunthorpe <jgg@nvidia.com>
    Cc: Jonathan Corbet <corbet@lwn.net>
    Cc: Krzysztof Kozlowski <krzk@kernel.org>
    Cc: Marc Rutland <mark.rutland@arm.com>
    Cc: Paolo Bonzini <pbonzini@redhat.com>
    Cc: Pasha Tatashin <pasha.tatashin@soleen.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Pratyush Yadav <ptyadav@amazon.de>
    Cc: Rob Herring <robh@kernel.org>
    Cc: Saravana Kannan <saravanak@google.com>
    Cc: Stanislav Kinsburskii <skinsburskii@linux.microsoft.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Thomas Gleinxer <tglx@linutronix.de>
    Cc: Thomas Lendacky <thomas.lendacky@amd.com>
    Cc: Will Deacon <will@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Zelin Deng <zelin.deng@linux.alibaba.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5993

--- mm/mm_init.c
+++ mm/mm_init.c
@@ -711,7 +711,7 @@ defer_init(int nid, unsigned long pfn, unsigned long end_pfn)
 	return false;
 }
 
-static void __meminit init_reserved_page(unsigned long pfn, int nid)
+static void __meminit __init_reserved_page(unsigned long pfn, int nid)
 {
 	pg_data_t *pgdat;
 	int zid;
@@ -742,10 +742,15 @@ static inline bool defer_init(int nid, unsigned long pfn, unsigned long end_pfn)
 	return false;
 }
 
-static inline void init_reserved_page(unsigned long pfn, int nid)
+static inline void __init_reserved_page(unsigned long pfn, int nid)
 {
 }
 #endif /* CONFIG_DEFERRED_STRUCT_PAGE_INIT */
+
+void __meminit init_reserved_page(unsigned long pfn, int nid)
+{
+	__init_reserved_page(pfn, nid);
+}
 
 /*
  * Initialised pages do not have PageReserved set. This function is
@@ -763,7 +768,7 @@ void __meminit reserve_bootmem_region(phys_addr_t start,
 		if (pfn_valid(start_pfn)) {
 			struct page *page = pfn_to_page(start_pfn);
 
-			init_reserved_page(start_pfn, nid);
+			__init_reserved_page(start_pfn, nid);
 
 			/* Avoid false-positive PageTail() */
 			INIT_LIST_HEAD(&page->lru);
