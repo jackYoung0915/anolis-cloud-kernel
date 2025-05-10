d15b4164d356e67ff6d9b5b9f7c0a12386859568
Ferry Meng <mengferry@linux.alibaba.com>
2025-05-10 09:30:00 +0800

    anolis: virtio-blk: add bidirectional request support
    ANBZ: #35136
    
    cherry picked from devel-6.6 commit 4285058739a9f041a2e0f732c641dd26b4c0db97.
    
    We support virtio-blk bidirectional request, which contains both read
    and write bios. Now we assume that A bidirectional request only
    contains two bios, 1st is write and 2nd is read.
    
    We should divide 'bios' into two sglists, so virtblk-req need extend its
    structure, use an extra sglist to store them.
    
    In order to avoid corruption of the logic of 'request', using a trick in
    'virtblk_map_user_bidirectional': when map iovec from usermode, set
    REQ_OP_WRITE for req->cmd_flags first, remove it, set read flag and
    repeat above steps.
    
    For block I/O request, add bidirectional flag for passthrough command.
    If a request is bidirectional, its bio contains read and write iovecs.
    
    Add extra and flag in virtblk_uring_cmd, which will be used in
    bidirectional request.
    
    'write_iov_count' refers to 'num of write iovec' in a bidirectional
    request. 'flag' indicate this is a bidirectional req or not.
    
    We assume that a bidirectional request should satisfy the model "first
    write then read", and they need to be continuous in iovec. In other
    words, they should be like this:
    
    	write - write - write - read - read
    
    In this example, virtblk_uring_cmd is:
    
    data: iovec addr base
    data_len: 0x5
    flag: 0x1
    write_iov_count: 0x3
    
    Signed-off-by: Ferry Meng <mengferry@linux.alibaba.com>
    Signed-off-by: Joseph Qi <joseph.qi@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6936

--- include/linux/blk_types.h
+++ include/linux/blk_types.h
@@ -452,6 +452,7 @@ enum req_flag_bits {
 	__REQ_DRV,		/* for driver use */
 	__REQ_FS_PRIVATE,	/* for file system (submitter) use */
 	__REQ_ATOMIC,		/* for atomic write operations */
+	__REQ_BIDIR,		/* request is a bi-directional */
 	/*
 	 * Command specific flags, keep last:
 	 */
@@ -484,6 +485,7 @@ enum req_flag_bits {
 #define REQ_DRV		(__force blk_opf_t)(1ULL << __REQ_DRV)
 #define REQ_FS_PRIVATE	(__force blk_opf_t)(1ULL << __REQ_FS_PRIVATE)
 #define REQ_ATOMIC	(__force blk_opf_t)(1ULL << __REQ_ATOMIC)
+#define REQ_BIDIR	((__force blk_opf_t)(1ULL << __REQ_BIDIR))
 
 #define REQ_NOUNMAP	(__force blk_opf_t)(1ULL << __REQ_NOUNMAP)
 
@@ -510,6 +512,11 @@ static inline enum req_op bio_op(const struct bio *bio)
 static inline bool op_is_write(blk_opf_t op)
 {
 	return !!(op & (__force blk_opf_t)1);
+}
+
+static inline bool op_is_bidirectional(blk_opf_t op)
+{
+	return op & REQ_BIDIR;
 }
 
 /*
