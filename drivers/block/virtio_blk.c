f3767a45c0934285ae485070785b40c304115fb5
Ferry Meng <mengferry@linux.alibaba.com>
2025-05-10 09:30:00 +0800

    anolis: virtio-blk: add virtio-blk chardev support
    ANBZ: #20938
    
    Introduce character interfaces for block device (per-device), facilitating
    access to block devices through io_uring I/O passsthrough.
    
    Besides, vblk initialize only use kmalloc with GFP_KERNEL flag, but for
    char device support, we should ensure cdev kobj must be zero before
    initialize. So better initial this struct with __GFP_ZERO flag.
    
    Now the character devices only named as
    
    	- /dev/vdXc0
    
    Currently, only one character interface is created for one actual
    virtblk device, although it has been partitioned.
    
    -----
    ANCK-6.6
    Since virtio_blk no longer maintainers its own refernce count, but
    instead relies on the refcount of gendisk, we use a hack approach to
    borrow gendisk's refcount mechanism to ensure that the lifetime of
    gendisk and vblk structures remain valid during usage of char device.
    
    Signed-off-by: Ferry Meng <mengferry@linux.alibaba.com>
    Reviewed-by: Joseph Qi <joseph.qi@linux.alibaba.com>
    Reviewed-by: Xuan Zhuo <xuanzhuo@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5629

--- drivers/block/virtio_blk.c
+++ drivers/block/virtio_blk.c
@@ -17,6 +17,7 @@
 #include <linux/numa.h>
 #include <linux/vmalloc.h>
 #include <uapi/linux/virtio_ring.h>
+#include <linux/cdev.h>
 
 #define PART_BITS 4
 #define VQ_NAME_LEN 16
@@ -24,6 +25,8 @@
 
 /* The maximum number of sg elements that fit into a virtqueue */
 #define VIRTIO_BLK_MAX_SG_ELEMS 32768
+
+#define VIRTBLK_MINORS		(1U << MINORBITS)
 
 #ifdef CONFIG_ARCH_NO_SG_CHAIN
 #define VIRTIO_BLK_INLINE_SG_CNT	0
@@ -44,6 +47,10 @@ MODULE_PARM_DESC(poll_queues, "The number of dedicated virtqueues for polling I/
 
 static int major;
 static DEFINE_IDA(vd_index_ida);
+
+static DEFINE_IDA(vd_chr_minor_ida);
+static dev_t vd_chr_devt;
+static struct class *vd_chr_class;
 
 static struct workqueue_struct *virtblk_wq;
 
@@ -84,6 +91,10 @@ struct virtio_blk {
 
 	/* For zoned device */
 	unsigned int zone_sectors;
+
+	/* For passthrough cmd */
+	struct cdev cdev;
+	struct device cdev_device;
 };
 
 struct virtblk_req {
@@ -1294,6 +1305,81 @@ static const struct blk_mq_ops virtio_mq_ops = {
 	.poll		= virtblk_poll,
 };
 
+static void virtblk_cdev_rel(struct device *dev)
+{
+	ida_free(&vd_chr_minor_ida, MINOR(dev->devt));
+}
+
+static void virtblk_cdev_del(struct cdev *cdev, struct device *cdev_device)
+{
+	cdev_device_del(cdev, cdev_device);
+	put_device(cdev_device);
+}
+
+static int virtblk_cdev_add(struct virtio_blk *vblk,
+		const struct file_operations *fops)
+{
+	struct cdev *cdev = &vblk->cdev;
+	struct device *cdev_device = &vblk->cdev_device;
+	int minor, ret;
+
+	minor = ida_alloc(&vd_chr_minor_ida, GFP_KERNEL);
+	if (minor < 0)
+		return minor;
+
+	cdev_device->parent = &vblk->vdev->dev;
+	cdev_device->devt = MKDEV(MAJOR(vd_chr_devt), minor);
+	cdev_device->class = vd_chr_class;
+	cdev_device->release = virtblk_cdev_rel;
+	device_initialize(cdev_device);
+
+	ret = dev_set_name(cdev_device, "%sc0", vblk->disk->disk_name);
+	if (ret)
+		goto fail;
+
+	cdev_init(cdev, fops);
+	ret = cdev_device_add(cdev, cdev_device);
+	if (ret)
+		goto fail;
+
+	return 0;
+
+fail:
+	put_device(cdev_device);
+	return ret;
+}
+
+static int virtblk_chr_open(struct inode *inode, struct file *file)
+{
+	int ret = 0;
+	struct virtio_blk *vblk = container_of(inode->i_cdev, struct virtio_blk, cdev);
+
+	if (vblk->disk)
+		get_device(disk_to_dev(vblk->disk));
+	else
+		ret = -ENXIO;
+
+	return ret;
+}
+
+static int virtblk_chr_release(struct inode *inode, struct file *file)
+{
+	struct virtio_blk *vblk = container_of(inode->i_cdev, struct virtio_blk, cdev);
+
+	if (!vblk->disk)
+		WARN_ON(1);
+	else
+		put_device(disk_to_dev(vblk->disk));
+
+	return 0;
+}
+
+static const struct file_operations virtblk_chr_fops = {
+	.owner		= THIS_MODULE,
+	.open		= virtblk_chr_open,
+	.release	= virtblk_chr_release,
+};
+
 static unsigned int virtblk_queue_depth;
 module_param_named(queue_depth, virtblk_queue_depth, uint, 0444);
 
@@ -1335,7 +1421,7 @@ static int virtblk_probe(struct virtio_device *vdev)
 	/* Prevent integer overflows and honor max vq size */
 	sg_elems = min_t(u32, sg_elems, VIRTIO_BLK_MAX_SG_ELEMS - 2);
 
-	vdev->priv = vblk = kmalloc(sizeof(*vblk), GFP_KERNEL);
+	vdev->priv = vblk = kzalloc(sizeof(*vblk), GFP_KERNEL);
 	if (!vblk) {
 		err = -ENOMEM;
 		goto out_free_index;
@@ -1577,6 +1663,8 @@ static int virtblk_probe(struct virtio_device *vdev)
 	if (err)
 		goto out_cleanup_disk;
 
+	WARN_ON(virtblk_cdev_add(vblk, &virtblk_chr_fops));
+
 	return 0;
 
 out_cleanup_disk:
@@ -1600,6 +1688,8 @@ static void virtblk_remove(struct virtio_device *vdev)
 
 	/* Make sure no work handler is accessing the device. */
 	flush_work(&vblk->config_work);
+
+	virtblk_cdev_del(&vblk->cdev, &vblk->cdev_device);
 
 	del_gendisk(vblk->disk);
 	blk_mq_free_tag_set(&vblk->tag_set);
@@ -1711,11 +1801,27 @@ static int __init virtio_blk_init(void)
 		goto out_destroy_workqueue;
 	}
 
+	error = alloc_chrdev_region(&vd_chr_devt, 0, VIRTBLK_MINORS,
+				"vblk-generic");
+	if (error < 0)
+		goto out_unregister_blkdev;
+
+	vd_chr_class = class_create("vblk-generic");
+	if (IS_ERR(vd_chr_class)) {
+		error = PTR_ERR(vd_chr_class);
+		goto out_unregister_chardev;
+	}
+
 	error = register_virtio_driver(&virtio_blk);
 	if (error)
-		goto out_unregister_blkdev;
+		goto out_destroy_class;
+
 	return 0;
 
+out_destroy_class:
+	class_destroy(vd_chr_class);
+out_unregister_chardev:
+	unregister_chrdev_region(vd_chr_devt, VIRTBLK_MINORS);
 out_unregister_blkdev:
 	unregister_blkdev(major, "virtblk");
 out_destroy_workqueue:
@@ -1726,6 +1832,8 @@ out_destroy_workqueue:
 static void __exit virtio_blk_fini(void)
 {
 	unregister_virtio_driver(&virtio_blk);
+	class_destroy(vd_chr_class);
+	unregister_chrdev_region(vd_chr_devt, VIRTBLK_MINORS);
 	unregister_blkdev(major, "virtblk");
 	destroy_workqueue(virtblk_wq);
 }
