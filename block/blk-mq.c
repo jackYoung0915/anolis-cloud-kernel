3e7fdd3616ff41ec9aae4a7e811e30b100e743a2
Ferry Meng <mengferry@linux.alibaba.com>
2025-05-10 09:30:00 +0800

    anolis: blk-mq: support dynamic request alloc
    ANBZ: #20938
    
    In blk-mq layer, tag_set->static_rqs is preallocated and distributed
    later. When queue_depth is too large , we want to avoid too much memory
    overhead caused by static preallocation.
    
    Design a new tag_set flag "BLK_MQ_F_DYN_ALLOC" and tag_set new member
    "nr_static_rqs". After setting the flag, tag No. above 'nr_static_rqs'
    requests (struct request + driver defined pdu ) will be allocated.
    Others will be allocated during blk_mq_alloc_request.
    
    old_pages = [sizeof(struct request) + sizeof(struct pdu)]
                    * queue_depth  / page_size
    
    new_pages = [sizeof(struct request) + sizeof(struct pdu)]
                    * nr_static_rqs / page_size
    
    Signed-off-by: Ferry Meng <mengferry@linux.alibaba.com>
    Reviewed-by: Joseph Qi <joseph.qi@linux.alibaba.com>
    Reviewed-by: Xuan Zhuo <xuanzhuo@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5629

--- block/blk-mq.c
+++ block/blk-mq.c
@@ -381,8 +381,14 @@ static struct request *blk_mq_rq_ctx_init(struct blk_mq_alloc_data *data,
 	struct blk_mq_ctx *ctx = data->ctx;
 	struct blk_mq_hw_ctx *hctx = data->hctx;
 	struct request_queue *q = data->q;
-	struct request *rq = tags->static_rqs[tag];
+	struct request *rq;
+	struct blk_mq_tag_set *set = data->q->tag_set;
 
+	if ((set->flags & BLK_MQ_F_DYN_ALLOC) && (tag >= set->nr_static_rqs))
+		tags->static_rqs[tag] = kmalloc(sizeof(struct request) +
+			set->cmd_size, GFP_KERNEL | __GFP_ZERO);
+
+	rq = tags->static_rqs[tag];
 	rq->q = q;
 	rq->mq_ctx = ctx;
 	rq->mq_hctx = hctx;
@@ -732,7 +738,7 @@ static void __blk_mq_free_request(struct request *rq)
 	struct request_queue *q = rq->q;
 	struct blk_mq_ctx *ctx = rq->mq_ctx;
 	struct blk_mq_hw_ctx *hctx = rq->mq_hctx;
-	const int sched_tag = rq->internal_tag;
+	const int sched_tag = rq->internal_tag, tag = rq->tag;
 
 	blk_crypto_free_request(rq);
 	blk_pm_mark_last_busy(rq);
@@ -741,10 +747,23 @@ static void __blk_mq_free_request(struct request *rq)
 	if (rq->rq_flags & RQF_MQ_INFLIGHT)
 		__blk_mq_dec_active_requests(hctx);
 
-	if (rq->tag != BLK_MQ_NO_TAG)
-		blk_mq_put_tag(hctx->tags, ctx, rq->tag);
-	if (sched_tag != BLK_MQ_NO_TAG)
+	if (tag != BLK_MQ_NO_TAG) {
+		if ((q->tag_set->flags & BLK_MQ_F_DYN_ALLOC) &&
+		    tag >= q->tag_set->nr_static_rqs) {
+			hctx->tags->static_rqs[tag] = NULL;
+			kfree(rq);
+		}
+		blk_mq_put_tag(hctx->tags, ctx, tag);
+	}
+	if (sched_tag != BLK_MQ_NO_TAG) {
+		if ((q->tag_set->flags & BLK_MQ_F_DYN_ALLOC) &&
+		    sched_tag >= q->tag_set->nr_static_rqs) {
+			hctx->sched_tags->static_rqs[sched_tag] = NULL;
+			kfree(rq);
+		}
 		blk_mq_put_tag(hctx->sched_tags, ctx, sched_tag);
+	}
+
 	blk_mq_sched_restart(hctx);
 	blk_queue_exit(q);
 }
@@ -3358,6 +3377,9 @@ void blk_mq_free_rqs(struct blk_mq_tag_set *set, struct blk_mq_tags *tags,
 			if (!rq)
 				continue;
 			set->ops->exit_request(set, rq, hctx_idx);
+			if ((set->flags & BLK_MQ_F_DYN_ALLOC) &&
+			    (i >= set->nr_static_rqs))
+				kfree(rq);
 			tags->static_rqs[i] = NULL;
 		}
 	}
@@ -3484,6 +3506,14 @@ static int blk_mq_alloc_rqs(struct blk_mq_tag_set *set,
 	 */
 	rq_size = round_up(sizeof(struct request) + set->cmd_size,
 				cache_line_size());
+
+	if (set->flags & BLK_MQ_F_DYN_ALLOC) {
+		if (!set->nr_static_rqs || (set->nr_static_rqs > depth))
+			set->nr_static_rqs = depth;
+
+		depth = set->nr_static_rqs;
+	}
+
 	left = rq_size * depth;
 
 	for (i = 0; i < depth; ) {
@@ -4732,6 +4762,7 @@ int blk_mq_alloc_sq_tag_set(struct blk_mq_tag_set *set,
 	set->queue_depth = queue_depth;
 	set->numa_node = NUMA_NO_NODE;
 	set->flags = set_flags;
+	set->nr_static_rqs = 0;
 	return blk_mq_alloc_tag_set(set);
 }
 EXPORT_SYMBOL_GPL(blk_mq_alloc_sq_tag_set);
