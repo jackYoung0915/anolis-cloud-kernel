643504c51b9bcfafe8d182d439425c9e2015fc22
Ferry Meng <mengferry@linux.alibaba.com>
2025-05-10 09:30:00 +0800

    anolis: virtio-blk: add hide_bdev extra feature bit
    ANBZ: #20938
    
    Add hide block device feature bit at ext-feature bit-2. This controls
    hide /dev/vdX, chardev still exists.
    
    Signed-off-by: Ferry Meng <mengferry@linux.alibaba.com>
    Reviewed-by: Joseph Qi <joseph.qi@linux.alibaba.com>
    Reviewed-by: Xuan Zhuo <xuanzhuo@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5629

--- drivers/block/virtio_blk_ext.c
+++ drivers/block/virtio_blk_ext.c
@@ -8,6 +8,7 @@
 /* ext feature bit definition */
 #define VIRTIO_BLK_EXT_F_RING_PAIR	(1U << 0)
 #define VIRTIO_BLK_EXT_F_RING_NO_ALIGN  (1U << 1)
+#define VIRTIO_BLK_EXT_F_HIDE_BLOCK     (1U << 2)
 #define VIRTIO_BLK_EXT_F_INVAL		(-1)
 
 #define VIRTIO_PCI_VSF_MAGIC_NUM			0x0
