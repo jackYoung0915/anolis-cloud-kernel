d70835640b2d7927788d6eea024057892e45a3c9
Xuan Zhuo <xuanzhuo@linux.alibaba.com>
2025-05-12 19:01:26 +0800

    anolis: virtio_net: avoid driver waiting for requests indefinitely
    ANBZ: #20951
    
    When the device is malicious or unreliable, it will cause the
    driver to loop infinitely while waiting for the request result,
    accompanied by a CPU hang.
    
    So, Adding a kernel module parameter cvq_timeout provides a
    channel to alleviate this issue, which defaults to 7 days,
    enough to be compatible with any device.
    
    Signed-off-by: Heng Qi <hengqi@linux.alibaba.com>
    Signed-off-by: Xuan Zhuo <xuanzhuo@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6946

--- drivers/net/virtio_net.c
+++ drivers/net/virtio_net.c
@@ -36,6 +36,10 @@ module_param(csum, bool, 0444);
 module_param(gso, bool, 0444);
 module_param(napi_tx, bool, 0644);
 
+/* 7 days are long enough by default. */
+static unsigned int cvq_timeout = 7 * 24 * 3600 * 1000;
+module_param(cvq_timeout, uint, 0644);
+
 #define VIRTIO_OFFLOAD_MAP_MIN	46
 #define VIRTIO_OFFLOAD_MAP_MAX	47
 #define VIRTIO_FEATURES_MAP_MIN	65
@@ -3535,6 +3539,7 @@ static bool virtnet_send_command_reply(struct virtnet_info *vi, u8 class, u8 cmd
 				       struct scatterlist *out,
 				       struct scatterlist *in)
 {
+	unsigned long time_end = jiffies + msecs_to_jiffies(cvq_timeout);
 	struct scatterlist *sgs[5], hdr, stat;
 	u32 out_num = 0, tmp, in_num = 0;
 	bool ok;
@@ -3578,6 +3583,14 @@ static bool virtnet_send_command_reply(struct virtnet_info *vi, u8 class, u8 cmd
 	 */
 	while (!virtqueue_get_buf(vi->cvq, &tmp) &&
 	       !virtqueue_is_broken(vi->cvq)) {
+		if (time_after_eq(jiffies, time_end)) {
+			netdev_warn(vi->dev,
+				    "Timeout occurs when waiting the CVQ "
+				    "request, break the virtio device.\n");
+			virtio_break_device(vi->vdev);
+			return false;
+		}
+
 		cond_resched();
 		cpu_relax();
 	}
