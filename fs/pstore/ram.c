3067f81db5f0fb9c74daae24a59a7c69c0e071df
Yuanhe Shu <xiangzao@linux.alibaba.com>
2025-05-15 16:46:42 +0800

    anolis: pstore: add ttyprobe frontend
    ANBZ: #21134
    
    Provide a pstore frontend which can log all messages that are send
    to tty drivers when there are some problems with drivers or there
    is no access to serial ports.
    
    Using pmsg requires us to redirect the output of the user state.
    When we need to globally view the serial output of various processes,
    it is tedious to redirect the output of each process. We think pmsg is
    more suitable for targeted viewing of certain processes' output, and
    we also need a tool that can quickly do a global view so that we can
    get user-state printed data if the tty driver is working abnormally or
    if we don't have serial access.
    
    Signed-off-by: Yuanhe Shu <xiangzao@linux.alibaba.com>
    Signed-off-by: Xingrui Yi <yixingrui@linux.alibaba.com>
    Reviewed-by: Xunlei Pang <xlpang@linux.alibaba.com>
    Reviewed-by: Qinyun Tan <qinyuntan@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5318

--- fs/pstore/ram.c
+++ fs/pstore/ram.c
@@ -45,6 +45,10 @@ static ulong ramoops_pmsg_size = MIN_MEM_SIZE;
 module_param_named(pmsg_size, ramoops_pmsg_size, ulong, 0400);
 MODULE_PARM_DESC(pmsg_size, "size of user space message log");
 
+static ulong ramoops_ttyprobe_size = MIN_MEM_SIZE;
+module_param_named(ttyprobe_size, ramoops_ttyprobe_size, ulong, 0400);
+MODULE_PARM_DESC(ttyprobe_size, "size of ttyprobe message log");
+
 static unsigned long long mem_address;
 module_param_hw(mem_address, ullong, other, 0400);
 MODULE_PARM_DESC(mem_address,
@@ -82,6 +86,7 @@ struct ramoops_context {
 	struct persistent_ram_zone *cprz;	/* Console zone */
 	struct persistent_ram_zone **fprzs;	/* Ftrace zones */
 	struct persistent_ram_zone *mprz;	/* PMSG zone */
+	struct persistent_ram_zone *tprz;	/* TTYPROBE zone */
 	phys_addr_t phys_addr;
 	unsigned long size;
 	unsigned int memtype;
@@ -89,6 +94,7 @@ struct ramoops_context {
 	size_t console_size;
 	size_t ftrace_size;
 	size_t pmsg_size;
+	size_t ttyprobe_size;
 	u32 flags;
 	struct persistent_ram_ecc_info ecc_info;
 	unsigned int max_dump_cnt;
@@ -99,6 +105,7 @@ struct ramoops_context {
 	unsigned int max_ftrace_cnt;
 	unsigned int ftrace_read_cnt;
 	unsigned int pmsg_read_cnt;
+	unsigned int ttyprobe_read_cnt;
 	struct pstore_info pstore;
 };
 
@@ -112,6 +119,7 @@ static int ramoops_pstore_open(struct pstore_info *psi)
 	cxt->console_read_cnt = 0;
 	cxt->ftrace_read_cnt = 0;
 	cxt->pmsg_read_cnt = 0;
+	cxt->ttyprobe_read_cnt = 0;
 	return 0;
 }
 
@@ -214,6 +222,9 @@ static ssize_t ramoops_pstore_read(struct pstore_record *record)
 
 	if (!prz_ok(prz) && !cxt->pmsg_read_cnt++)
 		prz = ramoops_get_next_prz(&cxt->mprz, 0 /* single */, record);
+
+	if (!prz_ok(prz) && !cxt->ttyprobe_read_cnt++)
+		prz = ramoops_get_next_prz(&cxt->tprz, 0 /* single */, record);
 
 	/* ftrace is last since it may want to dynamically allocate memory. */
 	if (!prz_ok(prz)) {
@@ -336,6 +347,11 @@ static int notrace ramoops_pstore_write(struct pstore_record *record)
 	} else if (record->type == PSTORE_TYPE_PMSG) {
 		pr_warn_ratelimited("PMSG shouldn't call %s\n", __func__);
 		return -EINVAL;
+	} else if (record->type == PSTORE_TYPE_TTYPROBE) {
+		if (!cxt->tprz)
+			return -ENOMEM;
+		persistent_ram_write(cxt->tprz, record->buf, record->size);
+		return 0;
 	}
 
 	if (record->type != PSTORE_TYPE_DMESG)
@@ -427,6 +443,9 @@ static int ramoops_pstore_erase(struct pstore_record *record)
 	case PSTORE_TYPE_PMSG:
 		prz = cxt->mprz;
 		break;
+	case PSTORE_TYPE_TTYPROBE:
+		prz = cxt->tprz;
+		break;
 	default:
 		return -EINVAL;
 	}
@@ -458,6 +477,9 @@ static void ramoops_free_przs(struct ramoops_context *cxt)
 
 	/* Free console PRZ */
 	persistent_ram_free(&cxt->cprz);
+
+	/* Free ttyprobe PRZ*/
+	persistent_ram_free(&cxt->tprz);
 
 	/* Free dump PRZs */
 	if (cxt->dprzs) {
@@ -687,6 +709,7 @@ static int ramoops_parse_dt(struct platform_device *pdev,
 	parse_u32("console-size", pdata->console_size, 0);
 	parse_u32("ftrace-size", pdata->ftrace_size, 0);
 	parse_u32("pmsg-size", pdata->pmsg_size, 0);
+	parse_u32("ttyprobe-size", pdata->ttyprobe_size, 0);
 	parse_u32("ecc-size", pdata->ecc_info.ecc_size, 0);
 	parse_u32("flags", pdata->flags, 0);
 	parse_u32("max-reason", pdata->max_reason, pdata->max_reason);
@@ -707,9 +730,10 @@ static int ramoops_parse_dt(struct platform_device *pdev,
 	parent_node = of_get_parent(of_node);
 	if (!of_node_name_eq(parent_node, "reserved-memory") &&
 	    !pdata->console_size && !pdata->ftrace_size &&
-	    !pdata->pmsg_size && !pdata->ecc_info.ecc_size) {
+	    !pdata->pmsg_size && !pdata->ttyprobe_size && !pdata->ecc_info.ecc_size) {
 		pdata->console_size = pdata->record_size;
 		pdata->pmsg_size = pdata->record_size;
+		pdata->ttyprobe_size = pdata->record_size;
 	}
 	of_node_put(parent_node);
 
@@ -767,6 +791,8 @@ static int ramoops_probe(struct platform_device *pdev)
 		pdata->ftrace_size = rounddown_pow_of_two(pdata->ftrace_size);
 	if (pdata->pmsg_size && !is_power_of_2(pdata->pmsg_size))
 		pdata->pmsg_size = rounddown_pow_of_two(pdata->pmsg_size);
+	if (pdata->ttyprobe_size && !is_power_of_2(pdata->ttyprobe_size))
+		pdata->ttyprobe_size = rounddown_pow_of_two(pdata->ttyprobe_size);
 
 	cxt->size = pdata->mem_size;
 	cxt->phys_addr = pdata->mem_address;
@@ -775,13 +801,14 @@ static int ramoops_probe(struct platform_device *pdev)
 	cxt->console_size = pdata->console_size;
 	cxt->ftrace_size = pdata->ftrace_size;
 	cxt->pmsg_size = pdata->pmsg_size;
+	cxt->ttyprobe_size = pdata->ttyprobe_size;
 	cxt->flags = pdata->flags;
 	cxt->ecc_info = pdata->ecc_info;
 
 	paddr = cxt->phys_addr;
 
 	dump_mem_sz = cxt->size - cxt->console_size - cxt->ftrace_size
-			- cxt->pmsg_size;
+		- cxt->pmsg_size - cxt->ttyprobe_size;
 	err = ramoops_init_przs("dmesg", dev, cxt, &cxt->dprzs, &paddr,
 				dump_mem_sz, cxt->record_size,
 				&cxt->max_dump_cnt, 0, 0);
@@ -795,6 +822,11 @@ static int ramoops_probe(struct platform_device *pdev)
 
 	err = ramoops_init_prz("pmsg", dev, cxt, &cxt->mprz, &paddr,
 				cxt->pmsg_size, 0);
+	if (err)
+		goto fail_init;
+
+	err = ramoops_init_prz("ttyprobe", dev, cxt, &cxt->tprz, &paddr,
+				cxt->ttyprobe_size, 0);
 	if (err)
 		goto fail_init;
 
@@ -827,6 +859,8 @@ static int ramoops_probe(struct platform_device *pdev)
 		cxt->pstore.flags |= PSTORE_FLAGS_FTRACE;
 	if (cxt->pmsg_size)
 		cxt->pstore.flags |= PSTORE_FLAGS_PMSG;
+	if (cxt->ttyprobe_size)
+		cxt->pstore.flags |= PSTORE_FLAGS_TTYPROBE;
 
 	/*
 	 * Since bufsize is only used for dmesg crash dumps, it
@@ -860,6 +894,7 @@ static int ramoops_probe(struct platform_device *pdev)
 	ramoops_console_size = pdata->console_size;
 	ramoops_pmsg_size = pdata->pmsg_size;
 	ramoops_ftrace_size = pdata->ftrace_size;
+	ramoops_ttyprobe_size = pdata->ttyprobe_size;
 
 	pr_info("using 0x%lx@0x%llx, ecc: %d\n",
 		cxt->size, (unsigned long long)cxt->phys_addr,
@@ -931,6 +966,7 @@ static void __init ramoops_register_dummy(void)
 	pdata.console_size = ramoops_console_size;
 	pdata.ftrace_size = ramoops_ftrace_size;
 	pdata.pmsg_size = ramoops_pmsg_size;
+	pdata.ttyprobe_size = ramoops_ttyprobe_size;
 	/* If "max_reason" is set, its value has priority over "dump_oops". */
 	if (ramoops_max_reason >= 0)
 		pdata.max_reason = ramoops_max_reason;
