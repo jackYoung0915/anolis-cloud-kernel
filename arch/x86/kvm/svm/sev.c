eec985b933784b6a27166c7a4a9aeeb6403c23b2
hanliyang <hanliyang@hygon.cn>
2025-05-15 16:19:39 +0800

    anolis: KVM: SVM: The ASID range available for CSV2 guests vary across different firmware
    ANBZ: #23279
    
    For firmware with a build ID < 1810, if CPUID_0x8000_001F_EDX.bits0-31 is
    1, the ASID range available for CSV2 guests is [0, 0) (i.e., no ASIDs are
    available). Otherwise, the range is [1, CPUID_0x8000_001F_EDX.bits0-31).
    
    For firmware with a build ID >= 1810, the ASID range available for CSV2
    guests is [1, CPUID_0x8000_001F_ECX.bits0-31].
    
    Fixes: 37a1f4ca7a29 ("anolis: KVM: SVM: Fix the available ASID range for CSV2 guest")
    Signed-off-by: hanliyang <hanliyang@hygon.cn>
    Reviewed-by: Guixin Liu <kanie@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5584

--- arch/x86/kvm/svm/sev.c
+++ arch/x86/kvm/svm/sev.c
@@ -146,10 +146,10 @@ static int sev_asid_new(bool es_active, const char *userid, u32 userid_len)
 	max_asid = es_active ? min_sev_asid - 1 : max_sev_asid;
 
 	/*
-	 * No matter what the min_sev_asid is, all asids in range
+	 * When the firmware is with build ID >= 1810, all asids in range
 	 * [1, max_sev_asid] can be used for CSV2 guest on Hygon CPUs.
 	 */
-	if (boot_cpu_data.x86_vendor == X86_VENDOR_HYGON)
+	if (is_x86_vendor_hygon() && hygon_csv_build >= 1810)
 		max_asid = max_sev_asid;
 again:
 	pos = find_next_zero_bit(sev_asid_bitmap, max_sev_asid, min_asid);
@@ -2840,12 +2840,18 @@ void __init sev_hardware_setup(void)
 	if (!boot_cpu_has(X86_FEATURE_SEV_ES))
 		goto out;
 
-	/*
-	 * The ASIDs from 1 to max_sev_asid are available for hygon CSV2
-	 * guest.
-	 */
-	if (boot_cpu_data.x86_vendor == X86_VENDOR_HYGON) {
-		pr_info("CSV2 supported: %u ASIDs\n", max_sev_asid);
+	if (is_x86_vendor_hygon()) {
+		if (hygon_csv_build < 1810 && min_sev_asid <= 1)
+			goto out;
+
+		/*
+		 * If firmware version >= 1810, the ASIDs from 1 to max_sev_asid
+		 * are available for hygon CSV2 guest.
+		 * If firmware version < 1810, the ASIDs from 1 to CPUID function
+		 * 0x8000_001F_EDX are available for hygon CSV2 guest.
+		 */
+		pr_info("CSV2 supported: %u ASIDs\n",
+			hygon_csv_build >= 1810 ? max_sev_asid : min_sev_asid - 1);
 		sev_es_supported = true;
 		goto out;
 	}
