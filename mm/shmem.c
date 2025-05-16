834b2a906e09c67e0b1953cd3dec50de72a36545
Kemeng Shi <shikemeng@huaweicloud.com>
2025-05-17 01:09:35 +0800

    mm: shmem: avoid unpaired folio_unlock() in shmem_swapin_folio()
    ANBZ: #9728
    
    commit e08d5f515613a9860bfee7312461a19f422adb5e upstream.
    
    Patch series "Some random fixes and cleanup to shmem", v3.
    
    This series contains some simple fixes and cleanup which are made during
    learning shmem.  More details can be found in respective patches.
    
    This patch (of 5):
    
    If we get a folio from swap_cache_get_folio() successfully but encounter a
    failure before the folio is locked, we will unlock the folio which was not
    previously locked.
    
    Put the folio and set it to NULL when a failure occurs before the folio is
    locked to fix the issue.
    
    [Zelin Deng: Hmm.. the diff segment of the orignal patch can be
    completely confused when doing git am, as there're 2 identical code
    context where the diff segment can be applied in without any conflict.
    Put them in right place.]
    
    Link: https://lkml.kernel.org/r/20250516170939.965736-1-shikemeng@huaweicloud.com
    Link: https://lkml.kernel.org/r/20250516170939.965736-2-shikemeng@huaweicloud.com
    Fixes: 058313515d5a ("mm: shmem: fix potential data corruption during shmem swapin")
    Signed-off-by: Kemeng Shi <shikemeng@huaweicloud.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Reviewed-by: Kairui Song <kasong@tencent.com>
    Cc: Hugh Dickins <hughd@google.com>
    Cc: kernel test robot <oliver.sang@intel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Zelin Deng <zelin.deng@linux.alibaba.com>

--- mm/shmem.c
+++ mm/shmem.c
@@ -2359,6 +2359,8 @@ static int shmem_swapin_folio(struct inode *inode, pgoff_t index,
 		 */
 		split_order = shmem_split_large_entry(inode, index, swap, gfp);
 		if (split_order < 0) {
+			folio_put(folio);
+			folio = NULL;
 			error = split_order;
 			goto failed;
 		}
