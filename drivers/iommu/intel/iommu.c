1a0cd93cbf9a522827829617d338858070688172
Lu Baolu <baolu.lu@linux.intel.com>
2025-05-20 15:58:49 +0800

    iommu/vt-d: Restore context entry setup order for aliased devices
    ANBZ: #13617
    
    commit 320302baed05c6456164652541f23d2a96522c06 upstream.
    
    Commit 2031c469f816 ("iommu/vt-d: Add support for static identity domain")
    changed the context entry setup during domain attachment from a
    set-and-check policy to a clear-and-reset approach. This inadvertently
    introduced a regression affecting PCI aliased devices behind PCIe-to-PCI
    bridges.
    
    Specifically, keyboard and touchpad stopped working on several Apple
    Macbooks with below messages:
    
     kernel: platform pxa2xx-spi.3: Adding to iommu group 20
     kernel: input: Apple SPI Keyboard as
     /devices/pci0000:00/0000:00:1e.3/pxa2xx-spi.3/spi_master/spi2/spi-APP000D:00/input/input0
     kernel: DMAR: DRHD: handling fault status reg 3
     kernel: DMAR: [DMA Read NO_PASID] Request device [00:1e.3] fault addr
     0xffffa000 [fault reason 0x06] PTE Read access is not set
     kernel: DMAR: DRHD: handling fault status reg 3
     kernel: DMAR: [DMA Read NO_PASID] Request device [00:1e.3] fault addr
     0xffffa000 [fault reason 0x06] PTE Read access is not set
     kernel: applespi spi-APP000D:00: Error writing to device: 01 0e 00 00
     kernel: DMAR: DRHD: handling fault status reg 3
     kernel: DMAR: [DMA Read NO_PASID] Request device [00:1e.3] fault addr
     0xffffa000 [fault reason 0x06] PTE Read access is not set
     kernel: DMAR: DRHD: handling fault status reg 3
     kernel: applespi spi-APP000D:00: Error writing to device: 01 0e 00 00
    
    Fix this by restoring the previous context setup order.
    
    Fixes: 2031c469f816 ("iommu/vt-d: Add support for static identity domain")
    Closes: https://lore.kernel.org/all/4dada48a-c5dd-4c30-9c85-5b03b0aa01f0@bfh.ch/
    Cc: stable@vger.kernel.org
    Signed-off-by: Lu Baolu <baolu.lu@linux.intel.com>
    Reviewed-by: Kevin Tian <kevin.tian@intel.com>
    Reviewed-by: Yi Liu <yi.l.liu@intel.com>
    Link: https://lore.kernel.org/r/20250514060523.2862195-1-baolu.lu@linux.intel.com
    Link: https://lore.kernel.org/r/20250520075849.755012-2-baolu.lu@linux.intel.com
    Signed-off-by: Joerg Roedel <jroedel@suse.de>
    Signed-off-by: Jay Chen <jkchen@linux.alibaba.com>
    Reviewed-by: Shuai Xue <xueshuai@linxu.alibaba.com>
    Reviewed-by: Guanghui Feng <guanghuifeng@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5822

--- drivers/iommu/intel/iommu.c
+++ drivers/iommu/intel/iommu.c
@@ -1809,6 +1809,7 @@ static int dmar_domain_attach_device(struct dmar_domain *domain,
 		return ret;
 
 	info->domain = domain;
+	info->domain_attached = true;
 	spin_lock_irqsave(&domain->lock, flags);
 	list_add(&info->link, &domain->devices);
 	spin_unlock_irqrestore(&domain->lock, flags);
@@ -3265,6 +3266,10 @@ void device_block_translation(struct device *dev)
 	struct intel_iommu *iommu = info->iommu;
 	unsigned long flags;
 
+	/* Device in DMA blocking state. Noting to do. */
+	if (!info->domain_attached)
+		return;
+
 	if (info->domain)
 		cache_tag_unassign_domain(info->domain, dev, IOMMU_NO_PASID);
 
@@ -3275,6 +3280,9 @@ void device_block_translation(struct device *dev)
 		else
 			domain_context_clear(info);
 	}
+
+	/* Device now in DMA blocking state. */
+	info->domain_attached = false;
 
 	if (!info->domain)
 		return;
@@ -4360,6 +4368,9 @@ static int identity_domain_attach_dev(struct iommu_domain *domain, struct device
 		ret = intel_pasid_setup_pass_through(iommu, dev, IOMMU_NO_PASID);
 	else
 		ret = device_setup_pass_through(dev);
+
+	if (!ret)
+		info->domain_attached = true;
 
 	return ret;
 }
