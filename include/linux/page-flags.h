e87d4e442fce43fa6a822bce434a9455550d3c99
Li Qian <liqian1556@phytium.com.cn>
2025-05-20 15:09:15 +0800

    anolis: phytium: pswiotlb: Add PSWIOTLB mechanism to improve DMA performance
    ANBZ: #21132
    
    This patch added additional "memory copy" to improve D2H direction
    DMA performance on Phytium Server SoCs.
    
    Signed-off-by: Cui Chao <cuichao1753@phytium.com.cn>
    Signed-off-by: Li Qian <LiQian1556@phytium.com.cn>
    Reviewed-by: Jay Chen <jkchen@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5319

--- include/linux/page-flags.h
+++ include/linux/page-flags.h
@@ -203,6 +203,13 @@ enum pageflags {
 	/* Reuse PG_dirty to indicate whether the duplicate page is a master or slave */
 	PG_dup_slave = PG_dirty,
 #endif
+
+#ifdef CONFIG_PSWIOTLB
+	/* check if pswiotlb is sync already */
+	PG_pswiotlbsync = __NR_PAGEFLAGS + 1,
+	/* check if the page is used for pswiotlb */
+	PG_pswiotlb,
+#endif
 };
 
 #define PAGEFLAGS_MASK		((1UL << NR_PAGEFLAGS) - 1)
