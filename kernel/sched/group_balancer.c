ede3a68d1ebd279a2d757b300fe8c985cdb89400
Cruz Zhao <CruzZhao@linux.alibaba.com>
2025-05-21 14:51:08 +0800

    anolis: sched: make sure root_cpumask of group balancer is online cpumask
    ANBZ: #8765
    
    Under certain scenarios (such as virtualization environments, and
    parameters like max cpus are configured in the cmdline), the
    cpu_possible_mask may be incorrect, where cpu_possible_mask contains
    some cpus which doesn't exist. Under these scenarios, group balancer
    may reference null pointer when building group balancer sched domains.
    
    To avoid unexpected scenarios, we use the intersection of
    housekeeping_cpumask(HK_FLAG_DOMAIN) and cpu_online_mask as
    root_cpumask, making sure that root_cpumask of group_balancer is online
    cpumask.
    
    Consider that the cpumask of each topology may not reliable when boot,
    we validate the topology level again before we build group balancer
    sched domains.
    
    Signed-off-by: Cruz Zhao <CruzZhao@linux.alibaba.com>
    Reviewed-by: Tianchen Ding <dtcccc@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5434

--- kernel/sched/group_balancer.c
+++ kernel/sched/group_balancer.c
@@ -960,6 +960,8 @@ static int build_group_balancer_root_domain(void)
 	return 0;
 }
 
+static void validate_topology_levels(void);
+
 /* BFS to build group balancer sched domain tree. */
 static int build_group_balancer_sched_domains(void)
 {
@@ -970,6 +972,8 @@ static int build_group_balancer_sched_domains(void)
 	struct group_balancer_sched_domain *parent, *n;
 	char *name = NULL;
 
+	update_group_balancer_root_cpumask();
+	validate_topology_levels();
 	/*
 	 * The group balancer sched domain is a tree.
 	 * If the root was not built on boot, build the root node first.
@@ -1268,7 +1272,7 @@ cleanup_root:
 
 void update_group_balancer_root_cpumask(void)
 {
-	cpumask_copy(&root_cpumask, housekeeping_cpumask(HK_FLAG_DOMAIN));
+	cpumask_and(&root_cpumask, housekeeping_cpumask(HK_FLAG_DOMAIN), cpu_online_mask);
 }
 
 static int __init group_balancer_init(void)
