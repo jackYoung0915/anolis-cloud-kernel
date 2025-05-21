47e20559ddad3aeeb6fa43e4e64dd0493aa13bb0
liqiang <liq@ubuntu-liun.yunsilicon.com>
2025-05-21 15:02:35 +0800

    anolis: drivers: update Yunsilicon drivers to version 2412GA
    ANBZ: #21138
    
    Improving xscale driver performance and stability.
    sw ver: 1.4.0.321.H159
    
    Signed-off-by: Liqiang <liq@yunsilicon.com>
    Reviewed-by: Tianxin <tianx@yunsilicon.com>
    Reviewed-by: Weihonggang <weihg@yunsilicon.com>
    Reviewed-by: Jacky <jacky@yunsilicon.com>

--- drivers/infiniband/hw/xsc/xsc_ib_compat.h
+++ drivers/infiniband/hw/xsc/xsc_ib_compat.h
@@ -7,10 +7,6 @@
 #ifndef XSC_IB_COMPAT_H
 #define XSC_IB_COMPAT_H
 
-/*
- * adaptive to different ib_core versions
- */
-
 struct xsc_ib_ucontext;
 
 int xsc_ib_create_ah(struct ib_ah *ibah, struct rdma_ah_init_attr *ah_attr,
@@ -20,10 +16,12 @@ int xsc_ib_create_ah(struct ib_ah *ibah, struct rdma_ah_init_attr *ah_attr,
 
 int xsc_ib_destroy_ah(struct ib_ah *ah, u32 flags);
 #define xsc_ib_destroy_ah_def() int xsc_ib_destroy_ah(struct ib_ah *ah, u32 flags)
-int xsc_ib_destroy_qp(struct ib_qp *qp, struct ib_udata *udata);
+
 int xsc_ib_create_cq(struct ib_cq *ibcq,
 		     const struct ib_cq_init_attr *attr,
 		     struct ib_udata *udata);
+
+int xsc_ib_destroy_qp(struct ib_qp *qp, struct ib_udata *udata);
 int xsc_ib_dereg_mr(struct ib_mr *ibmr, struct ib_udata *udata);
 
 // from main.c static functions
@@ -38,8 +36,6 @@ int xsc_ib_destroy_cq(struct ib_cq *cq, struct ib_udata *udata);
 #define xsc_ib_destroy_cq_def() int xsc_ib_destroy_cq(struct ib_cq *cq, struct ib_udata *udata)
 
 #define xsc_ib_destroy_qp_def() int xsc_ib_destroy_qp(struct ib_qp *qp, struct ib_udata *udata)
-#define xsc_ib_create_cq_def() int xsc_ib_create_cq(struct ib_cq *ibcq,\
-	const struct ib_cq_init_attr *attr, struct ib_udata *udata)
 #define xsc_ib_dereg_mr_def() int xsc_ib_dereg_mr(struct ib_mr *ibmr, struct ib_udata *udata)
 #define xsc_ib_alloc_ucontext_def() int xsc_ib_alloc_ucontext(\
 	struct ib_ucontext *uctx, struct ib_udata *udata)
@@ -48,15 +44,8 @@ int xsc_ib_destroy_cq(struct ib_cq *cq, struct ib_udata *udata);
 
 #define RET_VALUE(x) (x)
 
-#ifdef IB_ALLOC_MR_HAVE_UDATA
-struct ib_mr *xsc_ib_alloc_mr(struct ib_pd *pd, enum ib_mr_type mr_type,
-			      u32 max_num_sg, struct ib_udata *udata);
-#define xsc_ib_alloc_mr_def() struct ib_mr *xsc_ib_alloc_mr(\
-	struct ib_pd *pd, enum ib_mr_type mr_type, u32 max_num_sg, struct ib_udata *udata)
-#else
 struct ib_mr *xsc_ib_alloc_mr(struct ib_pd *pd, enum ib_mr_type mr_type, u32 max_num_sg);
 #define xsc_ib_alloc_mr_def() struct ib_mr *xsc_ib_alloc_mr(\
 	struct ib_pd *pd, enum ib_mr_type mr_type, u32 max_num_sg)
-#endif
 
 #endif
