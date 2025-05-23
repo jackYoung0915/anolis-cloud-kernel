a33e4f085392018456196d3da17966ca6a2b0f43
He Chuyue <hechuyue@wxiat.com>
2025-05-23 11:02:38 +0800

    anolis: sw64: perf: fix compilation error
    ANBZ: #4688
    
    Since the definition of EM_ARCH used under perf is inconsistent with the
    kernel, fix it to make it consistent.
    
    Signed-off-by: He Chuyue <hechuyue@wxiat.com>
    Reviewed-by: He Sheng <hesheng@wxiat.com>
    Signed-off-by: Gu Zitao <guzitao@wxiat.com>

--- tools/perf/util/dwarf-regs.c
+++ tools/perf/util/dwarf-regs.c
@@ -18,6 +18,10 @@
 #define EM_LOONGARCH	258 /* LoongArch */
 #endif
 
+#ifndef EM_SW64
+#define EM_SW64		0x9916 /* SW64 */
+#endif
+
 /* Define const char * {arch}_register_tbl[] */
 #define DEFINE_DWARF_REGSTR_TABLE
 #include "../arch/x86/include/dwarf-regs-table.h"
@@ -61,7 +65,7 @@ const char *get_dwarf_regstr(unsigned int n, unsigned int machine)
 		return __get_dwarf_regstr(xtensa_regstr_tbl, n);
 	case EM_LOONGARCH:
 		return __get_dwarf_regstr(loongarch_regstr_tbl, n);
-	case EM_SW_64:
+	case EM_SW64:
 		return __get_dwarf_regstr(sw_64_regstr_tbl, n);
 	default:
 		pr_err("ELF MACHINE %x is not supported.\n", machine);
