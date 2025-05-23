b820cba6d73e6e76173bb7d7a25c235b88a35ab4
Jinqian Yang <yangjinqian1@huawei.com>
2025-05-23 20:45:33 +0800

    KVM: arm64: using kvm_vgic_global_state for ipiv
    ANBZ: #33010
    
    commit 730562db1d901a6d175a76c5a35c4d4a867d150d openEuler.
    
    when kvm-arm.vgic_v4_enable=0 kvm-arm.ipiv_enabled=1 is configured in the
    cmdline, the Host KVM displays "ipiv enabled", but ipiv is not enabled.
    gic_data.rdists.has_rvpeid is hardware-level information, which does not
    indicate that GICv4.1 is enabled for KVM.
    
    So whether the host supports IPIV depends on KVM enables GICv4.1m instead
    of hardware supports GICv4.1.
    
    Signed-off-by: Jinqian Yang <yangjinqian1@huawei.com>
    [zhangxinghao:
    	Only the context text is different. No actual code is changed.
    Conflicts:
    	arch/arm64/include/asm/kvm_host.h
    	arch/arm64/kvm/arm.c
    	drivers/irqchip/irq-gic-v3.c]
    Signed-off-by: zhangxinghao <zhangxinghao@kunlunit.com>
    Reviewed-by: Bin Guo <guobin@linux.alibaba.com>
    Reviewed-by: Shuai Xue <xueshuai@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6790

--- arch/arm64/include/asm/kvm_host.h
+++ arch/arm64/include/asm/kvm_host.h
@@ -1505,8 +1505,5 @@ static inline void host_kvm_vcpu_pmu_resync_el0(void)
 #endif
 
 u32 kvm_pv_cpu_freq_get(struct kvm_vcpu *vcpu);
-#ifdef CONFIG_ARM64_HISI_IPIV
-extern bool kvm_ipiv_support;
-#endif
 
 #endif /* __ARM64_KVM_HOST_H__ */
