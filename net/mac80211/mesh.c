5894c219dc086852b1da874688d6dba6fd8ac6ab
shaomingyin <shao.mingyin@zte.com.cn>
2025-05-27 14:04:16 +0800

    wifi: mac80211: fix mesh id corruption on 32 bit systems
    ANBZ: #21516
    
    commit 6e48ebffc2db5419b3a51cfc509bde442252b356 upstream.
    
    Since the changed field size was increased to u64, mesh_bss_info_changed
    pulls invalid bits from the first 3 bytes of the mesh id, clears them, and
    passes them on to ieee80211_link_info_change_notify, because
    ifmsh->mbss_changed was not updated to match its size.
    Fix this by turning into ifmsh->mbss_changed into an unsigned long array with
    64 bit size.
    
    Fixes: CVE-2024-57899
    Fixes: 15ddba5f4311 ("wifi: mac80211: consistently use u64 for BSS changes")
    Reported-by: Thomas Hühn <thomas.huehn@hs-nordhausen.de>
    Signed-off-by: Felix Fietkau <nbd@nbd.name>
    Link: https://lore.kernel.org/r/20230913050134.53536-1-nbd@nbd.name
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Signed-off-by: Shao Mingyin <shao.mingyin@zte.com.cn>

--- net/mac80211/mesh.c
+++ net/mac80211/mesh.c
@@ -985,7 +985,7 @@ void ieee80211_mbss_info_change_notify(struct ieee80211_sub_if_data *sdata,
 
 	/* if we race with running work, worst case this work becomes a noop */
 	for_each_set_bit(bit, &bits, sizeof(changed) * BITS_PER_BYTE)
-		set_bit(bit, &ifmsh->mbss_changed);
+		set_bit(bit, ifmsh->mbss_changed);
 	set_bit(MESH_WORK_MBSS_CHANGED, &ifmsh->wrkq_flags);
 	ieee80211_queue_work(&sdata->local->hw, &sdata->work);
 }
@@ -1067,7 +1067,7 @@ void ieee80211_stop_mesh(struct ieee80211_sub_if_data *sdata)
 
 	/* clear any mesh work (for next join) we may have accrued */
 	ifmsh->wrkq_flags = 0;
-	ifmsh->mbss_changed = 0;
+	memset(ifmsh->mbss_changed, 0, sizeof(ifmsh->mbss_changed));
 
 	local->fif_other_bss--;
 	atomic_dec(&local->iff_allmultis);
@@ -1522,9 +1522,9 @@ static void mesh_bss_info_changed(struct ieee80211_sub_if_data *sdata)
 	struct ieee80211_if_mesh *ifmsh = &sdata->u.mesh;
 	u32 bit, changed = 0;
 
-	for_each_set_bit(bit, &ifmsh->mbss_changed,
+	for_each_set_bit(bit, ifmsh->mbss_changed,
 			 sizeof(changed) * BITS_PER_BYTE) {
-		clear_bit(bit, &ifmsh->mbss_changed);
+		clear_bit(bit, ifmsh->mbss_changed);
 		changed |= BIT(bit);
 	}
 
