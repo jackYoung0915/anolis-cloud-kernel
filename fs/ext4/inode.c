b2ce14c24cb31b17a4755d4cc8461fa22d927f88
Joseph Qi <joseph.qi@linux.alibaba.com>
2025-05-28 14:33:16 +0800

    anolis: Revert "ext4: implement buffered read iomap path"
    ANBZ: #21672
    
    This reverts commit b0205aef984b83cc0e3231472877273ce74d508a.
    
    We'll support ext4 large folio in buffer head way later.
    
    Signed-off-by: Joseph Qi <joseph.qi@linux.alibaba.com>

--- fs/ext4/inode.c
+++ fs/ext4/inode.c
@@ -3544,47 +3544,13 @@ const struct iomap_ops ext4_iomap_report_ops = {
 	.iomap_begin = ext4_iomap_begin_report,
 };
 
-static int ext4_iomap_buffered_read_begin(struct inode *inode, loff_t offset,
-			loff_t length, unsigned int flags, struct iomap *iomap,
-			struct iomap *srcmap)
-{
-	int ret;
-	struct ext4_map_blocks map;
-	u8 blkbits = inode->i_blkbits;
-
-	if (unlikely(ext4_forced_shutdown(inode->i_sb)))
-		return -EIO;
-	if ((offset >> blkbits) > EXT4_MAX_LOGICAL_BLOCK)
-		return -EINVAL;
-	/* Inline data support is not yet available. */
-	if (WARN_ON_ONCE(ext4_has_inline_data(inode)))
-		return -ERANGE;
-
-	/* Calculate the first and last logical blocks respectively. */
-	map.m_lblk = offset >> blkbits;
-	map.m_len = min_t(loff_t, (offset + length - 1) >> blkbits,
-			  EXT4_MAX_LOGICAL_BLOCK) - map.m_lblk + 1;
-
-	ret = ext4_map_blocks(NULL, inode, &map, 0);
-	if (ret < 0)
-		return ret;
-
-	ext4_set_iomap(inode, iomap, &map, offset, length, flags);
-	return 0;
-}
-
-const struct iomap_ops ext4_iomap_buffered_read_ops = {
-	.iomap_begin = ext4_iomap_buffered_read_begin,
-};
-
 static int ext4_iomap_read_folio(struct file *file, struct folio *folio)
 {
-	return iomap_read_folio(folio, &ext4_iomap_buffered_read_ops);
+	return 0;
 }
 
 static void ext4_iomap_readahead(struct readahead_control *rac)
 {
-	iomap_readahead(rac, &ext4_iomap_buffered_read_ops);
 }
 
 static int ext4_iomap_writepages(struct address_space *mapping,
