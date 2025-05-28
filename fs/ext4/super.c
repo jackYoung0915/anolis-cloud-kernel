378c6a5af1740a091403d8c42d85652007b9c277
Joseph Qi <joseph.qi@linux.alibaba.com>
2025-05-28 14:28:57 +0800

    anolis: Revert "anolis: ext4: don't allow remount with buffered iomap"
    ANBZ: #21672
    
    This reverts commit 3bea1fbb521595c111d0a9c119d5049507d9c8a2.
    
    We'll support ext4 large folio in buffer head way later.
    
    Signed-off-by: Joseph Qi <joseph.qi@linux.alibaba.com>

--- fs/ext4/super.c
+++ fs/ext4/super.c
@@ -1712,7 +1712,8 @@ enum {
 	Opt_dioread_nolock, Opt_dioread_lock,
 	Opt_discard, Opt_nodiscard, Opt_init_itable, Opt_noinit_itable,
 	Opt_max_dir_size_kb, Opt_nojournal_checksum, Opt_nombcache,
-	Opt_no_prefetch_block_bitmaps, Opt_mb_optimize_scan, Opt_buffered_iomap,
+	Opt_no_prefetch_block_bitmaps, Opt_mb_optimize_scan,
+	Opt_buffered_iomap, Opt_nobuffered_iomap,
 	Opt_errors, Opt_data, Opt_data_err, Opt_jqfmt, Opt_dax_type,
 #ifdef CONFIG_EXT4_DEBUG
 	Opt_fc_debug_max_replay, Opt_fc_debug_force
@@ -1856,6 +1857,7 @@ static const struct fs_parameter_spec ext4_param_specs[] = {
 						Opt_no_prefetch_block_bitmaps),
 	fsparam_s32	("mb_optimize_scan",	Opt_mb_optimize_scan),
 	fsparam_flag	("buffered_iomap",	Opt_buffered_iomap),
+	fsparam_flag	("nobuffered_iomap",	Opt_nobuffered_iomap),
 	fsparam_string	("check",		Opt_removed),	/* mount option from ext2/3 */
 	fsparam_flag	("nocheck",		Opt_removed),	/* mount option from ext2/3 */
 	fsparam_flag	("reservation",		Opt_removed),	/* mount option from ext2/3 */
@@ -1952,6 +1954,8 @@ static const struct mount_opts {
 	 MOPT_SET},
 	{Opt_buffered_iomap, EXT4_MOUNT2_BUFFERED_IOMAP,
 	 MOPT_SET | MOPT_2 | MOPT_EXT4_ONLY},
+	{Opt_nobuffered_iomap, EXT4_MOUNT2_BUFFERED_IOMAP,
+	 MOPT_CLEAR | MOPT_2 | MOPT_EXT4_ONLY},
 #ifdef CONFIG_EXT4_DEBUG
 	{Opt_fc_debug_force, EXT4_MOUNT2_JOURNAL_FAST_COMMIT,
 	 MOPT_SET | MOPT_2 | MOPT_EXT4_ONLY},
@@ -2438,11 +2442,6 @@ static int ext4_parse_param(struct fs_context *fc, struct fs_parameter *param)
 			return -EINVAL;
 		}
 		return 0;
-	case Opt_buffered_iomap:
-		ext4_msg(NULL, KERN_WARNING,
-			 "buffered iomap enabled. Waring: EXPERIMENTAL, use at your own risk");
-		ctx_set_mount_opt2(ctx, EXT4_MOUNT2_BUFFERED_IOMAP);
-		return 0;
 	}
 
 	/*
@@ -2830,6 +2829,10 @@ static int ext4_check_opt_consistency(struct fs_context *fc,
 		return -EINVAL;
 	}
 
+	if (ctx_test_mount_opt2(ctx, EXT4_MOUNT2_BUFFERED_IOMAP))
+		ext4_msg(NULL, KERN_WARNING,
+			 "Warning: mounting with an experimental option 'buffered_iomap'");
+
 	err = ext4_check_test_dummy_encryption(fc, sb);
 	if (err)
 		return err;
@@ -2872,12 +2875,6 @@ fail_dax_change_remount:
 			    (sbi->s_mount_opt2 & EXT4_MOUNT2_DAX_NEVER) ||
 			    !(sbi->s_mount_opt2 & EXT4_MOUNT2_DAX_INODE))) {
 			goto fail_dax_change_remount;
-		}
-
-		if (ctx_test_mount_opt2(ctx, EXT4_MOUNT2_BUFFERED_IOMAP) &&
-		    !test_opt2(sb, BUFFERED_IOMAP)) {
-			ext4_msg(NULL, KERN_ERR, "can't enable buffered iomap while remounting");
-			return -EINVAL;
 		}
 	}
 
