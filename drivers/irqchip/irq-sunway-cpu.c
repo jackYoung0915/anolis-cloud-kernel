jeevandeepj994/anolis_perf

commit d6ed426f5249a47eb2eafbbf5745546d9be9855d

anolis: sw64: add NMI support
ANBZ: #4688

Support the Non-Maskable Interrupt(NMI).

Any type of interrupt can be configured to be non-maskable. NMI has its
separate stack, also has a separate SAVE_ALL_NMI and RESTORE_ALL_NMI.

Signed-off-by: Gao Chen <gaochen@wxiat.com>
Signed-off-by: Gu Zitao <guzitao@wxiat.com>
Reviewed-by: Guixin Liu <kanie@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/3541

drivers/irqchip/irq-sunway-cpu.c
@@ -184,8 +184,10 @@ asmlinkage void do_entInt(unsigned long type, unsigned long vector,
 	/* restart idle routine if it is interrupted */
 	if (regs->pc > (u64)__idle_start && regs->pc < (u64)__idle_end)
 		regs->pc = (u64)__idle_start;
-
-	irq_enter();
+	if (regs->cause != -2)
+		irq_enter();
+	else
+		nmi_enter();
 	old_regs = set_irq_regs(regs);
 
 #ifdef CONFIG_SUBARCH_C4
@@ -269,7 +271,10 @@ asmlinkage void do_entInt(unsigned long type, unsigned long vector,
 
 out:
 	set_irq_regs(old_regs);
-	irq_exit();
+	if (regs->cause != -2)
+		irq_exit();
+	else
+		nmi_exit();
 }
 EXPORT_SYMBOL(do_entInt);
 

arch/sw_64/include/asm/core.h
@@ -78,6 +78,7 @@ static inline bool core_is_ht(void)
 extern void entArith(void);
 extern void entIF(void);
 extern void entInt(void);
+extern void entNMI(void);
 extern void entMM(void);
 extern void entSys(void);
 extern void entUna(void);

arch/sw_64/include/asm/csr.h
@@ -55,6 +55,9 @@
 #define CSR_IDA_MATCH		0xc5
 #define CSR_IDA_MASK		0xc6
 #define CSR_BASE_KREGS		0xe0
+#define CSR_NMI_STACK		0xe5
+#define CSR_NMI_SCRATCH		0xe6
+#define CSR_NMI_MASK		0xe7
 #define CSR_PS			0xe8
 #define CSR_PC			0xe9
 #define CSR_EARG0		0xea
@@ -83,7 +86,8 @@
 static inline unsigned long sw64_read_csr(unsigned long x)
 {
 	unsigned long __val;
-	__asm__ __volatile__("csrr %0,%1" : "=r"(__val) : "i"(x));
+
+	__asm__ __volatile__("csrr %0,%1; csrr %0,%1" : "=r"(__val) : "i"(x));
 	return __val;
 }
 

arch/sw_64/include/asm/hmcall.h
@@ -44,6 +44,9 @@
 #define HMC_sendii		0x3E
 #define HMC_rti			0x3F
 
+/* 0x40  - 0x7F : Hypervisor Level HMC routine */
+#define HMC_rti_nmi             0x40
+#define HMC_setup_nmi           0x41
 
 /* 0x80  - 0xBF : User Level HMC routine */
 #include <uapi/asm/hmcall.h>
@@ -185,6 +188,7 @@ __CALL_HMC_RW1(rdio64, unsigned long, unsigned long);
 __CALL_HMC_RW1(rdio32, unsigned int, unsigned long);
 __CALL_HMC_W2(wrent, void*, unsigned long);
 __CALL_HMC_W2(tbisasid, unsigned long, unsigned long);
+__CALL_HMC_W2(setup_nmi, unsigned long, unsigned long);
 __CALL_HMC_W1(wrkgp, unsigned long);
 __CALL_HMC_RW2(wrperfmon, unsigned long, unsigned long, unsigned long);
 __CALL_HMC_RW3(sendii, unsigned long, unsigned long, unsigned long, unsigned long);

arch/sw_64/kernel/entry_c4.S
@@ -14,6 +14,123 @@
 	.text
 	.set noat
 
+	.macro SAVE_ALL_NMI
+	csrw	$sp, CSR_NMI_SCRATCH
+	csrr	$sp, CSR_NMI_STACK
+
+	ldi	$sp, -PT_REGS_SIZE($sp)
+	stl	$0, PT_REGS_R0($sp)
+	stl	$1, PT_REGS_R1($sp)
+	stl	$2, PT_REGS_R2($sp)
+	stl	$3, PT_REGS_R3($sp)
+	stl	$4, PT_REGS_R4($sp)
+	stl	$5, PT_REGS_R5($sp)
+	stl	$6, PT_REGS_R6($sp)
+	stl	$7, PT_REGS_R7($sp)
+	stl	$8, PT_REGS_R8($sp)
+	stl	$9, PT_REGS_R9($sp)
+	stl	$10, PT_REGS_R10($sp)
+	stl	$11, PT_REGS_R11($sp)
+	stl	$12, PT_REGS_R12($sp)
+	stl	$13, PT_REGS_R13($sp)
+	stl	$14, PT_REGS_R14($sp)
+	stl	$15, PT_REGS_R15($sp)
+	stl	$16, PT_REGS_R16($sp)
+	stl	$17, PT_REGS_R17($sp)
+	stl	$18, PT_REGS_R18($sp)
+	stl	$19, PT_REGS_R19($sp)
+	stl	$20, PT_REGS_R20($sp)
+	stl	$21, PT_REGS_R21($sp)
+	stl	$22, PT_REGS_R22($sp)
+	stl	$23, PT_REGS_R23($sp)
+	stl	$24, PT_REGS_R24($sp)
+	stl	$25, PT_REGS_R25($sp)
+	stl	$26, PT_REGS_R26($sp)
+	stl	$27, PT_REGS_R27($sp)
+	stl	$28, PT_REGS_R28($sp)
+	stl	$29, PT_REGS_GP($sp)
+
+	/* Due to a flaw in the CSR design, in some cases the
+	 * read CSR instruction is sent before the write instruction,
+	 * so it needs to be read twice to ensure that the correct
+	 * CSR value is read. Don`t delete it!
+	 */
+	csrr	$1, CSR_NMI_SCRATCH
+	csrr	$1, CSR_NMI_SCRATCH
+	csrr	$2, CSR_PS
+	csrr	$3, CSR_PC
+	csrr    $4, CSR_CAUSE
+	csrr	$16, CSR_EARG0
+	csrr	$17, CSR_EARG1
+	csrr	$18, CSR_EARG2
+	stl	$16, PT_REGS_EARG0($sp)
+	stl	$17, PT_REGS_EARG1($sp)
+	stl	$18, PT_REGS_EARG2($sp)
+
+	stl     $1, PT_REGS_SP($sp)
+	stl	$2, PT_REGS_PS($sp)
+	stl	$3, PT_REGS_PC($sp)
+	stl     $4, PT_REGS_CAUSE($sp)
+	ldi	$1, NO_SYSCALL
+	stl	$1, PT_REGS_ORIG_R0($sp)
+	csrr	$8, CSR_KTP
+#ifdef CONFIG_FRAME_POINTER
+	ldi	$sp, -STACKFRAME_SIZE($sp)
+	stl	$3, STACKFRAME_PC($sp)
+	stl	$15, STACKFRAME_FP($sp)
+	mov	$sp, $15
+#endif
+	br      $27, 1f
+1:      ldgp    $29, 0($27)
+	call	$26, save_nmi_ctx
+	ldl	$16, (PT_REGS_EARG0 + STACKFRAME_SIZE)($sp)
+	ldl	$17, (PT_REGS_EARG1 + STACKFRAME_SIZE)($sp)
+	ldl	$18, (PT_REGS_EARG2 + STACKFRAME_SIZE)($sp)
+	.endm
+
+	.macro RESTORE_ALL_NMI
+	call	$26, restore_nmi_ctx
+
+	ldi	$sp, STACKFRAME_SIZE($sp)
+
+	ldl	$1, PT_REGS_PS($sp)
+	ldl	$2, PT_REGS_PC($sp)
+	csrw	$1, CSR_PS
+	csrw	$2, CSR_PC
+
+	ldl	$0, PT_REGS_R0($sp)
+	ldl	$1, PT_REGS_R1($sp)
+	ldl	$2, PT_REGS_R2($sp)
+	ldl	$3, PT_REGS_R3($sp)
+	ldl	$4, PT_REGS_R4($sp)
+	ldl	$5, PT_REGS_R5($sp)
+	ldl	$6, PT_REGS_R6($sp)
+	ldl	$7, PT_REGS_R7($sp)
+	ldl	$8, PT_REGS_R8($sp)
+	ldl	$9, PT_REGS_R9($sp)
+	ldl	$10, PT_REGS_R10($sp)
+	ldl	$11, PT_REGS_R11($sp)
+	ldl	$12, PT_REGS_R12($sp)
+	ldl	$13, PT_REGS_R13($sp)
+	ldl	$14, PT_REGS_R14($sp)
+	ldl	$15, PT_REGS_R15($sp)
+	ldl	$16, PT_REGS_R16($sp)
+	ldl	$17, PT_REGS_R17($sp)
+	ldl	$18, PT_REGS_R18($sp)
+	ldl	$19, PT_REGS_R19($sp)
+	ldl	$20, PT_REGS_R20($sp)
+	ldl	$21, PT_REGS_R21($sp)
+	ldl	$22, PT_REGS_R22($sp)
+	ldl	$23, PT_REGS_R23($sp)
+	ldl	$24, PT_REGS_R24($sp)
+	ldl	$25, PT_REGS_R25($sp)
+	ldl	$26, PT_REGS_R26($sp)
+	ldl	$27, PT_REGS_R27($sp)
+	ldl	$28, PT_REGS_R28($sp)
+	ldl     $29, PT_REGS_GP($sp)
+	ldl	$sp, PT_REGS_SP($sp)
+	.endm
+
 	.macro SAVE_ALL
 	csrw	$sp, CSR_SP
 	csrw	$1, CSR_SCRATCH
@@ -147,6 +264,18 @@
  */
 
 	.align 4
+	.globl entNMI
+	.ent entNMI
+entNMI:
+	SAVE_ALL_NMI
+	br      $27, 1f
+1:      ldgp    $29, 0($27)
+	ldi	$19, STACKFRAME_SIZE($sp)
+	call	$26, do_entInt
+	br	ret_from_nmi
+	.end entNMI
+
+	.align 4
 	.globl entInt
 	.ent entInt
 entInt:
@@ -259,6 +388,16 @@ entSys:
 	call	$26, do_entSys
 	br	ret_from_sys_call
 	.end entSys
+
+	.align 4
+	.globl ret_from_nmi
+	.ent ret_from_nmi
+ret_from_nmi:
+	br	$27, 1f
+1:	ldgp	$29, 0($27)
+	RESTORE_ALL_NMI
+	sys_call HMC_rti_nmi
+	.end ret_from_nmi
 
 	.align 4
 	.globl ret_from_sys_call

arch/sw_64/kernel/irq.c
@@ -163,7 +163,17 @@ void __init init_IRQ(void)
 	 * Just in case the platform init_irq() causes interrupts/mchecks
 	 * (as is the case with RAWHIDE, at least).
 	 */
+	struct page __maybe_unused *nmi_stack_page = alloc_pages_node(
+		cpu_to_node(smp_processor_id()),
+		THREADINFO_GFP, THREAD_SIZE_ORDER);
+	unsigned long nmi_stack __maybe_unused = nmi_stack_page ?
+		(unsigned long)page_address(nmi_stack_page) : 0;
+
 	wrent(entInt, 0);
+	if (IS_ENABLED(CONFIG_SUBARCH_C4) && is_in_host()) {
+		sw64_write_csr_imb(nmi_stack + THREAD_SIZE, CSR_NMI_STACK);
+		wrent(entNMI, 6);
+	}
 
 	sw64_init_irq();
 	irqchip_init();

arch/sw_64/kernel/smp.c
@@ -111,6 +111,9 @@ static void downshift_freq(void) { }
 void smp_callin(void)
 {
 	int cpuid;
+	struct page  __maybe_unused *nmi_stack_page;
+	unsigned long __maybe_unused nmi_stack;
+
 	save_ktp();
 	upshift_freq();
 	cpuid = smp_processor_id();
@@ -137,6 +140,17 @@ void smp_callin(void)
 	current->active_mm = &init_mm;
 	/* update csr:ptbr */
 	update_ptbr_sys(virt_to_phys(init_mm.pgd));
+
+	if (IS_ENABLED(CONFIG_SUBARCH_C4) && is_in_host()) {
+		nmi_stack_page = alloc_pages_node(
+				cpu_to_node(smp_processor_id()),
+				THREADINFO_GFP,
+				THREAD_SIZE_ORDER);
+		nmi_stack = nmi_stack_page ?
+			(unsigned long)page_address(nmi_stack_page) : 0;
+		sw64_write_csr_imb(nmi_stack + THREAD_SIZE, CSR_NMI_STACK);
+		wrent(entNMI, 6);
+	}
 
 	/* inform the notifiers about the new cpu */
 	notify_cpu_starting(cpuid);

arch/sw_64/kernel/traps.c
@@ -2006,6 +2006,25 @@ syscall_out:
 		syscall_trace_leave();
 }
 
+struct nmi_ctx {
+	unsigned long csr_sp;
+	unsigned long csr_scratch;
+};
+
+DEFINE_PER_CPU(struct nmi_ctx, nmi_context);
+
+void save_nmi_ctx(void)
+{
+	this_cpu_write(nmi_context.csr_sp, sw64_read_csr(CSR_SP));
+	this_cpu_write(nmi_context.csr_scratch, sw64_read_csr(CSR_SCRATCH));
+}
+
+void restore_nmi_ctx(void)
+{
+	sw64_write_csr_imb(this_cpu_read(nmi_context.csr_sp), CSR_SP);
+	sw64_write_csr_imb(this_cpu_read(nmi_context.csr_scratch), CSR_SCRATCH);
+}
+
 void
 trap_init(void)
 {

jackYoung0915/anolis-cloud-kernel

commit 3f494aa7ba87669dda86529e40763db205724530

anolis: sw64: fix nmi_[enter/exit] error in CORE3B
ANBZ: #4688

In commit d6ed426f5249 ("anolis: sw64: add NMI support"), since CORE3B does not
have regs->cause, if some random case happens to take the value of
-2, then it enters the wrong judgment.

Fixes: d6ed426f5249 ("anolis: sw64: add NMI support")

Signed-off-by: Wu Liliu <wuliliu@wxiat.com>
Reviewed-by: He Sheng <hesheng@wxiat.com>
Signed-off-by: Gu Zitao <guzitao@wxiat.com>

drivers/irqchip/irq-sunway-cpu.c
@@ -87,10 +87,10 @@ asmlinkage void noinstr do_entInt(struct pt_regs *regs)
 	/* restart idle routine if it is interrupted */
 	if (regs->pc > (u64)__idle_start && regs->pc < (u64)__idle_end)
 		regs->pc = (u64)__idle_start;
-	if (regs->cause != -2)
-		irq_enter();
-	else
+	if (unlikely(regs->cause == -2 && IS_ENABLED(CONFIG_SUBARCH_C4)))
 		nmi_enter();
+	else
+		irq_enter();
 	old_regs = set_irq_regs(regs);
 
 #ifdef CONFIG_PM
@@ -176,10 +176,10 @@ asmlinkage void noinstr do_entInt(struct pt_regs *regs)
 
 out:
 	set_irq_regs(old_regs);
-	if (regs->cause != -2)
-		irq_exit();
-	else
+	if (unlikely(regs->cause == -2 && IS_ENABLED(CONFIG_SUBARCH_C4)))
 		nmi_exit();
+	else
+		irq_exit();
 }
 EXPORT_SYMBOL(do_entInt);
 