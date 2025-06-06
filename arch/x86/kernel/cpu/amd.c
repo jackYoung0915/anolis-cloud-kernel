394ed7675b8b434d64824813c4cea269b963b68a
Rik van Riel <riel@surriel.com>
2025-06-06 13:10:34 -0400

    x86/mm: Fix early boot use of INVPLGB
    ANBZ: #28080
    
    commit cb6075bc62dc6a9cd7ab3572758685fdf78e3e20 upstream
    
    The INVLPGB instruction has limits on how many pages it can invalidate
    at once. That limit is enumerated in CPUID, read by the kernel, and
    stored in 'invpgb_count_max'. Ranged invalidation, like
    invlpgb_kernel_range_flush() break up their invalidations so
    that they do not exceed the limit.
    
    However, early boot code currently attempts to do ranged
    invalidation before populating 'invlpgb_count_max'. There is a
    for loop which is basically:
    
    	for (...; addr < end; addr += invlpgb_count_max*PAGE_SIZE)
    
    If invlpgb_kernel_range_flush is called before the kernel has read
    the value of invlpgb_count_max from the hardware, the normally
    bounded loop can become an infinite loop if invlpgb_count_max is
    initialized to zero.
    
    Fix that issue by initializing invlpgb_count_max to 1.
    
    This way INVPLGB at early boot time will be a little bit slower
    than normal (with initialized invplgb_count_max), and not an
    instant hang at bootup time.
    
    Fixes: b7aa05cbdc52 ("x86/mm: Add INVLPGB support code")
    Signed-off-by: Rik van Riel <riel@surriel.com>
    Signed-off-by: Dave Hansen <dave.hansen@linux.intel.com>
    Link: https://lore.kernel.org/all/20250606171112.4013261-3-riel%40surriel.com
    Signed-off-by: Arukonda Rahul <arukonda.rahul@amd.com>
    Signed-off-by: mohanasv <mohanasv@amd.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Reviewed-by: Guanghui Feng <guanghuifeng@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6204

--- arch/x86/kernel/cpu/amd.c
+++ arch/x86/kernel/cpu/amd.c
@@ -33,7 +33,7 @@
  * Node Identifiers[10:8]
  */
 static u32 nodes_per_socket = 1;
-u16 invlpgb_count_max __ro_after_init;
+u16 invlpgb_count_max __ro_after_init = 1;
 
 
 static inline int rdmsrl_amd_safe(unsigned msr, unsigned long long *p)
