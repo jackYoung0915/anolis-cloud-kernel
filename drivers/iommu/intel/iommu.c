a1be71f86189fb2c04a9f23fbb74ace18c5696d2
gaorui <gao.rui@zte.com.cn>
2025-06-11 20:24:17 +0800

    iommu/vt-d: Setup scalable mode context entry in probe path
    ANBZ: #21698
    
    commit 301f1a80487fd2f51012533792583d4425e8b8c0 upstream.
    
    In contrast to legacy mode, the DMA translation table is configured in
    the PASID table entry instead of the context entry for scalable mode.
    For this reason, it is more appropriate to set up the scalable mode
    context entry in the device_probe callback and direct it to the
    appropriate PASID table.
    
    The iommu domain attach/detach operations only affect the PASID table
    entry. Therefore, there is no need to modify the context entry when
    configuring the translation type and page table.
    
    The only exception is the kdump case, where context entry setup is
    postponed until the device driver invokes the first DMA interface.
    
    Signed-off-by: Lu Baolu <baolu.lu@linux.intel.com>
    Reviewed-by: Kevin Tian <kevin.tian@intel.com>
    Link: https://lore.kernel.org/r/20240305013305.204605-4-baolu.lu@linux.intel.com
    Signed-off-by: Joerg Roedel <jroedel@suse.de>
    Signed-off-by: gaorui <gao.rui@zte.com.cn>

--- drivers/iommu/intel/iommu.c
+++ drivers/iommu/intel/iommu.c
@@ -4003,6 +4003,10 @@ static int prepare_domain_attach_device(struct iommu_domain *domain,
 		dmar_domain->agaw--;
 	}
 
+	if (sm_supported(iommu) && !dev_is_real_dma_subdevice(dev) &&
+	    context_copied(iommu, info->bus, info->devfn))
+		return intel_pasid_setup_sm_context(dev);
+
 	return 0;
 }
 
@@ -4310,6 +4314,14 @@ static struct iommu_device *intel_iommu_probe_device(struct device *dev)
 			dev_err(dev, "PASID table allocation failed\n");
 			kfree(info);
 			return ERR_PTR(ret);
+		}
+
+		if (!context_copied(iommu, info->bus, info->devfn)) {
+			ret = intel_pasid_setup_sm_context(dev);
+			if (ret) {
+				intel_pasid_free_table(dev);
+				return ERR_PTR(ret);
+			}
 		}
 	}
 
