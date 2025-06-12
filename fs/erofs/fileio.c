7fc3abd8b21f1ef01108269b858d36abd6b95372
Tatsuyuki Ishi <ishitatsuyuki@google.com>
2025-06-12 19:18:25 +0900

    erofs: impersonate the opener's credentials when accessing backing file
    ANBZ: #11854
    
    commit 905eeb2b7c33adda23a966aeb811ab4cb9e62031 upstream.
    
    Previously, file operations on a file-backed mount used the current
    process' credentials to access the backing FD. Attempting to do so on
    Android lead to SELinux denials, as ACL rules on the backing file (e.g.
    /system/apex/foo.apex) is restricted to a small set of process.
    Arguably, this error is redundant and leaking implementation details, as
    access to files on a mount is already ACL'ed by path.
    
    Instead, override to use the opener's cred when accessing the backing
    file. This makes the behavior similar to a loop-backed mount, which
    uses kworker cred when accessing the backing file and does not cause
    SELinux denials.
    
    Signed-off-by: Tatsuyuki Ishi <ishitatsuyuki@google.com>
    Reviewed-by: Gao Xiang <hsiangkao@linux.alibaba.com>
    Reviewed-by: Hongbo Li <lihongbo22@huawei.com>
    Link: https://lore.kernel.org/r/20250612-b4-erofs-impersonate-v1-1-8ea7d6f65171@google.com
    Signed-off-by: Gao Xiang <hsiangkao@linux.alibaba.com>
    Acked-by: Jingbo Xu <jefflexu@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5535

--- fs/erofs/fileio.c
+++ fs/erofs/fileio.c
@@ -46,6 +46,7 @@ static void erofs_fileio_ki_complete(struct kiocb *iocb, long ret, long res2)
 
 static void erofs_fileio_rq_submit(struct erofs_fileio_rq *rq)
 {
+	const struct cred *old_cred;
 	struct iov_iter iter;
 	int ret;
 
@@ -59,7 +60,9 @@ static void erofs_fileio_rq_submit(struct erofs_fileio_rq *rq)
 		rq->iocb.ki_flags = IOCB_DIRECT;
 	iov_iter_bvec(&iter, READ, rq->bvecs, rq->bio.bi_vcnt,
 		      rq->bio.bi_iter.bi_size);
+	old_cred = override_creds(rq->iocb.ki_filp->f_cred);
 	ret = vfs_iocb_iter_read(rq->iocb.ki_filp, &rq->iocb, &iter);
+	revert_creds(old_cred);
 	if (ret != -EIOCBQUEUED)
 		erofs_fileio_ki_complete(&rq->iocb, ret, 0);
 }
