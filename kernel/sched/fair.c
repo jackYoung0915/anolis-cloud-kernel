fe926bbb3c7ec3e6defafd75efed9805c54f3004
Cruz Zhao <CruzZhao@linux.alibaba.com>
2025-06-13 21:09:37 +0800

    anolis: sched: adjust the condition of gb_load_balance
    ANBZ: #8765
    
    When load balance, we should migrate task groups when we cannot balance
    the sched groups, instead of migrating task groups at the entry of load
    balance. By the way, we should call task_tick_gb() when tick, to lower
    the task groups which belongs to a higher level group balancer sched
    domain.
    
    Signed-off-by: Cruz Zhao <CruzZhao@linux.alibaba.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -12522,6 +12522,24 @@ static int should_we_balance(struct lb_env *env)
 	return group_balance_cpu(sg) == env->dst_cpu;
 }
 
+#ifdef CONFIG_GROUP_BALANCER
+static inline bool gb_need_redo(struct lb_env *env)
+{
+	if (group_balancer_enabled())
+		return env->gb_need_redo;
+	return false;
+}
+
+static inline void unset_gb_need_redo(struct lb_env *env)
+{
+	if (group_balancer_enabled())
+		env->gb_need_redo = false;
+}
+#else
+static inline bool gb_need_redo(struct lb_env *env) { return false; }
+static inline void unset_gb_need_redo(struct lb_env *env) { }
+#endif
+
 /*
  * Check this_cpu to ensure it is balanced within domain. Attempt to move
  * tasks if there is an imbalance.
@@ -12554,6 +12572,9 @@ static int load_balance(int this_cpu, struct rq *this_rq,
 #ifdef CONFIG_GROUP_IDENTITY
 		.migration_type	= type,
 		.id_need_redo	= true,
+#endif
+#ifdef CONFIG_GROUP_BALANCER
+		.gb_need_redo	= true,
 #endif
 	};
 
@@ -12588,7 +12609,6 @@ redo:
 
 	ld_moved = 0;
 	if (busiest->nr_running > 1 || expellee_only(busiest)) {
-		gb_load_balance(&env);
 		/*
 		 * Attempt to move tasks. If find_busiest_group has found
 		 * an imbalance but busiest->nr_running <= 1, the group is
@@ -12599,6 +12619,8 @@ redo:
 		env.loop_max  = min(sysctl_sched_nr_migrate, busiest->nr_running);
 
 more_balance:
+		if (!gb_need_redo(&env))
+			gb_load_balance(&env);
 		rq_lock_irqsave(busiest, &rf);
 		update_rq_clock(busiest);
 
@@ -12651,15 +12673,17 @@ more_balance:
 		 */
 		if ((env.flags & LBF_DST_PINNED) && env.imbalance > 0) {
 
-			/* Prevent to re-select dst_cpu via env's CPUs */
-			__cpumask_clear_cpu(env.dst_cpu, env.cpus);
+			if (!gb_need_redo(&env)) {
+				/* Prevent to re-select dst_cpu via env's CPUs */
+				__cpumask_clear_cpu(env.dst_cpu, env.cpus);
 
-			env.dst_rq	 = cpu_rq(env.new_dst_cpu);
-			env.dst_cpu	 = env.new_dst_cpu;
-			env.flags	&= ~LBF_DST_PINNED;
-			env.loop	 = 0;
-			env.loop_break	 = sched_nr_migrate_break;
-
+				env.dst_rq	 = cpu_rq(env.new_dst_cpu);
+				env.dst_cpu	 = env.new_dst_cpu;
+				env.flags	&= ~LBF_DST_PINNED;
+				env.loop	 = 0;
+				env.loop_break	 = sched_nr_migrate_break;
+			}
+			unset_gb_need_redo(&env);
 			/*
 			 * Go back to "more_balance" rather than "redo" since we
 			 * need to continue with same src_cpu.
@@ -12679,6 +12703,11 @@ more_balance:
 
 		/* All tasks on this runqueue were pinned by CPU affinity */
 		if (unlikely(env.flags & LBF_ALL_PINNED)) {
+			if (env.imbalance > 0 && gb_need_redo(&env)) {
+				unset_gb_need_redo(&env);
+				cpumask_and(cpus, sched_domain_span(sd), cpu_active_mask);
+				goto redo;
+			}
 			__cpumask_clear_cpu(cpu_of(busiest), cpus);
 			/*
 			 * Attempting to continue load balancing at the current
@@ -12703,6 +12732,10 @@ more_balance:
 		goto redo;
 	}
 #endif
+	if (env.imbalance > 0 && gb_need_redo(&env)) {
+		unset_gb_need_redo(&env);
+		goto redo;
+	}
 
 	if (!ld_moved) {
 		schedstat_inc(sd->lb_failed[idle]);
@@ -13934,6 +13967,7 @@ static void task_tick_fair(struct rq *rq, struct task_struct *curr, int queued)
 
 	task_tick_core(rq, curr);
 	task_tick_gi(rq);
+	task_tick_gb(curr);
 }
 
 /*
