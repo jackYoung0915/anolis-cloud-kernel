580f4d7786043c2cbd1e09d4995c0c14bee67618
hanliyang <hanliyang@hygon.cn>
2025-06-16 11:35:21 +0800

    anolis: virt: csv-guest: Introduce CSV RTMR uapi interface
    ANBZ: #22411
    
    Introduce communication interfaces between user space and kernel space.
    
    Users should issue an CSV_CMD_RTMR ioctl request to inform the guest
    kernel to handle RTMR commands. Users populate the struct csv_rtmr_req to
    indicate the RTMR subcommand and its buffer. The RTMR subcommand for user
    space is defined in the enum csv_guest_user_rtmr_subcmd. Users provide a
    buffer for struct csv_guest_user_rtmr_status to request the RTMR_STATUS
    subcommand, a buffer for struct csv_guest_user_rtmr_start for RTMR_START,
    a buffer for struct csv_guest_user_rtmr_read for RTMR_READ, and a buffer
    for struct csv_guest_user_rtmr_extend for RTMR_EXTEND.
    
    Signed-off-by: hanliyang <hanliyang@hygon.cn>
    Reviewed-by: Guixin Liu <kanie@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5485

--- drivers/virt/coco/csv-guest/csv-guest.h
+++ drivers/virt/coco/csv-guest/csv-guest.h
@@ -11,6 +11,7 @@
 
 #include <linux/ioctl.h>
 #include <linux/types.h>
+#include <uapi/linux/psp-hygon.h>
 
 /* Length of the user input datas used in VMMCALL */
 #define CSV_REPORT_USER_DATA_LEN	64
@@ -38,5 +39,86 @@ struct csv_report_req {
  * standard errno on other general error cases.
  */
 #define CSV_CMD_GET_REPORT	_IOWR('D', 1, struct csv_report_req)
+
+enum csv_guest_user_rtmr_subcmd {
+	CSV_GUEST_USER_RTMR_STATUS	= 0x1,
+	CSV_GUEST_USER_RTMR_START	= 0x2,
+	CSV_GUEST_USER_RTMR_READ	= 0x3,
+	CSV_GUEST_USER_RTMR_EXTEND	= 0x4,
+};
+
+/**
+ * struct csv_guest_user_rtmr_status - RTMR_STATUS subcommand parameters
+ *
+ * @version: the RTMR version used in the guest. When in state
+ *	     RTMR_STATE_INIT, it will be fixed.
+ * @state: the state of the guest's RTMR.
+ */
+struct csv_guest_user_rtmr_status {
+	__u16 version;	/* Out */
+	__u8  state;	/* Out */
+} __packed;
+
+/**
+ * struct csv_guest_user_rtmr_start - RTMR_START subcommand parameters
+ *
+ * @version: the RTMR version requested by the guest.
+ */
+struct csv_guest_user_rtmr_start {
+	__u16 version;	/* In,Out */
+} __packed;
+
+/**
+ * struct csv_guest_user_rtmr_read - RTMR_READ subcommand parameters
+ *
+ * @bitmap: the bitmap specified the RTMR registers to read.
+ * @data: the buffer to store RTMR registers' data returned by the firmware.
+ *	  If read more than one RTMR register, user should provide a larger
+ *	  buffer.
+ */
+struct csv_guest_user_rtmr_read {
+	__u32 bitmap;
+	__u8  data[CSV_RTMR_REG_SIZE];
+} __packed;
+
+/**
+ * struct csv_guest_user_rtmr_extend - RTMR_EXTEND subcommand parameters
+ *
+ * @index: the index for extending the RTMR register.
+ * @rsvd: reserved, just for alignment.
+ * @data_len: the length of the data to be extended.
+ * @data: the data to be extended to the RTMR register.
+ */
+struct csv_guest_user_rtmr_extend {
+	__u8  index;
+	__u8  rsvd;
+	__u16 data_len;
+	__u8  data[CSV_RTMR_EXTEND_LEN];
+} __packed;
+
+/**
+ * struct csv_rtmr_req - RTMR request command
+ *
+ * @buf: the RTMR subcommand buffer.
+ * @len: the length of RTMR subcommand.
+ * @subcmd_id: the identifier of the RTMR subcommand.
+ * @rsvd: reserved, just for alignment.
+ * @fw_error_code: the return code that a RTMR subcommand returned by the
+ *		   firmware.
+ */
+struct csv_rtmr_req {
+	__u64 buf;		/* In */
+	__u64 len;		/* In,Out */
+	__u16 subcmd_id;	/* In */
+	__u16 rsvd;		/* Reserved */
+	__u32 fw_error_code;	/* Out */
+} __packed;
+
+/*
+ * CSV_CMD_RTMR - RTMR request with user data using secure call.
+ *
+ * Returns 0 on success, and standard errono on other failures.
+ */
+#define CSV_CMD_RTMR	_IOWR('D', 2, struct csv_rtmr_req)
 
 #endif /* __VIRT_CSVGUEST_H__ */
