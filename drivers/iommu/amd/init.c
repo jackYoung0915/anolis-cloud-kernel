d1161db711aa8ea4b5b1490569ddb6899c2aa725
Dheeraj Kumar Srivastava <dheerajkumar.srivastava@amd.com>
2025-07-02 15:07:57 +0530

    iommu/amd: Refactor AMD IOMMU debugfs initial setup
    ANBZ: #30840
    
    commit ad48b1dd14fb217e0a0b0af46744a3d1f6f26dea upstream
    
    Rearrange initial setup of AMD IOMMU debugfs to segregate per IOMMU
    setup and setup which is common for all IOMMUs. This ensures that common
    debugfs paths (introduced in subsequent patches) are created only once
    instead of being created for each IOMMU.
    
    With the change, there is no need to use lock as amd_iommu_debugfs_setup()
    will be called only once during AMD IOMMU initialization. So remove lock
    acquisition in amd_iommu_debugfs_setup().
    
    Signed-off-by: Dheeraj Kumar Srivastava <dheerajkumar.srivastava@amd.com>
    Reviewed-by: Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
    Reviewed-by: Vasant Hegde <vasant.hegde@amd.com>
    Link: https://lore.kernel.org/r/20250702093804.849-2-dheerajkumar.srivastava@amd.com
    Signed-off-by: Will Deacon <will@kernel.org>
    Signed-off-by: suryasaimadhu <SaiMadhu.KoyyalaHariVenkata@amd.com>
    Signed-off-by: PvsNarasimha <PVS.NarasimhaRao@amd.com>
    Reviewed-by Guanghui Feng <guanghuifeng@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6496

--- drivers/iommu/amd/init.c
+++ drivers/iommu/amd/init.c
@@ -3359,7 +3359,6 @@ int amd_iommu_enable_faulting(unsigned int cpu)
  */
 static int __init amd_iommu_init(void)
 {
-	struct amd_iommu *iommu;
 	int ret;
 
 	ret = iommu_go_to_state(IOMMU_INITIALIZED);
@@ -3373,8 +3372,8 @@ static int __init amd_iommu_init(void)
 	}
 #endif
 
-	for_each_iommu(iommu)
-		amd_iommu_debugfs_setup(iommu);
+	if (!ret)
+		amd_iommu_debugfs_setup();
 
 	return ret;
 }
