6cd98229bfed2945b5638271f93b0259360669a2
Cruz Zhao <CruzZhao@linux.alibaba.com>
2025-07-09 16:31:16 +0800

    anolis: sched: control the interval of gb_load_balance()
    ANBZ: #8765
    
    To reduce turbulence, the interval of gb_load_balance() should be
    controlled, and we set it to twice the lower interval. And to prevent
    unnecessary migration, we only migrate task groups when the imbalance
    pct exceeds the threshold 117.
    
    Signed-off-by: Cruz Zhao <CruzZhao@linux.alibaba.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -622,6 +622,7 @@ struct task_group {
 	bool			group_balancer;
 	bool			leap_level;
 	unsigned long		leap_level_timestamp;
+	unsigned long		adjust_level_timestamp;
 	raw_spinlock_t		gb_lock;
 #endif
 	long			priority;
