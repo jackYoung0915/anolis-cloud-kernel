0f93069cede28fc7c887cba852eb0d65f8d9a7cd
CruzZhao <CruzZhao@linux.alibaba.com>
2025-07-11 11:07:51 +0800

    anolis: sched: fix the bug that soft_cpus doesn't work sometimes
    ANBZ: #8765
    
    When we changed cpus_ptr to task_allowed_cpu, we missed a few
    things, which will cause that soft_cpus doesn't work sometimes.
    And we fix this problem by changing cpus_ptr into task_allowed_cpu.
    
    Fixes: ef3e9915a32e ("anolis: sched: introduce task_allowed_cpu when selecting running cpus")
    
    Signed-off-by: CruzZhao <CruzZhao@linux.alibaba.com>
    Reviewed-by: Tianchen Ding <dtcccc@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5509

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1524,6 +1524,7 @@ DECLARE_PER_CPU_SHARED_ALIGNED(struct rq, runqueues);
 #define cpu_curr(cpu)		(cpu_rq(cpu)->curr)
 #define raw_rq()		raw_cpu_ptr(&runqueues)
 
+static inline const struct cpumask *task_allowed_cpu(struct task_struct *p);
 struct sched_group;
 #ifdef CONFIG_SCHED_CORE
 enum sched_cookie_flags {
@@ -1660,7 +1661,7 @@ static inline bool sched_group_cookie_match(struct rq *rq,
 	if (!sched_core_enabled(rq))
 		return true;
 
-	for_each_cpu_and(cpu, sched_group_span(group), p->cpus_ptr) {
+	for_each_cpu_and(cpu, sched_group_span(group), task_allowed_cpu(p)) {
 		if (sched_core_cookie_match(cpu_rq(cpu), p))
 			return true;
 	}
@@ -2840,7 +2841,7 @@ extern void set_cpus_allowed_common(struct task_struct *p, struct affinity_conte
 static inline bool task_allowed_on_cpu(struct task_struct *p, int cpu)
 {
 	/* When not in the task's cpumask, no point in looking further. */
-	if (!cpumask_test_cpu(cpu, p->cpus_ptr))
+	if (!cpumask_test_cpu(cpu, task_allowed_cpu(p)))
 		return false;
 
 	/* Can @cpu run a user thread? */
