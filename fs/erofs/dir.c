8d36f083f558d58d463c6be7b8ff8cc055126f5e
Chao Yu <chao@kernel.org>
2025-07-21 10:13:52 +0800

    erofs: support to readahead dirent blocks in erofs_readdir()
    ANBZ: #21513
    
    commit df0ce6cefa453d2236381645e529a27ef2f0a573 upstream.
    
    This patch supports to readahead more blocks in erofs_readdir(), it can
    enhance readdir performance in large direcotry.
    
    readdir test in a large directory which contains 12000 sub-files.
    
    		files_per_second
    Before:		926385.54
    After:		2380435.562
    
    Meanwhile, let's introduces a new sysfs entry to control readahead
    bytes to provide more flexible policy for readahead of readdir().
    - location: /sys/fs/erofs/<disk>/dir_ra_bytes
    - default value: 16384
    - disable readahead: set the value to 0
    
    Signed-off-by: Chao Yu <chao@kernel.org>
    Reviewed-by: Gao Xiang <hsiangkao@linux.alibaba.com>
    Link: https://lore.kernel.org/r/20250721021352.2495371-1-chao@kernel.org
    [ Gao Xiang: minor styling adjustment. ]
    [ Gao Xiang: Use PAGE_ALIGN() instead of DIV_ROUND_UP_POW2(). ]
    Signed-off-by: Gao Xiang <hsiangkao@linux.alibaba.com>
    Acked-by: Joseph Qi <joseph.qi@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5567

--- fs/erofs/dir.c
+++ fs/erofs/dir.c
@@ -47,8 +47,12 @@ static int erofs_readdir(struct file *f, struct dir_context *ctx)
 	struct inode *dir = file_inode(f);
 	struct erofs_buf buf = __EROFS_BUF_INITIALIZER;
 	struct super_block *sb = dir->i_sb;
+	struct file_ra_state *ra = &f->f_ra;
 	unsigned long bsz = sb->s_blocksize;
 	unsigned int ofs = erofs_blkoff(sb, ctx->pos);
+	pgoff_t ra_pages =
+		PAGE_ALIGN(EROFS_I_SB(dir)->dir_ra_bytes) >> PAGE_SHIFT;
+	pgoff_t nr_pages = PAGE_ALIGN(dir->i_size) >> PAGE_SHIFT;
 	int err = 0;
 	bool initial = true;
 
@@ -61,6 +65,16 @@ static int erofs_readdir(struct file *f, struct dir_context *ctx)
 		if (fatal_signal_pending(current)) {
 			err = -ERESTARTSYS;
 			break;
+		}
+
+		/* readahead blocks to enhance performance for large directories */
+		if (ra_pages) {
+			pgoff_t idx = PAGE_ALIGN(ctx->pos) >> PAGE_SHIFT;
+			pgoff_t pages = min(nr_pages - idx, ra_pages);
+
+			if (pages > 1 && !ra_has_index(ra, idx))
+				page_cache_sync_readahead(dir->i_mapping, ra,
+							  f, idx, pages);
 		}
 
 		de = erofs_bread(&buf, dbstart, true);
