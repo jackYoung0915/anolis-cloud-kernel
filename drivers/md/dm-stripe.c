82c863b3a6f74b9a1b13b6ef2e192412bca3ff8b
Mikulas Patocka <mpatocka@redhat.com>
2025-08-11 13:17:32 +0200

    dm-stripe: fix a possible integer overflow
    ANBZ: #25832
    
    commit 1071d560afb4c245c2076494226df47db5a35708 upstream.
    
    There's a possible integer overflow in stripe_io_hints if we have too
    large chunk size. Test if the overflow happened, and if it did, don't set
    limits->io_min and limits->io_opt;
    
    Signed-off-by: Mikulas Patocka <mpatocka@redhat.com>
    Reviewed-by: John Garry <john.g.garry@oracle.com>
    Suggested-by: Dongsheng Yang <dongsheng.yang@linux.dev>
    Cc: stable@vger.kernel.org
    
    [Fixes conflicts]
    Fixes: CVE-2025-39940
    Assisted-by: PatchPilot <patchpilot@openanolis.org>
    Signed-off-by: Jingbo Xu <jefflexu@linux.alibaba.com>
    Acked-by: Joseph Qi <joseph.qi@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/7113

--- drivers/md/dm-stripe.c
+++ drivers/md/dm-stripe.c
@@ -15,6 +15,7 @@
 #include <linux/dax.h>
 #include <linux/slab.h>
 #include <linux/log2.h>
+#include <linux/overflow.h>
 
 static struct workqueue_struct *dm_stripe_wq;
 
@@ -456,10 +457,13 @@ static void stripe_io_hints(struct dm_target *ti,
 			    struct queue_limits *limits)
 {
 	struct stripe_c *sc = ti->private;
-	unsigned int chunk_size = sc->chunk_size << SECTOR_SHIFT;
+	unsigned int io_min, io_opt;
 
-	blk_limits_io_min(limits, chunk_size);
-	blk_limits_io_opt(limits, chunk_size * sc->stripes);
+	if (!check_shl_overflow(sc->chunk_size, SECTOR_SHIFT, &io_min) &&
+	    !check_mul_overflow(io_min, sc->stripes, &io_opt)) {
+		blk_limits_io_min(limits, io_min);
+		blk_limits_io_opt(limits, io_opt);
+	}
 }
 
 static struct target_type stripe_target = {
