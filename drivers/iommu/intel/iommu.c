0870d6dcf1ff6c66bf0be12f01f0f937b1e4ae00
Shuai Xue <xueshuai@linux.alibaba.com>
2025-08-13 10:01:21 +0800

    anolis: Revert "iommu/vt-d: Setup scalable mode context entry in probe path"
    ANBZ: #13617
    
    This reverts commit 8475ea6101d14c8962ef9186f4ef6dae10608f07.
    Reviewed-by: Jay Chen <jkchen@linux.alibaba.com>
    Reviewed-by: Guanghui Feng <guanghuifeng@linux.alibaba.com>
    Reviewed-by: Shuai Xue <xueshuai@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5796

--- drivers/iommu/intel/iommu.c
+++ drivers/iommu/intel/iommu.c
@@ -3988,10 +3988,6 @@ static int prepare_domain_attach_device(struct iommu_domain *domain,
 		dmar_domain->agaw--;
 	}
 
-	if (sm_supported(iommu) && !dev_is_real_dma_subdevice(dev) &&
-	    context_copied(iommu, info->bus, info->devfn))
-		return intel_pasid_setup_sm_context(dev);
-
 	return 0;
 }
 
@@ -4299,14 +4295,6 @@ static struct iommu_device *intel_iommu_probe_device(struct device *dev)
 			dev_err(dev, "PASID table allocation failed\n");
 			kfree(info);
 			return ERR_PTR(ret);
-		}
-
-		if (!context_copied(iommu, info->bus, info->devfn)) {
-			ret = intel_pasid_setup_sm_context(dev);
-			if (ret) {
-				intel_pasid_free_table(dev);
-				return ERR_PTR(ret);
-			}
 		}
 	}
 
