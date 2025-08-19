e29e60da880e3fb9e93e834efdc94310082de165
Kefeng Wang <wangkefeng.wang@huawei.com>
2025-08-19 20:10:56 +0800

    mm: memory-failure: use kill_accessing_process() in ghes
    ANCK ID: #IJSBBS
    
    commit a81f1aa5c8ea118127614142b13a67095d947a89 openEuler.
    
    Use kill_accessing_process() instead of force_sig() to pass addr
    to userspace.
    
    Signed-off-by: Kefeng Wang <wangkefeng.wang@huawei.com>
    Signed-off-by: Jinjiang Tu <tujinjiang@huawei.com>
    (cherry picked from commit a81f1aa5c8ea118127614142b13a67095d947a89)
    Conflicts:
    	drivers/acpi/apei/ghes.c
    	include/linux/mm.h
    	mm/memory-failure.c
    [Context conflicts.]
    Signed-off-by: Liu Jian <liujian56@huawei.com>

--- mm/memory-failure.c
+++ mm/memory-failure.c
@@ -863,9 +863,9 @@ static const struct mm_walk_ops hwpoison_walk_ops = {
  * is proper in most cases, but it could be wrong when the application
  * process has multiple entries mapping the error page.
  */
-static int kill_accessing_process(unsigned long pfn, int flags)
+int kill_accessing_process(unsigned long pfn, int flags, bool force_kill)
 {
-	int ret;
+	int ret, ret_kill = -EINVAL;
 	struct task_struct *p = current;
 	struct hwpoison_walk priv = {
 		.pfn = pfn,
@@ -885,7 +885,14 @@ static int kill_accessing_process(unsigned long pfn, int flags)
 	 * SIGBUS is needed.
 	 */
 	if (ret == 1 && priv.tk.addr)
-		kill_proc(&priv.tk, pfn, flags);
+		ret_kill = kill_proc(&priv.tk, pfn, flags);
+
+	if (force_kill && (ret_kill < 0)) {
+		pr_err("%#lx: Sending force SIGBUS to %s:%d due to hardware memory corruption\n",
+				pfn, p->comm, task_pid_nr(p));
+		force_sig(SIGBUS);
+	}
+
 	mmap_read_unlock(p->mm);
 
 	return ret > 0 ? -EHWPOISON : 0;
@@ -2099,7 +2106,7 @@ retry:
 	} else if (res == -EHWPOISON) {
 		if (flags & MF_ACTION_REQUIRED) {
 			folio = page_folio(p);
-			res = kill_accessing_process(folio_pfn(folio), flags);
+			res = kill_accessing_process(folio_pfn(folio), flags, false);
 		}
 		action_result(pfn, MF_MSG_ALREADY_POISONED, MF_FAILED);
 		return res;
@@ -2437,7 +2444,7 @@ try_again:
 	if (TestSetPageHWPoison(p)) {
 		res = -EHWPOISON;
 		if (flags & MF_ACTION_REQUIRED)
-			res = kill_accessing_process(pfn, flags);
+			res = kill_accessing_process(pfn, flags, false);
 		if (flags & MF_COUNT_INCREASED)
 			put_page(p);
 		action_result(pfn, MF_MSG_ALREADY_POISONED, MF_FAILED);
