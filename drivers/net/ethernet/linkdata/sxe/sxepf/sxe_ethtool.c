a3c4c824e3266ae2c6db5ca37da06cb899b5ac30
Xuan Zhuo <xuanzhuo@linux.alibaba.com>
2025-08-19 10:10:48 +0800

    anolis: Revert "net: ethtool: pass a pointer to parameters to get/set_rxfh ethtool ops"
    ANBZ: #23529
    
    This reverts commit ce793fe6969377fc8269ef5ef82956550eb6fa3b.
    This reverts commit 00529157dc6b1c6a502456eab5998b71d2784020.
    
    Revert the new api for get/set_rxfh.
    
    Signed-off-by: Xuan Zhuo <xuanzhuo@linux.alibaba.com>
    Reviewed-by: Guixin Liu <kanie@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5649

--- drivers/net/ethernet/linkdata/sxe/sxepf/sxe_ethtool.c
+++ drivers/net/ethernet/linkdata/sxe/sxepf/sxe_ethtool.c
@@ -1221,23 +1221,25 @@ static void sxe_rss_redir_tbl_get(struct sxe_adapter *adapter, u32 *indir)
 		indir[i] = adapter->rss_indir_tbl[i] & rss_m;
 }
 
-static int sxe_get_rxfh(struct net_device *netdev, struct ethtool_rxfh_param *rxfh)
+static int sxe_get_rxfh(struct net_device *netdev, u32 *indir, u8 *key,
+			u8 *hfunc)
 {
 	struct sxe_adapter *adapter = netdev_priv(netdev);
 
-	rxfh->hfunc = ETH_RSS_HASH_TOP;
+	if (hfunc)
+		*hfunc = ETH_RSS_HASH_TOP;
 
-	if (rxfh->indir)
-		sxe_rss_redir_tbl_get(adapter, rxfh->indir);
+	if (indir)
+		sxe_rss_redir_tbl_get(adapter, indir);
 
-	if (rxfh->key)
-		memcpy(rxfh->key, adapter->rss_key, sxe_get_rxfh_key_size(netdev));
+	if (key)
+		memcpy(key, adapter->rss_key, sxe_get_rxfh_key_size(netdev));
 
 	return 0;
 }
 
-static int sxe_set_rxfh(struct net_device *netdev, struct ethtool_rxfh_param *rxfh,
-			struct netlink_ext_ack *extack)
+static int sxe_set_rxfh(struct net_device *netdev, const u32 *redir,
+			const u8 *key, const u8 hfunc)
 {
 	u16 i, max_queues;
 	struct sxe_adapter *adapter = netdev_priv(netdev);
@@ -1246,33 +1248,33 @@ static int sxe_set_rxfh(struct net_device *netdev, struct ethtool_rxfh_param *rx
 	struct sxe_hw *hw = &adapter->hw;
 
 	LOG_DEBUG_BDF("rss=%u, tbl_entries=%u\n", rss, tbl_entries);
-	if (rxfh->hfunc != ETH_RSS_HASH_NO_CHANGE && rxfh->hfunc != ETH_RSS_HASH_TOP) {
-		LOG_ERROR_BDF("sxe unsupport hfunc[%d]\n", rxfh->hfunc);
-		return -EOPNOTSUPP;
+	if (hfunc) {
+		LOG_ERROR_BDF("sxe unsupport hfunc[%d]\n", hfunc);
+		return -EINVAL;
 	}
 
-	if (rxfh->indir) {
+	if (redir) {
 		max_queues = min_t(int, adapter->rx_ring_ctxt.num, rss);
 
 		if ((adapter->cap & SXE_SRIOV_ENABLE) && max_queues < 2)
 			max_queues = 2;
 
 		for (i = 0; i < tbl_entries; i++) {
-			if (rxfh->indir[i] >= max_queues) {
+			if (redir[i] >= max_queues) {
 				LOG_ERROR_BDF("indir[%u]=%u > max_que=%u\n", i,
-					      rxfh->indir[i], max_queues);
+					      redir[i], max_queues);
 				return -EINVAL;
 			}
 		}
 
 		for (i = 0; i < tbl_entries; i++)
-			adapter->rss_indir_tbl[i] = rxfh->indir[i];
+			adapter->rss_indir_tbl[i] = redir[i];
 
 		hw->dbu.ops->rss_redir_tbl_set_all(hw, adapter->rss_indir_tbl);
 	}
 
-	if (rxfh->key) {
-		memcpy(adapter->rss_key, rxfh->key, sxe_get_rxfh_key_size(netdev));
+	if (key) {
+		memcpy(adapter->rss_key, key, sxe_get_rxfh_key_size(netdev));
 		hw->dbu.ops->rss_key_set_all(hw, adapter->rss_key);
 	}
 
