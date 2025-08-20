19978aefc43299c2f82948b0de450443ee0b0ad3
Yang Rong <youngrong@linux.alibaba.com>
2025-08-20 20:32:19 +0800

    anolis: mm/memory_hotplug: refactor memory_block_online()
    ANBZ: #18841
    
    This commit refactors the `memory_block_online()` function to prepare
    for deferred memory online support.
    
    Signed-off-by: Yang Rong <youngrong@linux.alibaba.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5656

--- include/linux/memory_hotplug.h
+++ include/linux/memory_hotplug.h
@@ -177,6 +177,10 @@ extern int add_one_highpage(struct page *page, int pfn, int bad_ppro);
 extern void adjust_present_page_count(struct page *page,
 				      struct memory_group *group,
 				      long nr_pages);
+extern void __adjust_present_page_count(struct page *page,
+				      struct memory_group *group,
+				      long nr_pages, struct zone *zone,
+				      int phase);
 /* VM interface that may be used by firmware interface */
 extern int mhp_init_memmap_on_memory(unsigned long pfn, unsigned long nr_pages,
 				     struct zone *zone);
