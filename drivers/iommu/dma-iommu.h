64d105fd01e274fdd370a8844002074c4253d4d4
Lei Gong <gonglei25@huawei.com>
2025-08-20 16:51:33 +0800

    iommu/ummu-core: get iova_domain from iommu_domain
    ANCK ID: #IJSBBS
    
    commit 21ffa054bd69b6577d2e6ec774738f27b8f8d704 openEuler.
    
    In order to achieve on-demand iova allocation, this patch add a temporal
    function 'iommu_get_iova_domain' to get the iova_domain from
    iommu_domain. This function will be deleted when the on-demond iova
    feature is built in.This is a temporary solution that will evolve along
    the main line.
    
    Signed-off-by: Yuhao Zhang <yuhao.zhang@huawei.com>
    Signed-off-by: Lei Gong <gonglei25@huawei.com>
    (cherry picked from commit 21ffa054bd69b6577d2e6ec774738f27b8f8d704)
    [Context conflicts.]
    Signed-off-by: Liu Jian <liujian56@huawei.com>

--- drivers/iommu/dma-iommu.h
+++ drivers/iommu/dma-iommu.h
@@ -22,6 +22,10 @@ void iommu_dma_get_resv_regions(struct device *dev, struct list_head *list);
 int iommu_dma_sw_msi(struct iommu_domain *domain, struct msi_desc *desc,
 		     phys_addr_t msi_addr);
 
+#ifdef CONFIG_UB_UMMU_CORE
+struct iova_domain *iommu_get_iova_domain(struct iommu_domain *domain);
+#endif
+
 extern bool iommu_dma_forcedac;
 
 #else /* CONFIG_IOMMU_DMA */
@@ -58,6 +62,13 @@ static inline int iommu_dma_sw_msi(struct iommu_domain *domain,
 {
 	return -ENODEV;
 }
+
+#ifndef CONFIG_UB_UMMU_CORE
+static inline struct iova_domain *iommu_get_iova_domain(struct iommu_domain *domain)
+{
+	return NULL;
+}
+#endif
 
 #endif	/* CONFIG_IOMMU_DMA */
 #endif	/* __DMA_IOMMU_H */
