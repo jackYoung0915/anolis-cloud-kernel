28f760af37832dfd41cd834125392f1c68369062
Lifeng Zheng <zhenglifeng1@huawei.com>
2025-08-21 15:15:58 +0800

    cpufreq: cppc: Fix invalid return value in hisi_cppc_cpufreq_get_rate()
    ANBZ: #26062
    
    commit d7c560f56e528fbb009f5f2b70cc813aad66661d upstream
    
    driver inclusion
    category: bugfix
    bugzilla: https://gitee.com/openeuler/kernel/issues/ICTP7R
    
    ----------------------------------------------------------------------
    
    Returning a negative error code in a function with an unsigned
    return type is a pretty bad idea. Return 0 is enough when something wrong.
    
    Fixes: cf7de25878a1 ("cppc_cpufreq: Fix possible null pointer dereference")
    Signed-off-by: Lifeng Zheng <zhenglifeng1@huawei.com>
    Signed-off-by: Hongye Lin <linhongye@h-partners.com>
    Signed-off-by: Lin Ruier <2878455989@qq.com>

--- drivers/cpufreq/cppc_cpufreq.c
+++ drivers/cpufreq/cppc_cpufreq.c
@@ -710,7 +710,7 @@ static unsigned int hisi_cppc_cpufreq_get_rate(unsigned int cpu)
 	int ret;
 
 	if (!policy)
-		return -ENODEV;
+		return 0;
 
 	cpu_data = policy->driver_data;
 
@@ -718,7 +718,7 @@ static unsigned int hisi_cppc_cpufreq_get_rate(unsigned int cpu)
 
 	ret = cppc_get_desired_perf(cpu, &desired_perf);
 	if (ret < 0)
-		return -EIO;
+		return 0;
 
 	return cppc_cpufreq_perf_to_khz(cpu_data, desired_perf);
 }
