743f9329e028477172825f884b181a30217b5251
Jay Chen <jkchen@linux.alibaba.com>
2025-08-21 10:20:23 +0800

    anolis: Revert "anolis: iommu/dma: Fix not fully traversing iova reservations issue"
    ANBZ: #13617
    
    This reverts commit 8e688d7457cadbbde0ce56b2ce1983c69f70a7ca.
    Reviewed-by: Shuai Xue <xueshuai@linux.alibaba.com>
    Reviewed-by: Jay Chen <jkchen@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5816

--- drivers/iommu/dma-iommu.c
+++ drivers/iommu/dma-iommu.c
@@ -736,7 +736,7 @@ static int iommu_dma_init_domain(struct iommu_domain *domain, struct device *dev
 		}
 
 		ret = 0;
-		goto iova_reserve;
+		goto done_unlock;
 	}
 
 	init_iova_domain(iovad, 1UL << order, base_pfn);
@@ -751,7 +751,6 @@ static int iommu_dma_init_domain(struct iommu_domain *domain, struct device *dev
 	    (!device_iommu_capable(dev, IOMMU_CAP_DEFERRED_FLUSH) || iommu_dma_init_fq(domain)))
 		domain->type = IOMMU_DOMAIN_DMA;
 
-iova_reserve:
 	ret = iova_reserve_iommu_regions(dev, domain);
 
 done_unlock:
