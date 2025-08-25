2edab8cf815cf7ae7ec47f6510aca58880f41e18
Ashish Kalra <ashish.kalra@amd.com>
2025-08-25 21:46:38 +0000

    crypto: ccp: Skip SEV and SNP INIT for kdump boot
    ANBZ: #32741
    
    commit 8c571019d8a817b701888926529a5d7a826b947b upstream.
    
    Since SEV or SNP may already be initialized in the previous kernel,
    attempting to initialize them again in the kdump kernel can result
    in SNP initialization failures, which in turn lead to IOMMU
    initialization failures. Moreover, SNP/SEV guests are not run under a
    kdump kernel, so there is no need to initialize SEV or SNP during
    kdump boot.
    
    Skip SNP and SEV INIT if doing kdump boot.
    
    Tested-by: Sairaj Kodilkar <sarunkod@amd.com>
    Signed-off-by: Ashish Kalra <ashish.kalra@amd.com>
    Link: https://lore.kernel.org/r/d884eff5f6180d8b8c6698a6168988118cf9cba1.1756157913.git.ashish.kalra@amd.com
    Signed-off-by: Joerg Roedel <joerg.roedel@amd.com>
    Signed-off-by: Yuanhe Shu <xiangzao@linux.alibaba.com>
    Reviewed-by: Qinyun Tan <qinyuntan@linux.alibaba.com>
    Reviewed-by: Guanghui Feng <guanghuifeng@linux.alibaba.com>
    Reviewed-by: Tianjia Zhang <tianjia.zhang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6705

--- drivers/crypto/ccp/sev-dev.c
+++ drivers/crypto/ccp/sev-dev.c
@@ -26,6 +26,7 @@
 #include <linux/fs.h>
 #include <linux/fs_struct.h>
 #include <linux/psp.h>
+#include <linux/crash_dump.h>
 
 #include <asm/smp.h>
 #include <asm/cacheflush.h>
@@ -484,6 +485,15 @@ static int __sev_platform_init_locked(int *error)
 
 	if (!psp || !psp->sev_data)
 		return -ENODEV;
+
+	/*
+	 * Skip SNP/SEV initialization under a kdump kernel as SEV/SNP
+	 * may already be initialized in the previous kernel. Since no
+	 * SNP/SEV guests are run under a kdump kernel, there is no
+	 * need to initialize SNP or SEV during kdump boot.
+	 */
+	if (is_kdump_kernel())
+		return 0;
 
 	sev = psp->sev_data;
 
