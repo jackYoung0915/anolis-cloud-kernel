0a1c3f8a9e416d4786193bc8cbe977fea1b4f56f
Pincheng Wang <pincheng.plct@isrc.iscas.ac.cn>
2025-08-27 00:29:37 +0800

    riscv: hwprobe: export Zilsd and Zclsd ISA extensions
    ANBZ: #33375
    
    commit 6118ebed3bdf896038f58d0d1804f551f33e8643 upstream.
    
    Export Zilsd and Zclsd ISA extensions through hwprobe.
    
    Signed-off-by: Pincheng Wang <pincheng.plct@isrc.iscas.ac.cn>
    Reviewed-by: Nutty Liu <nutty.liu@hotmail.com>
    Link: https://patch.msgid.link/20250826162939.1494021-4-pincheng.plct@isrc.iscas.ac.cn
    [pjw@kernel.org: fixed whitespace; updated to apply]
    Signed-off-by: Paul Walmsley <pjw@kernel.org>
    Signed-off-by: Ruidong Tian <tianruidong@linux.alibaba.com>

--- arch/riscv/kernel/sys_hwprobe.c
+++ arch/riscv/kernel/sys_hwprobe.c
@@ -109,6 +109,7 @@ static void hwprobe_isa_ext0(struct riscv_hwprobe *pair,
 		EXT_KEY(ZBS);
 		EXT_KEY(ZCA);
 		EXT_KEY(ZCB);
+		EXT_KEY(ZCLSD);
 		EXT_KEY(ZCMOP);
 		EXT_KEY(ZICBOM);
 		EXT_KEY(ZICBOP);
@@ -118,6 +119,7 @@ static void hwprobe_isa_ext0(struct riscv_hwprobe *pair,
 		EXT_KEY(ZIHINTNTL);
 		EXT_KEY(ZIHINTPAUSE);
 		EXT_KEY(ZIHPM);
+		EXT_KEY(ZILSD);
 		EXT_KEY(ZIMOP);
 		EXT_KEY(ZKND);
 		EXT_KEY(ZKNE);
