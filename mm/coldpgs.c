jeevandeepj994/anolis_perf

anolis: mm: coldpgs: batch free unref pages
ANBZ: #6075

To free unref page list in one shot may result in scheduling delay, when
the number of pages is enormous.  This takes filter->batch into
consideration when freeing unref page list in reclaim_coldpgs_from_list.

Signed-off-by: Xu Yu <xuyu@linux.alibaba.com>
Signed-off-by: Huang Haowei <hwhuang@linux.alibaba.com>

mm/coldpgs.c
@@ -925,7 +925,33 @@ keep:
 #ifdef CONFIG_ARCH_WANT_BATCHED_UNMAP_TLB_FLUSH
 	my_try_to_unmap_flush();
 #endif
-	my_free_unref_page_list(&free_pages);
+
+	if (filter->batch) {
+		/* Free pages in batch */
+		LIST_HEAD(batch_free_pages);
+
+		batch = 0;
+
+		while (!list_empty(&free_pages)) {
+			page = lru_to_page(&free_pages);
+			list_move(&page->lru, &batch_free_pages);
+
+			if (++batch >= filter->batch) {
+				my_free_unref_page_list(&batch_free_pages);
+
+				cond_resched();
+				batch = 0;
+				INIT_LIST_HEAD(&batch_free_pages);
+			}
+		}
+
+		/* Don't forget the remaining pages */
+		if (!list_empty(&batch_free_pages))
+			my_free_unref_page_list(&batch_free_pages);
+	} else {
+		/* Free pages in one shot */
+		my_free_unref_page_list(&free_pages);
+	}
 
 	/* Put all pages back to the list */
 	list_splice(&keep_pages, list);

jackYoung0915/anolis-cloud-kernel

ANBZ: #22924
cherry-picked from https://gitee.com/anolis/cloud-kernel/pulls/1982
[Zelin Deng: Convert to folio]
Link: https://gitee.com/anolis/cloud-kernel/pulls/1982
Signed-off-by: Zelin Deng <zelin.deng@linux.alibaba.com>
Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/5526

mm/coldpgs.c
@@ -934,7 +934,33 @@ keep:
 	/* Free folios that are eligible for releasing */
 	my_mem_cgroup_uncharge_list(&free_folios);
 	my_try_to_unmap_flush();
-	my_free_unref_page_list(&free_folios);
+
+	if (filter->batch) {
+		/* Free folios in batch */
+		LIST_HEAD(batch_free_folios);
+
+		batch = 0;
+
+		while (!list_empty(&free_folios)) {
+			folio = lru_to_folio(&free_folios);
+			list_move(&folio->lru, &batch_free_folios);
+
+			if (++batch >= filter->batch) {
+				my_free_unref_page_list(&batch_free_folios);
+
+				cond_resched();
+				batch = 0;
+				INIT_LIST_HEAD(&batch_free_folios);
+			}
+		}
+
+		/* Don't forget the remaining pages */
+		if (!list_empty(&batch_free_folios))
+			my_free_unref_page_list(&batch_free_folios);
+	} else {
+		/* Free pages in one shot */
+		my_free_unref_page_list(&free_folios);
+	}
 
 	/* Put all pages back to the list */
 	list_splice(&keep_folios, list);