jeevandeepj994/anolis_perf

anolis: mm: kidled: refactor cold slab
ANBZ: #1702

The previous cold slab use an internal field to record the age,
it will increase the object size and bring in fragmentation.

The patch refactor the implementation to cut down the defect.
It will reuse the page->mem_cgroup because slab will not in
the memcg when kmem accouting disable, and use an extra pointer
to store the age when kmem accouting enable, it is an specal
obj_cgroup pointer allocated by memcg_alloc_page_obj_cgroups.

Reviewed-by: Xu Yu <xuyu@linux.alibaba.com>
Signed-off-by: zhongjiang-ali <zhongjiang-ali@linux.alibaba.com>
Signed-off-by: Rongwei Wang <rongwei.wang@linux.alibaba.com>
Acked-by: Gang Deng <gavin.dg@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/565

mm/slab.h
@@ -330,8 +330,16 @@ static inline void memcg_slab_post_alloc_hook(struct kmem_cache *s,
 		if (likely(p[i])) {
 			page = virt_to_head_page(p[i]);
 
+#ifdef CONFIG_KIDLED
+			/*
+			 * Slab has allocated the obj_cgroups when create an new slab.
+			 * Do not count the account in the memcg in case allocation fails
+			 */
+			if (unlikely(!page_has_obj_cgroups(page))) {
+#else
 			if (!page_has_obj_cgroups(page) &&
-			    memcg_alloc_page_obj_cgroups(page, s, flags)) {
+				memcg_alloc_page_obj_cgroups(page, s, flags)) {
+#endif
 				obj_cgroup_uncharge(objcg, obj_full_size(s));
 				continue;
 			}
@@ -450,6 +458,10 @@ static __always_inline void unaccount_slab_page(struct page *page, int order,
 {
 	if (memcg_kmem_enabled())
 		memcg_free_page_obj_cgroups(page);
+	else {
+		VM_BUG_ON_PAGE(!page_has_slab_age(page), page);
+		kidled_free_slab_age(page);
+	}
 
 	mod_node_page_state(page_pgdat(page), cache_vmstat_idx(s),
 			    -(PAGE_SIZE << order));

jackYoung0915/anolis-cloud-kernel

ANBZ: #22925
[Backport note]
In kernel 6.6, the association of slab and memcg is managed uniformly
through the memcg_data structure. For this backport, we made the
following modifications to align with our older kernel version:
1. When kmem memcg accounting is disabled, we reuse the memcg_data
field to store the slab age pointer, and mark its usage by MEMCG_DATA_SLAB_AGE.
2. When kmem memcg accounting is enabled, we extend the (struct obj_cgroup *)
array allocated by memcg_alloc_slab_cgroups, reserving an extra pointer to
store the age for each slab object.
These adjustments ensure correct slab age management compatible
with our kernel version and reduce memory fragmentation.
Signed-off-by: Weilin Tong <tongweilin@linux.alibaba.com>
Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/5537

mm/slab.h
@@ -452,7 +452,8 @@ static inline struct obj_cgroup **slab_objcgs(struct slab *slab)
 
 	VM_BUG_ON_PAGE(memcg_data && !(memcg_data & MEMCG_DATA_OBJCGS),
 							slab_page(slab));
-	VM_BUG_ON_PAGE(memcg_data & MEMCG_DATA_KMEM, slab_page(slab));
+	VM_BUG_ON_PAGE((memcg_data & MEMCG_DATA_FLAGS_MASK) != MEMCG_DATA_KMEM,
+		       slab_page(slab));
 
 	return (struct obj_cgroup **)(memcg_data & ~MEMCG_DATA_FLAGS_MASK);
 }
@@ -653,6 +654,10 @@ static __always_inline void unaccount_slab(struct slab *slab, int order,
 {
 	if (memcg_kmem_online())
 		memcg_free_slab_cgroups(slab);
+	else {
+		VM_BUG_ON_PAGE(!page_has_slab_age(slab), slab);
+		kidled_free_slab_age(slab);
+	}
 
 	mod_node_page_state(slab_pgdat(slab), cache_vmstat_idx(s),
 			    -(PAGE_SIZE << order));