07500f136dc1e26f18bcaf329af6a5b0ae0a48cc
Valentin Schneider <vschneid@redhat.com>
2025-08-29 16:11:16 +0800

    sched/fair: Add related data structure for task based throttle
    ANBZ: #27204
    
    commit 2cd571245b43492867bf1b4252485f3e6647b643 upstream.
    
    Add related data structures for this new throttle functionality.
    
    Tesed-by: K Prateek Nayak <kprateek.nayak@amd.com>
    Signed-off-by: Valentin Schneider <vschneid@redhat.com>
    Signed-off-by: Aaron Lu <ziqianlu@bytedance.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Chengming Zhou <chengming.zhou@linux.dev>
    Tested-by: Valentin Schneider <vschneid@redhat.com>
    Tested-by: Matteo Martelli <matteo.martelli@codethink.co.uk>
    Link: https://lore.kernel.org/r/20250829081120.806-2-ziqianlu@bytedance.com
    Signed-off-by: Yi Tao <escape@linux.alibaba.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -3592,6 +3592,9 @@ static void __sched_fork(unsigned long clone_flags, struct task_struct *p)
 
 #ifdef CONFIG_FAIR_GROUP_SCHED
 	p->se.cfs_rq			= NULL;
+#ifdef CONFIG_CFS_BANDWIDTH
+	init_cfs_throttle_work(p);
+#endif
 #endif
 
 #ifdef CONFIG_GROUP_IDENTITY
