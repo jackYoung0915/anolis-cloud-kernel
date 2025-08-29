b643492c717a23384857196754a39144a7fdfced
Aaron Lu <ziqianlu@bytedance.com>
2025-08-29 16:11:19 +0800

    sched/fair: Task based throttle time accounting
    ANBZ: #27204
    
    commit eb962f251fbba251a0d34897d6170f7616d70c52 upstream.
    
    With task based throttle model, the previous way to check cfs_rq's
    nr_queued to decide if throttled time should be accounted doesn't work
    as expected, e.g. when a cfs_rq which has a single task is throttled,
    that task could later block in kernel mode instead of being dequeued on
    limbo list and accounting this as throttled time is not accurate.
    
    Rework throttle time accounting for a cfs_rq as follows:
    - start accounting when the first task gets throttled in its hierarchy;
    - stop accounting on unthrottle.
    
    Note that there will be a time gap between when a cfs_rq is throttled
    and when a task in its hierarchy is actually throttled. This accounting
    mechanism only starts accounting in the latter case.
    
    Suggested-by: Chengming Zhou <chengming.zhou@linux.dev> # accounting mechanism
    Co-developed-by: K Prateek Nayak <kprateek.nayak@amd.com> # simplify implementation
    Signed-off-by: K Prateek Nayak <kprateek.nayak@amd.com>
    Signed-off-by: Aaron Lu <ziqianlu@bytedance.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Tested-by: Valentin Schneider <vschneid@redhat.com>
    Tested-by: Matteo Martelli <matteo.martelli@codethink.co.uk>
    Tested-by: K Prateek Nayak <kprateek.nayak@amd.com>
    Link: https://lore.kernel.org/r/20250829081120.806-5-ziqianlu@bytedance.com
    Signed-off-by: Yi Tao <escape@linux.alibaba.com>
    Reviewed-by: Cruz Zhao <CruzZhao@linux.alibaba.com>
    Reviewed-by: Cruz Zhao <cruzzhao@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6018

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6753,14 +6753,12 @@ enqueue_entity(struct cfs_rq *cfs_rq, struct sched_entity *se, int flags)
 	if (cfs_rq->nr_running == 1) {
 		check_enqueue_throttle(cfs_rq);
 #ifdef CONFIG_CFS_BANDWIDTH
-		if (throttled_hierarchy(cfs_rq)) {
+		if (cfs_rq->pelt_clock_throttled) {
 			struct rq *rq = rq_of(cfs_rq);
 
-			if (cfs_rq->pelt_clock_throttled) {
-				cfs_rq->throttled_clock_pelt_time += rq_clock_pelt(rq) -
-					cfs_rq->throttled_clock_pelt;
-				cfs_rq->pelt_clock_throttled = 0;
-			}
+			cfs_rq->throttled_clock_pelt_time += rq_clock_pelt(rq) -
+				cfs_rq->throttled_clock_pelt;
+			cfs_rq->pelt_clock_throttled = 0;
 		}
 #endif
 
@@ -7453,7 +7451,7 @@ static void throttle_cfs_rq_work(struct callback_head *work)
 		goto unlock;
 	update_rq_clock(rq);
 	WARN_ON_ONCE(p->throttled || !list_empty(&p->throttle_node));
-	dequeue_task_fair(rq, p, DEQUEUE_SLEEP);
+	dequeue_task_fair(rq, p, DEQUEUE_SLEEP | DEQUEUE_THROTTLE);
 	list_add(&p->throttle_node, &cfs_rq->throttled_limbo_list);
 	/*
 	 * Must not set throttled before dequeue or dequeue will
@@ -7605,6 +7603,14 @@ static inline void task_throttle_setup_work(struct task_struct *p)
 	task_work_add(p, &p->sched_throttle_work, TWA_RESUME);
 }
 
+static void record_throttle_clock(struct cfs_rq *cfs_rq)
+{
+	struct rq *rq = rq_of(cfs_rq);
+
+	if (cfs_rq_throttled(cfs_rq) && !cfs_rq->throttled_clock)
+		cfs_rq->throttled_clock = rq_clock(rq);
+}
+
 static int tg_throttle_down(struct task_group *tg, void *data)
 {
 	struct rq *rq = data;
@@ -7669,7 +7675,6 @@ static bool throttle_cfs_rq(struct cfs_rq *cfs_rq)
 	 * throttled-list.  rq->lock protects completion.
 	 */
 	cfs_rq->throttled = 1;
-	cfs_rq->throttled_clock = rq_clock(rq);
 	return true;
 }
 
@@ -8347,6 +8352,7 @@ static void task_throttle_setup_work(struct task_struct *p) {}
 static bool task_is_throttled(struct task_struct *p) { return false; }
 static void dequeue_throttled_task(struct task_struct *p, int flags) {}
 static bool enqueue_throttled_task(struct task_struct *p) { return false; }
+static void record_throttle_clock(struct cfs_rq *cfs_rq) {}
 
 static inline int cfs_rq_throttled(struct cfs_rq *cfs_rq)
 {
@@ -8625,6 +8631,7 @@ static void dequeue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 	struct cfs_rq *cfs_rq;
 	struct sched_entity *se = &p->se;
 	int task_sleep = flags & DEQUEUE_SLEEP;
+	int task_throttled = flags & DEQUEUE_THROTTLE;
 	int idle_h_nr_running = task_has_idle_policy(p);
 	bool was_sched_idle = sched_idle_rq(rq);
 
@@ -8657,6 +8664,9 @@ static void dequeue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 		if (cfs_rq_is_idle(cfs_rq))
 			idle_h_nr_running = 1;
 
+		if (throttled_hierarchy(cfs_rq) && task_throttled)
+			record_throttle_clock(cfs_rq);
+
 		/* Don't dequeue parent if it has other entities besides us */
 		if (cfs_rq->load.weight) {
 			/* Avoid re-evaluating load for this entity: */
@@ -8686,6 +8696,8 @@ static void dequeue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 		if (cfs_rq_is_idle(cfs_rq))
 			idle_h_nr_running = 1;
 
+		if (throttled_hierarchy(cfs_rq) && task_throttled)
+			record_throttle_clock(cfs_rq);
 	}
 
 	/* At this point se is NULL and we are at root level*/
