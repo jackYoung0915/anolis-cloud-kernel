c2c720a900bdf6d756fbaf25492f107e487ba1fd
Jinjiang Tu <tujinjiang@huawei.com>
2025-08-30 14:23:49 +0800

    mm/hugetlb: fix surplus pages in dissolve_free_huge_page()
    ANCK ID: #IJSBBS
    
    commit cb402bbdabcaa5a765068c5b8673bbfc1c264242 upstream.
    
    In dissolve_free_huge_page(), free huge pages are dissolved without
    adjusting surplus count. However, free huge pages may be accounted as
    surplus pages, and will lead to wrong surplus count.
    
    I reproduce this issue on qemu. The steps are:
    1) Node1 is memory-less at first. Hot-add memory to node1 by executing
    the two commands in qemu monitor:
      object_add memory-backend-ram,id=mem1,size=1G
      device_add pc-dimm,id=dimm1,memdev=mem1,node=1
    2) online one memory block of Node1 with：
      echo online_movable > /sys/devices/system/node/node1/memoryX/state
    3) create 64 huge pages for node1
    4) run a program to reserve (don't consume) all the huge pages
    5) echo 0 > nr_huge_pages for node1. After this step, free huge pages in
    Node1 are surplus.
    6) create 80 huge pages for node0
    7) offline memory of node1, The memory range to offline contains the free
    surplus huge pages created in step3) ~ step5)
      echo offline > /sys/devices/system/node/node1/memoryX/state
    8) kill the program in step 4)
    
    The result:
               Node0     Node1
    total       80        0
    free        80        0
    surplus     0         61
    
    To fix it, adjust surplus when destroying huge pages if the node has
    surplus pages in dissolve_free_hugetlb_folio().
    
    The result with this patch:
               Node0     Node1
    total       80        0
    free        80        0
    surplus     0         0
    
    Link: https://lkml.kernel.org/r/20250304132106.2872754-1-tujinjiang@huawei.com
    Fixes: c8721bbbdd36 ("mm: memory-hotplug: enable memory hotplug to handle hugepage")
    Signed-off-by: Jinjiang Tu <tujinjiang@huawei.com>
    Acked-by: David Hildenbrand <david@redhat.com>
    Acked-by: Oscar Salvador <osalvador@suse.de>
    Cc: Jinjiang Tu <tujinjiang@huawei.com>
    Cc: Kefeng Wang <wangkefeng.wang@huawei.com>
    Cc: Muchun Song <muchun.song@linux.dev>
    Cc: Nanyong Sun <sunnanyong@huawei.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    Conflicts:
    	mm/hugetlb.c
    [Context conflicts.]
    Signed-off-by: Jinjiang Tu <tujinjiang@huawei.com>
    (cherry picked from commit b047fa94f941034f1db399af834b04e08eae4b61)
    Conflicts:
    	mm/hugetlb.c
    [Context conflicts.]
    Signed-off-by: Liu Jian <liujian56@huawei.com>

--- mm/hugetlb.c
+++ mm/hugetlb.c
@@ -2283,6 +2283,8 @@ retry:
 
 	if (!folio_ref_count(folio)) {
 		struct hstate *h = folio_hstate(folio);
+		bool adjust_surplus = false;
+
 		if (!available_huge_pages(h))
 			goto out;
 
@@ -2305,7 +2307,9 @@ retry:
 			goto retry;
 		}
 
-		remove_hugetlb_folio(h, folio, false);
+		if (h->surplus_huge_pages_node[folio_nid(folio)])
+			adjust_surplus = true;
+		remove_hugetlb_folio(h, folio, adjust_surplus);
 		h->max_huge_pages--;
 		spin_unlock_irq(&hugetlb_lock);
 
