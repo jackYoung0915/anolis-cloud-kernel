a58fcd802bd830087bd1a7336bed4ce53ecb999a
Zelin Deng <zelin.deng@linux.alibaba.com>
2025-09-11 03:19:25 -0400

    anolis: Revert "x86: pgtable: convert __tlb_remove_table() to use struct ptdesc"
    ANBZ: #24557
    
    This reverts commit 8409e3403503b240cb0363e2cb8f4a7797bc4ea3.
    
    Signed-off-by: Zelin Deng <zelin.deng@linux.alibaba.com>

--- arch/x86/include/asm/tlb.h
+++ arch/x86/include/asm/tlb.h
@@ -31,26 +31,24 @@ static inline void tlb_flush(struct mmu_gather *tlb)
  */
 static inline void __tlb_remove_table(void *table)
 {
-	struct ptdesc *ptdesc = (struct ptdesc *)table;
-
-	pagetable_free(ptdesc);
+	free_page_and_swap_cache(table);
 }
 
 #ifdef CONFIG_PT_RECLAIM
 static inline void __tlb_remove_table_one_rcu(struct rcu_head *head)
 {
-	struct ptdesc *ptdesc;
+	struct page *page;
 
-	ptdesc = container_of(head, struct ptdesc, pt_rcu_head);
-	__tlb_remove_table(ptdesc);
+	page = container_of(head, struct page, rcu_head);
+	put_page(page);
 }
 
 static inline void __tlb_remove_table_one(void *table)
 {
-	struct ptdesc *ptdesc;
+	struct page *page;
 
-	ptdesc = table;
-	call_rcu(&ptdesc->pt_rcu_head, __tlb_remove_table_one_rcu);
+	page = table;
+	call_rcu(&page->rcu_head, __tlb_remove_table_one_rcu);
 }
 #define __tlb_remove_table_one __tlb_remove_table_one
 #endif /* CONFIG_PT_RECLAIM */
