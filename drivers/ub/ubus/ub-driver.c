72910d7d12f1d1464bb2f5af3854a919936183c0
Junlong Zheng <zhengjunlong@huawei.com>
2025-09-19 09:51:10 +0800

    ub:ubus: Support for UB Bus DMA Configuration Function
    ANBZ: #IJSBBS
    
    commit fae045218cb429e88a3eb818974ed0fcdd822ea6 openEuler.
    
    Support for UB bus dma_configure and dma_cleanup callbacks,
    to enable UB bus device DMA data path.
    
    Signed-off-by: Junlong Zheng <zhengjunlong@huawei.com>
    Signed-off-by: qiushengming <qiushengming1@huawei.com>

--- drivers/ub/ubus/ub-driver.c
+++ drivers/ub/ubus/ub-driver.c
@@ -4,11 +4,174 @@
  */
 
 #include <linux/device.h>
+#include <linux/dma-mapping.h>
+#include <linux/dma-map-ops.h>
+#include <linux/iommu.h>
 
 #include "ubus_inner.h"
 
+static struct fwnode_handle *iommu_fwnode;
+void ub_iommu_fwnode_handle_set(const struct fwnode_handle *fwnode)
+{
+	iommu_fwnode = (struct fwnode_handle *)fwnode;
+}
+EXPORT_SYMBOL_GPL(ub_iommu_fwnode_handle_set);
+
+const struct fwnode_handle *ub_iommu_fwnode_handle_get(void)
+{
+	return iommu_fwnode;
+}
+EXPORT_SYMBOL_GPL(ub_iommu_fwnode_handle_get);
+
+static void ubct_dma_setup(struct device *dev)
+{
+	struct ub_entity *uent = to_ub_entity(dev);
+	u64 end, mask, size;
+	u8 addr_limit;
+
+	addr_limit = uent->ubc->attr.mem_size_limit;
+	if (!addr_limit) {
+		pr_warn(FW_BUG "ub bus controller missing memory address limit\n");
+		return;
+	}
+
+	size = (addr_limit >= SZ_64) ? U64_MAX : (1ULL << addr_limit);
+	end = size - 1;
+	mask = DMA_BIT_MASK(ilog2(end) + 1);
+	dev->bus_dma_limit = end;
+	dev->coherent_dma_mask = min(dev->coherent_dma_mask, mask);
+	*dev->dma_mask = min(*dev->dma_mask, mask);
+}
+
+static int ubct_iommu_fwspec_init(struct device *dev, u32 id,
+				  struct fwnode_handle *fwnode,
+				  bool *fwspec_exists)
+{
+	int ret;
+
+	*fwspec_exists = dev_iommu_fwspec_get(dev) != NULL;
+	ret = iommu_fwspec_init(dev, fwnode);
+	if (ret)
+		return ret;
+	if (*fwspec_exists)
+		return 0;
+
+	return iommu_fwspec_add_ids(dev, &id, 1);
+}
+
+static int ubct_iommu_configure(struct device *dev, bool *fwspec_exists)
+{
+	if (!iommu_fwnode) {
+		dev_err(dev, "ubus's iommu_fwnode not ready\n");
+		return -ENODEV;
+	}
+
+	/* input id is 0, ummu not care */
+	return ubct_iommu_fwspec_init(dev, 0, iommu_fwnode, fwspec_exists);
+}
+
+static int handle_iommu_configure_error(struct device *dev, int err)
+{
+	if (err == -EPROBE_DEFER)
+		return -EPROBE_DEFER;
+
+	dev_warn(dev, "Adding to IOMMU failed: %d\n", err);
+	return 0;
+}
+
+static int ub_hybrid_iommu_configure(struct device *dev)
+{
+	bool fwspec_exists;
+	int err;
+
+	err = ubct_iommu_configure(dev, &fwspec_exists);
+	if (err) {
+		if (err != -EPROBE_DEFER)
+			dev_err(dev, "viot iommu configure: %d\n", err);
+
+		return handle_iommu_configure_error(dev, err);
+	}
+	if (fwspec_exists)
+		return 0;
+
+	if (dev->bus && !device_iommu_mapped(dev)) {
+		err = iommu_probe_device(dev);
+		if (err)
+			return handle_iommu_configure_error(dev, err);
+	}
+
+	return 0;
+}
+
+static int ub_hybrid_dma_configure(struct device *dev, enum dev_dma_attr attr)
+{
+	int ret;
+
+	if (attr == DEV_DMA_NOT_SUPPORTED) {
+		set_dma_ops(dev, &dma_dummy_ops);
+		return 0;
+	}
+
+	ubct_dma_setup(dev);
+
+	ret = ub_hybrid_iommu_configure(dev);
+	if (ret == -EPROBE_DEFER)
+		return -EPROBE_DEFER;
+
+	arch_setup_dma_ops(dev, attr == DEV_DMA_COHERENT);
+
+	return 0;
+}
+
+/* Translate from FW */
+static enum dev_dma_attr ub_dma_attr_trans(u16 dma_attr)
+{
+	switch (dma_attr) {
+	case 0:
+		return DEV_DMA_NON_COHERENT;
+	case 1:
+		return DEV_DMA_COHERENT;
+	default:
+		return DEV_DMA_NOT_SUPPORTED;
+	}
+}
+
+static int ub_dma_configure(struct device *dev)
+{
+	struct ub_driver *udrv = to_ub_driver(dev->driver);
+	struct ub_entity *uent = to_ub_entity(dev);
+	struct ub_bus_controller *ubc;
+	int ret;
+
+	ubc = ub_ubc_get(uent->ubc);
+
+	ret = ub_hybrid_dma_configure(dev,
+				      ub_dma_attr_trans(ubc->attr.dma_cca));
+	if (!ret && !udrv->driver_managed_dma) {
+		ret = iommu_device_use_default_domain(dev);
+		if (ret)
+			arch_teardown_dma_ops(dev);
+	}
+
+	ub_ubc_put(ubc);
+	dev_info(dev, "dma_configure ret = %d\n", ret);
+	return ret;
+}
+
+static void ub_dma_cleanup(struct device *dev)
+{
+	struct ub_driver *driver = to_ub_driver(dev->driver);
+
+	if (!driver->driver_managed_dma)
+		iommu_device_unuse_default_domain(dev);
+
+	dev_dbg(dev, "dma_cleanup\n");
+}
+
 struct bus_type ub_bus_type = {
 	.name = "ub",
+	.dma_configure = ub_dma_configure,
+	.dma_cleanup = ub_dma_cleanup,
 };
 EXPORT_SYMBOL_GPL(ub_bus_type);
 
