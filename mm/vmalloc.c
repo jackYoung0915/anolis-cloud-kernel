3159a3e445d53e577066168d6a90b45b3e21ad0b
Mike Rapoport (Microsoft) <rppt@kernel.org>
2025-09-21 08:44:57 +0300

    kho: add support for preserving vmalloc allocations
    ANBZ: #26808
    
    commit a667300bd53f272a3055238bcefe108f88836270 upstream.
    
    A vmalloc allocation is preserved using binary structure similar to global
    KHO memory tracker.  It's a linked list of pages where each page is an
    array of physical address of pages in vmalloc area.
    
    kho_preserve_vmalloc() hands out the physical address of the head page to
    the caller.  This address is used as the argument to kho_vmalloc_restore()
    to restore the mapping in the vmalloc address space and populate it with
    the preserved pages.
    
    [Zelin Deng: Exposed vmalloc APIs are needed but they are introduced
    by other large patches, just make them exposed in this patch]
    
    [pasha.tatashin@soleen.com: free chunks using free_page() not kfree()]
      Link: https://lkml.kernel.org/r/mafs0a52idbeg.fsf@kernel.org
    [akpm@linux-foundation.org: coding-style cleanups]
    Link: https://lkml.kernel.org/r/20250921054458.4043761-4-rppt@kernel.org
    Signed-off-by: Mike Rapoport (Microsoft) <rppt@kernel.org>
    Reviewed-by: Pratyush Yadav <pratyush@kernel.org>
    Cc: Alexander Graf <graf@amazon.com>
    Cc: Baoquan He <bhe@redhat.com>
    Cc: Changyuan Lyu <changyuanl@google.com>
    Cc: Chris Li <chrisl@kernel.org>
    Cc: Jason Gunthorpe <jgg@nvidia.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Zelin Deng <zelin.deng@linux.alibaba.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5993

--- mm/vmalloc.c
+++ mm/vmalloc.c
@@ -665,7 +665,7 @@ int vmap_pages_range_noflush(unsigned long addr, unsigned long end,
  * RETURNS:
  * 0 on success, -errno on failure.
  */
-static int vmap_pages_range(unsigned long addr, unsigned long end,
+int vmap_pages_range(unsigned long addr, unsigned long end,
 		pgprot_t prot, struct page **pages, unsigned int page_shift)
 {
 	int err;
@@ -2487,6 +2487,11 @@ static inline unsigned int vm_area_page_order(struct vm_struct *vm)
 #endif
 }
 
+unsigned int get_vm_area_page_order(struct vm_struct *vm)
+{
+	return vm_area_page_order(vm);
+}
+
 static inline void set_vm_area_page_order(struct vm_struct *vm, unsigned int order)
 {
 #ifdef CONFIG_HAVE_ARCH_HUGE_VMALLOC
@@ -2624,7 +2629,7 @@ static void clear_vm_uninitialized_flag(struct vm_struct *vm)
 	vm->flags &= ~VM_UNINITIALIZED;
 }
 
-static struct vm_struct *__get_vm_area_node(unsigned long size,
+struct vm_struct *__get_vm_area_node(unsigned long size,
 		unsigned long align, unsigned long shift, unsigned long flags,
 		unsigned long start, unsigned long end, int node,
 		gfp_t gfp_mask, const void *caller)
