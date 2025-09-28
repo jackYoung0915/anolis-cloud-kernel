281b45c5b8fa3e2f7f82a8a3ed8526366bf60edf
Cruz Zhao <CruzZhao@linux.alibaba.com>
2025-09-28 10:55:12 +0800

    anolis: sched: prevent SCHED_IDLE tasks from starving
    ANBZ: #22143
    
    The ID_LOAD_BALANCE mechanism was not properly handling the
    logic in pick_next_task() when core sched enabled, which could
    lead to SCHED_IDLE tasks being starved in certain scenarios.
    To avoid this, we have modified the triggering mechanism of
    ID_LOAD_BALANCE as follows:
     - At the beginning of pick_next_task, set rq->pulled = false.
     - If core sched is enabled, balance_fair() will be called
       before pick_task(). Inside balance_fair(), if the current
       rq is a sched_idle_rq, we will trigger newidle_balance().
     - If core sched is not enabled, pick_next_task_fair() will be
       called. In pick_next_task_fair(), if the current rq is a
       sched_idle_rq, we will first goto idle, invoke
       newidle_balance() to pull tasks from other CPUs, and then
       pick task from runqueue.
    
    Fixes: ("23f51bbec63a anolis: sched: introduce ID_LOAD_BALANCE")
    Signed-off-by: Cruz Zhao <CruzZhao@linux.alibaba.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8917,9 +8917,11 @@ static void task_dead_fair(struct task_struct *p)
 static int
 balance_fair(struct rq *rq, struct task_struct *prev, struct rq_flags *rf)
 {
-	if (rq->nr_running)
+	if (rq->nr_running && (!sched_feat(ID_LOAD_BALANCE) || !sched_idle_rq(rq)))
 		return 1;
 
+	if (sched_feat(ID_LOAD_BALANCE))
+		rq->pulled = true;
 	return newidle_balance(rq, rf) != 0;
 }
 #endif /* CONFIG_SMP */
@@ -9189,13 +9191,11 @@ bool id_can_stop_tick(struct rq *rq)
 }
 #endif
 
-static struct task_struct *__pick_task_fair(struct rq *rq)
+static struct task_struct *pick_task_fair(struct rq *rq)
 {
 	struct sched_entity *se;
 	struct cfs_rq *cfs_rq;
 
-	if (sched_feat(ID_LOAD_BALANCE) && sched_idle_rq(rq) && !rq->pulled)
-		return NULL;
 again:
 	cfs_rq = &rq->cfs;
 	if (!cfs_rq->nr_queued)
@@ -9218,14 +9218,6 @@ again:
 	return task_of(se);
 }
 
-static struct task_struct *pick_task_fair(struct rq *rq)
-{
-	if (sched_feat(ID_LOAD_BALANCE))
-		rq->pulled = false;
-
-	return __pick_task_fair(rq);
-}
-
 static void __set_next_task_fair(struct rq *rq, struct task_struct *p, bool first);
 static void set_next_task_fair(struct rq *rq, struct task_struct *p, bool first);
 
@@ -9236,13 +9228,13 @@ pick_next_task_fair(struct rq *rq, struct task_struct *prev, struct rq_flags *rf
 	struct task_struct *p;
 	int new_tasks;
 
-	if (sched_feat(ID_LOAD_BALANCE))
-		rq->pulled = false;
-
 	update_rq_on_expel(rq);
 	push_expellee(rq);
 again:
-	p = __pick_task_fair(rq);
+	if (sched_feat(ID_LOAD_BALANCE) && sched_idle_rq(rq) && !rq->pulled)
+		p = NULL;
+	else
+		p = pick_task_fair(rq);
 	if (!p)
 		goto idle;
 	se = &p->se;
@@ -9304,11 +9296,15 @@ idle:
 	 * possible for any higher priority task to appear. In that case we
 	 * must re-start the pick_next_entity() loop.
 	 */
-	if (new_tasks < 0)
+	if (new_tasks < 0) {
+		if (sched_feat(ID_LOAD_BALANCE) && !rq->pulled)
+			rq->pulled = true;
 		return RETRY_TASK;
+	}
 
 	if (new_tasks > 0) {
-		rq->pulled = true;
+		if (sched_feat(ID_LOAD_BALANCE) && !rq->pulled)
+			rq->pulled = true;
 		goto again;
 	}
 
