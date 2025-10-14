832c5396556e3f6d6b3b77d832117236a959d93f
Jay Chen <jkchen@linux.alibaba.com>
2025-10-14 17:32:58 +0800

    anolis: add mainline merge modifications about smmu v3 interface
    ANBZ: #13617
    
    The modifications come from 42f0cbb2a253bcd7d4f20e80462014622f19d88e
    upstream patch
    
    Signed-off-by: Jay Chen <jkchen@linux.alibaba.com>
    Reviewed-by: Shuai Xue <xueshuai@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5852

--- drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.c
+++ drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.c
@@ -3291,6 +3291,7 @@ arm_smmu_domain_alloc_paging_flags(struct device *dev, u32 flags,
 	struct arm_smmu_master *master = dev_iommu_priv_get(dev);
 	struct arm_smmu_device *smmu = master->smmu;
 	const u32 PAGING_FLAGS = IOMMU_HWPT_ALLOC_DIRTY_TRACKING |
+				 IOMMU_HWPT_ALLOC_PASID |
 				 IOMMU_HWPT_ALLOC_NEST_PARENT;
 	struct arm_smmu_domain *smmu_domain;
 	int ret;
