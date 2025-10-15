186900b3aec77d00dab009ff647164062d5bdd4c
Hao Ge <gehao@kylinos.cn>
2025-10-15 22:16:42 +0800

    slab: reset slab->obj_ext when freeing and it is OBJEXTS_ALLOC_FAIL
    ANBZ: #32420
    
    commit 86f54f9b6c17d6567c69e3a6fed52fdf5d7dbe93 upstream.
    
    If obj_exts allocation failed, slab->obj_exts is set to OBJEXTS_ALLOC_FAIL,
    But we do not clear it when freeing the slab. Since OBJEXTS_ALLOC_FAIL and
    MEMCG_DATA_OBJEXTS currently share the same bit position, during the
    release of the associated folio, a VM_BUG_ON_FOLIO() check in
    folio_memcg_kmem() is triggered because the OBJEXTS_ALLOC_FAIL flag was
    not cleared, causing it to be interpreted as a kmem folio (non-slab)
    with MEMCG_OBJEXTS_DATA flag set, which is invalid because
    MEMCG_OBJEXTS_DATA is supposed to be set only on slabs.
    
    Another problem that predates sharing the OBJEXTS_ALLOC_FAIL and
    MEMCG_DATA_OBJEXTS bits is that on configurations with
    is_check_pages_enabled(), the non-cleared bit in page->memcg_data will
    trigger a free_page_is_bad() failure "page still charged to cgroup"
    
    When freeing a slab, we clear slab->obj_exts if the obj_ext array has
    been successfully allocated. So let's clear it also when the allocation
    has failed.
    
    Fixes: 09c46563ff6d ("codetag: debug: introduce OBJEXTS_ALLOC_FAIL to mark failed slab_ext allocations")
    Fixes: 7612833192d5 ("slab: Reuse first bit for OBJEXTS_ALLOC_FAIL")
    Link: https://lore.kernel.org/all/20251015141642.700170-1-hao.ge@linux.dev/
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Hao Ge <gehao@kylinos.cn>
    Reviewed-by: Suren Baghdasaryan <surenb@google.com>
    Reviewed-by: Harry Yoo <harry.yoo@oracle.com>
    Signed-off-by: Vlastimil Babka <vbabka@suse.cz>
    Signed-off-by: Yuanhe Shu <xiangzao@linux.alibaba.com>
    Acked-by: David Rientjes <rientjes@google.com>
    Acked-by: Kent Overstreet <kent.overstreet@linux.dev>
    Acked-by: Muchun Song <muchun.song@linux.dev>
    Reviewed-by: Kees Cook <kees@kernel.org>
    Reviewed-by: Hao Li <hao.li@linux.dev>
    Reviewed-by: Miaohe Lin <linmiaohe@huawei.com>
    Acked-by: David Hildenbrand <david@redhat.com>
    Reviewed-by: Pasha Tatashin <pasha.tatashin@soleen.com>
    Acked-by: Shakeel Butt <shakeel.butt@linux.dev>
    Acked-by: SeongJae Park <sj@kernel.org>
    Acked-by: Yu Zhao <yuzhao@google.com>
    Reviewed-by: Anshuman Khandual <anshuman.khandual@arm.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6766

--- mm/slab.h
+++ mm/slab.h
@@ -635,8 +635,15 @@ static inline void free_slab_obj_exts(struct slab *slab)
 	struct slabobj_ext *obj_exts;
 
 	obj_exts = slab_obj_exts(slab);
-	if (!obj_exts)
+	if (!obj_exts) {
+		/*
+		 * If obj_exts allocation failed, slab->obj_exts is set to
+		 * OBJEXTS_ALLOC_FAIL. In this case, we end up here and should
+		 * clear the flag.
+		 */
+		slab->obj_exts = 0;
 		return;
+	}
 
 	/* kidled: free the age array stored in the tail slot.
 	 * Only when kmem is enabled (!cgroup_memory_nokmem); when kmem is
