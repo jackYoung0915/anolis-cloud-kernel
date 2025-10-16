99be8a43bb2057df7ec45ec984df14d9af933938
Qinyun Tan <qinyuntan@linux.alibaba.com>
2025-10-16 16:19:19 +0800

    anolis: spec: Add Git commit and branch information to kernel RPM package description
    ANBZ: #26245
    
    This change modifies the RPM build process to include Git
    source information in the kernel package description.
    
    The changes include:
    
    - Modifying genspec.sh to retrieve Git commit hash and
    branch information during build
    
    - Updating kernel.spec.template to include this information
    in the package description
    
    Signed-off-by: Qinyun Tan <qinyuntan@linux.alibaba.com>
    Reviewed-by: Qiao Ma <mqaio@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5867

--- anolis/genspec.sh
+++ anolis/genspec.sh
@@ -5,6 +5,22 @@
 mkdir -p ${DIST_OUTPUT}
 cp -f ${DIST_RPM}/${DIST_SPEC_TEMPLATE} ${DIST_OUTPUT}/${DIST_SPEC_FILE}
 
+# get git commit hash and branch information
+GIT_COMMIT=$(git rev-parse HEAD 2>/dev/null)
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to get Git commit information" >&2
+    exit 1
+fi
+
+GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to get Git branch information" >&2
+    exit 1
+fi
+
+sed -i "s/%%GIT_COMMIT%%/${GIT_COMMIT}/g" ${DIST_OUTPUT}/${DIST_SPEC_FILE}
+sed -i "s|%%GIT_BRANCH%%|${GIT_BRANCH}|g" ${DIST_OUTPUT}/${DIST_SPEC_FILE}
+
 for changelog_file in $(ls ${DIST_CHANGELOG} | sort)
 do
     sed -i "/%changelog/r ${DIST_CHANGELOG}/${changelog_file}" ${DIST_OUTPUT}/${DIST_SPEC_FILE}
