7cabd63ec29cce44de19a4f6f47c6decc540a7d1
Jason Gunthorpe <jgg@nvidia.com>
2025-10-23 15:22:36 -0300

    iommu/vt-d: Use the generic iommu page table
    ANBZ: #26808
    
    commit d373449d8e97891434db0c64afca79d903c1194e upstream.
    
    Replace the VT-d iommu_domain implementation of the VT-d second stage and
    first stage page tables with the iommupt VTDSS and x86_64
    pagetables. x86_64 is shared with the AMD driver.
    
    There are a couple notable things in VT-d:
    - Like AMD the second stage format is not sign extended, unlike AMD it
      cannot decode a full 64 bits. The first stage format is a normal sign
      extended x86 page table
    - The HW caps can indicate how many levels, how many address bits and what
      leaf page sizes are supported in HW. As before the highest number of
      levels that can translate the entire supported address width is used.
      The supported page sizes are adjusted directly from the dedicated
      first/second stage cap bits.
    - VTD requires flushing 'write buffers'. This logic is left unchanged,
      the write buffer flushes on any gather flush or through iotlb_sync_map.
    - Like ARM, VTD has an optional non-coherent page table walker that
      requires cache flushing. This is supported through PT_FEAT_DMA_INCOHERENT
      the same as ARM, however x86 can't use the DMA API for flush, it must
      call the arch function clflush_cache_range()
    - The PT_FEAT_DYNAMIC_TOP can probably be supported on VT-d someday for the
      second stage when it uses 128 bit atomic stores for the HW context
      structures.
    - PT_FEAT_VTDSS_FORCE_WRITEABLE is used to work around ERRATA_772415_SPR17
    - A kernel command line parameter "sp_off" disables all page sizes except
      4k
    
    Remove all the unused iommu_domain page table code. The debugfs paths have
    their own independent page table walker that is left alone for now.
    
    This corrects a race with the non-coherent walker that the ARM
    implementations have fixed:
    
         CPU 0                               CPU 1
      pfn_to_dma_pte()                    pfn_to_dma_pte()
       pte = &parent[offset];
       if (!dma_pte_present(pte)) {
         try_cmpxchg64(&pte->val)
    					pte = &parent[offset];
    					.. dma_pte_present(pte) ..
    				        [...]
    					// iommu_map() completes
    					// Device does DMA
         domain_flush_cache(pte)
    
    The CPU 1 mapping operation shares a page table level with the CPU 0
    mapping operation. CPU 0 installed a new page table level but has not
    flushed it yet. CPU1 returns from iommu_map() and the device does DMA. The
    non coherent walker fails to see the new table level installed by CPU 0
    and fails the DMA with non-present.
    
    The iommupt PT_FEAT_DMA_INCOHERENT implementation uses the ARM design of
    storing a flag when CPU 0 completes the flush. If the flag is not set CPU
    1 will also flush to ensure the HW can fully walk to the PTE being
    installed.
    
    Cc: Tina Zhang <tina.zhang@intel.com>
    Reviewed-by: Lu Baolu <baolu.lu@linux.intel.com>
    Reviewed-by: Kevin Tian <kevin.tian@intel.com>
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>
    Signed-off-by: Joerg Roedel <joerg.roedel@amd.com>
    Signed-off-by: Zelin Deng <zelin.deng@linux.alibaba.com>
    Reviewed-by: Vasant Hegde <vasant.hegde@amd.com>
    Reviewed-by: Christophe Leroy <christophe.leroy@csgroup.eu>
    Reviewed-by: Randy Dunlap <rdunlap@infradead.org>
    Reviewed-by: Rae Moar <rmoar@google.com>
    Reviewed-by: David Gow <davidgow@google.com>
    Reviewed-by: Kees Cook <kees@kernel.org>
    Reviewed-by: Maxime Ripard <mripard@kernel.org>
    Reviewed-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Reviewed-by: Guanghui Feng <guanghuifeng@linux.alibaba.com>
    Reviewed-by: Shuai Xue <xueshuai@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6802

--- drivers/iommu/intel/pasid.c
+++ drivers/iommu/intel/pasid.c
@@ -444,10 +444,11 @@ int intel_pasid_setup_second_level(struct intel_iommu *iommu,
 				   struct dmar_domain *domain,
 				   struct device *dev, u32 pasid)
 {
+	struct pt_iommu_vtdss_hw_info pt_info;
 	struct pasid_entry *pte;
-	struct dma_pte *pgd;
-	u64 pgd_val;
 	u16 did;
+
+	pt_iommu_vtdss_hw_info(&domain->sspt, &pt_info);
 
 	/*
 	 * If hardware advertises no support for second level
@@ -459,8 +460,6 @@ int intel_pasid_setup_second_level(struct intel_iommu *iommu,
 		return -EINVAL;
 	}
 
-	pgd = domain->pgd;
-	pgd_val = virt_to_phys(pgd);
 	did = domain_id_iommu(domain, iommu);
 
 	spin_lock(&iommu->lock);
@@ -475,7 +474,7 @@ int intel_pasid_setup_second_level(struct intel_iommu *iommu,
 		return -EBUSY;
 	}
 
-	pasid_pte_config_second_level(iommu, pte, pgd_val, domain->agaw,
+	pasid_pte_config_second_level(iommu, pte, pt_info.ssptptr, pt_info.aw,
 				      did, domain->dirty_tracking);
 	spin_unlock(&iommu->lock);
 
@@ -625,9 +624,11 @@ static void pasid_pte_config_nestd(struct intel_iommu *iommu,
 				   struct dmar_domain *s2_domain,
 				   u16 did)
 {
-	struct dma_pte *pgd = s2_domain->pgd;
+	struct pt_iommu_vtdss_hw_info pt_info;
 
 	lockdep_assert_held(&iommu->lock);
+
+	pt_iommu_vtdss_hw_info(&s2_domain->sspt, &pt_info);
 
 	pasid_clear_entry(pte);
 
@@ -648,10 +649,10 @@ static void pasid_pte_config_nestd(struct intel_iommu *iommu,
 	if (s2_domain->force_snooping)
 		pasid_set_pgsnp(pte);
 
-	pasid_set_slptr(pte, virt_to_phys(pgd));
+	pasid_set_slptr(pte, pt_info.ssptptr);
 	pasid_set_fault_enable(pte);
 	pasid_set_domain_id(pte, did);
-	pasid_set_address_width(pte, s2_domain->agaw);
+	pasid_set_address_width(pte, pt_info.aw);
 	pasid_set_page_snoop(pte, !!ecap_smpwc(iommu->ecap));
 	if (s2_domain->dirty_tracking)
 		pasid_set_ssade(pte);
