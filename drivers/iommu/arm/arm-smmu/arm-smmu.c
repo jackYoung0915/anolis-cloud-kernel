d5f1d113c102df676ab2c86eb4687f9b8d90ed26
Nicolin Chen <nicolinc@nvidia.com>
2025-10-22 19:21:09 -0700

    iommu: Pass in old domain to attach_dev callback functions
    ANBZ: #31594
    
    commit fd714986e4e46effa6697b13d32918fc59608ccb upstream.
    
    The IOMMU core attaches each device to a default domain on probe(). Then,
    every new "attach" operation has a fundamental meaning of two-fold:
     - detach from its currently attached (old) domain
     - attach to a given new domain
    
    Modern IOMMU drivers following this pattern usually want to clean up the
    things related to the old domain, so they call iommu_get_domain_for_dev()
    to fetch the old domain.
    
    Pass in the old domain pointer from the core to drivers, aligning with the
    set_dev_pasid op that does so already.
    
    Ensure all low-level attach fcuntions in the core can forward the correct
    old domain pointer. Thus, rework those functions as well.
    
    Suggested-by: Jason Gunthorpe <jgg@nvidia.com>
    Reviewed-by: Kevin Tian <kevin.tian@intel.com>
    Reviewed-by: Jason Gunthorpe <jgg@nvidia.com>
    Signed-off-by: Nicolin Chen <nicolinc@nvidia.com>
    Reviewed-by: Lu Baolu <baolu.lu@linux.intel.com>
    Signed-off-by: Joerg Roedel <joerg.roedel@amd.com>
    Signed-off-by: Shuai Xue <xueshuai@linux.alibaba.com>
    Reviewed-by: Matthew Rosato <mjrosato@linux.ibm.com>
    Reviewed-by: Gerd Bayer <gbayer@linux.ibm.com>
    Reviewed-by: Farhan Ali <alifm@linux.ibm.com>
    Reviewed-by: Niklas Schnelle <schnelle@linux.ibm.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6624

--- drivers/iommu/arm/arm-smmu/arm-smmu.c
+++ drivers/iommu/arm/arm-smmu/arm-smmu.c
@@ -1163,7 +1163,8 @@ static void arm_smmu_master_install_s2crs(struct arm_smmu_master_cfg *cfg,
 	}
 }
 
-static int arm_smmu_attach_dev(struct iommu_domain *domain, struct device *dev)
+static int arm_smmu_attach_dev(struct iommu_domain *domain, struct device *dev,
+			       struct iommu_domain *old)
 {
 	struct arm_smmu_domain *smmu_domain = to_smmu_domain(domain);
 	struct iommu_fwspec *fwspec = dev_iommu_fwspec_get(dev);
@@ -1232,7 +1233,8 @@ static int arm_smmu_attach_dev_type(struct device *dev,
 }
 
 static int arm_smmu_attach_dev_identity(struct iommu_domain *domain,
-					struct device *dev)
+					struct device *dev,
+					struct iommu_domain *old)
 {
 	return arm_smmu_attach_dev_type(dev, S2CR_TYPE_BYPASS);
 }
@@ -1247,7 +1249,8 @@ static struct iommu_domain arm_smmu_identity_domain = {
 };
 
 static int arm_smmu_attach_dev_blocked(struct iommu_domain *domain,
-				       struct device *dev)
+				       struct device *dev,
+				       struct iommu_domain *old)
 {
 	return arm_smmu_attach_dev_type(dev, S2CR_TYPE_FAULT);
 }
