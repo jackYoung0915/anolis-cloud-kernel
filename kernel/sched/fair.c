726c4dea2265dc72c2b3e2945323612720d0d2e5
Cruz Zhao <CruzZhao@linux.alibaba.com>
2025-10-28 15:48:22 +0800

    anolis: sched: continue to pull tasks if we haven't pull non-idle tasks
    ANBZ: #26705
    
    For ID_LOAD_BALANCE, when we tried to pull non-idle tasks to idle only
    cpu, we should continue after load_balance(), rather than before.
    
    Fixes: 23f51bbec63a ("anolis: sched: introduce ID_LOAD_BALANCE")
    Signed-off-by: Cruz Zhao <CruzZhao@linux.alibaba.com>
    Reviewed-by: Tianchen Ding <dtcccc@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5922

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -13244,10 +13244,6 @@ static int newidle_balance(struct rq *this_rq, struct rq_flags *rf)
 
 		update_next_balance(sd, &next_balance);
 
-		/* We wanna pull non-idle tasks to idle task only cpu. */
-		if (sched_feat(ID_LOAD_BALANCE) && sched_idle_rq(this_rq) && !this_rq->pulled)
-			continue;
-
 		if (get_avg_idle(this_rq) < curr_cost + sd->max_newidle_lb_cost)
 			break;
 
@@ -13264,6 +13260,10 @@ static int newidle_balance(struct rq *this_rq, struct rq_flags *rf)
 			curr_cost += domain_cost;
 			t0 = t1;
 		}
+
+		/* We wanna pull non-idle tasks to idle task only cpu. */
+		if (sched_feat(ID_LOAD_BALANCE) && sched_idle_rq(this_rq) && !this_rq->pulled)
+			continue;
 
 		/*
 		 * Stop searching for tasks to pull if there are
