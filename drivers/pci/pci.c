d7849fe31dfbfc3505d31b7401cb127099cca0f7
leoliu-oc <leoliu-oc@zhaoxin.com>
2025-10-29 15:28:25 +0800

    anolis: PCI: Fix PCIe unplug Enumeration Long Blocking Issue
    ANBZ: #26812
    
    When a USB4 device is inserted, a PCIe tunnel will be established.
    Then, the PCIe hierarchy within the USB4 device will be enumerated.
    If the device is suddenly unplugged during the enumeration process,
    it is possible that the function pci_find_next_ext_capability called when
    the PCI driver scans the PCIe extended capability may take a very long
    time, perhaps more than 20 seconds.
    
    Signed-off-by: leoliu-oc <leoliu-oc@zhaoxin.com>
    Reviewed-by: Guixin Liu <kanie@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/5931

--- drivers/pci/pci.c
+++ drivers/pci/pci.c
@@ -591,6 +591,13 @@ u16 pci_find_next_ext_capability(struct pci_dev *dev, u16 start, int cap)
 		return 0;
 
 	while (ttl-- > 0) {
+#if defined(CONFIG_CPU_SUP_ZHAOXIN) || defined(CONFIG_CPU_SUP_CENTAUR)
+		if (boot_cpu_data.x86_vendor == X86_VENDOR_CENTAUR ||
+		    boot_cpu_data.x86_vendor == X86_VENDOR_ZHAOXIN)
+			if (header == 0xffffffff)
+				return 0;
+#endif
+
 		if (PCI_EXT_CAP_ID(header) == cap && pos != start)
 			return pos;
 
