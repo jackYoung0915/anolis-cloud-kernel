a972bff1f74d69e0d3771a5747178c80ecaa5d47
leoliu-oc <leoliu-oc@zhaoxin.com>
2025-10-30 14:25:08 +0800

    anolis: Add platform-specific quirks for ZX-200 device
    ANBZ: #26839
    
    Adds platform-specific handling for Zhaoxin ZX-200 in both AHCI and xHCI
    drivers.
    
    For non-Zhaoxin CPU platforms, it applies the following quirks:
    1. In AHCI driver (ahci.c):
       - Sets AHCI_HFLAG_32BIT_ONLY flag to restrict DMA operations to 32-bit
         addressing
    
    2. In xHCI driver (xhci-pci.c):
       - Sets XHCI_RESET_ON_RESUME quirk to handle device resume issues
       - Sets XHCI_NO_64BIT_SUPPORT quirk to disable 64-bit DMA addressing
    
    These changes ensure proper functionality and compatibility of Zhaoxin
    ZX-200 on third-party platforms.
    
    Reviewed-by: Tony W. Wang <tonywwang@zhaoxin.com>
    Tested-by: Lyle Li <lyleli@zhaoxin.com>
    Signed-off-by: leoliu-oc <leoliu-oc@zhaoxin.com>

--- drivers/usb/host/xhci-pci.c
+++ drivers/usb/host/xhci-pci.c
@@ -307,6 +307,16 @@ static int xhci_pci_reinit(struct xhci_hcd *xhci, struct pci_dev *pdev)
 	return 0;
 }
 
+static bool is_zhaoxin_cpu(void)
+{
+#ifdef CONFIG_X86
+	if (boot_cpu_data.x86_vendor == X86_VENDOR_ZHAOXIN ||
+	    boot_cpu_data.x86_vendor == X86_VENDOR_CENTAUR)
+		return true;
+#endif
+	return false;
+}
+
 static void xhci_pci_quirks(struct device *dev, struct xhci_hcd *xhci)
 {
 	struct pci_dev                  *pdev = to_pci_dev(dev);
@@ -574,6 +584,12 @@ static void xhci_pci_quirks(struct device *dev, struct xhci_hcd *xhci)
 
 		if (pdev->device == 0x9203)
 			xhci->quirks |= XHCI_TRB_OVERFETCH;
+	}
+
+	if (pdev->vendor == PCI_VENDOR_ID_ZHAOXIN && !is_zhaoxin_cpu()) {
+		xhci->quirks |= XHCI_NO_64BIT_SUPPORT;
+		if (pdev->device == 0x9203)
+			xhci->quirks |= XHCI_RESET_ON_RESUME;
 	}
 
 	if (pdev->vendor == PCI_DEVICE_ID_CADENCE &&
