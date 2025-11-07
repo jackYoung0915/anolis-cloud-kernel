b7907b0d15913df543b912bc16b28ebb92d1812e
lutong <lutong14@huawei.com>
2025-11-07 11:44:25 +0800

    KVM: arm64: Deactivate the spurious vtimer interrupt
    ANBZ: #32936
    
    commit 387b558fec9f8b0d3e0a4d48b5912301c1477bdd openEuler.
    
    when hypervisor receives a vtimer interrupt but ISTATUS is 0,
    kvm_timer_update_irq will not be executed to
    inject this interrupt into the VM.
    Since EOImode is 1 and the timer interrupt
    has IRQD_FORWARDED_TO_VCPU flag,
    hypervisor will not write ICC_DIR_EL1 to deactivate the interrupt.
    This interrupt remains in active state,
    blocking subsequent interrupt from being process.
    
    Fixes: 9e01dc76be6a ("KVM: arm/arm64: arch_timer: Assign the phys timer on VHE systems")
    Signed-off-by: Kunkun Jiang <jiangkunkun@huawei.com>
    Signed-off-by: Jia Qingtong <jiaqingtong@huawei.com>
    Signed-off-by: Chengfei Huang <huangchengfei3@huawei.com>
    Signed-off-by: Lutong <lutong14@huawei.com>
    [zhangxinghao: Resolved conflict in arch/arm64/kvm/arch_timer.c -
    adapted changes to devel-6.6 kernel version.]
    Signed-off-by: zhangxinghao <zhangxinghao@kunlunit.com>
    
    ---

--- arch/arm64/kvm/arch_timer.c
+++ arch/arm64/kvm/arch_timer.c
@@ -220,6 +220,14 @@ static void soft_timer_cancel(struct hrtimer *hrt)
 	hrtimer_cancel(hrt);
 }
 
+static inline void set_timer_irq_phys_active(struct arch_timer_context *ctx, bool active)
+{
+	int r;
+
+	r = irq_set_irqchip_state(ctx->host_timer_irq, IRQCHIP_STATE_ACTIVE, active);
+	WARN_ON(r);
+}
+
 static irqreturn_t kvm_arch_timer_handler(int irq, void *dev_id)
 {
 	struct kvm_vcpu *vcpu = *(struct kvm_vcpu **)dev_id;
@@ -244,6 +252,8 @@ static irqreturn_t kvm_arch_timer_handler(int irq, void *dev_id)
 
 	if (kvm_timer_should_fire(ctx))
 		kvm_timer_update_irq(vcpu, true, ctx);
+	else
+		set_timer_irq_phys_active(ctx, false);
 
 	if (userspace_irqchip(vcpu->kvm) &&
 	    !static_branch_unlikely(&has_gic_active_state))
@@ -640,13 +650,6 @@ static void timer_restore_state(struct arch_timer_context *ctx)
 	ctx->loaded = true;
 out:
 	local_irq_restore(flags);
-}
-
-static inline void set_timer_irq_phys_active(struct arch_timer_context *ctx, bool active)
-{
-	int r;
-	r = irq_set_irqchip_state(ctx->host_timer_irq, IRQCHIP_STATE_ACTIVE, active);
-	WARN_ON(r);
 }
 
 static void kvm_timer_vcpu_load_gic(struct arch_timer_context *ctx)
