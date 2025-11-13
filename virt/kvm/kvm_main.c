d51fa85c47fe65291bd23aaf37bc99eaa2d420a1
leizongkun <leizongkun@huawei.com>
2025-11-13 11:36:39 +0800

    kvm: add struct kvm if only CONFIG_KVM enabled
    ANBZ: #IJSBSS
    
    commit b5582ec504a9e8c7e0b005c72cacee9cae11580d openEuler.
    
    when CONFIG_ETMEM is N, the mm_struct has kvm element
    
    Signed-off-by: wangzhigang <wangzhigang17@huawei.com>
    Signed-off-by: zhangliang <zhangliang5@huawei.com>
    Signed-off-by: leizongkun <leizongkun@huawei.com>
    [frankyj915: Resolve conflicts in
    	include/linux/mm_types.h
    	virt/kvm/kvm_main.c]
    Signed-off-by: frankyj915 <yangjieyj.yang@huawei.com>

--- virt/kvm/kvm_main.c
+++ virt/kvm/kvm_main.c
@@ -1361,6 +1361,10 @@ static void kvm_destroy_vm(struct kvm *kvm)
 
 	kvm_destroy_pm_notifier(kvm);
 	kvm_uevent_notify_change(KVM_EVENT_DESTROY_VM, kvm);
+
+	if (mm->kvm == kvm)
+		mm->kvm = NULL;
+
 	kvm_destroy_vm_debugfs(kvm);
 	kvm_arch_sync_events(kvm);
 	mutex_lock(&kvm_lock);
@@ -5554,6 +5558,9 @@ static int kvm_dev_ioctl_create_vm(unsigned long type)
 		r = PTR_ERR(file);
 		goto put_kvm;
 	}
+
+	if (kvm->mm->kvm == NULL)
+		kvm->mm->kvm = kvm;
 
 	/*
 	 * Don't call kvm_put_kvm anymore at this point; file->f_op is
