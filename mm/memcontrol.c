d127067029b090ccf996b6d9a86b442fd52405ed
root <root@da-yanadou.byxa.com>
2025-11-14 13:19:19 +0800

    memcg: always call cond_resched() after fn()
    I am seeing soft lockup on certain machine types when a cgroup OOMs.  This
    is happening because killing the process in certain machine might be very
    slow, which causes the soft lockup and RCU stalls.  This happens usually
    when the cgroup has MANY processes and memory.oom.group is set.
    
    Example I am seeing in real production:
    
           [462012.244552] Memory cgroup out of memory: Killed process 3370438 (crosvm) ....
           ....
           [462037.318059] Memory cgroup out of memory: Killed process 4171372 (adb) ....
           [462037.348314] watchdog: BUG: soft lockup - CPU#64 stuck for 26s! [stat_manager-ag:1618982]
           ....
    
    Quick look at why this is so slow, it seems to be related to serial flush
    for certain machine types.  For all the crashes I saw, the target CPU was
    at console_flush_all().
    
    In the case above, there are thousands of processes in the cgroup, and it
    is soft locking up before it reaches the 1024 limit in the code (which
    would call the cond_resched()).  So, cond_resched() in 1024 blocks is not
    sufficient.
    
    Remove the counter-based conditional rescheduling logic and call
    cond_resched() unconditionally after each task iteration, after fn() is
    called.  This avoids the lockup independently of how slow fn() is.
    
    Link: https://lkml.kernel.org/r/20250523-memcg_fix-v1-1-ad3eafb60477@debian.org
    Fixes: ade81479c7dd ("memcg: fix soft lockup in the OOM process")
    Signed-off-by: Breno Leitao <leitao@debian.org>
    Suggested-by: Rik van Riel <riel@surriel.com>
    Acked-by: Shakeel Butt <shakeel.butt@linux.dev>
    Cc: Michael van der Westhuizen <rmikey@meta.com>
    Cc: Usama Arif <usamaarif642@gmail.com>
    Cc: Pavel Begunkov <asml.silence@gmail.com>
    Cc: Chen Ridong <chenridong@huawei.com>
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Michal Hocko <mhocko@kernel.org>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Muchun Song <muchun.song@linux.dev>
    Cc: Roman Gushchin <roman.gushchin@linux.dev>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -1525,7 +1525,6 @@ int mem_cgroup_scan_tasks(struct mem_cgroup *memcg,
 {
 	struct mem_cgroup *iter;
 	int ret = 0;
-	int i = 0;
 
 	for_each_mem_cgroup_tree(iter, memcg) {
 		struct css_task_iter it;
@@ -1533,10 +1532,9 @@ int mem_cgroup_scan_tasks(struct mem_cgroup *memcg,
 
 		css_task_iter_start(&iter->css, CSS_TASK_ITER_PROCS, &it);
 		while (!ret && (task = css_task_iter_next(&it))) {
-			/* Avoid potential softlockup warning */
-			if ((++i & 1023) == 0)
-				cond_resched();
 			ret = fn(task, arg);
+			/* Avoid potential softlockup warning */
+			cond_resched();
 		}
 		css_task_iter_end(&it);
 		if (ret) {
