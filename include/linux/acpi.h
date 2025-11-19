8fe7224ea0aacd0cdf4007a7a0e945919f94ed5b
James Morse <james.morse@arm.com>
2025-11-19 12:22:31 +0000

    ACPI / PPTT: Add a helper to fill a cpumask from a processor container
    ANBZ: #31060
    
    commit 796e29b857aed89f83f70f2c199585c45db5dc0f upstream.
    
    The ACPI MPAM table uses the UID of a processor container specified in
    the PPTT to indicate the subset of CPUs and cache topology that can
    access each MPAM System Component (MSC).
    
    This information is not directly useful to the kernel. The equivalent
    cpumask is needed instead.
    
    Add a helper to find the processor container by its id, then walk
    the possible CPUs to fill a cpumask with the CPUs that have this
    processor container as a parent.
    
    CC: Dave Martin <dave.martin@arm.com>
    Reviewed-by: Sudeep Holla <sudeep.holla@arm.com>
    Reviewed-by: Jonathan Cameron <jonathan.cameron@huawei.com>
    Reviewed-by: Fenghua Yu <fenghuay@nvidia.com>
    Reviewed-by: Gavin Shan <gshan@redhat.com>
    Reviewed-by: Shaopeng Tan <tan.shaopeng@jp.fujitsu.com>
    Reviewed-by: Hanjun Guo <guohanjun@huawei.com>
    Reviewed-by: Jeremy Linton <jeremy.linton@arm.com>
    Tested-by: Fenghua Yu <fenghuay@nvidia.com>
    Tested-by: Shaopeng Tan <tan.shaopeng@jp.fujitsu.com>
    Tested-by: Peter Newman <peternewman@google.com>
    Tested-by: Carl Worth <carl@os.amperecomputing.com>
    Tested-by: Gavin Shan <gshan@redhat.com>
    Tested-by: Zeng Heng <zengheng4@huawei.com>
    Tested-by: Hanjun Guo <guohanjun@huawei.com>
    Signed-off-by: James Morse <james.morse@arm.com>
    Signed-off-by: Ben Horgan <ben.horgan@arm.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Wei Chen <wei.chen@arm.com>
    Reviewed-by: Jiankang Chen <jkchen@linux.alibaba.com>
    Reviewed-by: Shuai Xue <xueshuai@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6702

--- include/linux/acpi.h
+++ include/linux/acpi.h
@@ -1504,6 +1504,7 @@ int find_acpi_cpu_topology(unsigned int cpu, int level);
 int find_acpi_cpu_topology_cluster(unsigned int cpu);
 int find_acpi_cpu_topology_package(unsigned int cpu);
 int find_acpi_cpu_topology_hetero_id(unsigned int cpu);
+void acpi_pptt_get_cpus_from_container(u32 acpi_cpu_id, cpumask_t *cpus);
 #else
 static inline int acpi_pptt_cpu_is_thread(unsigned int cpu)
 {
@@ -1525,6 +1526,8 @@ static inline int find_acpi_cpu_topology_hetero_id(unsigned int cpu)
 {
 	return -EINVAL;
 }
+static inline void acpi_pptt_get_cpus_from_container(u32 acpi_cpu_id,
+						     cpumask_t *cpus) { }
 #endif
 
 #ifdef CONFIG_ARM64
