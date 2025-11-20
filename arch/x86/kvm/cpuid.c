aae255440b4e468b52acbe711dd70378a45d56cf
Zhao Liu <zhao1.liu@intel.com>
2025-11-20 13:07:17 +0800

    KVM: x86: Advertise MOVRS CPUID to userspace
    ANBZ: #35251
    
    commit f24ef0093dd8cf60ed7f93a82fb16335fdcbc310 upstream.
    
    Define the feature flag for MOVRS and advertise support to userspace when
    the feature is supported by the host.
    
    MOVRS is a new set of instructions introduced in the Intel platform
    Diamond Rapids, to provide load instructions that carry a read-shared
    hint.
    
    Functionally, MOVRS family is equivalent to existing load instructions,
    but its read-shared hint indicates that the source memory location is
    likely to become read-shared by multiple processors, i.e., read in the
    future by at least one other processor before it is written (assuming it
    is ever written in the future). This hint could optimize the behavior of
    the caches, especially shared caches, for this data for future reads by
    multiple processors. Additionally, MOVRS family also includes a software
    prefetch instruction, PREFETCHRST2, that carries the same read-shared
    hint. [*]
    
    MOVRS family is enumerated by CPUID single-bit (0x7.0x1.EAX[bit 31]).
    Since it's on a densely-populated CPUID leaf and some other bits on
    this leaf have kernel usages, define this new feature in cpufeatures.h,
    but hide it in /proc/cpuinfo due to lack of current kernel usage.
    
    Advertise MOVRS bit to userspace directly. It's safe, since there's no
    new VMX controls or additional host enabling required for guests to use
    this feature.
    
    [*]: Intel Architecture Instruction Set Extensions and Future Features
         (rev.059).
    
    Intel-SIG: commit f24ef0093dd8 KVM: x86: Advertise MOVRS CPUID to userspace.
    DMR ISAs enalbing
    
    Tested-by: Xudong Hao <xudong.hao@intel.com>
    Signed-off-by: Zhao Liu <zhao1.liu@intel.com>
    Reviewed-by: Xiaoyao Li <xiaoyao.li@intel.com>
    Reviewed-by: Paolo Bonzini <pbonzini@redhat.com>
    Link: https://patch.msgid.link/20251120050720.931449-2-zhao1.liu@intel.com
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    [ Quanxian Wang: amend commit log ]
    Signed-off-by: Quanxian Wang <quanxian.wang@intel.com>
    Reviewed-by: Guixin Liu <kanie@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/7024

--- arch/x86/kvm/cpuid.c
+++ arch/x86/kvm/cpuid.c
@@ -693,7 +693,7 @@ void kvm_set_cpu_caps(void)
 	kvm_cpu_cap_init(CPUID_7_1_EAX,
 		F(SHA512) | F(SM3) | F(SM4) | F(AVX_VNNI) | F(AVX512_BF16) |
 		F(CMPCCXADD) | F(FZRM) | F(FSRS) | F(FSRC) | F(AMX_FP16) |
-		F(AVX_IFMA) | F(LAM)
+		F(AVX_IFMA) | F(LAM) | F(MOVRS)
 	);
 
 	kvm_cpu_cap_init_kvm_defined(CPUID_7_1_EDX,
