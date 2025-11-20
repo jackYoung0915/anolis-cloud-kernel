bd460456ad85ca7093b4e1d4a5f012af79d545fd
Zhao Liu <zhao1.liu@intel.com>
2025-11-20 13:07:20 +0800

    KVM: x86: Advertise AVX10_VNNI_INT CPUID to userspace
    ANBZ: #35251
    
    commit 062768f426895b958417c85582826e20c44f477c upstream.
    
    Define and advertise AVX10_VNNI_INT CPUID to userspace when it's supported
    by the host.
    
    AVX10_VNNI_INT (0x24.0x1.ECX[bit 2]) is a discrete feature bit
    introduced on Intel Diamond Rapids, which enumerates the support for
    EVEX VPDP* instructions for INT8/INT16 [*].
    
    Since this feature has no actual kernel usages, define it as a KVM-only
    feature in reverse_cpuid.h.
    
    Advertise new CPUID subleaf 0x24.0x1 with AVX10_VNNI_INT bit to
    userspace for guest use. It's safe since no additional enabling work
    is needed in the host kernel.
    
    [*]: Intel Advanced Vector Extensions 10.2 Architecture Specification
         (rev 5.0).
    
    Intel-SIG: commit 062768f42689 KVM: x86: Advertise AVX10_VNNI_INT CPUID to userspace.
    DMR ISAs enalbing
    
    Tested-by: Xudong Hao <xudong.hao@intel.com>
    Signed-off-by: Zhao Liu <zhao1.liu@intel.com>
    Reviewed-by: Xiaoyao Li <xiaoyao.li@intel.com>
    Reviewed-by: Paolo Bonzini <pbonzini@redhat.com>
    Link: https://patch.msgid.link/20251120050720.931449-5-zhao1.liu@intel.com
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    [ Quanxian Wang: amend commit log ]
    Signed-off-by: Quanxian Wang <quanxian.wang@intel.com>
    Reviewed-by: Guixin Liu <kanie@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/7024

--- arch/x86/kvm/cpuid.c
+++ arch/x86/kvm/cpuid.c
@@ -724,6 +724,10 @@ void kvm_set_cpu_caps(void)
 		F(AMX_MOVRS)
 	);
 
+	kvm_cpu_cap_init_kvm_defined(CPUID_24_1_ECX,
+		F(AVX10_VNNI_INT)
+	);
+
 	kvm_cpu_cap_init(CPUID_8000_0001_ECX,
 		F(LAHF_LM) | F(CMP_LEGACY) | 0 /*SVM*/ | 0 /* ExtApicSpace */ |
 		F(CR8_LEGACY) | F(ABM) | F(SSE4A) | F(MISALIGNSSE) |
@@ -1209,6 +1213,7 @@ static inline int __do_cpuid_func(struct kvm_cpuid_array *array, u32 function)
 			break;
 		}
 
+		max_idx = entry->eax = min(entry->eax, 1u);
 		/*
 		 * The AVX10 version is encoded in EBX[7:0].  Note, the version
 		 * is guaranteed to be >=1 if AVX10 is supported.  Note #2, the
@@ -1218,9 +1223,20 @@ static inline int __do_cpuid_func(struct kvm_cpuid_array *array, u32 function)
 		cpuid_entry_override(entry, CPUID_24_0_EBX);
 		entry->ebx |= avx10_version;
 
-		entry->eax = 0;
 		entry->ecx = 0;
 		entry->edx = 0;
+
+		/* KVM only supports up to 0x24.0x1, capped above via min(). */
+		if (max_idx >= 1) {
+			entry = do_host_cpuid(array, function, 1);
+			if (!entry)
+				goto out;
+
+			cpuid_entry_override(entry, CPUID_24_1_ECX);
+			entry->eax = 0;
+			entry->ebx = 0;
+			entry->edx = 0;
+		}
 		break;
 	}
 	case KVM_CPUID_SIGNATURE: {
