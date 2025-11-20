5694d5143ada1a906ea4c2dc2a909aee3a582e8f
Zhao Liu <zhao1.liu@intel.com>
2025-11-20 13:07:18 +0800

    KVM: x86: Advertise AMX CPUIDs in subleaf 0x1E.0x1 to userspace
    ANBZ: #35251
    
    commit 58cbaf64e6530fa37752ff1362129842ac94fee0 upstream.
    
    Define and advertise AMX CPUIDs (0x1E.0x1) to userspace when the leaf is
    supported by the host.
    
    Intel Diamond Rapids adds new AMX instructions to support new formats
    and memory operations [*], and introduces the CPUID subleaf 0x1E.0x1
    to centralize the discrete AMX feature bits within EAX.
    
    Since these AMX features have no actual kernel usages, define them as
    KVM-only features in reverse_cpuid.h.
    
    In addition to the new features, CPUID 0x1E.0x1.EAX[bits 0-3] are
    aliaseed positions of existing AMX feature bits distributed across the
    0x7 leaves. To avoid duplicate feature names, name these aliases with an
    *_ALIAS suffix, and define them in reverse_cpuid.h as KVM-only features
    as well.
    
    Advertise new CPUID subleaf 0x1E.0x1 with its AMX CPUID feature bits to
    userspace for guest use. It's safe since no additional enabling work
    is needed in the host kernel.
    
    [*]: Intel Architecture Instruction Set Extensions and Future Features
         (rev.059).
    
    Intel-SIG: commit 58cbaf64e653 KVM: x86: Advertise AMX CPUIDs in subleaf 0x1E.0x1 to userspace.
    DMR ISAs enalbing
    
    Tested-by: Xudong Hao <xudong.hao@intel.com>
    Signed-off-by: Zhao Liu <zhao1.liu@intel.com>
    Reviewed-by: Xiaoyao Li <xiaoyao.li@intel.com>
    Reviewed-by: Paolo Bonzini <pbonzini@redhat.com>
    Link: https://patch.msgid.link/20251120050720.931449-3-zhao1.liu@intel.com
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    [ Quanxian Wang: amend commit log ]
    Signed-off-by: Quanxian Wang <quanxian.wang@intel.com>

--- arch/x86/kvm/cpuid.c
+++ arch/x86/kvm/cpuid.c
@@ -716,6 +716,12 @@ void kvm_set_cpu_caps(void)
 		F(AVX10_128) | F(AVX10_256) | F(AVX10_512)
 	);
 
+	kvm_cpu_cap_init_kvm_defined(CPUID_1E_1_EAX,
+		F(AMX_INT8_ALIAS) | F(AMX_BF16_ALIAS) | F(AMX_COMPLEX_ALIAS) |
+		F(AMX_FP16_ALIAS) | F(AMX_FP8) | F(AMX_TF32) | F(AMX_AVX512) |
+		F(AMX_MOVRS)
+	);
+
 	kvm_cpu_cap_init(CPUID_8000_0001_ECX,
 		F(LAHF_LM) | F(CMP_LEGACY) | 0 /*SVM*/ | 0 /* ExtApicSpace */ |
 		F(CR8_LEGACY) | F(ABM) | F(SSE4A) | F(MISALIGNSSE) |
@@ -1172,6 +1178,20 @@ static inline int __do_cpuid_func(struct kvm_cpuid_array *array, u32 function)
 		if (!kvm_cpu_cap_has(X86_FEATURE_AMX_TILE)) {
 			entry->eax = entry->ebx = entry->ecx = entry->edx = 0;
 			break;
+		}
+
+		max_idx = entry->eax = min(entry->eax, 1u);
+
+		/* KVM only supports up to 0x1e.0x1, capped above via min(). */
+		if (max_idx >= 1) {
+			entry = do_host_cpuid(array, function, 1);
+			if (!entry)
+				goto out;
+
+			cpuid_entry_override(entry, CPUID_1E_1_EAX);
+			entry->ebx = 0;
+			entry->ecx = 0;
+			entry->edx = 0;
 		}
 		break;
 	case 0x24: {
