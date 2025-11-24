jackYoung0915/anolis-cloud-kernel

commit f2e6570f1a318006c6c1d056a834c7bdcbfb30d6

anolis: sched: support to set absolute_expel to task and cgroup
ANBZ: #7705

In certain scenarios, it is not necessary to set all highclass tasks as
absolute_expeller, so we support setting absolute_expeller for both
tasks and cgroups.
 - For tasks: Use the prctl syscall:
     prctl(PR_SET_IDENTITY, pid, ID_ABSOLUTE_EXPELLER, NULL, NULL)
 - For cgroups: echo 3 > /sys/fs/cgroup/$cgroup/cpu.bvt_warp_ns

It should be noted that enabling ID_ABSOLUTE_EXPEL is not required when
setting absolute_expeller for tasks or cgroups. If ID_ABSOLUTE_EXPEL is
enabled, however, all highclass tasks will automatically be set as
absolute_expeller.

Signed-off-by: Cruz Zhao <CruzZhao@linux.alibaba.com>
Reviewed-by: Tianchen Ding <dtcccc@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/5898

kernel/sched/fair.c
@@ -624,6 +624,7 @@ enum {
 	TYPE_NORMAL,
 	TYPE_LS,
 	TYPE_STRICT,
+	TYPE_MOST_STRICT,
 };
 
 static DEFINE_MUTEX(identity_mutex);
@@ -638,6 +639,18 @@ static inline bool is_highclass(struct sched_entity *se)
 static inline bool is_expeller(struct sched_entity *se)
 {
 	return test_identity(se, ID_SMT_EXPELLER);
+}
+
+static inline bool
+is_absolute_expeller(struct sched_entity *se)
+{
+	return test_identity(se, ID_ABSOLUTE_EXPELLER);
+}
+
+static inline bool
+task_is_absolute_expeller(struct task_struct *p)
+{
+	return p && test_identity(&p->se, ID_ABSOLUTE_EXPELLER);
 }
 
 static inline bool is_idle_saver(struct sched_entity *se)
@@ -681,6 +694,9 @@ static inline bool need_expel(int this_cpu, bool *expel_by_smt_sibling)
 	}
 #endif
 	if (sched_feat(ID_ABSOLUTE_EXPEL) && rq->nr_high_running)
+		return true;
+
+	if (rq->nr_absolute_expeller)
 		return true;
 
 	return false;
@@ -1171,6 +1187,10 @@ id_update_make_up(struct task_group *tg, struct rq *rq, struct cfs_rq *cfs_rq,
 
 	if (__is_underclass(se))
 		rq->nr_under_make_up += coefficient * cfs_rq->nr_tasks;
+
+	if (is_absolute_expeller(se))
+		rq->nr_absolute_expeller_make_up +=
+			coefficient * cfs_rq->nr_tasks;
 }
 
 static __always_inline void
@@ -1182,15 +1202,17 @@ id_commit_make_up(struct rq *rq, bool commit)
 	if (commit) {
 		rq->nr_high_running += rq->nr_high_make_up;
 		rq->nr_under_running += rq->nr_under_make_up;
+		rq->nr_absolute_expeller += rq->nr_absolute_expeller_make_up;
 	}
 
 	rq->nr_high_make_up = 0;
 	rq->nr_under_make_up = 0;
+	rq->nr_absolute_expeller_make_up = 0;
 }
 #endif
 
 static __always_inline void
-id_update_nr_running(struct task_group *tg, struct rq *rq, long delta)
+id_update_nr_running(struct task_group *tg, struct task_struct *p, struct rq *rq, long delta)
 {
 	struct sched_entity *se;
 
@@ -1207,6 +1229,17 @@ id_update_nr_running(struct task_group *tg, struct rq *rq, long delta)
 
 	if (__is_underclass(se))
 		rq->nr_under_running += delta;
+
+	if (is_absolute_expeller(se))
+		rq->nr_absolute_expeller += delta;
+
+	/*
+	 * If a task was set absolute_expeller by prctl syscall, count for it separately,
+	 * so that no errors will occur when both the task and the task group are set to
+	 * absolute_expeller at the same time.
+	 */
+	if (task_is_absolute_expeller(p))
+		rq->nr_absolute_expeller += delta > 0 ? 1 : -1;
 }
 
 static inline bool id_regard_as_idle(struct rq *rq)
@@ -1523,7 +1556,7 @@ static void __update_identity(struct task_group *tg, int flags)
 				__dequeue_entity(cfs_rq, se);
 			hierarchy_update_nr_expel_immune(se, -ei_delta);
 			if (!throttled)
-				id_update_nr_running(tg, rq, -delta);
+				id_update_nr_running(tg, NULL, rq, -delta);
 
 			update_curr(cfs_rq);
 			se->vruntime -= __id_min_vruntime(cfs_rq, se);
@@ -1538,7 +1571,7 @@ static void __update_identity(struct task_group *tg, int flags)
 				__enqueue_entity(cfs_rq, se);
 			hierarchy_update_nr_expel_immune(se, ei_delta);
 			if (!throttled)
-				id_update_nr_running(tg, rq, delta);
+				id_update_nr_running(tg, NULL, rq, delta);
 
 			update_min_vruntime(cfs_rq);
 		}
@@ -1592,6 +1625,8 @@ static void __update_task_identity(struct task_struct *p, int flags)
 			rq->nr_high_running--;
 		if (__is_underclass(se))
 			rq->nr_under_running--;
+		if (task_is_absolute_expeller(p))
+			rq->nr_absolute_expeller--;
 
 		update_curr(cfs_rq);
 		se->vruntime -= __id_min_vruntime(cfs_rq, se);
@@ -1609,6 +1644,8 @@ static void __update_task_identity(struct task_struct *p, int flags)
 			rq->nr_high_running++;
 		if (__is_underclass(se))
 			rq->nr_under_running++;
+		if (task_is_absolute_expeller(p))
+			rq->nr_absolute_expeller++;
 
 		update_min_vruntime(cfs_rq);
 	}
@@ -1661,6 +1698,9 @@ int update_bvt_warp_ns(struct task_group *tg, s64 val)
 	case TYPE_STRICT:
 		flags = ID_HIGHCLASS | ID_IDLE_SEEKER | ID_SMT_EXPELLER;
 		break;
+	case TYPE_MOST_STRICT:
+		flags = ID_HIGHCLASS | ID_IDLE_SEEKER | ID_SMT_EXPELLER | ID_ABSOLUTE_EXPELLER;
+		break;
 	default:
 		ret = -ERANGE;
 		goto unlock;
@@ -1695,8 +1735,10 @@ int update_identity(struct task_group *tg, struct task_struct *p, s64 val)
 
 	/*
 	 * Tasks stuck in root group can update their id_flags.
+	 * Tasks can be set and clear absolute_expeller.
 	 */
-	if (p && !(p->flags & PF_NO_SETAFFINITY))
+	if (p && !(p->flags & PF_NO_SETAFFINITY) &&
+	    val != ID_ABSOLUTE_EXPELLER && val != 0)
 		return -EINVAL;
 
 	mutex_lock(&identity_mutex);
@@ -1987,11 +2029,13 @@ id_entity_before(struct sched_entity *a, struct sched_entity *b)
 {
 	bool a_is_underclass = __is_underclass(a);
 	bool b_is_underclass = __is_underclass(b);
+	struct rq *rq = rq_of((cfs_rq_of(a)));
 
 	if (group_identity_disabled())
 		return entity_before(a, b);
 
-	if (sched_feat(ID_ABSOLUTE_EXPEL) && a_is_underclass != b_is_underclass)
+	if ((sched_feat(ID_ABSOLUTE_EXPEL) || rq->nr_absolute_expeller) &&
+	    a_is_underclass != b_is_underclass)
 		return b_is_underclass;
 
 	return (s64)(__id_vruntime(a) - __id_vruntime(b)) < 0;
@@ -2475,7 +2519,7 @@ id_commit_make_up(struct rq *rq, bool commit)
 #endif
 
 static __always_inline void
-id_update_nr_running(struct task_group *tg, struct rq *rq, long delta)
+id_update_nr_running(struct task_group *tg, struct task_struct *p, struct rq *rq, long delta)
 {
 }
 
@@ -8394,7 +8438,7 @@ enqueue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 
 	/* At this point se is NULL and we are at root level*/
 	add_nr_running(rq, 1);
-	id_update_nr_running(task_group(p), rq, 1);
+	id_update_nr_running(task_group(p), p, rq, 1);
 
 	/*
 	 * Since new tasks are assigned an initial util_avg equal to
@@ -8523,7 +8567,7 @@ static void dequeue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 
 	/* At this point se is NULL and we are at root level*/
 	sub_nr_running(rq, 1);
-	id_update_nr_running(task_group(p), rq, -1);
+	id_update_nr_running(task_group(p), p, rq, -1);
 
 	/* balance early to pull high priority tasks */
 	if (unlikely(!was_sched_idle && sched_idle_rq(rq)))

kernel/sched/sched.h
@@ -738,6 +738,7 @@ static inline bool group_identity_disabled(void);
 #define ID_SMT_EXPELLER		0x0004
 #define ID_IDLE_SAVER		0x0008
 #define ID_IDLE_SEEKER		0x0010
+#define ID_ABSOLUTE_EXPELLER	0x0020
 #define IDENTITY_FLAGS_MASK	0x00ff
 
 /*
@@ -1335,8 +1336,10 @@ struct rq {
 	unsigned int		nr_high_running;
 	unsigned int		nr_under_running;
 	unsigned int		nr_expel_immune;
+	unsigned int		nr_absolute_expeller;
 	long			nr_high_make_up;
 	long			nr_under_make_up;
+	long			nr_absolute_expeller_make_up;
 	bool			smt_expeller;
 	bool			smt_expellee;
 	bool			on_expel;

commit 328d59743d33ef2d8ab5d66bb8140fabb89f990e

anolis: sched: fix bugs of task absolute_expeller
ANBZ: #7705

Fix two bugs of task absolute_expeller:
 - When set absoltue_expeller to task in root task group,
   rq->nr_absolute_expeller will not increase.
 - When absolute_expeller task deads, the kernel will panic, because
   we set TASK_DEAD before clear_identity(), which will cause cond
   resched, and will cause stack release without dequeue task, and
   will panic when next schedule. To fix this problem, we call
   clear_identity() before set TASK_DEAD.

Fixes: f2e6570f1a31 ("anolis: sched: support to set absolute_expel to task and cgroup")
Fixes: 9824b30c896d ("anolis: sched: Introduce per task identity")
Signed-off-by: CruzZhao <CruzZhao@linux.alibaba.com>

kernel/sched/core.c
@@ -5775,13 +5775,13 @@ static void __sched notrace __schedule(bool preempt)
 
 void __noreturn do_task_dead(void)
 {
+	clear_task_identity(current);
+
 	/* Causes final put_task_struct in finish_task_switch(): */
 	set_special_state(TASK_DEAD);
 
 	/* Tell freezer to ignore us: */
 	current->flags |= PF_NOFREEZE;
-
-	clear_task_identity(current);
 
 	__schedule(false);
 	BUG();

kernel/sched/fair.c
@@ -1207,8 +1207,10 @@ id_update_nr_running(struct task_group *tg, struct task_struct *p, struct rq *rq
 	if (group_identity_disabled())
 		return;
 
-	if (!tg || tg == &root_task_group)
+	if (!tg)
 		return;
+	if (tg == &root_task_group)
+		goto task_absolute_expeller;
 
 	se = tg->se[rq->cpu];
 
@@ -1226,6 +1228,7 @@ id_update_nr_running(struct task_group *tg, struct task_struct *p, struct rq *rq
 	 * so that no errors will occur when both the task and the task group are set to
 	 * absolute_expeller at the same time.
 	 */
+task_absolute_expeller:
 	if (task_is_absolute_expeller(p))
 		rq->nr_absolute_expeller += delta > 0 ? 1 : -1;
 }