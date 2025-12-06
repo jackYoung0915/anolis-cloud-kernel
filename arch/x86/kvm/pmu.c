3dc168835b6f4c48a65197fd6e5f166f3d40cc03
Sean Christopherson <seanjc@google.com>
2025-12-05 16:17:10 -0800

    KVM: x86/pmu: Elide WRMSRs when loading guest PMCs if values already match
    ANBZ: #38211
    
    commit b0b6a8d3be16ea742bf835407e9968378c0c753c upstream.
    
    When loading a mediated PMU state, elide the WRMSRs to load PMCs with the
    guest's value if the value in hardware already matches the guest's value.
    For the relatively common case where neither the guest nor the host is
    actively using the PMU, i.e. when all/many counters are '0', eliding the
    WRMSRs reduces the latency of handling VM-Exit by a measurable amount
    (WRMSR is significantly more expensive than RDPMC).
    
    As measured by KVM-Unit-Tests' CPUID VM-Exit testcase, this provides a
    a ~25% reduction in latency (4k => 3k cycles) on Intel Emerald Rapids,
    and a ~13% reduction (6.2k => 5.3k cycles) on AMD Turin.
    
    Cc: Manali Shukla <manali.shukla@amd.com>
    Tested-by: Xudong Hao <xudong.hao@intel.com>
    Tested-by: Manali Shukla <manali.shukla@amd.com>
    Link: https://patch.msgid.link/20251206001720.468579-35-seanjc@google.com
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    [sandipan: backport to devel-6.6: use native_read_pmc() since this tree
     lacks the rdpmc() expression-form helper]
    Signed-off-by: Sandipan Das <sandipan.das@amd.com>
    Signed-off-by: fuwenxin <fuwenxin@linux.alibaba.com>
    Reviewed-by: Guanghui Feng <guanghuifeng@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/8329

--- arch/x86/kvm/pmu.c
+++ arch/x86/kvm/pmu.c
@@ -1171,13 +1171,15 @@ static void kvm_pmu_load_guest_pmcs(struct kvm_vcpu *vcpu)
 	for (i = 0; i < pmu->nr_arch_gp_counters; i++) {
 		pmc = &pmu->gp_counters[i];
 
-		wrmsrl(gp_counter_msr(i), pmc->counter);
+		if (pmc->counter != native_read_pmc(i))
+			wrmsrl(gp_counter_msr(i), pmc->counter);
 		wrmsrl(gp_eventsel_msr(i), pmc->eventsel_hw);
 	}
 	for (i = 0; i < pmu->nr_arch_fixed_counters; i++) {
 		pmc = &pmu->fixed_counters[i];
 
-		wrmsrl(fixed_counter_msr(i), pmc->counter);
+		if (pmc->counter != native_read_pmc(INTEL_PMC_FIXED_RDPMC_BASE | i))
+			wrmsrl(fixed_counter_msr(i), pmc->counter);
 	}
 }
 
