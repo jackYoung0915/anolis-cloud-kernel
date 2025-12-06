8f1e7590f023cfdcff77756b1f62ecc1f34c0f34
Kan Liang <kan.liang@linux.intel.com>
2025-12-05 16:16:40 -0800

    perf: Add APIs to create/release mediated guest vPMUs
    ANBZ: #38211
    
    commit eff95e170275d9e80b968f335cd03d0ac250d2d1 upstream.
    
    Currently, exposing PMU capabilities to a KVM guest is done by emulating
    guest PMCs via host perf events, i.e. by having KVM be "just" another user
    of perf.  As a result, the guest and host are effectively competing for
    resources, and emulating guest accesses to vPMU resources requires
    expensive actions (expensive relative to the native instruction).  The
    overhead and resource competition results in degraded guest performance
    and ultimately very poor vPMU accuracy.
    
    To address the issues with the perf-emulated vPMU, introduce a "mediated
    vPMU", where the data plane (PMCs and enable/disable knobs) is exposed
    directly to the guest, but the control plane (event selectors and access
    to fixed counters) is managed by KVM (via MSR interceptions).  To allow
    host perf usage of the PMU to (partially) co-exist with KVM/guest usage
    of the PMU, KVM and perf will coordinate to a world switch between host
    perf context and guest vPMU context near VM-Enter/VM-Exit.
    
    Add two exported APIs, perf_{create,release}_mediated_pmu(), to allow KVM
    to create and release a mediated PMU instance (per VM).  Because host perf
    context will be deactivated while the guest is running, mediated PMU usage
    will be mutually exclusive with perf analysis of the guest, i.e. perf
    events that do NOT exclude the guest will not behave as expected.
    
    To avoid silent failure of !exclude_guest perf events, disallow creating a
    mediated PMU if there are active !exclude_guest events, and on the perf
    side, disallowing creating new !exclude_guest perf events while there is
    at least one active mediated PMU.
    
    Exempt PMU resources that do not support mediated PMU usage, i.e. that are
    outside the scope/view of KVM's vPMU and will not be swapped out while the
    guest is running.
    
    Guard mediated PMU with a new kconfig to help readers identify code paths
    that are unique to mediated PMU support, and to allow for adding arch-
    specific hooks without stubs.  KVM x86 is expected to be the only KVM
    architecture to support a mediated PMU in the near future (e.g. arm64 is
    trending toward a partitioned PMU implementation), and KVM x86 will select
    PERF_GUEST_MEDIATED_PMU unconditionally, i.e. won't need stubs.
    
    Immediately select PERF_GUEST_MEDIATED_PMU when KVM x86 is enabled so that
    all paths are compile tested.  Full KVM support is on its way...
    
    [sean: add kconfig and WARNing, rewrite changelog, swizzle patch ordering]
    Suggested-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: Kan Liang <kan.liang@linux.intel.com>
    Signed-off-by: Mingwei Zhang <mizhang@google.com>
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Tested-by: Xudong Hao <xudong.hao@intel.com>
    Link: https://patch.msgid.link/20251206001720.468579-5-seanjc@google.com
    [sandipan: backport to devel-6.6: no __free_event() in v6.6; placed
     mediated_pmu_unaccount_event() call in _free_event() instead, and
     used goto err_callchain_buffer on mediated_pmu_account_event() failure
     since the error path ends in free_event_rcu() which handles security
     cleanup; PERF_PMU_CAP_MEDIATED_VPMU assigned 0x0800 matching upstream
     despite gaps at 0x0200/0x0400 which are not defined in v6.6]
    Signed-off-by: Sandipan Das <sandipan.das@amd.com>
    Signed-off-by: fuwenxin <fuwenxin@linux.alibaba.com>
    Reviewed-by: Guanghui Feng <guanghuifeng@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/8329

--- include/linux/perf_event.h
+++ include/linux/perf_event.h
@@ -293,6 +293,7 @@ struct perf_event_pmu_context;
 #define PERF_PMU_CAP_NO_EXCLUDE			0x0040
 #define PERF_PMU_CAP_AUX_OUTPUT			0x0080
 #define PERF_PMU_CAP_EXTENDED_HW_TYPE		0x0100
+#define PERF_PMU_CAP_MEDIATED_VPMU		0x0800
 
 /**
  * pmu::scope
@@ -1792,6 +1793,12 @@ extern void perf_event_task_tick(void);
 extern int perf_event_account_interrupt(struct perf_event *event);
 extern int perf_event_period(struct perf_event *event, u64 value);
 extern u64 perf_event_pause(struct perf_event *event, bool reset);
+
+#ifdef CONFIG_PERF_GUEST_MEDIATED_PMU
+int perf_create_mediated_pmu(void);
+void perf_release_mediated_pmu(void);
+#endif
+
 #else /* !CONFIG_PERF_EVENTS: */
 static inline void *
 perf_aux_output_begin(struct perf_output_handle *handle,
