e973f893b1cf564d79a72b5ed0dbfd2709bd1e93
D. Wythe <alibuda@linux.alibaba.com>
2025-12-15 18:41:56 +0800

    net/smc: fix hang in smc_smcr_terminate_all due to lgr leak
    When an incoming smc sock is killed due to an external exception before
    being accepted by the server, the socket is silently destroyed within
    smc_accept_dequeue() without notification to the user space.
    
    However, smc_conn_free() is not invoked during this process, resulting in
    the leakage of lgr and link. Consequently, when attempting to unload the
    SMC module, the process hangs indefinitely, waiting for the lgrs_deleted
    event that will never be triggered[0].
    
    [0]
     NET: Unregistered PF_SMC protocol family
     INFO: task rmmod:14414 blocked for more than 120 seconds.
     "echo 0 > /proc/sys/kernel/hung_task_timeout_secs" disables this message.
     Call Trace:
      <TASK>
      __schedule+0x34d/0x9e0
      schedule+0x5a/0xd0
      smc_smcr_terminate_all+0x2ec/0x350 [smc]
      ? cpuusage_read+0x10/0x10
      smc_lgrs_shutdown+0x96/0xe0 [smc]
      smc_exit+0x31/0xa3 [smc]
      __do_sys_delete_module+0x1d2/0x320
      do_syscall_64+0x55/0x80
      ? clear_bhb_loop+0x15/0x70
      ? clear_bhb_loop+0x15/0x70
      ? clear_bhb_loop+0x15/0x70
      ? clear_bhb_loop+0x15/0x70
      entry_SYSCALL_64_after_hwframe+0x6e/0xd8
     RIP: 0033:0x7fb7d73c70cb
     RSP: 002b:00007ffd55a178b8 EFLAGS: 00000206 ORIG_RAX: 00000000000000b0
     RAX: ffffffffffffffda RBX: 000055bf27a2e7f0 RCX: 00007fb7d73c70cb
     RDX: 000000000000000a RSI: 0000000000000800 RDI: 000055bf27a2e858
     RBP: 0000000000000000 R08: 0000000000000000 R09: 0000000000000000
     R10: 00007fb7d750fac0 R11: 0000000000000206 R12: 00007ffd55a17ae0
     R13: 00007ffd55a18d91 R14: 000055bf27a2e2a0 R15: 000055bf27a2e7f0
      </TASK>
    
    Fixes: 127f49705823 ("net/smc: release clcsock from tcp_listen_worker")
    Signed-off-by: D. Wythe <alibuda@linux.alibaba.com>

--- net/smc/af_smc.c
+++ net/smc/af_smc.c
@@ -1942,6 +1942,8 @@ struct sock *smc_accept_dequeue(struct sock *parent,
 				sock_release(isk->clcsock);
 				isk->clcsock = NULL;
 			}
+			if (!isk->use_fallback)
+				smc_conn_free(&isk->conn);
 			sock_put(new_sk); /* final */
 			continue;
 		}
