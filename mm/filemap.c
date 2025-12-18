intel-lab-lkp/linux

mm/filemap: unify read/write dropbehind naming
The read side is filemap_end_dropbehind_read(), while the write side
used folio_ as the prefix rather than filemap_. The read side makes more
sense, unify the naming such that the write side follows that.

Signed-off-by: Jens Axboe <axboe@kernel.dk>

mm/filemap.c
@@ -1604,7 +1604,7 @@ static void filemap_end_dropbehind(struct folio *folio)
  * completes. Do that now. If we fail, it's likely because of a big folio -
  * just reset dropbehind for that case and latter completions should invalidate.
  */
-static void folio_end_dropbehind_write(struct folio *folio)
+static void filemap_end_dropbehind_write(struct folio *folio)
 {
 	/*
 	 * Hitting !in_task() should not happen off RWF_DONTCACHE writeback,
@@ -1659,7 +1659,7 @@ void folio_end_writeback(struct folio *folio)
 	acct_reclaim_writeback(folio);
 
 	if (folio_dropbehind)
-		folio_end_dropbehind_write(folio);
+		filemap_end_dropbehind_write(folio);
 	folio_put(folio);
 }
 EXPORT_SYMBOL(folio_end_writeback);

jackYoung0915/anolis-cloud-kernel

ANBZ: #27274
commit 1da7a06d9ce4edea3370945af8bfcc71b7744788 upstream.
Link: https://lore.kernel.org/20250527133255.452431-5-axboe@kernel.dk
Signed-off-by: Christian Brauner <brauner@kernel.org>
Signed-off-by: Weilin Tong <tongweilin@linux.alibaba.com>
Acked-by: Joseph Qi <joseph.qi@linux.alibaba.com>
Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/6111

mm/filemap.c
@@ -1560,7 +1560,7 @@ static void filemap_end_dropbehind(struct page *page)
  * completes. Do that now. If we fail, it's likely because of a big page -
  * just reset dropbehind for that case and latter completions should invalidate.
  */
-static void page_end_dropbehind_write(struct page *page)
+static void filemap_end_dropbehind_write(struct page *page)
 {
 	/*
 	 * Hitting !in_task() should not happen off RWF_DONTCACHE writeback,
@@ -1612,7 +1612,7 @@ void end_page_writeback(struct page *page)
 	wake_up_page(page, PG_writeback);
 
 	if (page_dropbehind)
-		page_end_dropbehind_write(page);
+		filemap_end_dropbehind_write(page);
 	put_page(page);
 }
 EXPORT_SYMBOL(end_page_writeback);