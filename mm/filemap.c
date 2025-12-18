intel-lab-lkp/linux

mm/filemap: use filemap_end_dropbehind() for read invalidation
Use the filemap_end_dropbehind() helper rather than calling
folio_unmap_invalidate() directly, as we need to check if the folio has
been redirtied or marked for writeback once the folio lock has been
re-acquired.

Cc: stable@vger.kernel.org
Reported-by: Trond Myklebust <trondmy@hammerspace.com>
Fixes: 8026e49bff9b ("mm/filemap: add read support for RWF_DONTCACHE")
Link: https://lore.kernel.org/linux-fsdevel/ba8a9805331ce258a622feaca266b163db681a10.camel@hammerspace.com/
Signed-off-by: Jens Axboe <axboe@kernel.dk>

mm/filemap.c
@@ -2644,8 +2644,7 @@ static inline bool pos_same_folio(loff_t pos1, loff_t pos2, struct folio *folio)
 	return (pos1 >> shift == pos2 >> shift);
 }
 
-static void filemap_end_dropbehind_read(struct address_space *mapping,
-					struct folio *folio)
+static void filemap_end_dropbehind_read(struct folio *folio)
 {
 	if (!folio_test_dropbehind(folio))
 		return;
@@ -2653,7 +2652,7 @@ static void filemap_end_dropbehind_read(struct address_space *mapping,
 		return;
 	if (folio_trylock(folio)) {
 		if (folio_test_clear_dropbehind(folio))
-			folio_unmap_invalidate(mapping, folio, 0);
+			filemap_end_dropbehind(folio);
 		folio_unlock(folio);
 	}
 }
@@ -2774,7 +2773,7 @@ put_folios:
 		for (i = 0; i < folio_batch_count(&fbatch); i++) {
 			struct folio *folio = fbatch.folios[i];
 
-			filemap_end_dropbehind_read(mapping, folio);
+			filemap_end_dropbehind_read(folio);
 			folio_put(folio);
 		}
 		folio_batch_init(&fbatch);

jackYoung0915/anolis-cloud-kernel

ANBZ: #27274
commit 25b065a744ff0c1099bb357be1c40030b5a14c07 upstream.
Link: https://lore.kernel.org/20250527133255.452431-3-axboe@kernel.dk
Signed-off-by: Christian Brauner <brauner@kernel.org>
Signed-off-by: Weilin Tong <tongweilin@linux.alibaba.com>
Acked-by: Joseph Qi <joseph.qi@linux.alibaba.com>
Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/6111

mm/filemap.c
@@ -2559,8 +2559,7 @@ static inline bool pos_same_page(loff_t pos1, loff_t pos2, struct page *page)
 }
 
 
-static void filemap_end_dropbehind_read(struct address_space *mapping,
-					struct page *page)
+static void filemap_end_dropbehind_read(struct page *page)
 {
 	if (!PageDropbehind(page))
 		return;
@@ -2568,7 +2567,7 @@ static void filemap_end_dropbehind_read(struct address_space *mapping,
 		return;
 	if (trylock_page(page)) {
 		if (TestClearPageDropbehind(page))
-			page_unmap_invalidate(mapping, page, 0);
+			filemap_end_dropbehind(page);
 		unlock_page(page);
 	}
 }
@@ -2699,7 +2698,7 @@ put_pages:
 		for (i = 0; i < pg_nr; i++) {
 			struct page *page = pages[i];
 
-			filemap_end_dropbehind_read(mapping, page);
+			filemap_end_dropbehind_read(page);
 			put_page(page);
 		}
 	} while (iov_iter_count(iter) && iocb->ki_pos < isize && !error);