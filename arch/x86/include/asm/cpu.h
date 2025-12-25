2e18255ef0fcdde94181b21ea221cc332871cdf4
leoliu-oc <leoliu-oc@zhaoxin.com>
2025-12-25 15:53:19 +0800

    x86/split-lock: Remove unused TIF_SLD bit
    ANBZ: #28521
    
    commit ef79970d7ccdc4e8855aa6079fc2f4797a6807fb upstream.
    
    Changes to the "warn" mode of split lock handling mean that TIF_SLD is
    never set.
    
    Remove the bit, and the functions that use it.
    
    Signed-off-by: Tony Luck <tony.luck@intel.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Link: https://lore.kernel.org/r/20220310204854.31752-3-tony.luck@intel.com
    Signed-off-by: Lyle Li <lyleli@zhaoxin.com>
    Signed-off-by: leoliu-oc <leoliu-oc@zhaoxin.com>

--- arch/x86/include/asm/cpu.h
+++ arch/x86/include/asm/cpu.h
@@ -49,7 +49,6 @@ unsigned int x86_model(unsigned int sig);
 unsigned int x86_stepping(unsigned int sig);
 #ifdef CONFIG_CPU_SUP_INTEL
 extern void __init sld_setup(struct cpuinfo_x86 *c);
-extern void switch_to_sld(unsigned long tifn);
 extern bool handle_user_split_lock(struct pt_regs *regs, long error_code);
 extern bool handle_guest_split_lock(unsigned long ip);
 extern void handle_bus_lock(struct pt_regs *regs);
@@ -58,7 +57,6 @@ extern u64 tsx_ctrl_clear(void);
 extern void tsx_ctrl_restore(u64 tsx_ctrl);
 #else
 static inline void __init sld_setup(struct cpuinfo_x86 *c) {}
-static inline void switch_to_sld(unsigned long tifn) {}
 static inline bool handle_user_split_lock(struct pt_regs *regs, long error_code)
 {
 	return false;
