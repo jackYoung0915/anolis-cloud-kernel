e5addbad2b218c5c2b4325dc15ef7cc4f7d1fd59
LeoLiu-oc <leoliu-oc@zhaoxin.com>
2025-12-25 16:40:47 +0800

    anolis: pinctrl: Zhaoxin: Add KH-50000 support
    ANBZ: #28524
    
    This patch adds GPIO and pinctrl driver support for the Zhaoxin KH-50000
    chipset. It includes:
    
    A new driver file `pinctrl-kh50000.c` which defines the pin
    configuration and GPIO functionality for the KH-50000 platform, covering a
    wide range of pins from general-purpose GPIOs to specialized functions
    like USB, SMBus, and LPC.
    
    Updates to `Kconfig` to introduce the `PINCTRL_KH50000` configuration
    option, enabling build support for the new driver.
    
    Modifications to `Makefile` to compile the new `pinctrl-kh50000.o` module
    when `CONFIG_PINCTRL_KH50000` is enabled.
    
    Enhancements to `pinctrl-zhaoxin.h` to introduce new macros,
    data structures, and enumerations necessary for supporting the KH-50000's
    specific pin configurations and GPIO types.
    
    Refinements in `pinctrl-zhaoxin.c` to improve the generic Zhaoxin
    pinctrl/GPIO driver, including updates to interrupt handling and probe
    functions to accommodate the new hardware.
    
    Improvements to `pinctrl-kx7000.c` to enhance code quality and
    maintainability.
    
    This change enables full GPIO functionality support for the Zhaoxin
    KH-50000 platform.
    
    Signed-off-by: LeoLiu-oc <leoliu-oc@zhaoxin.com>

--- drivers/pinctrl/zhaoxin/pinctrl-zhaoxin.h
+++ drivers/pinctrl/zhaoxin/pinctrl-zhaoxin.h
@@ -1,7 +1,8 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
+
 /*
  * Zhaoxin pinctrl common code
- * Copyright(c) 2023 Shanghai Zhaoxin Corporation. All rights reserved.
+ * Copyright(c) 2021 Shanghai Zhaoxin Corporation. All rights reserved.
  */
 
 #ifndef PINCTRL_zhaoxin_H
@@ -18,8 +19,45 @@
 
 struct platform_device;
 struct device;
+struct zhaoxin_pinctrl;
 
-/**
+#define PMIO_RX90 100
+#define PMIO_RX8C 200
+
+#define ZX_CAL_ARRAY(a, b)		\
+	{				\
+		.pmio_offset = (a),	\
+		.size = (b),		\
+	}
+
+#define ZX_CAL_INDEX_ARRAY(a, b, c)		\
+	{					\
+		.reg_port_base = (PMIO_RX90),	\
+		.reg_data_base = (PMIO_RX8C),	\
+		.index = (a),			\
+		.cal_array = (b),		\
+		.size = (c),			\
+	}
+
+#define ZX_CAL_INDEX_ARRAY_MASK(a, b, c, d, e)	\
+	{					\
+		.reg_port_base = (PMIO_RX90),	\
+		.reg_data_base = (PMIO_RX8C),	\
+		.index = (a),			\
+		.cal_array = (b),		\
+		.size = (c),			\
+		.bits_num = (d),		\
+		.mask = (e),			\
+	}
+
+#define ZHAOXIN_GPP(s, e, g)					\
+	{							\
+		.zhaoxin_range_pin_base = (s),			\
+		.zhaoxin_range_pin_size = ((e) - (s) + 1),	\
+		.zhaoxin_range_gpio_base = (g),			\
+	}
+
+/*
  * struct zhaoxin_pingroup pin define
  */
 struct zhaoxin_pingroup {
@@ -30,12 +68,12 @@ struct zhaoxin_pingroup {
 	const unsigned int *modes;
 };
 
-/**
+/*
  * struct zhaoxin_function
  */
 struct zhaoxin_function {
 	const char *name;
-	const char * const *groups;
+	const char *const *groups;
 	size_t ngroups;
 };
 
@@ -46,13 +84,12 @@ struct zhaoxin_function {
  * @zhaoxin_range_gpio_base
  */
 struct zhaoxin_pin_map2_gpio {
-//	unsigned int barno;
 	unsigned int zhaoxin_range_pin_base;
 	unsigned int zhaoxin_range_pin_size;
 	int zhaoxin_range_gpio_base;
 };
 
-#define MAX_GPIO		256
+#define MAX_GPIO 256
 
 struct reg_cal_array {
 	int pmio_offset;
@@ -64,6 +101,7 @@ struct reg_calibrate {
 	const int reg_cal_size;
 	const int *cal_array;
 	const int size;
+	bool is_pmio;
 };
 
 struct index_cal_array {
@@ -72,6 +110,8 @@ struct index_cal_array {
 	int index;
 	int *cal_array;
 	int size;
+	int bits_num;
+	u16 mask;
 };
 
 struct zhaoxin_pin_topology {
@@ -84,13 +124,19 @@ struct zhaoxin_pin_topology {
 	const struct index_cal_array *trigger_cal;
 };
 
-#define TRIGGER_FALL_EDGE	0
-#define TRIGGER_RISE_EDGE	1
-#define TRIGGER_BOTH_EDGE	2
-#define TRIGGER_LOW_LEVEL	3
-#define TRIGGER_HIGH_LEVEL	4
+#define TRIGGER_FALL_EDGE 0
+#define TRIGGER_RISE_EDGE 1
+#define TRIGGER_BOTH_EDGE 2
+#define TRIGGER_LOW_LEVEL 3
+#define TRIGGER_HIGH_LEVEL 4
 
-#define	ZHAOXIN_GPIO_BASE_NOMAP -1
+#define ZHAOXIN_GPIO_BASE_NOMAP -1
+
+enum zx_gpio_type {
+	ZX_TYPE_ERROR = 0,
+	ZX_TYPE_GPIO,
+	ZX_TYPE_PGPIO,
+};
 
 struct zhaoxin_pinctrl_soc_data {
 	const char *uid;
@@ -101,12 +147,14 @@ struct zhaoxin_pinctrl_soc_data {
 	const struct zhaoxin_function *functions;
 	size_t nfunctions;
 	const struct zhaoxin_pin_topology *pin_topologys;
+
+	enum zx_gpio_type (*gpio_type)(struct zhaoxin_pinctrl *pctrl, unsigned int pin);
+
+	void (*private_init)(struct zhaoxin_pinctrl *pctrl);
+
 	const struct zhaoxin_pin_map2_gpio *zhaoxin_pin_maps;
 	size_t pin_map_size;
 };
-
-const struct zhaoxin_pinctrl_soc_data *
-	zhaoxin_pinctrl_get_soc_data(struct platform_device *pdev);
 
 struct zhaoxin_pinctrl {
 	struct device *dev;
@@ -118,6 +166,10 @@ struct zhaoxin_pinctrl {
 	const struct zhaoxin_pinctrl_soc_data *soc;
 	const struct zhaoxin_pin_topology *pin_topologys;
 	struct zhaoxin_pin_map2_gpio *pin_maps;
+
+	enum zx_gpio_type (*gpio_type)(struct zhaoxin_pinctrl *pctrl, unsigned int pin);
+
+	void (*private_init)(struct zhaoxin_pinctrl *pctrl);
 	size_t pin_map_size;
 	int irq;
 	int pmio_base;
@@ -125,6 +177,10 @@ struct zhaoxin_pinctrl {
 	int pmio_rx90;
 	int pmio_rx8c;
 };
+
+const struct zhaoxin_pinctrl_soc_data *zhaoxin_pinctrl_get_soc_data(struct platform_device *pdev);
+void zx_pad_write16(struct zhaoxin_pinctrl *pctrl, u8 index, u16 value);
+u16 zx_pad_read16(struct zhaoxin_pinctrl *pctrl, u8 index);
 
 int zhaoxin_pinctrl_probe_by_hid(struct platform_device *pdev);
 int zhaoxin_pinctrl_probe_by_uid(struct platform_device *pdev);
