6975040a02aa4cd3a7b6458f51890ef719599871
Yanjing Zhou <zhouyanjing@hygon.cn>
2025-12-26 03:56:41 +0000

    anolis: KVM: selftests: Add support for Hygon processors
    ANBZ: #28798
    
    Avoid failures on Hygon processors due to
    reserved HyperTransport region and hypercall.
    
    Hygon-SIG: commit none anolis: KVM: selftests:
    Add support for Hygon processors
    
    Signed-off-by: Yanjing Zhou <zhouyanjing@hygon.cn>
    Cc: hygon-arch@list.openanolis.cn
    Reviewed-by: Xiaochen Shen <shenxiaochen@hygon.cn>
    Reviewed-by: Bin Guo <guobin@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6284

--- tools/testing/selftests/kvm/x86_64/pmu_event_filter_test.c
+++ tools/testing/selftests/kvm/x86_64/pmu_event_filter_test.c
@@ -445,6 +445,11 @@ static bool use_amd_pmu(void)
 		 is_zen3(family, model));
 }
 
+static bool use_hygon_pmu(void)
+{
+	return host_cpu_is_hygon;
+}
+
 /*
  * "MEM_INST_RETIRED.ALL_LOADS", "MEM_INST_RETIRED.ALL_STORES", and
  * "MEM_INST_RETIRED.ANY" from https://perfmon-events.intel.com/
@@ -924,7 +929,7 @@ int main(int argc, char *argv[])
 	TEST_REQUIRE(kvm_has_cap(KVM_CAP_PMU_EVENT_FILTER));
 	TEST_REQUIRE(kvm_has_cap(KVM_CAP_PMU_EVENT_MASKED_EVENTS));
 
-	TEST_REQUIRE(use_intel_pmu() || use_amd_pmu());
+	TEST_REQUIRE(use_intel_pmu() || use_amd_pmu() || use_hygon_pmu());
 	guest_code = use_intel_pmu() ? intel_guest_code : amd_guest_code;
 
 	vm = vm_create_with_one_vcpu(&vcpu, guest_code);
@@ -934,7 +939,7 @@ int main(int argc, char *argv[])
 
 	TEST_REQUIRE(sanity_check_pmu(vcpu));
 
-	if (use_amd_pmu())
+	if (use_amd_pmu() || use_hygon_pmu())
 		test_amd_deny_list(vcpu);
 
 	test_without_filter(vcpu);
@@ -947,7 +952,7 @@ int main(int argc, char *argv[])
 	    supports_event_mem_inst_retired() &&
 	    kvm_cpu_property(X86_PROPERTY_PMU_NR_GP_COUNTERS) >= 3)
 		vcpu2 = vm_vcpu_add(vm, 2, intel_masked_events_guest_code);
-	else if (use_amd_pmu())
+	else if (use_amd_pmu() || use_hygon_pmu())
 		vcpu2 = vm_vcpu_add(vm, 2, amd_masked_events_guest_code);
 
 	if (vcpu2)
