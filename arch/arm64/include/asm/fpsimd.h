dd4cf324d0138885e39e334cf05b48d79e0ba8f8
Mark Brown <broonie@kernel.org>
2026-01-07 03:20:50 +0000

    arm64/fpsimd: Support FEAT_FPMR
    ANBZ: #29815
    
    [ Upstream commit 203f2b95a882dc46dd9873562167db69a1f61711 ]
    
    FEAT_FPMR defines a new EL0 accessible register FPMR use to configure the
    FP8 related features added to the architecture at the same time. Detect
    support for this register and context switch it for EL0 when present.
    
    Due to the sharing of responsibility for saving floating point state
    between the host kernel and KVM FP8 support is not yet implemented in KVM
    and a stub similar to that used for SVCR is provided for FPMR in order to
    avoid bisection issues. To make it easier to share host state with the
    hypervisor we store FPMR as a hardened usercopy field in uw (along with
    some padding).
    
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Link: https://lore.kernel.org/r/20240306-arm64-2023-dpisa-v5-3-c568edc8ed7f@kernel.org
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Jia He <justin.he@arm.com>

--- arch/arm64/include/asm/fpsimd.h
+++ arch/arm64/include/asm/fpsimd.h
@@ -64,6 +64,7 @@ struct cpu_fp_state {
 	void *sve_state;
 	void *sme_state;
 	u64 *svcr;
+	u64 *fpmr;
 	unsigned int sve_vl;
 	unsigned int sme_vl;
 	enum fp_type *fp_type;
@@ -130,6 +131,7 @@ extern void sve_kernel_enable(const struct arm64_cpu_capabilities *__unused);
 extern void sme_kernel_enable(const struct arm64_cpu_capabilities *__unused);
 extern void sme2_kernel_enable(const struct arm64_cpu_capabilities *__unused);
 extern void fa64_kernel_enable(const struct arm64_cpu_capabilities *__unused);
+extern void cpu_enable_fpmr(const struct arm64_cpu_capabilities *__unused);
 
 extern u64 read_zcr_features(void);
 extern u64 read_smcr_features(void);
