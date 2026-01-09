5f553ae7e9bd072b5306c4d90b84d02c998121e2
David <david.sw@alibaba-inc.com>
2026-01-09 11:34:50 +0800

    net/smc: cap allocation order for SMC-R physically contiguous buffers
    The alloc_page() cannot satisfy requests exceeding MAX_PAGE_ORDER,
    and attempting such allocations will lead to guaranteed failures
    and potential kernel warnings.
    
    Change the SMC-R buffer allocation logic to handle large buffer requests
    more gracefully:
    
    For SMCR_PHYS_CONT_BUFS, the allocation order is now capped to
    MAX_PAGE_ORDER, ensures the attempts to allocate the largest possible
    physically contiguous chunk instead of failing with an invalid order,
    which also avoid redundant "try-fail-degrade" cycles in __smc_buf_create().
    
    For SMCR_MIXED_BUFS, If it's order exceeds MAX_PAGE_ORDER, skips the
    doomed physical allocation attempt and fallback to virtual memory
    immediately.
    
    Signed-off-by: D. Wythe <alibuda@linux.alibaba.com>

--- net/smc/smc_core.c
+++ net/smc/smc_core.c
@@ -2487,18 +2487,21 @@ static struct smc_buf_desc *smcr_new_buf_create(struct smc_link_group *lgr,
 
 	switch (lgr->buf_type) {
 	case SMCR_PHYS_CONT_BUFS:
+		buf_desc->order = min(buf_desc->order, MAX_PAGE_ORDER);
+		fallthrough;
 	case SMCR_MIXED_BUFS:
-		buf_desc->order = get_order(bufsize);
-		buf_desc->pages = alloc_pages(GFP_KERNEL | __GFP_NOWARN |
-					      __GFP_NOMEMALLOC | __GFP_COMP |
-					      __GFP_NORETRY | __GFP_ZERO,
-					      buf_desc->order);
-		if (buf_desc->pages) {
-			buf_desc->cpu_addr =
-				(void *)page_address(buf_desc->pages);
-			buf_desc->len = bufsize;
-			buf_desc->is_vm = false;
-			break;
+		if (buf_desc->order <= MAX_PAGE_ORDER) {
+			buf_desc->pages = alloc_pages(GFP_KERNEL | __GFP_NOWARN |
+						      __GFP_NOMEMALLOC | __GFP_COMP |
+						      __GFP_NORETRY | __GFP_ZERO,
+						      buf_desc->order);
+			if (buf_desc->pages) {
+				buf_desc->cpu_addr =
+					(void *)page_address(buf_desc->pages);
+				buf_desc->len = bufsize;
+				buf_desc->is_vm = false;
+				break;
+			}
 		}
 		if (lgr->buf_type == SMCR_PHYS_CONT_BUFS)
 			goto out;
