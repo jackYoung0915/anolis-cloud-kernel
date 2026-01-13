9cefbd087b0b2569d8b31df41266a3efd0fa0b16
zhangjunmar <12733587+zhangjunmar@user.noreply.gitee.com>
2026-01-13 10:15:57 +0800

    anolis: cpu vulnerabilities: support to flush all types of branches rather than indirect branches type only in the Branch Target Buffer with the IBPB command.
    ANBZ: #29446
    
    The IBPB command only flushes indirect branch types in the Branch Target
    Buffer on HYGON C86-4G CPUs and the later CPUs, but all types of branches
    need to be flushed with the IBPB command to mitigate SRSO(INCEPTION) in
    some scenarios.The full mitigation could be achieved by updating
    microcode for AMD. However, HYGON can offer this function by setting bit
    55 of MSR_ZEN4_BP_CFG rather than updating microcode which is indicated
    by has_microcode variable. We define a new kernel command-line interface
    to set this bit. We also provide a new function ibpb_can_flush_all() as
    a replacement for the AMD state indicated by has_microcode variable.
    Users could control whether IBPB flushes all branches via the parameter
    in kernel command-line according to the attack risks and the
    performance costs.
    
    Hygon-SIG: commit none hygon cpu vulnerabilities: support to flush all types of branches rather than indirect branches type only in the Branch Target Buffer with the IBPB command.
    
    Signed-off-by: zhangjun <zhangjun@hygon.cn>
    Cc: hygon-arch@list.openanolis.cn
    Reviewed-by: Guixin Liu <kanie@linux.alibaba.com>
    Reviewed-by: Guanghui Feng <guanghuifeng@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6365

--- arch/x86/kernel/cpu/bugs.c
+++ arch/x86/kernel/cpu/bugs.c
@@ -2013,6 +2013,48 @@ int arch_prctl_spec_ctrl_get(struct task_struct *task, unsigned long which)
 	}
 }
 
+/**
+ * enum ibpb_brtype_cmd - IBPB action control flag
+ * @IBPB_FLUSH_IND:IBPB command only flushes indirect branches in branch target buffer
+ * @IBPB_FLUSH_ALL:IBPB command flushes all types of branches in branch target buffer
+ */
+enum ibpb_brtype_cmd {
+	IBPB_FLUSH_IND,
+	IBPB_FLUSH_ALL,
+};
+static enum ibpb_brtype_cmd ibpb_brtype __ro_after_init = IBPB_FLUSH_ALL;
+/**
+ * The kernel parameter ibpb_brtype is used to control
+ * whether IBPB flushes all branches or indirect branches:
+ * ibpb_brtype=     [X86, HYGON only]
+ *                IBPB action control flag
+ *                Format: { ibpb-all | ibpb-ind }
+ *                ibpb-all -- IBPB flushes all types of branches,this is the default value.
+ *                ibpb-ind -- IBPB flushes only indirect branches.
+ */
+static int __init ibpb_brtype_cmdline(char *str)
+{
+	if (boot_cpu_data.x86_vendor != X86_VENDOR_HYGON)
+		return 0;
+
+	if (!str)
+		return -EINVAL;
+
+	if (!strcmp(str, "ibpb-all")) {
+		ibpb_brtype = IBPB_FLUSH_ALL;
+		pr_info("IBPB flushes all branches.\n");
+	} else if (!strcmp(str, "ibpb-ind")) {
+		ibpb_brtype = IBPB_FLUSH_IND;
+		pr_info("IBPB flushes only indirect branches.\n");
+	} else
+		pr_err("Ignoring unknown ibpb branch type option (%s).", str);
+
+	return 0;
+}
+early_param("ibpb_brtype", ibpb_brtype_cmdline);
+
+#define IBPB_FLUSH_ALL_BIT 55
+
 void x86_spec_ctrl_setup_ap(void)
 {
 	if (boot_cpu_has(X86_FEATURE_MSR_SPEC_CTRL))
@@ -2020,6 +2062,13 @@ void x86_spec_ctrl_setup_ap(void)
 
 	if (ssb_mode == SPEC_STORE_BYPASS_DISABLE)
 		x86_amd_ssb_disable();
+
+	if ((boot_cpu_data.x86_vendor == X86_VENDOR_HYGON) &&
+		(boot_cpu_data.x86 == 0x18)) {
+		if ((boot_cpu_data.x86_model > 0x3) &&
+			(ibpb_brtype == IBPB_FLUSH_ALL))
+			msr_set_bit(MSR_ZEN4_BP_CFG, IBPB_FLUSH_ALL_BIT);
+	}
 }
 
 bool itlb_multihit_kvm_mitigation;
@@ -2201,6 +2250,29 @@ early_param("spec_rstack_overflow", srso_parse_cmdline);
 
 #define SRSO_NOTICE "WARNING: See https://kernel.org/doc/html/latest/admin-guide/hw-vuln/srso.html for mitigation options."
 
+/*
+ * ibpb_can_flush_all() - set IBPB flush type according to the cmdline param
+ *                      - and check whether IBPB can flush all branches
+ * @return: true when IBPB can flush all types of branches and
+ *          false when IBPB can flush only indirect branches.
+ */
+bool ibpb_can_flush_all(void)
+{
+	if ((boot_cpu_data.x86_vendor == X86_VENDOR_HYGON) &&
+		(boot_cpu_data.x86 == 0x18)) {
+		if (boot_cpu_data.x86_model <= 0x3) {
+			return true;
+		} else if (ibpb_brtype == IBPB_FLUSH_ALL) {
+			msr_set_bit(MSR_ZEN4_BP_CFG, IBPB_FLUSH_ALL_BIT);
+			return true;
+		}
+		return false;
+	}
+
+	pr_err("WARNING: this ibpb check is only used for HYGON.\n");
+	return false;
+}
+
 static void __init srso_select_mitigation(void)
 {
 	bool has_microcode;
@@ -2213,6 +2285,10 @@ static void __init srso_select_mitigation(void)
 	 * for guests to verify whether IBPB is a viable mitigation.
 	 */
 	has_microcode = boot_cpu_has(X86_FEATURE_IBPB_BRTYPE) || cpu_has_ibpb_brtype_microcode();
+
+	if (boot_cpu_data.x86_vendor == X86_VENDOR_HYGON)
+		has_microcode = ibpb_can_flush_all();
+
 	if (!has_microcode) {
 		pr_warn("IBPB-extending microcode not applied!\n");
 		pr_warn(SRSO_NOTICE);
