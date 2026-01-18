616e7dd9a3153d21c1ee45e3d187563a26e6dea7
Shivank Garg <shivankg@amd.com>
2026-01-18 19:09:40 +0000

    mm/khugepaged: map dirty/writeback pages failures to EAGAIN
    ANBZ: #30191
    
    cherry-picked from https://lore.kernel.org/lkml/20260118190939.8986-4-shivankg@amd.com/
    
    When collapse_file encounters dirty or writeback pages in file-backed
    mappings, it currently returns SCAN_FAIL which maps to -EINVAL.  This is
    misleading as EINVAL suggests invalid arguments, whereas dirty/writeback
    pages represent transient conditions that may resolve on retry.
    
    Introduce SCAN_PAGE_DIRTY_OR_WRITEBACK to cover both dirty and writeback
    states, mapping it to -EAGAIN.  For MADV_COLLAPSE, this provides userspace
    with a clear signal that retry may succeed after writeback completes.  For
    khugepaged, this is harmless as it will naturally revisit the range during
    periodic scans after async writeback completes.
    
    Fixes: 34488399fa08 ("mm/madvise: add file and shmem support to MADV_COLLAPSE")
    Reported-by: Branden Moore <Branden.Moore@amd.com>
    Closes: https://lore.kernel.org/all/4e26fe5e-7374-467c-a333-9dd48f85d7cc@amd.com
    Reviewed-by: Dev Jain <dev.jain@arm.com>
    Reviewed-by: Lance Yang <lance.yang@linux.dev>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Reviewed-by: wang lian <lianux.mm@gmail.com>
    Acked-by: David Hildenbrand (Red Hat) <david@kernel.org>
    Signed-off-by: Shivank Garg <shivankg@amd.com>
    Signed-off-by: Yuanhe Shu <xiangzao@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6470

--- mm/khugepaged.c
+++ mm/khugepaged.c
@@ -60,6 +60,7 @@ enum scan_result {
 	SCAN_STORE_FAILED,
 	SCAN_COPY_MC,
 	SCAN_PAGE_FILLED,
+	SCAN_PAGE_DIRTY_OR_WRITEBACK,
 };
 
 #define CREATE_TRACE_POINTS
@@ -1932,11 +1933,11 @@ static int collapse_file(struct mm_struct *mm, unsigned long addr,
 				 */
 				xas_unlock_irq(&xas);
 				filemap_flush(mapping);
-				result = SCAN_FAIL;
+				result = SCAN_PAGE_DIRTY_OR_WRITEBACK;
 				goto xa_unlocked;
 			} else if (folio_test_writeback(folio)) {
 				xas_unlock_irq(&xas);
-				result = SCAN_FAIL;
+				result = SCAN_PAGE_DIRTY_OR_WRITEBACK;
 				goto xa_unlocked;
 			} else if (folio_trylock(folio)) {
 				folio_get(folio);
@@ -1983,7 +1984,7 @@ static int collapse_file(struct mm_struct *mm, unsigned long addr,
 			 * folio is dirty because it hasn't been flushed
 			 * since first write.
 			 */
-			result = SCAN_FAIL;
+			result = SCAN_PAGE_DIRTY_OR_WRITEBACK;
 			goto out_unlock;
 		}
 
@@ -2746,6 +2747,7 @@ static int madvise_collapse_errno(enum scan_result r)
 	case SCAN_PAGE_LRU:
 	case SCAN_DEL_PAGE_LRU:
 	case SCAN_PAGE_FILLED:
+	case SCAN_PAGE_DIRTY_OR_WRITEBACK:
 		return -EAGAIN;
 	/*
 	 * Other: Trying again likely not to succeed / error intrinsic to
