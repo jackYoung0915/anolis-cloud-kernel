8d98c2d97777129bdac92233842130ad2b92060a
Anup Patel <anup.patel@oss.qualcomm.com>
2026-01-20 13:29:49 +0530

    RISC-V: KVM: Check host Ssaia extension when creating AIA irqchip
    ANBZ: #35388
    
    commit c61ec3e8cc5d46fa269434a9ec16ca36d362e0dd upstream.
    
    The KVM user-space may create KVM AIA irqchip before checking
    VCPU Ssaia extension availability so KVM AIA irqchip must fail
    when host does not have Ssaia extension.
    
    Fixes: 89d01306e34d ("RISC-V: KVM: Implement device interface for AIA irqchip")
    Signed-off-by: Anup Patel <anup.patel@oss.qualcomm.com>
    Link: https://lore.kernel.org/r/20260120080013.2153519-4-anup.patel@oss.qualcomm.com
    Signed-off-by: Anup Patel <anup@brainfault.org>
    Signed-off-by: Wang Yechao <wang.yechao255@zte.com.cn>
    Reviewed-by: Ruidong Tian <tianruidong@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/7044

--- arch/riscv/kvm/aia_device.c
+++ arch/riscv/kvm/aia_device.c
@@ -11,6 +11,7 @@
 #include <linux/irqchip/riscv-imsic.h>
 #include <linux/kvm_host.h>
 #include <linux/uaccess.h>
+#include <linux/cpufeature.h>
 
 static void unlock_vcpus(struct kvm *kvm, int vcpu_lock_idx)
 {
@@ -51,6 +52,9 @@ static int aia_create(struct kvm_device *dev, u32 type)
 
 	if (irqchip_in_kernel(kvm))
 		return -EEXIST;
+
+	if (!riscv_isa_extension_available(NULL, SSAIA))
+		return -ENODEV;
 
 	ret = -EBUSY;
 	if (!lock_all_vcpus(kvm))
