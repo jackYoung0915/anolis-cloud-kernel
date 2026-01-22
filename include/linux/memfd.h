817c0a19396deb005dfa4e03a283f0630fb0411c
Pratyush Yadav (Google) <pratyush@kernel.org>
2026-01-22 16:18:39 +0100

    memfd: export alloc_file()
    ANBZ: #26808
    
    commit 71e2b5eadbad43d33f0e2cf6d767395273ba5eaa upstream.
    
    Patch series "mm: memfd_luo hotfixes".
    
    This series contains a couple of fixes for memfd preservation using LUO.
    
    This patch (of 3):
    
    The Live Update Orchestrator's (LUO) memfd preservation works by
    preserving all the folios of a memfd, re-creating an empty memfd on the
    next boot, and then inserting back the preserved folios.
    
    Currently it creates the file by directly calling shmem_file_setup().
    This leaves out other work done by alloc_file() like setting up the file
    mode, flags, or calling the security hooks.
    
    Export alloc_file() to let memfd_luo use it.  Rename it to
    memfd_alloc_file() since it is no longer private and thus needs a
    subsystem prefix.
    
    [Zelin Deng: Fix conflicts due to no "FD_ADD".]
    
    Link: https://lkml.kernel.org/r/20260122151842.4069702-1-pratyush@kernel.org
    Link: https://lkml.kernel.org/r/20260122151842.4069702-2-pratyush@kernel.org
    Signed-off-by: Pratyush Yadav (Google) <pratyush@kernel.org>
    Reviewed-by: Mike Rapoport (Microsoft) <rppt@kernel.org>
    Reviewed-by: Pasha Tatashin <pasha.tatashin@soleen.com>
    Cc: Baolin Wang <baolin.wang@linux.alibaba.com>
    Cc: Hugh Dickins <hughd@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Zelin Deng <zelin.deng@linux.alibaba.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6646

--- include/linux/memfd.h
+++ include/linux/memfd.h
@@ -7,12 +7,17 @@
 #ifdef CONFIG_MEMFD_CREATE
 extern long memfd_fcntl(struct file *file, unsigned int cmd, unsigned int arg);
 struct folio *memfd_alloc_folio(struct file *memfd, pgoff_t idx);
+struct file *memfd_alloc_file(const char *name, unsigned int flags);
 #else
 static inline long memfd_fcntl(struct file *f, unsigned int c, unsigned int a)
 {
 	return -EINVAL;
 }
 static inline struct folio *memfd_alloc_folio(struct file *memfd, pgoff_t idx)
+{
+	return ERR_PTR(-EINVAL);
+}
+static inline struct file *memfd_alloc_file(const char *name, unsigned int flags)
 {
 	return ERR_PTR(-EINVAL);
 }
