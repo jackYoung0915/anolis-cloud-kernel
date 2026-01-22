a984a0f73bf4c4ab3dbeb5b2a3a2fdc20fef5f78
Baolin Wang <baolin.wang@linux.alibaba.com>
2026-01-22 17:59:33 +0800

    anolis: mm: use the new follow_pfnmap API
    ANBZ: #30843
    
    Use the new follow_pfnmap() API for follow_pfn().
    
    Signed-off-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Reviewed-by: Guo Ren <guoren@kernel.org>
    Reviewed-by: Anup Patel <anup@brainfault.org>
    Reviewed-by: Shuai Xue <xueshuai@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6601

--- mm/memory.c
+++ mm/memory.c
@@ -7023,18 +7023,17 @@ EXPORT_SYMBOL_GPL(follow_pfnmap_end);
 int follow_pfn(struct vm_area_struct *vma, unsigned long address,
 	unsigned long *pfn)
 {
+	struct follow_pfnmap_args args = { .vma = vma, .address = address };
 	int ret = -EINVAL;
-	spinlock_t *ptl;
-	pte_t *ptep;
 
 	if (!(vma->vm_flags & (VM_IO | VM_PFNMAP)))
 		return ret;
 
-	ret = follow_pte(vma->vm_mm, address, &ptep, &ptl);
+	ret = follow_pfnmap_start(&args);
 	if (ret)
 		return ret;
-	*pfn = pte_pfn(ptep_get(ptep));
-	pte_unmap_unlock(ptep, ptl);
+	*pfn = args.pfn;
+	follow_pfnmap_end(&args);
 	return 0;
 }
 EXPORT_SYMBOL(follow_pfn);
