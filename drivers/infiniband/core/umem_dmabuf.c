3fc490599bcb404b6b498239c14f3e50afa23b0c
Leon Romanovsky <leonro@nvidia.com>
2026-01-24 21:14:13 +0200

    dma-buf: Rename .move_notify() callback to a clearer identifier
    ANBZ: #31969
    
    commit ef246da8e63c486780dca4d9b4d79589cbebf5e5 upstream.
    
    Rename the .move_notify() callback to .invalidate_mappings() to make its
    purpose explicit and highlight that it is responsible for invalidating
    existing mappings.
    
    Suggested-by: Christian König <christian.koenig@amd.com>
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Leon Romanovsky <leonro@nvidia.com>
    Link: https://lore.kernel.org/r/20260124-dmabuf-revoke-v5-1-f98fca917e96@nvidia.com
    Signed-off-by: Christian König <christian.koenig@amd.com>
    [Ruidong: delete change in
    	drivers/gpu/drm/virtio/virtgpu_prime.c: not support dma-buf yet
    	drivers/gpu/drm/xe/tests/xe_dma_buf.c: not support xe drivers
    	drivers/gpu/drm/xe/xe_dma_buf.c: not support xe drivers
    ]
    Signed-off-by: Ruidong Tian <tianruidong@linux.alibaba.com>
    Reviewed-by: Shuai Xue <xueshuai@linux.alibaba.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6864

--- drivers/infiniband/core/umem_dmabuf.c
+++ drivers/infiniband/core/umem_dmabuf.c
@@ -124,7 +124,7 @@ struct ib_umem_dmabuf *ib_umem_dmabuf_get(struct ib_device *device,
 	if (check_add_overflow(offset, (unsigned long)size, &end))
 		return ret;
 
-	if (unlikely(!ops || !ops->move_notify))
+	if (unlikely(!ops || !ops->invalidate_mappings))
 		return ret;
 
 	dmabuf = dma_buf_get(fd);
@@ -181,7 +181,7 @@ ib_umem_dmabuf_unsupported_move_notify(struct dma_buf_attachment *attach)
 
 static struct dma_buf_attach_ops ib_umem_dmabuf_attach_pinned_ops = {
 	.allow_peer2peer = true,
-	.move_notify = ib_umem_dmabuf_unsupported_move_notify,
+	.invalidate_mappings = ib_umem_dmabuf_unsupported_move_notify,
 };
 
 struct ib_umem_dmabuf *ib_umem_dmabuf_get_pinned(struct ib_device *device,
