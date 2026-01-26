2535cc90edb680b657bde872617e25e1ff1ca4e0
Jason Zeng <jason.zeng@intel.com>
2026-01-25 22:54:20 -0800

    anolis: Revert "anolis: x86/resctrl: Add mount option for memory bandwidth HWDRC"
    ANBZ: #31045
    
    This reverts commit fcab6177eed5c4db63e594def5cf457959f93155.
    
    Signed-off-by: Jason Zeng <jason.zeng@intel.com>
    Reviewed-by: Qinyun Tan <qinyuntan@linux.alibaba.com>
    Reviewed-by: Xiaochen Shen <shenxiaochen@hygon.cn>
    Reviewed-by: Jiankang Chen <jkchen@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6611

--- fs/resctrl/internal.h
+++ fs/resctrl/internal.h
@@ -58,7 +58,6 @@ struct rdt_fs_context {
 	bool				enable_cdpl3;
 	bool				enable_mba_mbps;
 	bool				enable_debug;
-	bool				enable_hwdrc_mb;
 };
 
 static inline struct rdt_fs_context *rdt_fc2context(struct fs_context *fc)
@@ -247,14 +246,6 @@ static inline  bool is_mba_sc(struct rdt_resource *r)
 		return false;
 
 	return r->membw.mba_sc;
-}
-
-static inline bool is_hwdrc_enabled(struct rdt_resource *r)
-{
-	if (!r)
-		r = resctrl_arch_get_resource(RDT_RESOURCE_MBA);
-
-	return r->membw.hwdrc_mb;
 }
 
 extern struct mutex rdtgroup_mutex;
