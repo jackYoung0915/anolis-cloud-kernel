50652df28e8a9937c62cdd5d6033dbaea035b239
Jason Zeng <jason.zeng@intel.com>
2026-01-25 23:52:17 -0800

    anolis: Revert "x86/resctrl: Add resctrl_arch_ prefix to pseudo lock functions"
    ANBZ: #31045
    
    This reverts commit 46675abeaadea4d56bca73b2a9ed9bcbdaf7d8c4.
    
    Signed-off-by: Jason Zeng <jason.zeng@intel.com>
    Reviewed-by: Qinyun Tan <qinyuntan@linux.alibaba.com>
    Reviewed-by: Xiaochen Shen <shenxiaochen@hygon.cn>
    Reviewed-by: Jiankang Chen <jkchen@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6611

--- arch/x86/kernel/cpu/resctrl/pseudo_lock.c
+++ arch/x86/kernel/cpu/resctrl/pseudo_lock.c
@@ -62,8 +62,7 @@ static const struct class pseudo_lock_class = {
 };
 
 /**
- * resctrl_arch_get_prefetch_disable_bits - prefetch disable bits of supported
- *                                          platforms
+ * get_prefetch_disable_bits - prefetch disable bits of supported platforms
  * @void: It takes no parameters.
  *
  * Capture the list of platforms that have been validated to support
@@ -77,13 +76,13 @@ static const struct class pseudo_lock_class = {
  * in the SDM.
  *
  * When adding a platform here also add support for its cache events to
- * resctrl_arch_measure_l*_residency()
+ * measure_cycles_perf_fn()
  *
  * Return:
  * If platform is supported, the bits to disable hardware prefetchers, 0
  * if platform is not supported.
  */
-u64 resctrl_arch_get_prefetch_disable_bits(void)
+static u64 get_prefetch_disable_bits(void)
 {
 	if (boot_cpu_data.x86_vendor != X86_VENDOR_INTEL ||
 	    boot_cpu_data.x86 != 6)
@@ -411,7 +410,7 @@ static void pseudo_lock_free(struct rdtgroup *rdtgrp)
 }
 
 /**
- * resctrl_arch_pseudo_lock_fn - Load kernel memory into cache
+ * pseudo_lock_fn - Load kernel memory into cache
  * @_rdtgrp: resource group to which pseudo-lock region belongs
  *
  * This is the core pseudo-locking flow.
@@ -429,7 +428,7 @@ static void pseudo_lock_free(struct rdtgroup *rdtgrp)
  *
  * Return: 0. Waiter on waitqueue will be woken on completion.
  */
-int resctrl_arch_pseudo_lock_fn(void *_rdtgrp)
+static int pseudo_lock_fn(void *_rdtgrp)
 {
 	struct rdtgroup *rdtgrp = _rdtgrp;
 	struct pseudo_lock_region *plr = rdtgrp->plr;
@@ -715,7 +714,7 @@ int rdtgroup_locksetup_enter(struct rdtgroup *rdtgrp)
 	 * Not knowing the bits to disable prefetching implies that this
 	 * platform does not support Cache Pseudo-Locking.
 	 */
-	prefetch_disable_bits = resctrl_arch_get_prefetch_disable_bits();
+	prefetch_disable_bits = get_prefetch_disable_bits();
 	if (prefetch_disable_bits == 0) {
 		rdt_last_cmd_puts("Pseudo-locking not supported\n");
 		return -EINVAL;
@@ -880,8 +879,7 @@ bool rdtgroup_pseudo_locked_in_hierarchy(struct rdt_domain *d)
 }
 
 /**
- * resctrl_arch_measure_cycles_lat_fn - Measure cycle latency to read
- *                                      pseudo-locked memory
+ * measure_cycles_lat_fn - Measure cycle latency to read pseudo-locked memory
  * @_plr: pseudo-lock region to measure
  *
  * There is no deterministic way to test if a memory region is cached. One
@@ -894,7 +892,7 @@ bool rdtgroup_pseudo_locked_in_hierarchy(struct rdt_domain *d)
  *
  * Return: 0. Waiter on waitqueue will be woken on completion.
  */
-int resctrl_arch_measure_cycles_lat_fn(void *_plr)
+static int measure_cycles_lat_fn(void *_plr)
 {
 	struct pseudo_lock_region *plr = _plr;
 	u32 saved_low, saved_high;
@@ -1078,7 +1076,7 @@ out:
 	return 0;
 }
 
-int resctrl_arch_measure_l2_residency(void *_plr)
+static int measure_l2_residency(void *_plr)
 {
 	struct pseudo_lock_region *plr = _plr;
 	struct residency_counts counts = {0};
@@ -1116,7 +1114,7 @@ out:
 	return 0;
 }
 
-int resctrl_arch_measure_l3_residency(void *_plr)
+static int measure_l3_residency(void *_plr)
 {
 	struct pseudo_lock_region *plr = _plr;
 	struct residency_counts counts = {0};
@@ -1214,18 +1212,18 @@ static int pseudo_lock_measure_cycles(struct rdtgroup *rdtgrp, int sel)
 	plr->cpu = cpu;
 
 	if (sel == 1)
-		thread = kthread_create_on_node(resctrl_arch_measure_cycles_lat_fn,
-						plr, cpu_to_node(cpu),
+		thread = kthread_create_on_node(measure_cycles_lat_fn, plr,
+						cpu_to_node(cpu),
 						"pseudo_lock_measure/%u",
 						cpu);
 	else if (sel == 2)
-		thread = kthread_create_on_node(resctrl_arch_measure_l2_residency,
-						plr, cpu_to_node(cpu),
+		thread = kthread_create_on_node(measure_l2_residency, plr,
+						cpu_to_node(cpu),
 						"pseudo_lock_measure/%u",
 						cpu);
 	else if (sel == 3)
-		thread = kthread_create_on_node(resctrl_arch_measure_l3_residency,
-						plr, cpu_to_node(cpu),
+		thread = kthread_create_on_node(measure_l3_residency, plr,
+						cpu_to_node(cpu),
 						"pseudo_lock_measure/%u",
 						cpu);
 	else
@@ -1324,7 +1322,7 @@ int rdtgroup_pseudo_lock_create(struct rdtgroup *rdtgrp)
 
 	plr->thread_done = 0;
 
-	thread = kthread_create_on_node(resctrl_arch_pseudo_lock_fn, rdtgrp,
+	thread = kthread_create_on_node(pseudo_lock_fn, rdtgrp,
 					cpu_to_node(plr->cpu),
 					"pseudo_lock/%u", plr->cpu);
 	if (IS_ERR(thread)) {
