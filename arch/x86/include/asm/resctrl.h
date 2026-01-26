29becba5772bb336b9c5d6ec0ca52d9879cb299f
Jason Zeng <jason.zeng@intel.com>
2026-01-25 21:35:05 -0800

    anolis: Revert "x86/resctrl: Add the interface to assign a hardware counter"
    ANBZ: #31045
    
    This reverts commit 53014429148a0d942d33bbacc56c247836755582.
    
    Signed-off-by: Jason Zeng <jason.zeng@intel.com>
    Reviewed-by: Qinyun Tan <qinyuntan@linux.alibaba.com>
    Reviewed-by: Xiaochen Shen <shenxiaochen@hygon.cn>
    Reviewed-by: Jiankang Chen <jkchen@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6611

--- arch/x86/include/asm/resctrl.h
+++ arch/x86/include/asm/resctrl.h
@@ -222,9 +222,6 @@ void resctrl_arch_mbm_cntr_assign_disable(void);
 void resctrl_arch_event_config_set(void *info);
 u32 resctrl_arch_event_config_get(void *d, enum resctrl_event_id eventid);
 
-int resctrl_arch_assign_cntr(void *dom, enum resctrl_event_id evtid,
-			     u32 rmid, u32 cntr_id, u32 closid, bool assign);
-
 u64 resctrl_arch_get_prefetch_disable_bits(void);
 int resctrl_arch_pseudo_lock_fn(void *_plr);
 int resctrl_arch_measure_cycles_lat_fn(void *_plr);
