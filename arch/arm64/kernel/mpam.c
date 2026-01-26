6a862821f78abd2dddd5c7ced3e1ae679e2a09d3
Jason Zeng <jason.zeng@intel.com>
2026-01-25 23:46:25 -0800

    anolis: Revert "arm64: mpam: Context switch the MPAM registers"
    ANBZ: #31045
    
    This reverts commit db08e78d00b8608723221714859b0ce1890fcaf5.
    
    Signed-off-by: Jason Zeng <jason.zeng@intel.com>
    Reviewed-by: Qinyun Tan <qinyuntan@linux.alibaba.com>
    Reviewed-by: Xiaochen Shen <shenxiaochen@hygon.cn>
    Reviewed-by: Jiankang Chen <jkchen@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6611

--- arch/arm64/kernel/mpam.c
+++ arch/arm64/kernel/mpam.c
@@ -4,11 +4,7 @@
 #include <asm/mpam.h>
 
 #include <linux/jump_label.h>
-#include <linux/percpu.h>
 
 DEFINE_STATIC_KEY_FALSE(arm64_mpam_has_hcr);
 EXPORT_SYMBOL_FOR_KVM(arm64_mpam_has_hcr);
-DEFINE_STATIC_KEY_FALSE(mpam_enabled);
 EXPORT_SYMBOL_FOR_KVM(mpam_enabled);
-DEFINE_PER_CPU(u64, arm64_mpam_default);
-DEFINE_PER_CPU(u64, arm64_mpam_current);
