6df4e6e78ec8c28e5646d2e5bfebd94f73a7558e
Jason Zeng <jason.zeng@intel.com>
2026-01-25 23:34:57 -0800

    anolis: Revert "anolis: ACPI / PPTT: Downgrade the revision requirement in acpi_pptt_get_cpumask_from_cache_id_and_level"
    ANBZ: #31045
    
    This reverts commit 02a1af8f13c84089353b3532591c9dff93a21fc3.
    
    Signed-off-by: Jason Zeng <jason.zeng@intel.com>
    Reviewed-by: Qinyun Tan <qinyuntan@linux.alibaba.com>
    Reviewed-by: Xiaochen Shen <shenxiaochen@hygon.cn>
    Reviewed-by: Jiankang Chen <jkchen@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6611

--- drivers/acpi/pptt.c
+++ drivers/acpi/pptt.c
@@ -1173,11 +1173,7 @@ int acpi_pptt_get_cpumask_from_cache_id_and_level(u32 cache_id, u32 cache_level,
 		return -ENOENT;
 	}
 
-	/*
-	 * FIXME: Since this function does not actually use the cache id in the
-	 * PPTT table, we downgrade the revision requirement.
-	 */
-	if (table->revision < 2) {
+	if (table->revision < 3) {
 		acpi_put_table(table);
 		return -ENOENT;
 	}
