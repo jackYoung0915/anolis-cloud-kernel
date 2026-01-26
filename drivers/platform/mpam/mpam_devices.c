e7e35384420f700774927f3c3b52009b3dff6956
Jason Zeng <jason.zeng@intel.com>
2026-01-25 23:34:55 -0800

    anolis: Revert "anolis: arm_mpam: Add cache msc info parsing for Yitian710 specific MPAM ACPI table"
    ANBZ: #31045
    
    This reverts commit c4d02e76df6c83226ba80ad65a26256d5b865b67.
    
    Signed-off-by: Jason Zeng <jason.zeng@intel.com>
    Reviewed-by: Qinyun Tan <qinyuntan@linux.alibaba.com>
    Reviewed-by: Xiaochen Shen <shenxiaochen@hygon.cn>
    Reviewed-by: Jiankang Chen <jkchen@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6611

--- drivers/platform/mpam/mpam_devices.c
+++ drivers/platform/mpam/mpam_devices.c
@@ -367,12 +367,8 @@ static int get_cpumask_from_cache_id(u32 cache_id, u32 cache_level,
 	int iter_cache_id;
 	struct device_node *iter;
 
-	if (!acpi_disabled) {
-		if (mpam_current_machine == MPAM_YITIAN710)
-			return acpi_pptt_get_cpumask_from_cache_id_and_level(
-					cache_id, cache_level, affinity);
+	if (!acpi_disabled)
 		return acpi_pptt_get_cpumask_from_cache_id(cache_id, affinity);
-	}
 
 	for_each_possible_cpu(cpu) {
 		iter = of_get_cpu_node(cpu, NULL);
