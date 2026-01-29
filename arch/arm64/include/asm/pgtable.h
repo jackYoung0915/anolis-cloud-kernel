727e297ab5ff2edf3ce0a27dd03dcad7753a009d
Baolin Wang <baolin.wang@linux.alibaba.com>
2026-01-29 14:59:14 +0800

    arm64: mm: fix using the incorrect pte_cont() to check contiguous PTEs
    ANBZ: #9728
    
    cherry-picked from https://lore.kernel.org/all/de141225-a0c1-41fd-b3e1-bcab09827ddd@linux.alibaba.com/
    
    Non-present PTEs (device-private, device-exclusive) can reach
    clear_flush_young_ptes() through folio_referenced_one()->
    clear_flush_young_ptes_notify(). These entries may have bit 52 set as
    part of their encoding, but they aren't valid contiguous mappings.
    
    Fix this issue by using pte_valid_cont() instead of pte_cont().
    
    Fixes: e66c54fea004 ("anolis: arm64: mm: implement the architecture-specific test_and_clear_young_ptes()")
    Fixes: f7a6480ca425 ("arm64: mm: implement the architecture-specific clear_flush_young_ptes()")
    Signed-off-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Reviewed-by Shuai Xue <xueshuai@linxu.alibaba.com>
    Reviewed-by: Shuai Xue <xueshuai@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6439

--- arch/arm64/include/asm/pgtable.h
+++ arch/arm64/include/asm/pgtable.h
@@ -1532,7 +1532,7 @@ static inline int test_and_clear_young_ptes(struct vm_area_struct *vma,
 					    unsigned long addr, pte_t *ptep,
 					    unsigned int nr)
 {
-	if (likely(nr == 1 && !pte_cont(__ptep_get(ptep))))
+	if (likely(nr == 1 && !pte_valid_cont(__ptep_get(ptep))))
 		return __ptep_test_and_clear_young(vma, addr, ptep);
 
 	return contpte_test_and_clear_young_ptes(vma, addr, ptep, nr);
@@ -1562,7 +1562,7 @@ static inline int clear_flush_young_ptes(struct vm_area_struct *vma,
 					 unsigned long addr, pte_t *ptep,
 					 unsigned int nr)
 {
-	if (likely(nr == 1 && !pte_cont(__ptep_get(ptep))))
+	if (likely(nr == 1 && !pte_valid_cont(__ptep_get(ptep))))
 		return __ptep_clear_flush_young(vma, addr, ptep);
 
 	return contpte_clear_flush_young_ptes(vma, addr, ptep, nr);
