1e7ae233b4a6bb1024ed6125787029989a587c38
Vipin Sharma <vipinsh@google.com>
2026-01-29 21:24:51 +0000

    vfio/pci: Register a file handler with Live Update Orchestrator
    ANBZ: #26808
    
    cherry-picked from https://lore.kernel.org/all/20260323235817.1960573-7-dmatlack@google.com/
    
    Register a live update file handler for vfio-pci device files. Add stub
    implementations of all required callbacks so that registration does not
    fail (i.e. to avoid breaking git-bisect).
    
    This file handler will be extended in subsequent commits to enable a
    device bound to vfio-pci to run without interruption while the host is
    going through a kexec Live Update.
    
    Put this support behind a new Kconfig VFIO_PCI_LIVEUPDATE that is marked
    experimental and default-disabled until more of the device preservation
    support has landed in the kernel.
    
    Signed-off-by: Vipin Sharma <vipinsh@google.com>
    Co-developed-by: David Matlack <dmatlack@google.com>
    Signed-off-by: David Matlack <dmatlack@google.com>
    Signed-off-by: Zelin Deng <zelin.deng@linux.alibaba.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Reviewed-by: Shuai Xue <xueshuai@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6709

--- drivers/vfio/pci/vfio_pci_priv.h
+++ drivers/vfio/pci/vfio_pci_priv.h
@@ -101,4 +101,18 @@ static inline bool vfio_pci_is_vga(struct pci_dev *pdev)
 	return (pdev->class >> 8) == PCI_CLASS_DISPLAY_VGA;
 }
 
+#ifdef CONFIG_VFIO_PCI_LIVEUPDATE
+int __init vfio_pci_liveupdate_init(void);
+void vfio_pci_liveupdate_cleanup(void);
+#else
+static inline int vfio_pci_liveupdate_init(void)
+{
+	return 0;
+}
+
+static inline void vfio_pci_liveupdate_cleanup(void)
+{
+}
+#endif /* CONFIG_VFIO_PCI_LIVEUPDATE */
+
 #endif
