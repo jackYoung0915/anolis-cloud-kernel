646ccdc836619eeef428d6ce9575da5967bd08e8
David Matlack <dmatlack@google.com>
2026-01-29 21:24:49 +0000

    PCI: Add API to track PCI devices preserved across Live Update
    ANBZ: #26808
    
    cherry-picked from https://lore.kernel.org/all/20260323235817.1960573-3-dmatlack@google.com/
    
    Add an API to enable the PCI subsystem to participate in a Live Update
    and track all devices that are being preserved by drivers. Since this
    support is still under development, hide it behind a new Kconfig
    PCI_LIVEUPDATE that is marked experimental.
    
    This API will be used in subsequent commits by the vfio-pci driver to
    preserve VFIO devices across Live Update.
    
    [Zelin Deng: 1. defined cmp_int, 2. use kabi field.]
    
    Signed-off-by: David Matlack <dmatlack@google.com>
    Signed-off-by: Zelin Deng <zelin.deng@linux.alibaba.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Reviewed-by: Shuai Xue <xueshuai@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6709

--- include/linux/pci.h
+++ include/linux/pci.h
@@ -40,6 +40,7 @@
 #include <linux/resource_ext.h>
 #include <linux/msi_api.h>
 #include <uapi/linux/pci.h>
+#include <linux/liveupdate.h>
 
 #include <linux/pci_ids.h>
 
@@ -549,7 +550,8 @@ struct pci_dev {
 	u8		tph_req_type;	/* TPH requester type */
 #endif
 
-	CK_KABI_RESERVE(1)
+	CK_KABI_USE_SPLIT(1, unsigned int liveupdate_incoming:1); /* Preserved by previous kernel */
+	CK_KABI_USE_SPLIT(1, unsigned int liveupdate_outgoing:1); /* Preserved for next kernel */
 	CK_KABI_RESERVE(2)
 	CK_KABI_RESERVE(3)
 	CK_KABI_RESERVE(4)
@@ -2740,5 +2742,41 @@ void pci_ims_free_irq(struct pci_dev *pdev, struct msi_map map);
 #define pci_WARN_ONCE(pdev, condition, fmt, arg...) \
 	WARN_ONCE(condition, "%s %s: " fmt, \
 		  dev_driver_string(&(pdev)->dev), pci_name(pdev), ##arg)
+
+#ifdef CONFIG_PCI_LIVEUPDATE
+int pci_liveupdate_preserve(struct pci_dev *dev);
+void pci_liveupdate_unpreserve(struct pci_dev *dev);
+int pci_liveupdate_retrieve(struct pci_dev *dev);
+void pci_liveupdate_finish(struct pci_dev *dev);
+int pci_liveupdate_register_flb(struct liveupdate_file_handler *fh);
+void pci_liveupdate_unregister_flb(struct liveupdate_file_handler *fh);
+#else
+static inline int pci_liveupdate_preserve(struct pci_dev *dev)
+{
+	return -EOPNOTSUPP;
+}
+
+static inline void pci_liveupdate_unpreserve(struct pci_dev *dev)
+{
+}
+
+static inline int pci_liveupdate_retrieve(struct pci_dev *dev)
+{
+	return -EOPNOTSUPP;
+}
+
+static inline void pci_liveupdate_finish(struct pci_dev *dev)
+{
+}
+
+static inline int pci_liveupdate_register_flb(struct liveupdate_file_handler *fh)
+{
+	return -EOPNOTSUPP;
+}
+
+static inline void pci_liveupdate_unregister_flb(struct liveupdate_file_handler *fh)
+{
+}
+#endif
 
 #endif /* LINUX_PCI_H */
