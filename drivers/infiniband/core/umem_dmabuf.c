0b24d540a1b77685186f5d4b57c754547c616533
Leon Romanovsky <leonro@nvidia.com>
2026-01-31 07:34:15 +0200

    dma-buf: Make .invalidate_mapping() truly optional
    ANBZ: #31969
    
    commit 575157b1b5fcfcdd0f9c42d635dcf8219f8c86dc upstream.
    
    The .invalidate_mapping() callback is documented as optional, yet it
    effectively became mandatory whenever importer_ops were provided. This
    led to cases where RDMA non-ODP code had to supply an empty stub.
    
    Relax the checks in the dma-buf core so the callback can be omitted,
    allowing RDMA code to drop the unnecessary function.
    
    Removing the stub allows the next patch to tell that RDMA does not support
    .invalidate_mapping() by checking for a NULL op.
    
    Signed-off-by: Leon Romanovsky <leonro@nvidia.com>
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Christian König <christian.koenig@amd.com>
    Link: https://lore.kernel.org/r/20260131-dmabuf-revoke-v7-5-463d956bd527@nvidia.com
    Signed-off-by: Ruidong Tian <tianruidong@linux.alibaba.com>
    Reviewed-by: Shuai Xue <xueshuai@linux.alibaba.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6864

--- drivers/infiniband/core/umem_dmabuf.c
+++ drivers/infiniband/core/umem_dmabuf.c
@@ -170,18 +170,8 @@ out_release_dmabuf:
 }
 EXPORT_SYMBOL(ib_umem_dmabuf_get);
 
-static void
-ib_umem_dmabuf_unsupported_move_notify(struct dma_buf_attachment *attach)
-{
-	struct ib_umem_dmabuf *umem_dmabuf = attach->importer_priv;
-
-	ibdev_warn_ratelimited(umem_dmabuf->umem.ibdev,
-			       "Invalidate callback should not be called when memory is pinned\n");
-}
-
 static struct dma_buf_attach_ops ib_umem_dmabuf_attach_pinned_ops = {
 	.allow_peer2peer = true,
-	.invalidate_mappings = ib_umem_dmabuf_unsupported_move_notify,
 };
 
 struct ib_umem_dmabuf *ib_umem_dmabuf_get_pinned(struct ib_device *device,
