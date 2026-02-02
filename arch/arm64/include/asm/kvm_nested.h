d5054bb6eddfb97b1fcbfb1f0d360548c31e4989
Marc Zyngier <maz@kernel.org>
2026-02-02 02:51:17 +0000

    KVM: arm64: nv: Tag shadow S2 entries with guest's leaf S2 level
    ANBZ: #31782
    
    commit b1a3a94812b95fb8ae410d1ca04a4cc3d61a7503 upstream.
    
    Populate bits [56:55] of the leaf entry with the level provided
    by the guest's S2 translation. This will allow us to better scope
    the invalidation by remembering the mapping size.
    
    Of course, this assume that the guest will issue an invalidation
    with an address that falls into the same leaf. If the guest doesn't,
    we'll over-invalidate.
    
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Link: https://lore.kernel.org/r/20240614144552.2773592-13-maz@kernel.org
    Signed-off-by: Oliver Upton <oliver.upton@linux.dev>
    Signed-off-by: Jia He <justin.he@arm.com>
    Reviewed-by: Bin Guo <guobin@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6809

--- arch/arm64/include/asm/kvm_nested.h
+++ arch/arm64/include/asm/kvm_nested.h
@@ -5,6 +5,7 @@
 #include <linux/bitfield.h>
 #include <asm/kvm_emulate.h>
 #include <linux/kvm_host.h>
+#include <asm/kvm_pgtable.h>
 
 static inline bool vcpu_has_nv(const struct kvm_vcpu *vcpu)
 {
@@ -201,5 +202,12 @@ static inline bool kvm_auth_eretax(struct kvm_vcpu *vcpu, u64 *elr)
 		BUG_ON(__c >= NR_CPUS);				\
 		(FIX_VNCR - __c);				\
 	})
+
+#define KVM_NV_GUEST_MAP_SZ	(KVM_PGTABLE_PROT_SW1 | KVM_PGTABLE_PROT_SW0)
+
+static inline u64 kvm_encode_nested_level(struct kvm_s2_trans *trans)
+{
+	return FIELD_PREP(KVM_NV_GUEST_MAP_SZ, trans->level);
+}
 
 #endif /* __ARM64_KVM_NESTED_H */
