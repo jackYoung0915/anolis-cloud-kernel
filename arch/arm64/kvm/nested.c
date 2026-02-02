798dd8b9c05203d85ce6c52a20fa9d760728ac3e
Fuad Tabba <tabba@google.com>
2026-02-02 15:22:53 +0000

    KVM: arm64: nv: Avoid NV stage-2 code when NV is not supported
    ANBZ: #31782
    
    commit 0c4762e26879acc101790269382f230f22fd6905 upstream.
    
    The NV stage-2 manipulation functions kvm_nested_s2_unmap(),
    kvm_nested_s2_wp(), and others, are being called for any stage-2
    manipulation regardless of whether nested virtualization is supported or
    enabled for the VM.
    
    For protected KVM (pKVM), `struct kvm_pgtable` uses the
    `pkvm_mappings` member of the union. This member aliases `ia_bits`,
    which is used by the non-protected NV code paths. Attempting to
    read `pgt->ia_bits` in these functions results in treating
    protected mapping pointers or state values as bit-shift amounts.
    This triggers a UBSAN shift-out-of-bounds error:
    
        UBSAN: shift-out-of-bounds in arch/arm64/kvm/nested.c:1127:34
        shift exponent 174565952 is too large for 64-bit type 'unsigned long'
        Call trace:
         __ubsan_handle_shift_out_of_bounds+0x28c/0x2c0
         kvm_nested_s2_unmap+0x228/0x248
         kvm_arch_flush_shadow_memslot+0x98/0xc0
         kvm_set_memslot+0x248/0xce0
    
    Since pKVM and NV are mutually exclusive, prevent entry into these
    NV handling functions if the VM has not allocated any nested MMUs
    (i.e., `kvm->arch.nested_mmus_size` is 0).
    
    Fixes: 7270cc9157f47 ("KVM: arm64: nv: Handle VNCR_EL2 invalidation from MMU notifiers")
    Suggested-by: Marc Zyngier <maz@kernel.org>
    Signed-off-by: Fuad Tabba <tabba@google.com>
    Link: https://patch.msgid.link/20260202152310.113467-1-tabba@google.com
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Signed-off-by: Jia He <justin.he@arm.com>

--- arch/arm64/kvm/nested.c
+++ arch/arm64/kvm/nested.c
@@ -1056,6 +1056,9 @@ void kvm_nested_s2_wp(struct kvm *kvm)
 
 	lockdep_assert_held_write(&kvm->mmu_lock);
 
+	if (!kvm->arch.nested_mmus_size)
+		return;
+
 	for (i = 0; i < kvm->arch.nested_mmus_size; i++) {
 		struct kvm_s2_mmu *mmu = &kvm->arch.nested_mmus[i];
 
@@ -1071,6 +1074,9 @@ void kvm_nested_s2_unmap(struct kvm *kvm, bool may_block)
 	int i;
 
 	lockdep_assert_held_write(&kvm->mmu_lock);
+
+	if (!kvm->arch.nested_mmus_size)
+		return;
 
 	for (i = 0; i < kvm->arch.nested_mmus_size; i++) {
 		struct kvm_s2_mmu *mmu = &kvm->arch.nested_mmus[i];
@@ -1088,6 +1094,9 @@ void kvm_nested_s2_flush(struct kvm *kvm)
 
 	lockdep_assert_held_write(&kvm->mmu_lock);
 
+	if (!kvm->arch.nested_mmus_size)
+		return;
+
 	for (i = 0; i < kvm->arch.nested_mmus_size; i++) {
 		struct kvm_s2_mmu *mmu = &kvm->arch.nested_mmus[i];
 
@@ -1099,6 +1108,9 @@ void kvm_nested_s2_flush(struct kvm *kvm)
 void kvm_arch_flush_shadow_all(struct kvm *kvm)
 {
 	int i;
+
+	if (!kvm->arch.nested_mmus_size)
+		return;
 
 	for (i = 0; i < kvm->arch.nested_mmus_size; i++) {
 		struct kvm_s2_mmu *mmu = &kvm->arch.nested_mmus[i];
