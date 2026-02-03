92a372cdd3aacbd1677706973d88eb675bf947f0
Fuad Tabba <tabba@google.com>
2026-02-03 06:44:16 +0000

    KVM: arm64: Specialize handling of host fpsimd state on trap
    ANBZ: #31782
    
    commit e511e08a9f496948b13aac50610f2d17335f56c3 upstream.
    
    In subsequent patches, n/vhe will diverge on saving the host
    fpsimd/sve state when taking a guest fpsimd/sve trap. Add a
    specialized helper to handle it.
    
    No functional change intended.
    
    Reviewed-by: Mark Brown <broonie@kernel.org>
    Reviewed-by: Oliver Upton <oliver.upton@linux.dev>
    Signed-off-by: Fuad Tabba <tabba@google.com>
    Link: https://lore.kernel.org/r/20240603122852.3923848-5-tabba@google.com
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Signed-off-by: Jia He <justin.he@arm.com>

--- arch/arm64/kvm/hyp/vhe/switch.c
+++ arch/arm64/kvm/hyp/vhe/switch.c
@@ -312,6 +312,11 @@ static bool kvm_hyp_handle_sysreg_vhe(struct kvm_vcpu *vcpu, u64 *exit_code)
 	return kvm_hyp_handle_sysreg(vcpu, exit_code);
 }
 
+static void kvm_hyp_save_fpsimd_host(struct kvm_vcpu *vcpu)
+{
+	__fpsimd_save_state(*host_data_ptr(fpsimd_state));
+}
+
 static const exit_handler_fn hyp_exit_handlers[] = {
 	[0 ... ESR_ELx_EC_MAX]		= NULL,
 	[ESR_ELx_EC_CP15_32]		= kvm_hyp_handle_cp15_32,
