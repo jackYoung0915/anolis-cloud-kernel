ekuiter/torte-linux

platform/x86/intel/ifs: Add Clearwater Forest to CPU support list
Add Clearwater Forest (INTEL_ATOM_DARKMONT_X) to the x86 match table of
Intel In Field Scan (IFS) driver, enabling IFS functionality on this
processor.

Signed-off-by: Jithu Joseph <jithu.joseph@intel.com>
Link: https://lore.kernel.org/r/20241210203152.1136463-1-jithu.joseph@intel.com
Reviewed-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>
Signed-off-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>

drivers/platform/x86/intel/ifs/core.c
@@ -20,6 +20,7 @@ static const struct x86_cpu_id ifs_cpu_ids[] __initconst = {
 	X86_MATCH(INTEL_GRANITERAPIDS_X, ARRAY_GEN0),
 	X86_MATCH(INTEL_GRANITERAPIDS_D, ARRAY_GEN0),
 	X86_MATCH(INTEL_ATOM_CRESTMONT_X, ARRAY_GEN1),
+	X86_MATCH(INTEL_ATOM_DARKMONT_X, ARRAY_GEN1),
 	{}
 };
 MODULE_DEVICE_TABLE(x86cpu, ifs_cpu_ids);

jackYoung0915/anolis-cloud-kernel

ANBZ: #25041
commit 6c0a473fc5f89dabbed0af605a09370b533aa856 upstream.
Intel-SIG: commit 6c0a473fc5f8 platform/x86/intel/ifs: Add Clearwater Forest to CPU support list
Add IFS support for ClearWater Forest
[ Zhang Rui: resolve conflict (X86_MATCH changed) and amend commit log ]
Signed-off-by: Zhang Rui <rui.zhang@intel.com>
Reviewed-by: Guanghui Feng <guanghuifeng@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/5782

drivers/platform/x86/intel/ifs/core.c
@@ -21,6 +21,7 @@ static const struct x86_cpu_id ifs_cpu_ids[] __initconst = {
 	X86_MATCH(GRANITERAPIDS_X, ARRAY_GEN0),
 	X86_MATCH(GRANITERAPIDS_D, ARRAY_GEN0),
 	X86_MATCH(ATOM_CRESTMONT_X, ARRAY_GEN1),
+	X86_MATCH(ATOM_DARKMONT_X, ARRAY_GEN1),
 	{}
 };
 MODULE_DEVICE_TABLE(x86cpu, ifs_cpu_ids);