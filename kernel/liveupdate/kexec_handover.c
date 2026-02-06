f6c0ddafdea085e8cc16d2d57ae526e8805b3b08
Jason Miu <jasonmiu@google.com>
2026-02-05 18:14:28 -0800

    kho: remove finalize state and clients
    ANBZ: #26808
    
    commit 6b0dd42d7681af148e13df3806f251bc3dc7c36e upstream.
    
    Eliminate the `kho_finalize()` function and its associated state from the
    KHO subsystem.  The transition to a radix tree for memory tracking makes
    the explicit "finalize" state and its serialization step obsolete.
    
    Remove the `kho_finalize()` and `kho_finalized()` APIs and their stub
    implementations.  Update KHO client code and the debugfs interface to no
    longer call or depend on the `kho_finalize()` mechanism.
    
    Complete the move towards a stateless KHO, simplifying the overall design
    by removing unnecessary state management.
    
    Link: https://lkml.kernel.org/r/20260206021428.3386442-3-jasonmiu@google.com
    Signed-off-by: Jason Miu <jasonmiu@google.com>
    Reviewed-by: Pasha Tatashin <pasha.tatashin@soleen.com>
    Reviewed-by: Mike Rapoport (Microsoft) <rppt@kernel.org>
    Cc: Alexander Graf <graf@amazon.com>
    Cc: Baoquan He <bhe@redhat.com>
    Cc: Changyuan Lyu <changyuanl@google.com>
    Cc: David Matlack <dmatlack@google.com>
    Cc: David Rientjes <rientjes@google.com>
    Cc: Jason Gunthorpe <jgg@nvidia.com>
    Cc: Pratyush Yadav <pratyush@kernel.org>
    Cc: Ran Xiaokai <ran.xiaokai@zte.com.cn>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Zelin Deng <zelin.deng@linux.alibaba.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/7648

--- kernel/liveupdate/kexec_handover.c
+++ kernel/liveupdate/kexec_handover.c
@@ -69,8 +69,7 @@ early_param("kho", kho_parse_enable);
 
 struct kho_out {
 	void *fdt;
-	bool finalized;
-	struct mutex lock; /* protects KHO FDT finalization */
+	struct mutex lock; /* protects KHO FDT */
 
 	struct kho_radix_tree radix_tree;
 	struct kho_debugfs dbg;
@@ -81,7 +80,6 @@ static struct kho_out kho_out = {
 	.radix_tree = {
 		.lock = __MUTEX_INITIALIZER(kho_out.radix_tree.lock),
 	},
-	.finalized = false,
 };
 
 /**
@@ -1245,23 +1243,6 @@ void kho_restore_free(void *mem)
 		folio_put(folio);
 }
 EXPORT_SYMBOL_GPL(kho_restore_free);
-
-int kho_finalize(void)
-{
-	if (!kho_enable)
-		return -EOPNOTSUPP;
-
-	guard(mutex)(&kho_out.lock);
-	kho_out.finalized = true;
-
-	return 0;
-}
-
-bool kho_finalized(void)
-{
-	guard(mutex)(&kho_out.lock);
-	return kho_out.finalized;
-}
 
 struct kho_in {
 	phys_addr_t fdt_phys;
