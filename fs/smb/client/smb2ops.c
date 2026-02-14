c7f88aac77012a42e5fbdc73bf986bfab01718ed
Shyam Prasad N <sprasad@microsoft.com>
2026-02-14 18:29:13 +0800

    cifs: some missing initializations on replay
    ANBZ: #31324
    
    commit c854ab481ece4b3e5f4c2e8b22824f015ff874a5 stable.
    
    commit 14f66f44646333d2bfd7ece36585874fd72f8286 upstream.
    
    In several places in the code, we have a label to signify
    the start of the code where a request can be replayed if
    necessary. However, some of these places were missing the
    necessary reinitializations of certain local variables
    before replay.
    
    This change makes sure that these variables get initialized
    after the label.
    
    Cc: stable@vger.kernel.org
    Reported-by: Yuchan Nam <entropy1110@gmail.com>
    Tested-by: Yuchan Nam <entropy1110@gmail.com>
    Signed-off-by: Shyam Prasad N <sprasad@microsoft.com>
    Signed-off-by: Steve French <stfrench@microsoft.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    
    Signed-off-by: PatchPilot <patchpilot@openanolis.org>
    Signed-off-by: eileen <huangy2023@163.com>

--- fs/smb/client/smb2ops.c
+++ fs/smb/client/smb2ops.c
@@ -1138,6 +1138,7 @@ smb2_set_ea(const unsigned int xid, struct cifs_tcon *tcon,
 
 replay_again:
 	/* reinitialize for possible replay */
+	used_len = 0;
 	flags = CIFS_CP_CREATE_CLOSE_OP;
 	oplock = SMB2_OPLOCK_LEVEL_NONE;
 	server = cifs_pick_channel(ses);
@@ -1536,6 +1537,7 @@ smb2_ioctl_query_info(const unsigned int xid,
 
 replay_again:
 	/* reinitialize for possible replay */
+	buffer = NULL;
 	flags = CIFS_CP_CREATE_CLOSE_OP;
 	oplock = SMB2_OPLOCK_LEVEL_NONE;
 	server = cifs_pick_channel(ses);
