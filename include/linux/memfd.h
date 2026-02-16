9711e2e1e65cf82d130918e0c39c049770adbbae
Pratyush Yadav (Google) <pratyush@kernel.org>
2026-02-16 19:59:32 +0100

    memfd: export memfd_{add,get}_seals()
    ANBZ: #26808
    
    cherry-picked from https://lore.kernel.org/all/20260216185946.1215770-2-pratyush@kernel.org/
    
    Support for preserving file seals will be added to memfd preservation
    using the Live Update Orchestrator (LUO). Export
    memfd_{add,get}_seals)() so memfd_luo can use them to manipulate the
    seals.
    
    Signed-off-by: Pratyush Yadav (Google) <pratyush@kernel.org>
    Acked-by: Mike Rapoport (Microsoft) <rppt@kernel.org>
    Signed-off-by: Zelin Deng <zelin.deng@linux.alibaba.com>
    Reviewed-by: Jason Gunthorpe <jgg@nvidia.com>
    Reviewed-by: Kevin Tian <kevin.tian@intel.com>
    Reviewed-by: Vasant Hegde <vasant.hegde@amd.com>
    Reviewed-by: Christophe Leroy <christophe.leroy@csgroup.eu>
    Reviewed-by: Lu Baolu <baolu.lu@linux.intel.com>
    Reviewed-by: Randy Dunlap <rdunlap@infradead.org>
    Reviewed-by: Rae Moar <rmoar@google.com>
    Reviewed-by: David Gow <davidgow@google.com>
    Reviewed-by: Kees Cook <kees@kernel.org>
    Reviewed-by: Maxime Ripard <mripard@kernel.org>
    Reviewed-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Reviewed-by: Guanghui Feng <guanghuifeng@linux.alibaba.com>
    Reviewed-by: Shuai Xue <xueshuai@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6802

--- include/linux/memfd.h
+++ include/linux/memfd.h
@@ -9,6 +9,8 @@ extern long memfd_fcntl(struct file *file, unsigned int cmd, unsigned int arg);
 struct folio *memfd_alloc_folio(struct file *memfd, pgoff_t idx);
 struct file *memfd_alloc_file(const char *name, unsigned int flags);
 unsigned int *memfd_file_seals_ptr(struct file *file);
+int memfd_get_seals(struct file *file);
+int memfd_add_seals(struct file *file, unsigned int seals);
 #else
 static inline long memfd_fcntl(struct file *f, unsigned int c, unsigned int a)
 {
@@ -26,6 +28,16 @@ static inline struct file *memfd_alloc_file(const char *name, unsigned int flags
 static inline unsigned int *memfd_file_seals_ptr(struct file *file)
 {
 	return NULL;
+}
+
+static inline int memfd_get_seals(struct file *file)
+{
+	return -EINVAL;
+}
+
+static inline int memfd_add_seals(struct file *file, unsigned int seals)
+{
+	return -EINVAL;
 }
 #endif
 
