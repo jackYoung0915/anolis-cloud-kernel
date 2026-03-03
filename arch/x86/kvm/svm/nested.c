67005a0c3f20c0c26b0b907bb8cce4985e49c3b0
Yosry Ahmed <yosry@kernel.org>
2026-03-03 00:33:55 +0000

    KVM: nSVM: Avoid clearing VMCB_LBR in vmcb12
    ANBZ: #36296
    
    commit b53ab5167a81537777ac780bbd93d32613aa3bda upstream.
    
    svm_copy_lbrs() always marks VMCB_LBR dirty in the destination VMCB.
    However, nested_svm_vmexit() uses it to copy LBRs to vmcb12, and
    clearing clean bits in vmcb12 is not architecturally defined.
    
    Move vmcb_mark_dirty() to callers and drop it for vmcb12.
    
    This also facilitates incoming refactoring that does not pass the entire
    VMCB to svm_copy_lbrs().
    
    Fixes: d20c796ca370 ("KVM: x86: nSVM: implement nested LBR virtualization")
    Cc: stable@vger.kernel.org
    Signed-off-by: Yosry Ahmed <yosry@kernel.org>
    Link: https://patch.msgid.link/20260303003421.2185681-2-yosry@kernel.org
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    
    [Fixes conflicts by PatchPilot]
    Fixes: CVE-2026-46071
    Assisted-by: PatchPilot <patchpilot@openanolis.org>
    Signed-off-by: Xunlei Pang <xlpang@linux.alibaba.com>
    Reviewed-by: Guanghui Feng <guanghuifeng@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/8260

--- arch/x86/kvm/svm/nested.c
+++ arch/x86/kvm/svm/nested.c
@@ -626,6 +626,7 @@ static void nested_vmcb02_prepare_save(struct vcpu_svm *svm, struct vmcb *vmcb12
 	} else {
 		svm_copy_lbrs(vmcb02, vmcb01);
 	}
+	vmcb_mark_dirty(vmcb02, VMCB_LBR);
 	svm_update_lbrv(&svm->vcpu);
 }
 
@@ -1086,10 +1087,12 @@ int nested_svm_vmexit(struct vcpu_svm *svm)
 		kvm_make_request(KVM_REQ_EVENT, &svm->vcpu);
 
 	if (unlikely(guest_can_use(vcpu, X86_FEATURE_LBRV) &&
-		     (svm->nested.ctl.virt_ext & LBR_CTL_ENABLE_MASK)))
+		     (svm->nested.ctl.virt_ext & LBR_CTL_ENABLE_MASK))) {
 		svm_copy_lbrs(vmcb12, vmcb02);
-	else
+	} else {
 		svm_copy_lbrs(vmcb01, vmcb02);
+		vmcb_mark_dirty(vmcb01, VMCB_LBR);
+	}
 
 	svm_update_lbrv(vcpu);
 
