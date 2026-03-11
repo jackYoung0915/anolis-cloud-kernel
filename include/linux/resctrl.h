eb4fc9d223655327af8af1e4754d3f502097e5fd
Jason Zeng <jason.zeng@intel.com>
2026-03-11 01:02:44 -0700

    anolis: Revert "x86,fs/resctrl: Implement "io_alloc" enable/disable handlers"
    ANBZ: #31045
    
    This reverts commit d25ffa678fc7b92983e28dcc8cb81d729a8b259d.
    
    Signed-off-by: Jason Zeng <jason.zeng@intel.com>
    Reviewed-by: Qinyun Tan <qinyuntan@linux.alibaba.com>
    Reviewed-by: Xiaochen Shen <shenxiaochen@hygon.cn>
    Reviewed-by: Jiankang Chen <jkchen@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6611

--- include/linux/resctrl.h
+++ include/linux/resctrl.h
@@ -440,27 +440,6 @@ void resctrl_arch_reset_rmid(struct rdt_resource *r, struct rdt_domain *d,
  */
 void resctrl_arch_reset_rmid_all(struct rdt_resource *r, struct rdt_domain *d);
 
-/**
- * resctrl_arch_io_alloc_enable() - Enable/disable io_alloc feature.
- * @r:		The resctrl resource.
- * @enable:	Enable (true) or disable (false) io_alloc on resource @r.
- *
- * This can be called from any CPU.
- *
- * Return:
- * 0 on success, <0 on error.
- */
-int resctrl_arch_io_alloc_enable(struct rdt_resource *r, bool enable);
-
-/**
- * resctrl_arch_get_io_alloc_enabled() - Get io_alloc feature state.
- * @r:		The resctrl resource.
- *
- * Return:
- * true if io_alloc is enabled or false if disabled.
- */
-bool resctrl_arch_get_io_alloc_enabled(struct rdt_resource *r);
-
 extern unsigned int resctrl_rmid_realloc_threshold;
 extern unsigned int resctrl_rmid_realloc_limit;
 void resctrl_file_fflags_init(const char *config, unsigned long fflags);
