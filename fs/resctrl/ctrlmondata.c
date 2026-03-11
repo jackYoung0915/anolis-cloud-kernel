5c4afb8ccecc28feb470cbfaaedb9f2a4a684b5f
Jason Zeng <jason.zeng@intel.com>
2026-03-11 01:02:36 -0700

    anolis: Revert "fs/resctrl: Modify struct rdt_parse_data to pass mode and CLOSID"
    ANBZ: #31045
    
    This reverts commit 1e50ca047f6079c75da29582b6b644af602d63d8.
    
    Signed-off-by: Jason Zeng <jason.zeng@intel.com>

--- fs/resctrl/ctrlmondata.c
+++ fs/resctrl/ctrlmondata.c
@@ -22,8 +22,7 @@
 #include "internal.h"
 
 struct rdt_parse_data {
-	u32			closid;
-	enum rdtgrp_mode	mode;
+	struct rdtgroup *rdtgrp;
 	char *buf;
 };
 
@@ -75,8 +74,8 @@ static int parse_bw(struct rdt_parse_data *data, struct resctrl_schema *s,
 		    struct rdt_domain *d)
 {
 	struct resctrl_staged_config *cfg;
+	u32 closid = data->rdtgrp->closid;
 	struct rdt_resource *r = s->res;
-	u32 closid = data->closid;
 	u32 bw_val;
 
 	cfg = &d->staged_config[s->conf_type];
@@ -154,10 +153,9 @@ static bool cbm_validate(char *buf, u32 *data, struct rdt_resource *r)
 static int parse_cbm(struct rdt_parse_data *data, struct resctrl_schema *s,
 		     struct rdt_domain *d)
 {
-	enum rdtgrp_mode mode = data->mode;
+	struct rdtgroup *rdtgrp = data->rdtgrp;
 	struct resctrl_staged_config *cfg;
 	struct rdt_resource *r = s->res;
-	u32 closid = data->closid;
 	u32 cbm_val;
 
 	cfg = &d->staged_config[s->conf_type];
@@ -170,7 +168,7 @@ static int parse_cbm(struct rdt_parse_data *data, struct resctrl_schema *s,
 	 * Cannot set up more than one pseudo-locked region in a cache
 	 * hierarchy.
 	 */
-	if (mode == RDT_MODE_PSEUDO_LOCKSETUP &&
+	if (rdtgrp->mode == RDT_MODE_PSEUDO_LOCKSETUP &&
 	    rdtgroup_pseudo_locked_in_hierarchy(d)) {
 		rdt_last_cmd_puts("Pseudo-locked region in hierarchy\n");
 		return -EINVAL;
@@ -179,7 +177,8 @@ static int parse_cbm(struct rdt_parse_data *data, struct resctrl_schema *s,
 	if (!cbm_validate(data->buf, &cbm_val, r))
 		return -EINVAL;
 
-	if ((mode == RDT_MODE_EXCLUSIVE || mode == RDT_MODE_SHAREABLE) &&
+	if ((rdtgrp->mode == RDT_MODE_EXCLUSIVE ||
+	     rdtgrp->mode == RDT_MODE_SHAREABLE) &&
 	    rdtgroup_cbm_overlaps_pseudo_locked(d, cbm_val)) {
 		rdt_last_cmd_puts("CBM overlaps with pseudo-locked region\n");
 		return -EINVAL;
@@ -189,14 +188,14 @@ static int parse_cbm(struct rdt_parse_data *data, struct resctrl_schema *s,
 	 * The CBM may not overlap with the CBM of another closid if
 	 * either is exclusive.
 	 */
-	if (rdtgroup_cbm_overlaps(s, d, cbm_val, closid, true)) {
+	if (rdtgroup_cbm_overlaps(s, d, cbm_val, rdtgrp->closid, true)) {
 		rdt_last_cmd_puts("Overlaps with exclusive group\n");
 		return -EINVAL;
 	}
 
-	if (rdtgroup_cbm_overlaps(s, d, cbm_val, closid, false)) {
-		if (mode == RDT_MODE_EXCLUSIVE ||
-		    mode == RDT_MODE_PSEUDO_LOCKSETUP) {
+	if (rdtgroup_cbm_overlaps(s, d, cbm_val, rdtgrp->closid, false)) {
+		if (rdtgrp->mode == RDT_MODE_EXCLUSIVE ||
+		    rdtgrp->mode == RDT_MODE_PSEUDO_LOCKSETUP) {
 			rdt_last_cmd_puts("Overlaps with other group\n");
 			return -EINVAL;
 		}
@@ -256,8 +255,7 @@ next:
 	list_for_each_entry(d, &r->domains, list) {
 		if (d->id == dom_id) {
 			data.buf = dom;
-			data.closid = rdtgrp->closid;
-			data.mode = rdtgrp->mode;
+			data.rdtgrp = rdtgrp;
 			if (parse_ctrlval(&data, s, d))
 				return -EINVAL;
 			if (rdtgrp->mode == RDT_MODE_PSEUDO_LOCKSETUP) {
