ffccc9f1c10927e990d91869c00241f417abaf37
Liming An <anliming1@h-partners.com>
2026-03-19 20:45:29 +0800

    iommu/ummu: Add deprecation markers for ummu SVA APIs
    ANBZ: #IJSBBS
    
    commit 8763959c3c0e0ef3b55e7963dd4f31ae2cd31f68 openEuler.
    Mark several UMMU SVA APIs as deprecated and indicate their
    replacement functions:
    
    - ummu_sva_grant_range -> iommu_sva_grant
    - ummu_sva_ungrant_range -> iommu_sva_ungrant
    - ummu_is_ksva -> iommu_is_ksva_domain
    - ummu_is_sva -> iommu_is_ksva_domain
    - ummu_sva_bind_device -> iommu_sva_bind_device_isolated
    - ummu_sva_unbind_device -> iommu_sva_unbind_device_isolated
    - ummu_ksva_bind_device -> iommu_ksva_bind_device
    - ummu_ksva_unbind_device -> iommu_ksva_unbind_device
    
    Fixes: 9db5eb9c30ff ("iommu/ummu-core: core interfaces for ummu drivers")
    
    Signed-off-by: Yanlong Zhu <zhuyanlong3@huawei.com>
    Signed-off-by: Liming An <anliming1@h-partners.com>
    Signed-off-by: qiushengming <qiushengming1@huawei.com>

--- include/linux/ummu_core.h
+++ include/linux/ummu_core.h
@@ -606,6 +606,7 @@ static inline int ummu_drain_pages(struct iova_slot *slot, dma_addr_t iova,
 /* UMMU SVA API */
 /**
  * ummu_sva_grant_range() - Grant va range permission to sva.
+ * @Deprecated: use iommu_sva_grant instead.
  * @sva: related sva handle.
  * @va: va start
  * @size: va size
@@ -630,6 +631,7 @@ int ummu_sva_grant_range(struct iommu_sva *sva, void *va, size_t size, int perm,
 
 /**
  * ummu_sva_ungrant_range() - Ungrant va range permission from sva.
+ * @Deprecated: use iommu_sva_ungrant instead.
  * @sva: related sva handle.
  * @va: va start
  * @size: va size
@@ -671,6 +673,7 @@ struct iommu_domain *ummu_core_get_domain_by_tid(struct device *dev,
 
 /**
  * ummu_is_ksva() - Check whether the UMMU works in ksva mode.
+ * @Deprecated: use iommu_is_ksva_domain instead.
  * @domain: related iommu domain
  *
  * Return: true or false.
@@ -679,6 +682,7 @@ bool ummu_is_ksva(struct iommu_domain *domain);
 
 /**
  * ummu_is_sva() - Check whether the UMMU works in sva mode.
+ * @Deprecated: use iommu_is_ksva_domain instead.
  * @domain: related iommu domain
  *
  * Return: true or false.
@@ -703,6 +707,7 @@ u32 ummu_sva_get_features(struct device *dev);
 
 /**
  * ummu_sva_bind_device() - Bind device to a process mm.
+ * @Deprecated: use iommu_sva_bind_device_isolated instead.
  * @dev: related device.
  * @mm: process memory management.
  * @drvdata: ummu_param related to tid.
@@ -722,6 +727,7 @@ struct iommu_sva *ummu_sva_bind_device(struct device *dev, struct mm_struct *mm,
 
 /**
  * ummu_ksva_bind_device() - Bind device to kernel mm.
+ * @Deprecated: use iommu_ksva_bind_device instead.
  * @dev: related device.
  * @drvdata: ummu_param related to tid. ksva doesn't support bypass mapt.
  *
@@ -729,7 +735,15 @@ struct iommu_sva *ummu_sva_bind_device(struct device *dev, struct mm_struct *mm,
  */
 struct iommu_sva *ummu_ksva_bind_device(struct device *dev,
 					struct ummu_param *drvdata);
+/**
+ * ummu_sva_unbind_device() - Unbind device to a process mm.
+ * @Deprecated: use iommu_sva_unbind_device_isolated instead.
+ */
 void ummu_sva_unbind_device(struct iommu_sva *handle);
+/**
+ * ummu_ksva_unbind_device() - Unbind device to kernel mm.
+ * @Deprecated: use iommu_ksva_unbind_device instead.
+ */
 void ummu_ksva_unbind_device(struct iommu_sva *handle);
 
 /* UMMU CORE API */
