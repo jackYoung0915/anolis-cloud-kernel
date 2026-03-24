c4eb63a65309b7c363c0be4bb06cbd183c96a10e
Fuad Tabba <tabba@google.com>
2026-03-24 08:01:13 +0000

    KVM: arm64: Fix prototype for __sve_save_state/__sve_restore_state
    ANBZ: #31782
    
    commit 45f4ea9bcfe909b3461059990b1e232e55dde809 upstream.
    
    Since the prototypes for __sve_save_state/__sve_restore_state at
    hyp were added, the underlying macro has acquired a third
    parameter for saving/restoring ffr.
    
    Fix the prototypes to account for the third parameter, and
    restore the ffr for the guest since it is saved.
    
    Suggested-by: Mark Brown <broonie@kernel.org>
    Signed-off-by: Fuad Tabba <tabba@google.com>
    Reviewed-by: Mark Brown <broonie@kernel.org>
    Link: https://lore.kernel.org/r/20240603122852.3923848-3-tabba@google.com
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Signed-off-by: Jia He <justin.he@arm.com>

--- arch/arm64/kvm/hyp/include/hyp/switch.h
+++ arch/arm64/kvm/hyp/include/hyp/switch.h
@@ -337,7 +337,8 @@ static inline void __hyp_sve_restore_guest(struct kvm_vcpu *vcpu)
 	 */
 	sve_cond_update_zcr_vq(vcpu_sve_max_vq(vcpu) - 1, SYS_ZCR_EL2);
 	__sve_restore_state(vcpu_sve_pffr(vcpu),
-			    &vcpu->arch.ctxt.fp_regs.fpsr);
+			    &vcpu->arch.ctxt.fp_regs.fpsr,
+			    true);
 
 	/*
 	 * The effective VL for a VM could differ from the max VL when running a
