ebd65c0142f2ea43b23fbcf052a3a8af9eeaa939
fuwenxin <fuwenxin@linux.alibaba.com>
2026-03-30 19:27:57 +0800

    iommu: debug-pagealloc: Check mapped/unmapped kernel  memory
    ANBZ: #32419
    
    commit a8258ffed2efdf533bdc756141eeb7bc5301ad4f upstream.
    
    Now, as the page_ext holds count of IOMMU mappings, we can use it to
    assert that any page allocated/freed is indeed not in the IOMMU.
    
    The sanitizer doesn’t protect against mapping/unmapping during this
    period. However, that’s less harmful as the page is not used by the
    kernel.
    
    Reviewed-by: Samiullah Khawaja <skhawaja@google.com>
    Reviewed-by: Lu Baolu <baolu.lu@linux.intel.com>
    Signed-off-by: Mostafa Saleh <smostafa@google.com>
    Reviewed-by: Pranjal Shrivastava <praan@google.com>
    Signed-off-by: Joerg Roedel <joerg.roedel@amd.com>
    Signed-off-by: fuwenxin <fuwenxin@linux.alibaba.com>
    Reviewed-by: Guanghui Feng <guanghuifeng@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6801

--- drivers/iommu/iommu-debug-pagealloc.c
+++ drivers/iommu/iommu-debug-pagealloc.c
@@ -9,6 +9,8 @@
 #include <linux/iommu-debug-pagealloc.h>
 #include <linux/kernel.h>
 #include <linux/page_ext.h>
+#include <linux/page_owner.h>
+#include <linux/pfn.h>
 
 #include "iommu-priv.h"
 
@@ -31,7 +33,7 @@ struct page_ext_operations page_iommu_debug_ops = {
 
 static struct page_ext *get_iommu_page_ext(phys_addr_t phys)
 {
-	struct page *page = phys_to_page(phys);
+	struct page *page = pfn_to_page(PHYS_PFN(phys));
 	struct page_ext *page_ext = page_ext_get(page);
 
 	return page_ext;
@@ -71,6 +73,28 @@ static void iommu_debug_dec_page(phys_addr_t phys)
 static size_t iommu_debug_page_size(struct iommu_domain *domain)
 {
 	return 1UL << __ffs(domain->pgsize_bitmap);
+}
+
+static bool iommu_debug_page_count(const struct page *page)
+{
+	unsigned int ref;
+	struct page_ext *page_ext = page_ext_get((struct page *)page);
+	struct iommu_debug_metadata *d = get_iommu_data(page_ext);
+
+	ref = atomic_read(&d->ref);
+	page_ext_put(page_ext);
+	return ref != 0;
+}
+
+void __iommu_debug_check_unmapped(const struct page *page, int numpages)
+{
+	while (numpages--) {
+		if (WARN_ON(iommu_debug_page_count(page))) {
+			pr_warn("iommu: Detected page leak!\n");
+			dump_page_owner(page);
+		}
+		page++;
+	}
 }
 
 void __iommu_debug_map(struct iommu_domain *domain, phys_addr_t phys, size_t size)
