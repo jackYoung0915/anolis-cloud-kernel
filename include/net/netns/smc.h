8620c9d21fc46a733e78b8951f6270d4bfddfdec
Bitao Hu <yaoma@linux.alibaba.com>
2026-03-30 18:59:38 +0800

    anolis: kabi: fix incorrect CK_KABI_REPLACE(CK_KABI_RESERVE()) usage breaking symvers
    ANBZ: #32385
    
    Several call sites use CK_KABI_REPLACE(CK_KABI_RESERVE(n), new_field)
    to consume reserved KABI slots. This is wrong because CK_KABI_RESERVE()
    is the public macro that appends a trailing semicolon, causing genksyms
    to see a double semicolon (';;') in the struct definition under the
    __GENKSYMS__ path. This changes the CRC of all symbols depending on the
    affected structs, silently breaking KABI stability.
    
    The cloud-kernel devel-6.6 branch does not enforce KABI compliance;
    therefore, KABI-related macros have been removed.
    
    Signed-off-by: Bitao Hu <yaoma@linux.alibaba.com>
    Reviewed-by: Guixin Liu <kanie@linux.alibaba.com>
    Acked-by: Joseph Qi <joseph.qi@linux.alibaba.com>
    Reviewed-by: Wen Gu <guwen@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6689

--- include/net/netns/smc.h
+++ include/net/netns/smc.h
@@ -36,11 +36,14 @@ struct netns_smc {
 	struct socket			*rsvd_sock[SMC_IWARP_RSVD_PORTS_NUM];
 	struct smc_dump_ctx		*dump_ctx;
 
-	CK_KABI_RESERVE(1)
-	CK_KABI_REPLACE(struct {CK_KABI_RESERVE(2); CK_KABI_RESERVE(3); CK_KABI_RESERVE(4); },
-			long sysctl_mem[3])
-	CK_KABI_REPLACE(CK_KABI_RESERVE(5), atomic_long_t memory_allocated)
+	long sysctl_mem[3];
+	atomic_long_t memory_allocated;
 
+	CK_KABI_RESERVE(1)
+	CK_KABI_RESERVE(2)
+	CK_KABI_RESERVE(3)
+	CK_KABI_RESERVE(4)
+	CK_KABI_RESERVE(5)
 	CK_KABI_RESERVE(6)
 	CK_KABI_RESERVE(7)
 	CK_KABI_RESERVE(8)
