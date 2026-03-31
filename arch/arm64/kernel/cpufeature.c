543fa86f6c422fd0d8d672feeaf482a9f4203727
Jia He <justin.he@arm.com>
2026-03-31 07:03:32 +0000

    arm64: cpufeature: Detect HCR_EL2.NV1 being RES0
    From: Marc Zyngier <maz@kernel.org>
    
    ANBZ: #31782
    
    [ Upstream commit da9af5071b25cb81682c3506c96516fe55186577 ]
    
    A variant of FEAT_E2H0 not being implemented exists in the form of
    HCR_EL2.E2H being RES1 *and* HCR_EL2.NV1 being RES0 (indicating that
    only VHE is supported on the host and nested guests).
    
    Add the necessary infrastructure for this new CPU capability.
    
    Reviewed-by: Suzuki K Poulose <suzuki.poulose@arm.com>
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Reviewed-by: Catalin Marinas <catalin.marinas@arm.com>
    Link: https://lore.kernel.org/r/20240122181344.258974-7-maz@kernel.org
    Signed-off-by: Oliver Upton <oliver.upton@linux.dev>
    Signed-off-by: Jia He <justin.he@arm.com>

--- arch/arm64/kernel/cpufeature.c
+++ arch/arm64/kernel/cpufeature.c
@@ -2874,6 +2874,13 @@ static const struct arm64_cpu_capabilities arm64_features[] = {
 		.cpu_enable = cpu_enable_fpmr,
 		ARM64_CPUID_FIELDS(ID_AA64PFR2_EL1, FPMR, IMP)
 	},
+	{
+		.desc = "NV1",
+		.capability = ARM64_HAS_HCR_NV1,
+		.type = ARM64_CPUCAP_SYSTEM_FEATURE,
+		.matches = has_nv1,
+		ARM64_CPUID_FIELDS_NEG(ID_AA64MMFR4_EL1, E2H0, NI_NV1)
+	},
 #ifdef CONFIG_ARM64_POE
 	{
 		.desc = "Stage-1 Permission Overlay Extension (S1POE)",
