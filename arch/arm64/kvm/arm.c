3f68cd8c5e015ef3504c42663c98d4f745375a08
Oliver Upton <oliver.upton@linux.dev>
2026-04-07 07:02:29 +0000

    KVM: arm64: Get rid of vCPU-scoped feature bitmap
    ANBZ: #31782
    
    commit 1de10b7d13a971f9ab90c5ed300b76d6e0c0db38 upstream.
    
    The vCPU-scoped feature bitmap was left in place a couple of releases
    ago in case the change to VM-scoped vCPU features broke anyone. Nobody
    has complained and the interop between VM and vCPU bitmaps is pretty
    gross. Throw it out.
    
    Link: https://lore.kernel.org/r/20230920195036.1169791-9-oliver.upton@linux.dev
    Signed-off-by: Oliver Upton <oliver.upton@linux.dev>
    [Backport comments]
    Since anolis has backported ("KVM: arm64: nv: Hoist vcpu_has_nv() into
    is_hyp_ctxt()"), see ANCK cb7965655104, adjust the changes in
    arch/arm64/include/asm/kvm_emulate.h
    Signed-off-by: Jia He <justin.he@arm.com>
    Reviewed-by: Bin Guo <guobin@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6809

--- arch/arm64/kvm/arm.c
+++ arch/arm64/kvm/arm.c
@@ -447,7 +447,6 @@ int kvm_arch_vcpu_create(struct kvm_vcpu *vcpu)
 
 	/* Force users to call KVM_ARM_VCPU_INIT */
 	vcpu_clear_flag(vcpu, VCPU_INITIALIZED);
-	bitmap_zero(vcpu->arch.features, KVM_VCPU_MAX_FEATURES);
 
 	vcpu->arch.mmu_page_cache.gfp_zero = __GFP_ZERO;
 
@@ -1434,7 +1433,8 @@ static bool kvm_vcpu_init_changed(struct kvm_vcpu *vcpu,
 {
 	unsigned long features = init->features[0];
 
-	return !bitmap_equal(vcpu->arch.features, &features, KVM_VCPU_MAX_FEATURES);
+	return !bitmap_equal(vcpu->kvm->arch.vcpu_features, &features,
+			     KVM_VCPU_MAX_FEATURES);
 }
 
 static int kvm_setup_vcpu(struct kvm_vcpu *vcpu)
@@ -1462,10 +1462,10 @@ static int __kvm_vcpu_set_target(struct kvm_vcpu *vcpu,
 	mutex_lock(&kvm->arch.config_lock);
 
 	if (test_bit(KVM_ARCH_FLAG_VCPU_FEATURES_CONFIGURED, &kvm->arch.flags) &&
-	    !bitmap_equal(kvm->arch.vcpu_features, &features, KVM_VCPU_MAX_FEATURES))
+	    kvm_vcpu_init_changed(vcpu, init))
 		goto out_unlock;
 
-	bitmap_copy(vcpu->arch.features, &features, KVM_VCPU_MAX_FEATURES);
+	bitmap_copy(kvm->arch.vcpu_features, &features, KVM_VCPU_MAX_FEATURES);
 
 	ret = kvm_setup_vcpu(vcpu);
 	if (ret)
@@ -1474,7 +1474,6 @@ static int __kvm_vcpu_set_target(struct kvm_vcpu *vcpu,
 	/* Now we know what it is, we can reset it. */
 	kvm_reset_vcpu(vcpu);
 
-	bitmap_copy(kvm->arch.vcpu_features, &features, KVM_VCPU_MAX_FEATURES);
 	set_bit(KVM_ARCH_FLAG_VCPU_FEATURES_CONFIGURED, &kvm->arch.flags);
 	vcpu_set_flag(vcpu, VCPU_INITIALIZED);
 	ret = 0;
