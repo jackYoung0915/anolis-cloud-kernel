507726a83d3df5627c6e8897ae5e8c82b8df7e83
Yahui Liu <liuyahui6@huawei.com>
2026-04-14 17:01:46 +0800

    ub:ubus: change the condition of io decoder creating page table or not
    ANBZ: #IJSBBS
    
    commit 7e13e6c7692c252d95cdd8638d5e1b9ecce93959 openEuler.
    
    Now whether io decoder will create page table is determined by
    ubc->cluster, but ub controller already has one configuration item that
    serves this purpose: CFG1_BASIC.Supported Feature.Decoder MATT
    jurisdiction. Use this config to determine whether io decoder will create
    page table or not.
    
    Fixes: abc591c50df5 ("ub:ubus: Supports decoder event processing")
    Signed-off-by: Yahui Liu <liuyahui6@huawei.com>
    Signed-off-by: qiushengming <qiushengming1@huawei.com>

--- include/ub/ubus/ubus.h
+++ include/ub/ubus/ubus.h
@@ -160,8 +160,22 @@ struct ub_port {
 	enum ub_link_state link_state;
 	u8 link_event;
 
+#ifndef __GENKSYMS__
+	union {
+		struct {
+			atomic_t port_mgmt_state;
+		};
+		u64 reserved1;
+	};
+#else
 	CK_KABI_RESERVE(1)
+#endif
+
+#ifndef __GENKSYMS__
+	struct mutex *port_lock;
+#else
 	CK_KABI_RESERVE(2)
+#endif
 };
 
 struct ue_map {
@@ -270,7 +284,17 @@ struct ub_entity {
 
 	u16 upi;
 
+#ifndef __GENKSYMS__
+	union {
+		struct {
+			atomic_t ent_mgmt_state;
+			u32 task_src;
+		};
+		u64 reserved1;
+	};
+#else
 	CK_KABI_RESERVE(1)
+#endif
 	CK_KABI_RESERVE(2)
 	CK_KABI_RESERVE(3)
 	CK_KABI_RESERVE(4)
@@ -291,13 +315,24 @@ struct ub_entity {
 /* UB bus error event callbacks */
 struct ub_error_handlers {
 	/* UB function reset prepare or completed */
+
+	/*
+	 * Deprecated: This function is going to be removed in future version.
+	 * Please use ub_reset_prepare_return() and ub_reset_done_with_pret()
+	 * instead, which provides more complete return value processing.
+	 */
 	void (*ub_reset_prepare)(struct ub_entity *uent);
 	void (*ub_reset_done)(struct ub_entity *uent);
 	ub_ers_result_t (*ub_error_detected)(struct ub_entity *uent, ub_channel_state_t state);
 	ub_ers_result_t (*ub_resource_enabled)(struct ub_entity *uent);
 
+#ifndef __GENKSYMS__
+	int (*ub_reset_prepare_return)(struct ub_entity *uent);
+	int (*ub_reset_done_with_pret)(struct ub_entity *uent, int pret);
+#else
 	CK_KABI_RESERVE(1)
 	CK_KABI_RESERVE(2)
+#endif
 	CK_KABI_RESERVE(3)
 	CK_KABI_RESERVE(4)
 };
@@ -379,7 +414,11 @@ struct ub_driver {
 	struct ub_dynids dynids;
 	bool driver_managed_dma;
 
+#ifndef __GENKSYMS__
+	int (*reinit)(struct ub_entity *uent);
+#else
 	CK_KABI_RESERVE(1)
+#endif
 	CK_KABI_RESERVE(2)
 	CK_KABI_RESERVE(3)
 	CK_KABI_RESERVE(4)
@@ -431,10 +470,10 @@ struct ub_bus_controller {
 	void *data;
 	struct dentry *debug_root;
 
-	CK_KABI_RESERVE(1)
-	CK_KABI_RESERVE(2)
-	CK_KABI_RESERVE(3)
-	CK_KABI_RESERVE(4)
+	CK_CK_KABI_RESERVE(1)
+	CK_CK_KABI_RESERVE(2)
+	CK_CK_KABI_RESERVE(3)
+	CK_CK_KABI_RESERVE(4)
 };
 
 struct ub_bus_instance_info {
@@ -457,8 +496,8 @@ struct ub_bus_instance {
 	struct list_head uents;
 	struct mutex lock;
 
-	CK_KABI_RESERVE(1)
-	CK_KABI_RESERVE(2)
+	CK_CK_KABI_RESERVE(1)
+	CK_CK_KABI_RESERVE(2)
 };
 
 #define ub_bi_is_dynamic(bi) ((bi)->info.type == UBUS_INSTANCE_DYNAMIC_SERVER \
@@ -484,7 +523,12 @@ enum ub_port_event {
 	UB_PORT_EVENT_LINK_DOWN,
 	UB_PORT_EVENT_LINK_UP,
 	UB_PORT_EVENT_RESET_PREPARE,
+#ifndef __GENKSYMS__
+	UB_PORT_EVENT_RESET_DONE,
+	UB_PORT_EVENT_RESET_FAILED
+#else
 	UB_PORT_EVENT_RESET_DONE
+#endif
 };
 
 struct ub_share_port_ops {
@@ -492,8 +536,8 @@ struct ub_share_port_ops {
 	void (*reset_done)(struct ub_entity *uent, u16 port_id);
 	void (*event_notify)(struct ub_entity *uent, u16 port_id, int event);
 
-	CK_KABI_RESERVE(1)
-	CK_KABI_RESERVE(2)
+	CK_CK_KABI_RESERVE(1)
+	CK_CK_KABI_RESERVE(2)
 };
 
 struct ub_vdm_pld {
@@ -567,11 +611,17 @@ struct ub_entity *ub_get_ent_by_guid(const struct ub_guid *guid);
  * @uent: UB entity.
  * @enable: Enable or disable.
  *
+ * Deprecated: This function is going to be removed in future version.
+ * Please use ub_entity_enable_return() instead, which provides more complete
+ * return value processing.
+ *
  * Enable or disable the communication channel between entity and user host.
  *
  * Context: Any context, it will take mutex_lock()/mutex_unlock().
  */
 void ub_entity_enable(struct ub_entity *uent, u8 enable);
+
+int ub_entity_enable_return(struct ub_entity *uent, u8 enable);
 
 /**
  * ub_set_user_info() - Initialize host information for the entity.
@@ -1040,6 +1090,8 @@ static inline struct ub_entity *ub_entity_get(struct ub_entity *uent)
 { return NULL; }
 static inline void ub_entity_put(struct ub_entity *uent) {}
 static inline void ub_entity_enable(struct ub_entity *uent, u8 enable) {}
+static inline int ub_entity_enable_return(struct ub_entity *uent, u8 enable)
+{ return -ENODEV; }
 static inline int ub_set_user_info(struct ub_entity *uent)
 { return -ENODEV; }
 static inline void ub_unset_user_info(struct ub_entity *uent) {}
