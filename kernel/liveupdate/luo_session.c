246ed27db5c88971c5def2e4e79bf2af533eea81
Pasha Tatashin <pasha.tatashin@soleen.com>
2026-04-15 19:37:38 +0000

    liveupdate: fix return value on session allocation failure
    ANBZ: #26808
    
    commit 0562b572ce591858749fc2f9477567e7e5c5d99f upstream.
    
    When session allocation fails during deserialization, the global 'err'
    variable was not updated before returning.  This caused subsequent calls
    to luo_session_deserialize() to incorrectly report success.
    
    Ensure 'err' is set to the error code from PTR_ERR(session).  This ensures
    that an error is correctly returned to userspace when it attempts to open
    /dev/liveupdate in the new kernel if deserialization failed.
    
    Link: https://lore.kernel.org/20260415193738.515491-1-pasha.tatashin@soleen.com
    Signed-off-by: Pasha Tatashin <pasha.tatashin@soleen.com>
    Reviewed-by: Pratyush Yadav (Google) <pratyush@kernel.org>
    Cc: David Matlack <dmatlack@google.com>
    Cc: Mike Rapoport <rppt@kernel.org>
    Cc: Samiullah Khawaja <skhawaja@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Zelin Deng <zelin.deng@linux.alibaba.com>
    Reviewed-by: Baolin Wang <baolin.wang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/7648

--- kernel/liveupdate/luo_session.c
+++ kernel/liveupdate/luo_session.c
@@ -514,11 +514,12 @@ int luo_session_deserialize(void)
 {
 	struct luo_session_header *sh = &luo_session_global.incoming;
 	static bool is_deserialized;
-	static int err;
+	static int saved_err;
+	int err;
 
 	/* If has been deserialized, always return the same error code */
 	if (is_deserialized)
-		return err;
+		return saved_err;
 
 	is_deserialized = true;
 	if (!sh->active)
@@ -548,7 +549,7 @@ int luo_session_deserialize(void)
 				(int)sizeof(sh->ser[i].name),
 				sh->ser[i].name, session);
 			err = PTR_ERR(session);
-			return err;
+			goto save_err;
 		}
 
 		err = luo_session_insert(sh, session);
@@ -556,7 +557,7 @@ int luo_session_deserialize(void)
 			pr_warn("Failed to insert session [%s] %pe\n",
 				session->name, ERR_PTR(err));
 			luo_session_free(session);
-			return err;
+			goto save_err;
 		}
 
 		scoped_guard(mutex, &session->mutex) {
@@ -566,7 +567,7 @@ int luo_session_deserialize(void)
 		if (err) {
 			pr_warn("Failed to deserialize files for session [%s] %pe\n",
 				session->name, ERR_PTR(err));
-			return err;
+			goto save_err;
 		}
 	}
 
@@ -575,6 +576,9 @@ int luo_session_deserialize(void)
 	sh->ser = NULL;
 
 	return 0;
+save_err:
+	saved_err = err;
+	return err;
 }
 
 int luo_session_serialize(void)
