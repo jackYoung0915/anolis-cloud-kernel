jackYoung0915/anolis-cloud-kernel

commit 11d097609da4c093ef3f722de5674d7f5edbc96e

xfs: take XFS_MMAPLOCK_EXCL xfs_file_write_zero_eof
ANBZ: #20084

commit acfbac776496f2093e9facf7876b4015ef8c3d1d upstream.

xfs_file_write_zero_eof is the only caller of xfs_zero_range that does
not take XFS_MMAPLOCK_EXCL (aka the invalidate lock).  Currently that
is actually the right thing, as an error in the iomap zeroing code will
also take the invalidate_lock to clean up, but to fix that deadlock we
need a consistent locking pattern first.

The only extra thing that XFS_MMAPLOCK_EXCL will lock out are read
pagefaults, which isn't really needed here, but also not actively
harmful.

[justin] use ASSERT in xfs_zero_range() instead of xfs_assert_ilocked() to save backport
effort.

Signed-off-by: Christoph Hellwig <hch@lst.de>
Reviewed-by: Darrick J. Wong <djwong@kernel.org>
Signed-off-by: Carlos Maiolino <cem@kernel.org>
Signed-off-by: Jia He <justin.he@arm.com>
Reviewed-by: Joseph Qi <joseph.qi@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/5129

fs/xfs/xfs_iomap.c
@@ -1429,6 +1429,8 @@ xfs_zero_range(
 {
 	struct inode		*inode = VFS_I(ip);
 
+	ASSERT(xfs_isilocked(ip, XFS_ILOCK_EXCL | XFS_MMAPLOCK_EXCL));
+
 	if (IS_DAX(inode))
 		return dax_zero_range(inode, pos, len, did_zero,
 				      &xfs_dax_write_iomap_ops);

fs/xfs/xfs_file.c
@@ -381,6 +381,7 @@ xfs_file_write_zero_eof(
 {
 	struct xfs_inode	*ip = XFS_I(iocb->ki_filp->f_mapping->host);
 	loff_t			isize;
+	int			error;
 
 	/*
 	 * We need to serialise against EOF updates that occur in IO completions
@@ -428,7 +429,12 @@ xfs_file_write_zero_eof(
 	}
 
 	trace_xfs_zero_eof(ip, isize, iocb->ki_pos - isize);
-	return xfs_zero_range(ip, isize, iocb->ki_pos - isize, NULL);
+
+	xfs_ilock(ip, XFS_MMAPLOCK_EXCL);
+	error = xfs_zero_range(ip, isize, iocb->ki_pos - isize, NULL);
+	xfs_iunlock(ip, XFS_MMAPLOCK_EXCL);
+
+	return error;
 }
 
 /*

commit 91f1ac1acd558d2ed71fe375bf8b1f1e77aba791

anolis: xfs: fix typo error in xfs_zero_range() assertion
ANBZ: #20084

Fix incorrect lock type in assertion, change XFS_ILOCK_EXCL to
XFS_IOLOCK_EXCL to match the expected lock state for the function.

Fixes: 11d097609da4 ("xfs: take XFS_MMAPLOCK_EXCL xfs_file_write_zero_eof")
Signed-off-by: Ferry Meng <mengferry@linux.alibaba.com>

fs/xfs/xfs_iomap.c
@@ -1438,7 +1438,7 @@ xfs_zero_range(
 {
 	struct inode		*inode = VFS_I(ip);
 
-	ASSERT(xfs_isilocked(ip, XFS_ILOCK_EXCL | XFS_MMAPLOCK_EXCL));
+	ASSERT(xfs_isilocked(ip, XFS_IOLOCK_EXCL | XFS_MMAPLOCK_EXCL));
 
 	if (IS_DAX(inode))
 		return dax_zero_range(inode, pos, len, did_zero,