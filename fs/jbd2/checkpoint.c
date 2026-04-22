jackYoung0915/anolis-cloud-kernel

commit c74ba6622f62e740223807f72db7cf3bd77f6d89

anolis: jbd2: create jbd2-ckpt thread for journal checkpoint
ANBZ: #8366

This is trying to do jbd2 checkpoint in a specific kernel thread, then
checkpoint won't be under io throttle control.

Signed-off-by: Joseph Qi <joseph.qi@linux.alibaba.com>
Signed-off-by: Jiufei Xue <jiufei.xue@linux.alibaba.com>
Reviewed-by: Gao Xiang <hsiangkao@linux.alibaba.com>
Link: https://gitee.com/anolis/cloud-kernel/pulls/2795

fs/jbd2/checkpoint.c
@@ -88,7 +88,18 @@ __releases(&journal->j_state_lock)
 			spin_unlock(&journal->j_list_lock);
 			write_unlock(&journal->j_state_lock);
 			if (chkpt) {
-				jbd2_log_do_checkpoint(journal);
+				DEFINE_WAIT(wait);
+
+				prepare_to_wait(
+					&journal->j_wait_done_checkpoint, &wait,
+					TASK_UNINTERRUPTIBLE);
+				mutex_unlock(&journal->j_checkpoint_mutex);
+				wake_up(&journal->j_wait_checkpoint);
+				schedule();
+				mutex_lock(&journal->j_checkpoint_mutex);
+				finish_wait(&journal->j_wait_done_checkpoint,
+					    &wait);
+				jbd2_debug(1, "wake up checkpoint thread.\n");
 			} else if (jbd2_cleanup_journal_tail(journal) <= 0) {
 				/*
 				 * We were able to recover space or the

fs/jbd2/journal.c
@@ -191,6 +191,9 @@ loop:
 	if (journal->j_flags & JBD2_UNMOUNT)
 		goto end_loop;
 
+	if (kthread_should_stop())
+		goto end_loop;
+
 	jbd2_debug(1, "commit_sequence=%u, commit_request=%u\n",
 		journal->j_commit_sequence, journal->j_commit_request);
 
@@ -261,9 +264,40 @@ end_loop:
 	return 0;
 }
 
+static int jbd2_checkpoint_thread(void *arg)
+{
+	journal_t *journal = arg;
+	DEFINE_WAIT(wait);
+
+	jbd2_debug(1, "%s\n", __func__);
+	journal->j_checkpoint_task = current;
+
+loop:
+	prepare_to_wait(&journal->j_wait_checkpoint, &wait,
+			TASK_INTERRUPTIBLE);
+	wake_up_all(&journal->j_wait_done_checkpoint);
+	schedule();
+	finish_wait(&journal->j_wait_checkpoint, &wait);
+
+	if (journal->j_flags & JBD2_UNMOUNT)
+		goto end_loop;
+
+	mutex_lock(&journal->j_checkpoint_mutex);
+	jbd2_log_do_checkpoint(journal);
+	mutex_unlock(&journal->j_checkpoint_mutex);
+
+	goto loop;
+
+end_loop:
+	journal->j_checkpoint_task = NULL;
+	wake_up_all(&journal->j_wait_done_checkpoint);
+	jbd2_debug(1, "%s exiting.\n", __func__);
+	return 0;
+}
+
 static int jbd2_journal_start_thread(journal_t *journal)
 {
-	struct task_struct *t;
+	struct task_struct *t, *t_ckpt;
 
 	t = kthread_run(kjournald2, journal, "jbd2/%s",
 			journal->j_devname);
@@ -271,6 +305,17 @@ static int jbd2_journal_start_thread(journal_t *journal)
 		return PTR_ERR(t);
 
 	wait_event(journal->j_wait_done_commit, journal->j_task != NULL);
+
+	t_ckpt = kthread_run(jbd2_checkpoint_thread, journal, "jbd2-ckpt/%s",
+			journal->j_devname);
+	if (IS_ERR(t_ckpt)) {
+		kthread_stop(t);
+		return PTR_ERR(t_ckpt);
+	}
+
+	wait_event(journal->j_wait_done_checkpoint,
+		   journal->j_checkpoint_task != NULL);
+
 	return 0;
 }
 
@@ -286,6 +331,14 @@ static void journal_kill_thread(journal_t *journal)
 		write_lock(&journal->j_state_lock);
 	}
 	write_unlock(&journal->j_state_lock);
+
+	while (journal->j_checkpoint_task) {
+		mutex_lock(&journal->j_checkpoint_mutex);
+		wake_up(&journal->j_wait_checkpoint);
+		wait_event(journal->j_wait_done_checkpoint,
+			   journal->j_checkpoint_task == NULL);
+		mutex_unlock(&journal->j_checkpoint_mutex);
+	}
 }
 
 /*
@@ -1584,6 +1637,8 @@ static journal_t *journal_init_common(struct block_device *bdev,
 
 	init_waitqueue_head(&journal->j_wait_transaction_locked);
 	init_waitqueue_head(&journal->j_wait_done_commit);
+	init_waitqueue_head(&journal->j_wait_checkpoint);
+	init_waitqueue_head(&journal->j_wait_done_checkpoint);
 	init_waitqueue_head(&journal->j_wait_commit);
 	init_waitqueue_head(&journal->j_wait_updates);
 	init_waitqueue_head(&journal->j_wait_reserved);

fs/ext4/super.c
@@ -4983,6 +4983,7 @@ static int ext4_load_and_init_journal(struct super_block *sb,
 	}
 
 	set_task_ioprio(sbi->s_journal->j_task, ctx->journal_ioprio);
+	set_task_ioprio(sbi->s_journal->j_checkpoint_task, ctx->journal_ioprio);
 
 	sbi->s_journal->j_submit_inode_data_buffers =
 		ext4_journal_submit_inode_data_buffers;
@@ -6572,6 +6573,7 @@ static int __ext4_remount(struct fs_context *fc, struct super_block *sb)
 	if (sbi->s_journal) {
 		ext4_init_journal_params(sb, sbi->s_journal);
 		set_task_ioprio(sbi->s_journal->j_task, ctx->journal_ioprio);
+		set_task_ioprio(sbi->s_journal->j_checkpoint_task, ctx->journal_ioprio);
 	}
 
 	/* Flush outstanding errors before changing fs state */

include/linux/jbd2.h
@@ -845,6 +845,16 @@ struct journal_s
 	wait_queue_head_t	j_wait_commit;
 
 	/**
+	 * @j_wait_done_checkpoint: Wait queue for waiting for checkpoint to complete.
+	 */
+	wait_queue_head_t	j_wait_done_checkpoint;
+
+	/**
+	 * @j_wait_checkpoint: Wait queue to trigger checkpointing.
+	 */
+	wait_queue_head_t	j_wait_checkpoint;
+
+	/**
 	 * @j_wait_updates: Wait queue to wait for updates to complete.
 	 */
 	wait_queue_head_t	j_wait_updates;
@@ -1206,6 +1216,13 @@ struct journal_s
 	 */
 	int			(*j_finish_inode_data_buffers)
 					(struct jbd2_inode *);
+
+	/**
+	 * @j_checkpoint_task:
+	 *
+	 * Pointer to the current checkpoint thread for this journal.
+	 */
+	struct task_struct	*j_checkpoint_task;
 
 	/*
 	 * Journal statistics

commit 7b71a82e29e8bc2cc22102380da331e3dcaa456a

anolis: jbd2: do not take j_checkpoint_mutex in journal_kill_thread
ANBZ: #33452

Take j_checkpoint_mutex in journal_kill_thread() is useless when wake up
checkpoint thread, and it may introduce theoretical deadlock risk, so
remove it.

And add more JBD2_UNMOUNT check to avoid theoretical lost wake-up in
check point thread.

Also use mutex_lock_io() instead of mutex_lock() for j_checkpoint_mutex
to keep consistency.

Fixes: c74ba6622f62 ("anolis: jbd2: create jbd2-ckpt thread for journal checkpoint")
Signed-off-by: Joseph Qi <joseph.qi@linux.alibaba.com>

fs/jbd2/checkpoint.c
@@ -96,9 +96,9 @@ __releases(&journal->j_state_lock)
 				mutex_unlock(&journal->j_checkpoint_mutex);
 				wake_up(&journal->j_wait_checkpoint);
 				schedule();
-				mutex_lock(&journal->j_checkpoint_mutex);
 				finish_wait(&journal->j_wait_done_checkpoint,
 					    &wait);
+				mutex_lock_io(&journal->j_checkpoint_mutex);
 				jbd2_debug(1, "wake up checkpoint thread.\n");
 			} else if (jbd2_cleanup_journal_tail(journal) <= 0) {
 				/*

fs/jbd2/journal.c
@@ -276,13 +276,14 @@ loop:
 	prepare_to_wait(&journal->j_wait_checkpoint, &wait,
 			TASK_INTERRUPTIBLE);
 	wake_up_all(&journal->j_wait_done_checkpoint);
-	schedule();
+	if (!(journal->j_flags & JBD2_UNMOUNT))
+		schedule();
 	finish_wait(&journal->j_wait_checkpoint, &wait);
 
 	if (journal->j_flags & JBD2_UNMOUNT)
 		goto end_loop;
 
-	mutex_lock(&journal->j_checkpoint_mutex);
+	mutex_lock_io(&journal->j_checkpoint_mutex);
 	jbd2_log_do_checkpoint(journal);
 	mutex_unlock(&journal->j_checkpoint_mutex);
 
@@ -333,11 +334,9 @@ static void journal_kill_thread(journal_t *journal)
 	write_unlock(&journal->j_state_lock);
 
 	while (journal->j_checkpoint_task) {
-		mutex_lock(&journal->j_checkpoint_mutex);
 		wake_up(&journal->j_wait_checkpoint);
 		wait_event(journal->j_wait_done_checkpoint,
 			   journal->j_checkpoint_task == NULL);
-		mutex_unlock(&journal->j_checkpoint_mutex);
 	}
 }
 