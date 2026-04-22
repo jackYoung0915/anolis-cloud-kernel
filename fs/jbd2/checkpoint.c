33467438ae3ad1db5bff916e19d1939f2baffdf3
Joseph Qi <joseph.qi@linux.alibaba.com>
2026-04-22 09:01:49 +0800

    anolis: jbd2: do not take j_checkpoint_mutex in journal_kill_thread
    ANBZ: #33453
    
    Take j_checkpoint_mutex in journal_kill_thread() is useless when wake up
    checkpoint thread, and it may introduce theoretical deadlock risk, so
    remove it.
    
    And add more JBD2_UNMOUNT check to avoid theoretical lost wake-up in
    check point thread.
    
    Also use mutex_lock_io() instead of mutex_lock() for j_checkpoint_mutex
    to keep consistency.
    
    Fixes: 3999cdd966d5 ("alinux: jbd2: create jbd2-ckpt thread for journal checkpoint")
    Signed-off-by: Joseph Qi <joseph.qi@linux.alibaba.com>

--- fs/jbd2/checkpoint.c
+++ fs/jbd2/checkpoint.c
@@ -149,8 +149,8 @@ void __jbd2_log_wait_for_space(journal_t *journal)
 				mutex_unlock(&journal->j_checkpoint_mutex);
 				wake_up(&journal->j_wait_checkpoint);
 				schedule();
-				mutex_lock(&journal->j_checkpoint_mutex);
 				finish_wait(&journal->j_wait_done_checkpoint, &wait);
+				mutex_lock_io(&journal->j_checkpoint_mutex);
 				jbd_debug(1, "wake up checkpoint thread.\n");
 			} else if (jbd2_cleanup_journal_tail(journal) == 0) {
 				/* We were able to recover space; yay! */
