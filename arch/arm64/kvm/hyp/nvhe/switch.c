384618b7aecc441c5b8de00b5f182be735087a49
Marc Zyngier <maz@kernel.org>
2026-04-27 07:07:01 +0000

    KVM: arm64: Drop trapping of PAuth instructions/keys
    ANBZ: #31782
    
    commit 814ad8f96e929fa9c60bd360d2f7bccfc1df0111 upstream.
    
    We currently insist on disabling PAuth on vcpu_load(), and get to
    enable it on first guest use of an instruction or a key (ignoring
    the NV case for now).
    
    It isn't clear at all what this is trying to achieve: guests tend
    to use PAuth when available, and nothing forces you to expose it
    to the guest if you don't want to. This also isn't totally free:
    we take a full GPR save/restore between host and guest, only to
    write ten 64bit registers. The "value proposition" escapes me.
    
    So let's forget this stuff and enable PAuth eagerly if exposed to
    the guest. This results in much simpler code. Performance wise,
    that's not bad either (tested on M2 Pro running a fully automated
    Debian installer as the workload):
    
    - On a non-NV guest, I can see reduction of 0.24% in the number
      of cycles (measured with perf over 10 consecutive runs)
    
    - On a NV guest (L2), I see a 2% reduction in wall-clock time
      (measured with 'time', as M2 doesn't have a PMUv3 and NV
      doesn't support it either)
    
    So overall, a much reduced complexity and a (small) performance
    improvement.
    
    Reviewed-by: Oliver Upton <oliver.upton@linux.dev>
    Link: https://lore.kernel.org/r/20240419102935.1935571-16-maz@kernel.org
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    
    [Backport comments]
    this_cpu_ptr_hyp_sym->this_cpu_ptr_hyp_sym_wrapper to fix the compiling
    failure
    Signed-off-by: Jia He <justin.he@arm.com>
    Reviewed-by: Bin Guo <guobin@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6809

--- arch/arm64/kvm/hyp/nvhe/switch.c
+++ arch/arm64/kvm/hyp/nvhe/switch.c
@@ -228,7 +228,6 @@ static const exit_handler_fn hyp_exit_handlers[] = {
 	[ESR_ELx_EC_IABT_LOW]		= kvm_hyp_handle_iabt_low,
 	[ESR_ELx_EC_DABT_LOW]		= kvm_hyp_handle_dabt_low,
 	[ESR_ELx_EC_WATCHPT_LOW]	= kvm_hyp_handle_watchpt_low,
-	[ESR_ELx_EC_PAC]		= kvm_hyp_handle_ptrauth,
 };
 
 static const exit_handler_fn pvm_exit_handlers[] = {
@@ -239,7 +238,6 @@ static const exit_handler_fn pvm_exit_handlers[] = {
 	[ESR_ELx_EC_IABT_LOW]		= kvm_hyp_handle_iabt_low,
 	[ESR_ELx_EC_DABT_LOW]		= kvm_hyp_handle_dabt_low,
 	[ESR_ELx_EC_WATCHPT_LOW]	= kvm_hyp_handle_watchpt_low,
-	[ESR_ELx_EC_PAC]		= kvm_hyp_handle_ptrauth,
 };
 
 static const exit_handler_fn *kvm_get_exit_handler_array(struct kvm_vcpu *vcpu)
