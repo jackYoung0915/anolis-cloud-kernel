jeevandeepj994/anolis_perf

anolis: jbd2: track slow handle which is preventing transaction committing
ANBZ: #8366

While transaction is going to commit, it first sets its state to be
T_LOCKED and waits all outstanding handles to complete, and the
committing transaction will always be in locked state so long as it
has outstanding handles, also the whole fs will be locked and all later
fs modification operations will be stucked in wait_transaction_locked().

It's hard to tell why handles are that slow, so here we add a new staic
tracepoint to track such slow handle, and show io wait time and sched
wait time, output likes below:
fsstress-20347 [024] ....  1570.305454: jbd2_slow_handle_stats: dev 254,17
tid 15853 type 4 line_no 3101 interval 126 sync 0 requested_blocks 24
dirtied_blocks 0 trans_wait 122 space_wait 0 sched_wait 0 io_wait 126

"trans_wait 122" means that this current committing transaction has been
locked for 122ms, due to this handle is not completed quickly.

From "io_wait 126", we can see that io is the major reason.

In this patch, we also add a per fs control file used to determine
whether a handle can be considered to be slow.
    /proc/fs/jbd2/vdb1-8/stall_thresh
default value is 100ms, users can set new threshold by echoing new value
to this file.

Later I also plan to add a proc file fs per fs to record these info.

Signed-off-by: Xiaoguang Wang <xiaoguang.wang@linux.alibaba.com>
Signed-off-by: Joseph Qi <joseph.qi@linux.alibaba.com>

fs/jbd2/journal.c
@@ -1449,6 +1449,54 @@ static const struct proc_ops jbd2_force_copy_proc_ops = {
 	.proc_release           = single_release,
 };
 
+static int jbd2_seq_stall_thresh_show(struct seq_file *m, void *v)
+{
+	journal_t *journal = m->private;
+
+	seq_printf(m, "%lu\n", journal->j_stall_thresh);
+	return 0;
+}
+
+static int jbd2_seq_stall_thresh_open(struct inode *inode, struct file *filp)
+{
+	journal_t *journal = pde_data(inode);
+
+	return single_open(filp, jbd2_seq_stall_thresh_show, journal);
+}
+
+static ssize_t jbd2_seq_stall_thresh_write(struct file *file,
+			const char __user *buf, size_t count, loff_t *offset)
+{
+	struct inode *inode = file_inode(file);
+	journal_t *journal = pde_data(inode);
+	char buffer[PROC_NUMBUF];
+	unsigned long long stall_thresh;
+	int err;
+
+	memset(buffer, 0, sizeof(buffer));
+	if (count > sizeof(buffer) - 1)
+		count = sizeof(buffer) - 1;
+	if (copy_from_user(buffer, buf, count)) {
+		err = -EFAULT;
+		goto out;
+	}
+
+	err = kstrtoull(strstrip(buffer), 0, &stall_thresh);
+	if (err)
+		goto out;
+	WRITE_ONCE(journal->j_stall_thresh, stall_thresh);
+out:
+	return err < 0 ? err : count;
+}
+
+static const struct proc_ops jbd2_stall_thresh_proc_ops = {
+	.proc_open              = jbd2_seq_stall_thresh_open,
+	.proc_read              = seq_read,
+	.proc_write             = jbd2_seq_stall_thresh_write,
+	.proc_lseek             = seq_lseek,
+	.proc_release           = single_release,
+};
+
 static struct proc_dir_entry *proc_jbd2_stats;
 
 static void jbd2_stats_proc_init(journal_t *journal)
@@ -1461,6 +1509,8 @@ static void jbd2_stats_proc_init(journal_t *journal)
 				 &jbd2_force_copy_proc_ops, journal);
 		proc_create_data("stats", 0444, journal->j_proc_entry,
 				 &jbd2_stats_proc_ops, journal);
+		proc_create_data("stall_thresh", 0644, journal->j_proc_entry,
+				 &jbd2_stall_thresh_proc_ops, journal);
 	}
 }
 
@@ -1469,6 +1519,7 @@ static void jbd2_stats_proc_exit(journal_t *journal)
 	remove_proc_entry("info", journal->j_proc_entry);
 	remove_proc_entry("force_copy", journal->j_proc_entry);
 	remove_proc_entry("stats", journal->j_proc_entry);
+	remove_proc_entry("stall_thresh", journal->j_proc_entry);
 	remove_proc_entry(journal->j_devname, proc_jbd2_stats);
 }
 
@@ -1762,6 +1813,7 @@ static journal_t *journal_init_common(struct block_device *bdev,
 	journal->j_commit_interval = (HZ * JBD2_DEFAULT_MAX_COMMIT_AGE);
 	journal->j_min_batch_time = 0;
 	journal->j_max_batch_time = 15000; /* 15ms */
+	journal->j_stall_thresh = JBD2_DEFAULT_TRANS_STALL_THRESH;
 	atomic_set(&journal->j_reserved_credits, 0);
 	lockdep_init_map(&journal->j_trans_commit_map, "jbd2_handle",
 			 &jbd2_trans_commit_key, 0);

jackYoung0915/anolis-cloud-kernel

ANBZ: #33406
cherry picked from devel-6.6 commit 91ee0077341638021ad66da72d25d20ea559f7ca.

fs/jbd2/journal.c
@@ -1411,6 +1411,54 @@ static const struct proc_ops jbd2_force_copy_proc_ops = {
 	.proc_release		= single_release,
 };
 
+static int jbd2_seq_stall_thresh_show(struct seq_file *m, void *v)
+{
+	journal_t *journal = m->private;
+
+	seq_printf(m, "%lu\n", journal->j_stall_thresh);
+	return 0;
+}
+
+static int jbd2_seq_stall_thresh_open(struct inode *inode, struct file *filp)
+{
+	journal_t *journal = pde_data(inode);
+
+	return single_open(filp, jbd2_seq_stall_thresh_show, journal);
+}
+
+static ssize_t jbd2_seq_stall_thresh_write(struct file *file,
+			const char __user *buf, size_t count, loff_t *offset)
+{
+	struct inode *inode = file_inode(file);
+	journal_t *journal = pde_data(inode);
+	char buffer[PROC_NUMBUF];
+	unsigned long stall_thresh;
+	int err;
+
+	memset(buffer, 0, sizeof(buffer));
+	if (count > sizeof(buffer) - 1)
+		count = sizeof(buffer) - 1;
+	if (copy_from_user(buffer, buf, count)) {
+		err = -EFAULT;
+		goto out;
+	}
+
+	err = kstrtoul(strstrip(buffer), 0, &stall_thresh);
+	if (err)
+		goto out;
+	WRITE_ONCE(journal->j_stall_thresh, stall_thresh);
+out:
+	return err < 0 ? err : count;
+}
+
+static const struct proc_ops jbd2_stall_thresh_proc_ops = {
+	.proc_open		= jbd2_seq_stall_thresh_open,
+	.proc_read		= seq_read,
+	.proc_write		= jbd2_seq_stall_thresh_write,
+	.proc_lseek		= seq_lseek,
+	.proc_release		= single_release,
+};
+
 static struct proc_dir_entry *proc_jbd2_stats;
 
 static void jbd2_stats_proc_init(journal_t *journal)
@@ -1423,6 +1471,8 @@ static void jbd2_stats_proc_init(journal_t *journal)
 				 &jbd2_force_copy_proc_ops, journal);
 		proc_create_data("stats", 0444, journal->j_proc_entry,
 				 &jbd2_stats_proc_ops, journal);
+		proc_create_data("stall_thresh", 0644, journal->j_proc_entry,
+				 &jbd2_stall_thresh_proc_ops, journal);
 	}
 }
 
@@ -1431,6 +1481,7 @@ static void jbd2_stats_proc_exit(journal_t *journal)
 	remove_proc_entry("info", journal->j_proc_entry);
 	remove_proc_entry("force_copy", journal->j_proc_entry);
 	remove_proc_entry("stats", journal->j_proc_entry);
+	remove_proc_entry("stall_thresh", journal->j_proc_entry);
 	remove_proc_entry(journal->j_devname, proc_jbd2_stats);
 }
 
@@ -1760,6 +1811,7 @@ static journal_t *journal_init_common(struct block_device *bdev,
 	journal->j_commit_interval = (HZ * JBD2_DEFAULT_MAX_COMMIT_AGE);
 	journal->j_min_batch_time = 0;
 	journal->j_max_batch_time = 15000; /* 15ms */
+	journal->j_stall_thresh = JBD2_DEFAULT_TRANS_STALL_THRESH;
 	atomic_set(&journal->j_reserved_credits, 0);
 	lockdep_init_map(&journal->j_trans_commit_map, "jbd2_handle",
 			 &journal->jbd2_trans_commit_key, 0);