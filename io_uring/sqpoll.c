jackYoung0915/anolis-cloud-kernel

commit 96cebad5e1f1afff75b181a924a1259ed2063c82

anolis: io_uring: re-add sqthread percpu polling support
ANBZ: #8410

add sqthread percpu polling feature to acnk-6.6.

----
we should deal with sqthread
creation method 'create_io_thread' when applying SQTHREAD_PERCPU feature
in ANCK, which used to be 'kthread_create'.

There are some cases we found and solved in this patch:

1. avoid premature destruction in sqthread_percpu_polling

We found a cornor case in sqthread_percpu_polling usage: There are two
processes (not in the same thread group) sharing the same sqthread.
Besides, the first process created the sqthread. If the first process
end its execution, the sqthread end its life and exit too, causing the
second process's halt.

This behaviour is not accepted. And the reason is: we want to make
'sqd->refs' to be the trigger for sqthread exiting, but when the first
process end, it will also let sqthread end. Thus, we should check
sqd->refs to make sure there isn't exist others using this sqthread.

2. modify create_io_thread to adapt sqthread_percpu_poll.

In contrast to original kthread_create sqthread, create_io_thread is
more suitable for threads in the same group sharing one sqthread. But
this is not perfect for io_thread sharing between processes( like
sqthread_percpu usage).

This patch originates from a cornor case:
There are two processes sharing one sqthread, and the process0
create the sqthread. If we send a SIGKILL to the process0, sqthread
can't EOL normally.

The reason is that sqthread shared io_uring_files and mm with process0
by using 'CLONE_VM' 'CLONE_FILES' etc.
As we 'kill -9 process0', duing to reference count, the io_uring_file
finally will be released by sqthread, not process0. What we want is:

process0..1..2 <io_uring_release> ->
	<io_put_sq_data> ->
		sqd->refs-- && sqd->refs == 0 ? <io_sq_thread_stop> ->
			io_sq_thread EOL.

But now it changes to: io_sq_thread should get out of its loop, and
release io_uring file in <do_exit>. But we use 'sqd->refs' judgement to

io_uring/sqpoll.c
@@ -25,6 +25,9 @@ enum {
 	IO_SQ_THREAD_SHOULD_PARK,
 };
 
+DEFINE_MUTEX(percpu_sqd_lock);
+struct io_sq_data __percpu **percpu_sqd;
+
 void io_sq_thread_unpark(struct io_sq_data *sqd)
 	__releases(&sqd->lock)
 {
@@ -65,14 +68,28 @@ void io_sq_thread_stop(struct io_sq_data *sqd)
 	wait_for_completion(&sqd->exited);
 }
 
-void io_put_sq_data(struct io_sq_data *sqd)
+void io_put_sq_data(struct io_ring_ctx *ctx, struct io_sq_data *sqd)
 {
+	int percpu = 0;
+
+	if ((ctx->flags & IORING_SETUP_SQ_AFF) &&
+	    (ctx->flags & IORING_SETUP_SQPOLL_PERCPU))
+		percpu = 1;
+
+	if (percpu)
+		mutex_lock(&percpu_sqd_lock);
+
 	if (refcount_dec_and_test(&sqd->refs)) {
 		WARN_ON_ONCE(atomic_read(&sqd->park_pending));
 
 		io_sq_thread_stop(sqd);
+		if (percpu)
+			*per_cpu_ptr(percpu_sqd, sqd->sq_cpu) = NULL;
 		kfree(sqd);
 	}
+
+	if (percpu)
+		mutex_unlock(&percpu_sqd_lock);
 }
 
 static __cold void io_sqd_update_thread_idle(struct io_sq_data *sqd)
@@ -95,7 +112,7 @@ void io_sq_thread_finish(struct io_ring_ctx *ctx)
 		io_sqd_update_thread_idle(sqd);
 		io_sq_thread_unpark(sqd);
 
-		io_put_sq_data(sqd);
+		io_put_sq_data(ctx, sqd);
 		ctx->sq_data = NULL;
 	}
 }
@@ -131,11 +148,11 @@ static struct io_sq_data *io_attach_sq_data(struct io_uring_params *p)
 }
 
 static struct io_sq_data *io_get_sq_data(struct io_uring_params *p,
-					 bool *attached)
+					 bool *attached, bool *percpu_found)
 {
 	struct io_sq_data *sqd;
 
-	*attached = false;
+	*attached = *percpu_found = false;
 	if (p->flags & IORING_SETUP_ATTACH_WQ) {
 		sqd = io_attach_sq_data(p);
 		if (!IS_ERR(sqd)) {
@@ -145,6 +162,19 @@ static struct io_sq_data *io_get_sq_data(struct io_uring_params *p,
 		/* fall through for EPERM case, setup new sqd/task */
 		if (PTR_ERR(sqd) != -EPERM)
 			return sqd;
+	}
+
+	if ((p->flags & IORING_SETUP_SQ_AFF) &&
+	    (p->flags & IORING_SETUP_SQPOLL_PERCPU)) {
+		mutex_lock(&percpu_sqd_lock);
+		sqd = *per_cpu_ptr(percpu_sqd, p->sq_thread_cpu);
+		if (sqd) {
+			refcount_inc(&sqd->refs);
+			mutex_unlock(&percpu_sqd_lock);
+			*percpu_found = true;
+			return sqd;
+		}
+		mutex_unlock(&percpu_sqd_lock);
 	}
 
 	sqd = kzalloc(sizeof(*sqd), GFP_KERNEL);
@@ -211,8 +241,16 @@ static bool io_sqd_handle_event(struct io_sq_data *sqd)
 	if (test_bit(IO_SQ_THREAD_SHOULD_PARK, &sqd->state) ||
 	    signal_pending(current)) {
 		mutex_unlock(&sqd->lock);
-		if (signal_pending(current))
+		if (signal_pending(current)) {
 			did_sig = get_signal(&ksig);
+			if (did_sig && sqd->sq_cpu != -1 &&
+			    refcount_read(&sqd->refs) != 0) {
+				mutex_lock(&percpu_sqd_lock);
+				if (*per_cpu_ptr(percpu_sqd, sqd->sq_cpu) == sqd)
+					did_sig = false;
+				mutex_unlock(&percpu_sqd_lock);
+			}
+		}
 		cond_resched();
 		mutex_lock(&sqd->lock);
 		sqd->sq_cpu = raw_smp_processor_id();
@@ -372,13 +410,26 @@ __cold int io_sq_offload_create(struct io_ring_ctx *ctx,
 	if (ctx->flags & IORING_SETUP_SQPOLL) {
 		struct task_struct *tsk;
 		struct io_sq_data *sqd;
-		bool attached;
+		bool attached, percpu_found;
 
 		ret = security_uring_sqpoll();
 		if (ret)
 			return ret;
 
-		sqd = io_get_sq_data(p, &attached);
+		if ((ctx->flags & IORING_SETUP_ATTACH_WQ) &&
+		    (ctx->flags & IORING_SETUP_SQPOLL_PERCPU)) {
+			/* ATTACH_WQ and SQPOLL_PERCPU are mutual exclusive */
+			ret = -EINVAL;
+			goto err;
+		}
+		if ((ctx->flags & IORING_SETUP_SQPOLL_PERCPU) &&
+		    !(ctx->flags & IORING_SETUP_SQ_AFF)) {
+			/* SQPOLL_PERCPU and SQ_AFF should both exist */
+			ret = -EINVAL;
+			goto err;
+		}
+
+		sqd = io_get_sq_data(p, &attached, &percpu_found);
 		if (IS_ERR(sqd)) {
 			ret = PTR_ERR(sqd);
 			goto err;
@@ -399,7 +450,7 @@ __cold int io_sq_offload_create(struct io_ring_ctx *ctx,
 
 		if (ret < 0)
 			goto err;
-		if (attached)
+		if (attached || percpu_found)
 			return 0;
 
 		if (p->flags & IORING_SETUP_SQ_AFF) {
@@ -426,7 +477,8 @@ __cold int io_sq_offload_create(struct io_ring_ctx *ctx,
 
 		sqd->task_pid = current->pid;
 		sqd->task_tgid = current->tgid;
-		tsk = create_io_thread(io_sq_thread, sqd, NUMA_NO_NODE);
+		tsk = create_io_thread(io_sq_thread, sqd, NUMA_NO_NODE,
+				!!(ctx->flags & IORING_SETUP_SQPOLL_PERCPU));
 		if (IS_ERR(tsk)) {
 			ret = PTR_ERR(tsk);
 			goto err_sqpoll;
@@ -434,12 +486,19 @@ __cold int io_sq_offload_create(struct io_ring_ctx *ctx,
 
 		sqd->thread = tsk;
 		task_to_put = get_task_struct(tsk);
+		if ((p->flags & IORING_SETUP_SQ_AFF) &&
+		    (p->flags & IORING_SETUP_SQPOLL_PERCPU)) {
+			mutex_lock(&percpu_sqd_lock);
+			*per_cpu_ptr(percpu_sqd, sqd->sq_cpu) = sqd;
+			mutex_unlock(&percpu_sqd_lock);
+		}
 		ret = io_uring_alloc_task_context(tsk, ctx);
 		wake_up_new_task(tsk);
 		if (ret)
 			goto err;
-	} else if (p->flags & IORING_SETUP_SQ_AFF) {
-		/* Can't have SQ_AFF without SQPOLL */
+	} else if (p->flags & (IORING_SETUP_SQ_AFF |
+			       IORING_SETUP_SQPOLL_PERCPU)) {
+		/* Can't have SQ_AFF or SQPOLL_PERCPU without SQPOLL */
 		ret = -EINVAL;
 		goto err;
 	}

include/linux/sched/task.h
@@ -95,7 +95,8 @@ extern void exit_itimers(struct task_struct *);
 extern pid_t kernel_clone(struct kernel_clone_args *kargs);
 struct task_struct *copy_process(struct pid *pid, int trace, int node,
 				 struct kernel_clone_args *args);
-struct task_struct *create_io_thread(int (*fn)(void *), void *arg, int node);
+struct task_struct *create_io_thread(int (*fn)(void *), void *arg, int node,
+					bool unshare);
 struct task_struct *fork_idle(int);
 extern pid_t kernel_thread(int (*fn)(void *), void *arg, const char *name,
 			    unsigned long flags);

include/uapi/linux/io_uring.h
@@ -165,6 +165,7 @@ enum {
  * Only one task is allowed to submit requests
  */
 #define IORING_SETUP_SINGLE_ISSUER	(1U << 12)
+#define IORING_SETUP_SQPOLL_PERCPU	(1U << 31) /* percpu SQ poll thread */
 
 /*
  * Defer running task work to get events.

io_uring/io-wq.c
@@ -792,7 +792,7 @@ static void create_worker_cont(struct callback_head *cb)
 	worker = container_of(cb, struct io_worker, create_work);
 	clear_bit_unlock(0, &worker->create_state);
 	wq = worker->wq;
-	tsk = create_io_thread(io_wq_worker, worker, NUMA_NO_NODE);
+	tsk = create_io_thread(io_wq_worker, worker, NUMA_NO_NODE, false);
 	if (!IS_ERR(tsk)) {
 		io_init_new_worker(wq, worker, tsk);
 		io_worker_release(worker);
@@ -862,7 +862,7 @@ fail:
 	if (index == IO_WQ_ACCT_BOUND)
 		set_bit(IO_WORKER_F_BOUND, &worker->flags);
 
-	tsk = create_io_thread(io_wq_worker, worker, NUMA_NO_NODE);
+	tsk = create_io_thread(io_wq_worker, worker, NUMA_NO_NODE, false);
 	if (!IS_ERR(tsk)) {
 		io_init_new_worker(wq, worker, tsk);
 	} else if (!io_should_retry_thread(worker, PTR_ERR(tsk))) {

io_uring/io_uring.c
@@ -137,6 +137,8 @@ struct io_defer_entry {
 	u32			seq;
 };
 
+extern struct io_sq_data __percpu **percpu_sqd;
+
 /* requests with any of those set should undergo io_disarm_next() */
 #define IO_DISARM_MASK (REQ_F_ARM_LTIMEOUT | REQ_F_LINK_TIMEOUT | REQ_F_FAIL)
 #define IO_REQ_LINK_FLAGS (REQ_F_LINK | REQ_F_HARDLINK)
@@ -4171,7 +4173,7 @@ static long io_uring_setup(u32 entries, struct io_uring_params __user *params)
 			IORING_SETUP_SQE128 | IORING_SETUP_CQE32 |
 			IORING_SETUP_SINGLE_ISSUER | IORING_SETUP_DEFER_TASKRUN |
 			IORING_SETUP_NO_MMAP | IORING_SETUP_REGISTERED_FD_ONLY |
-			IORING_SETUP_NO_SQARRAY))
+			IORING_SETUP_NO_SQARRAY | IORING_SETUP_SQPOLL_PERCPU))
 		return -EINVAL;
 
 	return io_uring_create(entries, &p, params);
@@ -4460,7 +4462,7 @@ static __cold int io_register_iowq_max_workers(struct io_ring_ctx *ctx,
 	if (sqd) {
 		mutex_unlock(&ctx->uring_lock);
 		mutex_unlock(&sqd->lock);
-		io_put_sq_data(sqd);
+		io_put_sq_data(ctx, sqd);
 		mutex_lock(&ctx->uring_lock);
 	}
 
@@ -4488,9 +4490,8 @@ err:
 	if (sqd) {
 		mutex_unlock(&ctx->uring_lock);
 		mutex_unlock(&sqd->lock);
-		io_put_sq_data(sqd);
+		io_put_sq_data(ctx, sqd);
 		mutex_lock(&ctx->uring_lock);
-
 	}
 	return ret;
 }
@@ -4711,6 +4712,8 @@ out_fput:
 
 static int __init io_uring_init(void)
 {
+	int cpu;
+
 #define __BUILD_BUG_VERIFY_OFFSET_SIZE(stype, eoffset, esize, ename) do { \
 	BUILD_BUG_ON(offsetof(stype, ename) != eoffset); \
 	BUILD_BUG_ON(sizeof_field(stype, ename) != esize); \
@@ -4800,6 +4803,9 @@ static int __init io_uring_init(void)
 				sizeof_field(struct io_kiocb, cmd.data), NULL);
 
 	iou_wq = alloc_workqueue("iou_exit", WQ_UNBOUND, 64);
+	percpu_sqd = alloc_percpu(struct io_sq_data *);
+	for_each_possible_cpu(cpu)
+		*per_cpu_ptr(percpu_sqd, cpu) = NULL;
 
 #ifdef CONFIG_SYSCTL
 	register_sysctl_init("kernel", kernel_io_uring_disabled_table);

io_uring/sqpoll.h
@@ -25,6 +25,6 @@ void io_sq_thread_finish(struct io_ring_ctx *ctx);
 void io_sq_thread_stop(struct io_sq_data *sqd);
 void io_sq_thread_park(struct io_sq_data *sqd);
 void io_sq_thread_unpark(struct io_sq_data *sqd);
-void io_put_sq_data(struct io_sq_data *sqd);
+void io_put_sq_data(struct io_ring_ctx *ctx, struct io_sq_data *sqd);
 void io_sqpoll_wait_sq(struct io_ring_ctx *ctx);
 int io_sqpoll_wq_cpu_affinity(struct io_ring_ctx *ctx, cpumask_var_t mask);

kernel/fork.c
@@ -2854,13 +2854,22 @@ struct task_struct * __init fork_idle(int cpu)
  * The returned task is inactive, and the caller must fire it up through
  * wake_up_new_task(p). All signals are blocked in the created task.
  */
-struct task_struct *create_io_thread(int (*fn)(void *), void *arg, int node)
+struct task_struct *create_io_thread(int (*fn)(void *), void *arg, int node,
+					bool unshare)
 {
-	unsigned long flags = CLONE_FS|CLONE_FILES|CLONE_SIGHAND|CLONE_THREAD|
-				CLONE_IO;
+	unsigned long flags = unshare ? 0 : (CLONE_FS|CLONE_FILES|
+					     CLONE_SIGHAND|CLONE_THREAD|
+					     CLONE_IO|CLONE_VM);
+	/* we use 'unshare' flag to try to create an independent io_thread,
+	 * 'unshare' describes whether child share parent's mm directly (with
+	 * refcount add one), or it should copy mm/files when copy_process().
+	 * By setting this flag, the io_thread won't share parent's mm
+	 * directly, but can be shared among different tasks, and looks more
+	 * reasonably.
+	 */
 	struct kernel_clone_args args = {
-		.flags		= ((lower_32_bits(flags) | CLONE_VM |
-				    CLONE_UNTRACED) & ~CSIGNAL),
+		.flags		= ((lower_32_bits(flags) | CLONE_UNTRACED)
+					& ~CSIGNAL),
 		.exit_signal	= (lower_32_bits(flags) & CSIGNAL),
 		.fn		= fn,
 		.fn_arg		= arg,

commit 085cf2d885cbba6fcb021c27579211fa8796aa60

anolis: io_uring: add cpu check in io_get_sq_data
ANBZ: #34474

cherry picked from devel-6.6 commit bf609a5a59dcd13d6966d37677e9f20fdeba663c.

For IORING_SETUP_SQPOLL_PERCPU, we should ensure "p->sq_thread_cpu" should
be a legal value. Thus we need to add a check for this.

Fixes: 96cebad5e1f1 ("anolis: io_uring: re-add sqthread percpu polling support")
Signed-off-by: Ferry Meng <mengferry@linux.alibaba.com>
Signed-off-by: Joseph Qi <joseph.qi@linux.alibaba.com>

io_uring/sqpoll.c
@@ -197,8 +197,12 @@ static struct io_sq_data *io_get_sq_data(struct io_uring_params *p,
 
 	if ((p->flags & IORING_SETUP_SQ_AFF) &&
 	    (p->flags & IORING_SETUP_SQPOLL_PERCPU)) {
+		int cpu = p->sq_thread_cpu;
+
+		if (cpu >= nr_cpu_ids || !cpu_online(cpu))
+			return ERR_PTR(-EINVAL);
 		mutex_lock(&percpu_sqd_lock);
-		sqd = *per_cpu_ptr(percpu_sqd, p->sq_thread_cpu);
+		sqd = *per_cpu_ptr(percpu_sqd, cpu);
 		if (sqd) {
 			if (sqd->task_tgid != current->tgid) {
 				mutex_unlock(&percpu_sqd_lock);