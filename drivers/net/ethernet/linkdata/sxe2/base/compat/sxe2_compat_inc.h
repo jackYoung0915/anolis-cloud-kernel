8f5ff3adb9dc3ba7ca56cde48e9c7f8668e9523f
liujie_answer <liujie5@linkdatatechnology.com>
2026-05-11 14:07:14 +0800

    anolis: net: update linkdata ethernet driver features.
    ANBZ: #34147
    
    This commit is to update Linkdata ethernet driver features.
    
    Signed-off-by: liujie_answer <liujie5@linkdatatechnology.com>

---
+++ drivers/net/ethernet/linkdata/sxe2/base/compat/sxe2_compat_inc.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/**
+ * Copyright (C), 2020, Linkdata Technologies Co., Ltd.
+ *
+ * @file: sxe2_compat_inc.h
+ * @author: Linkdata
+ * @date: 2025.02.16
+ * @brief:
+ * @note:
+ */
+#ifndef __SXE2_COMPAT_INC_H__
+#define __SXE2_COMPAT_INC_H__
+
+#include <linux/filter.h>
+
+#endif
