29c3e63c12a2a817dda83b29abed1a043fd3f9d5
Qihang Zhou <zhouqihang@linux.alibaba.com>
2026-05-11 12:49:39 +0800

    sched: Bypass bandwitdh checks with runtime disabled RT_GROUP_SCHED
    ANBZ: #35087
    
    commit 277e0909754e9f3c82def97150d2f3ea700098f1 upstream.
    
    When RT_GROUPs are compiled but not exposed, their bandwidth cannot
    be configured (and it is not initialized for non-root task_groups neither).
    Therefore bypass any checks of task vs task_group bandwidth.
    
    This will achieve behavior very similar to setups that have
    (On a related note, this may allow having RT tasks with
    CONFIG_RT_GROUP_SCHED and cgroup v2 hierarchy.)
    
    Signed-off-by: Michal Koutný <mkoutny@suse.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20250310170442.504716-7-mkoutny@suse.com
    Signed-off-by: Qihang Zhou <zhouqihang@linux.alibaba.com>

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -2956,8 +2956,8 @@ static int sched_rt_global_constraints(void)
 
 int sched_rt_can_attach(struct task_group *tg, struct task_struct *tsk)
 {
-	/* Don't accept realtime tasks when there is no way for them to run */
-	if (rt_task(tsk) && tg->rt_bandwidth.rt_runtime == 0)
+	/* Don't accept real-time tasks when there is no way for them to run */
+	if (rt_group_sched_enabled() && rt_task(tsk) && tg->rt_bandwidth.rt_runtime == 0)
 		return 0;
 
 	return 1;
