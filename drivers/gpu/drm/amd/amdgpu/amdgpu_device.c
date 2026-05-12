ekuiter/torte-linux

drm/amdgpu: fix cyan_skillfish2 gpu info fw handling
If the board supports IP discovery, we don't need to
parse the gpu info firmware.

Closes: https://gitlab.freedesktop.org/drm/amd/-/issues/4721
Fixes: 14a3b7769a81 ("drm/amdgpu: add support for cyan skillfish gpu_info")
Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
@@ -2665,6 +2665,8 @@ static int amdgpu_device_parse_gpu_info_fw(struct amdgpu_device *adev)
 		chip_name = "navi12";
 		break;
 	case CHIP_CYAN_SKILLFISH:
+		if (adev->discovery.bin)
+			return 0;
 		chip_name = "cyan_skillfish";
 		break;
 	}

jackYoung0915/anolis-cloud-kernel

ANBZ: #35154
commit 0be4d79f8a1b6ac792fdf401cbd3a7216a45c1f9 stable.
commit 7fa666ab07ba9e08f52f357cb8e1aad753e83ac6 upstream.
Backport to 6.18.
Fixes: fa819e3a7c1e ("drm/amdgpu: add support for cyan skillfish gpu_info")
(cherry picked from commit 5427e32fa3a0ba9a016db83877851ed277b065fb)
Signed-off-by: Sasha Levin <sashal@kernel.org>
Assisted-by: PatchPilot <patchpilot@openanolis.org>
Signed-off-by: Weilin Tong <tongweilin@linux.alibaba.com>

drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
@@ -1941,6 +1941,8 @@ static int amdgpu_device_parse_gpu_info_fw(struct amdgpu_device *adev)
 		chip_name = "navi12";
 		break;
 	case CHIP_CYAN_SKILLFISH:
+		if (adev->mman.discovery_bin)
+			return 0;
 		chip_name = "cyan_skillfish";
 		break;
 	}