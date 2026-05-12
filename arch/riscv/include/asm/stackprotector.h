6bf8ac35a8cee7c1ebea3dc5ef4dd398866c88f8
Gao Rui <gao.rui@zte.com.cn>
2026-05-12 11:14:10 +0800

    anolis: riscv: Fix compilation error for canary offset exceeding 2047
    ANBZ: #35131
    
        On the RISC-V architecture, enabling KABI causes the canary offset to exceed
    the instruction limit of 2047, resulting in compilation errors such as:
    
    cc1: error: '2136' is not a valid offset in '-mstack-protector-guard-offset='
    
        The fix is as follows: when the canary offset exceeds 2047, enable global mode
    to resolve this issue.
    
    Signed-off-by: Gao Rui <gao.rui@zte.com.cn>
    Reviewed-by: Fangyu Yu <fangyu.yu@linux.alibaba.com>
    Reviewed-by: Ruidong Tian <tianruidong@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6937

--- arch/riscv/include/asm/stackprotector.h
+++ arch/riscv/include/asm/stackprotector.h
@@ -3,6 +3,10 @@
 #ifndef _ASM_RISCV_STACKPROTECTOR_H
 #define _ASM_RISCV_STACKPROTECTOR_H
 
+#ifdef CONFIG_STACKPROTECTOR_PER_TASK
+#include <generated/stackguard.h>
+#endif
+
 extern unsigned long __stack_chk_guard;
 
 /*
@@ -16,7 +20,18 @@ static __always_inline void boot_init_stack_canary(void)
 	unsigned long canary = get_random_canary();
 
 	current->stack_canary = canary;
-	if (!IS_ENABLED(CONFIG_STACKPROTECTOR_PER_TASK))
+	if (!IS_ENABLED(CONFIG_STACKPROTECTOR_PER_TASK)) {
 		__stack_chk_guard = current->stack_canary;
+	} else {
+		/*
+		 * Per-task mode with global fallback: all tasks share a global canary,
+		 * due to offset overflow.
+		 * Per-task TLS mode: canary accessed via tp register offset,
+		 * no need to set __stack_chk_guard.
+		 */
+#if defined(CONFIG_STACKPROTECTOR_GUARD_GLOBAL)
+		__stack_chk_guard = current->stack_canary;
+#endif
+	}
 }
 #endif /* _ASM_RISCV_STACKPROTECTOR_H */
