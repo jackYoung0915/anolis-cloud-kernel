76b3d023025da4fed530f702a515758d3fa512ea
Marc Zyngier <maz@kernel.org>
2026-05-13 08:15:09 +0000

    KVM: arm64: nv: Add emulation for ERETAx instructions
    ANBZ: #31782
    
    commit 6ccc971ee2c61a1ffb487e46bf6184f7df6aacfb upstream.
    
    FEAT_NV has the interesting property of relying on ERET being
    trapped. An added complexity is that it also traps ERETAA and
    ERETAB, meaning that the Pointer Authentication aspect of these
    instruction must be emulated.
    
    Add an emulation of Pointer Authentication, limited to ERETAx
    (always using SP_EL2 as the modifier and ELR_EL2 as the pointer),
    using the Generic Authentication instructions.
    
    The emulation, however small, is placed in its own compilation
    unit so that it can be avoided if the configuration doesn't
    include it (or the toolchan in not up to the task).
    
    Reviewed-by: Joey Gouly <joey.gouly@arm.com>
    Reviewed-by: Oliver Upton <oliver.upton@linux.dev>
    Link: https://lore.kernel.org/r/20240419102935.1935571-13-maz@kernel.org
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Signed-off-by: Jia He <justin.he@arm.com>
    Reviewed-by: Bin Guo <guobin@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/6809

--- arch/arm64/include/asm/kvm_nested.h
+++ arch/arm64/include/asm/kvm_nested.h
@@ -64,6 +64,18 @@ extern bool forward_smc_trap(struct kvm_vcpu *vcpu);
 
 int kvm_init_nv_sysregs(struct kvm *kvm);
 
+#ifdef CONFIG_ARM64_PTR_AUTH
+bool kvm_auth_eretax(struct kvm_vcpu *vcpu, u64 *elr);
+#else
+static inline bool kvm_auth_eretax(struct kvm_vcpu *vcpu, u64 *elr)
+{
+	/* We really should never execute this... */
+	WARN_ON_ONCE(1);
+	*elr = 0xbad9acc0debadbad;
+	return false;
+}
+#endif
+
 #define vncr_fixmap(c)						\
 	({							\
 		u32 __c = (c);					\
