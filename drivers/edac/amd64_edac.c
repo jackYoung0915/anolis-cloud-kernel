16ae66ca34e76ebcc01c6304d578ac14ade54d36
Wenhui Fan <fanwh@hygon.cn>
2026-05-19 20:30:04 +0800

    anolis: EDAC/amd64: The width of hash value is 2 bits for Hygon family 18h model 6h processors
    ANBZ: #35240
    
    It has 2 bits hash value when hash enabled for hygon family 18h model 6h.
    
    Hygon-SIG: commit none hygon anolis: EDAC/amd64: The width of hash value
    is 2 bits for Hygon family 18h model 6h processors
    
    Hygon-SIG: commit none hygon anolis: EDAC/amd64: The width of hash value
    is 2 bits for Hygon family 18h model 6h processors
    
    Signed-off-by: Wenhui Fan <fanwh@hygon.cn>
    Reviewed-by: Ruidong Tian <tianruidong@linux.alibaba.com>
    Reviewed-by: Yuanhe Shu <xiangzao@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/7090

--- drivers/edac/amd64_edac.c
+++ drivers/edac/amd64_edac.c
@@ -1263,7 +1263,11 @@ static int hygon_umc_normaddr_to_sysaddr(u64 norm_addr, u16 nid, u8 umc,
 	case 5:	intlv_num_chan = 3; break;
 	case 7:	intlv_num_chan = 4; break;
 
-	case 8: intlv_num_chan = 1;
+	case 8:
+		if (boot_cpu_data.x86_model >= 0x6)
+			intlv_num_chan = 2;
+		else
+			intlv_num_chan = 1;
 		hash_enabled = true;
 		break;
 	default:
@@ -1398,17 +1402,22 @@ static int hygon_umc_normaddr_to_sysaddr(u64 norm_addr, u16 nid, u8 umc,
 	}
 
 	if (hash_enabled) {
-		/* Save some parentheses and grab ls-bit at the end. */
 		hashed_bit =	(ctx.ret_addr >> 12) ^
 				(ctx.ret_addr >> 18) ^
 				(ctx.ret_addr >> 21) ^
 				(ctx.ret_addr >> 30) ^
 				cs_id;
 
-		hashed_bit &= BIT(0);
-
-		if (hashed_bit != ((ctx.ret_addr >> intlv_addr_bit) & BIT(0)))
-			ctx.ret_addr ^= BIT(intlv_addr_bit);
+		if (boot_cpu_data.x86_model >= 0x6) {
+			hashed_bit &= 0x3;
+			if (hashed_bit != ((ctx.ret_addr >> intlv_addr_bit) & 0x3))
+				ctx.ret_addr = (ctx.ret_addr & ~((u64)3 << intlv_addr_bit)) |
+						(hashed_bit << intlv_addr_bit);
+		} else {
+			hashed_bit &= BIT(0);
+			if (hashed_bit != ((ctx.ret_addr >> intlv_addr_bit) & BIT(0)))
+				ctx.ret_addr ^= BIT(intlv_addr_bit);
+		}
 	}
 
 	/* The channel hashing process. */
