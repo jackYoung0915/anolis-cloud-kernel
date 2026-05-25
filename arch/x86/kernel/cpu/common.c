422ee5e43b996d0e4ead7ff711f08a7b431fb9fb
Xin Li (Intel) <xin@zytor.com>
2026-05-25 11:39:03 +0800

    x86/fred: Move FRED RSP initialization into a separate function
    ANBZ: #33261
    
    commit 73270c1f2369fb37683121ebe097cd37172602b6 upstream.
    
    To enable FRED earlier, move the RSP initialization out of
    cpu_init_fred_exceptions() into cpu_init_fred_rsps().
    
    This is required as the FRED RSP initialization depends on the availability
    of the CPU entry areas which are set up late in trap_init(),
    
    No functional change intended. Marked with Fixes as it's a depedency for
    the real fix.
    
    Intel-SIG: commit 73270c1f2369 x86/fred: Move FRED RSP initialization into a separate function.
    backport upstream Enable FRED earlier patch series
    
    Fixes: 14619d912b65 ("x86/fred: FRED entry/exit and dispatch code")
    Signed-off-by: Xin Li (Intel) <xin@zytor.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Link: https://lore.kernel.org/all/20240709154048.3543361-3-xin@zytor.com
    [ Aubrey Li: amend commit log ]
    Signed-off-by: Aubrey Li <aubrey.li@linux.intel.com>
    Reviewed-by: Yuanhe Shu <xiangzao@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/7102

--- arch/x86/kernel/cpu/common.c
+++ arch/x86/kernel/cpu/common.c
@@ -2293,10 +2293,12 @@ void cpu_init_exception_handling(void)
 		elf_hwcap2 |= HWCAP2_FSGSBASE;
 	}
 
-	if (cpu_feature_enabled(X86_FEATURE_FRED))
+	if (cpu_feature_enabled(X86_FEATURE_FRED)) {
 		cpu_init_fred_exceptions();
-	else
+		cpu_init_fred_rsps();
+	} else {
 		load_current_idt();
+	}
 }
 
 /*
