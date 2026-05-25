f443249b4988a4cf202adc246a5b7938e600663c
LeoLiu-oc <leoliu-oc@zhaoxin.com>
2026-05-25 16:38:53 +0800

    anolis: Revert "anolis: x86/delay: add support for Zhaoxin ZXPAUSE instruction"
    ANBZ: #26820
    
    This reverts commit df7bb4066b8d044f32ac0a8e2f775d366a0a5107.
    
    Update to new version.
    The current version is the old one. To Update to the new version,
    withdraw this old version first.
    
    Signed-off-by: LeoLiu-oc <leoliu-oc@zhaoxin.com>

--- arch/x86/include/asm/cpufeatures.h
+++ arch/x86/include/asm/cpufeatures.h
@@ -13,7 +13,7 @@
 /*
  * Defines x86 CPU feature bits
  */
-#define NCAPINTS			24	   /* N 32-bit words worth of info */
+#define NCAPINTS			23	   /* N 32-bit words worth of info */
 #define NBUGINTS			2	   /* N 32-bit bug flags */
 
 /*
@@ -494,8 +494,6 @@
 #define X86_FEATURE_HYGON_CIS_SM3	(22*32 + 1) /* "sm3" SM3 instructions */
 #define X86_FEATURE_HYGON_CIS_SM4	(22*32 + 2) /* "sm4" SM4 instructions */
 
-/* VIA/Cyrix/Centaur-defined CPU features, CPUID level 0xC0000006, word 23 */
-#define X86_FEATURE_ZXPAUSE		(23*32 + 0) /* ZHAOXIN ZXPAUSE */
 #define X86_FEATURE_PREFETCHI		(20*32+20) /* Prefetch Data/Instruction to Cache Level */
 #define X86_FEATURE_AVX512_BMM		(20*32+23) /* AVX512 Bit Matrix Multiply instructions */
 
