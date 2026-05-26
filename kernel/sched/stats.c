1177afbad14fc8e280a161116c189f9330ee3338
Qihang Zhou <zhouqihang@linux.alibaba.com>
2026-05-26 10:06:24 +0800

    anolis: sched/sli: fix block_latency and nr_uninterruptible not accounting for non-iowait D state
    ANBZ: #8657
    
    Move task_cpu_update_block() call outside the if (p->in_iowait) check
    so that both SCHED_LAT_BLOCK and nr_uninterruptible are properly
    accumulated for tasks in TASK_UNINTERRUPTIBLE state that are not
    in iowait.
    
    Previously, task_cpu_update_block() was only called inside the
    if (p->in_iowait) branch, which meant:
    1. cpu.block_latency (SCHED_LAT_BLOCK) was never accumulated for
       non-iowait D-state tasks, always routing to SCHED_LAT_IOBLOCK.
    2. nr_uninterruptible in cpu.exstat was never incremented for
       non-iowait D-state tasks.
    
    After this fix, vfork()-style blocking (D state without iowait)
    correctly shows in both cpu.block_latency and nr_uninterruptible.
    
    Fixes: d07cea81631f ("anolis: sched: Add cgroup-level blocked time histograms")
    Fixes: 8fed776f8ef4 ("anolis: sched, cpuacct: Move scheduling latency data from cpuacct to task_group")
    Signed-off-by: Qihang Zhou <zhouqihang@linux.alibaba.com>
    Reviewed-by: Peng Wang <peng_wang@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/7121

--- kernel/sched/stats.c
+++ kernel/sched/stats.c
@@ -84,8 +84,8 @@ void __update_stats_enqueue_sleeper(struct rq *rq, struct task_struct *p,
 		__schedstat_add(stats->sum_block_runtime, delta);
 
 		if (p) {
+			task_cpu_update_block(p, delta);
 			if (p->in_iowait) {
-				task_cpu_update_block(p, delta);
 				__schedstat_add(stats->iowait_sum, delta);
 				__schedstat_inc(stats->iowait_count);
 				trace_sched_stat_iowait(p, delta);
