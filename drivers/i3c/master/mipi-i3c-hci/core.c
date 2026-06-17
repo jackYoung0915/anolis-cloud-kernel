761a4f0cb0f1adffd64653140d34a1e3ea0317fb
kbackport <kbackport@kernel.org>
2026-06-17 10:26:07 +0800

    i3c: mipi-i3c-hci: Fix race in DMA ring dequeue
    ANBZ: #34998
    
    commit 1dca8aee80eea76d2aae21265de5dd64f6ba0f09 upstream.
    
    The HCI DMA dequeue path (hci_dma_dequeue_xfer()) may be invoked for
    multiple transfers that timeout around the same time.  However, the
    function is not serialized and can race with itself.
    
    When a timeout occurs, hci_dma_dequeue_xfer() stops the ring, processes
    incomplete transfers, and then restarts the ring.  If another timeout
    triggers a parallel call into the same function, the two instances may
    interfere with each other - stopping or restarting the ring at unexpected
    times.
    
    Add a mutex so that hci_dma_dequeue_xfer() is serialized with respect to
    itself.
    
    Fixes: 9ad9a52cce282 ("i3c/master: introduce the mipi-i3c-hci driver")
    Cc: stable@vger.kernel.org
    Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>
    Reviewed-by: Frank Li <Frank.Li@nxp.com>
    Link: https://patch.msgid.link/20260306072451.11131-7-adrian.hunter@intel.com
    Signed-off-by: Alexandre Belloni <alexandre.belloni@bootlin.com>
    (cherry picked from commit 1dca8aee80eea76d2aae21265de5dd64f6ba0f09)
    
    [Fixes conflicts by Kbackport]
    Fixes: CVE-2026-43353
    Assisted-by: PatchPilot <patchpilot@openanolis.org>
    Signed-off-by: Xunlei Pang <xlpang@linux.alibaba.com>
    Reviewed-by: Guixin Liu <kanie@linux.alibaba.com>
    Link: https://gitee.com/anolis/cloud-kernel/pulls/8223

--- drivers/i3c/master/mipi-i3c-hci/core.c
+++ drivers/i3c/master/mipi-i3c-hci/core.c
@@ -739,6 +739,9 @@ static int i3c_hci_probe(struct platform_device *pdev)
 	hci = devm_kzalloc(&pdev->dev, sizeof(*hci), GFP_KERNEL);
 	if (!hci)
 		return -ENOMEM;
+
+	mutex_init(&hci->control_mutex);
+
 	hci->base_regs = devm_platform_ioremap_resource(pdev, 0);
 	if (IS_ERR(hci->base_regs))
 		return PTR_ERR(hci->base_regs);
