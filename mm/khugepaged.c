openeuler-mirror/kernel

mm/hwpoison: return -EFAULT when copy fail in copy_mc_[user]_highpage()
hulk inclusion
category: feature
bugzilla: https://gitee.com/openeuler/kernel/issues/I9GCZS
CVE: NA

-------------------------------

If hardware errors are encountered during page copying, returning the bytes
not copied is not meaningful, and the caller cannot do any processing on
the remaining data. Returning -EFAULT is more reasonable, which represents
a hardware error encountered during the copying.

Signed-off-by: Tong Tiangen <tongtiangen@huawei.com>

mm/khugepaged.c
@@ -804,7 +804,7 @@ static int __collapse_huge_page_copy(pte_t *pte,
 			continue;
 		}
 		src_page = pte_page(pteval);
-		if (copy_mc_user_highpage(page, src_page, _address, vma) > 0) {
+		if (copy_mc_user_highpage(page, src_page, _address, vma)) {
 			result = SCAN_COPY_MC;
 			break;
 		}
@@ -2071,7 +2071,7 @@ xa_unlocked:
 			clear_highpage(hpage + (index % HPAGE_PMD_NR));
 			index++;
 		}
-		if (copy_mc_highpage(hpage + (page->index % HPAGE_PMD_NR), page) > 0) {
+		if (copy_mc_highpage(hpage + (page->index % HPAGE_PMD_NR), page)) {
 			result = SCAN_COPY_MC;
 			goto rollback;
 		}

jackYoung0915/anolis-cloud-kernel

ANCK ID: #IJSBBS
commit b5bbf422452cfd6827c973103dd5e840506a5d45 openEuler.
(cherry picked from commit b5bbf422452cfd6827c973103dd5e840506a5d45)
Conflicts:
	mm/khugepaged.c
[caused by commit 96ab411740eb ("mm: khugepaged: support shmem mTHP copy")]
Signed-off-by: Liu Jian <liujian56@huawei.com>

mm/khugepaged.c
@@ -941,7 +941,7 @@ static enum scan_result __collapse_huge_page_copy(pte_t *pte, struct folio *foli
 			continue;
 		}
 		src_page = pte_page(pteval);
-		if (copy_mc_user_highpage(page, src_page, src_addr, vma) > 0) {
+		if (copy_mc_user_highpage(page, src_page, src_addr, vma)) {
 			result = SCAN_COPY_MC;
 			break;
 		}
@@ -2435,7 +2435,7 @@ xa_unlocked:
 		}
 
 		for (i = 0; i < nr_pages; i++) {
-			if (copy_mc_highpage(dst, folio_page(folio, i)) > 0) {
+			if (copy_mc_highpage(dst, folio_page(folio, i))) {
 				result = SCAN_COPY_MC;
 				goto rollback;
 			}